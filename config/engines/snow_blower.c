ab412bec0fafb0a9b0e9c3f752ea3e5255c1409f
Andrey B <arro239@gmail.com>
2014-04-25 22:50:40 -0500

    ?

---
+++ config/engines/snow_blower.c
@@ -0,0 +1,14 @@
+/**
+ * @file    snow_blower.c
+ * @brief	Default configuration of a single-cylinder engine
+ *
+ * @date Sep 9, 2013
+ * @author Andrey Belomutskiy, (c) 2012-2014
+ */
+
+#include "main.h"
+
+#if EFI_ENGINE_SNOW_BLOWER
+
+
+#endif
