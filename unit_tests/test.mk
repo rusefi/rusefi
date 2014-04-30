080bf548f8e5b05fb68b789aa5d0f45a8f63db87
Andrey B <arro239@gmail.com>
2014-04-30 11:05:25 -0500

    manual sync

--- unit_tests/test.mk
+++ unit_tests/test.mk
@@ -1,5 +1,4 @@
 TEST_SRC_C = boards.c \
-	test_data_structures/test_engine_math.c \
 	test_basic_math/test_find_index.c \
 	test_basic_math/test_interpolation_3d.c \
 	test_idle_controller.c \
@@ -7,6 +6,7 @@ TEST_SRC_C = boards.c \
 	
 TEST_SRC_CPP = test_util.cpp \
 	test_data_structures/test_event_registry.cpp \
+	test_data_structures/test_engine_math.cpp \
 	test_trigger_decoder.cpp \
 	test_fuel_map.cpp \
 	test_signal_executor.cpp
