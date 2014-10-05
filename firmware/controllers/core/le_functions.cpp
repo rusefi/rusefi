275dfae4f8789d440ffa679de96bd15ed5735603
rusEfi <russianefi@gmail.com>
2014-10-05 16:03:04 -0500

    auto-sync

--- firmware/controllers/core/le_functions.cpp
+++ firmware/controllers/core/le_functions.cpp
@@ -9,6 +9,15 @@
 #include "le_functions.h"
 #include "allsensors.h"
 
+extern LENameOrdinalPair * LE_FIRST;
+
+static LENameOrdinalPair leRpm(LE_METHOD_RPM, "rpm");
+static LENameOrdinalPair leFan(LE_METHOD_FAN, "fan");
+static LENameOrdinalPair leCoolant(LE_METHOD_COOLANT, "coolant");
+static LENameOrdinalPair leFanOnSetting(LE_METHOD_FAN_ON_SETTING, "fan_on_setting");
+static LENameOrdinalPair leFanOffSetting(LE_METHOD_FAN_OFF_SETTING, "fan_off_setting");
+
+
 #if EFI_PROD_CODE || EFI_SIMULATOR
 float getLEValue(Engine *engine, le_action_e action) {
 	switch(action) {
--- firmware/controllers/core/logic_expression.cpp
+++ firmware/controllers/core/logic_expression.cpp
@@ -21,12 +21,6 @@ static LENameOrdinalPair leMore(LE_OPERATOR_MORE, ">");
 static LENameOrdinalPair leMoreOrEqual(LE_OPERATOR_MORE_OR_EQUAL, ">=");
 static LENameOrdinalPair leNot(LE_OPERATOR_NOT, "not");
 
-static LENameOrdinalPair leRpm(LE_METHOD_RPM, "rpm");
-static LENameOrdinalPair leFan(LE_METHOD_FAN, "fan");
-static LENameOrdinalPair leCoolant(LE_METHOD_COOLANT, "coolant");
-static LENameOrdinalPair leFanOnSetting(LE_METHOD_FAN_ON_SETTING, "fan_on_setting");
-static LENameOrdinalPair leFanOffSetting(LE_METHOD_FAN_OFF_SETTING, "fan_off_setting");
-
 LENameOrdinalPair::LENameOrdinalPair(le_action_e action, const char *name) {
 	this->action = action;
 	this->name = name;
