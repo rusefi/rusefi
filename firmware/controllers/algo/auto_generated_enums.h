0da4fac8b32556efd55b2e2e77a87cf9af89d0d6
rusEfi <russianefi@gmail.com>
2014-10-07 09:03:01 -0500

    auto-sync

---
+++ firmware/controllers/algo/auto_generated_enums.h
@@ -0,0 +1,8 @@
+#ifndef _A_H_HEADER_
+#define _A_H_HEADER_
+// auto-generated from../../firmware/controllers/algo/io_pins.h
+
+
+#include "io_pins.h"
+const char *getIo_pin_e(io_pin_e value);
+#endif /*_A_H_HEADER_ */
