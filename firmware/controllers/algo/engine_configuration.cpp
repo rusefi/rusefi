ece606f471666ea65be3ea5fc4592ac5df7428b2
rusEfi <russianefi@gmail.com>
2014-10-09 01:07:01 -0500

    auto-sync

--- firmware/controllers/algo/engine_configuration.cpp
+++ firmware/controllers/algo/engine_configuration.cpp
@@ -283,6 +283,9 @@ void setDefaultConfiguration(engine_configuration_s *engineConfiguration, board_
 	for (int i = 0; i < JOYSTICK_PIN_COUNT; i++) {
 		boardConfiguration->joystickPins[i] = GPIO_NONE;
 	}
+	for (int i = 0; i < LE_COMMAND_COUNT; i++) {
+		boardConfiguration->le_formulas[i][0] = 0;
+	}
 
 	boardConfiguration->idleValvePin = GPIOE_2;
 	boardConfiguration->idleValvePinMode = OM_DEFAULT;
