236b80546338ebe4e2f099bdda285204049e7696
rusEfi <russianefi@gmail.com>
2014-11-02 20:06:24 -0600

    auto-sync

---
+++ java_console/ui/src/com/rusefi/ui/widgets/DetachedSensor.java
@@ -0,0 +1,86 @@
+package com.rusefi.ui.widgets;
+
+import com.irnems.core.Sensor;
+import com.rusefi.io.CommandQueue;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * (c) Andrey Belomutskiy
+ * 11/2/14
+ */
+public class DetachedSensor {
+    private static final Collection<Sensor> MOCKABLE = Arrays.asList(Sensor.CLT, Sensor.AFR, Sensor.IAT, Sensor.MAF,
+            Sensor.TPS);
+
+    /**
+     * We need to trick the JSlider into displaying float values
+     */
+    private static final int _5_VOLTS_WITH_DECIMAL = 50;
+
+    private Sensor sensor;
+    private int width;
+    JPanel content = new JPanel(new BorderLayout());
+    JFrame frame;
+    JPanel mockControlPanel = new JPanel(new BorderLayout());
+
+    public DetachedSensor(Sensor sensor, int width) {
+        this.sensor = sensor;
+        this.width = width;
+        frame = new JFrame();
+        frame.setAlwaysOnTop(true);
+        onChange(sensor);
+    }
+
+    public void show(MouseEvent e) {
+        frame.setVisible(true);
+        frame.setLocation(e.getXOnScreen(), e.getYOnScreen());
+    }
+
+    public void onChange(Sensor sensor) {
+        this.sensor = sensor;
+        frame.setTitle(sensor.getName());
+        showMockControl();
+    }
+
+    public void showMockControl() {
+        mockControlPanel.removeAll();
+        boolean isMockable = MOCKABLE.contains(sensor);
+        if (isMockable) {
+            Component mockComponent = createMockVoltageSlider(sensor);
+            mockControlPanel.add(mockComponent);
+        }
+        UpDownImage.trueLayout(content);
+        int size = width;
+        int h = isMockable ? (int) (size * 1.5) : size;
+        frame.setSize(size, h);
+    }
+
+
+    private static Component createMockVoltageSlider(final Sensor sensor) {
+        /**
+         */
+        final JSlider slider = new JSlider(0, _5_VOLTS_WITH_DECIMAL);
+        slider.setLabelTable(SensorGauge.SLIDER_LABELS);
+        slider.setPaintLabels(true);
+        slider.setPaintTicks(true);
+        slider.setMajorTickSpacing(10);
+        slider.setMinorTickSpacing(5);
+
+        slider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                double value = slider.getValue() / 10.0;
+                CommandQueue.getInstance().write("set_mock_" + sensor.name().toLowerCase() + "_voltage " + value);
+            }
+        });
+
+        return slider;
+    }
+}
