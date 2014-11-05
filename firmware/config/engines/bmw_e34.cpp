8f695176c41116f43c580fd35a000320b97756c6
rusEfi <russianefi@gmail.com>
2014-11-05 16:03:24 -0600

    auto-sync

--- firmware/config/engines/bmw_e34.cpp
+++ firmware/config/engines/bmw_e34.cpp
@@ -3,6 +3,10 @@
  *
  * set_engine_type 25
  *
+ * Coil pairs: 1-6, 2-5, 3-4
+ *
+ * Injector groups: 1,2,3 è 4,5,6
+ *
  * @date Nov 5, 2014
  * @author Andrey Belomutskiy, (c) 2012-2014
  */
@@ -16,5 +20,8 @@ void setBmwE43(engine_configuration_s *engineConfiguration) {
 
 	engineConfiguration->triggerConfig.triggerType = TT_TOOTHED_WHEEL_60_2;
 	engineConfiguration->cylindersCount = 6;
+	engineConfiguration->firingOrder = FO_1_THEN_5_THEN_3_THEN_6_THEN_2_THEN_4;
+	engineConfiguration->injectionMode = IM_BATCH;
+	engineConfiguration->ignitionMode = IM_WASTED_SPARK;
 
 }
