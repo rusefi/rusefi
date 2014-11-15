b3cbb8bc444a9ec057564d955d60eb56c55f2c72
rusEfi <russianefi@gmail.com>
2014-11-14 20:03:06 -0600

    auto-sync

---
+++ firmware/config/engines/test_engine.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file	test_engine.cpp
+ *
+ * @date Nov 14, 2014
+ * @author Andrey Belomutskiy, (c) 2012-2014
+ */
+
+#include "main.h"
+#include "test_engine.h"
+
+void setTestEngineConfiguration(engine_configuration_s *engineConfiguration) {
+
+}
