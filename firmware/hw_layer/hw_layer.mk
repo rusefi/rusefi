a6b9ec0f9300b56c0bd9994128415651aca191dc
rusEfi <russianefi@gmail.com>
2014-12-05 10:05:25 -0600

    auto-sync

--- firmware/hw_layer/hw_layer.mk
+++ firmware/hw_layer/hw_layer.mk
@@ -4,7 +4,6 @@ HW_LAYERSRC = $(PROJECT_DIR)/hw_layer/pin_repository.c \
 	$(PROJECT_DIR)/hw_layer/mcp3208.c \
 	$(PROJECT_DIR)/hw_layer/max31855.c \
 	$(PROJECT_DIR)/hw_layer/lcd/lcd_HD44780.c \
-	$(PROJECT_DIR)/hw_layer/HIP9011.c \
 	$(PROJECT_DIR)/hw_layer/microsecond_timer.c \
 	$(PROJECT_DIR)/hw_layer/serial_over_usb/usbcfg.c \
 	$(PROJECT_DIR)/hw_layer/serial_over_usb/usbconsole.c \
@@ -21,5 +20,6 @@ HW_LAYER_SRC_CPP = $(PROJECT_DIR)/hw_layer/hardware.cpp \
 	$(PROJECT_DIR)/hw_layer/board_test.cpp \
 	$(PROJECT_DIR)/hw_layer/pwm_generator.cpp \
 	$(PROJECT_DIR)/hw_layer/trigger_input.cpp \
+	$(PROJECT_DIR)/hw_layer/HIP9011.cpp \
 	$(PROJECT_DIR)/hw_layer/stm32f4/mpu_util.cpp
 	
