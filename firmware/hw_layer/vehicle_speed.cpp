265d8889fb3fc4b0ebfeb9382f7a70474d41f8dd
rusEfi <russianefi@gmail.com>
2014-12-26 23:05:52 -0600

    auto-sync

---
+++ firmware/hw_layer/vehicle_speed.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file vehicle_speed.cpp
+ *
+ * @date Dec 26, 2014
+ * @author Andrey Belomutskiy, (c) 2012-2014
+ */
+
+#include "vehicle_speed.h"
+
+void initVehicleSpeed(void) {
+
+}
+
---
+++ firmware/hw_layer/vehicle_speed.h
@@ -0,0 +1,15 @@
+/**
+ * @file vehicle_speed.h
+ *
+ *@date Dec 26, 2014
+ * @author Andrey Belomutskiy, (c) 2012-2014
+ */
+
+#ifndef HW_LAYER_VEHICLE_SPEED_H_
+#define HW_LAYER_VEHICLE_SPEED_H_
+
+#include "main.h"
+
+void initVehicleSpeed(void);
+
+#endif /* HW_LAYER_VEHICLE_SPEED_H_ */
