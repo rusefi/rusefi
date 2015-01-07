3186ff017fb3d3fba85d5424052a1e0bbdb8429c
rusEfi <russianefi@gmail.com>
2015-01-07 10:04:42 -0600

    auto-sync

--- firmware/config/stm32f1egt/mcuconf.h
+++ firmware/config/stm32f1egt/mcuconf.h
@@ -192,7 +192,7 @@
 /*
  * USB driver system settings.
  */
-#define STM32_USB_USE_USB1                  FALSE
+#define STM32_USB_USE_USB1                  TRUE
 #define STM32_USB_LOW_POWER_ON_SUSPEND      FALSE
 #define STM32_USB_USB1_HP_IRQ_PRIORITY      13
 #define STM32_USB_USB1_LP_IRQ_PRIORITY      14
