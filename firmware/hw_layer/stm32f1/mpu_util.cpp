6d2dcb9845e6c58df917ae1ddcb9966da8617eb2
rusEfi <russianefi@gmail.com>
2015-01-09 23:03:46 -0600

    auto-sync

---
+++ firmware/hw_layer/stm32f1/mpu_util.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file	mpu_util.cpp
+ *
+ * @date Jan 9, 2015
+ * @author Andrey Belomutskiy, (c) 2012-2014
+ */
+
+#include "mpu_util.h"
+
+void initSpiCs(SPIConfig *spiConfig, brain_pin_e csPin) {
+
+}
+
