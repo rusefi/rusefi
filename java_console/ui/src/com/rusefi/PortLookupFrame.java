4bdd901f9af63eb263c941bfec0bc6ea7f646971
rusEfi <russianefi@gmail.com>
2015-01-16 18:03:56 -0600

    auto-sync

--- java_console/ui/src/com/rusefi/PortLookupFrame.java
+++ java_console/ui/src/com/rusefi/PortLookupFrame.java
@@ -24,7 +24,7 @@ import java.util.List;
  */
 public class PortLookupFrame {
 
-    public static final String RUS_EFI_C_2012_2014 = "rusEfi (c) 2012-2014";
+    public static final String RUS_EFI_C_2012_2014 = "rusEfi (c) 2012-2015";
     public static final String URI = "http://rusefi.com/?java_console";
 
     public static void chooseSerialPort() {
