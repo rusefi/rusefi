75cbd13d13b3178c8f7a5f2fc92035b295ac0361
rusEfi <russianefi@gmail.com>
2015-01-18 08:03:52 -0600

    auto-sync

---
+++ java_console/models/src/com/rusefi/trigger/MiataNA.java
@@ -0,0 +1,12 @@
+package com.rusefi.trigger;
+
+/**
+ * (c) Andrey Belomutskiy
+ * 1/18/2015
+ */
+public class MiataNA implements TriggerShape {
+    @Override
+    public int getTotalToothCount() {
+        return 6;
+    }
+}
---
+++ java_console/models/src/com/rusefi/trigger/TriggerShapeHolder.java
@@ -0,0 +1,9 @@
+package com.rusefi.trigger;
+
+/**
+ * (c) Andrey Belomutskiy
+ * 1/18/2015
+ */
+public class TriggerShapeHolder {
+    public static TriggerShape CURRENT = new MiataNA();
+}
