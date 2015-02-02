49b6338a096165954e0ad99661bc91efe2db5416
rusEfi <russianefi@gmail.com>
2015-02-02 12:06:45 -0600

    auto-sync

--- firmware/controllers/flash_main.h
+++ firmware/controllers/flash_main.h
@@ -12,7 +12,7 @@
 #include "engine_configuration.h"
 #include "engine.h"
 
-#define FLASH_DATA_VERSION 6395
+#define FLASH_DATA_VERSION 6667
 
 void readFromFlash(void);
 void initFlash(Logging *sharedLogger, Engine *engine);
