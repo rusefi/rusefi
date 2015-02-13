04f6e3f0249e92cfec613c28d99178b58078954d
rusEfi <russianefi@gmail.com>
2015-02-13 14:04:38 -0600

    auto-sync

--- unit_tests/test_basic_math/test_find_index.cpp
+++ unit_tests/test_basic_math/test_find_index.cpp
@@ -11,66 +11,59 @@
 #include <stdio.h>
 #include "engine_configuration.h"
 
+static int testIndex(const int expected, const float array[], int size, float value) {
+	assertEquals(expected, findIndex(array, size, value));
+	assertEquals(expected, findIndex2(array, size, value));
+}
+
 void testFindIndex(void) {
 	printf("*************************************************** testFindIndex\r\n");
-	int result;
-
+/*
 	{
 		// in real life we only use powers of 2 for sizes
 		float array5[] = { 1, 2, 3, 4, 5 };
 		const int size5 = 5;
 		printf("To the right5\r\n");
-		result = findIndex(array5, size5, 10.0);
-		assertEquals(4, result);
+		testIndex(4, array5, size5, 10.0);
 
 		printf("Middle2\r\n");
-		result = findIndex(array5, size5, 4);
-		assertEquals(3, result);
+		testIndex(3, array5, size5, 4);
 
 		printf("Middle2\r\n");
-		result = findIndex(array5, size5, 3.1);
-		assertEquals(2, result);
+		testIndex(2, array5, size5, 3.1);
 
 	}
+*/
 	{
 		float array4[] = { 1, 2, 3, 4 };
 		const int size4 = 4;
 
 		printf("To the left\r\n");
-		result = findIndex(array4, size4, -1.0);
-		assertEquals(-1, result);
+		testIndex(-1, array4, size4, -1.0);
 
 		printf("To the right4\r\n");
-		result = findIndex(array4, size4, 10.0);
-		assertEquals(3, result);
+		testIndex(3, array4, size4, 10.0);
 
 		printf("On the edge\r\n");
-		result = findIndex(array4, size4, 4.0);
-		assertEquals(3, result);
+		testIndex(3, array4, size4, 4.0);
 
 		printf("Another1\r\n");
-		result = findIndex(array4, size4, 3.9);
-		assertEquals(2, result);
+		testIndex(2, array4, size4, 3.9);
 
 		printf("Another2\r\n");
-		result = findIndex(array4, size4, 4.1);
-		assertEquals(3, result);
+		testIndex(3, array4, size4, 4.1);
 
 		printf("Another3\r\n");
-		result = findIndex(array4, size4, 2);
-		assertEquals(1, result);
+		testIndex(1, array4, size4, 2);
 
 		printf("Left edge1\r\n");
-		result = findIndex(array4, size4, 1);
-		assertEquals(0, result);
+		testIndex(0, array4, size4, 1);
 
 		printf("Left edge2\r\n");
-		result = findIndex(array4, size4, 1.1);
-		assertEquals(0, result);
+		testIndex(0, array4, size4, 1.1);
 
 		printf("Middle\r\n");
-		result = findIndex(array4, size4, 3);
-		assertEquals(2, result);
+		testIndex(2, array4, size4, 3);
 	}
 }
 
