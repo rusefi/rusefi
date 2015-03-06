2d8ed8cce2484d08bafced8262afadbe4a5f2db9
rusEfi <russianefi@gmail.com>
2015-03-06 16:11:10 -0600

    auto-sync

---
+++ java_console/io/src/com/rusefi/binaryprotocol/CRC.java
@@ -0,0 +1,15 @@
+package com.rusefi.binaryprotocol;
+
+import java.util.zip.CRC32;
+
+/**
+ * (c) Andrey Belomutskiy
+ * 3/6/2015
+ */
+public class CRC {
+    public static int crc32(byte buf[], int size) {
+        CRC32 c = new CRC32();
+        c.update(buf, 0, size);
+        return (int) c.getValue();
+    }
+}
