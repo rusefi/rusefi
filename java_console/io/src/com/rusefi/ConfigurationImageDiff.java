bd1ac1e94b4aeed09091309d20eb116e267faca7
rusEfi <russianefi@gmail.com>
2015-03-07 15:04:54 -0600

    auto-sync

---
+++ java_console/io/src/com/rusefi/ConfigurationImageDiff.java
@@ -0,0 +1,34 @@
+package com.rusefi;
+
+import com.rusefi.core.Pair;
+
+/**
+ * (c) Andrey Belomutskiy
+ * 3/6/2015
+ */
+public class ConfigurationImageDiff {
+    public static Pair<Integer, Integer> findDifferences(ConfigurationImage image1, ConfigurationImage image2, int position) {
+        byte[] c1 = image1.getContent();
+        byte[] c2 = image2.getContent();
+        int length = c1.length;
+        if (length != c2.length)
+            throw new IllegalArgumentException("Length mismatch");
+
+        while (position < length && c1[position] == c2[position])
+            position++;
+        if (position == length)
+            return null; // no difference
+
+        int startOfDiff = position;
+        int prevDiff = position;
+
+        while (position - prevDiff < 4 && position < length) {
+            if (c1[position] != c2[position]) {
+                prevDiff = position;
+            }
+            position++;
+        }
+
+        return new Pair<>(startOfDiff, position);
+    }
+}
---
+++ java_console/io/src/com/rusefi/test/ConfigurationImageDiffTest.java
@@ -0,0 +1,42 @@
+package com.rusefi.test;
+
+import com.rusefi.ConfigurationImage;
+import com.rusefi.core.Pair;
+import org.junit.Test;
+
+import static com.rusefi.ConfigurationImageDiff.findDifferences;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * (c) Andrey Belomutskiy
+ * 3/6/2015
+ */
+public class ConfigurationImageDiffTest {
+    @Test
+    public void testCompare() {
+        {
+            byte[] data1 = {1, 2, 3, 4, 5, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6};
+            byte[] data2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 1, 2, 3, 4, 5, 6};
+            Pair<Integer, Integer> p = findDifferences(new ConfigurationImage(data1), new ConfigurationImage(data2), 0);
+            assertNotNull(p);
+            assertEquals(5, (int) p.first);
+            assertEquals(15, (int) p.second);
+        }
+        {
+            byte[] data1 = {1, 2, 3, 4, 5, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6};
+            byte[] data2 = {1, 2, 3, 4, 5, 6, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6};
+            Pair<Integer, Integer> p = findDifferences(new ConfigurationImage(data1), new ConfigurationImage(data2), 0);
+            assertNotNull(p);
+            assertEquals(5, (int) p.first);
+            assertEquals(9, (int) p.second);
+        }
+        {
+            byte[] data1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 1, 0, 0, 0, 0, 0};
+            byte[] data2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 1, 2, 3, 4, 5, 6};
+            Pair<Integer, Integer> p = findDifferences(new ConfigurationImage(data1), new ConfigurationImage(data2), 0);
+            assertEquals(13, (int) p.first);
+            assertEquals(18, (int) p.second);
+        }
+    }
+}
