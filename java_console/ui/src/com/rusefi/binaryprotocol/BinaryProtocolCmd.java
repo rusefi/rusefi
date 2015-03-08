207c712ca9cba081056d1508b799477476155cda
rusEfi <russianefi@gmail.com>
2015-03-08 13:04:49 -0500

    auto-sync

--- java_console/ui/src/com/rusefi/binaryprotocol/BinaryProtocolCmd.java
+++ java_console/ui/src/com/rusefi/binaryprotocol/BinaryProtocolCmd.java
@@ -1,22 +1,20 @@
 package com.rusefi.binaryprotocol;
 
-import com.romraider.Settings;
 import com.romraider.editor.ecu.ECUEditor;
 import com.romraider.util.SettingsManager;
 import com.rusefi.ConfigurationImage;
 import com.rusefi.Logger;
 import com.rusefi.RomRaiderWrapper;
+import com.rusefi.UploadChanges;
 import com.rusefi.io.serial.PortHolder;
 import jssc.SerialPort;
-import jssc.SerialPortException;
-
-import java.io.IOException;
 
 /**
  * (c) Andrey Belomutskiy
  * 3/6/2015
  */
 public class BinaryProtocolCmd {
+    static BinaryProtocol bp;
 
     public static void main(String[] args) throws Exception {
         if (args.length != 1) {
@@ -26,7 +24,7 @@ public class BinaryProtocolCmd {
 
         String port = args[0];
 
-        Logger logger = Logger.STDOUT;
+        Logger logger = UploadChanges.logger;
 
         SerialPort serialPort;
         serialPort = new SerialPort(port);
@@ -34,15 +32,14 @@ public class BinaryProtocolCmd {
         if (!opened) {
             logger.error("failed to open " + port);
         }
-        BinaryProtocol bp = new BinaryProtocol(logger, serialPort);
+        bp = new BinaryProtocol(logger, serialPort);
 
         PortHolder.setupPort(serialPort, 38400);
         logger.info("Looks good");
         bp.exchange(new byte[]{'S'});
 
-        ConfigurationImage image = new ConfigurationImage(14008);
-
-        bp.readImage(image);
+        bp.readImage(14008);
+        ConfigurationImage image = bp.getController();
 
         image.saveToFile("rusefi_configuration.bin");
 
@@ -53,4 +50,7 @@ public class BinaryProtocolCmd {
 
     }
 
+    public static void scheduleBurn(ConfigurationImage newVersion) {
+        UploadChanges.scheduleBurn(newVersion, bp);
+    }
 }
