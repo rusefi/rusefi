6ef0ca0e676bbce92dad05e2031b2a6a355fcc3d
rusEfi <russianefi@gmail.com>
2015-04-09 23:12:35 -0500

    auto-sync

--- firmware/config/engines/gm_2_2.cpp
+++ firmware/config/engines/gm_2_2.cpp
@@ -1,6 +1,8 @@
 /*
  * @file gm_2_2.cpp
  *
+ * set_engine_type 30
+ *
  * @date Apr 9, 2015
  * @author Andrey Belomutskiy, (c) 2012-2015
  */
@@ -8,7 +10,13 @@
 #include "global.h"
 #include "gm_2_2.h"
 
+EXTERN_ENGINE;
+
 void setGm2_2(DECLARE_ENGINE_PARAMETER_F) {
+	board_configuration_s * boardConfiguration = &engineConfiguration->bc;
+
+	boardConfiguration->triggerInputPins[0] = GPIOC_6;
+	boardConfiguration->triggerInputPins[1] = GPIOA_8;
 
 }
 
