ec2b361f1f593315b36eb3e52666fc4a5562652b
rusEfi <russianefi@gmail.com>
2015-07-10 08:49:44 -0400

    auto-sync

--- rusefi/controllers/math/math.mk
+++
@@ -1,6 +0,0 @@
-
-CONTROLLERS_MATH_SRC = 
-
-CONTROLLERS_MATH_SRC_CPP = $(PROJECT_DIR)/controllers/math/engine_math.cpp \
-	$(PROJECT_DIR)/controllers/math/pid.cpp \
-	$(PROJECT_DIR)/controllers/math/speed_density.cpp
--- rusefi/flash_erase.bat
+++
@@ -1,4 +0,0 @@
-rem st-link_cli -c SWD ur -ME
-rem 0x080000 would erase both code and configuration
-rem use 0x040000 if you want to erase only configuration
-openocd-0.8.0.exe -f interface/stlink-v2.cfg -f board/stm32f4discovery.cfg -c init -c targets -c "halt" -c "flash erase_address 0x08000000 0x080000" -c shutdown
