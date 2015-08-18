02f764f6522255c083703e913441db1aea9f8dbb
rusEfi <russianefi@gmail.com>
2015-08-18 17:01:49 -0400

    auto-sync

--- firmware/controllers/tachometer.cpp
+++ firmware/controllers/tachometer.cpp
@@ -1,16 +1,29 @@
 /*
  * @file tachometer.cpp
+ * @brief This is about driving external analog tachometers
  *
  * @date Aug 18, 2015
  * @author Andrey Belomutskiy, (c) 2012-2015
  */
 
 #include "tachometer.h"
+#include "trigger_central.h"
 
 EXTERN_ENGINE;
+
+static OutputPin tachOut;
+
+static void tachSignalCallback(trigger_event_e ckpSignalType,
+		uint32_t index DECLARE_ENGINE_PARAMETER_S) {
+}
 
 void initTachometer(void) {
 	if (boardConfiguration->tachOutputPin == GPIO_UNASSIGNED)
 		return;
+
+	outputPinRegisterExt2("tach", &tachOut, boardConfiguration->tachOutputPin, &boardConfiguration->tachOutputPinMode);
+
+	addTriggerEventListener(tachSignalCallback, "tach", engine);
+
 
 }
