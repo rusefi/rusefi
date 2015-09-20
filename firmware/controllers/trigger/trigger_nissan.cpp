c2c6d4444cac32655e1dea8e876d9ff8edc2f60c
rusEfi <russianefi@gmail.com>
2015-09-19 20:01:38 -0400

    auto-sync

--- firmware/controllers/trigger/trigger_nissan.cpp
+++ firmware/controllers/trigger/trigger_nissan.cpp
@@ -8,5 +8,20 @@
 #include "trigger_nissan.h"
 
 void initializeNissan(TriggerShape *s) {
+	s->reset(FOUR_STROKE_CAM_SENSOR, false);
+	s->isSynchronizationNeeded = false;
+
+	float w = 5;
+
+	float base = 180;
+
+	base += 180;
+	// todo
+	base += 180;
+	// todo
+	base += 180;
+
+	s->addEvent(base - w, T_PRIMARY, TV_HIGH);
+	s->addEvent(base, T_PRIMARY, TV_LOW);
 
 }
