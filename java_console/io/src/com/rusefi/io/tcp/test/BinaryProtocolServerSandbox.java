9e80b5a57f3ada3ccd4d172b818af9f0af90cac8
rusEfi <russianefi@gmail.com>
2015-11-25 09:01:57 -0500

    auto-sync

---
+++ java_console/io/src/com/rusefi/io/tcp/test/BinaryProtocolServerSandbox.java
@@ -0,0 +1,9 @@
+package com.rusefi.io.tcp.test;
+
+import com.rusefi.io.tcp.BinaryProtocolServer;
+
+class BinaryProtocolServerSandbox {
+    public static void main(String[] args) {
+        BinaryProtocolServer.start();
+    }
+}
