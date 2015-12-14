b0bf4a7c08ef56c51b3b20775f8834d0b3b0937a
rusEfi <russianefi@gmail.com>
2015-12-14 13:01:18 -0500

    auto-sync

---
+++ firmware/controllers/trigger/trigger_toyota.cpp
@@ -0,0 +1,12 @@
+/*
+ * @file trigger_toyota.cpp
+ *
+ * https://thedeltaecho.wordpress.com/2010/03/14/2jz-ge-cam-crank-signals/
+ *
+ * @date Dec 14, 2015
+ * @author Andrey Belomutskiy, (c) 2012-2015
+ */
+
+#include "trigger_toyota.h"
+
+
