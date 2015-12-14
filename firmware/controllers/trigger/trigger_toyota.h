9bd55618beca5537d19d403e911934297d9a9564
rusEfi <russianefi@gmail.com>
2015-12-14 13:01:18 -0500

    auto-sync

---
+++ firmware/controllers/trigger/trigger_toyota.h
@@ -0,0 +1,15 @@
+/*
+ * @file trigger_toyota.h
+ *
+ * @date Dec 14, 2015
+ * @author Andrey Belomutskiy, (c) 2012-2015
+ */
+
+#ifndef CONTROLLERS_TRIGGER_TRIGGER_TOYOTA_H_
+#define CONTROLLERS_TRIGGER_TRIGGER_TOYOTA_H_
+
+#include "engine.h"
+
+void initialize2jzGE(TriggerShape *s);
+
+#endif /* CONTROLLERS_TRIGGER_TRIGGER_TOYOTA_H_ */
