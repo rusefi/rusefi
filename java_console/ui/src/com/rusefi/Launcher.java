914be6d251d30d440f8f89658d4960e3227a88a4
rusEfi <russianefi@gmail.com>
2015-12-26 09:01:32 -0500

    auto-sync

--- java_console/ui/src/com/rusefi/Launcher.java
+++ java_console/ui/src/com/rusefi/Launcher.java
@@ -32,7 +32,7 @@ import static com.rusefi.ui.storage.PersistentConfiguration.getConfig;
  * @see EngineSnifferPanel
  */
 public class Launcher {
-    public static final int CONSOLE_VERSION = 20151224;
+    public static final int CONSOLE_VERSION = 20151226;
     public static final boolean SHOW_STIMULATOR = false;
     private static final String TAB_INDEX = "main_tab";
     protected static final String PORT_KEY = "port";
