fc72230ea276b28c08e3b1bd83b1bb199e4ca27a
rusEfi <russianefi@gmail.com>
2016-01-19 23:02:48 -0500

    auto-sync

--- java_console/ui/src/com/rusefi/ui/config/DialogModel.java
+++ java_console/ui/src/com/rusefi/ui/config/DialogModel.java
@@ -8,11 +8,26 @@ import java.util.List;
  * 1/17/2016.
  */
 public class DialogModel {
-    private final String name;
+    private final String key;
+    private final String uiName;
     private final List<String> fields;
 
-    public DialogModel(String name, List<String> fields) {
-        this.name = name;
+    public DialogModel(String key, String uiName, List<String> fields) {
+        this.key = key;
+        this.uiName = uiName;
         this.fields = new ArrayList<>(fields);
+    }
+
+    public List<String> getFields() {
+        return fields;
+    }
+
+    @Override
+    public String toString() {
+        return "DialogModel{" +
+                "key='" + key + '\'' +
+                ", uiName='" + uiName + '\'' +
+                ", fields=" + fields.size() +
+                '}';
     }
 }
