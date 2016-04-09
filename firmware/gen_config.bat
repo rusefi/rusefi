2807b09b891c9fcc763cd794d99aab283c7aec4a
rusEfi <russianefi@gmail.com>
2016-04-09 12:02:01 -0400

    auto-sync

--- firmware/gen_config.bat
+++ firmware/gen_config.bat
@@ -1,2 +1,4 @@
+rem This batch files reads rusefi_config.txt and produses firmware persistent configuration headers
+rem the storage section of rusefi.ini is updated as well
 
 java -jar ../java_tools/ConfigDefinition.jar integration tunerstudio controllers\algo ../java_console
