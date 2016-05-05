233bf3ee5b1ad7ac14a49629a5b626382281d99a
rusEfi <russianefi@gmail.com>
2016-05-04 23:02:32 -0400

    auto-sync

---
+++ firmware/config/engines/zil130.cpp
@@ -0,0 +1,17 @@
+/*
+ * @file zil130.cpp
+ *
+ * http://rusefi.com/forum/viewtopic.php?f=15&t=644
+ *
+ * @date May 4, 2016
+ * @author Andrey Belomutskiy, (c) 2012-2016
+ */
+
+#include "engine_template.h"
+#include "custom_engine.h"
+
+EXTERN_ENGINE;
+
+void setZil130(DECLARE_ENGINE_PARAMETER_F) {
+	setCustomEngineConfiguration(PASS_ENGINE_PARAMETER_F);
+}
