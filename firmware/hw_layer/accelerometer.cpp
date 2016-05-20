090c3c55f4a9cd2951f9d0d62fa26021c4ecdee2
rusEfi <russianefi@gmail.com>
2016-05-19 22:03:18 -0400

    auto-sync

---
+++ firmware/hw_layer/accelerometer.cpp
@@ -0,0 +1,12 @@
+/*
+ * accelerometer.cpp
+ *
+ * @date May 19, 2016
+ * @author Andrey Belomutskiy, (c) 2012-2016
+ */
+
+#include "main.h"
+#include "lis302dl.h"
+
+
+
--- firmware/hw_layer/hw_layer.mk
+++ firmware/hw_layer/hw_layer.mk
@@ -29,5 +29,6 @@ HW_LAYER_EMS_CPP = $(HW_LAYER_EGT_CPP) \
 	$(PROJECT_DIR)/hw_layer/gpio_helper.cpp \
 	$(PROJECT_DIR)/hw_layer/stm32f4/mpu_util.cpp \
 	$(PROJECT_DIR)/hw_layer/rtc_helper.cpp \
+	$(PROJECT_DIR)/hw_layer/accelerometer.cpp \
 	$(PROJECT_DIR)/hw_layer/wbo.cpp
 	
