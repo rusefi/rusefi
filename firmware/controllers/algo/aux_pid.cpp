dd13e6ba89d89aa1f08da679904c3a48e9257859
rusEfi <russianefi@gmail.com>
2016-06-26 13:02:23 -0400

    auto-sync

---
+++ firmware/controllers/algo/aux_pid.cpp
@@ -0,0 +1,13 @@
+/*
+ * @file aux_pid.cpp
+ *
+ * @date Jun 26, 2016
+ * @author Andrey Belomutskiy, (c) 2012-2016
+ */
+
+#include "aux_pid.h"
+
+void initAuxPid(Logging *sharedLogger) {
+
+}
+
---
+++ firmware/controllers/algo/aux_pid.h
@@ -0,0 +1,15 @@
+/*
+ * @file aux_pid.h
+ *
+ * @date Jun 26, 2016
+ * @author Andrey Belomutskiy, (c) 2012-2016
+ */
+
+#ifndef CONTROLLERS_ALGO_AUX_PID_H_
+#define CONTROLLERS_ALGO_AUX_PID_H_
+
+#include "engine.h"
+
+void initAuxPid(Logging *sharedLogger);
+
+#endif /* CONTROLLERS_ALGO_AUX_PID_H_ */
