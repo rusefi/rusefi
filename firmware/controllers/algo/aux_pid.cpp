a8836cfc30f10ee0f1d93941c9c5203d4d3fefa2
rusEfi <russianefi@gmail.com>
2016-06-30 22:02:49 -0400

    auto-sync

--- firmware/controllers/algo/aux_pid.cpp
+++ firmware/controllers/algo/aux_pid.cpp
@@ -6,8 +6,10 @@
  */
 
 #include "aux_pid.h"
+#include "LocalVersionHolder.h"
 
 #if EFI_AUX_PID || defined(__DOXYGEN__)
+#include "pwm_generator.h"
 
 EXTERN_ENGINE
 ;
@@ -17,12 +19,22 @@ EXTERN_ENGINE
 
 static THD_WORKING_AREA(auxPidThreadStack, UTILITY_THREAD_STACK_SIZE);
 
+static LocalVersionHolder parametersVersion;
+static SimplePwm auxPid1;
+static OutputPin auxPid1Pin;
+static pid_s *altPidS = &persistentState.persistentConfiguration.engineConfiguration.alternatorControl;
+static Pid altPid(altPidS, 1, 90);
+
 static msg_t auxPidThread(int param) {
 	UNUSED(param);
 		chRegSetThreadName("AuxPidController");
 		while (true) {
 			int dt = maxI(10, engineConfiguration->auxPid1DT);
 			chThdSleepMilliseconds(dt);
+
+			if (parametersVersion.isOld())
+				altPid.reset();
+
 
 
 		}
@@ -35,6 +47,17 @@ void initAuxPid(Logging *sharedLogger) {
 	chThdCreateStatic(auxPidThreadStack, sizeof(auxPidThreadStack), LOWPRIO,
 			(tfunc_t) auxPidThread, NULL);
 
+	if (engineConfiguration->activateAuxPid1) {
+		return;
+	}
+
+	if (boardConfiguration->fsioPins[0] == GPIO_UNASSIGNED) {
+		return;
+	}
+
+	startSimplePwmExt(&auxPid1, "Aux PID", boardConfiguration->fsioPins[0],
+			&auxPid1Pin,
+			boardConfiguration->fsioFrequency[0], 0.1, applyPinState);
 }
 
 #endif
