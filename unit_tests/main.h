83e99bff750098bebeb12ecacd89e1a0f59fc060
rusEfi <russianefi@gmail.com>
2016-09-03 12:03:10 -0400

    auto-sync

--- unit_tests/main.h
+++ unit_tests/main.h
@@ -45,6 +45,7 @@ void print(const char *fmt, ...);
 void assertEqualsM2(const char *msg, float expected, float actual, float EPS);
 void assertEqualsM(const char *msg, float expected, float actual);
 void assertEqualsLM(const char *msg, long expected, long actual);
+void assertEqualsM4(const char *prefix, const char *msg, float expected, float actual);
 void assertEquals(float expected, float actual);
 void assertTrue(float actual);
 void assertTrueM(const char *msg, float actual);
