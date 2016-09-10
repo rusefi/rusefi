c78df4e9845f9d91c99288c8e52c8db80f63487d
rusEfi <russianefi@gmail.com>
2016-09-10 16:03:27 -0400

    auto-sync

---
+++ firmware/controllers/math/biquad.h
@@ -0,0 +1,20 @@
+/*
+ * @file biquad.h
+ *
+ * @date Sep 10, 2016
+ * @author Andrey Belomutskiy, (c) 2012-2016
+ */
+
+#ifndef CONTROLLERS_MATH_BIQUAD_H_
+#define CONTROLLERS_MATH_BIQUAD_H_
+
+class Biquad {
+public:
+    Biquad();
+    float getValue(float input);
+
+    float a0, a1, a2, b1, b2;
+    float z1, z2;
+};
+
+#endif /* CONTROLLERS_MATH_BIQUAD_H_ */
