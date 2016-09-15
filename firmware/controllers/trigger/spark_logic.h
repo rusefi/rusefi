1afc239f3ba98ea445d8218bb22450a39f9d6bea
rusEfi <russianefi@gmail.com>
2016-09-15 18:02:36 -0400

    auto-sync

---
+++ firmware/controllers/trigger/spark_logic.h
@@ -0,0 +1,15 @@
+/*
+ * @file spark_logic.h
+ *
+ * @date Sep 15, 2016
+ * @author Andrey Belomutskiy, (c) 2012-2016
+ */
+
+#ifndef CONTROLLERS_TRIGGER_SPARK_LOGIC_H_
+#define CONTROLLERS_TRIGGER_SPARK_LOGIC_H_
+
+#include "engine.h"
+
+int isInjectionEnabled(engine_configuration_s *engineConfiguration);
+
+#endif /* CONTROLLERS_TRIGGER_SPARK_LOGIC_H_ */
