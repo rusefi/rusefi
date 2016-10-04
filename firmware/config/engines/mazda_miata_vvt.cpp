d2142321c7fd13a20c1ba3eb8f64e141334a7f79
rusEfi <russianefi@gmail.com>
2016-10-04 13:03:25 -0400

    auto-sync

---
+++ firmware/config/engines/mazda_miata_vvt.cpp
@@ -0,0 +1,12 @@
+/*
+ * @file	mazda_miata_vvt.cpp
+ *
+ * @date Oct 4, 2016
+ * @author Andrey Belomutskiy, (c) 2012-2016
+ * http://rusefi.com/forum/viewtopic.php?f=3&t=1095
+ */
+
+#include "mazda_miata_vvt.h"
+
+
+
---
+++ firmware/config/engines/mazda_miata_vvt.h
@@ -0,0 +1,17 @@
+/*
+ * @file	mazda_miata_vvt.h
+ *
+ * @date Oct 4, 2016
+ * @author Andrey Belomutskiy, (c) 2012-2016
+ *
+ * http://rusefi.com/forum/viewtopic.php?f=3&t=1095
+ */
+
+#ifndef CONFIG_ENGINES_MAZDA_MIATA_VVT_H_
+#define CONFIG_ENGINES_MAZDA_MIATA_VVT_H_
+
+
+
+
+
+#endif /* CONFIG_ENGINES_MAZDA_MIATA_VVT_H_ */
