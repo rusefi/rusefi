8ef8b1e902c545a268834185adb8e4cc81a9b9e9
rusEfi <russianefi@gmail.com>
2016-11-01 21:03:07 -0400

    auto-sync

--- firmware/controllers/trigger/spark_logic.h
+++ firmware/controllers/trigger/spark_logic.h
@@ -14,7 +14,7 @@ int isInjectionEnabled(engine_configuration_s *engineConfiguration);
 void handleSpark(int revolutionIndex, bool limitedSpark, uint32_t trgEventIndex, int rpm,
 		IgnitionEventList *list DECLARE_ENGINE_PARAMETER_S);
 void initSparkLogic(Logging *sharedLogger);
-void turnSparkPinHigh(IgnitionOutputPin *output);
-void turnSparkPinLow(IgnitionOutputPin *output);
+void turnSparkPinHigh(IgnitionEvent *event);
+void turnSparkPinLow(IgnitionEvent *event);
 
 #endif /* CONTROLLERS_TRIGGER_SPARK_LOGIC_H_ */
