3ffda1aecd368ee89d3fb78c25674ba746fc9f09
rusEfi <russianefi@gmail.com>
2016-12-22 08:02:29 -0500

    auto-sync

--- java_console/io/src/com/rusefi/binaryprotocol/BinaryProtocol.java
+++ java_console/io/src/com/rusefi/binaryprotocol/BinaryProtocol.java
@@ -424,9 +424,22 @@ public class BinaryProtocol {
 
     public static void sendCrcPacket(byte[] command, Logger logger, IoStream stream) throws IOException {
         byte[] packet = IoHelper.makeCrc32Packet(command);
-        logger.info("Sending " + Arrays.toString(packet));
+        logger.info("Sending packet " + printHexBinary(command));
         stream.write(packet);
     }
+
+    private static final char[] hexCode = "0123456789ABCDEF".toCharArray();
+
+    private static String printHexBinary(byte[] data) {
+        StringBuilder r = new StringBuilder(data.length * 2);
+        for (byte b : data) {
+            r.append(hexCode[(b >> 4) & 0xF]);
+            r.append(hexCode[(b & 0xF)]);
+            r.append(' ');
+        }
+        return r.toString();
+    }
+
 
     /**
      * This method blocks until a confirmation is received
