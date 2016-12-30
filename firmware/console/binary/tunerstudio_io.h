474b99ba604beb25b57a7eb03c9fa2e4a8509eec
rusEfi <russianefi@gmail.com>
2016-12-29 21:02:50 -0500

    auto-sync

--- firmware/console/binary/tunerstudio_io.h
+++ firmware/console/binary/tunerstudio_io.h
@@ -35,7 +35,7 @@ typedef enum {
 
 // see 'blockingFactor' in rusefi.ini todo: move to rusefi_config.txt
 // see BLOCKING_FACTOR in java console code
-#define BLOCKING_FACTOR 400
+#define BLOCKING_FACTOR 320
 
 typedef struct {
 	BaseChannel * channel;
@@ -44,7 +44,7 @@ typedef struct {
 	 * we use 'blockingFactor = 320' in rusefi.ini
 	 * todo: should we just do (320 + CRC_WRAPPING_SIZE) ?
 	 */
-	char crcReadBuffer[BLOCKING_FACTOR + 10];
+	char crcReadBuffer[BLOCKING_FACTOR + 30];
 } ts_channel_s;
 
 #define TS_HELLO_COMMAND_DEPRECATED 'H' // 0x48
