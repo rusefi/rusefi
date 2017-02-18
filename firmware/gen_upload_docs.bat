53351bfe31647f7ecea9c28cd1ae1cdfce89114c
rusEfi <russianefi@gmail.com>
2017-02-18 11:02:21 -0500

    auto-sync

--- firmware/gen_upload_docs.bat
+++ firmware/gen_upload_docs.bat
@@ -3,5 +3,5 @@ call generate_docs
 
 rem http://www.ncftp.com/download/
 cd ../doxygen
-ncftpput -u u71977750-docs -p docspass rusefi.com /html html/*
+ncftpput -u u71977750-docs -p %RUSEFI_DOXYGEN_FTP_PASS% rusefi.com /html html/*
 
