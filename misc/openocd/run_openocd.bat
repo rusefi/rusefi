6eb7b990fc62e45d4ae485ed32ede655484d0ab5
rusefi <rusefi@users.noreply.github.com>
2017-02-20 17:26:17 -0500

    manual sync

--- misc/openocd/run_openocd.bat
+++ misc/openocd/run_openocd.bat
@@ -1,1 +1,3 @@
-openocd -f interface/stlink-v2.cfg -f target/stm32f4x_stlink.cfg -c init -c reset
+rem if you have an older Rev C discovery please update to latest Rev D firmware
+
+openocd-0.8.0 -f interface/stlink-v2.cfg -f target/stm32f4x_stlink.cfg -c init -c reset
