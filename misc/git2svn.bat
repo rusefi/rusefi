2d3dc8a126bd68579b3251d964a759df40fd21e4
rusefi <rusefi@users.noreply.github.com>
2017-02-20 19:20:31 -0500

    progress

--- misc/git2svn.bat
+++ misc/git2svn.bat
@@ -17,11 +17,11 @@ rem rd /s /q %RUSEFI_GIT_PATH%\java_console
 rem rd /s /q %RUSEFI_GIT_PATH%\unit_tests
 rem rd /s /q %RUSEFI_GIT_PATH%\win32_functional_tests
 
-cp -r firmware %RUSEFI_SVN_PATH%
-cp -r hardware %RUSEFI_SVN_PATH%
-rem cp -r java_console %RUSEFI_GIT_PATH%
-rem cp -r unit_tests %RUSEFI_GIT_PATH%
-rem cp -r win32_functional_tests %RUSEFI_GIT_PATH%
+cp -r firmware               %RUSEFI_SVN_PATH%
+rem cp -r hardware %RUSEFI_SVN_PATH%
+cp -r java_console           %RUSEFI_SVN_PATH%
+cp -r unit_tests             %RUSEFI_SVN_PATH%
+cp -r win32_functional_tests %RUSEFI_SVN_PATH%
 rem rm -f %RUSEFI_GIT_PATH%/readme.*
 rem rm -f %RUSEFI_GIT_PATH%/README.*
 rem cp -r README.* %RUSEFI_GIT_PATH%
@@ -31,7 +31,7 @@ rem cp -r readme.* %RUSEFI_GIT_PATH%
 rem echo Going to git copy location
 
 
-rem cd %RUSEFI_GIT_PATH%
+cd %RUSEFI_SVN_PATH%
 
 rem git config --global user.email "russianefi@gmail.com"
 rem git config --global user.name "rusEfi"
@@ -40,3 +40,6 @@ rem git pull
 rem git add *
 rem git commit -a -m "auto-sync"
 rem git push --repo https://rusefi:PASSWORD@github.com/rusefi/rusefi
+
+
+svn ci -m "auto-merge from github"
