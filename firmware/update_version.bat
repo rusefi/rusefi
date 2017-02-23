6ea06c1105b339f102a31ca10ada09a1a2d5f224
rusefi <rusefi@users.noreply.github.com>
2017-02-22 21:40:43 -0500

    version offset for a better transition from SD to GH

--- firmware/update_version.bat
+++ firmware/update_version.bat
@@ -1,3 +1,3 @@
 rem Let's regemerate 'svnversion.h'
 rem TODO: handle std err - for example, in case svn needs upgrade
-java -jar ../java_tools/version2header.jar
+java -jar ../java_tools/version2header.jar 10000
