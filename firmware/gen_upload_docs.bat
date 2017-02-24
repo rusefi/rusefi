76225c271a37a44551e098ca22f41ca25cd32c2f
rusefi <rusefi@users.noreply.github.com>
2017-02-24 11:26:45 -0500

    progress

--- firmware/gen_upload_docs.bat
+++ firmware/gen_upload_docs.bat
@@ -1,7 +1,11 @@
 svn up
-call generate_docs
+doxygen
+IF NOT ERRORLEVEL echo doxygen run FAILED
+IF NOT ERRORLEVEL 0 EXIT /B 1
+
 
 rem http://www.ncftp.com/download/
+rem actually Cygwin http://rusefi.com/wiki/index.php?title=Internal:Software:Build_Server
 cd ../doxygen
 ncftpput -R -v -u u71977750-docs -p %RUSEFI_DOXYGEN_FTP_PASS% rusefi.com /html html/*
 
