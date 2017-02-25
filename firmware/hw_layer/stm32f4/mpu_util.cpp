a4b34423b2bfed3b7855bf6985c507dbc5d23059
rusefi <rusefi@users.noreply.github.com>
2017-02-25 00:12:11 -0500

    #364

--- firmware/hw_layer/stm32f4/mpu_util.cpp
+++ firmware/hw_layer/stm32f4/mpu_util.cpp
@@ -347,22 +347,22 @@ BOR_Result_t BOR_Set(BOR_Level_t BORValue) {
 	}
 
 
-	FLASH_OBProgramInitTypeDef FLASH_Handle;
-
-	FLASH_Handle.BORLevel = (uint32_t)BORValue;
-	FLASH_Handle.OptionType = OPTIONBYTE_BOR;
-
-	HAL_FLASH_OB_Unlock();
-
-	HAL_FLASHEx_OBProgram(&FLASH_Handle);
-
-	HAL_StatusTypeDef status = HAL_FLASH_OB_Launch();
-
-	HAL_FLASH_OB_Lock();
-
-	if (status != HAL_OK) {
-		return BOR_Result_Error;
-	}
+//	FLASH_OBProgramInitTypeDef FLASH_Handle;
+//
+//	FLASH_Handle.BORLevel = (uint32_t)BORValue;
+//	FLASH_Handle.OptionType = OPTIONBYTE_BOR;
+//
+//	HAL_FLASH_OB_Unlock();
+//
+//	HAL_FLASHEx_OBProgram(&FLASH_Handle);
+//
+//	HAL_StatusTypeDef status = HAL_FLASH_OB_Launch();
+//
+//	HAL_FLASH_OB_Lock();
+//
+//	if (status != HAL_OK) {
+//		return BOR_Result_Error;
+//	}
 
 	return BOR_Result_Ok;
 }
