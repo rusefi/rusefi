339cea9ea71c5fd8505f4069a29b3dba6be46015
rusefi <rusefi@users.noreply.github.com>
2017-03-01 19:21:19 -0500

    opensr5

--- java_console/ui/src/com/rusefi/Launcher.java
+++ java_console/ui/src/com/rusefi/Launcher.java
@@ -44,7 +44,7 @@ import static com.rusefi.ui.storage.PersistentConfiguration.getConfig;
  * @see EngineSnifferPanel
  */
 public class Launcher {
-    public static final int CONSOLE_VERSION = 20170222;
+    public static final int CONSOLE_VERSION = 20170301;
     public static final boolean SHOW_STIMULATOR = false;
     private static final String TAB_INDEX = "main_tab";
     protected static final String PORT_KEY = "port";
