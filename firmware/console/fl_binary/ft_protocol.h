752d1a958bfafcb33a6b897c87a6679afc2dca15
rusefi <rusefi@users.noreply.github.com>
2017-03-14 22:30:46 -0500

    progress #372

---
+++ firmware/console/fl_binary/ft_protocol.h
@@ -0,0 +1,15 @@
+/*
+ * @file ft_protocol.h
+ *
+ * @date Mar 14, 2017
+ * @author Andrey Belomutskiy, (c) 2012-2017
+ */
+
+#ifndef CONSOLE_FL_BINARY_FT_PROTOCOL_H_
+#define CONSOLE_FL_BINARY_FT_PROTOCOL_H_
+
+#define START_TAG 0xAA
+#define MAGIC_TAG 0xBB
+#define END_TAG 0xCC
+
+#endif /* CONSOLE_FL_BINARY_FT_PROTOCOL_H_ */
