5d2c32ecc9d3c96518efdbed4bf5bcf601c28cbb
rusefi <rusefi@users.noreply.github.com>
2017-03-19 18:49:27 -0400

    refactoring: const

--- firmware/controllers/engine_controller.cpp
+++ firmware/controllers/engine_controller.cpp
@@ -72,15 +72,15 @@ extern EnginePins enginePins;
 
 persistent_config_container_s persistentState CCM_OPTIONAL;
 
-persistent_config_s *config = &persistentState.persistentConfiguration;
+const persistent_config_s *config = &persistentState.persistentConfiguration;
 
 /**
  * todo: it really looks like these fields should become 'static', i.e. private
  * the whole 'extern ...' pattern is less then perfect, I guess the 'God object' Engine
  * would be a smaller evil. Whatever is needed should be passed into methods/modules/files as an explicit parameter.
  */
-engine_configuration_s *engineConfiguration = &persistentState.persistentConfiguration.engineConfiguration;
-board_configuration_s *boardConfiguration = &persistentState.persistentConfiguration.engineConfiguration.bc;
+const engine_configuration_s *engineConfiguration = &persistentState.persistentConfiguration.engineConfiguration;
+const board_configuration_s *boardConfiguration = &persistentState.persistentConfiguration.engineConfiguration.bc;
 
 /**
  * CH_FREQUENCY is the number of system ticks in a second
