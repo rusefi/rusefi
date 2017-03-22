a0e5107ba2613acf6d8202ef3c25f4b0b77331d6
Fabien Poussin <fabien.poussin@gmail.com>
2017-03-22 19:45:11 +0100

    Moving boards to config folder.

--- firmware/boards/ST_STM32F4_DISCOVERY/board.mk
+++
@@ -1,5 +0,0 @@
-# List of all the board related files.
-BOARDSRC = $(PROJECT_DIR)/boards/ST_STM32F4_DISCOVERY/board.c
-
-# Required include directories
-BOARDINC = $(PROJECT_DIR)/boards/ST_STM32F4_DISCOVERY
