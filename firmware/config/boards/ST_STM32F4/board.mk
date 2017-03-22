6172ae0d1c6e4e64e8dc91621f2dce8ea107140a
rusefi <rusefi@users.noreply.github.com>
2017-03-22 16:35:07 -0400

    #380

---
+++ firmware/config/boards/ST_STM32F4/board.mk
@@ -0,0 +1,5 @@
+# List of all the board related files.
+BOARDSRC = $(PROJECT_DIR)/config/boards/ST_STM32F4/board.c
+
+# Required include directories
+BOARDINC = $(PROJECT_DIR)/config/boards/ST_STM32F4
