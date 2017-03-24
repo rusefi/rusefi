2fbe8c52c376b8603263afe2e43f5580cee7712f
Fabien Poussin <fabien.poussin@gmail.com>
2017-03-24 13:55:32 +0100

    Fixed FatFS library compilation.

---
+++ firmware/ext/fatfs.mk
@@ -0,0 +1,7 @@
+# FATFS files.
+FATFSSRC = ${CHIBIOS}/os/various/fatfs_bindings/fatfs_diskio.c \
+           ${CHIBIOS}/os/various/fatfs_bindings/fatfs_syscall.c \
+           ${PROJECT_DIR}/ext/ff.c \
+           ${PROJECT_DIR}/ext/option/unicode.c
+
+FATFSINC = ${PROJECT_DIR}/ext
