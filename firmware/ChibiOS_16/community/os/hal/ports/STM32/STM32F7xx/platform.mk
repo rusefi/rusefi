1e324d9eef6a0d4c471fdfb9354df9d484e6eae2
Fabien Poussin <fabien.poussin@gmail.com>
2017-03-29 17:51:59 +0200

    Deleting old ChibiOS folders

--- firmware/ChibiOS_16/community/os/hal/ports/STM32/STM32F7xx/platform.mk
+++
@@ -1,9 +0,0 @@
-include ${CHIBIOS}/os/hal/ports/STM32/STM32F7xx/platform.mk
-
-PLATFORMSRC += ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/FSMCv1/hal_fsmc.c \
-               ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/FSMCv1/hal_nand_lld.c \
-               ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/FSMCv1/hal_fsmc_sram.c \
-               ${CHIBIOS_CONTRIB}/os/hal/src/hal_fsmc_sdram.c
-
-PLATFORMINC += ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/FSMCv1 \
-               ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD
