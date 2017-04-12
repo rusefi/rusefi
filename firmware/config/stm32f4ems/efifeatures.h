c1c3376405768ffc78a7d21397e86d2eca211dd7
rusefi <rusefillc@gmail.com>
2017-04-12 10:41:16 -0400

    better CAN flexibility

--- firmware/config/stm32f4ems/efifeatures.h
+++ firmware/config/stm32f4ems/efifeatures.h
@@ -197,14 +197,6 @@
 // todo: switch to continues ADC conversion for fast ADC?
 #define EFI_INTERNAL_FAST_ADC_PWM	&PWMD4
 
-//#define EFI_CAN_DEVICE CAND2
-#define EFI_CAN_RX_AF 9
-#define EFI_CAN_TX_AF 9
-
-//#define EFI_CAN_DEVICE CAND1
-//#define EFI_CAN_RX_AF 9
-//#define EFI_CAN_TX_AF 9
-
 #define EFI_SPI1_AF 5
 
 #define EFI_SPI2_AF 5
