0999c74da3561b494233d7d6acc332d7cdd0000a
rusefi <arro239+rusefi@gmail.com>
2017-04-25 08:29:24 -0400

    warning light progress

--- firmware/controllers/system_fsio.h
+++ firmware/controllers/system_fsio.h
@@ -1,6 +1,6 @@
 // this https://en.wikipedia.org/wiki/Reverse_Polish_notation is generated automatically
 // from controllers/system_fsio.txt
-// on 2017-01-19_16_29_03
+// on 2017-04-25_08_26_37
 //
 //
 // in this file we define system FSIO expressions
@@ -12,8 +12,8 @@
 //
 
 // Human-readable: (fan and (coolant > fan_off_setting)) | (coolant > fan_on_setting)
-#define FAN_CONTROL_LOGIC "fan coolant fan_off_setting > & coolant fan_on_setting > |"
-// todo: star-up priming time should probably be configurable?
+#define FAN_CONTROL_LOGIC "fan coolant fan_off_setting > and coolant fan_on_setting > |"
+// todo: start-up fuel pump priming time should probably be configurable?
 
 // Human-readable: (time_since_boot < 4) | (rpm > 0)
 #define FUEL_PUMP_LOGIC "time_since_boot 4 < rpm 0 > |"
@@ -23,3 +23,7 @@
 
 // Human-readable: ac_on_switch
 #define AC_RELAY_LOGIC "ac_on_switch"
+
+// Human-readable: rpm > fsio_setting(2)
+#define SIMPLE_WARNING_LIGHT "rpm 2 fsio_setting >"
+//COMBINED_WARNING_LIGHT=rpm > fsio_setting(2) | coolant fsio_setting(3) | vbatt < fsio_setting(4)
