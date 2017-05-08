cf86a0fe4c036815bd422553908dfe61229df99b
rusefi <qweZXC21>
2017-05-08 08:15:46 -0400

    removing unused parameter

--- firmware/controllers/lcd_controller.cpp
+++ firmware/controllers/lcd_controller.cpp
@@ -116,7 +116,7 @@ void initLcdController(void) {
 	msObjectInit(&lcdLineStream, (uint8_t *) lcdLineBuffer, sizeof(lcdLineBuffer), 0);
 }
 
-static char * prepareVBattMapLine(engine_configuration_s *engineConfiguration, char *buffer) {
+static char * prepareVBattMapLine(char *buffer) {
 	char *ptr = buffer;
 	*ptr++ = 'V';
 	ptr = ftoa(ptr, getVBatt(PASS_ENGINE_PARAMETER_F), 10.0f);
@@ -126,7 +126,7 @@ static char * prepareVBattMapLine(engine_configuration_s *engineConfiguration, c
 	return ptr;
 }
 
-static char * prepareCltIatTpsLine(Engine *engine, char *buffer) {
+static char * prepareCltIatTpsLine(char *buffer) {
 	char *ptr = buffer;
 	*ptr++ = 'C';
 
@@ -321,7 +321,7 @@ static void fillWithSpaces(void) {
 	}
 }
 
-void updateHD44780lcd(Engine *engine) {
+void updateHD44780lcd(void) {
 	MenuItem *p = tree.topVisible;
 	int screenY = 0;
 	for (; screenY < tree.linesCount && p != NULL; screenY++) {
