1f6d6984e23814662b237c49c9c11ff9a4ac8508
rusefi <arro239+rusefi@gmail.com>
2017-05-12 22:31:02 -0400

    trigger debug mode

--- firmware/rusefi.cpp
+++ firmware/rusefi.cpp
@@ -251,5 +251,5 @@ int getRusEfiVersion(void) {
 		return 123; // this is here to make the compiler happy about the unused array
 	if (UNUSED_CCM_SIZE[0] * 0 != 0)
 		return 3211; // this is here to make the compiler happy about the unused array
-	return 20170511;
+	return 20170512;
 }
