9cfa5e149ac548c9c661eed75bb5e2ba93a6f011
rusefi <arro239+rusefi@gmail.com>
2017-05-21 10:05:33 -0400

    sniffer usability

--- java_console/ui/src/com/rusefi/Launcher.java
+++ java_console/ui/src/com/rusefi/Launcher.java
@@ -44,7 +44,7 @@ import static com.rusefi.ui.storage.PersistentConfiguration.getConfig;
  * @see EngineSnifferPanel
  */
 public class Launcher {
-    public static final int CONSOLE_VERSION = 20170507;
+    public static final int CONSOLE_VERSION = 20170521;
     public static final boolean SHOW_STIMULATOR = false;
     private static final String TAB_INDEX = "main_tab";
     protected static final String PORT_KEY = "port";
