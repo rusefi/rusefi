1d09615a5c2f4b7b2a6184b149602fd884bbdeff
rusefi <qweZXC21>
2017-05-25 22:52:45 -0400

    fixing build

--- simulator/simulator/rusEfiFunctionalTest.cpp
+++ simulator/simulator/rusEfiFunctionalTest.cpp
@@ -119,7 +119,7 @@ void rusEfiFunctionalTest(void) {
 	initMapAveraging(&sharedLogger, engine);
 #endif /* EFI_MAP_AVERAGING */
 
-	initMainEventListener(&sharedLogger, engine);
+	initMainEventListener(&sharedLogger PASS_ENGINE_PARAMETER_SUFFIX);
 
 	startStatusThreads();
 
