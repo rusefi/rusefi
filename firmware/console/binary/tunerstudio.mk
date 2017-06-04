06b345c9ef472e23e166a5e9d7ef187836676848
Andrei <andreika-git@users.noreply.github.com>
2017-06-04 15:50:31 +0300

    Bluetooth setup code integration

--- firmware/console/binary/tunerstudio.mk
+++ firmware/console/binary/tunerstudio.mk
@@ -1,3 +1,4 @@
 
 TUNERSTUDIO_SRC_CPP = $(PROJECT_DIR)/console/binary/tunerstudio_io.cpp \
-	$(PROJECT_DIR)/console/binary/tunerstudio.cpp 
+	$(PROJECT_DIR)/console/binary/tunerstudio.cpp \
+	$(PROJECT_DIR)/console/binary/bluetooth.cpp 
