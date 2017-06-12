8c847247adb2a54bf60bf22652b4ce0e39878973
Andrei <andreika-git@users.noreply.github.com>
2017-06-13 01:14:39 +0300

    EFI_MAIN_RELAY_CONTROL draft

--- firmware/controllers/system_fsio.h
+++ firmware/controllers/system_fsio.h
@@ -1,6 +1,6 @@
 // this https://en.wikipedia.org/wiki/Reverse_Polish_notation is generated automatically
 // from controllers/system_fsio.txt
-// on 2017-04-25_15_33_33
+// on 2017-06-12_17_45_41
 //
 //
 // in this file we define system FSIO expressions
@@ -23,6 +23,11 @@
 
 // Human-readable: ac_on_switch
 #define AC_RELAY_LOGIC "ac_on_switch"
+// Combined RPM, CLT and VBATT warning light
 
 // Human-readable: (rpm > fsio_setting(2)) | ((coolant > fsio_setting(3)) | (vbatt < fsio_setting(4)))
 #define COMBINED_WARNING_LIGHT "rpm 2 fsio_setting > coolant 3 fsio_setting > vbatt 4 fsio_setting < | |"
+//needed by EFI_MAIN_RELAY_CONTROL
+
+// Human-readable: (time_since_boot < 2) | (vbatt > 5)
+#define MAIN_RELAY_LOGIC "time_since_boot 2 < vbatt 5 > |"
