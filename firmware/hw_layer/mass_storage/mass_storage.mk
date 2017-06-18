38d11a81aa36bd80b48bbc990eb65726c50795da
rusefi <arro239+rusefi@gmail.com>
2017-06-18 03:12:18 -0400

    #140

--- firmware/hw_layer/mass_storage/mass_storage.mk
+++ firmware/hw_layer/mass_storage/mass_storage.mk
@@ -1,2 +1,3 @@
 
-HW_MASS_STORAGE_SRC_C = $(PROJECT_DIR)/hw_layer/mass_storage/usb_msd_cfg.c
+HW_MASS_STORAGE_SRC_C = $(PROJECT_DIR)/hw_layer/mass_storage/usb_msd_cfg.c \
+  $(PROJECT_DIR)/ChibiOS-Contrib/os/various/lib_scsi.c
