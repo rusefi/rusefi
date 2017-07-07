594dfb639d700e2d7eb5c6b03c92d3e552fc6772
rusefi <arro239+rusefi@gmail.com>
2017-07-06 21:21:45 -0400

    code formatting

--- firmware/controllers/engine_controller.cpp
+++ firmware/controllers/engine_controller.cpp
@@ -262,7 +262,7 @@ static void periodicSlowCallback(Engine *engine) {
 	/**
 	 * Update engine RPM state if needed (check timeouts).
 	 */
-	 engine->rpmCalculator.checkIfSpinning(PASS_ENGINE_PARAMETER_SIGNATURE);
+	engine->rpmCalculator.checkIfSpinning(PASS_ENGINE_PARAMETER_SIGNATURE);
 
 	if (engine->rpmCalculator.isStopped(PASS_ENGINE_PARAMETER_SIGNATURE)) {
 #if (EFI_PROD_CODE && EFI_ENGINE_CONTROL && EFI_INTERNAL_FLASH) || defined(__DOXYGEN__)
