2e28e792de6c0e617ce3beb02a9fb81198da78ce
rusefi <arro239+rusefi@gmail.com>
2017-07-24 19:38:22 -0400

    better error code names

--- firmware/controllers/core/interpolation.h
+++ firmware/controllers/core/interpolation.h
@@ -36,11 +36,11 @@ int needInterpolationLogging(void);
 template<typename vType>
 float interpolate3d(float x, float xBin[], int xBinSize, float y, float yBin[], int yBinSize, vType* map[]) {
 	if (cisnan(x)) {
-		warning(CUSTOM_OBD_14, "%f: x is NaN in interpolate3d", x);
+		warning(CUSTOM_INTEPOLATE_ERROR_3, "%f: x is NaN in interpolate3d", x);
 		return NAN;
 	}
 	if (cisnan(y)) {
-		warning(CUSTOM_OBD_13, "%f: y is NaN in interpolate3d", y);
+		warning(CUSTOM_INTEPOLATE_ERROR_2, "%f: y is NaN in interpolate3d", y);
 		return NAN;
 	}
 
