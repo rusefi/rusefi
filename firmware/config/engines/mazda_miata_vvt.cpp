66bb7260f6a3365700616b5615e852daa3d83c82
rusefi <arro239+rusefi@gmail.com>
2017-07-24 21:04:18 -0400

    idle PID defaults

--- firmware/config/engines/mazda_miata_vvt.cpp
+++ firmware/config/engines/mazda_miata_vvt.cpp
@@ -398,6 +398,10 @@ void setMazdaMiata2003EngineConfiguration(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 	engineConfiguration->debugMode = DBG_IDLE_CONTROL;
 	//set idle_offset 30
 	engineConfiguration->idleRpmPid.offset = 30;
+	engineConfiguration->idleRpmPid.pFactor = 0.07;
+	engineConfiguration->idleRpmPid.iFactor = 0.0001;
+	engineConfiguration->idleRpmPid.dFactor = 5;
+	engineConfiguration->idleRpmPid.period = 10;
 }
 
 void setMazdaMiata2003EngineConfigurationNewBoard(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
