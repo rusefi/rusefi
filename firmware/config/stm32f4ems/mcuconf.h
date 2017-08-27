2c7053fe29e6ade61472d6692d6a01dee4d1462e
rusefi <arro239+rusefi@gmail.com>
2017-08-26 20:11:11 -0400

    #475

--- firmware/config/stm32f4ems/mcuconf.h
+++ firmware/config/stm32f4ems/mcuconf.h
@@ -70,11 +70,12 @@
 #define STM32_HSE_ENABLED                   TRUE
 
 // rusEfi would automatically detect if we have 32768 quarts osc - see 'rtcWorks'
-#define STM32_LSE_ENABLED                   FALSE
+#define STM32_LSE_ENABLED                   TRUE
 
 #define STM32_CLOCK48_REQUIRED              TRUE
 #define STM32_SW                            STM32_SW_PLL
 #define STM32_PLLSRC                        STM32_PLLSRC_HSE
+
 #define STM32_PLLM_VALUE                    8
 #define STM32_PLLN_VALUE                    336
 #define STM32_PLLP_VALUE                    2
