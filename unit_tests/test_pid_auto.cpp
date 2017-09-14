130f157d4170c4128f3742b6362533bb967d5ae4
rusefi <arro239+rusefi@gmail.com>
2017-09-14 07:58:58 -0400

    PID auto tune

---
+++ unit_tests/test_pid_auto.cpp
@@ -0,0 +1,17 @@
+/*
+ * test_pid_auto.cpp
+ *
+ *  Created on: Sep 14, 2017
+ * @author Andrey Belomutskiy, (c) 2012-2017
+ */
+
+
+#include "main.h"
+
+efitimems_t mockTimeMs = 0;
+
+efitimems_t currentTimeMillis(void) {
+	return mockTimeMs;
+}
+
+
