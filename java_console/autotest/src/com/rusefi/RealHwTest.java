9a8a945677b01578cd63957c4f957bcc04df21f6
rusefi <arro239+rusefi@gmail.com>
2017-11-21 22:28:22 -0500

    better message

--- java_console/autotest/src/com/rusefi/RealHwTest.java
+++ java_console/autotest/src/com/rusefi/RealHwTest.java
@@ -10,11 +10,12 @@ import static com.rusefi.AutoTest.*;
  * 2/22/2015
  */
 public class RealHwTest {
+    private static final int STARTUP_SLEEP = 45;
     private static final long SECOND = 1000L;
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Sleeping to give OS time to connect VCP driver");
-        Thread.sleep(45 * SECOND);
+        System.out.println("Sleeping " + STARTUP_SLEEP + " seconds to give OS time to connect VCP driver");
+        Thread.sleep(STARTUP_SLEEP * SECOND);
         long start = System.currentTimeMillis();
         String port = startRealHardwareTest(args);
 
