a6ccf51bbb77bfa445f717a393944b599e1bec08
rusefi <arro239+rusefi@gmail.com>
2017-11-24 19:20:55 -0500

    #494

---
+++ java_console/ui/src/com/rusefi/ui/widgets/ClosedLoopControlQualityMetric.java
@@ -0,0 +1,28 @@
+package com.rusefi.ui.widgets;
+
+import com.rusefi.core.Sensor;
+import com.rusefi.core.SensorCentral;
+import com.rusefi.core.SensorStats;
+import com.rusefi.core.ValueSource;
+
+/**
+ * https://github.com/rusefi/rusefi/issues/494
+ * <p>
+ * 11/16/2017
+ * (c) Andrey Belomutskiy
+ *
+ * @see SensorStats
+ */
+public class ClosedLoopControlQualityMetric {
+    /**
+     * @param target what value are we trying to achieve
+     * @param result what value do we actually have
+     */
+    public ClosedLoopControlQualityMetric(ValueSource target, ValueSource result, Sensor destination) {
+
+    }
+
+//    void start()
+
+
+}
