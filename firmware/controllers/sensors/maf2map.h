ce8e36e28321962992b33b7050b18e2a8dd31a68
rusEfi <russianefi@gmail.com>
2018-01-20 14:53:58 -0500

    #538

---
+++ firmware/controllers/sensors/maf2map.h
@@ -0,0 +1,15 @@
+/*
+ * @file maf2map.h
+ *
+ * @author Andrey Belomutskiy, (c) 2012-2018
+ * @date Jan 20, 2018
+ */
+
+#ifndef CONTROLLERS_SENSORS_MAF2MAP_H_
+#define CONTROLLERS_SENSORS_MAF2MAP_H_
+
+
+
+
+
+#endif /* CONTROLLERS_SENSORS_MAF2MAP_H_ */
