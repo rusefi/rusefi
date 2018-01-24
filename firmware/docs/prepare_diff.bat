57a1d77af48b966749be6491e0d4a050cdba3e9a
rusefi <arro239+rusefi@gmail.com>
2018-01-24 08:22:35 -0500

    chibi merge

--- firmware/docs/prepare_diff.bat
+++ firmware/docs/prepare_diff.bat
@@ -1,16 +1,16 @@
-#
-# see also ../../misc/git_cheat_sheet.txt
-# both 'rusefi/ChibiOS/original' and 'rusefi/ChibiOS/rusefi' should have upstream branch changes merged at the same time
-#
-# comparing against rusEfi version of the unmodified in order to compare remote branches on the same timestamp
-#
+rem #
+rem # see also ../../misc/git_cheat_sheet.txt
+rem # both 'rusefi/ChibiOS/original' and 'rusefi/ChibiOS/rusefi' should have upstream branch changes merged at the same time
+rem #
+rem # comparing against rusEfi version of the unmodified in order to compare remote branches on the same timestamp
+rem #
 
-git clone -b stable_16.1.x https://github.com/rusefi/ChibiOS Chibios.16_original
-git -C Chibios.16_original pull
-git clone -b stable_rusefi https://github.com/rusefi/ChibiOS Chibios.16_rusefi
-git -C Chibios.16_rusefi pull
-diff -uwr Chibios.16_rusefi Chibios.16_original > rusefi_chibios_16.patch
-diff -uwr Chibios.16_original Chibios.16_rusefi > chibios_rusefi_16.patch
+rem git clone -b stable_16.1.x https://github.com/rusefi/ChibiOS Chibios.16_original
+rem git -C Chibios.16_original pull
+rem git clone -b stable_rusefi https://github.com/rusefi/ChibiOS Chibios.16_rusefi
+rem git -C Chibios.16_rusefi pull
+rem diff -uwr Chibios.16_rusefi Chibios.16_original > rusefi_chibios_16.patch
+rem diff -uwr Chibios.16_original Chibios.16_rusefi > chibios_rusefi_16.patch
 
  
 git clone -b stable_17.6.x https://github.com/rusefi/ChibiOS Chibios.17_original
