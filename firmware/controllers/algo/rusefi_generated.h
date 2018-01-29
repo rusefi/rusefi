ebbcb01a1ea9e59e0b57fb1126f65a7c9b34afa4
andreika-git <andreika-git@users.noreply.github.com>
2018-01-30 01:27:39 +0200

    Add Option: Force stepper parking every restart (#562)

--- firmware/controllers/algo/rusefi_generated.h
+++ firmware/controllers/algo/rusefi_generated.h
@@ -580,8 +580,8 @@
 #define vvtCamSensorUseRise_offset_hex 3d8
 #define measureMapOnlyInOneCylinder_offset 984
 #define measureMapOnlyInOneCylinder_offset_hex 3d8
-#define unused_board_984_19_offset 984
-#define unused_board_984_19_offset_hex 3d8
+#define stepperForceParkingEveryRestart_offset 984
+#define stepperForceParkingEveryRestart_offset_hex 3d8
 #define unused_board_984_20_offset 984
 #define unused_board_984_20_offset_hex 3d8
 #define unused_board_984_21_offset 984
