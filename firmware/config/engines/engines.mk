7dfd1cc4fd9070611b51ad463c715ec2927d4e7a
rusEfi <russianefi@gmail.com>
2018-01-31 21:27:45 -0500

    NA8

--- firmware/config/engines/engines.mk
+++ firmware/config/engines/engines.mk
@@ -9,6 +9,7 @@ ENGINES_SRC_CPP = $(PROJECT_DIR)/config/engines/ford_aspire.cpp \
 	$(PROJECT_DIR)/config/engines/bmw_e34.cpp \
 	$(PROJECT_DIR)/config/engines/mazda_miata.cpp \
 	$(PROJECT_DIR)/config/engines/mazda_miata_1_6.cpp \
+	$(PROJECT_DIR)/config/engines/mazda_miata_na8.cpp \
 	$(PROJECT_DIR)/config/engines/mazda_miata_vvt.cpp \
 	$(PROJECT_DIR)/config/engines/citroenBerlingoTU3JP.cpp \
 	$(PROJECT_DIR)/config/engines/dodge_neon.cpp \
---
+++ firmware/config/engines/mazda_miata_na8.h
@@ -0,0 +1,15 @@
+/*
+ * @file mazda_miata_na8.h
+ *
+ * @date Jan 31, 2018
+ * @author Andrey Belomutskiy, (c) 2012-2017
+ */
+
+#ifndef CONFIG_ENGINES_MAZDA_MIATA_NA8_H_
+#define CONFIG_ENGINES_MAZDA_MIATA_NA8_H_
+
+
+
+
+
+#endif /* CONFIG_ENGINES_MAZDA_MIATA_NA8_H_ */
