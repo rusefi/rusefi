ec5671e23c38bab3ee2bcbd6a387fda4ce60e3e2
rusefi <arro239+rusefi@gmail.com>
2018-09-02 18:59:02 -0400

    class name typo

--- java_console/tools/src/com/irnems/MedianFilter.java
+++ java_console/tools/src/com/irnems/MedianFilter.java
@@ -8,33 +8,28 @@ import java.util.List;
  * @author Andrey Belomutskiy
  *         8/5/13
  */
-public class MedialFilter {
-
-
+public class MedianFilter {
     private List<Double> values;
     private int size;
 
-
-
-
-    public MedialFilter(List<Double> values, int size) {
+    public MedianFilter(List<Double> values, int size) {
         this.values = values;
         this.size = size;
     }
 
     public static List<Double> filter(List<Double> values, int size) {
-        return new MedialFilter(values, size).filter();
+        return new MedianFilter(values, size).filter();
     }
 
     private List<Double> filter() {
 
-        List<Double> result = new ArrayList<Double>();
+        List<Double> result = new ArrayList<>();
 
         for(int i=0;i<values.size();i++) {
 
             int fromIndex = Math.max(0, i - size);
 
-            List<Double> copy = new ArrayList<Double>(values.subList(fromIndex, i+1));
+            List<Double> copy = new ArrayList<>(values.subList(fromIndex, i + 1));
 
             Collections.sort(copy);
 
