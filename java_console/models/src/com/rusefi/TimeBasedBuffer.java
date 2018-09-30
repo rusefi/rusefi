153baecbb87a79ddd2598652e47e9ce75ab9ed0e
rusefi <rusefillc@gmail.com>
2018-09-29 22:53:02 -0700

    ETB progress

--- java_console/models/src/com/rusefi/TimeBasedBuffer.java
+++ java_console/models/src/com/rusefi/TimeBasedBuffer.java
@@ -1,4 +1,47 @@
 package com.rusefi;
 
-public class TimeBasedBuffer {
+import java.util.TreeMap;
+
+/**
+ * Last X seconds of values.
+ * <p>
+ * This data structure holds only one value for each millisecond timestamp but that should be totally fine
+ * for our purposes
+ *
+ * @see EtbTestSequence
+ */
+public class TimeBasedBuffer implements DataBuffer {
+    private final TreeMap<Long, Double> values = new TreeMap<>();
+    private final long duration;
+
+    public TimeBasedBuffer(long duration) {
+        this.duration = duration;
+    }
+
+    private synchronized void truncate() {
+        long now = currentTimeMillis();
+        values.headMap(now - duration).clear();
+    }
+
+    protected long currentTimeMillis() {
+        return System.currentTimeMillis();
+    }
+
+    public synchronized void add(double value) {
+        values.put(currentTimeMillis(), value);
+    }
+
+    public synchronized int getSize() {
+        truncate();
+        return values.size();
+    }
+
+    public synchronized double[] getValues() {
+        truncate();
+        double[] result = new double[values.size()];
+        int i = 0;
+        for (Double v : values.values())
+            result[i++] = v;
+        return result;
+    }
 }
