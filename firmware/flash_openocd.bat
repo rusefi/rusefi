a1a22f4f3f4c2a219ed5ce66a5e78922532859f8
rusefi <rusefillc@gmail.com>
2018-10-14 10:03:52 -0400

    adjusting scripts

--- firmware/flash_openocd.bat
+++ firmware/flash_openocd.bat
@@ -5,6 +5,5 @@ cp -r ../misc/install/openocd .
 if not exist openocd/openocd.exe echo openocd/openocd.exe NOT FOUND
 if not exist openocd/openocd.exe exit -1
 
-openocd\openocd           -f openocd/stm32f429disc1.cfg                                    -c "program build/rusefi.bin verify reset exit 0x08000000"
+openocd\openocd -f openocd/stm32f4discovery.cfg  -c "program build/rusefi.bin verify reset exit 0x08000000"
 
-rem openocd-0.8.0.exe -f interface/stlink-v2.cfg -f board/stm32f4discovery.cfg -c "program rusefi.bin verify reset exit 0x08000000"
--- misc/install/run_openocd_new_board.bat
+++
@@ -1,5 +0,0 @@
-rem if you have an older Rev C discovery please update to latest Rev D firmware
-
-rem As of March 2017 this enabled GDB debugging using ARM Eclipse plug-in
-
-openocd\openocd.exe -f openocd/stm32f429disc1.cfg -c init -c reset
