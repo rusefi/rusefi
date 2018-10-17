c392ba884439cc0dc826dd532c1c23f4eca92286
rusefi <rusefillc@gmail.com>
2018-10-16 21:27:05 -0400

    #613

---
+++ firmware/hw_layer/sensors/yaw_rate_sensor.h
@@ -0,0 +1,16 @@
+/*
+ * yaw_rate_sensor.h
+ *
+ *  Created on: Oct 16, 2018
+ * @author Andrey Belomutskiy, (c) 2012-2018
+ */
+
+#ifndef HW_LAYER_SENSORS_YAW_RATE_SENSOR_H_
+#define HW_LAYER_SENSORS_YAW_RATE_SENSOR_H_
+
+#include "global.h"
+#include "engine.h"
+
+void initBoschYawRateSensor();
+
+#endif /* HW_LAYER_SENSORS_YAW_RATE_SENSOR_H_ */
