d5ddb0bc9dc94c1dc3a74be531f77ebf26d2be80
rusefi <rusefillc@gmail.com>
2018-10-30 05:32:13 -0400

    #619

--- firmware/controllers/trigger/decoders/trigger_misc.cpp
+++ firmware/controllers/trigger/decoders/trigger_misc.cpp
@@ -7,7 +7,21 @@
 
 #include "trigger_misc.h"
 
+// TT_FIAT_IAW_P8
 void configureFiatIAQ_P8(TriggerShape * s DECLARE_ENGINE_PARAMETER_SUFFIX) {
+	s->initialize(FOUR_STROKE_CAM_SENSOR, false);
 
+	s->isSynchronizationNeeded = true;
+
+
+	int width = 60;
+	s->tdcPosition = width;
+
+	s->addEvent2(width, T_PRIMARY, TV_RISE PASS_ENGINE_PARAMETER_SUFFIX);
+	s->addEvent2(180, T_PRIMARY, TV_FALL PASS_ENGINE_PARAMETER_SUFFIX);
+
+	s->addEvent2(180 + width, T_PRIMARY, TV_RISE PASS_ENGINE_PARAMETER_SUFFIX);
+	s->addEvent2(720, T_PRIMARY, TV_FALL PASS_ENGINE_PARAMETER_SUFFIX);
+	s->setTriggerSynchronizationGap(3);
 }
 
