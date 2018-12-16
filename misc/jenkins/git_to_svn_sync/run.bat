ba1dcdd1ed907e2e7a96370ea9e2b446e3cefc76
rusefi <rusefillc@gmail.com>
2018-12-15 22:39:49 -0500

    better folder structure

---
+++ misc/jenkins/git_to_svn_sync/run.bat
@@ -0,0 +1,5 @@
+mkdir -p C:\stuff\rusefi.git2svn\git\rusefi
+cd C:\stuff\rusefi.git2svn\git
+git clone https://github.com/rusefi/rusefi
+cd C:\stuff\rusefi.git2svn\git\rusefi
+call misc/jenkins/git_to_svn_sync/git2svn.bat
