8a1a4ff78724df4e975167ed19c26e681a004181
rusefi <rusefillc@gmail.com>
2018-12-16 00:19:15 -0500

    better folder structure

---
+++ misc/jenkins/functional_test_and_build_bundle/run.bat
@@ -0,0 +1,10 @@
+
+call misc/jenkins/functional_test_and_build_bundle/build_current_bundle.bat
+IF NOT ERRORLEVEL 0 echo build ERROR DETECTED
+IF NOT ERRORLEVEL 0 EXIT /B 1
+
+
+call misc/jenkins/functional_test_and_build_bundle/hw_test.bat
+IF NOT ERRORLEVEL 0 echo real hardware test ERROR DETECTED
+IF NOT ERRORLEVEL 0 EXIT /B 1
+
