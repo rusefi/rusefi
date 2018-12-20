9ec116608539dbb0b4c3b53783ec39a8b9821f1d
rusefi <rusefillc@gmail.com>
2018-12-20 00:44:53 -0500

    refactoring

--- java_tools/configuration_definition/src/com/rusefi/TSProjectConsumer.java
+++ java_tools/configuration_definition/src/com/rusefi/TSProjectConsumer.java
@@ -86,6 +86,7 @@ public class TSProjectConsumer implements ConfigurationConsumer {
             ConfigDefinition.totalTsSize = structure.writeTunerStudio("", tsWriter, 0);
             tsWriter.write("; total TS size = " + ConfigDefinition.totalTsSize + EOL);
             VariableRegistry.INSTANCE.register("TOTAL_CONFIG_SIZE", ConfigDefinition.totalTsSize);
+ //           TSProjectConsumer.writeTunerStudioFile(tsPath, tsWriter.toString());
         }
     }
 
