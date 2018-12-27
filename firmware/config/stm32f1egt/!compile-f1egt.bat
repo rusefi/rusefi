757304256bcadcd7abed01f4f6139fc769455106
rusefi <rusefillc@gmail.com>
2018-12-26 22:43:52 -0500

    stm32f1

--- firmware/config/stm32f1egt/!compile-f1egt.bat
+++ firmware/config/stm32f1egt/!compile-f1egt.bat
@@ -1,9 +1,10 @@
 rem 
-rem STM32F469 version of the firmware for https://rusefi.com/forum/viewtopic.php?f=4&t=1215
+rem STM32F103 version of the firmware
 rem
 
-cd ../../..
-set EXTRA_PARAMS="-DSTM32F469xx"
+cd ../..
+set EXTRA_PARAMS="-DSTM32F1XX"
 set PROJECT_CPU=ST_STM32F1
 set LDSCRIPT=config/stm32f1egt/STM32F103xB.ld
-call compile_and_program.bat -r
+make
+rem call compile_and_program.bat -r
