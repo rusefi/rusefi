9f39c11c7513c8afbbd0b6cba824a824d4a1b1aa
rusefi <rusefillc@gmail.com>
2019-01-04 00:30:15 -0500

    #34

--- unit_tests/test_sensors.cpp
+++ unit_tests/test_sensors.cpp
@@ -8,7 +8,6 @@
 #include "global.h"
 #include "thermistors.h"
 #include "allsensors.h"
-#include "hip9011_lookup.h"
 #include "engine_test_helper.h"
 
 static ThermistorConf tc;
@@ -58,35 +57,11 @@ void testTpsRateOfChange(void) {
 //	assertEquals(25, getTpsRateOfChange());
 }
 
-static void testHip9011lookup(void) {
-	print("************************************************** testHip9011lookup\r\n");
-
-	assertEqualsM2("", 3183.1013, getRpmByAngleWindowAndTimeUs(600, 360), 0.1);
-	assertEqualsM2("40us", 47746.5195, getRpmByAngleWindowAndTimeUs(40, 360), 0.1);
-
-	assertEqualsM2("600us 50 degree", 442.0974, getRpmByAngleWindowAndTimeUs(600, 50), 0.1);
-	assertEqualsM2("240us 50 degree", 1105.2435, getRpmByAngleWindowAndTimeUs(240, 50), 0.1);
-	assertEqualsM2("240us 50 degree", 6631.4619, getRpmByAngleWindowAndTimeUs(40, 50), 0.1);
-
-	assertEquals(0, getHip9011GainIndex(3));
-	assertEquals(0, getHip9011GainIndex(2));
-	assertEquals(47, getHip9011GainIndex(0.234));
-	assertEquals(63, getHip9011GainIndex(0.000001));
-
-	prepareHip9011RpmLookup(50);
-
-	assertEquals(31, getIntegrationIndexByRpm(1));
-	assertEquals(21, getIntegrationIndexByRpm(1100));
-	assertEquals(1, getIntegrationIndexByRpm(6600));
-	assertEquals(0, getIntegrationIndexByRpm(16600));
-}
-
 void testSensors(void) {
 	print("************************************************** testSensors\r\n");
 	testMapDecoding();
 	testTps();
 	testTpsRateOfChange();
-	testHip9011lookup();
 
 	ThermistorMath tm;
 	{
