68cb4a04524411368abe5a4f102221c4a36dd9d2
rusefi <rusefillc@gmail.com>
2019-01-04 00:51:32 -0500

    #35 trying new kind of parameter magic

--- firmware/hw_layer/HIP9011_logic.cpp
+++ firmware/hw_layer/HIP9011_logic.cpp
@@ -7,8 +7,19 @@
 
 #include "HIP9011_logic.h"
 
+EXTERN_ENGINE;
+
 HIP9011::HIP9011() {
 	needToInit = true;
 	state = NOT_READY;
 }
 
+#define BAND(bore) (900 / (PIF * (bore) / 2))
+
+/**
+ * @return frequency band we are interested in
+ */
+float getHIP9011Band(DEFINE_HIP_PARAMS) {
+	return GET_CONFIG_VALUE(knockBandCustom) == 0 ?
+			BAND(GET_CONFIG_VALUE(cylinderBore)) : GET_CONFIG_VALUE(knockBandCustom);
+}
