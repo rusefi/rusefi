aced64810f11c929461cd4157fc7b022ffd1641e
rusefi <rusefillc@gmail.com>
2019-01-05 21:49:47 -0500

    better folder structure

--- unit_tests/test.mk
+++ unit_tests/test.mk
@@ -1,27 +1,27 @@
-TEST_SRC_CPP = test_util.cpp \
-	unit_test_framework.cpp \
+TEST_SRC_CPP = unit_test_framework.cpp \
+	engine_test_helper.cpp \
 	boards.cpp \
 	global_execution_queue.cpp \
 	test_basic_math/test_find_index.cpp \
 	test_basic_math/test_interpolation_3d.cpp \
-	test_cj125.cpp \
-	test_ion.cpp \
-	test_hip9011.cpp \
-	test_data_structures/test_engine_math.cpp \
-	test_startOfCrankingPrimingPulse.cpp \
-	test_fasterEngineSpinningUp.cpp \
-	test_idle_controller.cpp \
-	test_trigger_decoder.cpp \
-	test_trigger_noiseless.cpp \
-	test_fuel_map.cpp \
-	test_fuelCut.cpp \
-	engine_test_helper.cpp \
-	test_pwm_generator.cpp \
-	test_logic_expression.cpp \
-	test_speed_density.cpp \
-	test_signal_executor.cpp \
-	test_sensors.cpp \
-	test_pid_auto.cpp \
-	test_accel_enrichment.cpp \
+	tests/test_util.cpp \
+	tests/test_ion.cpp \
+	tests/test_hip9011.cpp \
+	tests/test_cj125.cpp \
+	tests/test_engine_math.cpp \
+	tests/test_startOfCrankingPrimingPulse.cpp \
+	tests/test_fasterEngineSpinningUp.cpp \
+	tests/test_idle_controller.cpp \
+	tests/test_trigger_decoder.cpp \
+	tests/test_trigger_noiseless.cpp \
+	tests/test_fuel_map.cpp \
+	tests/test_fuelCut.cpp \
+	tests/test_pwm_generator.cpp \
+	tests/test_logic_expression.cpp \
+	tests/test_speed_density.cpp \
+	tests/test_signal_executor.cpp \
+	tests/test_sensors.cpp \
+	tests/test_pid_auto.cpp \
+	tests/test_accel_enrichment.cpp \
 	afm2mapConverter.cpp
 	
