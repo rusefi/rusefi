b037a6e0ca92d0bacc39ab3e82cfd8689eb979be
Matthew Kennedy <matthewkennedy@outlook.com>
2019-01-06 18:15:49 -0800

    Implement common for controllers (#645)
    * controller
    
    * remove stopping thread
    
    * move microsecond timer watchdog
    
    * docs
    
    * typo snuck in
    
    * go back to the old way

---
+++ firmware/controllers/system/ControllerBase.h
@@ -0,0 +1,23 @@
+/**
+ *  @file ControllerBase.h
+ * 
+ *  @date Jan 5, 2019
+ *  @author Matthew Kennedy, (c) 2019
+ */
+
+#pragma once
+
+// Sparse for now...
+class ControllerBase
+{
+private:
+    const char* m_name;
+
+public:
+    ControllerBase(const char* name)
+        : m_name(name)
+    {
+    }
+
+    const char* GetName() const { return m_name; }
+};
