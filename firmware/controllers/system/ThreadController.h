435a255043434f9d79f434c69a2d15910aa45532
Matthew Kennedy <matthewkennedy@outlook.com>
2019-01-06 18:15:49 -0800

    Implement common for controllers (#645)
    * controller
    
    * remove stopping thread
    
    * move microsecond timer watchdog
    
    * docs
    
    * typo snuck in
    
    * go back to the old way

---
+++ firmware/controllers/system/ThreadController.h
@@ -0,0 +1,62 @@
+/**
+ *  @file ThreadController.h
+ * 
+ *  @date Jan 5, 2019
+ *  @author Matthew Kennedy, (c) 2019
+ */
+
+#pragma once
+
+#include "ControllerBase.h"
+
+#include "ch.h"
+
+/**
+ * @brief A base class for a controller that requires its own thread.
+ * 
+ * Inherit from ThreadController.  Implement ThreadTask with the logic required for your thread.
+ * The template parameter specifies the size of the stack used for the thread.  (because we have to
+ * allocate the stack at compile time, it has to be a template parameter instead of a normal parameter)
+ */
+template <int TStackSize>
+class ThreadController : public ControllerBase
+{
+private:
+    THD_WORKING_AREA(m_threadstack, TStackSize);
+    const tprio_t m_prio;
+
+    /**
+     * The OS can only call a function with a single void* param.  We have
+     * to convert back to an instance of ThreadController, and call the task to run.
+     */
+    static void StaticThreadTaskAdapter(void* thread)
+    {
+        ThreadController* t = static_cast<ThreadController*>(thread);
+
+        // Run our thread task
+        t->ThreadTask();
+    }
+
+protected:
+    /**
+     * Override this function to implement your controller's thread's behavior.
+     */
+    virtual void ThreadTask() = 0;
+    thread_t* m_thread;
+
+public:
+    ThreadController(const char* name, tprio_t priority)
+        : ControllerBase(name)
+        , m_prio(priority)
+    {
+    }
+
+    /**
+     * @brief Start the thread.
+     */
+    virtual void Start()
+    {
+        m_thread = chThdCreateStatic(m_threadstack, sizeof(m_threadstack), m_prio, StaticThreadTaskAdapter, this);
+        m_thread->name = GetName();
+    }
+};
