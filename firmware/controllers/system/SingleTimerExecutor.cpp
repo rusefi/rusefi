dce2e17f1bce5a07fa2033441468396263756139
rusefi <rusefillc@gmail.com>
2019-01-09 00:25:45 -0500

    #655 clean-up

--- firmware/controllers/system/SingleTimerExecutor.cpp
+++ firmware/controllers/system/SingleTimerExecutor.cpp
@@ -21,12 +21,9 @@
 #include "efitime.h"
 #include "efilib2.h"
 
-#if EFI_PROD_CODE || defined(__DOXYGEN__)
+#if EFI_SIGNAL_EXECUTOR_ONE_TIMER || defined(__DOXYGEN__)
 #include "microsecond_timer.h"
 #include "tunerstudio_configuration.h"
-#endif
-
-#if (EFI_SIGNAL_EXECUTOR_ONE_TIMER && EFI_PROD_CODE )|| defined(__DOXYGEN__)
 #include "rfiutil.h"
 
 static Executor instance;
