ad420c462c3ef51b45e3bbb821b5b59400cef90e
rusefi <rusefillc@gmail.com>
2019-01-26 12:13:35 -0500

    progress but still does not work

--- firmware/config/boards/STM32F407VET6_Mini.bat
+++ firmware/config/boards/STM32F407VET6_Mini.bat
@@ -1,7 +1,11 @@
 
 echo "Compiling for https://rusefi.com/forum/viewtopic.php?f=4&t=1498"
 cd ../..
-set EXTRA_PARAMS="-DEFI_COMMUNICATION_PIN=GPIOB_9 -DSTM32_PLLM_VALUE=25"
-make clean
-make
+rem TODO: somehow this -DDUMMY is helping us to not mess up the parameters, why?!
+rem https://github.com/rusefi/rusefi/issues/684
+set EXTRA_PARAMS="-DDUMMY -DEFI_COMMUNICATION_PIN=GPIOB_9 -DSTM32_PLLM_VALUE=25 -DDEFAULT_ENGINE_TYPE=MINIMAL_PINS"
+make -j8 clean
+make -j8
+rm -f build/rusefi.dfu
+dir build
 ..\misc\hex2dfu\HEX2DFU.exe build/rusefi.hex   -out build/rusefi.dfu
