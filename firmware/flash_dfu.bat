6b27afcf00e35546c332103be5e2eee254314ae3
rusefi <rusefillc@gmail.com>
2019-01-26 12:13:35 -0500

    progress but still does not work

--- firmware/flash_dfu.bat
+++ firmware/flash_dfu.bat
@@ -5,6 +5,9 @@ rem dfu-util -a 0 -D rusefi_release.dfu -R
 rem ..\misc\hex2dfu\HEX2DFU.exe build/rusefi.hex   -out build/rusefi.dfu
 rem DfuSeCommand.exe -c -d --fn build/rusefi.dfu
 
+rem to read from comtroller use
+rem DfuSeCommand.exe -c -u --fn downloaded.dfu
+
 rem Note that DfuSe_Demo_V3.0.6_Setup.exe does not install "stm32 bootloader" device drivers automaticaly
 rem You need to point Windows at a relevant folder inside C:\Program Files (x86)\STMicroelectronics\Software\DfuSe v3.0.6\Bin\Driver
 
