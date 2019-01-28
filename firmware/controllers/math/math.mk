5bb24e01935e3ba6572ecf152c0e841bb486a4b7
rusefi <rusefillc@gmail.com>
2019-01-28 01:09:13 -0500

    better (?) location for files

--- firmware/controllers/math/math.mk
+++ firmware/controllers/math/math.mk
@@ -2,7 +2,5 @@
 CONTROLLERS_MATH_SRC = 
 
 CONTROLLERS_MATH_SRC_CPP = $(PROJECT_DIR)/controllers/math/engine_math.cpp \
-	$(PROJECT_DIR)/controllers/math/pid.cpp \
-	$(PROJECT_DIR)/controllers/math/biquad.cpp \
 	$(PROJECT_DIR)/controllers/math/pid_auto_tune.cpp \
 	$(PROJECT_DIR)/controllers/math/speed_density.cpp
