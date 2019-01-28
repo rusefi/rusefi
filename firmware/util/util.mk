0cd324ecacc523dc2a20eef81f5be993d3f53a39
rusefi <rusefillc@gmail.com>
2019-01-28 01:09:13 -0500

    better (?) location for files

--- firmware/util/util.mk
+++ firmware/util/util.mk
@@ -9,6 +9,8 @@ UTILSRC_CPP = $(PROJECT_DIR)/util/cyclic_buffer.cpp \
 	$(PROJECT_DIR)/util/loggingcentral.cpp \
 	$(PROJECT_DIR)/util/listener_array.cpp \
 	$(PROJECT_DIR)/util/cli_registry.cpp \
+	$(PROJECT_DIR)/util/pid.cpp \
+	$(PROJECT_DIR)/util/biquad.cpp \
 	$(PROJECT_DIR)/util/efilib.cpp \
 	$(PROJECT_DIR)/util/efilib2.cpp \
 	$(PROJECT_DIR)/util/LocalVersionHolder.cpp
