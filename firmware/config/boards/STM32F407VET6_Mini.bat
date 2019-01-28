dc7418308de6bc7abe60548f8926290d1f5e78b9
rusefi <rusefillc@gmail.com>
2019-01-27 20:33:05 -0500

    mini board

--- firmware/config/boards/STM32F407VET6_Mini.bat
+++ firmware/config/boards/STM32F407VET6_Mini.bat
@@ -5,7 +5,7 @@ rem TODO: somehow this -DDUMMY is helping us to not mess up the parameters, why?
 rem https://github.com/rusefi/rusefi/issues/684
 rem this board has only 512K flash so using custom FLASH_ADDR
 rem You probably want "flash0  : org = 0x08000000, len = 450K" in the .ld file
-set EXTRA_PARAMS="-DDUMMY -DEFI_COMMUNICATION_PIN=GPIOB_9 -DSTM32_PLLM_VALUE=25 -DDEFAULT_ENGINE_TYPE=MINIMAL_PINS -DFLASH_ADDR=0x08075000"
+set EXTRA_PARAMS="-DDUMMY -DEFI_COMMUNICATION_PIN=GPIOB_9 -DSTM32_PLLM_VALUE=25 -DSTM32_RTCPRE_VALUE=25 -DDEFAULT_ENGINE_TYPE=MINIMAL_PINS -DFLASH_ADDR=0x08075000"
 make -j8 clean
 make -j8
 rm -f build/rusefi.dfu
