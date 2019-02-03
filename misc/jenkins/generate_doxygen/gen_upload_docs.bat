93a06f61017b6de830b5de0e5b11fcd0d79950bf
rusefi <rusefillc@gmail.com>
2019-02-02 20:32:05 -0500

    tools: InteractiveHtmlBom is now a nightly job

--- misc/jenkins/generate_doxygen/gen_upload_docs.bat
+++ misc/jenkins/generate_doxygen/gen_upload_docs.bat
@@ -5,7 +5,8 @@ set RUSEFI_DOXYGEN_FTP_USER=u71977750-docs
 echo Should be executed from project root folder. Will try to upload to %RUSEFI_FTP_SERVER%
 
 rem ibom is part of Doxygen job simply in order to reduce workspace HDD usage on my tiny build server
-misc\jenkins\InteractiveHtmlBom\run.bat
+call misc\jenkins\InteractiveHtmlBom\run.bat
+echo Uploading IBOMs
 ncftpput -R -v -u %RUSEFI_DOXYGEN_FTP_USER% -p %RUSEFI_DOXYGEN_FTP_PASS% %RUSEFI_FTP_SERVER% /ibom hardware/ibom/*
 
 pwd
@@ -19,4 +20,5 @@ IF NOT ERRORLEVEL 0 EXIT /B 1
 rem http://www.ncftp.com/download/
 rem actually Cygwin http://rusefi.com/wiki/index.php?title=Internal:Software:Build_Server
 cd ../doxygen
+echo Uploading Doxygen
 ncftpput -R -v -u %RUSEFI_DOXYGEN_FTP_USER% -p %RUSEFI_DOXYGEN_FTP_PASS% %RUSEFI_FTP_SERVER% /html html/*
