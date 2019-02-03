2a0d5e4749ac9d4a42bd204a81e4994968cc6874
rusefi <rusefillc@gmail.com>
2019-02-03 00:50:10 -0500

    Multiple sync pattern for faster cranking #679

---
+++ unit_tests/tests/test_trigger_multi_sync.cpp
@@ -0,0 +1,18 @@
+/*
+ * @file test_trigger_multi_sync.cpp
+ *
+ * @date Feb 2, 2019
+ * @author Andrey Belomutskiy, (c) 2012-2019
+ */
+
+#include "engine_test_helper.h"
+
+TEST(trigger, miataNA) {
+
+
+
+	WITH_ENGINE_TEST_HELPER(MIATA_NA6_MAP);
+
+
+
+}
