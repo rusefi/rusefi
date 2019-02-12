b7b5c28be704d0809484773271b7fe11ca54e412
rusefi <rusefillc@gmail.com>
2019-02-12 01:24:42 -0500

    getRemainingStack() is back!

--- firmware/hw_layer/stm32f7/mpu_util.cpp
+++ firmware/hw_layer/stm32f7/mpu_util.cpp
@@ -39,12 +39,10 @@ int getRemainingStack(thread_t *otp) {
 
 	int remainingStack;
     if (ch.dbg.isr_cnt > 0) {
-remainingStack = 9999;
 		// ISR context
-// todo		remainingStack = (int)(r13 - 1) - (int)&__main_stack_base__;
+		remainingStack = (int)(r13 - 1) - (int)&__main_stack_base__;
 	} else {
-remainingStack = 9999;
-// todo		remainingStack = (int)(r13 - 1) - (int)otp->p_stklimit;
+		remainingStack = (int)(r13 - 1) - (int)otp->wabase;
 	}
 	otp->remainingStack = remainingStack;
 	return remainingStack;
