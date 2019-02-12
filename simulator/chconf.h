2e89652a0067f3860ef09151eaeee78d560187c6
rusefi <rusefillc@gmail.com>
2019-02-11 23:57:29 -0500

    CH_DBG_FILL_THREADS

--- simulator/chconf.h
+++ simulator/chconf.h
@@ -548,7 +548,7 @@
  * @note    The default is @p FALSE.
  */
 #if !defined(CH_DBG_FILL_THREADS)
-#define CH_DBG_FILL_THREADS                 FALSE
+#define CH_DBG_FILL_THREADS                 TRUE
 #endif
 
 /**
