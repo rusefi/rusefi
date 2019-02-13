c34ba20aa08daba3613b3845fa599d7788a58e8d
rusefi <rusefillc@gmail.com>
2019-02-12 19:05:06 -0500

    getMaxUsedStack() - stack usage reseach mini progress

--- firmware/util/efilib2.cpp
+++ firmware/util/efilib2.cpp
@@ -44,11 +44,13 @@ int getMaxUsedStack(uint8_t *ptr, int size) {
 	/**
 	 * maximum used stack size total stack buffer size minus position of first modified byte
 	 */
+#if ! EFI_UNIT_TEST
 	int used = 0;
 	for (int i = 0; i < size; i++) {
 		if (ptr[i] != CH_DBG_STACK_FILL_VALUE) {
 			return size - i;
 		}
 	}
+#endif /* EFI_UNIT_TEST */
 	return 0;
 }
