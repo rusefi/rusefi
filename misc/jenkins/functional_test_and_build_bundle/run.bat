6bcd9fd2fe932ba62eca940d1d26df41b9ebdf02
rusefi <rusefillc@gmail.com>
2019-02-28 22:36:46 -0500

    #699 one step back

--- misc/jenkins/functional_test_and_build_bundle/run.bat
+++ misc/jenkins/functional_test_and_build_bundle/run.bat
@@ -4,9 +4,9 @@ IF NOT ERRORLEVEL 0 echo build ERROR DETECTED
 IF NOT ERRORLEVEL 0 EXIT /B 1
 
 
-call misc/jenkins/functional_test_and_build_bundle/simulator_test.bat
-IF NOT ERRORLEVEL 0 echo simulator test ERROR DETECTED
-IF NOT ERRORLEVEL 0 EXIT /B 1
+rem call misc/jenkins/functional_test_and_build_bundle/simulator_test.bat
+rem IF NOT ERRORLEVEL 0 echo simulator test ERROR DETECTED
+rem IF NOT ERRORLEVEL 0 EXIT /B 1
 
 
 call misc/jenkins/functional_test_and_build_bundle/hw_test.bat
