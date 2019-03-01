beea63c179feeaf622fc6569f037873dade9e889
rusefi <rusefillc@gmail.com>
2019-02-28 19:02:32 -0500

    simulator back into CI

--- misc/jenkins/functional_test_and_build_bundle/run.bat
+++ misc/jenkins/functional_test_and_build_bundle/run.bat
@@ -4,6 +4,11 @@ IF NOT ERRORLEVEL 0 echo build ERROR DETECTED
 IF NOT ERRORLEVEL 0 EXIT /B 1
 
 
+call misc/jenkins/functional_test_and_build_bundle/simulator_test.bat
+IF NOT ERRORLEVEL 0 echo simulator test ERROR DETECTED
+IF NOT ERRORLEVEL 0 EXIT /B 1
+
+
 call misc/jenkins/functional_test_and_build_bundle/hw_test.bat
 IF NOT ERRORLEVEL 0 echo real hardware test ERROR DETECTED
 IF NOT ERRORLEVEL 0 EXIT /B 1
