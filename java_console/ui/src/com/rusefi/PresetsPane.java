e0d54d8698701304e3c1d089d042a12039d44322
rusEfi <russianefi@gmail.com>
2019-03-02 23:12:23 -0500

    ETB

--- java_console/ui/src/com/rusefi/PresetsPane.java
+++ java_console/ui/src/com/rusefi/PresetsPane.java
@@ -16,6 +16,7 @@ public class PresetsPane {
     private static final int MIATA_NA6_VAF = 57;
     private static final int MAZDA_MIATA_2003 = 47;
     private static final int TEST_V_12 = 49;
+    private static final int ETB_BENCH = 58;
 
     private JPanel content = new JPanel(new GridLayout(2, 4));
 
@@ -24,6 +25,7 @@ public class PresetsPane {
         content.add(new SetEngineTypeCommandControl("Miata NA6 Stage 1", "engines/miata_na.png", MIATA_NA6_MAP).getContent());
         content.add(new SetEngineTypeCommandControl("Miata NB2", "engines/miata_nb.png", MAZDA_MIATA_2003).getContent());
         content.add(new SetEngineTypeCommandControl("V12", "engines/v12", TEST_V_12).getContent());
+        content.add(new SetEngineTypeCommandControl("ETB bench", "", ETB_BENCH).getContent());
     }
 
     public JPanel getContent() {
