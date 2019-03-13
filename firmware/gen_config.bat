13f360a588a7348d456e52069d68925d5cd659ce
rusefi <rusefillc@gmail.com>
2019-03-12 20:34:10 -0400

    I am lazy

--- firmware/gen_config.bat
+++ firmware/gen_config.bat
@@ -2,3 +2,9 @@ rem This batch files reads rusefi_config.txt and produses firmware persistent co
 rem the storage section of rusefi.ini is updated as well
 
 java -jar ../java_tools/ConfigDefinition.jar integration tunerstudio controllers\algo ../java_console
+
+
+rem This would automatically copy latest file to 'dev' TS project
+set ts_path="%HOMEDRIVE%%HOMEPATH%\Documents\TunerStudioProjects\dev\projectCfg"
+echo %ts_path%
+cp tunerstudio/rusefi.ini %ts_path%\mainController.ini
