8e3e326603867632d229e60d4ea384d630cef9dc
rusefi <rusefillc@gmail.com>
2019-03-20 16:54:09 -0400

    rusEfi F767 does not start on my custom board #708

--- misc/jenkins/compile_other_versions/build_version.bat
+++ misc/jenkins/compile_other_versions/build_version.bat
@@ -1,5 +1,5 @@
 rm -rf temp
-set folder=snapshot_%date:~10%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%_%stm_arch%_rusefi
+set folder=snapshot_%date:~10%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%_%bundle_name%_rusefi
 set folder=temp\%folder%
 
 rem this replaces spaces with 0s - that's needed before 10am
@@ -12,7 +12,7 @@ rem TODO: extract FTP duplication with 407 build
 
 echo open ftp://u71977750-build:%RUSEFI_BUILD_FTP_PASS%@%FTP_SERVER%/ > ftp_commands.txt
 echo binary >> ftp_commands.txt
-echo put rusefi_bundle.zip rusefi_bundle_%stm_arch%.zip >> ftp_commands.txt
+echo put rusefi_bundle.zip rusefi_bundle_%bundle_name%.zip >> ftp_commands.txt
 
 echo exit >> ftp_commands.txt
 
