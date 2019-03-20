fab5b3e68e2863201f5259f74224c7b1e54d2c89
rusefi <rusefillc@gmail.com>
2019-03-20 16:54:09 -0400

    rusEfi F767 does not start on my custom board #708

--- misc/jenkins/compile_other_versions/run.bat
+++ misc/jenkins/compile_other_versions/run.bat
@@ -16,9 +16,9 @@ cd firmware\config\boards\NUCLEO_F746
 call !compile-nucleo_f746.bat
 cd ..
 pwd
-set stm_arch=stm32f746
+set bundle_name=stm32f746_nucleo
 call misc\jenkins\compile_other_versions\build_version.bat
-echo Done with F746
+echo Done with F746-nucleo
 
 
 cd firmware
@@ -29,7 +29,19 @@ cd firmware\config\boards\NUCLEO_F767
 call !compile-nucleo_f767.bat
 cd ..
 pwd
-set stm_arch=stm32f767
+set bundle_name=stm32f767_nucleo
 call misc\jenkins\compile_other_versions\build_version.bat
+echo Done with F767-nucleo
 
-echo Done with F767
+cd firmware
+call clean.bat
+cd ..
+echo Compiling F767
+cd firmware\config\boards\NUCLEO_F767
+call !compile-f767.bat
+cd ..
+pwd
+set bundle_name=stm32f767_osc
+call misc\jenkins\compile_other_versions\build_version.bat
+echo Done with F767-osc
+
