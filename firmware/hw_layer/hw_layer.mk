bfa0b6662fed6daeab4fc7cc3e5a696d2496dc6f
Andrei <andreika-git@users.noreply.github.com>
2019-03-28 18:06:07 +0200

    More ports/stm32/* paths

--- firmware/hw_layer/hw_layer.mk
+++ firmware/hw_layer/hw_layer.mk
@@ -34,15 +34,15 @@ HW_LAYER_EMS_CPP = $(HW_LAYER_EGT_CPP) \
 	$(PROJECT_DIR)/hw_layer/backup_ram.cpp
 	
 ifeq ($(PROJECT_CPU),ST_STM32F7)
-HW_LAYER_EMS += $(PROJECT_DIR)/hw_layer/stm32f7/stm32f7xx_hal_flash.c \
-				$(PROJECT_DIR)/hw_layer/stm32f7/stm32f7xx_hal_flash_ex.c
+HW_LAYER_EMS += $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f7/stm32f7xx_hal_flash.c \
+				$(PROJECT_DIR)/hw_layer/ports/stm32/stm32f7/stm32f7xx_hal_flash_ex.c
 HW_LAYER_EMS_CPP += $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f7/mpu_util.cpp \
 	$(PROJECT_DIR)/hw_layer/ports/stm32/stm32_common.cpp
 endif
 
 ifeq ($(PROJECT_CPU),ST_STM32F4)
-HW_LAYER_EMS += $(PROJECT_DIR)/hw_layer/stm32f4/stm32f4xx_hal_flash.c \
-				$(PROJECT_DIR)/hw_layer/stm32f4/stm32f4xx_hal_flash_ex.c
+HW_LAYER_EMS += $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f4/stm32f4xx_hal_flash.c \
+				$(PROJECT_DIR)/hw_layer/ports/stm32/stm32f4/stm32f4xx_hal_flash_ex.c
 HW_LAYER_EMS_CPP += $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f4/mpu_util.cpp \
 	$(PROJECT_DIR)/hw_layer/ports/stm32/stm32_common.cpp
 endif
--- firmware/hw_layer/ports/stm32/stm32f0/hw_stm32.mk
+++ firmware/hw_layer/ports/stm32/stm32f0/hw_stm32.mk
@@ -1,4 +1,4 @@
 
 
-HW_STM32_SRC_CPP = $(PROJECT_DIR)/hw_layer/stm32f0/mpu_util.cpp
+HW_STM32_SRC_CPP = $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f0/mpu_util.cpp
 	
