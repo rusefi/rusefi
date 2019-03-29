5311bfa50243f3b028d9d47825c442248d7047dd
rusefi <rusefillc@gmail.com>
2019-03-29 09:40:43 -0400

    unifying_file_naming

--- firmware/hw_layer/sensors/sensors.mk
+++ firmware/hw_layer/sensors/sensors.mk
@@ -1,3 +1,3 @@
-HW_SENSORS_SRC = $(PROJECT_DIR)/hw_layer/sensors/CJ125.cpp \
-	$(PROJECT_DIR)/hw_layer/CJ125_logic.cpp \
+HW_SENSORS_SRC = $(PROJECT_DIR)/hw_layer/sensors/cj125.cpp \
+	$(PROJECT_DIR)/hw_layer/cj125_logic.cpp \
 	$(PROJECT_DIR)/hw_layer/sensors/yaw_rate_sensor.cpp
