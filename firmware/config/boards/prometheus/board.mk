f40eed3cfd9ccbcfa7011bb06771594c3ec88ee0
rusefi <rusefillc@gmail.com>
2019-03-29 11:24:25 -0400

    naming convention

--- firmware/config/boards/prometheus/board.mk
+++ firmware/config/boards/prometheus/board.mk
@@ -1,21 +1,21 @@
 # List of all the board related files.
-BOARDSRC = $(PROJECT_DIR)/config/boards/Prometheus/board.c
+BOARDSRC = $(PROJECT_DIR)/config/boards/prometheus/board.c
 BOARDSRC_CPP = $(PROJECT_DIR)/config/boards/Prometheus/board_configuration.cpp
 
 # Required include directories
-BOARDINC = $(PROJECT_DIR)/config/boards/Prometheus
+BOARDINC = $(PROJECT_DIR)/config/boards/prometheus
 
 # Override LD script
 ifeq ($(USE_BOOTLOADER),yes)
   # include Prometheus bootloader code
-  BOOTLOADERINC= $(PROJECT_DIR)/bootloader/Prometheus/$(PROMETHEUS_BOARD)
+  BOOTLOADERINC= $(PROJECT_DIR)/bootloader/prometheus/$(PROMETHEUS_BOARD)
 endif
 
 ifeq ($(PROMETHEUS_BOARD),405)
-LDSCRIPT= $(PROJECT_DIR)/config/boards/Prometheus/STM32F405xG.ld
+LDSCRIPT= $(PROJECT_DIR)/config/boards/prometheus/STM32F405xG.ld
 DDEFS += -DDEFAULT_ENGINE_TYPE=PROMETHEUS_DEFAULTS -DSTM32F405xx
 else
 # Override DEFAULT_ENGINE_TYPE
-LDSCRIPT= $(PROJECT_DIR)/config/boards/Prometheus/STM32F469xI.ld
+LDSCRIPT= $(PROJECT_DIR)/config/boards/prometheus/STM32F469xI.ld
 DDEFS += -DDEFAULT_ENGINE_TYPE=PROMETHEUS_DEFAULTS -DSTM32F469xx
 endif
