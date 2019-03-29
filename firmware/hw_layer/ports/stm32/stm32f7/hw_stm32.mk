2363d1b4ae910ba544e8116e9782ad8837551081
andreika-git <andreika-git@users.noreply.github.com>
2019-03-29 07:46:48 +0200

    Fix bootloader (#717)
    * Use hw_stm32.mk
    
    * Fix bootloader

--- firmware/hw_layer/ports/stm32/stm32f7/hw_stm32.mk
+++ firmware/hw_layer/ports/stm32/stm32f7/hw_stm32.mk
@@ -1,4 +1,4 @@
 
-
-HW_STM32_SRC_CPP = $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f7/mpu_util.cpp
+HW_LAYER_EMS_CPP += $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f7/mpu_util.cpp \
+	$(PROJECT_DIR)/hw_layer/ports/stm32/stm32_common.cpp
 	
