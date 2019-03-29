d6ff3617436d78ea1c0dc83f6b6a60bb1cd54345
andreika-git <andreika-git@users.noreply.github.com>
2019-03-29 07:46:48 +0200

    Fix bootloader (#717)
    * Use hw_stm32.mk
    
    * Fix bootloader

--- firmware/hw_layer/hw_layer.mk
+++ firmware/hw_layer/hw_layer.mk
@@ -36,19 +36,16 @@ HW_LAYER_EMS_CPP = $(HW_LAYER_EGT_CPP) \
 ifeq ($(PROJECT_CPU),ST_STM32F7)
 HW_LAYER_EMS += $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f7/stm32f7xx_hal_flash.c \
 				$(PROJECT_DIR)/hw_layer/ports/stm32/stm32f7/stm32f7xx_hal_flash_ex.c
-HW_LAYER_EMS_CPP += $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f7/mpu_util.cpp \
-	$(PROJECT_DIR)/hw_layer/ports/stm32/stm32_common.cpp
+include $(PROJECT_DIR)/hw_layer/$(CPU_HWLAYER)/hw_stm32.mk
 endif
 
 ifeq ($(PROJECT_CPU),ST_STM32F4)
 HW_LAYER_EMS += $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f4/stm32f4xx_hal_flash.c \
 				$(PROJECT_DIR)/hw_layer/ports/stm32/stm32f4/stm32f4xx_hal_flash_ex.c
-HW_LAYER_EMS_CPP += $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f4/mpu_util.cpp \
-	$(PROJECT_DIR)/hw_layer/ports/stm32/stm32_common.cpp
+include $(PROJECT_DIR)/hw_layer/$(CPU_HWLAYER)/hw_stm32.mk
 endif
 	
 ifeq ($(PROJECT_CPU),ST_STM32F1)
-HW_LAYER_EMS_CPP += $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f1/mpu_util.cpp \
-	$(PROJECT_DIR)/hw_layer/ports/stm32/stm32_common.cpp
+include $(PROJECT_DIR)/hw_layer/$(CPU_HWLAYER)/hw_stm32.mk
 endif
 	
---
+++ firmware/hw_layer/ports/stm32/stm32f1/hw_stm32.mk
@@ -0,0 +1,3 @@
+
+HW_LAYER_EMS_CPP += $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f1/mpu_util.cpp \
+	$(PROJECT_DIR)/hw_layer/ports/stm32/stm32_common.cpp
