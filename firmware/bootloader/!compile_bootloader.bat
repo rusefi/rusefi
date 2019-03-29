76b8be870ed6ef04a0359731f6096c45c37cafda
rusefi <rusefillc@gmail.com>
2019-03-29 11:24:53 -0400

    version into file

--- firmware/bootloader/!compile_bootloader.bat
+++ firmware/bootloader/!compile_bootloader.bat
@@ -16,7 +16,7 @@ echo Bootloader build success.
 
 cd blbuild
 rem Generate a header file with binary bootloader code
-java -jar ../../../java_tools/bin2header.jar bootloader.bin %BOOTLOADER_CODE_DESTINATION_FILE% "static const volatile uint8_t bootloader_code[] BOOTLOADER_SECTION"
+java -jar ../../../java_tools/bin2header.jar bootloader.bin %BOOTLOADER_CODE_DESTINATION_FILE% "%BOOTLOADER_COMMENT% static const volatile uint8_t bootloader_code[] BOOTLOADER_SECTION"
 cd ..
 
 rem Touch 'bootloader_storage.c' to update its modification date (needed for make)
