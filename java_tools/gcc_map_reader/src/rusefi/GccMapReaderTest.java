aa67ad0079dd542de82130d0d72bc8304a25e973
rusefi <rusefillc@gmail.com>
2019-03-31 16:36:57 -0400

    generate_memory_usage_report/gcc_map_reader BUG? #721

--- java_tools/gcc_map_reader/src/rusefi/GccMapReaderTest.java
+++ java_tools/gcc_map_reader/src/rusefi/GccMapReaderTest.java
@@ -3,7 +3,6 @@ package rusefi;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -13,8 +12,13 @@ public class GccMapReaderTest {
     private static final String BSS = "bss";
 
     @Test
-    public void testSingleLine() {
-        List<GccMapReader.Record> r = GccMapReader.process(Collections.singletonList(" .bss.PWMD1     0x1fff9a0c       0x18 build_kinetis/obj/hal_pwm_lld.o"), BSS);
+    public void testSimpleLinesWithTagInTheMiddle() {
+        List<GccMapReader.Record> r = GccMapReader.process(Arrays.asList(
+                " .bss.PWMD1     0x1fff9a0c       0x18 build_kinetis/obj/hal_pwm_lld.o",
+                GccMapReader.START_OF_DATA_TAG,
+                " .bss.PWMD2     0x1fff9a0c       0x18 build_kinetis/obj/hal_pwm_lld.o"
+
+        ), BSS);
         assertNotNull(r);
         assertEquals(1, r.size());
         assertEquals(0x18, r.get(0).getSize());
@@ -23,6 +27,7 @@ public class GccMapReaderTest {
     @Test
     public void testMultiLine() {
         List<GccMapReader.Record> r = GccMapReader.process(Arrays.asList(
+                GccMapReader.START_OF_DATA_TAG,
                 " .bss._ZL12turnOffEvent",
                 "0x1fff9db8       0x60 build_kinetis/obj/aux_valves.o"), BSS);
         assertNotNull(r);
@@ -33,6 +38,7 @@ public class GccMapReaderTest {
     @Test
     public void testThreeLine() {
         List<GccMapReader.Record> r = GccMapReader.process(Arrays.asList(
+                GccMapReader.START_OF_DATA_TAG,
                 " .bss.ch_idle_thread_wa",
                 "                0x1fff8d10      0x610 build_kinetis/obj/chsys.o",
                 "                0x1fff8d10                ch_idle_thread_wa"), BSS);
@@ -44,6 +50,7 @@ public class GccMapReaderTest {
     @Test
     public void testIssue() {
         List<GccMapReader.Record> r = GccMapReader.process(Arrays.asList(
+                GccMapReader.START_OF_DATA_TAG,
                 ".bss            0x1fff8d10     0xf2e4 load address 0x0002b1bc",
                 "                0x1fff8d10                . = ALIGN (0x4)",
                 "                0x1fff8d10                _bss_start = .",
@@ -62,7 +69,9 @@ public class GccMapReaderTest {
 
     @Test
     public void testData() {
-        String[] strings = {" .text._dbg_check_suspend",
+        String[] strings = {
+                GccMapReader.START_OF_DATA_TAG,
+                " .text._dbg_check_suspend",
                 "                0x00000000       0x1c build_kinetis/obj/chdebug.o",
                 " .rodata._dbg_check_disable.str1.4",
                 "                0x00000000        0x5 build_kinetis/obj/chdebug.o",
