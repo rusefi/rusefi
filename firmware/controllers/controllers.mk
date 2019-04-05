f9fa7604bcd177184d6c599fd6b0f7d855c2f277
rusefi <rusefillc@gmail.com>
2019-04-05 01:02:34 -0400

    The Big Refactoring of 2019: folder structure #723

--- firmware/controllers/controllers.mk
+++ firmware/controllers/controllers.mk
@@ -1,8 +1,13 @@
+CONTROLLERS_DIR=$(PROJECT_DIR)/controllers
 
 CONTROLLERSSRC =
 	
-CONTROLLERS_SRC_CPP = $(PROJECT_DIR)/controllers/settings.cpp \
-	$(PROJECT_DIR)/controllers/electronic_throttle.cpp \
+CONTROLLERS_SRC_CPP = \
+	$(CONTROLLERS_DIR)/actuators/electronic_throttle.cpp \
+	$(CONTROLLERS_DIR)/actuators/alternator_controller.cpp \
+	$(CONTROLLERS_DIR)/actuators/malfunction_indicator.cpp \
+	$(CONTROLLERS_DIR)/actuators/tachometer.cpp \
+	$(PROJECT_DIR)/controllers/settings.cpp \
 	$(PROJECT_DIR)/controllers/error_handling.cpp \
 	$(PROJECT_DIR)/controllers/map_averaging.cpp \
 	$(PROJECT_DIR)/controllers/flash_main.cpp \
@@ -10,9 +15,11 @@ CONTROLLERS_SRC_CPP = $(PROJECT_DIR)/controllers/settings.cpp \
 	$(PROJECT_DIR)/controllers/idle_thread.cpp \
 	$(PROJECT_DIR)/controllers/pwm_tester.cpp \
 	$(PROJECT_DIR)/controllers/obd2.cpp \
-	$(PROJECT_DIR)/controllers/malfunction_indicator.cpp \
-	$(PROJECT_DIR)/controllers/alternator_controller.cpp \
 	$(PROJECT_DIR)/controllers/lcd_controller.cpp \
-	$(PROJECT_DIR)/controllers/tachometer.cpp \
  	$(PROJECT_DIR)/controllers/engine_controller.cpp \
 	$(PROJECT_DIR)/controllers/persistent_store.cpp
+
+CONTROLLERS_INC=\
+	$(CONTROLLERS_DIR) \
+	$(CONTROLLERS_DIR)/actuators \
+
