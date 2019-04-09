77ede53ef573a7aa72215b77797cec8d58ae555b
rusefi <rusefillc@gmail.com>
2019-04-09 18:37:25 -0400

    reducing duplication removing unused (?) defines removing value space overlap between PROJECT_BOARD and PROJECT_CPU

--- firmware/rusefi.mk
+++ firmware/rusefi.mk
@@ -8,32 +8,30 @@ $(error Please run 'make' again)
 endif
 
 ifeq ($(PROJECT_BOARD),)
-  PROJECT_BOARD = ST_STM32F4
+  PROJECT_BOARD = st_stm32f4
 endif
-DDEFS += -D$(PROJECT_BOARD)
 
 ifeq ($(PROJECT_CPU),)
-  PROJECT_CPU = ST_STM32F4
+  PROJECT_CPU = ARCH_STM32F4
 endif
-DDEFS += -D$(PROJECT_CPU)
 
 -include $(PROJECT_DIR)/config/boards/$(PROJECT_BOARD)/config.mk
 
 # CPU-dependent defs
-ifeq ($(PROJECT_CPU),ST_STM32F7)
+ifeq ($(PROJECT_CPU),ARCH_STM32F7)
 CPU_STARTUP = startup_stm32f7xx.mk
 CPU_PLATFORM = STM32F7xx/platform.mk
 CPU_HWLAYER = ports/stm32/stm32f7
 endif
 
-ifeq ($(PROJECT_CPU),ST_STM32F4)
+ifeq ($(PROJECT_CPU),ARCH_STM32F4)
 CPU_STARTUP = startup_stm32f4xx.mk
 CPU_PLATFORM = STM32F4xx/platform.mk
 CPU_HWLAYER = ports/stm32/stm32f4
 endif
 
 
-ifeq ($(PROJECT_CPU),ST_STM32F1)
+ifeq ($(PROJECT_CPU),ARCH_STM32F1)
 CPU_STARTUP = startup_stm32f1xx.mk
 CPU_PLATFORM = STM32F1xx/platform.mk
 CPU_HWLAYER = ports/stm32/stm32f1
