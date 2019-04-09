12e07bc3ab1420f275560a0fd5272f1468bfa0a7
dron0gus <dron0gus@gmail.com>
2019-04-10 02:12:35 +0300

    Some more include fixes for linux (#738)
    * Some more include fixes for linux
    
    * Pass cross-compile path through CROSS_COMPILE
    
    * Lower-upper case conversion fixes for linux

--- firmware/config/boards/st_stm32f4/board.mk
+++ firmware/config/boards/st_stm32f4/board.mk
@@ -1,9 +1,9 @@
 # List of all the board related files.
-BOARDSRC = $(PROJECT_DIR)/config/boards/ST_STM32F4/board.c
+BOARDSRC = $(PROJECT_DIR)/config/boards/st_stm32f4/board.c
 BOARDSRC_CPP =
 
 # Required include directories
-BOARDINC = $(PROJECT_DIR)/config/boards/ST_STM32F4
+BOARDINC = $(PROJECT_DIR)/config/boards/st_stm32f4
 
 # MCU defines
 DDEFS += -DSTM32F407xx
