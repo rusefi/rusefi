5316d7ad6a247d7d3761622fb43d623fa6d6a31f
rusefi <rusefillc@gmail.com>
2019-04-09 23:01:45 -0400

    The Big Refactoring of 2019: folder structure #723

--- firmware/controllers/controllers.mk
+++ firmware/controllers/controllers.mk
@@ -10,17 +10,21 @@ CONTROLLERS_SRC_CPP = \
 	$(CONTROLLERS_DIR)/actuators/idle_thread.cpp \
 	$(CONTROLLERS_DIR)/actuators/pwm_tester.cpp \
 	$(CONTROLLERS_DIR)/actuators/algo/aux_pid.cpp \
-	$(CONTROLLERS_DIR)/system/signal_executor_sleep.cpp \
+	$(CONTROLLERS_DIR)/actuators/lcd_controller.cpp \
+	$(CONTROLLERS_DIR)/scheduling/signal_executor_sleep.cpp \
+	$(CONTROLLERS_DIR)/scheduling/signal_executor.cpp \
 	$(CONTROLLERS_DIR)/scheduling/single_timer_executor.cpp \
+	$(CONTROLLERS_DIR)/scheduling/pwm_generator_logic.cpp \
+	$(CONTROLLERS_DIR)/scheduling/event_queue.cpp \
 	$(PROJECT_DIR)/controllers/settings.cpp \
 	$(PROJECT_DIR)/controllers/error_handling.cpp \
 	$(PROJECT_DIR)/controllers/map_averaging.cpp \
 	$(PROJECT_DIR)/controllers/flash_main.cpp \
 	$(PROJECT_DIR)/controllers/injector_central.cpp \
 	$(PROJECT_DIR)/controllers/obd2.cpp \
-	$(PROJECT_DIR)/controllers/lcd_controller.cpp \
  	$(PROJECT_DIR)/controllers/engine_controller.cpp \
-	$(PROJECT_DIR)/controllers/persistent_store.cpp
+	$(PROJECT_DIR)/controllers/persistent_store.cpp \
+	
 
 CONTROLLERS_INC=\
 	$(CONTROLLERS_DIR) \
--- firmware/controllers/system/system.mk
+++ firmware/controllers/system/system.mk
@@ -1,8 +1,8 @@
 
 SYSTEMSRC =
 
-SYSTEMSRC_CPP =	$(PROJECT_DIR)/controllers/system/pwm_generator_logic.cpp \
-	$(PROJECT_DIR)/controllers/system/event_queue.cpp \
+SYSTEMSRC_CPP =	\
 	$(PROJECT_DIR)/controllers/system/efi_gpio.cpp \
-	$(PROJECT_DIR)/controllers/system/dc_motor.cpp
+	$(PROJECT_DIR)/controllers/system/dc_motor.cpp \
+	
 	
