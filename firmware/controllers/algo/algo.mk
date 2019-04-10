f554572c54cdb34de9d193f6459fb815fc88f58c
rusefi <rusefillc@gmail.com>
2019-04-09 23:01:45 -0400

    The Big Refactoring of 2019: folder structure #723

--- firmware/controllers/algo/algo.mk
+++ firmware/controllers/algo/algo.mk
@@ -1,6 +1,8 @@
 
 CONTROLLERS_ALGO_SRC = $(PROJECT_DIR)/controllers/algo/map_adjuster.c \
-	$(PROJECT_DIR)/controllers/algo/nmea.c
+	$(PROJECT_DIR)/controllers/algo/nmea.c \
+	
+	
 	
 CONTROLLERS_ALGO_SRC_CPP = $(PROJECT_DIR)/controllers/algo/advance_map.cpp \
 	$(PROJECT_DIR)/controllers/algo/malfunction_central.cpp \
@@ -13,4 +15,4 @@ CONTROLLERS_ALGO_SRC_CPP = $(PROJECT_DIR)/controllers/algo/advance_map.cpp \
 	$(PROJECT_DIR)/controllers/algo/lcd_menu_tree.cpp \
 	$(PROJECT_DIR)/controllers/algo/event_registry.cpp \
 	$(PROJECT_DIR)/controllers/algo/algo.cpp \
-	$(PROJECT_DIR)/controllers/algo/signal_executor.cpp
+	
