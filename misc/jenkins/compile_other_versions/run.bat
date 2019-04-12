2cf0467bc4ca94af003ed7672b5d15e680910d25
rusefi <rusefillc@gmail.com>
2019-04-12 17:44:05 -0400

    prometheus CI

--- misc/jenkins/compile_other_versions/run.bat
+++ misc/jenkins/compile_other_versions/run.bat
@@ -45,3 +45,31 @@ set bundle_name=stm32f767_osc
 call misc\jenkins\compile_other_versions\build_version.bat
 echo Done with F767-osc
 
+
+
+cd firmware
+call clean.bat
+cd ..
+echo Compiling prometheus_405
+cd firmware\config\boards\prometheus
+call !compile-prometheus-405.bat
+cd ..
+pwd
+set bundle_name=prometheus_405
+call misc\jenkins\compile_other_versions\build_version.bat
+echo Done with prometheus_405
+
+
+cd firmware
+call clean.bat
+cd ..
+echo Compiling prometheus_469
+cd firmware\config\boards\prometheus
+call !compile-prometheus-469.bat
+cd ..
+pwd
+set bundle_name=prometheus_469
+call misc\jenkins\compile_other_versions\build_version.bat
+echo Done with prometheus_469
+
+
