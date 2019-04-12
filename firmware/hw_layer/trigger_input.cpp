903815532df9036f7b4785c350b441dc76490559
rusefi <rusefillc@gmail.com>
2019-04-12 18:10:47 -0400

    fix or safer code?

--- firmware/hw_layer/trigger_input.cpp
+++ firmware/hw_layer/trigger_input.cpp
@@ -14,7 +14,7 @@
 
 #include "global.h"
 
-#if (EFI_SHAFT_POSITION_INPUT && (HAL_USE_PAL == TRUE) || (HAL_USE_ICU == TRUE)) || defined(__DOXYGEN__)
+#if (EFI_SHAFT_POSITION_INPUT && (HAL_USE_PAL == TRUE || HAL_USE_ICU == TRUE)) || defined(__DOXYGEN__)
 
 #include "trigger_input.h"
 #include "digital_input_hw.h"
@@ -314,4 +314,4 @@ void applyNewTriggerInputPins(void) {
 	startTriggerInputPins();
 }
 
-#endif /* (EFI_SHAFT_POSITION_INPUT && (HAL_USE_PAL == TRUE) || (HAL_USE_ICU == TRUE)) */
+#endif /* (EFI_SHAFT_POSITION_INPUT && (HAL_USE_PAL == TRUE || HAL_USE_ICU == TRUE)) */
