a1ea65ed22805eb1f1930b46580144a4c4b6e3d9
rusefi <rusefillc@gmail.com>
2019-04-13 16:50:24 -0400

    more messages for build server

--- firmware/clean_compile_two_versions.bat
+++ firmware/clean_compile_two_versions.bat
@@ -7,6 +7,8 @@ make
 cp build\rusefi.elf deliver\rusefi_debug.elf
 cp build\rusefi.bin deliver\rusefi_debug.bin
 cp build\rusefi.hex deliver\rusefi_debug.hex
+echo Debug compilation results
+ls -l build
 
 call clean.bat
 make DEBUG_LEVEL_OPT='-O2'  EXTRA_PARAMS='-DEFI_ENABLE_ASSERTS=FALSE -DCH_DBG_ENABLE_TRACE=FALSE -DCH_DBG_ENABLE_ASSERTS=FALSE -DCH_DBG_ENABLE_STACK_CHECK=FALSE -DCH_DBG_FILL_THREADS=FALSE -DCH_DBG_THREADS_PROFILING=FALSE'
@@ -14,4 +16,5 @@ make DEBUG_LEVEL_OPT='-O2'  EXTRA_PARAMS='-DEFI_ENABLE_ASSERTS=FALSE -DCH_DBG_EN
 cp build\rusefi.elf deliver\rusefi_release.elf
 cp build\rusefi.bin deliver\rusefi_release.bin
 cp build\rusefi.hex deliver\rusefi_release.hex
-
+echo Release compilation results
+ls -l build
