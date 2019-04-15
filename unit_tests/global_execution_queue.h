d4dcfac535c7df885721a8d976601c69bc1d094b
rusefi <rusefillc@gmail.com>
2019-04-15 19:24:46 -0400

    random cleanup

--- unit_tests/global_execution_queue.h
+++ unit_tests/global_execution_queue.h
@@ -1,5 +1,5 @@
 /*
- * global_execution_queue.h
+ * @file global_execution_queue.h
  *
  *  Created on: Jan 9, 2019
  * @author Andrey Belomutskiy, (c) 2012-2019
@@ -13,7 +13,6 @@
 
 class TestExecutor : public ExecutorInterface {
 public:
-	TestExecutor();
 	void scheduleByTimestamp(scheduling_s *scheduling, efitimeus_t timeUs, schfunc_t callback, void *param);
 	void scheduleForLater(scheduling_s *scheduling, int delayUs, schfunc_t callback, void *param);
 	void clear();
