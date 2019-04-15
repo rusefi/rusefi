2cf166373e16a765ae986534c98b33765954bdf3
dron0gus <dron0gus@gmail.com>
2019-04-15 07:30:56 +0300

    Subaru EJ20G board: fix compilation after directory rename (#756)

--- firmware/config/boards/subaru-ej20gn/board.mk
+++ firmware/config/boards/subaru-ej20gn/board.mk
@@ -1,17 +1,17 @@
 # List of all the board related files.
-BOARDSRC = $(PROJECT_DIR)/config/boards/subaruej20g/board.c
-BOARDSRC_CPP = $(PROJECT_DIR)/config/boards/subaruej20g/board_configuration.cpp
+BOARDSRC = $(PROJECT_DIR)/config/boards/subaru-ej20gn/board.c
+BOARDSRC_CPP = $(PROJECT_DIR)/config/boards/subaru-ej20gn/board_configuration.cpp
 
 # Required include directories
-BOARDINC = $(PROJECT_DIR)/config/boards/subaruej20g
+BOARDINC = $(PROJECT_DIR)/config/boards/subaru-ej20gn
 
 # Override LD script
 ifeq ($(USE_BOOTLOADER),yes)
   # include Prometheus bootloader code
-  BOOTLOADERINC= $(PROJECT_DIR)/bootloader/subaruej20g
+  BOOTLOADERINC= $(PROJECT_DIR)/bootloader/subaru-ej20gn
 endif
 
-LDSCRIPT= $(PROJECT_DIR)/config/boards/subaruej20g/STM32F76xxI.ld
+LDSCRIPT= $(PROJECT_DIR)/config/boards/subaru-ej20gn/STM32F76xxI.ld
 
 # Override DEFAULT_ENGINE_TYPE
 DDEFS += -DDEFAULT_ENGINE_TYPE=SUBARUEJ20G_DEFAULTS -DSTM32F765xx
