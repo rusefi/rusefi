89dfd605eb81040d89ca398121803a3f94213e5a
rusefi <rusefillc@gmail.com>
2019-04-19 13:38:13 -0400

    non-default CI issues

--- firmware/config/boards/nucleo_f746/!compile-nucleo_f746.bat
+++ firmware/config/boards/nucleo_f746/!compile-nucleo_f746.bat
@@ -3,7 +3,7 @@ rem STM32F767 version of the firmware for Nucleo-F746ZG board
 rem
 
 cd ../../..
-set PROJECT_BOARD=NUCLEO_F746
+set PROJECT_BOARD=nucleo_F746
 set PROJECT_CPU=ST_STM32F7
 set EXTRA_PARAMS=-DDUMMY -DSTM32F746xx ^
  -DEFI_INJECTOR_PIN3=GPIO_UNASSIGNED ^
--- firmware/config/boards/nucleo_f767/!compile-nucleo_f767.bat
+++ firmware/config/boards/nucleo_f767/!compile-nucleo_f767.bat
@@ -3,7 +3,7 @@ rem STM32F767 version of the firmware for Nucleo-F767ZI board
 rem
 
 cd ../../..
-set PROJECT_BOARD=NUCLEO_F767
+set PROJECT_BOARD=nucleo_f767
 set PROJECT_CPU=ST_STM32F7
 set EXTRA_PARAMS=-DDUMMY -DSTM32F767xx ^
  -DEFI_INJECTOR_PIN3=GPIO_UNASSIGNED ^
@@ -13,6 +13,9 @@ set EXTRA_PARAMS=-DDUMMY -DSTM32F767xx ^
  -DCH_DBG_ENABLE_CHECKS=FALSE -DCH_DBG_ENABLE_TRACE=FALSE -DCH_DBG_ENABLE_ASSERTS=FALSE -DCH_DBG_ENABLE_STACK_CHECK=FALSE -DCH_DBG_FILL_THREADS=FALSE -DCH_DBG_THREADS_PROFILING=FALSE
 set DEBUG_LEVEL_OPT="-O2"
 make -j4
+if not exist build/rusefi.hex echo FAILED to compile nucleo_f767
+if not exist build/rusefi.hex exit -1
+
 ..\misc\hex2dfu\HEX2DFU.exe build/rusefi.hex -out build/rusefi.dfu
 
 rem Clean up so that no one inherits this value
