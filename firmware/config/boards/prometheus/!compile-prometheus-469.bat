1dd36f996e10ba25f77e7e61ef728ff97f888d03
rusefi <rusefillc@gmail.com>
2019-04-19 13:38:13 -0400

    non-default CI issues

--- firmware/config/boards/prometheus/!compile-prometheus-469.bat
+++ firmware/config/boards/prometheus/!compile-prometheus-469.bat
@@ -3,7 +3,7 @@ rem STM32F469 version of the firmware for https://rusefi.com/forum/viewtopic.php
 rem
 
 cd ../../..
-set PROJECT_BOARD=Prometheus
+set PROJECT_BOARD=prometheus
 set PROMETHEUS_BOARD=469
 set EXTRA_PARAMS="-DDUMMY -DSTM32F469xx -DEFI_ENABLE_ASSERTS=FALSE -DCH_DBG_ENABLE_TRACE=FALSE -DCH_DBG_ENABLE_ASSERTS=FALSE -DCH_DBG_ENABLE_STACK_CHECK=FALSE -DCH_DBG_FILL_THREADS=FALSE -DCH_DBG_THREADS_PROFILING=FALSE"
 set DEBUG_LEVEL_OPT="-O2"
@@ -11,6 +11,7 @@ set USE_BOOTLOADER=yes
 rem call compile.bat -r
 rem temp stuff trying to fix CI
 make clean
+mkdir .dep
 make -r
 
 rem Clean up so that no one inherits this value
