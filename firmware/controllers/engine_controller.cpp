97c4b845453142da3a76d94a4e57d8fbd242d98e
rusefi <rusefillc@gmail.com>
2019-04-20 17:35:24 -0400

    USB and TLE8888 not working for F4 or F767 #764 this RAM would be needed to run three smart SPI threads

--- firmware/controllers/engine_controller.cpp
+++ firmware/controllers/engine_controller.cpp
@@ -786,7 +786,7 @@ void initEngineContoller(Logging *sharedLogger DECLARE_ENGINE_PARAMETER_SUFFIX) 
 // help to notice when RAM usage goes up - if a code change adds to RAM usage these variables would fail
 // linking process which is the way to raise the alarm
 #ifndef RAM_UNUSED_SIZE
-#define RAM_UNUSED_SIZE 10000
+#define RAM_UNUSED_SIZE 8600
 #endif
 #ifndef CCM_UNUSED_SIZE
 #define CCM_UNUSED_SIZE 6800
