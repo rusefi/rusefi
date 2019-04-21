3d5338c1de973774400065d797a1e190a5a8f6ae
rusefi <rusefillc@gmail.com>
2019-04-21 12:24:31 -0400

    docs

--- firmware/controllers/actuators/electronic_throttle.cpp
+++ firmware/controllers/actuators/electronic_throttle.cpp
@@ -414,25 +414,26 @@ void setDefaultEtbBiasCurve(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 	engineConfiguration->etbBiasBins[0] = 0;
 	engineConfiguration->etbBiasBins[1] = 1;
 	engineConfiguration->etbBiasBins[2] = 2;
+	/**
+	 * This specific throttle has default position of about 4% open
+	 */
 	engineConfiguration->etbBiasBins[3] = 4;
 	engineConfiguration->etbBiasBins[4] = 7;
 	engineConfiguration->etbBiasBins[5] = 98;
 	engineConfiguration->etbBiasBins[6] = 99;
 	engineConfiguration->etbBiasBins[7] = 100;
 
-//	engineConfiguration->etbBiasValues[0] = -100 / 255.0f * 100;
-//	engineConfiguration->etbBiasValues[1] = -95 / 255.0f * 100;
-//	engineConfiguration->etbBiasValues[2] = -80 / 255.0f * 100;
-//	engineConfiguration->etbBiasValues[3] = 0 / 255.0f * 100;
-//	engineConfiguration->etbBiasValues[4] = 115 / 255.0f * 100;
-//	engineConfiguration->etbBiasValues[5] = 142 / 255.0f * 100;
-//	engineConfiguration->etbBiasValues[6] = 142 / 255.0f * 100;
-//	engineConfiguration->etbBiasValues[7] = 142 / 255.0f * 100;
-
+	/**
+	 * Some negative bias for below-default position
+	 */
 	engineConfiguration->etbBiasValues[0] = -20;
 	engineConfiguration->etbBiasValues[1] = -18;
 	engineConfiguration->etbBiasValues[2] = -17;
-	engineConfiguration->etbBiasValues[3] = 0 / 255.0f * 100;
+	/**
+	 * Zero bias for index which corresponds to default throttle position, when no current is applied
+	 * This specific throttle has default position of about 4% open
+	 */
+	engineConfiguration->etbBiasValues[3] = 0;
 	engineConfiguration->etbBiasValues[4] = 20;
 	engineConfiguration->etbBiasValues[5] = 21;
 	engineConfiguration->etbBiasValues[6] = 22;
