892be38a036e2039d4c2d3cc864a503eed981663
rusEfi <russianefi@gmail.com>
2019-04-30 19:22:12 -0400

    ETB tiny progress

--- firmware/controllers/engine_controller.cpp
+++ firmware/controllers/engine_controller.cpp
@@ -807,6 +807,6 @@ int getRusEfiVersion(void) {
 	if (initBootloader() != 0)
 		return 123;
 #endif /* EFI_BOOTLOADER_INCLUDE_CODE */
-	return 20190425;
+	return 20190430;
 }
 #endif /* EFI_UNIT_TEST */
