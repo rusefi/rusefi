01d70c86517661c18ead33bd55fc73c2e1af5c8d
960 <olaruud@hotmail.com>
2019-05-01 17:10:55 +0200

    Add files via upload

---
+++ firmware/config/engines/me7pnp.cpp
@@ -0,0 +1,18 @@
+/*
+ * me7pnp.cpp
+ *
+ *  Created on: 1. mai 2019
+ *      Author: Ola
+ */
+
+
+
+
+#include "me7pnp.h"
+#include "custom_engine.h"
+
+EXTERN_ENGINE;
+
+void vag_18_Turbo(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
+
+}
---
+++ firmware/config/engines/me7pnp.h
@@ -0,0 +1,19 @@
+/*
+ * me7pnp.h
+ *
+ *  Created on: 1. mai 2019
+ *      Author: Ola
+ */
+
+#ifndef CONFIG_ENGINES_ME7PNP_H_
+#define CONFIG_ENGINES_ME7PNP_H_
+
+#include "engine.h"
+
+void vag_18_Turbo(DECLARE_ENGINE_PARAMETER_SIGNATURE);
+
+
+
+
+
+#endif /* CONFIG_ENGINES_ME7PNP_H_ */
