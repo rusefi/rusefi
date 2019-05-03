e946964bbaf09d116372a2ea7790f7ea3958aeaf
rusefi <rusefillc@gmail.com>
2019-05-03 15:01:29 -0400

    more visible console version

--- java_console/ui/src/com/rusefi/Launcher.java
+++ java_console/ui/src/com/rusefi/Launcher.java
@@ -46,7 +46,7 @@ import static com.rusefi.ui.storage.PersistentConfiguration.getConfig;
  * @see EngineSnifferPanel
  */
 public class Launcher {
-    public static final int CONSOLE_VERSION = 20190502;
+    public static final int CONSOLE_VERSION = 20190503;
     public static final boolean SHOW_STIMULATOR = false;
     private static final String TAB_INDEX = "main_tab";
     protected static final String PORT_KEY = "port";
