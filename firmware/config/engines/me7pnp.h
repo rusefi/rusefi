d8747359af182d28b02a5a69329d66b435caea14
rusefi <rusefillc@gmail.com>
2019-05-03 18:44:59 -0400

    step towards me7pnp

---
+++ firmware/config/engines/me7pnp.h
@@ -0,0 +1,15 @@
+/*
+ * me7pnp.h
+ *
+ *  Created on: 1. mai 2019
+ *      Author: Ola
+ */
+
+#ifndef CONFIG_ENGINES_ME7PNP_H_
+#define CONFIG_ENGINES_ME7PNP_H_
+
+#include "engine_configuration.h"
+
+void vag_18_Turbo(DECLARE_CONFIG_PARAMETER_SIGNATURE);
+
+#endif /* CONFIG_ENGINES_ME7PNP_H_ */
