6817f3cb2da228259788dbca3d61e52e6855e4a6
960 <olaruud@hotmail.com>
2019-05-05 03:45:06 +0200

    Add files via upload
    Cleanup

--- firmware/config/boards/me7_pnp/board_configuration.cpp
+++ firmware/config/boards/me7_pnp/board_configuration.cpp
@@ -5,49 +5,47 @@
  *
  * @author Andrey Belomutskiy, (c) 2012-2019
  */
-
 #include "global.h"
 #include "engine.h"
 #include "engine_math.h"
 #include "allsensors.h"
 #include "fsio_impl.h"
 #include "engine_configuration.h"
+#include "smart_gpio.h"
 
-EXTERN_ENGINE;
+EXTERN_ENGINE
+;
 
-void setPinConfigurationOverrides(void) {
+static void vag_18_Turbo(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 }
-
 void setSerialConfigurationOverrides(void) {
 	boardConfiguration->useSerialPort = true;
-	engineConfiguration->binarySerialTxPin = GPIOD_8;
-	engineConfiguration->binarySerialRxPin = GPIOD_9;
-	engineConfiguration->consoleSerialTxPin = GPIOD_8;
-	engineConfiguration->consoleSerialRxPin = GPIOD_9;
-//	boardConfiguration->tunerStudioSerialSpeed = SERIAL_SPEED;
-//	engineConfiguration->uartConsoleSerialSpeed = SERIAL_SPEED;
+
+	//UART
+
+	engineConfiguration->binarySerialTxPin = GPIOB_10;
+	engineConfiguration->binarySerialRxPin = GPIOB_11;
+	engineConfiguration->consoleSerialTxPin = GPIOB_10;
+	engineConfiguration->consoleSerialRxPin = GPIOB_11;
+
 }
 
-void setSdCardConfigurationOverrides(void) {
-}
+void setPinConfigurationOverrides(void) {
 
-/**
- * @brief   Board-specific configuration code overrides.
- * @todo    Add your board-specific code, if any.
- */
-void setBoardConfigurationOverrides(void) {
-	setSerialConfigurationOverrides();
+	//NOT USED
 
-	engineConfiguration->runningLedPin = GPIOB_0; //green LED
-	engineConfiguration->warningLedPin = GPIO_UNASSIGNED;
-#if 0
-	engineConfiguration->vbattAdcChannel = EFI_ADC_13;
-	engineConfiguration->adcVcc = ADC_VCC;
-#endif
 	engineConfiguration->baroSensor.hwChannel = EFI_ADC_NONE;
-	engineConfiguration->throttlePedalPositionAdcChannel = EFI_ADC_NONE;
+	engineConfiguration->afr.hwChannel = EFI_ADC_NONE;
+	boardConfiguration->ignitionPins[8] = GPIO_UNASSIGNED;
+	boardConfiguration->ignitionPins[9] = GPIO_UNASSIGNED;
+	boardConfiguration->mainRelayPin = GPIO_UNASSIGNED;
+	boardConfiguration->idle.solenoidPin = GPIO_UNASSIGNED;
+	boardConfiguration->fanPin = GPIO_UNASSIGNED;
 
-	// not used
+}
+void setBoardConfigurationOverrides(void) {
+
+	// NOT USED
 	engineConfiguration->dizzySparkOutputPin = GPIO_UNASSIGNED;
 	engineConfiguration->externalKnockSenseAdc = EFI_ADC_NONE;
 	engineConfiguration->displayMode = DM_NONE;
@@ -57,22 +55,31 @@ void setBoardConfigurationOverrides(void) {
 	boardConfiguration->HD44780_db5 = GPIO_UNASSIGNED;
 	boardConfiguration->HD44780_db6 = GPIO_UNASSIGNED;
 	boardConfiguration->HD44780_db7 = GPIO_UNASSIGNED;
-	for (int i = 0; i < DIGIPOT_COUNT ; i++) {
+	for (int i = 0; i < DIGIPOT_COUNT; i++) {
 		boardConfiguration->digitalPotentiometerChipSelect[i] = GPIO_UNASSIGNED;
 	}
 	boardConfiguration->triggerSimulatorPins[1] = GPIO_UNASSIGNED;
 	boardConfiguration->triggerSimulatorPins[2] = GPIO_UNASSIGNED;
 	boardConfiguration->triggerSimulatorPinModes[1] = OM_DEFAULT;
 	boardConfiguration->triggerSimulatorPinModes[2] = OM_DEFAULT;
-	boardConfiguration->vehicleSpeedSensorInputPin = GPIO_UNASSIGNED;
 	boardConfiguration->boardTestModeJumperPin = GPIO_UNASSIGNED;
-	boardConfiguration->acRelayPin = GPIO_UNASSIGNED;
 	boardConfiguration->digitalPotentiometerSpiDevice = SPI_NONE;
 	boardConfiguration->max31855spiDevice = SPI_NONE;
+	engineConfiguration->warningLedPin = GPIO_UNASSIGNED;
+	engineConfiguration->runningLedPin = GPIO_UNASSIGNED;
+	boardConfiguration->useStepperIdle = false;
+	boardConfiguration->idle.stepperDirectionPin = GPIO_UNASSIGNED;
+	boardConfiguration->idle.stepperStepPin = GPIO_UNASSIGNED;
+	engineConfiguration->stepperEnablePin = GPIO_UNASSIGNED;
+	engineConfiguration->stepperEnablePinMode = OM_DEFAULT;
+	boardConfiguration->injectionPins[8] = GPIO_UNASSIGNED;
+	boardConfiguration->injectionPins[9] = GPIO_UNASSIGNED;
+	boardConfiguration->injectionPins[10] = GPIO_UNASSIGNED;
+	boardConfiguration->injectionPins[11] = GPIO_UNASSIGNED;
+	boardConfiguration->isHip9011Enabled = false;
 
-	/////////////////////////////////////////////////////////
-	boardConfiguration->is_enabled_spi_1 = false;
-	boardConfiguration->is_enabled_spi_2 = false;
-	boardConfiguration->is_enabled_spi_3 = false;
+}
+
+void setSdCardConfigurationOverrides(void) {
 }
 
