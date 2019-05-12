13e57187d7b4a16d2338f99af24e2e44741662f7
rusefi <rusefillc@gmail.com>
2019-05-12 13:21:50 -0400

    OBD-II at least something? #214 docs

--- firmware/controllers/obd2_via_can.c
+++ firmware/controllers/obd2_via_can.c
@@ -5,6 +5,11 @@
  * http://en.wikipedia.org/wiki/OBD-II_PIDs#CAN_.2811-bit.29_Bus_format
  * standards.sae.org/j2284/
  *
+ * OBD-II
+ *
+ * @see obd2.cpp
+ * TODO: remove this dead file?
+ *
  * @date Feb 14, 2014
  * @author Andrey Belomutskiy, (c) 2012-2018
  */
