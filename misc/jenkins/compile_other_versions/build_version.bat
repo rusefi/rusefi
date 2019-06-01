405f06efe36a1082acaf9843cbf38d594f1307f6
rusefi <rusefillc@gmail.com>
2019-06-01 12:34:29 -0400

    jenkins cleanup

--- misc/jenkins/compile_other_versions/build_version.bat
+++ misc/jenkins/compile_other_versions/build_version.bat
@@ -17,7 +17,11 @@ echo put rusefi_bundle.zip rusefi_bundle_%bundle_name%.zip >> ftp_commands.txt
 echo exit >> ftp_commands.txt
 
 cd temp
-call winscp.com /script=../ftp_commands.txt
-IF NOT ERRORLEVEL 0 echo winscp error DETECTED
-IF NOT ERRORLEVEL 0 EXIT /B 1
+mv rusefi_bundle.zip rusefi_bundle_%bundle_name%.zip
+
+ncftpput -u %RUSEFI_BUILD_FTP_USER% -p %RUSEFI_BUILD_FTP_PASS% %FTP_SERVER% . rusefi_bundle_%bundle_name%.zip
+
+rem call winscp.com /script=../ftp_commands.txt
+rem IF NOT ERRORLEVEL 0 echo winscp error DETECTED
+rem IF NOT ERRORLEVEL 0 EXIT /B 1
 cd ..
