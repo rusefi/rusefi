44cc5d516d1f9d7285270cea424e9e9b06b1a2de
rusefi <rusefillc@gmail.com>
2019-06-06 20:12:03 -0400

    manual sync with https://github.com/NOx-z/rusefi/tree/mc33816_spi

--- firmware/hw_layer/mc33816.h
+++ firmware/hw_layer/mc33816.h
@@ -8,6 +8,21 @@
 #ifndef HW_LAYER_MC33816_H_
 #define HW_LAYER_MC33816_H_
 
+const int MAX_SPI_MODE_A_TRANSFER_SIZE = 31;  //max size for register config transfer
+
+enum {
+	CODE_RAM1,
+	CODE_RAM2,
+	DATA_RAM
+};
+enum {
+	REG_MAIN,
+	REG_CH1,
+	REG_CH2,
+	REG_IO,
+	REG_DIAG
+};
+
 void initMc33816(void);
 
 #endif /* HW_LAYER_MC33816_H_ */
