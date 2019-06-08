0984cced20f3dc930cd7e39cea65d47a834e8e94
rusefi <rusefillc@gmail.com>
2019-06-08 16:47:10 -0400

    nicer bundle folder structure and reducing unneeded content #833

--- firmware/clean_compile_two_versions.bat
+++ firmware/clean_compile_two_versions.bat
@@ -20,8 +20,9 @@ echo "TIMESTAMP %date% %time%"
 make -j4 
 
 mv build\rusefi.elf deliver\rusefi.elf
-mv build\rusefi.bin deliver\rusefi.bin
 mv build\rusefi.hex deliver\rusefi.hex
+rem Keeping file in place since hw_test.bat would take it from current location
+cp build\rusefi.bin deliver\rusefi.bin
 echo Debug compilation results 2/2
 echo "TIMESTAMP %date% %time%"
 ls -l build
