0ed1e2ef2195410e0c23006fa551c27e4c3b17aa
rusefi <rusefillc@gmail.com>
2019-06-08 15:08:03 -0400

    reducing scripts duplication

--- firmware/config/boards/nucleo_f746/!compile-stm32f746_nucleo.bat
+++ firmware/config/boards/nucleo_f746/!compile-stm32f746_nucleo.bat
@@ -17,9 +17,4 @@ set DEBUG_LEVEL_OPT="-O2"
 make -j4
 ..\misc\hex2dfu\HEX2DFU.exe build/rusefi.hex -out build/rusefi.dfu
 
-rem Clean up so that no one inherits this value
-set PROJECT_BOARD=
-set PROJECT_CPU=
-set EXTRA_PARAMS=
-set DEBUG_LEVEL_OPT=
-set USE_BOOTLOADER=
+call config/boards/clean_env_variables.bat
--- firmware/config/boards/prometheus/!compile-prometheus_405.bat
+++ firmware/config/boards/prometheus/!compile-prometheus_405.bat
@@ -14,8 +14,4 @@ make clean
 mkdir .dep
 make -r
 
-rem Clean up so that no one inherits this value
-set PROJECT_BOARD=
-set EXTRA_PARAMS=
-set DEBUG_LEVEL_OPT=
-set USE_BOOTLOADER=
+call config/boards/clean_env_variables.bat
