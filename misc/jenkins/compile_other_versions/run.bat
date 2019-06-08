0fc028f603acafc5bccb4414334dd600e9a7ec36
rusefi <rusefillc@gmail.com>
2019-06-08 10:35:16 -0400

    reducing scripts duplication

--- misc/jenkins/compile_other_versions/run.bat
+++ misc/jenkins/compile_other_versions/run.bat
@@ -9,26 +9,20 @@ call misc\jenkins\build_java_console.bat
 if not exist java_console_binary/rusefi_console.jar exit -1
 
 
-call misc\jenkins\compile_other_versions\compile_and_upload.bat nucleo_f746 !compile-nucleo_f746.bat
-pwd
-echo %script_name%: Compiling F746
-if not exist build/rusefi.hex echo Just to confirm - FAILED to compile nucleo_f746
-if not exist build/rusefi.hex exit -1
-cd ..
-pwd
 set bundle_name=stm32f746_nucleo
+call misc\jenkins\compile_other_versions\compile_and_upload.bat nucleo_f746 stm32f746_nucleo
+IF NOT ERRORLEVEL 0 echo ERROR invoking compile_and_upload.bat
+IF NOT ERRORLEVEL 0 EXIT /B 1
+pwd
 call misc\jenkins\compile_other_versions\build_version.bat
 echo %script_name%: Done with F746-nucleo
 
 
-call misc\jenkins\compile_other_versions\compile_and_upload.bat nucleo_f767 !compile-nucleo_f767.bat
-pwd
-echo %script_name%: Compiling F767
-if not exist build/rusefi.hex echo Just to confirm - FAILED to compile nucleo_f767
-if not exist build/rusefi.hex exit -1
-cd ..
-pwd
 set bundle_name=stm32f767_nucleo
+call misc\jenkins\compile_other_versions\compile_and_upload.bat nucleo_f767 stm32f767_nucleo
+IF NOT ERRORLEVEL 0 echo ERROR invoking compile_and_upload.bat
+IF NOT ERRORLEVEL 0 EXIT /B 1
+pwd
 call misc\jenkins\compile_other_versions\build_version.bat
 echo %script_name%: Done with F767-nucleo
 
