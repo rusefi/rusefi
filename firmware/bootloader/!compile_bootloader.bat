c2f0fc2f17cc146c7625763c1678c5e9b6ce4e27
rusefi <rusefillc@gmail.com>
2019-06-08 15:10:54 -0400

    reducing scripts duplication

--- firmware/bootloader/!compile_bootloader.bat
+++ firmware/bootloader/!compile_bootloader.bat
@@ -1,18 +1,21 @@
 @echo off
 
+set script_name=!compile_bootloader.bat
+echo Entering %script_name%
+
 rem Needed for board overrides
 IF "%BOOTLOADER_CODE_DESTINATION_PATH%"=="" (SET BOOTLOADER_CODE_DESTINATION_PATH="..")
 
 set BOOTLOADER_CODE_DESTINATION_FILE=%BOOTLOADER_CODE_DESTINATION_PATH%/bootloader_generated.hxx
 rm -f %BOOTLOADER_CODE_DESTINATION_FILE% 
 
-echo Starting bootloader compilation...
+echo %script_name%: Starting bootloader compilation...
 make -j4 -f src/Makefile %1 %2 %3
 
 if errorlevel 1 echo make compilation failed
 if errorlevel 1 exit -1
  
-echo Bootloader build success.
+echo %script_name%: Bootloader build success.
 
 cd blbuild
 rem Generate a header file with binary bootloader code
@@ -21,3 +24,6 @@ cd ..
 
 rem Touch 'bootloader_storage.c' to update its modification date (needed for make)
 copy /b bootloader_storage.c +,,
+
+pwd
+call ../config/boards/clean_env_variables.bat
