30be91115513f6ff4ccae512f67ca21b09b06768
rusefi <rusefillc@gmail.com>
2019-06-08 22:05:52 -0400

    nicer bundle folder structure and reducing unneeded content #833

--- firmware/config/boards/STM32F407VET6_Black.bat
+++ firmware/config/boards/STM32F407VET6_Black.bat
@@ -5,7 +5,5 @@ rem TODO: somehow this -DDUMMY is helping us to not mess up the parameters, why?
 rem https://github.com/rusefi/rusefi/issues/684
 set EXTRA_PARAMS="-DDUMMY -DEFI_COMMUNICATION_PIN=GPIOA_7 -DDEFAULT_ENGINE_TYPE=MINIMAL_PINS"
 make -j8 clean
-make -j8
-rm -f build/rusefi.dfu
-dir build
-..\misc\hex2dfu\HEX2DFU.exe build/rusefi.hex   -out build/rusefi.dfu
+
+call config/boards/common_make.bat
