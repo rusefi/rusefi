814afaf6da6fb48131a5a5c9a0930f62b965e644
rusefi <rusefillc@gmail.com>
2019-06-08 21:26:08 -0400

    nicer bundle folder structure and reducing unneeded content #833

--- firmware/config/boards/common_make.bat
+++ firmware/config/boards/common_make.bat
@@ -2,7 +2,7 @@ set script_name=common_make.bat
 echo Entering %script_name%
 
 rem todo: start using env variable for number of threads or for '-r'
-maje -j4 -r
+make -j4 -r
 if not exist build/rusefi.hex echo FAILED to compile by %script_name%
 if not exist build/rusefi.hex exit -1
 
