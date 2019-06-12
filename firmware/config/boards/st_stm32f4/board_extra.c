c49f0943587ebc493c3bac5c927aad3e1a54b21b
rusefi <rusefillc@gmail.com>
2019-06-12 11:57:18 -0400

    we should not really mix ChibiOS stuff and out logic

---
+++ firmware/config/boards/st_stm32f4/board_extra.c
@@ -0,0 +1,35 @@
+
+/**
+ * @brief   Board-specific configuration code overrides.
+ * @todo    Add your board-specific code, if any.
+ */
+void setBoardConfigurationOverrides(void) {
+}
+
+/**
+ * @brief   Board-specific pin configuration code overrides. Needed by bootloader code.
+ * @todo    Add your board-specific code, if any.
+ */
+void setPinConfigurationOverrides(void) {
+}
+
+/**
+ * @brief   Board-specific Serial configuration code overrides. Needed by bootloader code.
+ * @todo    Add your board-specific code, if any.
+ */
+void setSerialConfigurationOverrides(void) {
+}
+
+/**
+ * @brief   Board-specific SD card configuration code overrides. Needed by bootloader code.
+ * @todo    Add your board-specific code, if any.
+ */
+void setSdCardConfigurationOverrides(void) {
+}
+
+/**
+ * @brief   Board-specific ADC channel configuration code overrides.
+ * @todo    Add your board-specific code, if any.
+ */
+void setAdcChannelOverrides(void) {
+}
