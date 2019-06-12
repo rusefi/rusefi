00afb035e506c3f8f013492b68b742094520fc8a
rusefi <rusefillc@gmail.com>
2019-06-12 11:57:18 -0400

    we should not really mix ChibiOS stuff and out logic

--- firmware/config/boards/st_stm32f4/board.mk
+++ firmware/config/boards/st_stm32f4/board.mk
@@ -1,5 +1,6 @@
 # List of all the board related files.
-BOARDSRC = $(PROJECT_DIR)/config/boards/st_stm32f4/board.c
+BOARDSRC = $(CHIBIOS)/os/hal/boards/ST_STM32F4_DISCOVERY/board.c \
+    $(PROJECT_DIR)/config/boards/st_stm32f4/board_extra.c
 BOARDSRC_CPP =
 
 # Required include directories
