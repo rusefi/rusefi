47a3577e27050678100d0ed6b4621dc529b95576
rusefi <rusefillc@gmail.com>
2019-06-15 16:43:30 -0400

    MK clean-up

--- unit_tests/test.mk
+++ unit_tests/test.mk
@@ -1,34 +1,9 @@
-TEST_SRC_CPP = unit_test_framework.cpp \
+FRAMEWORK_SRC_CPP = unit_test_framework.cpp \
 	engine_test_helper.cpp \
 	boards.cpp \
 	global_execution_queue.cpp \
 	test_basic_math/test_find_index.cpp \
 	test_basic_math/test_interpolation_3d.cpp \
 	test_basic_math/test_efilib.cpp \
-	tests/test_util.cpp \
-	tests/test_ion.cpp \
-	tests/test_on_demand_parameters.cpp \
-	tests/test_hip9011.cpp \
-	tests/test_cj125.cpp \
-	tests/test_cam_vtt_input.cpp \
-	tests/test_engine_math.cpp \
-	tests/test_startOfCrankingPrimingPulse.cpp \
-	tests/test_miata_na6_real_cranking.cpp \
-	tests/test_fasterEngineSpinningUp.cpp \
-	tests/test_idle_controller.cpp \
-	tests/test_trigger_decoder.cpp \
-	tests/test_trigger_noiseless.cpp \
-	tests/test_fuel_map.cpp \
-	tests/test_maf2map.cpp \
-	tests/test_fuelCut.cpp \
-	tests/test_trigger_multi_sync.cpp \
-	tests/test_pwm_generator.cpp \
-	tests/test_logic_expression.cpp \
-	tests/test_speed_density.cpp \
-	tests/test_signal_executor.cpp \
-	tests/test_sensors.cpp \
-	tests/test_pid_auto.cpp \
-	tests/test_accel_enrichment.cpp \
-	tests/test_gpiochip.cpp \
 	afm2mapConverter.cpp
 	
