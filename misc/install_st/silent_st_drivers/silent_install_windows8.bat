ef0ed939455cee3bda21d795961b64ea08dc4317
rusefi <rusefillc@gmail.com>
2019-06-16 19:32:52 -0400

    installer usability - ST drivers #845

---
+++ misc/install_st/silent_st_drivers/silent_install_windows8.bat
@@ -0,0 +1,28 @@
+@echo off
+cd "Virtual comport driver"
+cd Win8
+
+echo Let's install VCP driver silently
+if "%PROCESSOR_ARCHITEW6432%" == "AMD64" goto VCP_X64
+if "%PROCESSOR_ARCHITECTURE%" == "AMD64" goto VCP_X64
+start "" dpinst_x86.exe /s
+goto VCP_END
+:VCP_X64
+start "" dpinst_amd64.exe /s
+:VCP_END
+
+cd ../..
+echo Done installing VCP silently
+pwd
+
+echo Let's install ST-Link driver silently
+cd ST-LINK_USB_V2_1_Driver
+
+@echo off
+if "%PROCESSOR_ARCHITEW6432%" == "AMD64" goto X64
+if "%PROCESSOR_ARCHITECTURE%" == "AMD64" goto X64
+start "" dpinst_x86.exe /sw
+goto END
+:X64
+start "" dpinst_amd64.exe /sw
+:END
