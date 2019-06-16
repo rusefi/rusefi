0bf1653575595f2a3bed1dae865d07e7edf99303
rusefi <rusefillc@gmail.com>
2019-06-16 17:13:11 -0400

    removing dead text protocol support

--- firmware/controllers/engine_controller.cpp
+++ firmware/controllers/engine_controller.cpp
@@ -810,6 +810,6 @@ int getRusEfiVersion(void) {
 	if (initBootloader() != 0)
 		return 123;
 #endif /* EFI_BOOTLOADER_INCLUDE_CODE */
-	return 20190615;
+	return 20190616;
 }
 #endif /* EFI_UNIT_TEST */
