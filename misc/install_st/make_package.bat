d21743dbd8d37b381f3079f165c2828c7930b435
rusefi <rusefillc@gmail.com>
2019-06-16 19:29:50 -0400

    installer usability - ST drivers #845

---
+++ misc/install_st/make_package.bat
@@ -0,0 +1,10 @@
+rem Depends on ST Link and ST VCP being installed manually
+
+rm -rf silent_st_drivers/ST-LINK_USB_V2_1_Driver
+rm -rf silent_st_drivers/"Virtual comport driver"
+
+cp -r "C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK_USB_V2_1_Driver" silent_st_drivers
+cp -r "C:\Program Files (x86)\STMicroelectronics\Software\Virtual comport driver" silent_st_drivers
+
+rem compress 'silent_st_drivers' folder
+"C:\Program Files\7-Zip\7z.exe" a silent_st_drivers.exe -mmt -mx5 -sfx silent_st_drivers
