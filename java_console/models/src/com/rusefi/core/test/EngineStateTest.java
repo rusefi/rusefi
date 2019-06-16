20e2d5895097e7327b9c9fc9c3d6e868fcc0cbe6
rusefi <rusefillc@gmail.com>
2019-06-16 17:47:09 -0400

    also dead

--- java_console/models/src/com/rusefi/core/test/EngineStateTest.java
+++ java_console/models/src/com/rusefi/core/test/EngineStateTest.java
@@ -3,6 +3,7 @@ package com.rusefi.core.test;
 import com.rusefi.core.SensorCentral;
 import com.rusefi.core.EngineState;
 import com.rusefi.io.LinkDecoder;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.concurrent.atomic.AtomicInteger;
@@ -28,21 +29,5 @@ public class EngineStateTest {
         assertTrue(EngineState.startWithIgnoreCase("HELLO", "he"));
         assertFalse(EngineState.startWithIgnoreCase("HELLO", "hellllll"));
         assertFalse(EngineState.startWithIgnoreCase("HELLO", "ha"));
-    }
-
-    @Test
-    public void testRpm() {
-        final AtomicInteger rpmResult = new AtomicInteger();
-        EngineState es = new EngineState(new EngineState.EngineStateListenerImpl() {
-            public void onKeyValue(String key, String value) {
-                if ("rpm".equals(key))
-                    rpmResult.set(Integer.parseInt(value));
-            }
-        });
-        es.processNewData("line:7:", LinkDecoder.TEXT_PROTOCOL_DECODER);
-        es.processNewData(SensorCentral.RPM_KEY + SEPARATOR, LinkDecoder.TEXT_PROTOCOL_DECODER);
-        assertEquals(0, rpmResult.get());
-        es.processNewData("600\r", LinkDecoder.TEXT_PROTOCOL_DECODER);
-        assertEquals(600, rpmResult.get());
     }
 }
