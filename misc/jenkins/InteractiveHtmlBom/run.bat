09440e90894224c80ebc5932bae27eed37d436a6
rusEfi <russianefi@gmail.com>
2019-06-26 22:58:52 -0400

    usability

--- misc/jenkins/InteractiveHtmlBom/run.bat
+++ misc/jenkins/InteractiveHtmlBom/run.bat
@@ -1,6 +1,13 @@
+@echo off
+
 set kicad_path=c:\progra~1\KiCad
 set ibom_cmd=%kicad_path%\bin\python.exe C:\stuff\InteractiveHtmlBom\InteractiveHtmlBom\generate_interactive_bom.py --no-browser --name-format %%f_latest --dest-dir ../ibom 
 echo "ibom_cmd=%ibom_cmd%"
+
+pwd
+if not exist hardware/frankenso/frankenso.kicad_pcb echo hardware/frankenso/frankenso.kicad_pcb not found. Was this invoked from wrong folder?
+if not exist hardware/frankenso/frankenso.kicad_pcb exit -1
+
 %ibom_cmd% hardware/brain_board/brain_board_STM32F407.kicad_pcb
 %ibom_cmd% hardware/brain_board_176-pin/176-pin_board.kicad_pcb
 %ibom_cmd% hardware/CJ125_board/O2_input_CJ125.kicad_pcb
