e99f5ab0a6fdffad349eca16eef021d016a603ae
rusefi <rusefillc@gmail.com>
2019-06-27 07:36:12 -0400

    more IBOMs

--- misc/jenkins/InteractiveHtmlBom/run.bat
+++ misc/jenkins/InteractiveHtmlBom/run.bat
@@ -10,9 +10,9 @@ if not exist hardware/frankenso/frankenso.kicad_pcb exit -1
 
 %ibom_cmd% hardware/brain_board/brain_board_STM32F407.kicad_pcb
 %ibom_cmd% hardware/brain_board_176-pin/176-pin_board.kicad_pcb
-%ibom_cmd% hardware/CJ125_board/O2_input_CJ125.kicad_pcb
+%ibom_cmd% --extra-fields "mfg\,mfg#,vend1\,vend1#" hardware/CJ125_board/O2_input_CJ125.kicad_pcb --netlist-file hardware/CJ125_board/O2_input_CJ125.net
 %ibom_cmd% --extra-fields "MFG\,MFG#,VEND1\,VEND1#" hardware/frankenso/frankenso.kicad_pcb --netlist-file hardware/frankenso/frankenso.net
-%ibom_cmd% hardware/frankenstein/frankenstein.kicad_pcb
+%ibom_cmd% --extra-fields "mfg\,mfg#,vend1\,vend1#" hardware/frankenstein/frankenstein.kicad_pcb --netlist-file hardware/frankenstein/frankenstein.net
 %ibom_cmd% hardware/mini48-stm32/mini48-stm32.kicad_pcb
 %ibom_cmd% hardware/HighSideSwitch/VN750PS_E.kicad_pcb
 %ibom_cmd% hardware/can_board/can_brd_1.kicad_pcb
