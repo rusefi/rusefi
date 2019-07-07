6208f7579d2be84b3a5c568ab5cd83d1fc9c944f
rusefi <rusefillc@gmail.com>
2019-07-06 21:02:48 -0400

    reviving F4 compilation

--- firmware/controllers/engine_controller.cpp
+++ firmware/controllers/engine_controller.cpp
@@ -792,7 +792,7 @@ void initEngineContoller(Logging *sharedLogger DECLARE_ENGINE_PARAMETER_SUFFIX) 
 #define RAM_UNUSED_SIZE 7400
 #endif
 #ifndef CCM_UNUSED_SIZE
-#define CCM_UNUSED_SIZE 6600
+#define CCM_UNUSED_SIZE 4600
 #endif
 static char UNUSED_RAM_SIZE[RAM_UNUSED_SIZE];
 static char UNUSED_CCM_SIZE[CCM_UNUSED_SIZE] CCM_OPTIONAL;
