23c1243dbf744dca449f21509a3951201d5b4d0f
rusefi <rusefillc@gmail.com>
2019-07-14 11:09:51 -0400

    hiding weird panes

---
+++ java_console/ui/src/com/rusefi/PaneSettings.java
@@ -0,0 +1,18 @@
+package com.rusefi;
+
+import com.rusefi.ui.storage.Node;
+
+public class PaneSettings {
+    private static final String SHOW_ETB = "show_etb_pane";
+    private static final String SHOW_FUEL_TUNE = "show_fuel_tune_pane";
+    public boolean showEtbPane;
+    public boolean showFuelTunePane;
+    public boolean showTriggerShapePane;
+    public boolean showStimulatorPane;
+
+
+    public PaneSettings(Node config) {
+        showEtbPane = config.getBoolProperty(SHOW_ETB, false);
+        showFuelTunePane = config.getBoolProperty(SHOW_FUEL_TUNE, false);
+    }
+}
