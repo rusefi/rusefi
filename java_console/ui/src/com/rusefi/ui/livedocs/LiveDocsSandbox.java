3a1e5a0329b92d94fe96467e622e42f3c8f6bf45
rusefi <rusefillc@gmail.com>
2019-07-20 18:54:04 -0400

    vertical scrroll bar for formulas tab

--- java_console/ui/src/com/rusefi/ui/livedocs/LiveDocsSandbox.java
+++ java_console/ui/src/com/rusefi/ui/livedocs/LiveDocsSandbox.java
@@ -20,8 +20,8 @@ public class LiveDocsSandbox {
         comment.setForeground(Color.blue);
 
         JPanel panels = new JPanel(new MigLayout("fillx, gap 0, insets 0"));
-        panels.add(LiveDocPanel.getPanel("Coolant Sensor", "CLT", LDS_CLT_INDEX, ThermistorState.VALUES, ThermistorsMeta.CONTENT), "wrap");
-        panels.add(LiveDocPanel.getPanel("Intake Air Sensor", "CLT", LDS_CLT_INDEX, ThermistorState.VALUES, ThermistorsMeta.CONTENT), "wrap");
+        panels.add(LiveDocPanel.createPanel("Coolant Sensor", "CLT", LDS_CLT_INDEX, ThermistorState.VALUES, ThermistorsMeta.CONTENT), "wrap");
+        panels.add(LiveDocPanel.createPanel("Intake Air Sensor", "CLT", LDS_CLT_INDEX, ThermistorState.VALUES, ThermistorsMeta.CONTENT), "wrap");
 
         panels.add(getTChargePanel(), "wrap, grow");
 
