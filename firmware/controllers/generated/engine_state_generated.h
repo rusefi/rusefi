31d24ef5a5f93cb1eda3b2ca223afc3b64b9799d
rusefi <rusefillc@gmail.com>
2019-07-21 18:07:24 -0400

    fixing build?

--- firmware/controllers/generated/engine_state_generated.h
+++ firmware/controllers/generated/engine_state_generated.h
@@ -1,4 +1,4 @@
-// this section was generated automatically by rusEfi tool ConfigDefinition.jar based on integration/engine_state.txt Sun Jul 21 16:50:42 EDT 2019
+// this section was generated automatically by rusEfi tool ConfigDefinition.jar based on integration/engine_state.txt Sun Jul 21 18:03:23 EDT 2019
 // by class com.rusefi.output.CHeaderConsumer
 // begin
 #ifndef CONTROLLERS_GENERATED_ENGINE_STATE_GENERATED_H
@@ -34,31 +34,6 @@ struct pid_state_s {
 };
 
 typedef struct pid_state_s pid_state_s;
-
-// start of idle_state_s
-struct idle_state_s {
-	/**
-	 * offset 0
-	 */
-	idle_state_e idleState = (idle_state_e)0;
-	/**
-	 * that's current position with CLT and IAT corrections
-	 * offset 4
-	 */
-	percent_t currentIdlePosition = (percent_t)0;
-	/**
-	 * current position without adjustments (iacByTpsTaper, afterCrankingIACtaperDuration)
-	 * offset 8
-	 */
-	percent_t baseIdlePosition = (percent_t)0;
-	/**
-	 * offset 12
-	 */
-	int throttleUpState = (int)0;
-	/** total size 16*/
-};
-
-typedef struct idle_state_s idle_state_s;
 
 // start of speed_density_s
 struct speed_density_s {
@@ -100,6 +75,31 @@ struct speed_density_s {
 };
 
 typedef struct speed_density_s speed_density_s;
+
+// start of idle_state_s
+struct idle_state_s {
+	/**
+	 * offset 0
+	 */
+	idle_state_e idleState = (idle_state_e)0;
+	/**
+	 * that's current position with CLT and IAT corrections
+	 * offset 4
+	 */
+	percent_t currentIdlePosition = (percent_t)0;
+	/**
+	 * current position without adjustments (iacByTpsTaper, afterCrankingIACtaperDuration)
+	 * offset 8
+	 */
+	percent_t baseIdlePosition = (percent_t)0;
+	/**
+	 * offset 12
+	 */
+	int throttleUpState = (int)0;
+	/** total size 16*/
+};
+
+typedef struct idle_state_s idle_state_s;
 
 // start of engine_state2_s
 struct engine_state2_s {
@@ -146,4 +146,4 @@ typedef struct engine_state2_s engine_state2_s;
 
 #endif
 // end
-// this section was generated automatically by rusEfi tool ConfigDefinition.jar based on integration/engine_state.txt Sun Jul 21 16:50:42 EDT 2019
+// this section was generated automatically by rusEfi tool ConfigDefinition.jar based on integration/engine_state.txt Sun Jul 21 18:03:23 EDT 2019
