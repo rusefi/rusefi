a56c8320b79ca6bf4769b1c948a404d47ca6c2b0
rusefi <rusefillc@gmail.com>
2019-07-25 00:31:37 -0400

    removing least useful engine configurations

--- firmware/config/engines/snow_blower.cpp
+++
@@ -1,9 +0,0 @@
-/**
- * @file    snow_blower.cpp
- * @brief	Default configuration of a single-cylinder engine
- *
- * @date Sep 9, 2013
- * @author Andrey Belomutskiy, (c) 2012-2018
- */
-
-#include "global.h"
