bdc6915ae37b8bad6e7b8dda7af77e61577ede4d
andreika-git <andreika-git@users.noreply.github.com>
2019-07-27 16:52:01 +0300

    Kinetis files (pre-alpha) (#890)

---
+++ firmware/hw_layer/ports/kinetis/kinetis_pins.cpp
@@ -0,0 +1,147 @@
+/**
+ * @file	kinetis_pins.cpp
+ * @brief	Kinetis-compatible GPIO code
+ *
+ * @date Jun 02, 2019
+ * @author Andrey Belomutskiy, (c) 2012-2019
+ * @author andreika <prometheus.pcb@gmail.com>
+ */
+
+#include "global.h"
+#include "engine.h"
+#include "efi_gpio.h"
+
+#if EFI_GPIO_HARDWARE
+
+// This is the radical departure from STM32
+#define PORT_SIZE 18
+
+static ioportid_t ports[] = {GPIOA,
+		GPIOB,
+		GPIOC,
+		GPIOD,
+		GPIOE,
+		nullptr,
+		nullptr,
+		nullptr,
+};
+
+#define PIN_REPO_SIZE (sizeof(ports) / sizeof(ports[0])) * PORT_SIZE
+// todo: move this into PinRepository class
+static const char *PIN_USED[PIN_REPO_SIZE + BOARD_EXT_PINREPOPINS];
+
+#include "pin_repository.h"
+#include "io_pins.h"
+
+extern ioportid_t PORTS[];
+
+/**
+ * @deprecated - use hwPortname() instead
+ */
+const char *portname(ioportid_t GPIOx) {
+	if (GPIOx == GPIOA)
+		return "PA";
+	if (GPIOx == GPIOB)
+		return "PB";
+	if (GPIOx == GPIOC)
+		return "PC";
+	if (GPIOx == GPIOD)
+		return "PD";
+	if (GPIOx == GPIOE)
+		return "PE";
+	return "unknown";
+}
+
+static int getPortIndex(ioportid_t port) {
+	efiAssert(CUSTOM_ERR_ASSERT, port != NULL, "null port", -1);
+	if (port == GPIOA)
+		return 0;
+	if (port == GPIOB)
+		return 1;
+	if (port == GPIOC)
+		return 2;
+	if (port == GPIOD)
+		return 3;
+	if (port == GPIOE)
+		return 4;
+	firmwareError(CUSTOM_ERR_UNKNOWN_PORT, "unknown port");
+	return -1;
+}
+
+ioportid_t getBrainPort(brain_pin_e brainPin) {
+	return ports[(brainPin - GPIOA_0) / PORT_SIZE];
+}
+
+int getBrainPinIndex(brain_pin_e brainPin) {
+	return (brainPin - GPIOA_0) % PORT_SIZE;
+}
+
+int getBrainIndex(ioportid_t port, ioportmask_t pin) {
+	int portIndex = getPortIndex(port);
+	return portIndex * PORT_SIZE + pin;
+}
+
+ioportid_t getHwPort(const char *msg, brain_pin_e brainPin) {
+	if (brainPin == GPIO_UNASSIGNED || brainPin == GPIO_INVALID)
+		return GPIO_NULL;
+	if (brainPin < GPIOA_0 || brainPin > BRAIN_PIN_LAST_ONCHIP) {
+		firmwareError(CUSTOM_ERR_INVALID_PIN, "%s: Invalid brain_pin_e: %d", msg, brainPin);
+		return GPIO_NULL;
+	}
+	return PORTS[(brainPin - GPIOA_0) / PORT_SIZE];
+}
+
+/**
+ * this method returns the numeric part of pin name. For instance, for PC13 this would return '13'
+ */
+ioportmask_t getHwPin(const char *msg, brain_pin_e brainPin)
+{
+	if (brainPin == GPIO_UNASSIGNED || brainPin == GPIO_INVALID)
+			return EFI_ERROR_CODE;
+
+	if (brain_pin_is_onchip(brainPin))
+		return getBrainPinIndex(brainPin);
+
+	firmwareError(CUSTOM_ERR_INVALID_PIN, "%s: Invalid on-chip brain_pin_e: %d", msg, brainPin);
+	return EFI_ERROR_CODE;
+}
+
+/**
+ * Parse string representation of physical pin into brain_pin_e ordinal.
+ *
+ * @return GPIO_UNASSIGNED for "none", GPIO_INVALID for invalid entry
+ */
+brain_pin_e parseBrainPin(const char *str) {
+	if (strEqual(str, "none"))
+		return GPIO_UNASSIGNED;
+	// todo: create method toLowerCase?
+	if (str[0] != 'p' && str[0] != 'P') {
+		return GPIO_INVALID;
+	}
+	char port = str[1];
+	brain_pin_e basePin;
+	if (port >= 'a' && port <= 'z') {
+		basePin = (brain_pin_e) ((int) GPIOA_0 + PORT_SIZE * (port - 'a'));
+	} else if (port >= 'A' && port <= 'Z') {
+		basePin = (brain_pin_e) ((int) GPIOA_0 + PORT_SIZE * (port - 'A'));
+	} else {
+		return GPIO_INVALID;
+	}
+	const char *pinStr = str + 2;
+	int pin = atoi(pinStr);
+	return (brain_pin_e)(basePin + pin);
+}
+
+unsigned int getNumBrainPins(void) {
+	return PIN_REPO_SIZE;
+}
+
+void initBrainUsedPins(void) {
+	memset(PIN_USED, 0, sizeof(PIN_USED));
+}
+
+const char* & getBrainUsedPin(unsigned int idx) {
+	return PIN_USED[idx];
+}
+
+#endif /* EFI_GPIO_HARDWARE */
---
+++ firmware/hw_layer/trigger_input_comp.cpp
@@ -0,0 +1,135 @@
+/**
+ * @file	trigger_input_comp.cpp
+ * @brief	Position sensor hardware layer, Using hardware comparator
+ *
+ * @date Apr 13, 2019
+ * @author Andrey Belomutskiy, (c) 2012-2019
+ * @author andreika <prometheus.pcb@gmail.com>
+ */
+ 
+#include "global.h"
+
+#if (EFI_SHAFT_POSITION_INPUT && HAL_USE_COMP) || defined(__DOXYGEN__)
+
+#include "hal_comp.h"
+
+#include "trigger_input.h"
+#include "digital_input_hw.h"
+#include "pin_repository.h"
+#include "trigger_structure.h"
+#include "trigger_central.h"
+#include "engine_configuration.h"
+
+#define TRIGGER_SUPPORTED_CHANNELS 2
+
+extern bool hasFirmwareErrorFlag;
+
+EXTERN_ENGINE
+;
+static Logging *logger;
+
+int vvtEventRiseCounter = 0;
+int vvtEventFallCounter = 0;
+
+static void comp_shaft_callback(COMPDriver *comp) {
+	bool isRising = (comp_lld_get_status(comp) & COMP_IRQ_RISING) != 0;
+	int isPrimary = (comp == EFI_COMP_PRIMARY_DEVICE);
+	if (!isPrimary && !TRIGGER_SHAPE(needSecondTriggerInput)) {
+		return;
+	}
+	trigger_event_e signal;
+	if (isRising) {
+		signal = isPrimary ? (engineConfiguration->invertPrimaryTriggerSignal ? SHAFT_PRIMARY_FALLING : SHAFT_PRIMARY_RISING) : 
+			(engineConfiguration->invertSecondaryTriggerSignal ? SHAFT_SECONDARY_FALLING : SHAFT_SECONDARY_RISING);
+	} else {
+		signal = isPrimary ? (engineConfiguration->invertPrimaryTriggerSignal ? SHAFT_PRIMARY_RISING : SHAFT_PRIMARY_FALLING) : 
+			(engineConfiguration->invertSecondaryTriggerSignal ? SHAFT_SECONDARY_RISING : SHAFT_SECONDARY_FALLING);
+	}
+	hwHandleShaftSignal(signal);
+
+#ifdef EFI_TRIGGER_DEBUG_BLINK
+	__blink(1);
+#endif
+}
+
+// todo: add cam support?
+#if 0
+static void comp_cam_callback(COMPDriver *comp) {
+	if (isRising) {
+		vvtEventRiseCounter++;
+		hwHandleVvtCamSignal(TV_RISE);
+	} else {
+		vvtEventFallCounter++;
+		hwHandleVvtCamSignal(TV_FALL);
+	}
+}
+#endif
+
+static COMPConfig comp_shaft_cfg = {
+	COMP_OUTPUT_NORMAL, COMP_IRQ_BOTH,
+	comp_shaft_callback,
+	0
+};
+
+static bool isCompEnabled = false;
+
+void turnOnTriggerInputPins(Logging *sharedLogger) {
+	logger = sharedLogger;
+	compInit();
+	compStart(EFI_COMP_PRIMARY_DEVICE, &comp_shaft_cfg);
+
+	applyNewTriggerInputPins();
+}
+
+void startTriggerInputPins(void) {
+	//efiAssertVoid(CUSTOM_ERR_, !isCompEnabled, "isCompEnabled");
+
+	const float vRef = 5.0f;
+	const float vSensorRef = 2.5f;	// 2.5V resistor divider
+	// when VR sensor is silent, there's still some noise around vRef, so we need a small threshold to avoid false triggering
+	const float noSignalThreshold = 0.05f;	
+	
+	const int maxDacValue = 255;
+	const int vDac = (int)(int)efiRound(maxDacValue * (vSensorRef - noSignalThreshold) / vRef, 1.0f);
+	
+	int channel = EFI_COMP_TRIGGER_CHANNEL; // todo: use getInputCaptureChannel(hwPin);
+
+	// todo: set pin mode to default (analog/comparator)
+	//palSetPadMode(comp_channel_port[channel], comp_channel_pad[channel], PAL_MODE_INPUT_ANALOG);
+	
+	// no generic hal support for extended COMP configuration, so we use hal_lld layer...
+	osalSysLock();
+	comp_lld_set_dac_value(EFI_COMP_PRIMARY_DEVICE, vDac);
+	comp_lld_channel_enable(EFI_COMP_PRIMARY_DEVICE, channel);
+    osalSysUnlock();
+    
+	compEnable(EFI_COMP_PRIMARY_DEVICE);
+	isCompEnabled = true;
+}
+
+void stopTriggerInputPins(void) {
+	if (!isCompEnabled)
+		return;
+	compDisable(EFI_COMP_PRIMARY_DEVICE);
+	isCompEnabled = false;
+#if 0
+	for (int i = 0; i < TRIGGER_SUPPORTED_CHANNELS; i++) {
+		if (CONFIGB(triggerInputPins)[i]
+				!= activeConfiguration.bc.triggerInputPins[i]) {
+			turnOffTriggerInputPin(activeConfiguration.bc.triggerInputPins[i]);
+		}
+	}
+	if (engineConfiguration->camInput != activeConfiguration.camInput) {
+		turnOffTriggerInputPin(activeConfiguration.camInput);
+	}
+#endif
+}
+
+void applyNewTriggerInputPins(void) {
+	// first we will turn off all the changed pins
+	stopTriggerInputPins();
+	// then we will enable all the changed pins
+	startTriggerInputPins();
+}
+
+#endif /* EFI_SHAFT_POSITION_INPUT && HAL_USE_COMP */
