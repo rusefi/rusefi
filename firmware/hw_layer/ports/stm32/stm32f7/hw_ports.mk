2c99bf035df0986921a0e0b0a5bf98675ef3e8cb
rusefi <rusefillc@gmail.com>
2019-07-28 12:33:47 -0400

    software jump to DFU #809

--- firmware/hw_layer/ports/stm32/stm32f7/hw_ports.mk
+++ firmware/hw_layer/ports/stm32/stm32f7/hw_ports.mk
@@ -6,3 +6,5 @@ HW_LAYER_EMS_CPP += $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f7/mpu_util.cpp \
 	$(PROJECT_DIR)/hw_layer/ports/stm32/stm32_pins.cpp \
 	$(PROJECT_DIR)/hw_layer/ports/stm32/stm32_common.cpp
 	
+RUSEFIASM = $(PROJECT_DIR)/hw_layer/ports/stm32/rusEfiStartup.S
+	
