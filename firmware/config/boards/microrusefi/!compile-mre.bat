1ec07af6e28eb798a983f494a24159c5ce8e38ee
Matthew Kennedy <matthewkennedy@outlook.com>
2019-08-01 19:27:23 -0700

    microRusefi Default Configuration (#895)
    * poke
    
    * working MRE
    
    * why was this on?
    
    * comment, cleanup
    
    * default silent trigger error on
    
    * comment
    
    * remove extra
    
    * sensible default

--- firmware/config/boards/microrusefi/!compile-mre.bat
+++ firmware/config/boards/microrusefi/!compile-mre.bat
@@ -3,16 +3,8 @@ cd ../../..
 
 set LDSCRIPT = config/boards/NUCLEO_F767/STM32F76xxI.ld
 
-set PROJECT_BOARD=nucleo_f767
+set PROJECT_BOARD=microrusefi
 set PROJECT_CPU=ARCH_STM32F7
-set EXTRA_PARAMS=-DDUMMY ^
- -DSTM32F767xx ^
- -DEFI_USE_OSC=TRUE ^
- -DFIRMWARE_ID=\"microRusEfi\" ^
- -DEFI_COMMUNICATION_PIN=GPIOE_1 ^
- -DEFI_FATAL_ERROR_PIN=GPIOE_2 ^
- -DEFI_RUNNING_PIN=GPIOE_3 ^
- -DEFI_WARNING_PIN=GPIOE_4
 
 call config/boards/common_make.bat
 
