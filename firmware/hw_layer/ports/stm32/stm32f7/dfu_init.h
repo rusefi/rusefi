21c61bfaecb10f4db7e46860e5b5e6afd49cb695
rusefi <rusefillc@gmail.com>
2019-08-04 00:21:44 -0400

    software jump to DFU #809 very very dark magic it is

--- firmware/hw_layer/ports/stm32/stm32f7/dfu_init.h
+++ firmware/hw_layer/ports/stm32/stm32f7/dfu_init.h
@@ -8,8 +8,10 @@
 #ifndef HW_LAYER_PORTS_STM32_STM32F7_DFU_INIT_H_
 #define HW_LAYER_PORTS_STM32_STM32F7_DFU_INIT_H_
 
+// looks like this is System Control Block Base Address
 LDR R1, =0xE000ED00
 LDR R0, =0x1FF00000
+// so this must be Vector Table Offset Register?
 STR R0, [R1, #8]
 LDR SP, [R0, #0]
 LDR R0, [R0, #4]
---
+++ firmware/hw_layer/ports/stm32/stm32h7/dfu_init.h
@@ -0,0 +1,3 @@
+
+// https://community.st.com/s/question/0D50X00009bNBluSAG/boot-stm32h7-after-power-up
+// https://community.st.com/s/article/STM32H7-bootloader-jump-from-application
