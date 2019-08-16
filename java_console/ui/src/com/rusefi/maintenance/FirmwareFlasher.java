55ddab13772f11607eb5cc685ee4bff748a4fa20
rusEfi <russianefi@gmail.com>
2019-08-15 21:39:20 -0400

    magic way to use launcher from IDE

--- java_console/ui/src/com/rusefi/maintenance/FirmwareFlasher.java
+++ java_console/ui/src/com/rusefi/maintenance/FirmwareFlasher.java
@@ -1,5 +1,6 @@
 package com.rusefi.maintenance;
 
+import com.rusefi.Launcher;
 import com.rusefi.ui.StatusWindow;
 import org.jetbrains.annotations.NotNull;
 
@@ -19,12 +20,13 @@ import static com.rusefi.ui.storage.PersistentConfiguration.getConfig;
  */
 public class FirmwareFlasher {
     // Even on Windows openOCD insists on "/" for path separator
-    public static final String IMAGE_FILE = INPUT_FILES_PATH + "/" + "rusefi.bin";
-    public static final String IMAGE_NO_ASSERTS_FILE = INPUT_FILES_PATH + "/" + "rusefi_no_asserts.bin";
+    public static final String IMAGE_FILE = INPUT_FILES_PATH + File.separator + "rusefi.bin";
+    public static final String IMAGE_NO_ASSERTS_FILE = INPUT_FILES_PATH + File.separator + "rusefi_no_asserts.bin";
     /**
      * SWD ST-LINK/V2 mode
      */
-    private static final String OPENOCD_EXE = "openocd/openocd.exe";
+    private static final String OPENOCD_EXE = Launcher.TOOLS_PATH + File.separator + "openocd/openocd.exe";
+    // todo: combine this with Launcher#TOOLS_PATH?
     static final String BINARY_LOCATION = ".";
     private static final String SUCCESS_MESSAGE_TAG = "shutdown command invoked";
     private static final String FAILED_MESSAGE_TAG = "failed";
