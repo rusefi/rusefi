d6711295ba23695b9cf60e3afcaf2672ee51a0b3
ola <olaruud@hotmail.com>
2019-08-18 04:42:41 +0200

    Cleaned up

--- firmware/controllers/actuators/boost_controller.h
+++ firmware/controllers/actuators/boost_controller.h
@@ -9,15 +9,5 @@
 #define CONTROLLERS_ACTUATORS_BOOST_CONTROLLER_H_
 
 
-#include "engine.h"
-void initBoostCtrl(Logging *sharedLogger);
-void setBstPFactor(float p);
-void setBstIFactor(float p);
-void setBstDFactor(float p);
-void setDefaultBoostParameters(void);
-
-void onConfigurationChangeBoostCallback(engine_configuration_s *previousConfiguration);
-
-
 
 #endif /* CONTROLLERS_ACTUATORS_BOOST_CONTROLLER_H_ */
