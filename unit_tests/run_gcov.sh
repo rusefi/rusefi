d9e49f3e5a5bf23afdc40b2e2136cf60650f289a
osboxes.org <rusefillc@gmail.com>
2019-08-25 21:18:06 -0400

    mint
    lease enter the commit message for your changes. Lines starting

--- unit_tests/run_gcov.sh
+++ unit_tests/run_gcov.sh
@@ -4,13 +4,14 @@ rm -rf gcov_working_area
 
 mkdir gcov_working_area
 cd gcov_working_area
-ls -l 
 
 
+echo Looking for source code 
 find     ..                          -name *.c* >  source_files.txt
 find     ../../firmware/console/     -name *.c* >> source_files.txt
 find     ../../firmware/controllers/ -name *.c* >> source_files.txt
 
+wc -l source_files.txt
 
 xargs -L 1 -I {} cp {} . < source_files.txt
 
@@ -18,3 +19,7 @@ cp ../build/obj/* .
 
 gcov *.c* > gcov.log 2>gcov.err
 
+
+
+gcovr --html -o rusefi_unit_tests.html
+ls -l rusefi_unit_tests.html 
