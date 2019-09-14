1a3058fe71af7ccb6046ee11991973a1a2a4b1f9
Matthew Kennedy <matthewkennedy@outlook.com>
2019-09-14 13:45:28 -0700

    wire up producer

--- firmware/init/sensor/init_oil_pressure.cpp
+++ firmware/init/sensor/init_oil_pressure.cpp
@@ -1,28 +1,24 @@
-
-#include "error_handling.h"
-#include "function_pointer_sensor.h"
-#include "oil_pressure.h"
-
-#include "analog_input.h"
 #include "engine.h"
+#include "error_handling.h"
 #include "global.h"
-#include "interpolation.h"
-#include "os_access.h"
+#include "linear_sensor.h"
 
 EXTERN_ENGINE;
 
-// Wrapper to handle PASS_ENGINE_PARAMETER_SIGNATURE
-float getOilPressureLegacyWrapper() {
-	return getOilPressureLegacy(PASS_ENGINE_PARAMETER_SIGNATURE);
-}
-
-FunctionPointerSensor oilpSensor(SensorType::OilPressure, &getOilPressureLegacyWrapper);
+LinearSensor oilpSensor(SensorType::OilPressure);
 
 void initOilPressure() {
 	// Only register if we have a sensor
 	if (engineConfiguration->oilPressure.hwChannel == EFI_ADC_NONE) {
 		return;
 	}
+
+	oil_pressure_config_s *sensorCfg = &CONFIG(oilPressure);
+
+	// Limit to max given pressure
+	oilpSensor.configure(sensorCfg->v1, sensorCfg->value1, sensorCfg->v2, sensorCfg->value2, 0, sensorCfg->value2);
+
+	// Subscribe the sensor to ADC
 
 	if (!oilpSensor.Register()) {
 		warning(OBD_Oil_Pressure_Sensor_Malfunction, "Duplicate oilp sensor registration, ignoring");
