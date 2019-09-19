a20f353506a9c253da9703fb891d252e7068a280
Matthew Kennedy <matthewkennedy@outlook.com>
2019-09-18 17:23:14 -0700

    Kill Dead Code 2: Electric Boogaloo (#940)
    * remove dead
    
    * remove dead
    
    * deflex

--- firmware/controllers/sensors/sensors.mk
+++ firmware/controllers/sensors/sensors.mk
@@ -10,5 +10,4 @@ CONTROLLERS_SENSORS_SRC_CPP = 	$(PROJECT_DIR)/controllers/sensors/thermistors.cp
 	$(PROJECT_DIR)/controllers/sensors/ego.cpp \
 	$(PROJECT_DIR)/controllers/sensors/maf2map.cpp \
 	$(PROJECT_DIR)/controllers/sensors/hip9011_lookup.cpp \
-	$(PROJECT_DIR)/controllers/sensors/flex_fuel.cpp \
 	$(PROJECT_DIR)/controllers/sensors/oil_pressure.cpp
