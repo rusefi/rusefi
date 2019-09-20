ed53c914d10c98eccdfc9365aab51b4829259edc
Matthew Kennedy <matthewkennedy@outlook.com>
2019-09-19 19:28:59 -0700

    setup comments

--- firmware/init/sensor/init_oil_pressure.cpp
+++ firmware/init/sensor/init_oil_pressure.cpp
@@ -19,11 +19,13 @@ void initOilPressure() {
 	float val2 = sensorCfg->value2;
 
 	// Limit to max given pressure - val1 or val2 could be larger
+	// (sensor may be backwards, high voltage = low pressure)
 	float greaterOutput = val1 > val2 ? val1 : val2;
 
-	oilpSensor.configure(sensorCfg->v1, val1, sensorCfg->v2, val2, 0, greaterOutput);
+	// Allow slightly negative output (-5kpa) so as to not fail the sensor when engine is off
+	oilpSensor.configure(sensorCfg->v1, val1, sensorCfg->v2, val2, /*minOutput*/ -5, greaterOutput);
 
-	// Subscribe the sensor to ADC
+	// TODO: Subscribe the sensor to ADC
 
 	if (!oilpSensor.Register()) {
 		warning(OBD_Oil_Pressure_Sensor_Malfunction, "Duplicate oilp sensor registration, ignoring");
