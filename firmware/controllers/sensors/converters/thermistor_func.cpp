de8752ba809ad093598fe1c674ad0db2c344dd02
Matthew Kennedy <matthewkennedy@outlook.com>
2019-09-24 23:49:46 -0700

    add thermistor func

--- firmware/controllers/sensors/converters/thermistor_func.cpp
+++ firmware/controllers/sensors/converters/thermistor_func.cpp
@@ -1,5 +1,46 @@
 #include "thermistor_func.h"
 
-SensorResult ThermistorFunc::convert(float volts) const {
-	return {false, 0};
+#include <math.h>
+
+SensorResult ThermistorFunc::convert(float ohms) const {
+	// This resistance should have already been validated - only
+	// thing we can check is that it's non-negative
+	if(ohms <= 0)
+	{
+		return {false, 0};
+	}
+
+	float lnR = logf(ohms);
+
+	float lnR3 = lnR * lnR * lnR;
+
+	float recip = m_a + m_b * lnR + m_c * lnR3;
+
+	float kelvin = 1 / recip;
+
+	float celsius = kelvin - 273.15f;
+
+	return {true, celsius};
+}
+
+void ThermistorFunc::configure(thermistor_conf_s& cfg) {
+	float t1 = cfg.tempC_1 + 273.15f;
+	float t2 = cfg.tempC_2 + 273.15f;
+	float t3 = cfg.tempC_3 + 273.15f;
+	
+	// https://en.wikipedia.org/wiki/Steinhart%E2%80%93Hart_equation
+	float l1 = logf(cfg.resistance_1);
+	float l2 = logf(cfg.resistance_2);
+	float l3 = logf(cfg.resistance_3);
+
+	float y1 = 1 / t1;
+	float y2 = 1 / t2;
+	float y3 = 1 / t3;
+
+	float u2 = (y2 - y1) / (l2 - l1);
+	float u3 = (y3 - y1) / (l3 - l1);
+
+	m_c = ((u3 - u2) / (l3 - l2)) / (l1 + l2 + l3);
+	m_b = u2 - m_c * (l1 * l1 + l1 * l2 + l2 * l2);
+	m_a = y1 - (m_b + l1 * l1 * m_c) * l1;
 }
--- firmware/controllers/sensors/converters/thermistor_func.h
+++ firmware/controllers/sensors/converters/thermistor_func.h
@@ -1,8 +1,17 @@
 #pragma once
 
 #include "sensor_converter_func.h"
+#include "engine_configuration_generated_structures.h"
 
 class ThermistorFunc final : public SensorConverter {
 public:
 	SensorResult convert(float ohms) const override;
+
+	void configure(thermistor_conf_s& cfg);
+
+private:
+	// Steinhart-Hart coefficients
+	float m_a;
+	float m_b;
+	float m_c;
 };
