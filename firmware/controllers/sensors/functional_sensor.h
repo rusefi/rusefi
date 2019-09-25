aa25f0773725ecee97d4dc42f5de7892339532d6
Matthew Kennedy <matthewkennedy@outlook.com>
2019-09-25 00:00:04 -0700

    maybe that wasn't a good idea

--- firmware/controllers/sensors/functional_sensor.h
+++ firmware/controllers/sensors/functional_sensor.h
@@ -12,13 +12,6 @@
 
 #include <type_traits>
 
-struct FunctionalSensorBase : public StoredValueSensor {
-	FunctionalSensorBase(SensorType type)
-		: StoredValueSensor(type) {}
-
-	virtual void postRawValue(float value) = 0;
-};
-
 /**
  * @brief Class for sensors that convert from some raw floating point
  * value (ex: voltage, frequency, pulse width) to a sensor reading.
@@ -30,14 +23,10 @@ struct FunctionalSensorBase : public StoredValueSensor {
  * Register an instance of the new class with an interface
  * that provides and posts raw values so the sensor can update.
  */
-template <typename TFunc>
-class FunctionalSensor final : public FunctionalSensorBase {
-	static_assert(std::is_base_of_v<SensorConverter, TFunc>, "TFunc must inherit from SensorConverter");
-	static_assert(std::is_default_constructible_v<TFunc>, "TFunc must be default constructible");
-
+class FunctionalSensor final : public StoredValueSensor {
 public:
 	explicit FunctionalSensor(SensorType type)
-		: FunctionalSensorBase(type) {}
+		: StoredValueSensor(type) { }
 
 	void postRawValue(float inputValue) {
 		// Report the raw value
@@ -46,7 +35,14 @@ public:
 			*rawReportLocation = inputValue;
 		}
 
-		auto r = m_function.convert(inputValue);
+		// If no function is set, this sensor isn't valid.
+		auto function = m_function;
+		if (!function) {
+			invalidate();
+			return;
+		}
+
+		auto r = function->convert(inputValue);
 
 		// This has to happen so that we set the valid bit after
 		// the value is stored, to prevent the data race of reading
@@ -62,14 +58,13 @@ public:
 		m_rawReportingLocation = rawReportingLocation;
 	}
 
-	// Allow access to the underlying function
-	TFunc &f() {
-		return m_function;
+	void setFunction(SensorConverter& func) {
+		m_function = &func;
 	}
 
 private:
 	float *m_rawReportingLocation = nullptr;
 
 	// Conversion function for this sensor
-	TFunc m_function;
+	SensorConverter* m_function = nullptr;
 };
