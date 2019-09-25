6bedd954bef2f8c1155ebeccaef1c53a1eda6af8
Matthew Kennedy <matthewkennedy@outlook.com>
2019-09-24 22:00:53 -0700

    add to init

--- firmware/init/init.mk
+++ firmware/init/init.mk
@@ -1,3 +1,4 @@
 
 INIT_SRC_CPP =	$(PROJECT_DIR)/init/sensor/init_sensors.cpp \
-				$(PROJECT_DIR)/init/sensor/init_oil_pressure.cpp
+				$(PROJECT_DIR)/init/sensor/init_oil_pressure.cpp \
+				$(PROJECT_DIR)/init/sensor/init_temp_sensors.cpp
