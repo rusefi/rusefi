bc97ac741eb997b2a2cf06ed7293ba95dfddf652
Matthew Kennedy <matthewkennedy@outlook.com>
2019-10-02 17:41:42 -0700

    fix test

--- unit_tests/tests/test_sensors.cpp
+++ unit_tests/tests/test_sensors.cpp
@@ -10,7 +10,7 @@
 #include "allsensors.h"
 #include "engine_test_helper.h"
 
-static ThermistorConf tc;
+
 
 TEST(sensors, mapDecoding) {
 	WITH_ENGINE_TEST_HELPER(FORD_INLINE_6_1995);
@@ -55,31 +55,31 @@ TEST(sensors, testTpsRateOfChange) {
 //	assertEquals(25, getTpsRateOfChange());
 }
 
-TEST(sensors, thermistors) {
+TEST(sensors, Thermistor1) {
 
 	ThermistorMath tm;
-	{
-		setThermistorConfiguration(&tc, 32, 9500, 75, 2100, 120, 1000);
-		tm.setConfig(&tc.config);
-		float t = tm.getKelvinTemperatureByResistance(2100);
-		ASSERT_FLOAT_EQ(75 + KELV, t);
+	thermistor_conf_s tc = {32, 75, 120, 9500, 2100, 1000, 0};
+	tm.setConfig(&tc);
 
-		ASSERT_NEAR(-0.003, tm.s_h_a, EPS4D);
-		ASSERT_NEAR(0.001, tm.s_h_b, EPS4D);
-		ASSERT_NEAR(0.0, tm.s_h_c, EPS5D);
+	float t = tm.getKelvinTemperatureByResistance(2100);
+	ASSERT_FLOAT_EQ(75 + KELV, t);
 
-	}
+	ASSERT_NEAR(-0.003, tm.s_h_a, EPS4D);
+	ASSERT_NEAR(0.001, tm.s_h_b, EPS4D);
+	ASSERT_NEAR(0.0, tm.s_h_c, EPS5D);
+}
 
-	{
-		// 2003 Neon sensor
-		setThermistorConfiguration(&tc, 0, 32500, 30, 7550, 100, 700);
-		tm.setConfig(&tc.config);
+TEST(sensors, ThermistorNeon)
+{
+	ThermistorMath tm;
+	// 2003 Neon sensor
+	thermistor_conf_s tc = {0, 30, 100, 32500, 7550, 700, 0};
+	tm.setConfig(&tc);
 
-		float t = tm.getKelvinTemperatureByResistance(38000);
-		ASSERT_NEAR(-2.7983, t - KELV, EPS4D);
+	float t = tm.getKelvinTemperatureByResistance(38000);
+	ASSERT_NEAR(-2.7983, t - KELV, EPS4D);
 
-		assertEqualsM("A", 0.0009, tm.s_h_a);
-		assertEqualsM("B", 0.0003, tm.s_h_b);
-		ASSERT_NEAR(0.0, tm.s_h_c, EPS4D);
-	}
+	assertEqualsM("A", 0.0009, tm.s_h_a);
+	assertEqualsM("B", 0.0003, tm.s_h_b);
+	ASSERT_NEAR(0.0, tm.s_h_c, EPS4D);
 }
