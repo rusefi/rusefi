4058d482ff249664943710efb72c9883b6a5d2a8
Matthew Kennedy <matthewkennedy@outlook.com>
2019-10-13 22:14:56 -0700

    cleanup

--- firmware/console/status_loop.cpp
+++ firmware/console/status_loop.cpp
@@ -797,7 +797,6 @@ void updateTunerStudioState(TunerStudioOutputChannels *tsOutputChannels DECLARE_
 
 
 	tsOutputChannels->isWarnNow = engine->engineState.warnings.isWarningNow(timeSeconds, true);
-
 #if EFI_HIP_9011
 	tsOutputChannels->isKnockChipOk = (instance.invalidHip9011ResponsesCount == 0);
 #endif /* EFI_HIP_9011 */
