1c140cbd80fe09e7e144821b79c3329fdfb95ed7
rusefi <rusefillc@gmail.com>
2019-10-17 00:09:13 -0400

    date

--- firmware/controllers/engine_controller.cpp
+++ firmware/controllers/engine_controller.cpp
@@ -837,6 +837,6 @@ int getRusEfiVersion(void) {
 	if (initBootloader() != 0)
 		return 123;
 #endif /* EFI_BOOTLOADER_INCLUDE_CODE */
-	return 20191013;
+	return 20191016;
 }
 #endif /* EFI_UNIT_TEST */
