8423fa0acf1663e06489ddc90610f9eb778be1e3
rusefi <rusefillc@gmail.com>
2019-11-03 22:02:52 -0500

    pieces of #971

--- firmware/development/wave_analyzer.h
+++ firmware/development/wave_analyzer.h
@@ -23,34 +23,34 @@ public:
 	void onFallEvent();
 
 	digital_input_s *hw;
-	const char *name;
-	volatile int fallEventCounter;
-	volatile int riseEventCounter;
+	const char *name = nullptr;
+	volatile int fallEventCounter = 0;
+	volatile int riseEventCounter = 0;
 
-	int currentRevolutionCounter;
+	int currentRevolutionCounter = 0;
 
 	/**
 	 * Total ON time during last engine cycle
 	 */
-	efitimeus_t prevTotalOnTimeUs;
+	efitimeus_t prevTotalOnTimeUs = 0;
 
-	efitimeus_t totalOnTimeAccumulatorUs;
+	efitimeus_t totalOnTimeAccumulatorUs = 0;
 
-	volatile efitimeus_t lastActivityTimeUs; // timestamp in microseconds ticks
+	volatile efitimeus_t lastActivityTimeUs = 0; // timestamp in microseconds ticks
 	/**
 	 * time of signal fall event, in microseconds
 	 */
-	volatile efitimeus_t periodEventTimeUs;
-	volatile efitimeus_t widthEventTimeUs; // time of signal rise in microseconds
+	volatile efitimeus_t periodEventTimeUs = 0;
+	volatile efitimeus_t widthEventTimeUs = 0; // time of signal rise in microseconds
 
-	volatile efitimeus_t signalPeriodUs; // period between two signal rises in microseconds
+	volatile efitimeus_t signalPeriodUs = 0; // period between two signal rises in microseconds
 
 	/**
 	 * offset from engine cycle start in microseconds
 	 */
-	volatile efitimeus_t waveOffsetUs;
-	volatile efitimeus_t last_wave_low_widthUs; // time period in systimer ticks
-	volatile efitimeus_t last_wave_high_widthUs; // time period in systimer ticks
+	volatile efitimeus_t waveOffsetUs = 0;
+	volatile efitimeus_t last_wave_low_widthUs = 0; // time period in systimer ticks
+	volatile efitimeus_t last_wave_high_widthUs = 0; // time period in systimer ticks
 };
 
 void initWaveAnalyzer(Logging *sharedLogger);
