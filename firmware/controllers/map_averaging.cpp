eb4ceda52b970c8505c8cdd890520d3e25b69538
rusefi <rusefillc@gmail.com>
2019-11-05 22:49:11 -0500

    refactoring: removing unused parameter

--- firmware/controllers/map_averaging.cpp
+++ firmware/controllers/map_averaging.cpp
@@ -266,7 +266,7 @@ static void mapAveragingTriggerCallback(trigger_event_e ckpEventType,
 #if EFI_ENGINE_CONTROL
 	// this callback is invoked on interrupt thread
 	UNUSED(ckpEventType);
-	if (index != CONFIG(mapAveragingSchedulingAtIndex))
+	if (index != (uint32_t)CONFIG(mapAveragingSchedulingAtIndex))
 		return;
 
 	engine->m.beforeMapAveragingCb = getTimeNowLowerNt();
@@ -310,9 +310,9 @@ static void mapAveragingTriggerCallback(trigger_event_e ckpEventType,
 		// we are loosing precision in case of changing RPM - the further away is the event the worse is precision
 		// todo: schedule this based on closest trigger event, same as ignition works
 		scheduleByAngle(rpm, &startTimer[i][structIndex], samplingStart,
-				startAveraging, NULL, &engine->rpmCalculator PASS_ENGINE_PARAMETER_SUFFIX);
+				startAveraging, NULL PASS_ENGINE_PARAMETER_SUFFIX);
 		scheduleByAngle(rpm, &endTimer[i][structIndex], samplingEnd,
-				endAveraging, NULL, &engine->rpmCalculator PASS_ENGINE_PARAMETER_SUFFIX);
+				endAveraging, NULL PASS_ENGINE_PARAMETER_SUFFIX);
 		engine->m.mapAveragingCbTime = getTimeNowLowerNt()
 				- engine->m.beforeMapAveragingCb;
 	}
