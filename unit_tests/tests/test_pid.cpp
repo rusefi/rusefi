49ed6254e591e4a5766866c1f06e0f8888640748
Andrei <andreika-git@users.noreply.github.com>
2019-11-10 19:39:13 +0200

    Meaningful unit-tests for PidIndustrial

--- unit_tests/tests/test_pid.cpp
+++ unit_tests/tests/test_pid.cpp
@@ -88,3 +88,73 @@ TEST(util, pidLimits) {
 	ASSERT_EQ( 40.0,  pid.getOutput(/*target*/50, /*input*/0)) << "target=50, input=0 #3";
 
 }
+
+TEST(util, pidIndustrial) {
+	pid_s pidS;
+	pidS.pFactor = 1.0;
+	pidS.iFactor = 1.0;
+	pidS.dFactor = 1.0;
+	pidS.offset = 0;
+	pidS.minValue = 0;
+	pidS.maxValue = 100;
+	pidS.periodMs = 1;
+
+	PidIndustrial pid;
+	pid.initPidClass(&pidS);
+
+	// we want to compare with the "normal" PID controller
+	Pid pid0(&pidS);
+
+	// no additional features
+	pid.derivativeFilterLoss = 0;
+	pid.antiwindupFreq = 0;
+
+	float industValue = pid.getOutput(/*target*/1, /*input*/0);
+	// check if the first output is clamped because of large deviative
+	ASSERT_EQ(100.0, industValue);
+
+	// check if all output of the 'zeroed' PidIndustrial (w/o new features) is the same as our "normal" Pid
+	for (int i = 0; i < 10; i++) {
+		float normalValue = pid0.getOutput(1, 0);
+		ASSERT_EQ(normalValue, industValue) << "[" << i << "]";
+		industValue = pid.getOutput(1, 0);
+	}
+
+	pid.reset();
+
+	// now test the "derivative filter loss" param (some small value)
+	pid.derivativeFilterLoss = 0.01;
+
+	// now the first value is less (and not clipped!) due to the derivative filtering
+	ASSERT_EQ(67.671669f, pid.getOutput(1, 0));
+	// here we still have some leftovers of the initial D-term
+	ASSERT_EQ(45.4544487f, pid.getOutput(1, 0));
+	// but the value is quickly fading
+	ASSERT_EQ(30.6446342f, pid.getOutput(1, 0));
+
+	pid.reset();
+
+	// now test much stronger "derivative filter loss"
+	pid.derivativeFilterLoss = 0.1;
+
+	// now the first value is much less due to the derivative filtering
+	ASSERT_EQ(10.5288095f, pid.getOutput(1, 0));
+	// here we still have some leftovers of the initial D-term
+	ASSERT_EQ(10.0802946f, pid.getOutput(1, 0));
+	// but the fading is slower than with 'weaker' derivative filter above
+	ASSERT_EQ(9.65337563f, pid.getOutput(1, 0));
+
+	pid.reset();
+	pid.derivativeFilterLoss = 0;
+
+	// now test "anti-windup" param
+	pid.antiwindupFreq = 0.1;
+
+	// the first value is clipped, and that's when the anti-windup comes into effect
+	ASSERT_EQ(100.0f, pid.getOutput(1, 0));
+	// it stores a small negative offset in the I-term to avoid it's saturation!
+	ASSERT_EQ(-0.0455025025f, pid.getIntegration());
+	// and that's why the second output is smaller then that of normal PID (=1.00999999)
+	ASSERT_EQ(0.959497511f, pid.getOutput(1, 0));
+
+}
