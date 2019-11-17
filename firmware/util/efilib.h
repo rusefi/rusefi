9f33df9574e94f875a348b6ef65de73dfc207232
andreika-git <andreika-git@users.noreply.github.com>
2019-11-17 15:08:57 +0200

    cisnan() warnings fix (#1008)
    * Fix cisnan() macro warnings (strict-aliasing)
    
    * Hey, we found a bug thanks to our fix! Let's fix it too!

--- firmware/util/efilib.h
+++ firmware/util/efilib.h
@@ -45,7 +45,19 @@ int indexOf(const char *string, char ch);
 float atoff(const char *string);
 int atoi(const char *string);
 
+#if defined(__cplusplus) && defined(__OPTIMIZE__)
+#include <type_traits>
+// "g++ -O2" version, adds more strict type check and yet no "strict-aliasing" warnings!
+#define cisnan(f) ({ \
+	static_assert(sizeof(f) == sizeof(int32_t)); \
+	union cisnanu_t { std::remove_reference_t<decltype(f)> __f; int32_t __i; } __cisnan_u = { f }; \
+	__cisnan_u.__i == 0x7FC00000; \
+})
+#else
+// "g++ -O0" or other C++/C compilers
 #define cisnan(f) (*(((int*) (&f))) == 0x7FC00000)
+#endif /* __cplusplus && __OPTIMIZE__ */
+
 #define UNUSED(x) (void)(x)
   
 int absI(int32_t value);
