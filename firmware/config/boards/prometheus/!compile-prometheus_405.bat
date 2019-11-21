0562a4abbdc10c4361860c2c3cdfced88c848514
Matthew Kennedy <matthewkennedy@outlook.com>
2019-11-21 00:10:56 -0800

    put batch files back, but minimum logic

---
+++ firmware/config/boards/prometheus/!compile-prometheus_405.bat
@@ -0,0 +1,10 @@
+rem 
+rem STM32F405 version of the firmware for https://rusefi.com/forum/viewtopic.php?f=4&t=1215
+rem
+
+cd ../../..
+set PROJECT_BOARD=prometheus/f405
+
+call config/boards/common_make.bat
+
+call config/boards/clean_env_variables.bat
---
+++ firmware/config/boards/prometheus/!compile-prometheus_469.bat
@@ -0,0 +1,11 @@
+rem 
+rem STM32F469 version of the firmware for https://rusefi.com/forum/viewtopic.php?f=4&t=1215
+rem
+
+cd ../../..
+set PROJECT_BOARD=prometheus/f469
+set USE_BOOTLOADER=yes
+
+call config/boards/common_make.bat
+
+call config/boards/clean_env_variables.bat
