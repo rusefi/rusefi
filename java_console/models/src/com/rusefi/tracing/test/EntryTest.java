19ebea8b952e8efaba7d18c87f309cb3f2620dff
rusefi <rusefillc@gmail.com>
2019-11-22 16:29:39 -0500

    chrome tracing is pretty cool

---
+++ java_console/models/src/com/rusefi/tracing/test/EntryTest.java
@@ -0,0 +1,17 @@
+package com.rusefi.tracing.test;
+
+import com.rusefi.tracing.Entry;
+import com.rusefi.tracing.Phase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EntryTest {
+    @Test
+    public void testToString() {
+        Entry e = new Entry("hello", Phase.E, 0.1);
+
+        assertEquals("{\"name\":\"hello\",\"ph\":\"E\",\"tid\":0,\"pid\":0,\"ts\":0.1}", e.toString());
+
+    }
+}
