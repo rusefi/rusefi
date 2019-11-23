3bf29b6963f44be03b2c3882c6658d7a562352c3
rusefi <rusefillc@gmail.com>
2019-11-23 17:07:13 -0500

    CI instability: FATAL error: warn stream not initialized for 6039 #798

--- firmware/controllers/core/error_handling.cpp
+++ firmware/controllers/core/error_handling.cpp
@@ -16,7 +16,7 @@ static MemoryStream warningStream;
 static MemoryStream firmwareErrorMessageStream;
 #endif /* EFI_SIMULATOR || EFI_PROD_CODE */
 
-#define WARNING_BUFFER_SIZE 80
+#define WARNING_BUFFER_SIZE 81
 static char warningBuffer[WARNING_BUFFER_SIZE];
 static volatile bool isWarningStreamInitialized = false;
 
@@ -211,7 +211,7 @@ void onUnlockHook(void) {
  */
 void initErrorHandlingDataStructures(void) {
 #if EFI_SIMULATOR || EFI_PROD_CODE
-	msObjectInit(&warningStream, (uint8_t *) warningBuffer, WARNING_BUFFER_SIZE, 0);
+	msObjectInit(&warningStream, (uint8_t *) warningBuffer, WARNING_BUFFER_SIZE - 1, 0);
 	msObjectInit(&firmwareErrorMessageStream, errorMessageBuffer, sizeof(errorMessageBuffer), 0);
 #endif
 	isWarningStreamInitialized = true;
