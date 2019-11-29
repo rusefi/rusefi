a9c52e084ffdc7b3efce58a4ff0a7b93e5282ade
rusefi <rusefillc@gmail.com>
2019-11-29 00:26:17 -0500

    steps towards dual ETB

--- firmware/controllers/algo/engine_configuration.cpp
+++ firmware/controllers/algo/engine_configuration.cpp
@@ -308,8 +308,8 @@ void prepareVoidConfiguration(engine_configuration_s *engineConfiguration) {
 	engineConfiguration->vbattAdcChannel = EFI_ADC_NONE;
 	engineConfiguration->map.sensor.hwChannel = EFI_ADC_NONE;
 	engineConfiguration->mafAdcChannel = EFI_ADC_NONE;
-	engineConfiguration->engineConfiguration->tps1_1AdcChannel = EFI_ADC_NONE;
-	engineConfiguration->engineConfiguration->tps2_1AdcChannel = EFI_ADC_NONE;
+	engineConfiguration->tps1_1AdcChannel = EFI_ADC_NONE;
+	engineConfiguration->tps2_1AdcChannel = EFI_ADC_NONE;
 	engineConfiguration->acSwitchAdc = EFI_ADC_NONE;
 	engineConfiguration->externalKnockSenseAdc = EFI_ADC_NONE;
 	engineConfiguration->fuelLevelSensor = EFI_ADC_NONE;
