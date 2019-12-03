07d24c2346b9a8b34554804f12548f1eab691ca6
Matthew Kennedy <matthewkennedy@outlook.com>
2019-12-02 18:10:31 -0800

    actually build the board config

--- firmware/config/boards/proteus/board.mk
+++ firmware/config/boards/proteus/board.mk
@@ -1,6 +1,6 @@
 # List of all the board related files.
 BOARDSRC = $(CHIBIOS)/os/hal/boards/ST_NUCLEO144_F767ZI/board.c
-BOARDSRC_CPP = $(PROJECT_DIR)/config/boards/microrusefi/board_configuration.cpp
+BOARDSRC_CPP = $(PROJECT_DIR)/config/boards/proteus/board_configuration.cpp
 
 # Required include directories
 BOARDINC = $(PROJECT_DIR)/config/boards/nucleo_f767 $(PROJECT_DIR)/config/stm32f7ems
