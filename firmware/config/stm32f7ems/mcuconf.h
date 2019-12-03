9774bc2829a7bee09d8b9479d1cf5a9482d95c26
Matthew Kennedy <matthewkennedy@outlook.com>
2019-12-03 01:18:24 -0800

    adc with thread

--- firmware/config/stm32f7ems/mcuconf.h
+++ firmware/config/stm32f7ems/mcuconf.h
@@ -285,7 +285,7 @@
 #define STM32_PWM_USE_TIM3                  FALSE
 #define STM32_PWM_USE_TIM4                  TRUE
 #define STM32_PWM_USE_TIM5                  FALSE
-#define STM32_PWM_USE_TIM8                  TRUE
+#define STM32_PWM_USE_TIM8                  FALSE
 #define STM32_PWM_USE_TIM9                  FALSE
 #define STM32_PWM_TIM1_IRQ_PRIORITY         7
 #define STM32_PWM_TIM2_IRQ_PRIORITY         7
