5c40bb4628786b7c36ba2c3dc383299850e8dce8
rusefi <rusefillc@gmail.com>
2019-12-03 00:29:12 -0500

    #490

--- firmware/controllers/trigger/aux_valves.h
+++ firmware/controllers/trigger/aux_valves.h
@@ -2,28 +2,14 @@
  * @file aux_valves.h
  *
  * @date Nov 25, 2017
- * @author Andrey Belomutskiy, (c) 2012-2017
+ * @author Andrey Belomutskiy, (c) 2012-2019
  */
 
 #pragma once
 
 #include "engine.h"
 
-/*
-class AuxActor {
-public:
-	int phaseIndex;
-	int valveIndex;
-	angle_t extra;
-
-	AngleBasedEvent open;
-	AngleBasedEvent close;
-};
-*/
-
 void initAuxValves(Logging *sharedLogger DECLARE_ENGINE_PARAMETER_SUFFIX);
 void updateAuxValves(DECLARE_ENGINE_PARAMETER_SIGNATURE);
-//void plainPinTurnOn(AuxActor *current);
-void plainPinTurnOn(NamedOutputPin *output);
+void plainPinTurnOn(AuxActor *current);
 void plainPinTurnOff(NamedOutputPin *output);
-
