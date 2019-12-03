d5bfd76922a5d72cb664b29b871e61992c528e98
rusefi <rusefillc@gmail.com>
2019-12-02 20:31:49 -0500

    "sensor chart" for aux fast analog input

--- firmware/development/sensor_chart.h
+++ firmware/development/sensor_chart.h
@@ -2,15 +2,12 @@
  * @file	sensor_chart.h
  *
  * @date Dec 20, 2013
- * @author Andrey Belomutskiy, (c) 2012-2017
+ * @author Andrey Belomutskiy, (c) 2012-2019
  */
 
-#ifndef SENSOR_CHART_H_
-#define SENSOR_CHART_H_
+#pragma once
 
 #include "global.h"
 
 void scAddData(float angle, float value);
 void initSensorChart(void);
-
-#endif /* SENSOR_CHART_H_ */
