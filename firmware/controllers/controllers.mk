f3a058ab2618f655177b59c42049266903737a24
rusefi <rusefillc@gmail.com>
2019-12-04 00:35:05 -0500

    The Big Refactoring of 2019: folder structure #723

--- firmware/controllers/controllers.mk
+++ firmware/controllers/controllers.mk
@@ -19,24 +19,24 @@ CONTROLLERS_SRC_CPP = \
 	$(CONTROLLERS_DIR)/gauges/tachometer.cpp \
 	$(CONTROLLERS_DIR)/gauges/malfunction_indicator.cpp \
 	$(CONTROLLERS_DIR)/gauges/lcd_controller.cpp \
-	$(CONTROLLERS_DIR)/scheduling/signal_executor_sleep.cpp \
-	$(CONTROLLERS_DIR)/scheduling/single_timer_executor.cpp \
-	$(CONTROLLERS_DIR)/scheduling/pwm_generator_logic.cpp \
-	$(CONTROLLERS_DIR)/scheduling/event_queue.cpp \
-	$(PROJECT_DIR)/controllers/settings.cpp \
-	$(PROJECT_DIR)/controllers/core/error_handling.cpp \
-	$(PROJECT_DIR)/controllers/map_averaging.cpp \
-	$(PROJECT_DIR)/controllers/flash_main.cpp \
-	$(PROJECT_DIR)/controllers/injector_central.cpp \
-	$(PROJECT_DIR)/controllers/obd2.cpp \
- 	$(PROJECT_DIR)/controllers/engine_controller.cpp \
-	$(PROJECT_DIR)/controllers/persistent_store.cpp \
+	$(CONTROLLERS_DIR)/system/timer/signal_executor_sleep.cpp \
+	$(CONTROLLERS_DIR)/system/timer/single_timer_executor.cpp \
+	$(CONTROLLERS_DIR)/system/timer/pwm_generator_logic.cpp \
+	$(CONTROLLERS_DIR)/system/timer/event_queue.cpp \
+	$(CONTROLLERS_DIR)/settings.cpp \
+	$(CONTROLLERS_DIR)/core/error_handling.cpp \
+	$(CONTROLLERS_DIR)/map_averaging.cpp \
+	$(CONTROLLERS_DIR)/flash_main.cpp \
+	$(CONTROLLERS_DIR)/injector_central.cpp \
+	$(CONTROLLERS_DIR)/obd2.cpp \
+ 	$(CONTROLLERS_DIR)/engine_controller.cpp \
+	$(CONTROLLERS_DIR)/persistent_store.cpp \
 	
 
 CONTROLLERS_INC=\
 	$(CONTROLLERS_DIR) \
-	$(CONTROLLERS_DIR)/scheduling \
 	$(CONTROLLERS_DIR)/system \
+	$(CONTROLLERS_DIR)/system/timer \
 	$(CONTROLLERS_DIR)/algo \
 	$(CONTROLLERS_DIR)/trigger/decoders \
 	$(CONTROLLERS_DIR)/trigger \
