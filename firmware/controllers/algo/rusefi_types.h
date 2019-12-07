3660e75890236a243a23ec5fef14458a7cd0d6f1
960 <olaruud@hotmail.com>
2019-12-07 19:28:30 +0100

    Add files via upload

--- firmware/controllers/algo/rusefi_types.h
+++ firmware/controllers/algo/rusefi_types.h
@@ -90,11 +90,9 @@ typedef uint8_t iac_pid_mult_t[IAC_PID_MULT_SIZE][IAC_PID_MULT_SIZE];
 typedef float baro_corr_table_t[BARO_CORR_SIZE][BARO_CORR_SIZE];
 
 typedef float fsio_table_8x8_f32t[FSIO_TABLE_8][FSIO_TABLE_8];
-typedef uint8_t fsio_table_8x8_u8t[FSIO_TABLE_8][FSIO_TABLE_8];
 typedef float tps_tps_table_t[TPS_TPS_ACCEL_TABLE][TPS_TPS_ACCEL_TABLE];
 typedef uint8_t fsio_table_8x8_u8t[FSIO_TABLE_8][FSIO_TABLE_8];
 typedef float fsio_table_8x8_f32t[BOOST_LOAD_COUNT][BOOST_RPM_COUNT];
-typedef uint8_t vvt_table_t[VVT_LOAD_COUNT][VVT_RPM_COUNT];
 
 
 // this is different type simply to have different hi/low range in rusefi.ini
