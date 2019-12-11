663c0235563c61ab5f8722c69acef5b6df4a3a36
ola <olaruud@hotmail.com>
2019-12-12 00:38:28 +0100

    boost

--- firmware/controllers/algo/engine_configuration.cpp
+++ firmware/controllers/algo/engine_configuration.cpp
@@ -76,6 +76,7 @@
 #include "lada_kalina.h"
 #include "zil130.h"
 #include "honda_600.h"
+#include "boost_control.h"
 
 #if EFI_IDLE_CONTROL
 #include "idle_thread.h"
@@ -183,6 +184,10 @@ void incrementGlobalConfigurationVersion(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 #if EFI_ALTERNATOR_CONTROL
 	onConfigurationChangeAlternatorCallback(&activeConfiguration);
 #endif /* EFI_ALTERNATOR_CONTROL */
+
+#if EFI_BOOST_CONTROL
+	onConfigurationChangeBoostCallback(&activeConfiguration);
+#endif
 #if EFI_ELECTRONIC_THROTTLE_BODY
 	onConfigurationChangeElectronicThrottleCallback(&activeConfiguration);
 #endif /* EFI_ELECTRONIC_THROTTLE_BODY */
@@ -575,6 +580,7 @@ static void setDefaultIdleSpeedTarget(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 	setCurveValue(engineConfiguration->cltIdleRpmBins, engineConfiguration->cltIdleRpm, CLT_CURVE_SIZE, 90, 900);
 	setCurveValue(engineConfiguration->cltIdleRpmBins, engineConfiguration->cltIdleRpm, CLT_CURVE_SIZE, 100, 1000);
 	setCurveValue(engineConfiguration->cltIdleRpmBins, engineConfiguration->cltIdleRpm, CLT_CURVE_SIZE, 110, 1100);
+
 }
 
 static void setDefaultStepperIdleParameters(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
@@ -646,7 +652,10 @@ static void setDefaultEngineConfiguration(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 	setDefaultEtbParameters(PASS_CONFIG_PARAMETER_SIGNATURE);
 	setDefaultEtbBiasCurve(PASS_CONFIG_PARAMETER_SIGNATURE);
 #endif /* EFI_ELECTRONIC_THROTTLE_BODY */
+#if EFI_BOOST_CONTROL
+    setDefaultBoostParameters(PASS_CONFIG_PARAMETER_SIGNATURE);
 
+#endif
 	CONFIG(mafSensorType) = Bosch0280218037;
 	setBosch0280218037(config);
 
