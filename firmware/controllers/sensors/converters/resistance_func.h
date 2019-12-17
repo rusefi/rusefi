5c2b934e096a4362165499cc077591acc0838575
Matthew Kennedy <matthewkennedy@outlook.com>
2019-12-16 20:11:17 -0800

    add functions

---
+++ firmware/controllers/sensors/converters/resistance_func.h
@@ -0,0 +1,22 @@
+/**
+ * @author Matthew Kennedy, (c) 2019
+ * 
+ * A function to convert input voltage to resistance in a voltage divider.
+ * Configured with the value of the pullup resistor, and the voltage to which
+ * it's connected.
+ */
+
+#pragma once
+
+#include "sensor_converter_func.h"
+
+class ResistanceFunc final : public SensorConverter {
+public:
+	void configure(float supplyVoltage, float pullupResistor);
+
+	SensorResult convert(float inputValue) const override;
+
+private:
+	float m_supplyVoltage = 5.0f;
+	float m_pullupResistor = 1000.0f;
+};
---
+++ firmware/controllers/sensors/converters/thermistor_func.cpp
@@ -0,0 +1,47 @@
+/**
+ * @author Matthew Kennedy, (c) 2019
+ */
+
+#include "thermistor_func.h"
+
+#include "thermistors.h"
+
+#include <math.h>
+
+SensorResult ThermistorFunc::convert(float ohms) const {
+	// This resistance should have already been validated - only
+	// thing we can check is that it's non-negative
+	if (ohms <= 0) {
+		return {false, 0};
+	}
+
+	float lnR = logf(ohms);
+
+	float lnR3 = lnR * lnR * lnR;
+
+	float recip = m_a + m_b * lnR + m_c * lnR3;
+
+	float kelvin = 1 / recip;
+
+	float celsius = convertKelvinToCelcius(kelvin);
+
+	return {true, celsius};
+}
+
+void ThermistorFunc::configure(thermistor_conf_s &cfg) {
+	// https://en.wikipedia.org/wiki/Steinhart%E2%80%93Hart_equation
+	float l1 = logf(cfg.resistance_1);
+	float l2 = logf(cfg.resistance_2);
+	float l3 = logf(cfg.resistance_3);
+
+	float y1 = 1 / convertCelsiusToKelvin(cfg.tempC_1);
+	float y2 = 1 / convertCelsiusToKelvin(cfg.tempC_2);
+	float y3 = 1 / convertCelsiusToKelvin(cfg.tempC_3);
+
+	float u2 = (y2 - y1) / (l2 - l1);
+	float u3 = (y3 - y1) / (l3 - l1);
+
+	m_c = ((u3 - u2) / (l3 - l2)) / (l1 + l2 + l3);
+	m_b = u2 - m_c * (l1 * l1 + l1 * l2 + l2 * l2);
+	m_a = y1 - (m_b + l1 * l1 * m_c) * l1;
+}
