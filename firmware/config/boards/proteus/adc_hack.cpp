b6be52da10725b336ed47bca4615b2625ab744d0
Matthew Kennedy <matthewkennedy@outlook.com>
2019-12-17 01:47:27 -0800

    simplify, cache coherency

--- firmware/config/boards/proteus/adc_hack.cpp
+++ firmware/config/boards/proteus/adc_hack.cpp
@@ -7,28 +7,25 @@
 
 #include "efilib.h"
 
-#define ADC_SAMPLING_SLOW ADC_SAMPLE_56
-
-static ADCConversionGroup adcConvGroup = { FALSE, 1, nullptr, NULL,
-	ADC_TwoSamplingDelay_20Cycles,
+static ADCConversionGroup adcConvGroup = { FALSE, 1, nullptr, nullptr,
+	0,
 	ADC_CR2_SWSTART,
 	0, // sample times for channels 10...18
-	ADC_SMPR2_SMP_AN0(ADC_SAMPLE_56), // sample times for channels 0...9
+	ADC_SMPR2_SMP_AN9(ADC_SAMPLE_56),
 
 	0,	// htr
 	0,	// ltr
 
 	0,	// sqr1
 	0,	// sqr2
-	9	// sqr3 - vbatt is on pf3 = adc9
+	ADC_SQR3_SQ1_N(ADC_CHANNEL_IN9)	// sqr3 - vbatt is on pf3 = adc9
 };
 
-static adcsample_t samples[8];
+__ALIGNED(32) adcsample_t samples[8];
 
 // we use this as a hook to run near the rest of ADC init...
 void setAdcChannelOverrides(void) {
-	brain_pin_e vbattPin = GPIOF_3;
-	efiSetPadMode("adc input", vbattPin, PAL_MODE_INPUT_ANALOG);
+	efiSetPadMode("adc input", GPIOF_3, PAL_MODE_INPUT_ANALOG);
 
 	adcStart(&ADCD3, nullptr);
 }
@@ -38,11 +35,12 @@ adcsample_t vbattSampleProteus = 0;
 void proteusAdcHack()
 {
 	adcConvert(&ADCD3, &adcConvGroup, samples, 8);
+	SCB_InvalidateDCache_by_Addr(reinterpret_cast<uint32_t*>(samples), sizeof(samples));
 
 	uint32_t sum = 0;
 
-	for (const auto s : samples) {
-		sum += s;
+	for (int i = 0; i < 8; i++) {
+		sum += samples[i];
 	}
 
 	vbattSampleProteus = sum / efi::size(samples);
