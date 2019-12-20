9363d081c31ed3cf4ac50454942f472db1ee4b23
rusefi <rusefillc@gmail.com>
2019-12-19 20:38:48 -0500

    fresh integration

--- firmware/config/boards/kinetis/config/controllers/algo/engine_configuration_generated_structures.h
+++ firmware/config/boards/kinetis/config/controllers/algo/engine_configuration_generated_structures.h
@@ -1,4 +1,4 @@
-// this section was generated automatically by rusEfi tool ConfigDefinition.jar based on integration/rusefi_config.txt Wed Dec 11 16:41:07 EST 2019
+// this section was generated automatically by rusEfi tool ConfigDefinition.jar based on integration/rusefi_config.txt Thu Dec 19 20:38:11 EST 2019
 // by class com.rusefi.output.CHeaderConsumer
 // begin
 #ifndef CONFIG_BOARDS_KINETIS_CONFIG_CONTROLLERS_ALGO_ENGINE_CONFIGURATION_GENERATED_STRUCTURES_H
@@ -348,6 +348,93 @@ struct trigger_config_s {
 	offset 4 bit 2 */
 	bool useOnlyFirstChannel : 1;
 	/**
+	offset 4 bit 3 */
+	bool unusedBit_4_3 : 1;
+	/**
+	offset 4 bit 4 */
+	bool unusedBit_4_4 : 1;
+	/**
+	offset 4 bit 5 */
+	bool unusedBit_4_5 : 1;
+	/**
+	offset 4 bit 6 */
+	bool unusedBit_4_6 : 1;
+	/**
+	offset 4 bit 7 */
+	bool unusedBit_4_7 : 1;
+	/**
+	offset 4 bit 8 */
+	bool unusedBit_4_8 : 1;
+	/**
+	offset 4 bit 9 */
+	bool unusedBit_4_9 : 1;
+	/**
+	offset 4 bit 10 */
+	bool unusedBit_4_10 : 1;
+	/**
+	offset 4 bit 11 */
+	bool unusedBit_4_11 : 1;
+	/**
+	offset 4 bit 12 */
+	bool unusedBit_4_12 : 1;
+	/**
+	offset 4 bit 13 */
+	bool unusedBit_4_13 : 1;
+	/**
+	offset 4 bit 14 */
+	bool unusedBit_4_14 : 1;
+	/**
+	offset 4 bit 15 */
+	bool unusedBit_4_15 : 1;
+	/**
+	offset 4 bit 16 */
+	bool unusedBit_4_16 : 1;
+	/**
+	offset 4 bit 17 */
+	bool unusedBit_4_17 : 1;
+	/**
+	offset 4 bit 18 */
+	bool unusedBit_4_18 : 1;
+	/**
+	offset 4 bit 19 */
+	bool unusedBit_4_19 : 1;
+	/**
+	offset 4 bit 20 */
+	bool unusedBit_4_20 : 1;
+	/**
+	offset 4 bit 21 */
+	bool unusedBit_4_21 : 1;
+	/**
+	offset 4 bit 22 */
+	bool unusedBit_4_22 : 1;
+	/**
+	offset 4 bit 23 */
+	bool unusedBit_4_23 : 1;
+	/**
+	offset 4 bit 24 */
+	bool unusedBit_4_24 : 1;
+	/**
+	offset 4 bit 25 */
+	bool unusedBit_4_25 : 1;
+	/**
+	offset 4 bit 26 */
+	bool unusedBit_4_26 : 1;
+	/**
+	offset 4 bit 27 */
+	bool unusedBit_4_27 : 1;
+	/**
+	offset 4 bit 28 */
+	bool unusedBit_4_28 : 1;
+	/**
+	offset 4 bit 29 */
+	bool unusedBit_4_29 : 1;
+	/**
+	offset 4 bit 30 */
+	bool unusedBit_4_30 : 1;
+	/**
+	offset 4 bit 31 */
+	bool unusedBit_4_31 : 1;
+	/**
 	 * offset 8
 	 */
 	int customTotalToothCount;
@@ -557,6 +644,9 @@ struct engine_configuration_s {
 	/**
 	offset 76 bit 30 */
 	bool issue_294_31 : 1;
+	/**
+	offset 76 bit 31 */
+	bool unusedBit_34_31 : 1;
 	/**
 	 * Closed throttle. todo: extract these two fields into a structure
 	 * See also tps1_1AdcChannel
@@ -1476,6 +1566,96 @@ struct engine_configuration_s {
 	/**
 	offset 976 bit 1 */
 	bool todoClutchDownPinInverted : 1;
+	/**
+	offset 976 bit 2 */
+	bool unusedBit_247_2 : 1;
+	/**
+	offset 976 bit 3 */
+	bool unusedBit_247_3 : 1;
+	/**
+	offset 976 bit 4 */
+	bool unusedBit_247_4 : 1;
+	/**
+	offset 976 bit 5 */
+	bool unusedBit_247_5 : 1;
+	/**
+	offset 976 bit 6 */
+	bool unusedBit_247_6 : 1;
+	/**
+	offset 976 bit 7 */
+	bool unusedBit_247_7 : 1;
+	/**
+	offset 976 bit 8 */
+	bool unusedBit_247_8 : 1;
+	/**
+	offset 976 bit 9 */
+	bool unusedBit_247_9 : 1;
+	/**
+	offset 976 bit 10 */
+	bool unusedBit_247_10 : 1;
+	/**
+	offset 976 bit 11 */
+	bool unusedBit_247_11 : 1;
+	/**
+	offset 976 bit 12 */
+	bool unusedBit_247_12 : 1;
+	/**
+	offset 976 bit 13 */
+	bool unusedBit_247_13 : 1;
+	/**
+	offset 976 bit 14 */
+	bool unusedBit_247_14 : 1;
+	/**
+	offset 976 bit 15 */
+	bool unusedBit_247_15 : 1;
+	/**
+	offset 976 bit 16 */
+	bool unusedBit_247_16 : 1;
+	/**
+	offset 976 bit 17 */
+	bool unusedBit_247_17 : 1;
+	/**
+	offset 976 bit 18 */
+	bool unusedBit_247_18 : 1;
+	/**
+	offset 976 bit 19 */
+	bool unusedBit_247_19 : 1;
+	/**
+	offset 976 bit 20 */
+	bool unusedBit_247_20 : 1;
+	/**
+	offset 976 bit 21 */
+	bool unusedBit_247_21 : 1;
+	/**
+	offset 976 bit 22 */
+	bool unusedBit_247_22 : 1;
+	/**
+	offset 976 bit 23 */
+	bool unusedBit_247_23 : 1;
+	/**
+	offset 976 bit 24 */
+	bool unusedBit_247_24 : 1;
+	/**
+	offset 976 bit 25 */
+	bool unusedBit_247_25 : 1;
+	/**
+	offset 976 bit 26 */
+	bool unusedBit_247_26 : 1;
+	/**
+	offset 976 bit 27 */
+	bool unusedBit_247_27 : 1;
+	/**
+	offset 976 bit 28 */
+	bool unusedBit_247_28 : 1;
+	/**
+	offset 976 bit 29 */
+	bool unusedBit_247_29 : 1;
+	/**
+	offset 976 bit 30 */
+	bool unusedBit_247_30 : 1;
+	/**
+	offset 976 bit 31 */
+	bool unusedBit_247_31 : 1;
 	/**
 	 * offset 980
 	 */
@@ -2928,4 +3108,4 @@ typedef struct persistent_config_s persistent_config_s;
 
 #endif
 // end
-// this section was generated automatically by rusEfi tool ConfigDefinition.jar based on integration/rusefi_config.txt Wed Dec 11 16:41:07 EST 2019
+// this section was generated automatically by rusEfi tool ConfigDefinition.jar based on integration/rusefi_config.txt Thu Dec 19 20:38:11 EST 2019
