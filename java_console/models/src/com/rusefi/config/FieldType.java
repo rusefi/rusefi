871f86967fbc34318297015cfb446f76566b2893
rusefi <rusefillc@gmail.com>
2019-12-21 15:02:49 -0500

    Revert "DRAFT Shrink output channels, again (#1074)"
    This reverts commit 562ce6cc

--- java_console/models/src/com/rusefi/config/FieldType.java
+++ java_console/models/src/com/rusefi/config/FieldType.java
@@ -1,15 +1,12 @@
 package com.rusefi.config;
 
 public enum FieldType {
-    // Signed
-    INT8(1),
-    INT16(2),
     INT(4),
-
-    // Unsigned
-    UINT8(1),
-    UINT16(2),
-
+    /**
+     * signed 16 bit type
+     */
+    INT16(2),
+    INT8(1),
     BIT(/*bits are stored in 4 byte packs */4),
     FLOAT(4);
 
@@ -28,10 +25,8 @@ public enum FieldType {
             case FLOAT:
                 return FLOAT_TYPE_STRING;
             case INT16:
-            //case UINT16:
                 return SHORT_TYPE_STRING;
             case INT8:
-            //case UINT8:
                 return BYTE_TYPE_STRING;
             case INT:
             default:
