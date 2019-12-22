218f6ea7bd1e44110f84b97df3e0e9f0d5440b63
Matthew Kennedy <matthewkennedy@outlook.com>
2019-12-22 00:11:58 -0800

    field type cases

--- java_console/models/src/com/rusefi/config/FieldType.java
+++ java_console/models/src/com/rusefi/config/FieldType.java
@@ -16,7 +16,10 @@ public enum FieldType {
     public static final String INT_TYPE_STRING = "int";
     public static final String FLOAT_TYPE_STRING = "float";
     public static final String BYTE_TYPE_STRING = "byte";
+    public static final String UBYTE_TYPE_STRING = "ubyte";
     public static final String SHORT_TYPE_STRING = "short";
+    public static final String USHORT_TYPE_STRING = "ushort";
+
     private final int storageSize;
 
     FieldType(int storageSize) {
@@ -28,11 +31,13 @@ public enum FieldType {
             case FLOAT:
                 return FLOAT_TYPE_STRING;
             case INT16:
-            //case UINT16:
                 return SHORT_TYPE_STRING;
+            case UINT16:
+                return USHORT_TYPE_STRING;
             case INT8:
-            //case UINT8:
                 return BYTE_TYPE_STRING;
+            case UINT8:
+                return UBYTE_TYPE_STRING;
             case INT:
             default:
                 return INT_TYPE_STRING;
