1576a9f7318c7d02162ffe81fe43887b38a86821
rusefi <rusefillc@gmail.com>
2019-12-23 22:30:58 -0500

    adding simplicity

--- firmware/controllers/engine_controller.h
+++ firmware/controllers/engine_controller.h
@@ -9,14 +9,11 @@
 #pragma once
 
 #include "global.h"
-#include "engine_configuration.h"
-#include "engine.h"
 
 char * getPinNameByAdcChannel(const char *msg, adc_channel_e hwChannel, char *buffer);
 void initPeriodicEvents(DECLARE_ENGINE_PARAMETER_SIGNATURE);
 void initEngineContoller(Logging *sharedLogger DECLARE_ENGINE_PARAMETER_SUFFIX);
 void commonInitEngineController(Logging *sharedLogger DECLARE_ENGINE_PARAMETER_SUFFIX);
-void mostCommonInitEngineController(Logging *sharedLogger DECLARE_ENGINE_PARAMETER_SUFFIX);
 
 #if EFI_ENABLE_MOCK_ADC
 void setMockVoltage(int hwChannel, float voltage DECLARE_ENGINE_PARAMETER_SUFFIX);
