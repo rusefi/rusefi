8a9a6babdade5495f0edf54cd3447f53aab4b1b3
rusefi <rusefillc@gmail.com>
2019-12-30 00:43:32 -0500

    jlcpcb progress

--- hardware/Common_Rail_MC33816/gerbers/kicadPos2jlcpcb.bat
+++ hardware/Common_Rail_MC33816/gerbers/kicadPos2jlcpcb.bat
@@ -3,7 +3,7 @@ rem This script takes care only of the mechanical part of the transformation - t
 rem 
 
 set inputFile=Common_Rail_MC33816-bottom.pos
-set outputFile=Common_Rail_MC33816-cpl.csv
+set outputFile=MC33816_R0.3-cpl.csv
 
 echo "Translating from %inputFile% to %outputFile%"
 
@@ -11,7 +11,7 @@ echo "Translating from %inputFile% to %outputFile%"
 grep -v Package %inputFile% | grep -v # | gawk '{ sub(/\..*/,"", $6); print $1 "," $4 "mm," $5 "mm,Bottom," $6}' > %outputFile%.temp
 sed -f ../../tools/unquote.sed %outputFile%.temp > %outputFile%.all
 
-echo "Designator,Mid X,Mid Y,Layer,Rotation" > %outputFile%
+echo Designator,Mid X,Mid Y,Layer,Rotation> %outputFile%
 
 grep -v "L\|D" %outputFile%.all >> %outputFile%
 
--- hardware/Common_Rail_MC33816/gerbers/readme.md
+++ hardware/Common_Rail_MC33816/gerbers/readme.md
@@ -18,5 +18,7 @@
 Pre-assembly notes:
 
 -- 0.22uF needs at least 100V, but I only found a 50V option, so that one is not populated by china. This is QTY 4. 
+(C16 C17 C25 C27)
 
 -- 0R010 may not need to be 0R010, there are several 0R015. However this is not populated at this time. Further research might allow it to be populated. It's only QTY 1. 
+(R12)
