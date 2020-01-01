2ebdf2ad10c43dc27e26288d6867c4160f844c00
rusefi <rusefillc@gmail.com>
2019-12-31 20:39:11 -0500

    jlcpcb progress

--- hardware/Common_Rail_MC33816/gerbers/kicadBom2jlcpcb.bat
+++ hardware/Common_Rail_MC33816/gerbers/kicadBom2jlcpcb.bat
@@ -1,3 +1,4 @@
+@echo off
 
 set inputFile=../Common_Rail_MC33816.csv
 set outputFile=MC33816_R0.3-bom.csv
@@ -8,15 +9,7 @@ echo "Translating from %inputFile% to %outputFile%"
 
 echo Comment,Designator,Footprint,LCSC Part #> %outputFile%
                        
-grep JLCPCB %inputFile%
 
 grep JLCPCB %inputFile% > %outputFile%.temp
 
-sed -f removedoublecomma.sed %outputFile%.temp > %outputFile%.all
-
-
-
-
-rem  | gawk -F "," '{print $4}'
-
-
+sed -f removedoublecomma.sed %outputFile%.temp | gawk -F "," '{print $3 "," $1 "," $4 "," $10}' >> %outputFile%
