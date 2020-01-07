79766fd5176ab6bbf3a434fedd8696b228e1d612
Andrey Gusakov <dron0gus@gmail.com>
2020-01-07 13:37:05 +0300

    exti trigger: mark pins used, handle errors

--- firmware/hw_layer/digital_input_exti.h
+++ firmware/hw_layer/digital_input_exti.h
@@ -10,6 +10,6 @@
 #include "digital_input.h"
 
 #if HAL_USE_PAL
-void efiExtiEnablePin(const char *msg, brain_pin_e pin, uint32_t mode, palcallback_t cb, void *cb_data);
+int efiExtiEnablePin(const char *msg, brain_pin_e pin, uint32_t mode, palcallback_t cb, void *cb_data);
 void efiExtiDisablePin(brain_pin_e brainPin);
 #endif /* HAL_USE_PAL */
