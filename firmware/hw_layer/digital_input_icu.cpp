489e0df3c976ef72cbda99e0cc75ee976b8ad703
dron0gus <dron0gus@gmail.com>
2020-01-07 11:42:08 +0300

    triggers: fix coexistence of ICU and EXTI trigger drivers (#1085)

--- firmware/hw_layer/digital_input_icu.cpp
+++ firmware/hw_layer/digital_input_icu.cpp
@@ -156,6 +156,8 @@ icuchannel_t getInputCaptureChannel(brain_pin_e hwPin) {
  */
 //Nullable
 ICUDriver * getInputCaptureDriver(const char *msg, brain_pin_e hwPin) {
+	UNUSED(msg);
+
 	if (hwPin == GPIO_UNASSIGNED || hwPin == GPIO_INVALID) {
 		return NULL;
 	}
@@ -199,7 +201,6 @@ ICUDriver * getInputCaptureDriver(const char *msg, brain_pin_e hwPin) {
 		return &ICUD9;
 	}
 #endif
-	firmwareError(CUSTOM_ERR_NOT_INPUT_PIN, "%s: Not input pin %s", msg, hwPortname(hwPin));
 	return (ICUDriver *) NULL;
 }
 
