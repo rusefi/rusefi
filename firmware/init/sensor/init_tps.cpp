01f85819112bd0bd15f4685ea990f5624b19a905
Matthew Kennedy <matthew@mck-mbp-15.local>
2020-01-11 21:38:09 -0800

    tps init

---
+++ firmware/init/sensor/init_tps.cpp
@@ -0,0 +1,48 @@
+#include "adc_subscription.h"
+#include "engine.h"
+#include "error_handling.h"
+#include "global.h"
+#include "functional_sensor.h"
+#include "linear_func.h"
+#if EFI_TUNER_STUDIO
+#include "tunerstudio_configuration.h"
+#endif
+
+EXTERN_ENGINE;
+
+LinearFunc tpsFunc1p;
+//LinearFunc tpsFunc1s;
+LinearFunc tpsFunc2p;
+//LinearFunc tpsFunc2s;
+
+FunctionalSensor tpsSens1p(SensorType::Tps1Primary);
+//FunctionalSensor tpsSens1s(SensorType::Tps1Secondary);
+FunctionalSensor tpsSens2p(SensorType::Tps2Primary);
+//FunctionalSensor tpsSens2s(SensorType::Tps2Secondary);
+
+void initTpsFunc(LinearFunc& func, FunctionalSensor& sensor, adc_channel_e channel, float closed, float open) {
+	// Only register if we have a sensor
+	if (channel == EFI_ADC_NONE) {
+		return;
+	}
+
+	func.configure(
+		closed / 200.0f, 0,
+		open / 200.0f, 100, 
+		CONFIG(tpsErrorDetectionTooLow),
+		CONFIG(tpsErrorDetectionTooHigh)
+	);
+
+	sensor.setFunction(func);
+
+	AdcSubscription::SubscribeSensor(sensor, channel);
+
+	if (!sensor.Register()) {
+		// todo: handle
+	}
+}
+
+void initTps() {
+	initTpsFunc(tpsFunc1p, tpsSens1p, CONFIG(tps1_1AdcChannel), CONFIG(tpsMin), CONFIG(tpsMax));
+	initTpsFunc(tpsFunc2p, tpsSens2p, CONFIG(tps2_1AdcChannel), CONFIG(tps2Min), CONFIG(tps2Max));
+}
