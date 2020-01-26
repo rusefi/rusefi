d805a05b2c84e8c266793fd0579eec2ec705d73b
Matthew Kennedy <matthew@mck-mbp-15.stagpad.local>
2020-01-25 17:09:59 -0800

    conventions

--- firmware/hw_layer/sensors/cj125.cpp
+++ firmware/hw_layer/sensors/cj125.cpp
@@ -30,8 +30,8 @@ EXTERN_ENGINE;
 
 static Logging *logger;
 
-static unsigned char tx_buff[2] NO_CACHE;
-static unsigned char rx_buff[1] NO_CACHE;
+static uint8_t tx_buff[2] NO_CACHE;
+static uint8_t rx_buff[1] NO_CACHE;
 
 static CJ125 globalInstance;
 
@@ -80,15 +80,11 @@ static constexpr float lambdaLsu49[] = {
 };
 
 
-static int cjReadRegister(unsigned char regAddr) {
+static uint8_t cjReadRegister(uint8_t regAddr) {
 #if ! EFI_UNIT_TEST
 	spiSelect(driver);
 	tx_buff[0] = regAddr;
 	spiSend(driver, 1, tx_buff);
-	// safety?
-	chThdSleepMilliseconds(10);
-	
-	rx_buff[0] = 0;
 	spiReceive(driver, 1, rx_buff);
 	spiUnselect(driver);
 
@@ -101,7 +97,7 @@ static int cjReadRegister(unsigned char regAddr) {
 #endif /* EFI_UNIT_TEST */
 }
 
-static void cjWriteRegister(unsigned char regAddr, unsigned char regValue) {
+static void cjWriteRegister(uint8_t regAddr, uint8_t regValue) {
 	tx_buff[0] = regAddr;
 	tx_buff[1] = regValue;
 #ifdef CJ125_DEBUG_SPI
