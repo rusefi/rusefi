b47462b4debe8c01e98bf323ec668a1ad8d32f14
rusefi <rusefillc@gmail.com>
2020-02-04 01:22:04 -0500

    Three wire idle air valve #1122 ugly but should work to begin with

--- firmware/controllers/system/efi_gpio.cpp
+++ firmware/controllers/system/efi_gpio.cpp
@@ -135,6 +135,7 @@ void EnginePins::unregisterPins() {
 	unregisterOutputIfPinOrModeChanged(dizzyOutput, dizzySparkOutputPin, dizzySparkOutputPinMode);
 	unregisterOutputIfPinOrModeChanged(tachOut, tachOutputPin, tachOutputPinMode);
 	unregisterOutputIfPinOrModeChanged(idleSolenoidPin, idle.solenoidPin, idle.solenoidPinMode);
+	unregisterOutputIfPinOrModeChanged(secondIdleSolenoidPin, secondSolenoidPin, idle.solenoidPinMode);
 	unregisterOutputIfPinChanged(sdCsPin, sdCardCsPin);
 	unregisterOutputIfPinChanged(accelerometerCs, LIS302DLCsPin);
 
