b0d7cc9a22275744c7746437c817fd1748fcf3b0
rusefi <rusefillc@gmail.com>
2020-02-08 07:08:22 -0500

    partial Revert of "Stricter compile options (#1132)"

--- firmware/rusefi_rules.mk
+++ firmware/rusefi_rules.mk
@@ -1,5 +1,5 @@
-# Warnings-as-errors...
-RUSEFI_OPT = -Werror
+RUSEFI_OPT=-Werror -Wno-error=pointer-sign -Wno-error=unused-function -Wno-error=unused-variable -Wno-error=sign-compare -Wno-error=unused-parameter -Werror=missing-field-initializers
 
-# ...except these few
-RUSEFI_OPT += -Wno-error=unused-function -Wno-error=unused-variable -Wno-error=sign-compare -Wno-error=unused-parameter
+RUSEFI_OPT+=-Wno-error=missing-prototypes
+
+RUSEFI_OPT+=-Werror=switch
