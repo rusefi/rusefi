21853eb5007b60971d5641717fb7608cc4cca82f
rusEfi <russianefi@gmail.com>
2020-02-11 17:49:39 -0500

    FATAL error running MRE blinking test #1137 still not the fix?

--- firmware/controllers/engine_controller.cpp
+++ firmware/controllers/engine_controller.cpp
@@ -843,6 +843,6 @@ int getRusEfiVersion(void) {
 	if (initBootloader() != 0)
 		return 123;
 #endif /* EFI_BOOTLOADER_INCLUDE_CODE */
-	return 201200209;
+	return 201200211;
 }
 #endif /* EFI_UNIT_TEST */
