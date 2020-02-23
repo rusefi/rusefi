1d0143338785143ddc0fc29ec34ceece255e3ea7
Matthew Kennedy <matthewkennedy@outlook.com>
2020-02-22 17:23:28 -0800

    extra check

--- firmware/util/math/crc.h
+++ firmware/util/math/crc.h
@@ -27,6 +27,8 @@ uint32_t crc32inc(const void *buf, uint32_t crc, uint32_t size);
 template <class T>
 crc_t calc_crc(const T& object)
 {
+    static_assert(!std::is_pointer_v<T>, "Did you really mean to take the CRC of a pointer?");
+
     return crc32(reinterpret_cast<const void*>(&object), sizeof(T));
 }
 
