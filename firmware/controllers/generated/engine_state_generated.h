1ce1f43a4970b679437db0a3869f5b1f46b15b23
rusEfi <russianefi@gmail.com>
2020-02-25 22:01:13 -0500

    fresh generated

--- firmware/controllers/generated/engine_state_generated.h
+++ firmware/controllers/generated/engine_state_generated.h
@@ -1,4 +1,4 @@
-// this section was generated automatically by rusEfi tool ConfigDefinition.jar based on integration/engine_state.txt Tue Nov 05 23:21:45 EST 2019
+// this section was generated automatically by rusEfi tool ConfigDefinition.jar based on integration/engine_state.txt Sat Feb 22 15:19:24 EST 2020
 // by class com.rusefi.output.CHeaderConsumer
 // begin
 #ifndef CONTROLLERS_GENERATED_ENGINE_STATE_GENERATED_H
@@ -9,6 +9,99 @@ struct speed_density_s {
 	/**
 	offset 0 bit 0 */
 	bool isTChargeAirModel : 1;
+	/**
+	offset 0 bit 1 */
+	bool unusedBit_1_1 : 1;
+	/**
+	offset 0 bit 2 */
+	bool unusedBit_1_2 : 1;
+	/**
+	offset 0 bit 3 */
+	bool unusedBit_1_3 : 1;
+	/**
+	offset 0 bit 4 */
+	bool unusedBit_1_4 : 1;
+	/**
+	offset 0 bit 5 */
+	bool unusedBit_1_5 : 1;
+	/**
+	offset 0 bit 6 */
+	bool unusedBit_1_6 : 1;
+	/**
+	offset 0 bit 7 */
+	bool unusedBit_1_7 : 1;
+	/**
+	offset 0 bit 8 */
+	bool unusedBit_1_8 : 1;
+	/**
+	offset 0 bit 9 */
+	bool unusedBit_1_9 : 1;
+	/**
+	offset 0 bit 10 */
+	bool unusedBit_1_10 : 1;
+	/**
+	offset 0 bit 11 */
+	bool unusedBit_1_11 : 1;
+	/**
+	offset 0 bit 12 */
+	bool unusedBit_1_12 : 1;
+	/**
+	offset 0 bit 13 */
+	bool unusedBit_1_13 : 1;
+	/**
+	offset 0 bit 14 */
+	bool unusedBit_1_14 : 1;
+	/**
+	offset 0 bit 15 */
+	bool unusedBit_1_15 : 1;
+	/**
+	offset 0 bit 16 */
+	bool unusedBit_1_16 : 1;
+	/**
+	offset 0 bit 17 */
+	bool unusedBit_1_17 : 1;
+	/**
+	offset 0 bit 18 */
+	bool unusedBit_1_18 : 1;
+	/**
+	offset 0 bit 19 */
+	bool unusedBit_1_19 : 1;
+	/**
+	offset 0 bit 20 */
+	bool unusedBit_1_20 : 1;
+	/**
+	offset 0 bit 21 */
+	bool unusedBit_1_21 : 1;
+	/**
+	offset 0 bit 22 */
+	bool unusedBit_1_22 : 1;
+	/**
+	offset 0 bit 23 */
+	bool unusedBit_1_23 : 1;
+	/**
+	offset 0 bit 24 */
+	bool unusedBit_1_24 : 1;
+	/**
+	offset 0 bit 25 */
+	bool unusedBit_1_25 : 1;
+	/**
+	offset 0 bit 26 */
+	bool unusedBit_1_26 : 1;
+	/**
+	offset 0 bit 27 */
+	bool unusedBit_1_27 : 1;
+	/**
+	offset 0 bit 28 */
+	bool unusedBit_1_28 : 1;
+	/**
+	offset 0 bit 29 */
+	bool unusedBit_1_29 : 1;
+	/**
+	offset 0 bit 30 */
+	bool unusedBit_1_30 : 1;
+	/**
+	offset 0 bit 31 */
+	bool unusedBit_1_31 : 1;
 	/**
 	 * Speed-density logic: calculated air mass in one cylinder, in grams
 	 * offset 4
@@ -211,6 +304,93 @@ struct engine_state2_s {
 	offset 88 bit 2 */
 	bool isAutomaticIdle : 1;
 	/**
+	offset 88 bit 3 */
+	bool unusedBit_14_3 : 1;
+	/**
+	offset 88 bit 4 */
+	bool unusedBit_14_4 : 1;
+	/**
+	offset 88 bit 5 */
+	bool unusedBit_14_5 : 1;
+	/**
+	offset 88 bit 6 */
+	bool unusedBit_14_6 : 1;
+	/**
+	offset 88 bit 7 */
+	bool unusedBit_14_7 : 1;
+	/**
+	offset 88 bit 8 */
+	bool unusedBit_14_8 : 1;
+	/**
+	offset 88 bit 9 */
+	bool unusedBit_14_9 : 1;
+	/**
+	offset 88 bit 10 */
+	bool unusedBit_14_10 : 1;
+	/**
+	offset 88 bit 11 */
+	bool unusedBit_14_11 : 1;
+	/**
+	offset 88 bit 12 */
+	bool unusedBit_14_12 : 1;
+	/**
+	offset 88 bit 13 */
+	bool unusedBit_14_13 : 1;
+	/**
+	offset 88 bit 14 */
+	bool unusedBit_14_14 : 1;
+	/**
+	offset 88 bit 15 */
+	bool unusedBit_14_15 : 1;
+	/**
+	offset 88 bit 16 */
+	bool unusedBit_14_16 : 1;
+	/**
+	offset 88 bit 17 */
+	bool unusedBit_14_17 : 1;
+	/**
+	offset 88 bit 18 */
+	bool unusedBit_14_18 : 1;
+	/**
+	offset 88 bit 19 */
+	bool unusedBit_14_19 : 1;
+	/**
+	offset 88 bit 20 */
+	bool unusedBit_14_20 : 1;
+	/**
+	offset 88 bit 21 */
+	bool unusedBit_14_21 : 1;
+	/**
+	offset 88 bit 22 */
+	bool unusedBit_14_22 : 1;
+	/**
+	offset 88 bit 23 */
+	bool unusedBit_14_23 : 1;
+	/**
+	offset 88 bit 24 */
+	bool unusedBit_14_24 : 1;
+	/**
+	offset 88 bit 25 */
+	bool unusedBit_14_25 : 1;
+	/**
+	offset 88 bit 26 */
+	bool unusedBit_14_26 : 1;
+	/**
+	offset 88 bit 27 */
+	bool unusedBit_14_27 : 1;
+	/**
+	offset 88 bit 28 */
+	bool unusedBit_14_28 : 1;
+	/**
+	offset 88 bit 29 */
+	bool unusedBit_14_29 : 1;
+	/**
+	offset 88 bit 30 */
+	bool unusedBit_14_30 : 1;
+	/**
+	offset 88 bit 31 */
+	bool unusedBit_14_31 : 1;
+	/**
 	 * offset 92
 	 */
 	cranking_fuel_s cranking;
@@ -250,4 +430,4 @@ typedef struct engine_state2_s engine_state2_s;
 
 #endif
 // end
-// this section was generated automatically by rusEfi tool ConfigDefinition.jar based on integration/engine_state.txt Tue Nov 05 23:21:45 EST 2019
+// this section was generated automatically by rusEfi tool ConfigDefinition.jar based on integration/engine_state.txt Sat Feb 22 15:19:24 EST 2020
