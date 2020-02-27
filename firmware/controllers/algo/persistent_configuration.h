89e6a3354539e07bf500995508ef9926334cf11f
rusEfi <russianefi@gmail.com>
2020-02-27 01:32:26 -0500

    giving C code access to CONFIG

---
+++ firmware/controllers/algo/persistent_configuration.h
@@ -0,0 +1,19 @@
+/*
+ * @file persistent_configuration.h
+ *
+ * @date Feb 27, 2020
+ * @author Andrey Belomutskiy, (c) 2012-2020
+ */
+
+#pragma once
+
+#include "globalaccess.h"
+#include "crc.h"
+#include "engine_configuration_generated_structures.h"
+
+typedef struct {
+	int version;
+	int size;
+	persistent_config_s persistentConfiguration;
+	crc_t value;
+} persistent_config_container_s;
