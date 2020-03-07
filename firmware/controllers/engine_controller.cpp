bd93d9e20413ef307a43fc682670d18d6353d8fb
rusEfi <russianefi@gmail.com>
2020-03-07 15:55:08 -0500

    date

--- firmware/controllers/engine_controller.cpp
+++ firmware/controllers/engine_controller.cpp
@@ -849,6 +849,6 @@ int getRusEfiVersion(void) {
 	if (initBootloader() != 0)
 		return 123;
 #endif /* EFI_BOOTLOADER_INCLUDE_CODE */
-	return 201200305;
+	return 201200307;
 }
 #endif /* EFI_UNIT_TEST */
