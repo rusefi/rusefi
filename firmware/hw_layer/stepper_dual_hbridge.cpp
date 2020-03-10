adac5fba78fc9d53f277babaad646257ea23aef1
Matthew Kennedy <matthewkennedy@outlook.com>
2020-03-10 12:18:29 -0700

    Control stepper with dual H-bridges (#1121)
    * divorce stepper from hw
    
    * oops
    
    * ptr not ref
    
    * hbridge stepper
    
    * typo
    
    * hook up twin h
    
    * hook up hardware
    
    * use config field
    
    * hook up motor repo
    
    * guard tests
    
    * whoops
    
    * ok fix for real
    
    * ok that's the right thing
    
    * do full stepping
    
    Co-authored-by: Matthew Kennedy <matthew@mck-mbp-15.stagpad.local>
    Co-authored-by: Matthew Kennedy <matthew@mck-mbp-15.local>

---
+++ firmware/hw_layer/stepper_dual_hbridge.cpp
@@ -0,0 +1,53 @@
+
+#include "efifeatures.h"
+
+#if !EFI_UNIT_TEST
+
+#include "stepper.h"
+#include "dc_motor.h"
+
+static const int8_t phaseA[] = 
+{
+    1,
+    1,
+    -1,
+    -1
+};
+
+static const int8_t phaseB[] = 
+{
+    -1,
+    1,
+    1,
+    -1
+};
+
+void DualHBridgeStepper::initialize(DcMotor* motorPhaseA, DcMotor* motorPhaseB, float reactionTime)
+{
+    setReactionTime(reactionTime);
+
+    m_motorPhaseA = motorPhaseA;
+    m_motorPhaseB = motorPhaseB;
+}
+
+void DualHBridgeStepper::step(bool positive) {
+	// Check that we've been initialized
+	if (!m_motorPhaseA || !m_motorPhaseB) {
+		return;
+	}
+
+    // step phase, wrapping
+    if (positive) {
+        m_phase = (m_phase + 1) & 0x03;
+    } else {
+        m_phase = (m_phase - 1) & 0x03;
+    }
+
+    // Set phases according to the table
+    m_motorPhaseA->set(phaseA[m_phase]);
+    m_motorPhaseB->set(phaseB[m_phase]);
+
+    pause();
+}
+
+#endif
