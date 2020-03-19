9f5eece62ded25327d63fa9e5824c912b7b42d79
Matthew Kennedy <matthewkennedy@outlook.com>
2020-03-19 11:01:07 -0700

    split out can tx logic (#1197)
    * move to can folder
    
    * split out can rx
    
    * only print in debug mode
    
    * guard on feature flag
    
    * split out tx
    
    * headers
    
    * headers
    
    Co-authored-by: Matthew Kennedy <makenne@microsoft.com>

---
+++ firmware/controllers/can/can_dash.h
@@ -0,0 +1,13 @@
+/**
+ * @file	can_dash.h
+ *
+ * @date Mar 19, 2020
+ * @author Matthew Kennedy, (c) 2020
+ */
+
+#pragma once
+
+void canDashboardBMW();
+void canDashboardFiat();
+void canDashboardVAG();
+void canMazdaRX8();
