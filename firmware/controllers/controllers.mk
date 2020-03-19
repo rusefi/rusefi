f2994d6889f1c0b4023a9b023750efd0be5b9101
Matthew Kennedy <matthewkennedy@outlook.com>
2020-03-19 05:43:37 -0700

    split out can rx logic (#1196)
    * move to can folder
    
    * split out can rx
    
    * only print in debug mode
    
    * guard on feature flag
    
    * headers
    
    Co-authored-by: Matthew Kennedy <makenne@microsoft.com>

--- firmware/controllers/controllers.mk
+++ firmware/controllers/controllers.mk
@@ -35,6 +35,7 @@ CONTROLLERS_SRC_CPP = \
 	$(CONTROLLERS_DIR)/flash_main.cpp \
 	$(CONTROLLERS_DIR)/injector_central.cpp \
 	$(CONTROLLERS_DIR)/can/obd2.cpp \
+	$(CONTROLLERS_DIR)/can/can_rx.cpp \
  	$(CONTROLLERS_DIR)/engine_controller.cpp \
 	$(CONTROLLERS_DIR)/persistent_store.cpp \
 	
