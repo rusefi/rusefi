ef28f394a945defdc2bf87cc6b442751b90ee6e7
rusefi <rusefillc@gmail.com>
2020-03-20 09:53:45 -0400

    At least read all TPS inputs

--- firmware/controllers/engine_controller.cpp
+++ firmware/controllers/engine_controller.cpp
@@ -849,6 +849,6 @@ int getRusEfiVersion(void) {
 	if (initBootloader() != 0)
 		return 123;
 #endif /* EFI_BOOTLOADER_INCLUDE_CODE */
-	return 201200319;
+	return 201200320;
 }
 #endif /* EFI_UNIT_TEST */
