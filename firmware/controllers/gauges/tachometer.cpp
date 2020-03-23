abfc3c746f90677ffb269c6e2b5dcebdfde08f72
shadowm60 <shadowm60@gmail.com>
2020-03-23 23:48:58 +0200

    starting to make some progress...still fails after a few seconds

--- firmware/controllers/gauges/tachometer.cpp
+++ firmware/controllers/gauges/tachometer.cpp
@@ -16,25 +16,24 @@
 
 EXTERN_ENGINE;
 #define TEST_BENCH
-#ifdef TEST_BENCH
-static uint8_t loop = 0;
-#define setDbgHigh   enginePins.dizzyOutput.setValue(true)
-#define setDbgLow    enginePins.dizzyOutput.setValue(false)
-#endif
 
 
 struct tach_ctx {
 	OutputPin *Pin;
 	bool State;
 	int action;
+#ifdef EFI_UNIT_TEST
+	Engine *engine;
+	engine_configuration_s* engineConfiguration;
+	persistent_config_s* config;
+#endif	
 };
 
 static scheduling_s schHigh;
 static scheduling_s schLow;
 static tach_ctx ctx_high;
 static tach_ctx ctx_low;
-static angle_t angleHigh;
-static angle_t angleLow;
+static angle_t period;
 static int maxActions;
 
 static int multiplierFromEngineType(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
@@ -57,15 +56,24 @@ static int multiplierFromEngineType(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 
 static void setTach(void *param) {
 	auto ctx = reinterpret_cast<tach_ctx *>(param);
-	uint32_t timeStamp = getTimeNowUs();
+	uint32_t timeStamp = getTimeNowNt();
 	ctx->Pin->setValue(ctx->State);
 	ctx->action++;
 	if (ctx->action < maxActions){
 		if(ctx->State == true)
 		{
-			scheduleByAngle(&schHigh, timeStamp, angleHigh, {&setTach, &ctx} PASS_ENGINE_PARAMETER_SUFFIX);	
+#ifdef EFI_UNIT_TEST
+			scheduleByAngle(&schHigh, timeStamp, period, {&setTach, &ctx_high}, ctx_high.engine, ctx_high.engineConfiguration, ctx_high.config);
+#else			
+			scheduleByAngle(&schHigh, timeStamp, period, {&setTach, &ctx_high} PASS_ENGINE_PARAMETER_SUFFIX);	
+#endif			
 		}else{
-			scheduleByAngle(&schLow, timeStamp, angleLow, {&setTach, &ctx} PASS_ENGINE_PARAMETER_SUFFIX);
+#ifdef EFI_UNIT_TEST
+			scheduleByAngle(&schLow, timeStamp, period, {&setTach, &ctx_low}, ctx_low.engine, ctx_low.engineConfiguration, ctx_low.config);
+#else			
+
+			scheduleByAngle(&schLow, timeStamp, period, {&setTach, &ctx_low} PASS_ENGINE_PARAMETER_SUFFIX);
+#endif
 		}
 	}
 }
@@ -77,18 +85,7 @@ static void tachSignalCallback(trigger_event_e ckpSignalType,
 		return;
 	}
 
-#ifdef TEST_BENCH
-	if (loop == 0)
-	{
-		setDbgHigh;
-		loop++;
-	}
-	else
-	{
-		setDbgLow;
-		loop = 0;
-	}
-#endif	
+	angle_t angleDuty;
 
 #if EFI_UNIT_TEST
 	printf("tachSignalCallback(%d %d)\n", ckpSignalType, index);
@@ -103,8 +100,12 @@ static void tachSignalCallback(trigger_event_e ckpSignalType,
 		warning(CUSTOM_ERR_6709,"Check Tachometer Pulse per Rev!");
 		return;
 	}
+
+	ctx_high.action = 0;
+	ctx_low.action = 0;
+	
 	// What is the angle per tach output period?
-	const angle_t period = 360.0 / periods;
+	period = 360.0 / periods;
 
 	float duty;
 
@@ -123,18 +124,16 @@ static void tachSignalCallback(trigger_event_e ckpSignalType,
 	duty = maxF(0.1f, minF(duty, 0.9f));
 
 	// Use duty to compute the angle widths of high/low periods
-	angleHigh = period * duty;
-	angleLow = period - angleHigh;
+	angleDuty = period * duty;
 
 	// cam trigger vs. crank trigger vs. 2 stroke trigger shapes fire this callback either once
 	// per rev, or once every other rev.  Adjust appropriately.
 	maxActions = periods * multiplierFromEngineType(PASS_ENGINE_PARAMETER_SIGNATURE);
 
 	//schedule the first two events
-	ctx_high.action = 0;
 	scheduleByAngle(&schHigh, edgeTimestamp, 0.0, {&setTach, &ctx_high} PASS_ENGINE_PARAMETER_SUFFIX);
-	ctx_low.action = 0;
-	scheduleByAngle(&schLow, edgeTimestamp, angleLow, {&setTach, &ctx_low} PASS_ENGINE_PARAMETER_SUFFIX);
+
+	scheduleByAngle(&schLow, edgeTimestamp, angleDuty, {&setTach, &ctx_low} PASS_ENGINE_PARAMETER_SUFFIX);
 }
 
 void initTachometer(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
@@ -144,19 +143,28 @@ void initTachometer(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 	}
 
 	enginePins.tachOut.initPin("analog tach output", CONFIG(tachOutputPin), &CONFIG(tachOutputPinMode));
-#ifdef TEST_BENCH
-    enginePins.dizzyOutput.initPin("dizzy tach",  GPIOE_8,&engineConfiguration->dizzySparkOutputPinMode);
-#endif
+
 	ctx_high.Pin = &enginePins.tachOut;
 	ctx_high.State = true;
+#ifdef EFI_UNIT_TEST
+	ctx_high.engine = engine;
+	ctx_high.engineConfiguration = engineConfiguration;
+	ctx_high.config = config;
+#endif
 
 	ctx_low.Pin = &enginePins.tachOut;
 	ctx_low.State = false;
+#ifdef EFI_UNIT_TEST
+	ctx_low.engine = engine;
+	ctx_low.engineConfiguration = engineConfiguration;
+	ctx_low.config = config;
+#endif
 
+
+#ifdef EFI_UNIT_TEST
+	printf("initTachometer\n");
+#endif
 #if EFI_SHAFT_POSITION_INPUT
-	#if EFI_UNIT_TEST
-		printf("registerCkpListener: tach\n");
-	#endif
 	addTriggerEventListener(tachSignalCallback, "tach", engine);
 #endif /* EFI_SHAFT_POSITION_INPUT */
 }
--- unit_tests/tests/test_tacho.cpp
+++ unit_tests/tests/test_tacho.cpp
@@ -2,6 +2,7 @@
 
 extern WarningCodeState unitTestWarningCodeState;
 
+#ifdef INIT_IMPL
 typedef struct
 {
     uint8_t PulsePerRev;
@@ -259,3 +260,90 @@ TEST(tachometer, testGenericAbusePulsePerRev)
 
     doExeuteTest(tconfig);
 }
+#else
+
+
+TEST(tachometer, testPulsePerRev) {
+    // This engine has a tach pin set - we need that
+    WITH_ENGINE_TEST_HELPER(BMW_E34);
+
+    // We don't actually care about ign/inj at all, just tach
+    engineConfiguration->isInjectionEnabled = false;
+    engineConfiguration->isIgnitionEnabled = false;
+
+    // Configure tach pulse count
+    // 5 PPR, 25% duty
+    engineConfiguration->tachPulsePerRev = 4;
+    engineConfiguration->tachPulseDuractionMs = 0.5f;
+    engineConfiguration->tachPulseDurationAsDutyCycle = true;
+    engineConfiguration->tachPulseTriggerIndex = 0;
+
+    // Set predictable trigger settings
+    engineConfiguration->trigger.customTotalToothCount = 8;
+    engineConfiguration->trigger.customSkippedToothCount = 0;
+    engineConfiguration->useOnlyRisingEdgeForTrigger = false;
+    engineConfiguration->ambiguousOperationMode = FOUR_STROKE_CAM_SENSOR;
+	eth.applyTriggerWaveform();
+
+    // get the engine running - 6 revolutions
+    eth.fireTriggerEvents(48);
+
+    // ensure engine speed and position
+	ASSERT_EQ(1500,  GET_RPM()) << "RPM";
+	ASSERT_EQ(15,  engine->triggerCentral.triggerState.getCurrentIndex()) << "index #1";
+    ASSERT_EQ(engine->triggerCentral.triggerState.shaft_is_synchronized, true);
+
+    // Clean the slate
+	eth.clearQueue();
+
+	eth.moveTimeForwardMs(5 /*ms*/);
+
+    // This will schedule tach events!
+    eth.firePrimaryTriggerRise();
+    ASSERT_EQ(1500, eth.engine.rpmCalculator.rpmValue) << "RPM";
+
+    // Check that exactly 16 events got scheduled
+    std::cerr << "Scheduled events: " << engine->executor.size() << "\n" << std::endl;
+    ASSERT_EQ(engine->executor.size(), 2);
+    
+    //move to 11ms
+    eth.moveTimeForwardMs(6 /*ms*/);
+    eth.fireTriggerEventsWithDuration(5);
+    // Check that exactly 16 events got scheduled
+    std::cerr << "Scheduled events: " << engine->executor.size() << std::endl;
+    ASSERT_EQ(engine->executor.size(), 2);
+
+#ifdef ALL_TEST_bla
+    // Now we verify that the correct stuff got scheduled
+    float enginePeriod = 40000; // us
+    float tachPeriod = enginePeriod / 4;
+    float tachHighTime = tachPeriod * 0.25f;
+
+    // Build the times we expect the edges to have occured
+    float events[16];
+    for (int i = 0; i < 8; i++) {
+        auto periodStart = i * tachPeriod;
+        events[2 * i] = periodStart;
+        events[2 * i + 1] = periodStart + tachHighTime;
+    }
+
+    // Check that exactly 16 events got scheduled
+    std::cerr << "Scheduled events: " << engine->executor.size() << std::endl;
+    ASSERT_EQ(engine->executor.size(), 16);
+
+    auto start = eth.getTimeNowUs();
+
+    // Check that everything is in the right spot
+    for (int i = 0; i < 16; i++)
+    {
+        auto s = engine->executor.getForUnitTest(i);
+        ASSERT_NE(s, nullptr);
+
+        EXPECT_NEAR(start + events[i], s->momentX, 1);
+    }
+#endif
+    // Clean the slate
+	eth.clearQueue();
+}
+
+#endif
