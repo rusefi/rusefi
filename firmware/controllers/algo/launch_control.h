205fdcee6658b2730afb4a71173dc602edd7c182
rusefi <rusefillc@gmail.com>
2020-03-23 20:44:34 -0400

    launch control #203

---
+++ firmware/controllers/algo/launch_control.h
@@ -0,0 +1,12 @@
+/*
+ * @file launch_control.h
+ *
+ * @date Mar 23, 2020
+ * @author Andrey Belomutskiy, (c) 2012-2020
+ */
+
+#pragma once
+
+#include "engine.h"
+
+void initLaunchControl(Logging *sharedLogger DECLARE_ENGINE_PARAMETER_SUFFIX);
