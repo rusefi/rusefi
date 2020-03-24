77c69a54bdead4675b83d7a1280a2d48e567592a
shadowm60 <shadowm60@gmail.com>
2020-03-24 23:46:00 +0200

    Rework with SW PWM

--- firmware/controllers/gauges/tachometer.cpp
+++ firmware/controllers/gauges/tachometer.cpp
@@ -12,71 +12,23 @@
 
 #include "tachometer.h"
 #include "trigger_central.h"
-
+#include "pwm_generator.h"
 
 EXTERN_ENGINE;
-#define TEST_BENCH
 
 
-struct tach_ctx {
-	OutputPin *Pin;
-	bool State;
-	int action;
-#ifdef EFI_UNIT_TEST
-	Engine *engine;
-	engine_configuration_s* engineConfiguration;
-	persistent_config_s* config;
-#endif	
-};
+static SimplePwm tachControl("tach");
+static bool isTachEnabled;
 
-static scheduling_s schHigh;
-static scheduling_s schLow;
-static tach_ctx ctx_high;
-static tach_ctx ctx_low;
-static angle_t period;
-static int maxActions;
+static void applyTachoPinState(int stateIndex, PwmConfig *state) /* pwm_gen_callback */ {
+	efiAssertVoid(CUSTOM_ERR_6643, stateIndex < PWM_PHASE_MAX_COUNT, "invalid stateIndex");
+	efiAssertVoid(CUSTOM_ERR_PWM_CALLBACK_ASSERT, state->multiChannelStateSequence.waveCount == 1, "invalid idle waveCount");
+	OutputPin *output = state->outputPins[0];
+	int value = state->multiChannelStateSequence.getChannelState(/*channelIndex*/0, stateIndex);
 
-static int multiplierFromEngineType(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
-	switch (engineConfiguration->ambiguousOperationMode) {
-	// These modes have index 0 once per rev
-	case TWO_STROKE:
-	case FOUR_STROKE_CRANK_SENSOR:
-		return 1;
-	// This mode has index 0 once every other rev (once per cam rev)
-	case FOUR_STROKE_CAM_SENSOR:
-		return 2;
-	case FOUR_STROKE_SYMMETRICAL_CRANK_SENSOR:
-		// is this correct? or should it be 0.5?
-		return 1;
-	default:
-		//warning(0, "Unknown ");
-		return 1;
-	}
-}
-
-static void setTach(void *param) {
-	auto ctx = reinterpret_cast<tach_ctx *>(param);
-	uint32_t timeStamp = getTimeNowNt();
-	ctx->Pin->setValue(ctx->State);
-	ctx->action++;
-	if (ctx->action < maxActions){
-		if(ctx->State == true)
-		{
-#ifdef EFI_UNIT_TEST
-			scheduleByAngle(&schHigh, timeStamp, period, {&setTach, &ctx_high}, ctx_high.engine, ctx_high.engineConfiguration, ctx_high.config);
-#else			
-			scheduleByAngle(&schHigh, timeStamp, period, {&setTach, &ctx_high} PASS_ENGINE_PARAMETER_SUFFIX);	
-#endif			
-		}else{
-#ifdef EFI_UNIT_TEST
-			scheduleByAngle(&schLow, timeStamp, period, {&setTach, &ctx_low}, ctx_low.engine, ctx_low.engineConfiguration, ctx_low.config);
-#else			
-
-			scheduleByAngle(&schLow, timeStamp, period, {&setTach, &ctx_low} PASS_ENGINE_PARAMETER_SUFFIX);
-#endif
-		}
-	}
-}
+	if (!value || isTachEnabled)
+		output->setValue(value);
+}        
 
 static void tachSignalCallback(trigger_event_e ckpSignalType,
 		uint32_t index, efitick_t edgeTimestamp DECLARE_ENGINE_PARAMETER_SUFFIX) {
@@ -85,12 +37,11 @@ static void tachSignalCallback(trigger_event_e ckpSignalType,
 		return;
 	}
 
-	angle_t angleDuty;
-
 #if EFI_UNIT_TEST
 	printf("tachSignalCallback(%d %d)\n", ckpSignalType, index);
 #else
 	UNUSED(ckpSignalType);
+	UNUSED(edgeTimestamp);
 #endif
 
 	// How many tach pulse periods do we have?
@@ -101,39 +52,32 @@ static void tachSignalCallback(trigger_event_e ckpSignalType,
 		return;
 	}
 
-	ctx_high.action = 0;
-	ctx_low.action = 0;
-	
 	// What is the angle per tach output period?
-	period = 360.0 / periods;
-
 	float duty;
-
+	float cycleTimeMs = 60000.0 / GET_RPM();
+	float periodTimeMs = cycleTimeMs / periods;
+	float tachFreq = 1000.0 / periodTimeMs;
+	
 	if (engineConfiguration->tachPulseDurationAsDutyCycle) {
 		// Simple case - duty explicitly set
 		duty = engineConfiguration->tachPulseDuractionMs;
 	} else {
 		// Constant high-time mode - compute the correct duty cycle
-		float cycleTimeMs = 60000.0 / GET_RPM();
-		float periodTimeMs = cycleTimeMs / periods;
-
 		duty = engineConfiguration->tachPulseDuractionMs / periodTimeMs;
 	}
 
 	// limit to 10..90% duty
 	duty = maxF(0.1f, minF(duty, 0.9f));
 
-	// Use duty to compute the angle widths of high/low periods
-	angleDuty = period * duty;
+	tachControl.setSimplePwmDutyCycle(duty);
+	if (tachFreq >= 1.0){
+		tachControl.setFrequency(tachFreq);
+		isTachEnabled = true;
+	}else{
+		isTachEnabled = false;
+	}
+	
 
-	// cam trigger vs. crank trigger vs. 2 stroke trigger shapes fire this callback either once
-	// per rev, or once every other rev.  Adjust appropriately.
-	maxActions = periods * multiplierFromEngineType(PASS_ENGINE_PARAMETER_SIGNATURE);
-
-	//schedule the first two events
-	scheduleByAngle(&schHigh, edgeTimestamp, 0.0, {&setTach, &ctx_high} PASS_ENGINE_PARAMETER_SUFFIX);
-
-	scheduleByAngle(&schLow, edgeTimestamp, angleDuty, {&setTach, &ctx_low} PASS_ENGINE_PARAMETER_SUFFIX);
 }
 
 void initTachometer(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
@@ -142,31 +86,22 @@ void initTachometer(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 		return;
 	}
 
-	enginePins.tachOut.initPin("analog tach output", CONFIG(tachOutputPin), &CONFIG(tachOutputPinMode));
-
-	ctx_high.Pin = &enginePins.tachOut;
-	ctx_high.State = true;
-#ifdef EFI_UNIT_TEST
-	ctx_high.engine = engine;
-	ctx_high.engineConfiguration = engineConfiguration;
-	ctx_high.config = config;
-#endif
-
-	ctx_low.Pin = &enginePins.tachOut;
-	ctx_low.State = false;
-#ifdef EFI_UNIT_TEST
-	ctx_low.engine = engine;
-	ctx_low.engineConfiguration = engineConfiguration;
-	ctx_low.config = config;
-#endif
-
-
+	isTachEnabled = false;
 #ifdef EFI_UNIT_TEST
 	printf("initTachometer\n");
 #endif
+
 #if EFI_SHAFT_POSITION_INPUT
 	addTriggerEventListener(tachSignalCallback, "tach", engine);
 #endif /* EFI_SHAFT_POSITION_INPUT */
+
+startSimplePwmExt(&tachControl,
+				"analog tach output",
+				&engine->executor,
+				CONFIG(tachOutputPin),
+				&enginePins.tachOut,
+				1.0, 0.1, (pwm_gen_callback*)applyTachoPinState);
+
 }
 
 
