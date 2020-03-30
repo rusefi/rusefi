636eff83405b25440dc6614e2455b2d1f9a741f7
Matthew Kennedy <makenne@microsoft.com>
2020-03-30 12:11:36 -0700

    const

--- firmware/controllers/sensors/sensor_info_printing.cpp
+++ firmware/controllers/sensors/sensor_info_printing.cpp
@@ -3,11 +3,11 @@
 #include "efilib.h"
 #include "loggingcentral.h"
 
-void ProxySensor::showInfo(Logging* logger, const char* sensorName) {
+void ProxySensor::showInfo(Logging* logger, const char* sensorName) const {
 	scheduleMsg(logger, "Sensor \"%s\" proxied from sensor \"%s\"", sensorName, getSensorName(m_proxiedSensor));
 }
 
-void FunctionalSensor::showInfo(Logging* logger, const char* sensorName) {
+void FunctionalSensor::showInfo(Logging* logger, const char* sensorName) const {
 	const auto [valid, value] = get();
 	scheduleMsg(logger, "Sensor \"%s\": Raw value: %f Valid: %d Converted value %f", sensorName, m_rawValue, valid, value);
 }
