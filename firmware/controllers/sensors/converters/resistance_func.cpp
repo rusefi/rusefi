b1eb48d1a7f29b05c778edd5d20825fd71fcbfb3
Matthew Kennedy <matthewkennedy@outlook.com>
2020-04-07 00:00:58 -0700

    raise high voltage failure threshold

--- firmware/controllers/sensors/converters/resistance_func.cpp
+++ firmware/controllers/sensors/converters/resistance_func.cpp
@@ -16,8 +16,8 @@ SensorResult ResistanceFunc::convert(float raw) const {
 		return {false, 0.0f};
 	}
 
-	// If the voltage is very high (95% VCC), the sensor is open circuit.
-	if (raw > (m_supplyVoltage * 0.95f)) {
+	// If the voltage is very high (98% VCC), the sensor is open circuit.
+	if (raw > (m_supplyVoltage * 0.98f)) {
 		return {false, 1e6};
 	}
 
