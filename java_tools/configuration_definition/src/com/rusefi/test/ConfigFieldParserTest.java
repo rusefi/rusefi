7cc92b3b8c414f8c03de37ed2e5a6ba763fc999a
rusefi <rusefillc@gmail.com>
2020-04-12 23:32:22 -0400

    processor progress

--- java_tools/configuration_definition/src/com/rusefi/test/ConfigFieldParserTest.java
+++ java_tools/configuration_definition/src/com/rusefi/test/ConfigFieldParserTest.java
@@ -57,7 +57,7 @@ public class ConfigFieldParserTest {
         String test = "struct pid_s\n" +
                 "#define ERROR_BUFFER_SIZE 120\n" +
                 "#define ERROR_BUFFER_COUNT 120\n" +
-                "#define RESULT 120*@@ERROR_BUFFER_COUNT@@\n" +
+                "#define RESULT @@ERROR_BUFFER_SIZE@@*@@ERROR_BUFFER_COUNT@@\n" +
                 "\tint16_t periodMs;PID dTime;\"ms\",      1,      0,       0, 3000,      0\n" +
                 "end_struct\n" +
                 "";
@@ -68,7 +68,7 @@ public class ConfigFieldParserTest {
 
         assertEquals("#define ERROR_BUFFER_COUNT 120\n" +
                 "#define ERROR_BUFFER_SIZE 120\n" +
-                "#define RESULT 120*@@ERROR_BUFFER_COUNT@@\n", VariableRegistry.INSTANCE.getDefinesSection());
+                "#define RESULT 120*120\n", VariableRegistry.INSTANCE.getDefinesSection());
     }
 
     @Test
