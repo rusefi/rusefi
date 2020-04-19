c1a4bd857b451fee5194af7585a7949dd698f839
rusefi <rusefillc@gmail.com>
2020-04-19 14:23:01 -0400

    TT_HONDA_K_12_1

--- firmware/controllers/trigger/decoders/trigger_renix.cpp
+++ firmware/controllers/trigger/decoders/trigger_renix.cpp
@@ -15,7 +15,8 @@ void initializeRenix44_2_2(TriggerWaveform *s) {
 	s->initialize(FOUR_STROKE_SYMMETRICAL_CRANK_SENSOR);
 
 	// 44-2-2 is symmetrical so we only need to define one half
-	float tooth = 180.0 / 22; // hint: tooth = 8.181818 degrees
+	int count = 22;
+	float tooth = s->getCycleDuration() / count; // hint: tooth = 8.181818 degrees
 
 	float currentAngle = 0;
 	for (int i = 0;i < 20;i++) {
@@ -27,6 +28,6 @@ void initializeRenix44_2_2(TriggerWaveform *s) {
 	s->addEventAngle(currentAngle + tooth, T_PRIMARY, TV_RISE);
 
 	// float math error accumulates at this point so we have to spell out 180
-	s->addEventAngle(180, T_PRIMARY, TV_FALL);
+	s->addEventAngle(s->getCycleDuration(), T_PRIMARY, TV_FALL);
 
 }
