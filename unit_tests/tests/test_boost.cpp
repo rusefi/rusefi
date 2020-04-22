e61526a1ad6bbe0c47c650d22a3adc64cd339f1e
Matthew Kennedy <matthewkennedy@outlook.com>
2020-04-22 11:32:06 -0700

    tests

--- unit_tests/tests/test_boost.cpp
+++ unit_tests/tests/test_boost.cpp
@@ -10,8 +10,9 @@ using ::testing::NiceMock;
 TEST(BoostControl, Setpoint) {
 	MockVp3d targetMap;
 
+	// Just pass TPS input to output
 	EXPECT_CALL(targetMap, getValue(_, _))
-		.WillRepeatedly(Return(125.0f));
+		.WillRepeatedly([](float xRpm, float y) { return y; });
 
 	WITH_ENGINE_TEST_HELPER(TEST_ENGINE);
 
@@ -24,7 +25,12 @@ TEST(BoostControl, Setpoint) {
 	// Now init with mock target map
 	bc.init(nullptr, nullptr, &targetMap, nullptr);
 
-	EXPECT_FLOAT_EQ(bc.getSetpoint().value_or(-1), 125.0f);
+	// Should still return unxepected since TPS is invalid
+	EXPECT_EQ(bc.getSetpoint(), unexpected);
+
+	// Configure TPS, should get passthru of tps value
+	Sensor::setMockValue(SensorType::DriverThrottleIntent, 35.0f);
+	EXPECT_FLOAT_EQ(bc.getSetpoint().value_or(-1), 35.0f);
 }
 
 TEST(BoostControl, ObservePlant) {
@@ -33,17 +39,13 @@ TEST(BoostControl, ObservePlant) {
 	BoostController bc;
 	INJECT_ENGINE_REFERENCE(&bc);
 
+	engine->mockMapValue = NAN;
 	// Check that invalid MAP returns unexpected
 	EXPECT_EQ(bc.observePlant(), unexpected);
 
-	auto sensorCfg = engineConfiguration->map.sensor;
-	sensorCfg.highValue = 300;
-	sensorCfg.lowValue = 0;
-	engineConfiguration->mapLowValueVoltage = 0;
-	engineConfiguration->mapHighValueVoltage = 5;
+	// Test valid MAP value
+	engine->mockMapValue = 150.0f;
 
-	// Set the MAP, check that we get it back
-	setMockMapVoltage(2.5f PASS_ENGINE_PARAMETER_SUFFIX);
 	EXPECT_FLOAT_EQ(bc.observePlant().value_or(0), 150.0f);
 }
 
