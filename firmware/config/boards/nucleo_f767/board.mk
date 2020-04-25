b6780ab9d04fca2f19ee22807537ae530db6c3f4
rusefi <rusefillc@gmail.com>
2020-04-25 12:07:57 -0400

    wow maybe build time magic?! by DronoGus

--- firmware/config/boards/nucleo_f767/board.mk
+++ firmware/config/boards/nucleo_f767/board.mk
@@ -4,6 +4,7 @@ BOARDSRC_CPP = $(PROJECT_DIR)/config/boards/nucleo_f767/board_configuration.cpp
 
 # Required include directories
 BOARDINC = $(PROJECT_DIR)/config/boards/nucleo_f767 $(PROJECT_DIR)/config/stm32f7ems
+CONFDIR=config/stm32f7ems
 
 LDSCRIPT= $(PROJECT_DIR)/config/boards/nucleo_f767/STM32F76xxI.ld
 
