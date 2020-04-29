7686ee271cb8d04c45ec7d63ab47c25682440892
rusefi <rusefillc@gmail.com>
2020-04-29 18:46:51 -0400

    https://github.com/rusefi/rusefi_documentation/issues/37

--- firmware/hw_layer/cdm_ion_sense.cpp
+++ firmware/hw_layer/cdm_ion_sense.cpp
@@ -3,7 +3,7 @@
  *
  * Saab Ion Sensing Module integration
  *
- * See https://github.com/rusefi/rusefi_documentation/tree/master/misc/Saab_Trionic_8_Combustion%20Detection%20Module_on_Mazda_Miata_running_rusEfi
+ * See https://github.com/rusefi/rusefi/wiki/Saab_Trionic_8_Combustion-Detection-Module_on_Mazda_Miata_running_rusEfi
  *
  *  Created on: Dec 31, 2018
  * @author Andrey Belomutskiy, (c) 2012-2020
