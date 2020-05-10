2db29c7824fddd1805a3c598615aa6ae72b1115d
rusefi <rusefillc@gmail.com>
2020-05-10 18:03:11 -0400

    34-2 3 cam Toyota wheel progress

--- firmware/config/engines/toyota_jzs147.cpp
+++ firmware/config/engines/toyota_jzs147.cpp
@@ -125,6 +125,8 @@ void setToyota_2jz_vics(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
 	strcpy(CONFIG(vehicleName), "VVT example");
 
 
+	engineConfiguration->debugMode = DBG_VVT;
+
 	// todo: these magic values would be hardcoded once we find out proper magic values
 	engineConfiguration->fsio_setting[14] = -90;
 	engineConfiguration->fsio_setting[15] = +90;
