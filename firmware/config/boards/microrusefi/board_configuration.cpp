92227cb4cbfd99bd35a056a89148aabb5cc3d36d
rusefi <rusefillc@gmail.com>
2020-05-13 23:29:52 -0400

    SPI3

--- firmware/config/boards/microrusefi/board_configuration.cpp
+++ firmware/config/boards/microrusefi/board_configuration.cpp
@@ -204,6 +204,7 @@ void setBoardConfigurationOverrides(void) {
 	engineConfiguration->spi3misoPin = GPIOC_11;
 	engineConfiguration->spi3sckPin = GPIOC_10;
 	engineConfiguration->sdCardCsPin = GPIOB_9;
+	CONFIG(is_enabled_spi_3) = true;
 //	engineConfiguration->isSdCardEnabled = true;
 
 
