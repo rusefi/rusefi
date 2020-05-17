9c63c421b63494e679373df4f3dd12077c6aac8f
Matthew Kennedy <matthewkennedy@outlook.com>
2020-05-16 19:03:27 -0700

    fix one test

--- unit_tests/tests/test_binary_log.cpp
+++ unit_tests/tests/test_binary_log.cpp
@@ -2,11 +2,13 @@
 
 #include <gmock/gmock.h>
 
+using ::testing::ElementsAre;
+
 TEST(BinaryLogField, FieldHeader) {
 	scaled_channel<int8_t, 10> channel;
 	LogField field(channel, "name", "units", 2);
 
-	char buffer[64];
+	char buffer[56];
 	memset(buffer, 0xAA, sizeof(buffer));
 
 	// Should write 55 bytes
@@ -14,27 +16,28 @@ TEST(BinaryLogField, FieldHeader) {
 
 	// Expect correctly written header
 	EXPECT_THAT(buffer, ElementsAre(
-		0,	// type: int8_t
+		1,	// type: int8_t
 		// name - 34 bytes, 0 padded
 		'n', 'a', 'm', 'e', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 		// units - 10 bytes, 0 padded
 		'u', 'n', 'i', 't', 's', 0, 0, 0, 0, 0,
 		// display style: float
 		0,
-		// Scale (float, grumble)
-		0, 0, 0, 0,
+		// Scale = 0.1 (float)
+		0x3d, 0xcc, 0xcc, 0xcd,
 		// Transform - we always use 0
 		0, 0, 0, 0,
 		// Digits - 2, as configured
 		2,
 
 		// After end should be 0xAA, not touched
-		0xAA, 0xAA, 0xAA
+		0xAA
 	));
 }
 
 TEST(BinaryLogField, Value) {
-	uint32_t testValue = 0x12345678;
+	scaled_channel<uint32_t, 1> testValue;
+	*reinterpret_cast<uint32_t*>(&testValue) = 0x12345678;
 	LogField lf(testValue, "test", "unit", 0);
 
 	char buffer[6];
@@ -43,5 +46,5 @@ TEST(BinaryLogField, Value) {
 	EXPECT_EQ(4, lf.writeData(buffer));
 
 	// Check that big endian data was written, and bytes after weren't touched
-	EXPECT_THAT(buffer, ElementsAre(0x78, 0x56, 0x34, 0x12, 0xAA, 0xAA));
+	EXPECT_THAT(buffer, ElementsAre(0x12, 0x34, 0x56, 0x78, 0xAA, 0xAA));
 }
