bd8dd3f7a7886a5d5fc2e14e233e21addae96bbc
rusefi <rusefillc@gmail.com>
2020-05-20 02:51:14 -0400

    MAYBE this would even work?

--- java_console/ui/src/com/rusefi/tune/xml/Msq.java
+++ java_console/ui/src/com/rusefi/tune/xml/Msq.java
@@ -1,17 +1,29 @@
 package com.rusefi.tune.xml;
 
+import com.opensr5.ConfigurationImage;
+import com.opensr5.ini.IniFileModel;
+import com.opensr5.ini.field.IniField;
+
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class Msq {
+    public static final String outputXmlFileName = "output.msq";
+
     private final Page page = new Page();
 
     private final VersionInfo versionInfo;
 
     public Msq() {
         versionInfo = new VersionInfo("rusEFI+2020");
+    }
+
+    public void loadConstant(IniFileModel ini, String key, ConfigurationImage image) {
+        IniField field = ini.allIniFields.get(key);
+        String value = field.getValue(image);
+        getPage().constant.add(new Constant(field.getName(), field.getUnits(), value));
     }
 
     @XmlAttribute
--- java_console/ui/src/com/rusefi/ui/OnlineTab.java
+++ java_console/ui/src/com/rusefi/ui/OnlineTab.java
@@ -1,11 +1,19 @@
 package com.rusefi.ui;
 
+import com.rusefi.io.LinkManager;
+import com.rusefi.tools.ConsoleTools;
+import com.rusefi.tools.online.Online;
+import com.rusefi.tune.xml.Msq;
+import com.rusefi.xml.XmlUtil;
 import org.putgemin.VerticalFlowLayout;
 
 import javax.swing.*;
+import javax.xml.bind.JAXBException;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
 
 import static com.rusefi.ui.storage.PersistentConfiguration.getConfig;
 
@@ -30,6 +38,23 @@ public class OnlineTab {
             }
         });
         content.add(save);
+
+
+        JButton upload = new JButton("Upload");
+        upload.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Msq tune = ConsoleTools.toMsq(LinkManager.connector.getBinaryProtocol().getControllerConfiguration());
+                try {
+                    XmlUtil.writeXml(tune, Msq.class, Msq.outputXmlFileName);
+                    // todo: network upload should not happen on UI thread
+                    Online.upload(new File(Msq.outputXmlFileName), textField.getText());
+                } catch (JAXBException | IOException ex) {
+                    throw new IllegalStateException(ex);
+                }
+            }
+        });
+        content.add(upload);
     }
 
     public JPanel getContent() {
