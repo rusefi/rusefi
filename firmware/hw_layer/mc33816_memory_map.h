b71403de7a157fbc8ae96b85728062ef0f502bfd
NOx-z <50815960+NOx-z@users.noreply.github.com>
2020-05-19 23:35:18 -0700

    Mc33 timings (#1453)
    * mc33 flash check
    
    * DI sanity checks for setting HV config
    
    * mc33 timings
    
    Co-authored-by: Christopher W. Anderson <gitstuff@pswitch.com>

--- firmware/hw_layer/mc33816_memory_map.h
+++ firmware/hw_layer/mc33816_memory_map.h
@@ -2,6 +2,12 @@
 typedef enum {
     Iboost = 0,
     Ipeak = 1,
+	Ihold = 2,
+	Tpeak_off = 3,
+	Tpeak_tot = 4,
+	Tbypass = 5,
+	Thold_off = 6,
+	Thold_tot = 7,
 	Vboost_high = 64,
 	Vboost_low = 65,
 	Isense4_high = 66,
