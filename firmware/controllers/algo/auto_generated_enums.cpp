2bed0326842d7fb01a6c11375526c880b7d550af
rusefi <rusefillc@gmail.com>
2020-05-20 08:55:38 -0400

    Clean-up analog channels in "unit" tests fix #1456

--- firmware/controllers/algo/auto_generated_enums.cpp
+++ firmware/controllers/algo/auto_generated_enums.cpp
@@ -77,10 +77,6 @@ case EFI_ADC_ERROR:
   return "EFI_ADC_ERROR";
 case EFI_ADC_NONE:
   return "EFI_ADC_NONE";
-#if EFI_UNIT_TEST
-case TEST_MAF_CHANNEL:
-  return "EFI_TEST";
-#endif /* EFI_UNIT_TEST */
   }
  return NULL;
 }
