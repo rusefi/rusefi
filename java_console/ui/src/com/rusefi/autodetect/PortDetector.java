ec2778fdcfcef8b1c0a3d9fc924a68f56cdcd639
rusefi <rusefillc@gmail.com>
2020-05-21 01:16:14 -0400

    rusEFI console start-up time improvements

--- java_console/ui/src/com/rusefi/autodetect/PortDetector.java
+++ java_console/ui/src/com/rusefi/autodetect/PortDetector.java
@@ -1,5 +1,6 @@
 package com.rusefi.autodetect;
 
+import com.rusefi.FileLog;
 import jssc.SerialPortList;
 import org.jetbrains.annotations.Nullable;
 
@@ -19,12 +20,12 @@ public class PortDetector {
      * Connect to all serial ports and find out which one respond first
      */
     public static String autoDetectSerial() {
-        String[] serialPorts = SerialPortList.getPortNames();
+        String[] serialPorts = getPortNames();
         if (serialPorts.length == 0) {
             System.err.println("No serial ports detected");
             return null;
         }
-        System.out.println("Trying " + Arrays.toString(serialPorts));
+        FileLog.MAIN.logLine("Trying " + Arrays.toString(serialPorts));
         List<Thread> serialFinder = new ArrayList<>();
         CountDownLatch portFound = new CountDownLatch(1);
         AtomicReference<String> result = new AtomicReference<>();
@@ -38,9 +39,18 @@ public class PortDetector {
         } catch (InterruptedException e) {
             throw new IllegalStateException(e);
         }
+//        FileLog.MAIN.logLine("Found " + result.get());
         for (Thread thread : serialFinder)
             thread.interrupt();
+//        FileLog.MAIN.logLine("Returning " + result.get());
         return result.get();
+    }
+
+    private static String[] getPortNames() {
+//        long now = System.currentTimeMillis();
+        String[] portNames = SerialPortList.getPortNames();
+//        FileLog.MAIN.logLine("Took " + (System.currentTimeMillis() - now));
+        return portNames;
     }
 
     @Nullable
