40cffec3a0fc92c6fcb834535220e969678afaed
kon <k@smo.la>
2020-05-21 22:45:07 -0700

    added most important messages; no more errors

--- firmware/controllers/can/can_dash.cpp
+++ firmware/controllers/can/can_dash.cpp
@@ -50,7 +50,6 @@ EXTERN_ENGINE;
 #define W202_ALIVE	0x210
 #define W202_STAT_3 0x310
 
-
 //BMW E90 DASH
 #define E90_ABS_COUNTER 0x0C0
 #define E90_SEATBELT_COUNTER 0x0D7
@@ -65,16 +64,14 @@ EXTERN_ENGINE;
 
 static uint8_t rpmcounter;
 static uint16_t e90msgcounter;
-static uint8_t seatbeltcnt;
 static uint8_t abscounter = 0xF0;
 static uint8_t brakecnt_1 = 0xF0, brakecnt_2 = 0xF0;
 
 constexpr uint8_t e90_temp_offset = 49;
 
-static uint8_t mph_a, mph_2a, mph_last, tmp_cnt, gear_cnt;
-static uint16_t mph_counter = 0xF000;
-static time_msecs_t mph_timer;
-static time_msecs_t mph_ctr;
+#define E90_LIGHTS 0x21A
+
+static uint8_t rpmcounter;
 
 void canDashboardBMW(void) {
 	//BMW Dashboard
@@ -309,7 +306,7 @@ void canDashboardBMWE90()
 			msg[7] = brakecnt_2;
 		}
 	}
-
+				
 	{ //ABS counter
 		if (e90msgcounter % 2) {
 			abscounter++;
