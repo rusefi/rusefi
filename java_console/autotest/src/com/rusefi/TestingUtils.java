221e842c4a30b74dbb2c1e3a38911554debcffd0
rusefi <rusefillc@gmail.com>
2020-05-24 11:47:47 -0400

    rusEFI console does not start under java 11

--- java_console/autotest/src/com/rusefi/TestingUtils.java
+++ java_console/autotest/src/com/rusefi/TestingUtils.java
@@ -7,7 +7,6 @@ import com.rusefi.waves.EngineChart;
 import com.rusefi.waves.EngineReport;
 import com.rusefi.waves.RevolutionLog;
 import com.rusefi.waves.EngineChartParser;
-import sun.misc.IOUtils;
 
 import java.util.Arrays;
 import java.util.List;
