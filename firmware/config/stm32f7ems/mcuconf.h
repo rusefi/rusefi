34bd40b1f0edf1d960435ee9110011c478a46a7a
Matthew Kennedy <matthewkennedy@outlook.com>
2020-05-23 17:39:20 -0700

    correct timers

--- firmware/config/stm32f7ems/mcuconf.h
+++ firmware/config/stm32f7ems/mcuconf.h
@@ -283,10 +283,10 @@
 #define STM32_PWM_USE_TIM1                  FALSE
 #define STM32_PWM_USE_TIM2                  FALSE
 #define STM32_PWM_USE_TIM3                  FALSE
-#define STM32_PWM_USE_TIM4                  FALSE
+#define STM32_PWM_USE_TIM4                  TRUE
 #define STM32_PWM_USE_TIM5                  FALSE
 #define STM32_PWM_USE_TIM8                  TRUE
-#define STM32_PWM_USE_TIM9                  TRUE
+#define STM32_PWM_USE_TIM9                  FALSE
 #define STM32_PWM_TIM1_IRQ_PRIORITY         7
 #define STM32_PWM_TIM2_IRQ_PRIORITY         7
 #define STM32_PWM_TIM3_IRQ_PRIORITY         7
