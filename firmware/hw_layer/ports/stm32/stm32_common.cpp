3a7c6f242bbcf0dc414d5bd1f4b7a0bd8ee52795
Matthew Kennedy <matthewkennedy@outlook.com>
2020-05-28 13:02:41 -0700

    Hardware pwm (#1283)
    * hard pwm
    
    * fix, comments
    
    * PC6-9 and PD12-15
    
    * PB6-9
    
    * dynamic frequency
    
    * f7 mcuconf
    
    * format
    
    * relocate to common, support variable frequency
    
    * error handling
    
    * oops
    
    * fix test build
    
    * noop kinetis
    
    * better guards
    
    * guard for bl
    
    * guards don't work if they have typos
    
    * more mapping
    
    * oops
    
    * don't use for slow pwm
    
    * enable pwm
    
    * guard properly
    
    * save a little memory
    
    * use less mem
    
    * correct timers
    
    * all your ram are belong to us
    
    * update limit & comment
    
    Co-authored-by: Matthew Kennedy <makenne@microsoft.com>

--- firmware/hw_layer/ports/stm32/stm32_common.cpp
+++ firmware/hw_layer/ports/stm32/stm32_common.cpp
@@ -8,12 +8,14 @@
 
 #include "global.h"
 #include "efi_gpio.h"
+#include "expected.h"
 
 #ifndef EFI_PIN_ADC9
 #define EFI_PIN_ADC9 GPIOB_1
 #endif /* EFI_PIN_ADC9 */
 
 #if EFI_PROD_CODE
+#include "mpu_util.h"
 #include "backup_ram.h"
 extern ioportid_t PORTS[];
 #if defined(STM32F4XX) || defined(STM32F7XX)
@@ -138,8 +140,168 @@ int getAdcChannelPin(adc_channel_e hwChannel) {
 
 #endif /* HAL_USE_ADC */
 
-
 #if EFI_PROD_CODE
+
+#if HAL_USE_PWM
+namespace {
+struct stm32_pwm_config {
+	PWMDriver* const Driver;
+	const uint8_t Channel;
+	const uint8_t AlternateFunc;
+};
+
+class stm32_hardware_pwm : public hardware_pwm {
+public:
+	bool hasInit() const {
+		return m_driver != nullptr;
+	}
+
+	// 2MHz, 16-bit timer gets us a usable frequency range of 31hz to 10khz
+	static constexpr uint32_t c_timerFrequency = 2000000;
+
+	void start(const char* msg, const stm32_pwm_config& config, float frequency, float duty) {
+		m_driver = config.Driver;
+		m_channel = config.Channel;
+
+		m_period = c_timerFrequency / frequency;
+
+		// These timers are only 16 bit - don't risk overflow
+		if (m_period > 0xFFF0) {
+			firmwareError(CUSTOM_OBD_LOW_FREQUENCY, "PWM Frequency too low %f hz on pin \"%s\"", frequency, msg);
+			return;
+		}
+
+		// If we have too few usable bits, we run out of resolution, so don't allow that either.
+		// 200 counts = 0.5% resolution
+		if (m_period < 200) {
+			firmwareError(CUSTOM_OBD_HIGH_FREQUENCY, "PWM Frequency too high % hz on pin \"%s\"", frequency, msg);
+			return;
+		}
+
+		const PWMConfig pwmcfg = {
+			c_timerFrequency,
+			m_period,
+			nullptr,
+			{
+				{PWM_OUTPUT_ACTIVE_HIGH, nullptr},
+				{PWM_OUTPUT_ACTIVE_HIGH, nullptr},
+				{PWM_OUTPUT_ACTIVE_HIGH, nullptr},
+				{PWM_OUTPUT_ACTIVE_HIGH, nullptr}
+			},
+			0,
+			0
+		};
+
+		// Start the timer running
+		pwmStart(m_driver, &pwmcfg);
+
+		// Set initial duty cycle
+		setDuty(duty);
+	}
+
+	void setDuty(float duty) override {
+		if (!m_driver) {
+			firmwareError(OBD_PCM_Processor_Fault, "Attempted to set duty on null hard PWM device");
+			return;
+		}
+
+		pwm_lld_enable_channel(m_driver, m_channel, getHighTime(duty));
+	}
+
+private:
+	PWMDriver* m_driver = nullptr;
+	uint8_t m_channel = 0;
+	uint32_t m_period = 0;
+
+	pwmcnt_t getHighTime(float duty) const {
+		return m_period * duty;
+	}
+};
+}
+
+static expected<stm32_pwm_config> getConfigForPin(brain_pin_e pin) {
+	switch (pin) {
+#if STM32_PWM_USE_TIM1
+	case GPIOA_8: return stm32_pwm_config{&PWMD1, 0, 1};
+	case GPIOA_9: return stm32_pwm_config{&PWMD1, 1, 1};
+	case GPIOA_10: return stm32_pwm_config{&PWMD1, 2, 1};
+	case GPIOA_11: return stm32_pwm_config{&PWMD1, 3, 1};
+
+	case GPIOE_9: return stm32_pwm_config{&PWMD1, 0, 1};
+	case GPIOE_11: return stm32_pwm_config{&PWMD1, 1, 1};
+	case GPIOE_13: return stm32_pwm_config{&PWMD1, 2, 1};
+	case GPIOE_14: return stm32_pwm_config{&PWMD1, 3, 1};
+#endif
+#if STM32_PWM_USE_TIM2
+	case GPIOA_15: return stm32_pwm_config{&PWMD2, 0, 1};
+	case GPIOB_3: return stm32_pwm_config{&PWMD2, 1, 1};
+	case GPIOB_10: return stm32_pwm_config{&PWMD2, 2, 1};
+	case GPIOB_11: return stm32_pwm_config{&PWMD2, 3, 1};
+#endif
+#if STM32_PWM_USE_TIM3
+	case GPIOB_4: return stm32_pwm_config{&PWMD3, 0, 2};
+	case GPIOB_5: return stm32_pwm_config{&PWMD3, 1, 2};
+#endif
+#if STM32_PWM_USE_TIM4
+	case GPIOB_6: return stm32_pwm_config{&PWMD4, 0, 2};
+	case GPIOB_7: return stm32_pwm_config{&PWMD4, 1, 2};
+	case GPIOB_8: return stm32_pwm_config{&PWMD4, 2, 2};
+	case GPIOB_9: return stm32_pwm_config{&PWMD4, 3, 2};
+
+	case GPIOD_12: return stm32_pwm_config{&PWMD4, 0, 2};
+	case GPIOD_13: return stm32_pwm_config{&PWMD4, 1, 2};
+	case GPIOD_14: return stm32_pwm_config{&PWMD4, 2, 2};
+	case GPIOD_15: return stm32_pwm_config{&PWMD4, 3, 2};
+#endif
+#if STM32_PWM_USE_TIM8
+	case GPIOC_6: return stm32_pwm_config{&PWMD8, 0, 3};
+	case GPIOC_7: return stm32_pwm_config{&PWMD8, 1, 3};
+	case GPIOC_8: return stm32_pwm_config{&PWMD8, 2, 3};
+	case GPIOC_9: return stm32_pwm_config{&PWMD8, 3, 3};
+#endif
+	default: return unexpected;
+	}
+};
+
+stm32_hardware_pwm pwms[5];
+
+stm32_hardware_pwm* getNextPwmDevice() {
+	for (size_t i = 0; i < efi::size(pwms); i++) {
+		if (!pwms[i].hasInit()) {
+			return &pwms[i];
+		}
+	}
+
+	firmwareError(OBD_PCM_Processor_Fault, "Run out of hardware PWM devices!");
+	return nullptr;
+}
+
+/*static*/ hardware_pwm* hardware_pwm::tryInitPin(const char* msg, brain_pin_e pin, float frequencyHz, float duty) {
+	// Hardware PWM can't do very slow PWM - the timer counter is only 16 bits, so at 2MHz counting, that's a minimum of 31hz.
+	if (frequencyHz < 50) {
+		return nullptr;
+	}
+
+	auto cfg = getConfigForPin(pin);
+
+	// This pin can't do hardware PWM
+	if (!cfg) {
+		return nullptr;
+	}
+
+	if (stm32_hardware_pwm* device = getNextPwmDevice()) {
+		device->start(msg, cfg.Value, frequencyHz, duty);
+
+		// Finally connect the timer to physical pin
+		efiSetPadMode(msg, pin, PAL_MODE_ALTERNATE(cfg.Value.AlternateFunc));
+
+		return device;
+	}
+
+	return nullptr;
+}
+#endif
+
 void jump_to_bootloader() {
 	// leave DFU breadcrumb which assmebly startup code would check, see [rusefi][DFU] section in assembly code
 	*((unsigned long *)0x2001FFF0) = 0xDEADBEEF; // End of RAM
