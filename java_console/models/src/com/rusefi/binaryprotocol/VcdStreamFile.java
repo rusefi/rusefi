562457f0992c9062efc3b2d01ed685a8d6ff63b4
rusefi <rusefillc@gmail.com>
2020-05-30 13:01:59 -0400

    sweet usability

---
+++ java_console/models/src/com/rusefi/binaryprotocol/VcdStreamFile.java
@@ -0,0 +1,83 @@
+package com.rusefi.binaryprotocol;
+
+import com.rusefi.FileLog;
+import com.rusefi.composite.CompositeEvent;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * https://en.wikipedia.org/wiki/Value_change_dump
+ *
+ * @see FileLog#WIKI_URL
+ */
+public class VcdStreamFile implements StreamFile {
+    private static final String TAG_PRIMARY = "t";
+    private static final String TAG_SECONDARY = "s";
+    private static final String TAG_TRG = "r";
+    private static final String TAG_SYNC = "y";
+
+    private FileWriter writer;
+
+    public static void writeHeader(Writer writer, Date date) throws IOException {
+        writer.write("$date\n");
+        writer.write("\t" + date + "\n");
+        writer.write("$end\n" +
+                "$version\n" +
+                "        1.0\n" +
+                "$end\n" +
+                "$timescale\n" +
+                "        1ps\n" +
+                "$end\n" +
+                "$scope module test $end\n" +
+                "$var wire 1 " + TAG_PRIMARY + " PRI_TRG $end\n" +
+                "$var wire 1 " + TAG_SECONDARY + " SEC_TRG $end\n" +
+                "$var wire 1 " + TAG_TRG + " TRG $end\n" +
+                "$var wire 1 " + TAG_SYNC + " SYNC $end\n" +
+                "$upscope $end\n" +
+                "$enddefinitions $end\n" +
+                "$dumpvars\n");
+    }
+
+    public static void appendEvents(List<CompositeEvent> events, Writer writer) throws IOException {
+        for (CompositeEvent event : events) {
+            writer.write("#" + event.getTimestamp() + "\n");
+            writer.write(event.isPrimaryTriggerAsInt() + TAG_PRIMARY + "\n");
+            writer.write(event.isSecondaryTriggerAsInt() + TAG_SECONDARY + "\n");
+            writer.write(event.isTrgAsInt() + TAG_TRG + "\n");
+            writer.write(event.isSyncAsInt() + TAG_SYNC + "\n");
+
+        }
+        writer.flush();
+    }
+
+
+    @Override
+    public void append(List<CompositeEvent> events) {
+        try {
+            if (writer == null) {
+                String fileName = FileLog.DIR + "rusEFI_trigger_log_" + FileLog.getDate() + ".vcd";
+                writer = new FileWriter(fileName);
+                writeHeader(writer, new Date());
+            }
+            appendEvents(events, writer);
+        } catch (IOException e) {
+            // ignoring this one
+        }
+    }
+
+    @Override
+    public void close() {
+        if (writer != null) {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                // ignoring this one
+            }
+        }
+        writer = null;
+    }
+}
--- java_console/models/src/com/rusefi/composite/CompositeParser.java
+++ java_console/models/src/com/rusefi/composite/CompositeParser.java
@@ -1,11 +1,11 @@
 package com.rusefi.composite;
 
 import com.opensr5.ini.field.EnumIniField;
+import com.rusefi.binaryprotocol.VcdStreamFile;
 import com.rusefi.config.generated.Fields;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.StringWriter;
 import java.io.Writer;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -14,11 +14,6 @@ import java.util.Date;
 import java.util.List;
 
 public class CompositeParser {
-
-    public static final String TAG_PRIMARY = "t";
-    public static final String TAG_SECONDARY = "s";
-    public static final String TAG_TRG = "r";
-    public static final String TAG_SYNC = "y";
 
     public static List<CompositeEvent> parse(byte[] response) {
         ByteBuffer byteBuffer = ByteBuffer.wrap(response);
@@ -46,40 +41,12 @@ public class CompositeParser {
         return events;
     }
 
-    /**
-     * https://en.wikipedia.org/wiki/Value_change_dump
-     */
     public static void writeVCD(List<CompositeEvent> events, Writer writer, Date date) throws IOException {
-        writer.write("$date\n");
-        writer.write("\t" + date + "\n");
-        writer.write("$end\n" +
-                "$version\n" +
-                "        1.0\n" +
-                "$end\n" +
-                "$timescale\n" +
-                "        1ps\n" +
-                "$end\n" +
-                "$scope module test $end\n" +
-                "$var wire 1 " + TAG_PRIMARY + " PRI_TRG $end\n" +
-                "$var wire 1 " + TAG_SECONDARY + " SEC_TRG $end\n" +
-                "$var wire 1 " + TAG_TRG + " TRG $end\n" +
-                "$var wire 1 " + TAG_SYNC + " SYNC $end\n" +
-                "$upscope $end\n" +
-                "$enddefinitions $end\n" +
-                "$dumpvars\n");
-
-        for (CompositeEvent event : events) {
-            writer.write("#" + event.getTimestamp() + "\n");
-            writer.write(event.isPrimaryTriggerAsInt() + TAG_PRIMARY + "\n");
-            writer.write(event.isSecondaryTriggerAsInt() + TAG_SECONDARY + "\n");
-            writer.write(event.isTrgAsInt() + TAG_TRG + "\n");
-            writer.write(event.isSyncAsInt() + TAG_SYNC + "\n");
-
-        }
-        writer.flush();
+        VcdStreamFile.writeHeader(writer, date);
+        VcdStreamFile.appendEvents(events, writer);
     }
 
     public static void writeVCD(List<CompositeEvent> events, FileWriter fileWriter) throws IOException {
-           writeVCD(events, fileWriter, new Date());
+        writeVCD(events, fileWriter, new Date());
     }
 }
