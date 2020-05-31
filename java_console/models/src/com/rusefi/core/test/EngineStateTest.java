42a69be638848ff721649d1e02ace9b46c9594cc
rusefi <rusefillc@gmail.com>
2020-05-31 13:18:47 -0400

    progress

--- java_console/models/src/com/rusefi/core/test/EngineStateTest.java
+++ java_console/models/src/com/rusefi/core/test/EngineStateTest.java
@@ -1,29 +1,16 @@
 package com.rusefi.core.test;
 
-import com.rusefi.core.SensorCentral;
 import com.rusefi.core.EngineState;
-import com.rusefi.io.LinkDecoder;
-import org.junit.Ignore;
 import org.junit.Test;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
-import static com.rusefi.core.EngineState.SEPARATOR;
-import static junit.framework.Assert.*;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * @author Andrey Belomutskiy
  *         12/26/12
  */
 public class EngineStateTest {
-    @Test
-    public void packUnpack() {
-        String a = "rpm,100,";
-        String packed = EngineState.packString(a);
-        assertEquals("line:8:rpm,100,", packed);
-        assertEquals(a, LinkDecoder.TEXT_PROTOCOL_DECODER.unpack(packed));
-    }
-
     @Test
     public void startsWithIgnoreCase() {
         assertTrue(EngineState.startWithIgnoreCase("HELLO", "he"));
