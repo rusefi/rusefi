5d7c1f836fe9f5c6c96bd19c011d9e8d80ae19b7
Andrey Gusakov <dron0gus@gmail.com>
2020-06-01 23:04:20 +0300

    board: subaru eg33: Autogenerated

---
+++ firmware/controllers/generated/signature_subaru_eg33_f7.h
@@ -0,0 +1,8 @@
+//
+// was generated automatically by rusEfi tool ConfigDefinition.jar based on config/boards/subaru_eg33/config/gen_config.sh integration/rusefi_config.txt
+//
+
+#define SIGNATURE_BOARD subaru_eg33_f7
+#define SIGNATURE_DATE 2020.09.08
+#define SIGNATURE_HASH 1078744669
+#define TS_SIGNATURE "rusEFI 2020.09.08.subaru_eg33_f7.1078744669"
