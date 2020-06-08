ae7124547e1e55eeaf6011980a00a5521e52c284
rusefi <rusefillc@gmail.com>
2020-06-08 01:08:25 -0400

    .logicdata files are a bit broken fix #1482

--- java_console/models/src/com/rusefi/rusEFIVersion.java
+++ java_console/models/src/com/rusefi/rusEFIVersion.java
@@ -3,6 +3,6 @@ package com.rusefi;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class rusEFIVersion {
-    public static final int CONSOLE_VERSION = 20200607;
+    public static final int CONSOLE_VERSION = 20200608;
     public static AtomicReference<String> firmwareVersion = new AtomicReference<>("N/A");
 }
