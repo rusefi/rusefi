61a1292270557a818fd2374fba1a008adbde1444
rusefi <rusefillc@gmail.com>
2020-06-13 21:12:33 -0400

    REO progress

--- java_console/inifile/src/com/opensr5/ini/field/ArrayIniField.java
+++ java_console/inifile/src/com/opensr5/ini/field/ArrayIniField.java
@@ -3,7 +3,9 @@ package com.opensr5.ini.field;
 import com.opensr5.ConfigurationImage;
 import com.rusefi.config.Field;
 import com.rusefi.config.FieldType;
+import com.rusefi.tune.xml.Constant;
 
+import java.nio.ByteBuffer;
 import java.util.LinkedList;
 
 public class ArrayIniField extends IniField {
@@ -36,13 +38,32 @@ public class ArrayIniField extends IniField {
         for (int rowIndex = 0; rowIndex < rows; rowIndex++) {
             sb.append("\n\t");
             for (int colIndex = 0; colIndex < cols; colIndex++) {
-                Field f = new Field("", getOffset() + rowIndex * getType().getStorageSize(), getType());
+                Field f = new Field("", getOffset(rowIndex, colIndex), getType());
                 sb.append(' ');
                 sb.append(f.getAnyValue(image));
             }
         }
         sb.append("\n");
         return sb.toString();
+    }
+
+    private int getOffset(int rowIndex, int colIndex) {
+        return getOffset() + (rowIndex * cols + colIndex) * getType().getStorageSize();
+    }
+
+    @Override
+    public void setValue(ConfigurationImage image, Constant constant) {
+        String[] values = constant.getValue().trim().split("\\s+");
+        if (values.length != getRows() * getCols())
+            throw new IllegalStateException(values.length + " values while expecting " + getRows() + " by " + getCols() + " total " + getRows() * getCols());
+
+        for (int rowIndex = 0; rowIndex < rows; rowIndex++) {
+            for (int colIndex = 0; colIndex < cols; colIndex++) {
+                String value = values[rowIndex * cols + colIndex];
+                ByteBuffer wrapped = image.getByteBuffer(getOffset(), type.getStorageSize());
+                ScalarIniField.setValue(wrapped, type, value, Field.NO_BIT_OFFSET);
+            }
+        }
     }
 
     public static IniField parse(LinkedList<String> list) {
