72dc218e3e967cf37129095c1b3fac63b0e1e279
David Holdeman <dave@holdemanenterprises.com>
2020-06-16 09:50:22 -0700

    Fix #1492 redux (#1496)
    * use gen_config.conf to store flags, and gen_config.bat to call gen_config.sh
    
    * Try sh.exe first, and fixed indentation

--- firmware/gen_config.sh
+++ firmware/gen_config.sh
@@ -14,24 +14,7 @@ rm build/config.gen
 
 mkdir build
 
-java -DSystemOut.name=gen_config \
-	-Drusefi.generator.lazyfile.enabled=true \
-	-jar ../java_tools/ConfigDefinition.jar \
-	-definition integration/rusefi_config.txt \
-	-romraider integration \
-	-ts_destination tunerstudio \
-	-with_c_defines false \
-	-initialize_to_zero false \
-	-tool gen_config.sh \
-	-c_defines        controllers/generated/rusefi_generated.h \
-	-firing_order 	  controllers/algo/firing_order.h \
-	-c_destination    controllers/generated/engine_configuration_generated_structures.h \
-	-c_fsio_constants controllers/generated/fsio_enums_generated.def \
-	-c_fsio_getters   controllers/generated/fsio_getters.def \
-	-c_fsio_names     controllers/generated/fsio_names.def \
-	-c_fsio_strings   controllers/generated/fsio_strings.def \
-	-java_destination ../java_console/models/src/com/rusefi/config/generated/Fields.java \
-	-romraider_destination ../java_console/rusefi.xml
+java $(< gen_config.conf)
 
 [ $? -eq 0 ] || (echo "ERROR generating"; exit $?)
 
@@ -43,13 +26,13 @@ else
 	cp -v tunerstudio/rusefi_microrusefi.ini $TS_PATH/dev_mre/projectCfg/mainController.ini
 fi
 
-./gen_config_board.sh microrusefi
+sh gen_config_board.sh microrusefi
 [ $? -eq 0 ] || (echo "ERROR generating microrusefi"; exit $?)
 
-./gen_config_board.sh frankenso
+sh gen_config_board.sh frankenso
 [ $? -eq 0 ] || (echo "ERROR generating frankenso"; exit $?)
 
-./gen_config_board.sh prometheus
+sh gen_config_board.sh prometheus
 [ $? -eq 0 ] || (echo "ERROR generating prometheus"; exit $?)
 
 #cd config\boards\kinetis\config
