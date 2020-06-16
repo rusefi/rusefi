06dda18c9571f4d1e20ba4de1270cfc3f6638b21
David Holdeman <dave@holdemanenterprises.com>
2020-06-16 12:48:09 -0700

    converted gen_config_board.bat (#1498)

--- firmware/gen_config_board.bat
+++ firmware/gen_config_board.bat
@@ -1,34 +1,8 @@
 @echo off
-
-rem This batch files reads rusefi_config.txt and produses firmware persistent configuration headers
-rem the storage section of rusefi.ini is updated as well
-
-set BOARDNAME=%1
-
-if %BOARDNAME%.==. (
-    echo Board name parameter expected
-    exit -1
-) 
-
-echo BOARDNAME=%BOARDNAME%
-
-java -DSystemOut.name=gen_config_board ^
- -cp ../java_tools/ConfigDefinition.jar ^
- com.rusefi.board_generator.BoardReader ^
- -board %BOARDNAME% ^
- -firmware_path . ^
- -out tunerstudio ^
- -enumInputFile controllers/algo/rusefi_enums.h ^
- -enumInputFile controllers/algo/rusefi_hw_enums.h
-
-
-java -DSystemOut.name=gen_config_board ^
- -jar ../java_tools/ConfigDefinition.jar ^
- -definition integration\rusefi_config.txt ^
- -tool gen_config.bat ^
- -ts_destination tunerstudio ^
- -firing_order controllers/algo/firing_order.h ^
- -ts_output_name rusefi_%BOARDNAME%.ini ^
- -prepend tunerstudio/%BOARDNAME%_prefix.txt ^
- -prepend config/boards/%BOARDNAME%/prepend.txt
-
+sh.exe gen_config_board.sh || (
+  if exist C:\cygwin64 (
+    C:\cygwin64\bin\sh.exe gen_config_board.sh
+  ) else (
+    if exist C:\cygwin ( C:\cygwin\bin\sh.exe gen_config_board.sh )
+  )
+)
