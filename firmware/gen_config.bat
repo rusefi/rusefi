22d98795d1ce7b1120a1d491bc09abadee86929e
David <dave@holdemanenterprises.com>
2020-06-16 11:43:43 -0500

    Try sh.exe first, and fixed indentation

--- firmware/gen_config.bat
+++ firmware/gen_config.bat
@@ -1,6 +1,8 @@
 @echo off
-if exist C:\cygwin64 (
- C:\cygwin64\bin\sh.exe gen_config.sh
-) else (
- if exist C:\cygwin ( C:\cygwin\bin\sh.exe gen_config.sh )
+sh.exe gen_config.sh || (
+  if exist C:\cygwin64 (
+    C:\cygwin64\bin\sh.exe gen_config.sh
+  ) else (
+    if exist C:\cygwin ( C:\cygwin\bin\sh.exe gen_config.sh )
+  )
 )
