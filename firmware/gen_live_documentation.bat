7eaa955c87c93fcf60823bf7591c6bbc6a34f3ce
David <dave@holdemanenterprises.com>
2020-06-17 18:30:33 -0500

    add workflow and scripts

--- firmware/gen_live_documentation.bat
+++ firmware/gen_live_documentation.bat
@@ -1,37 +1,8 @@
-rem TODO better place for this stuff, more automation so that each file does not need to be handled separately. maybe regexp?
-
-rm gen_live_documentation.log
-
-java -DSystemOut.name=gen_live_documentation ^
- -jar ../java_tools/ConfigDefinition.jar ^
- -definition integration/pid_state.txt ^
- -java_destination ../java_console/models/src/com/rusefi/config/generated/PidState.java ^
- -c_destination controllers/generated/pid_state_generated.h
-
-java -DSystemOut.name=gen_live_documentation ^
- -jar ../java_tools/ConfigDefinition.jar ^
- -definition integration/engine_state.txt ^
- -java_destination ../java_console/models/src/com/rusefi/config/generated/EngineState.java ^
- -c_destination controllers/generated/engine_state_generated.h
-
-java -DSystemOut.name=gen_live_documentation ^
- -jar ../java_tools/ConfigDefinition.jar ^
- -definition integration/trigger_central.txt ^
- -java_destination ../java_console/models/src/com/rusefi/config/generated/TriggerCentral.java ^
- -c_destination controllers/generated/trigger_central_generated.h
-
-java -DSystemOut.name=gen_live_documentation ^
- -jar ../java_tools/ConfigDefinition.jar ^
- -definition integration/trigger_state.txt ^
- -java_destination ../java_console/models/src/com/rusefi/config/generated/TriggerState.java ^
- -c_destination controllers/generated/trigger_state_generated.h
-
-java -DSystemOut.name=gen_live_documentation ^
- -jar ../java_tools/ConfigDefinition.jar ^
- -definition integration/wall_fuel_state.txt ^
- -java_destination ../java_console/models/src/com/rusefi/config/generated/WallFuelState.java ^
- -c_destination controllers/generated/wall_fuel_generated.h
-
-java -DSystemOut.name=gen_live_documentation ^
- -cp ../java_tools/ConfigDefinition.jar;../java_tools/configuration_definition/lib/snakeyaml.jar ^
- com.rusefi.ldmp.UsagesReader integration/LiveData.yaml
+@echo off
+sh.exe gen_live_documentation.sh || (
+  if exist C:\cygwin64 (
+    C:\cygwin64\bin\sh.exe gen_live_documentation.sh
+  ) else (
+    if exist C:\cygwin ( C:\cygwin\bin\sh.exe gen_live_documentation.sh )
+  )
+)
