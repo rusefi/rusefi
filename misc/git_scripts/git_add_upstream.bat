39e98b9c0ca1dc7c2e1fdbdc88cb6bd383a44fce
rusefi <rusefillc@gmail.com>
2020-06-18 12:57:16 -0400

    new interesting fork!

--- misc/git_scripts/git_add_upstream.bat
+++ misc/git_scripts/git_add_upstream.bat
@@ -7,5 +7,6 @@ git remote add DonaldBecker https://github.com/DonaldBecker/rusefi
 git remote add dron0gus     https://github.com/dron0gus/rusefi 
 git remote add mck1117      https://github.com/mck1117/rusefi 
 git remote add NOx-z        https://github.com/NOx-z/rusefi 
+git remote add chuckwagoncomputing https://github.com/chuckwagoncomputing/rusefi 
 
 git remote -v
