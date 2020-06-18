9cb3669a69c8275273c2b8e18acf45aaa94d3078
rusefi <rusefillc@gmail.com>
2020-06-17 23:54:11 -0400

    TS plugin into bundle

--- misc/jenkins/build_java_console.bat
+++ misc/jenkins/build_java_console.bat
@@ -7,5 +7,10 @@ cd java_console
 call ant clean clean_out_folder jar
 cd ..
 
+echo Building TS plugin
+cd java_tools/ts_plugin_launcher
+call ant clean jar
+cd ../..
+
 if not exist java_console_binary/rusefi_console.jar echo CONSOLE COMPILATION FAILED
 echo java console looks good
