8438ffecde5f4477a6e454bd3e24f3a580ac241e
David <dave@holdemanenterprises.com>
2020-06-20 20:26:33 -0500

    remove call

--- misc/jenkins/build_java_console.sh
+++ misc/jenkins/build_java_console.sh
@@ -6,12 +6,12 @@ java -version
 echo "Building java console"
 pwd
 cd java_console
-call ant clean clean_out_folder jar
+ant clean clean_out_folder jar
 cd ..
 
 echo "Building TS plugin"
 cd java_tools/ts_plugin_launcher
-call ant clean jar
+ant clean jar
 cd ../..
 
 [ -e java_console_binary/rusefi_console.jar ] || (echo "CONSOLE COMPILATION FAILED"; exit 1)
