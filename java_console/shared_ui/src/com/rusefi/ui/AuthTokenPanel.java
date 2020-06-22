66ebaa41bb13bbdfd462bc6bf30a0d6ed98db236
rusefi <rusefillc@gmail.com>
2020-06-22 18:30:59 -0400

    REO progress token setup usability

--- java_console/shared_ui/src/com/rusefi/ui/AuthTokenPanel.java
+++ java_console/shared_ui/src/com/rusefi/ui/AuthTokenPanel.java
@@ -5,8 +5,12 @@ import com.rusefi.ui.util.URLLabel;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
+import java.awt.datatransfer.*;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 import static com.rusefi.ui.storage.PersistentConfiguration.getConfig;
 
@@ -30,24 +34,84 @@ public class AuthTokenPanel {
         String authToken = getAuthToken();
         System.out.println("Got from settings: " + authToken);
 
+        textField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                onTextChange();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                onTextChange();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                onTextChange();
+            }
+        });
+
+        final Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+
+        JButton paste = new JButton("Paste from clipboard");
+        paste.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    String data = (String) clipboard.getData(DataFlavor.stringFlavor);
+                    if (AutoTokenUtil.isToken(data)) {
+                        textField.setText(data);
+                    }
+                } catch (IOException | UnsupportedFlavorException ex) {
+                    // ignoring this exception
+                }
+            }
+        });
+
+        clipboard.addFlavorListener(e -> {
+            setPasteButtonEnabledBasedOnClipboardContent(clipboard, paste);
+        });
+
+        setPasteButtonEnabledBasedOnClipboardContent(clipboard, paste);
+
+        top.add(textField);
+        top.add(paste);
+/*
         JButton save = new JButton("Save");
         save.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                setAuthToken(AuthTokenPanel.this.textField.getText());
-                PersistentConfiguration.getConfig().save();
+                grabText();
             }
         });
-
-        top.add(textField);
         top.add(save);
-
+*/
         content.add(top);
         if (authToken.trim().isEmpty()) {
             authToken = TOKEN_WARNING;
-            content.add(new URLLabel("Get your token here", TOKEN_PROFILE_URL), BorderLayout.SOUTH);
+            content.add(new URLLabel("Get your token from your forum profile", TOKEN_PROFILE_URL), BorderLayout.SOUTH);
         }
         textField.setText(authToken);
+    }
+
+    private void setPasteButtonEnabledBasedOnClipboardContent(Clipboard clipboard, JButton paste) {
+        try {
+            String data = (String) clipboard.getData(DataFlavor.stringFlavor);
+            paste.setEnabled(AutoTokenUtil.isToken(data));
+        } catch (IOException | UnsupportedFlavorException ex) {
+            // ignoring this exception
+        }
+    }
+
+    private void grabText() {
+        setAuthToken(AuthTokenPanel.this.textField.getText());
+        PersistentConfiguration.getConfig().save();
+    }
+
+    private void onTextChange() {
+        if (AutoTokenUtil.isToken(textField.getText())) {
+            grabText();
+        }
     }
 
     public static void setAuthToken(String value) {
