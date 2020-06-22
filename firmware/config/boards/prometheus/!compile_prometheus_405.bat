ec1bcc1604231d0b3dfd299f942044d07fe1abe4
David <dave@holdemanenterprises.com>
2020-06-22 16:31:13 -0500

    convert prometheus

---
+++ firmware/config/boards/prometheus/!compile_prometheus_405.bat
@@ -0,0 +1,8 @@
+@echo off
+sh.exe compile_prometheus_405.sh || (
+  if exist C:\cygwin64 (
+    C:\cygwin64\bin\sh.exe compile_prometheus_405.sh
+  ) else (
+    if exist C:\cygwin ( C:\cygwin\bin\sh.exe compile_prometheus_405.sh )
+  )
+)
