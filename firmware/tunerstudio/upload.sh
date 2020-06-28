0a4a8b9c5b578e4769b5e313793a1cb1e6801215
Andrei <andreikagit@users.noreply.github.com>
2020-06-28 21:30:16 +0300

    try

---
+++ firmware/tunerstudio/upload.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+if [ ! "$1" ] || [ ! "$2" ] || [ ! "$3" ]; then
+ echo "No Secrets"
+ exit 0
+fi
+
+echo -e "\nUploading .ini files"
+
+for f in *.ini; do if [[ -f "$f" ]]; then
+  echo "Processing file $f:"
+  sig=$(grep "^ *signature *=" $f | cut -f2 -d "=")
+  if [ ! -z "$sig" -a "$sig" != " " ]; then
+    echo "* found signature: $sig"
+    if [[ "$sig" =~ rusEFI.*([0-9]{4}[^\"]+) ]]; then
+      sig=${BASH_REMATCH[1]}
+      sig="${sig//\./\/}.ini"
+      path="/online/ini/rusefi/$sig"
+      echo "* found path: $path"
+      #ncftpput -m -R -v -u "$1" -p "$2" "$3" autoupdate $path
+    fi
+  fi
+fi; done
