d7310ce78dcca0e92de0f1196eb6202ad54666bf
Andrei <andreikagit@users.noreply.github.com>
2020-06-28 09:33:12 +0300

    fix typo

--- firmware/config/boards/kinetis/config/gen_config.sh
+++ firmware/config/boards/kinetis/config/gen_config.sh
@@ -22,7 +22,7 @@ java \
  -definition integration/rusefi_config.txt \
  -cache tunerstudio/cache/kinetis \
  -ts_destination tunerstudio \
- -tool kineris_gen_config.bat \
+ -tool kinetis_gen_config.bat \
  -firing_order controllers/algo/firing_order.h \
  -with_c_defines false \
  -initialize_to_zero false \
