2720c0712ecf7409499ed55832647786003e40b8
David Holdeman <David Holdeman>
2020-06-30 08:55:36 -0400

    convert gen_upload_docs.bat

--- misc/jenkins/generate_doxygen/gen_upload_docs.bat
+++ misc/jenkins/generate_doxygen/gen_upload_docs.bat
@@ -1,23 +1,8 @@
-
-echo Should be executed from project root folder. Will try to upload to %RUSEFI_FTP_SERVER%
-
-rem ibom is part of Doxygen job simply in order to reduce workspace HDD usage on my tiny build server
-call misc\jenkins\InteractiveHtmlBom\run.bat
-echo Uploading IBOMs
-ncftpput -R -v -u %RUSEFI_DOXYGEN_FTP_USER% -p %RUSEFI_DOXYGEN_FTP_PASS% %RUSEFI_FTP_SERVER% /ibom hardware/ibom/*
-
-pwd
-cd firmware
-
-doxygen
-IF NOT ERRORLEVEL 0 echo doxygen run FAILED
-IF NOT ERRORLEVEL 0 EXIT /B 1
-
-
-rem http://www.ncftp.com/download/
-rem actually Cygwin http://rusefi.com/wiki/index.php?title=Internal:Software:Build_Server
-cd ../doxygen
-echo Uploading Doxygen
-ncftpput -R -v -u %RUSEFI_DOXYGEN_FTP_USER% -p %RUSEFI_DOXYGEN_FTP_PASS% %RUSEFI_FTP_SERVER% /html html/*
-IF NOT ERRORLEVEL 0 echo upload FAILED
-IF NOT ERRORLEVEL 0 EXIT /B 1
+@echo off
+sh.exe gen_upload_docs.sh || (
+  if exist C:\cygwin64 (
+    C:\cygwin64\bin\sh.exe gen_upload_docs.sh
+  ) else (
+    if exist C:\cygwin ( C:\cygwin\bin\sh.exe gen_upload_docs.sh )
+  )
+)
