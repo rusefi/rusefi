6fa138f3c1ad7c4f13bd11771f10da67d56ad76d
rusefi <rusefillc@gmail.com>
2020-07-02 00:12:47 -0400

    one day we will end up with gradle

---
+++ java_console/shared_io/src/main/java/com/rusefi/shared/FileUtil.java
@@ -0,0 +1,52 @@
+package com.rusefi.shared;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class FileUtil {
+    public static void unzip(String zipFileName, String destPath) throws IOException {
+        File destDir = new File(destPath);
+        byte[] buffer = new byte[1024];
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFileName));
+        ZipEntry zipEntry = zis.getNextEntry();
+        while (zipEntry != null) {
+            File newFile = newFile(destDir, zipEntry);
+            if (newFile.isDirectory()) {
+                System.out.println("Nothing to do for directory " + newFile);
+            } else {
+                unzipFile(buffer, zis, newFile);
+            }
+            zipEntry = zis.getNextEntry();
+        }
+        zis.closeEntry();
+        zis.close();
+        System.out.println("Unzip " + zipFileName + " to " + destPath + " worked!");
+    }
+
+    private static void unzipFile(byte[] buffer, ZipInputStream zis, File newFile) throws IOException {
+        System.out.println("Unzipping " + newFile);
+        FileOutputStream fos = new FileOutputStream(newFile);
+        int len;
+        while ((len = zis.read(buffer)) > 0) {
+            fos.write(buffer, 0, len);
+        }
+        fos.close();
+    }
+
+    private static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+
+        return destFile;
+    }
+}
