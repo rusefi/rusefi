5fe6362f0b8ac2d3b78d3a4c9804431ffc711a93
GitHub build-firmware Action <action@github.com>
2020-07-02 12:27:27 +0000

    Auto-generated configs and docs

--- java_console/models/src/com/rusefi/config/generated/TriggerState.java
+++ java_console/models/src/com/rusefi/config/generated/TriggerState.java
@@ -1,6 +1,6 @@
 package com.rusefi.config.generated;
 
-// this file was generated automatically by rusEfi tool ConfigDefinition.jar based on (unknown script) integration/trigger_state.txt Thu Jul 02 12:17:26 UTC 2020
+// this file was generated automatically by rusEfi tool ConfigDefinition.jar based on (unknown script) integration/trigger_state.txt Thu Jul 02 12:27:25 UTC 2020
 
 // by class com.rusefi.output.FileJavaFieldsConsumer
 import com.rusefi.config.*;
