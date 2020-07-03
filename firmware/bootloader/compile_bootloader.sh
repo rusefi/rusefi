eb7439c05bacfe1d50372cb2b2bec8607b66be6d
David Holdeman <david@chuckwagoncomputing.com>
2020-07-02 21:06:38 -0500

    moved rm and changed path back

--- firmware/bootloader/compile_bootloader.sh
+++ firmware/bootloader/compile_bootloader.sh
@@ -7,7 +7,6 @@ echo "Entering $SCRIPT_NAME"
 [ $BOOTLOADER_CODE_DESTINATION_PATH ] || { BOOTLOADER_CODE_DESTINATION_PATH=".."; }
 
 BOOTLOADER_CODE_DESTINATION_FILE=$BOOTLOADER_CODE_DESTINATION_PATH/bootloader_generated.hxx
-rm -f $BOOTLOADER_CODE_DESTINATION_FILE
 
 echo "$SCRIPT_NAME: Starting bootloader compilation..."
 make -j4 -f src/Makefile $1 $2 $3
@@ -18,6 +17,7 @@ echo "$SCRIPT_NAME: Bootloader build success."
 
 cd blbuild
 # Generate a header file with binary bootloader code
+rm -f $BOOTLOADER_CODE_DESTINATION_FILE
 java -jar ../../../java_tools/bin2header.jar bootloader.bin "$BOOTLOADER_CODE_DESTINATION_FILE" "$BOOTLOADER_COMMENT static const volatile uint8_t bootloader_code[] BOOTLOADER_SECTION"
 [ $? -eq 0 ] || { echo "$SCRIPT_NAME: error generating header file"; exit 1; }
 cd ..
