8b4f3a55cdf2488d6d7b8eb37ee77df112973f53
David Holdeman <david@chuckwagoncomputing.com>
2020-07-03 09:34:00 -0500

    redo logic

--- misc/jenkins/functional_test_and_build_bundle/run.sh
+++ misc/jenkins/functional_test_and_build_bundle/run.sh
@@ -8,7 +8,7 @@ sh misc/jenkins/functional_test_and_build_bundle/build_current_bundle.sh
 
 # Here we use last version of firmware produced by 'clean_compile_two_versions.bat'
 
-if [ "$RUSEFI_SKIP_HW" != "false" ]; then
+if [ ! "$RUSEFI_SKIP_HW" ] || [ "$RUSEFI_SKIP_HW" = "false" ]; then
  sh misc/jenkins/functional_test_and_build_bundle/hw_test.sh
  [ $? -eq 0 ] || { echo "real hardware test ERROR DETECTED"; exit 1; }
 fi
