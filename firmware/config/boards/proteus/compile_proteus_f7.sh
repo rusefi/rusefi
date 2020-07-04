88c24479e07f8d1ab99d6b176e40f2f2d0655081
andreika-git <andreika-git@users.noreply.github.com>
2020-07-04 17:46:27 +0300

    Fix bundle signatures (#1569)
    Co-authored-by: Andrei <andreikagit@users.noreply.github.com>

--- firmware/config/boards/proteus/compile_proteus_f7.sh
+++ firmware/config/boards/proteus/compile_proteus_f7.sh
@@ -6,5 +6,6 @@ export LDSCRIPT=config/boards/NUCLEO_F767/STM32F76xxI.ld
 
 export PROJECT_BOARD=proteus
 export PROJECT_CPU=ARCH_STM32F7
+export EXTRA_PARAMS=-DSHORT_BOARD_NAME=pro
 
 sh config/boards/common_make.sh
