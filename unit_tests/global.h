49d98e3ed0a247d6eb41fbbba85021d7a8077fe0
rusefi <rusefillc@gmail.com>
2020-07-03 21:38:56 -0400

    FATAL error: lsize mismatch 336/3342 #884

--- unit_tests/global.h
+++ unit_tests/global.h
@@ -18,6 +18,9 @@ typedef uint32_t iomode_t;
 typedef uint32_t ioportid_t;
 typedef uint32_t ioportmask_t;
 
+#define DL_OUTPUT_BUFFER 200
+#define INTERMEDIATE_LOGGING_BUFFER_SIZE 100
+
 // just a stub implementation for unit tests
 #define EXPECTED_REMAINING_STACK 1
 #define getCurrentRemainingStack() (999999)
