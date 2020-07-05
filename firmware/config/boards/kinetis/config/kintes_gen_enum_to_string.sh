7fe1dc9d5819981a207fc78a06c399399706822d
rusefi <rusefillc@gmail.com>
2020-07-04 20:59:48 -0400

    more bash

---
+++ firmware/config/boards/kinetis/config/kintes_gen_enum_to_string.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd ../../../..
+
+java -DSystemOut.name=gen_enum_to_string_kinetis ^
+ -jar ../java_tools/enum2string.jar ^
+ -inputPath . ^
+ -outputPath config/boards/kinetis/config/controllers/algo ^
+ -enumInputFile controllers/algo/rusefi_enums.h ^
+ -enumInputFile config/boards/kinetis/rusefi_hw_enums.h ^
+
+
+
