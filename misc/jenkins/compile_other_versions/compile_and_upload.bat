f6c9b7f2521f9c28ea150da8e733c593a59596a1
David Holdeman <david@chuckwagoncomputing.com>
2020-07-13 10:23:14 -0500

    add error checking

--- misc/jenkins/compile_other_versions/compile_and_upload.bat
+++ misc/jenkins/compile_other_versions/compile_and_upload.bat
@@ -1,3 +1,8 @@
 @echo off
 bash.exe misc\jenkins\compile_other_versions\compile.sh %1 %2 %3 %4
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+pwd
+
 bash.exe misc\jenkins\compile_other_versions\prepare_bundle.sh %1 %2 %3 %4
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+pwd
