deb43f98db047774f8b5fa0965a95d86b92fbb6d
rusefi <rusefillc@gmail.com>
2020-07-13 00:44:43 -0400

    proxy progress

--- java_console/io/src/main/java/com/rusefi/io/commands/HelloCommand.java
+++ java_console/io/src/main/java/com/rusefi/io/commands/HelloCommand.java
@@ -1,11 +1,17 @@
 package com.rusefi.io.commands;
 
 import com.opensr5.Logger;
+import com.rusefi.binaryprotocol.BinaryProtocolCommands;
+import com.rusefi.binaryprotocol.IncomingDataBuffer;
 import com.rusefi.config.generated.Fields;
 import com.rusefi.io.IoStream;
 import com.rusefi.io.tcp.BinaryProtocolServer;
+import org.jetbrains.annotations.Nullable;
 
+import java.io.EOFException;
 import java.io.IOException;
+
+import static com.rusefi.binaryprotocol.IoHelper.checkResponseCode;
 
 public class HelloCommand implements Command {
     private final Logger logger;
@@ -18,6 +24,14 @@ public class HelloCommand implements Command {
 
     public static void send(IoStream stream, Logger logger) throws IOException {
         stream.sendPacket(new byte[]{Fields.TS_HELLO_COMMAND}, logger);
+    }
+
+    @Nullable
+    public static String getHelloResponse(IncomingDataBuffer incomingData, Logger logger) throws EOFException {
+        byte[] response = incomingData.getPacket(logger, "[hello]", false);
+        if (!checkResponseCode(response, BinaryProtocolCommands.RESPONSE_OK))
+            return null;
+        return new String(response, 1, response.length - 1);
     }
 
     @Override
--- java_console/ui/src/test/java/com/rusefi/ServerTest.java
+++ java_console/ui/src/test/java/com/rusefi/ServerTest.java
@@ -1,18 +1,27 @@
 package com.rusefi;
 
+import com.opensr5.ConfigurationImage;
 import com.opensr5.Logger;
+import com.rusefi.config.generated.Fields;
+import com.rusefi.io.IoStream;
+import com.rusefi.io.commands.HelloCommand;
 import com.rusefi.server.Backend;
 import com.rusefi.server.ClientConnectionState;
+import com.rusefi.server.SessionDetails;
 import com.rusefi.server.UserDetails;
 import com.rusefi.tools.online.ProxyClient;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.Socket;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
+import static com.rusefi.TestHelper.createIniField;
+import static com.rusefi.TestHelper.prepareImage;
+import static com.rusefi.tools.online.ProxyClient.LOCALHOST;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -26,16 +35,13 @@ public class ServerTest {
     @Test
     public void testSessionTimeout() throws InterruptedException, IOException {
         int serverPort = 7000;
-
-        CountDownLatch serverCreated = new CountDownLatch(1);
-
-
+        int httpPort = 8000;
         Function<String, UserDetails> userDetailsResolver = authToken -> new UserDetails(authToken.substring(0, 5), authToken.charAt(6));
 
+        CountDownLatch serverCreated = new CountDownLatch(1);
         CountDownLatch allClientsDisconnected = new CountDownLatch(1);
         CountDownLatch onConnected = new CountDownLatch(2);
 
-        int httpPort = 8000;
         Backend backend = new Backend(userDetailsResolver, httpPort, logger) {
             @Override
             public void register(ClientConnectionState clientConnectionState) {
@@ -52,12 +58,11 @@ public class ServerTest {
         };
 
         Backend.runProxy(serverPort, serverCreated, backend);
-
         assertTrue(serverCreated.await(30, TimeUnit.SECONDS));
         assertEquals(0, backend.getCount());
 
 
-        new MockRusEfiDevice("00000000-1234-1234-1234-123456789012", "rusEFI 2020.07.06.frankenso_na6.2468827536", logger).connect(serverPort);
+        new MockRusEfiDevice(MockRusEfiDevice.TEST_TOKEN_1, "rusEFI 2020.07.06.frankenso_na6.2468827536", logger).connect(serverPort);
         new MockRusEfiDevice("12345678-1234-1234-1234-123456789012", "rusEFI 2020.07.11.proteus_f4.1986715563", logger).connect(serverPort);
 
         assertTrue(onConnected.await(30, TimeUnit.SECONDS));
@@ -71,5 +76,43 @@ public class ServerTest {
         assertTrue(allClientsDisconnected.await(30, TimeUnit.SECONDS));
 
     }
+
+    @Test
+    public void testRelayWorkflow() throws InterruptedException, IOException {
+        Function<String, UserDetails> userDetailsResolver = authToken -> new UserDetails(authToken.substring(0, 5), authToken.charAt(6));
+        int httpPort = 8001;
+        Backend backend = new Backend(userDetailsResolver, httpPort, logger);
+        int serverPort = 7001;
+        CountDownLatch serverCreated = new CountDownLatch(1);
+
+        // first start backend server
+        Backend.runProxy(serverPort, serverCreated, backend);
+        assertTrue(serverCreated.await(30, TimeUnit.SECONDS));
+
+
+        // create virtual controller
+        int controllerPort = 7002;
+        ConfigurationImage controllerImage = prepareImage(240, createIniField(Fields.CYLINDERSCOUNT));
+        CountDownLatch controllerCreated = new CountDownLatch(1);
+        TestHelper.createVirtualController(controllerImage, controllerPort, parameter -> controllerCreated.countDown(), logger);
+        assertTrue(controllerCreated.await(30, TimeUnit.SECONDS));
+
+
+        // start network broadcaster to connect controller with backend since in real life controller has only local serial port it does not have network
+        IoStream targetEcuSocket = TestHelper.createTestStream(controllerPort, logger);
+        HelloCommand.send(targetEcuSocket, logger);
+        String controllerSignature = HelloCommand.getHelloResponse(targetEcuSocket.getDataBuffer(), logger);
+
+        SessionDetails sessionDetails = MockRusEfiDevice.createTestSession(MockRusEfiDevice.TEST_TOKEN_1, controllerSignature);
+
+        BaseBroadcastingThread baseBroadcastingThread = new BaseBroadcastingThread(new Socket(LOCALHOST, serverPort),
+                sessionDetails,
+                logger) {
+            // todo
+        };
+        baseBroadcastingThread.start();
+
+    }
+
 
 }
