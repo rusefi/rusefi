c59af8415bef35414069f1f199ecf2a41ff83d16
David Holdeman <david@chuckwagoncomputing.com>
2020-07-13 08:37:47 -0500

    go back to gen_config running all boards

--- firmware/gen_config.sh
+++ firmware/gen_config.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
 
-#set -x
-#TS_PATH="/home/<user>/TunerStudioProjects/"
+echo "This script reads rusefi_config.txt and produces firmware persistent configuration headers"
+echo "The storage section of rusefi.ini is updated as well"
 
-echo "This batch files reads rusefi_config.txt and produses firmware persistent configuration headers"
-echo "the storage section of rusefi.ini is updated as well"
+rm -f gen_config.log
+rm -f gen_config_board.log
 
-rm gen_config.log
-rm gen_config_board.log
+bash gen_config_default.sh
+[ $? -eq 0 ] || { echo "ERROR generating default"; exit 1; }
 
-if [ -z "${TS_PATH}" ]; then
- echo "TS_PATH not defined"
- # it's nice to have default location
- TS_PATH="${HOMEDRIVE}${HOMEPATH}\Documents\TunerStudioProjects"
-fi
-
-echo "This would automatically copy latest file to 'dev' TS projects to ${TS_PATH}"
-cp -v tunerstudio/generated/rusefi.ini $TS_PATH/dev/projectCfg/mainController.ini
-cp -v tunerstudio/generated/rusefi_mre_f4.ini $TS_PATH/mre_f4/projectCfg/mainController.ini
-
-if [ "$1" = "kinetis" ]; then
-  cd config/boards/kinetis/config
-  bash gen_config.sh
-  [ $? -eq 0 ] || { echo "ERROR generating board kinetis kin"; exit 1; }
-else
-  bash gen_config_board.sh $1 $2
-  [ $? -eq 0 ] || { echo "ERROR generating board $BOARD_NAME $BOARD_SHORT_NAME"; exit 1; }
-fi
+for BOARD in "microrusefi mre_f7" "microrusefi mre_f4" "frankenso frankenso_na6" "prometheus prometheus_469" "prometheus prometheus_405" "proteus proteus_f7" "proteus proteus_f4"; do
+ BOARD_NAME="${BOARD% *}"
+ BOARD_SHORT_NAME="${BOARD#* }"
+ bash gen_config_board.sh $BOARD_NAME $BOARD_SHORT_NAME
+ [ $? -eq 0 ] || { echo "ERROR generating board $BOARD_NAME $BOARD_SHORT_NAME"; exit 1; }
+done
 
 exit 0
