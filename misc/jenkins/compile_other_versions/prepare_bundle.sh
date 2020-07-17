f6fad72b72806d229e412a9ac12720ecdfa35ee4
David Holdeman <david@chuckwagoncomputing.com>
2020-07-16 19:51:55 -0500

    Fix Bundles (#1610)
    * facepalm
    
    * define BUNDLE_NAME
    
    * oops

--- misc/jenkins/compile_other_versions/prepare_bundle.sh
+++ misc/jenkins/compile_other_versions/prepare_bundle.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+BUNDLE_NAME="$1"
 SCRIPT_NAME="prepare_bundle.sh"
-echo "Entering $SCRIPT_NAME with $BUNDLE_NAME"
+echo "Entering $SCRIPT_NAME with ${BUNDLE_NAME}"
 echo "RUSEFI_BUILD_FTP_USER=$RUSEFI_BUILD_FTP_USER"
 
 TIMESTAMP=$(date "+%Y%m%d_%H%M%S")
 
-export FOLDER="temp/snapshot_${TIMESTAMP}_${2}_rusefi"
+export FOLDER="temp/snapshot_${TIMESTAMP}_${BUNDLE_NAME}_rusefi"
 
-export BUNDLE_FULL_NAME="rusefi_bundle_$BUNDLE_NAME"
+export BUNDLE_FULL_NAME="rusefi_bundle_${BUNDLE_NAME}"
 
 bash misc/jenkins/build_working_folder.sh
 
