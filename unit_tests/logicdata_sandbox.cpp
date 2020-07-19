80ac91662ee22706e24c1a64dde7673d27a59ae7
rusefi <rusefillc@gmail.com>
2020-07-19 17:42:11 -0400

    let's add .logicdata for unit tests

--- unit_tests/logicdata_sandbox.cpp
+++ unit_tests/logicdata_sandbox.cpp
@@ -2,10 +2,27 @@
 #include <cstdio>
 #include "logicdata.h"
 
+static CompositeEvent events[100];
+
+
+void setEvent(CompositeEvent *events, int index,
+		int timestamp, bool primaryTrigger, bool secondaryTrigger, bool trg, bool sync, bool coil, bool injector) {
+	events[index].timestamp = timestamp;
+	events[index].primaryTrigger = primaryTrigger;
+}
+
 int main(int argc, char **argv) {
 	printf(".logicdata Sandbox 20200719\n");
 
-	writeFile();
+
+	int index = 0;
+    setEvent(events, index++, 10, false, false, false, false, false, false);
+    setEvent(events, index++, 20, true, false, true, false, false, false);
+    setEvent(events, index++, 30, false, false, false, false, false, false);
+    setEvent(events, index++, 1000030, false, false, false, false, true, false);
+    setEvent(events, index++, 2000030, false, false, true, false, false, true);
+
+	writeFile(events, index);
 
 	printf("Done!\n");
 }
