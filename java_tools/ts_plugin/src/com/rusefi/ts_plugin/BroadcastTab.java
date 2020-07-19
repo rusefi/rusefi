875bf811e86b0753b9b7668fa4ccc49d8972f467
rusefi <rusefillc@gmail.com>
2020-07-18 23:17:36 -0400

    proxy controls into TS plugin

---
+++ java_tools/ts_plugin/src/com/rusefi/ts_plugin/BroadcastTab.java
@@ -0,0 +1,12 @@
+package com.rusefi.ts_plugin;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BroadcastTab {
+    private final Component content = new JPanel();
+
+    public Component getContent() {
+        return content;
+    }
+}
