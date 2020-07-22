9828cf3ce16ecef836a258ae6d1d4676b20d93d0
rusefi <rusefillc@gmail.com>
2020-07-21 20:09:17 -0400

    better plugin file name and location

--- misc/jenkins/build_working_folder.sh
+++ misc/jenkins/build_working_folder.sh
@@ -50,7 +50,7 @@ fi
 
 cp java_console/rusefi_autoupdate.jar     $CONSOLE_FOLDER
 cp java_console_binary/rusefi_console.jar $CONSOLE_FOLDER
-cp java_tools/ts_plugin_launcher/build/jar/rusefi_plugin_launcher.jar $CONSOLE_FOLDER
+cp java_tools/ts_plugin_launcher/build/jar/rusefi_ts_plugin_launcher.jar $FOLDER
 cp simulator/build/rusefi_simulator.exe   $CONSOLE_FOLDER
 cp misc/console_launcher/rusefi_*.exe     $CONSOLE_FOLDER
 cp java_console/rusefi.xml                $CONSOLE_FOLDER
