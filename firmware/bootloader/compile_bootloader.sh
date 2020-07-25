ac047c5836c097bbbd30b58d7caf1c231cc9f1bf
rusefi <rusefillc@gmail.com>
2020-07-25 17:23:00 -0400

    bootloader not well? #1638 trying to fix error detection mechanism

--- firmware/bootloader/compile_bootloader.sh
+++ firmware/bootloader/compile_bootloader.sh
@@ -11,13 +11,14 @@ BOOTLOADER_CODE_DESTINATION_FILE=$BOOTLOADER_CODE_DESTINATION_PATH/bootloader_ge
 echo "$SCRIPT_NAME: Starting bootloader compilation..."
 make -j4 -f src/Makefile $1 $2 $3
 
-[ $? -eq 0 ] || { echo "make compilation failed"; exit 1; }
+rm -f blbuild/$BOOTLOADER_CODE_DESTINATION_FILE
+
+[ $? -eq 0 ] || { echo "bootloader compilation failed"; exit 1; }
  
 echo "$SCRIPT_NAME: Bootloader build success."
 
 cd blbuild
 # Generate a header file with binary bootloader code
-rm -f $BOOTLOADER_CODE_DESTINATION_FILE
 java -jar ../../../java_tools/bin2header.jar bootloader.bin "$BOOTLOADER_CODE_DESTINATION_FILE" "$BOOTLOADER_COMMENT static const volatile uint8_t bootloader_code[] BOOTLOADER_SECTION"
 [ $? -eq 0 ] || { echo "$SCRIPT_NAME: error generating header file"; exit 1; }
 cd ..
