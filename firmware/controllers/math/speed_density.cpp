b26bd6d891d760661bd959b5207ff175292c20c2
Matthew Kennedy <matthewkennedy@outlook.com>
2020-07-24 19:30:12 -0700

    move speed density

--- firmware/controllers/math/speed_density.cpp
+++ firmware/controllers/math/speed_density.cpp
@@ -112,42 +112,6 @@ temperature_t getTCharge(int rpm, float tps DECLARE_ENGINE_PARAMETER_SUFFIX) {
 	return Tcharge;
 }
 
-AirmassResult getSpeedDensityAirmass(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
-	ScopePerf perf(PE::GetSpeedDensityFuel);
-
-	/**
-	 * most of the values are pre-calculated for performance reasons
-	 */
-	float tChargeK = ENGINE(engineState.sd.tChargeK);
-	if (cisnan(tChargeK)) {
-		warning(CUSTOM_ERR_TCHARGE_NOT_READY2, "tChargeK not ready"); // this would happen before we have CLT reading for example
-		return {};
-	}
-
-	float map = getMap(PASS_ENGINE_PARAMETER_SIGNATURE);
-	efiAssert(CUSTOM_ERR_ASSERT, !cisnan(map), "NaN map", {});
-
-	engine->engineState.sd.manifoldAirPressureAccelerationAdjustment = engine->engineLoadAccelEnrichment.getEngineLoadEnrichment(PASS_ENGINE_PARAMETER_SIGNATURE);
-
-	float adjustedMap = engine->engineState.sd.adjustedManifoldAirPressure = map + engine->engineState.sd.manifoldAirPressureAccelerationAdjustment;
-	efiAssert(CUSTOM_ERR_ASSERT, !cisnan(adjustedMap), "NaN adjustedMap", {});
-
-	float airMass = SpeedDensityBase::getAirmassImpl(ENGINE(engineState.currentBaroCorrectedVE), adjustedMap, tChargeK PASS_ENGINE_PARAMETER_SUFFIX);
-	if (cisnan(airMass)) {
-		warning(CUSTOM_ERR_6685, "NaN airMass");
-		return {};
-	}
-#if EFI_PRINTF_FUEL_DETAILS
-	printf("getSpeedDensityAirmass map=%.2f adjustedMap=%.2f airMass=%.2f\t\n",
-			map, adjustedMap, engine->engineState.sd.adjustedManifoldAirPressure);
-#endif /*EFI_PRINTF_FUEL_DETAILS */
-
-	return {
-		airMass,
-		map,	// AFR/VE table Y axis
-	};
-}
-
 void setDefaultVETable(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 	setRpmTableBin(config->veRpmBins, FUEL_RPM_COUNT);
 	veMap.setAll(80);
