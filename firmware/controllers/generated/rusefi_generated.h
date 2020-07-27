30e70cf8b72a80c99b8e1fd5ed7998eafa9564f4
GitHub build-firmware Action <action@github.com>
2020-07-27 13:48:00 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated.h
+++ firmware/controllers/generated/rusefi_generated.h
@@ -1075,8 +1075,8 @@
 #define showHumanReadableWarning_offset 976
 #define showSdCardWarning_offset 76
 #define SIGNATURE_BOARD all
-#define SIGNATURE_DATE 2020.07.26
-#define SIGNATURE_HASH 1848812543
+#define SIGNATURE_DATE 2020.07.27
+#define SIGNATURE_HASH 173557045
 #define silentTriggerError_offset 1464
 #define slowAdcAlpha_offset 2088
 #define sparkDwellRpmBins_offset 332
@@ -1344,7 +1344,7 @@
 #define ts_show_spi true
 #define ts_show_trigger_comparator false
 #define ts_show_tunerstudio_port true
-#define TS_SIGNATURE "rusEFI 2020.07.26.all.1848812543"
+#define TS_SIGNATURE "rusEFI 2020.07.27.all.173557045"
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define tunerStudioSerialSpeed_offset 728
 #define twoWireBatchIgnition_offset 1476
