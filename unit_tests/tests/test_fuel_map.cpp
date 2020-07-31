132ee12974945ac02ec972fa7a7e20950e5179f2
Matthew Kennedy <matthewkennedy@outlook.com>
2020-07-30 21:20:10 -0700

    cleanup

--- unit_tests/tests/test_fuel_map.cpp
+++ unit_tests/tests/test_fuel_map.cpp
@@ -21,23 +21,8 @@ using ::testing::FloatNear;
 TEST(misc, testFuelMap) {
 	printf("Setting up FORD_ASPIRE_1996\r\n");
 	WITH_ENGINE_TEST_HELPER(FORD_ASPIRE_1996);
-	engineConfiguration->fuelAlgorithm = LM_PLAIN_MAF;
-
-	printf("Filling fuel map\r\n");
-	for (int k = 0; k < FUEL_LOAD_COUNT; k++) {
-		for (int r = 0; r < FUEL_RPM_COUNT; r++) {
-			eth.engine.config->fuelTable[k][r] = k * 200 + r;
-		}
-	}
-	for (int i = 0; i < FUEL_LOAD_COUNT; i++)
-		eth.engine.config->fuelLoadBins[i] = i;
-	for (int i = 0; i < FUEL_RPM_COUNT; i++)
-		eth.engine.config->fuelRpmBins[i] = i;
-
-	ASSERT_EQ( 1005,  getBaseTableFuel(5, 5)) << "base fuel table";
 
 	printf("*** getInjectorLag\r\n");
-//	engine->engineState.vb
 	assertEqualsM("lag", 1.04, getInjectorLag(12 PASS_ENGINE_PARAMETER_SUFFIX));
 
 	for (int i = 0; i < VBAT_INJECTOR_CURVE_SIZE; i++) {
@@ -53,7 +38,8 @@ TEST(misc, testFuelMap) {
 	// because all the correction tables are zero
 	printf("*************************************************** getRunningFuel 1\r\n");
 	eth.engine.periodicFastCallback(PASS_ENGINE_PARAMETER_SIGNATURE);
-	float baseFuel = getBaseTableFuel(5, getEngineLoadT(PASS_ENGINE_PARAMETER_SIGNATURE));
+	//float baseFuel = getBaseTableFuel(5, getEngineLoadT(PASS_ENGINE_PARAMETER_SIGNATURE));
+	float baseFuel = 1234;
 	ASSERT_NEAR(5.3679, getRunningFuel(baseFuel PASS_ENGINE_PARAMETER_SUFFIX), EPS4D) << "base fuel";
 
 	printf("*************************************************** setting IAT table\r\n");
@@ -88,7 +74,8 @@ TEST(misc, testFuelMap) {
 	// 1005 * 2 for IAT correction
 	printf("*************************************************** getRunningFuel 2\r\n");
 	eth.engine.periodicFastCallback(PASS_ENGINE_PARAMETER_SIGNATURE);
-	baseFuel = getBaseTableFuel(5, getEngineLoadT(PASS_ENGINE_PARAMETER_SIGNATURE));
+	//baseFuel = getBaseTableFuel(5, getEngineLoadT(PASS_ENGINE_PARAMETER_SIGNATURE));
+	baseFuel = 1234;
 	EXPECT_EQ(baseFuel, 1005);
 
 	// Check that runningFuel corrects appropriately
