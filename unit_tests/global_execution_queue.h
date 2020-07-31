2eca162a8a393ba09a58e3e21c31ad6fdf219649
Matthew Kennedy <matthewkennedy@outlook.com>
2020-07-31 14:41:29 -0700

    expose the problem

--- unit_tests/global_execution_queue.h
+++ unit_tests/global_execution_queue.h
@@ -12,6 +12,8 @@
 
 class TestExecutor : public ExecutorInterface {
 public:
+	~TestExecutor();
+
 	void scheduleByTimestamp(scheduling_s *scheduling, efitimeus_t timeUs, action_s action) override;
 	void scheduleByTimestampNt(scheduling_s *scheduling, efitick_t timeNt, action_s action) override;
 	void scheduleForLater(scheduling_s *scheduling, int delayUs, action_s action) override;
