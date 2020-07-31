e664a6d0172785bdd4877e731fec6792cea6d39d
rusefi <rusefillc@gmail.com>
2020-07-31 10:26:14 -0400

    refactoring

--- java_console/io/src/main/java/com/rusefi/binaryprotocol/BinaryProtocolCommands.java
+++ java_console/io/src/main/java/com/rusefi/binaryprotocol/BinaryProtocolCommands.java
@@ -7,17 +7,13 @@ import com.rusefi.config.generated.Fields;
  * 6/21/2017.
  */
 public interface BinaryProtocolCommands {
-    // see BLOCKING_FACTOR in firmware code
-    int BLOCKING_FACTOR = Fields.BLOCKING_FACTOR;
     byte RESPONSE_OK = Fields.TS_RESPONSE_OK;
     byte RESPONSE_BURN_OK = Fields.TS_RESPONSE_BURN_OK;
     byte RESPONSE_COMMAND_OK = Fields.TS_RESPONSE_COMMAND_OK;
-    char COMMAND_PROTOCOL = 'F';
+    char COMMAND_PROTOCOL = Fields.TS_COMMAND_F;
     // todo: make crc32CheckCommand shorted one day later - no need in 6 empty bytes
     char COMMAND_CRC_CHECK_COMMAND = Fields.TS_CRC_CHECK_COMMAND;
-    char COMMAND_PAGE = 'P';
-    char COMMAND_READ = Fields.TS_READ_COMMAND;
-    char COMMAND_CHUNK_WRITE = 'C'; // pageChunkWrite
-    char COMMAND_BURN = 'B'; // burnCommand
+    char COMMAND_CHUNK_WRITE = Fields.TS_CHUNK_WRITE_COMMAND;
+    char COMMAND_BURN = Fields.TS_BURN_COMMAND;
     char COMMAND_GET_STRUCT = '9'; // TS_GET_STRUCT
 }
