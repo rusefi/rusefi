18a2e48f042dce28b301b697818ceebd39643fe8
Matthew Kennedy <makenne@microsoft.com>
2020-08-08 01:17:23 -0700

    comment

--- firmware/util/math/biquad.h
+++ firmware/util/math/biquad.h
@@ -14,6 +14,8 @@ public:
     float filter(float input);
 
 	void configureBandpass(float samplingFrequency, float centerFrequency, float Q);
+
+	// Default Q = 1/sqrt(2) = 0.707 gives a maximally flat passband without any overshoot near the cutoff (ie, Butterworth)
 	void configureLowpass(float samplingFrequency, float cutoffFrequency, float Q = 0.707f);
 
 private:
