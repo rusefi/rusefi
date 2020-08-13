ce719c0e0104672cdf3fdbed018cd60ccd5b45be
David Holdeman <dave@holdemanenterprises.com>
2020-08-13 21:49:56 +0000

    check for dir existence

--- hardware/generate_diffs.sh
+++ hardware/generate_diffs.sh
@@ -2,5 +2,7 @@
 
 for file in $(find . -name "*.kicad_pcb"); do
 ../KiCad-Diff/kidiff_linux.py -w -s Git -b $(git rev-parse --short HEAD~1) -a $(git rev-parse --short HEAD) -d :0 $file
-ncftpput -R -v -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" /diffs/plots_$(basename "$file") $(dirname "$file")/plots
+if [ -d $(dirname "$file")/plots ]; then
+  ncftpput -R -v -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" /diffs/plots_$(basename "$file") $(dirname "$file")/plots
+fi
 done
