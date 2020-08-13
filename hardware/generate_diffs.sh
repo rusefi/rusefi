44e09ae414e5a55d3c4af6fc4f5daa14baf5cfea
David Holdeman <dave@holdemanenterprises.com>
2020-08-13 22:25:46 +0000

    fix indentation

--- hardware/generate_diffs.sh
+++ hardware/generate_diffs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 for file in $(find . -name "*.kicad_pcb"); do
-../KiCad-Diff/kidiff_linux.py -w -s Git -b $(git rev-parse --short HEAD~1) -a $(git rev-parse --short HEAD) -d :0 $file
-if [ -d $(dirname "$file")/plots ] && [ -n "$RUSEFI_FTP_SERVER" ]; then
-  ncftpput -R -v -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" /diffs/plots_$(basename "$file") $(dirname "$file")/plots
-fi
+  ../KiCad-Diff/kidiff_linux.py -w -s Git -b $(git rev-parse --short HEAD~1) -a $(git rev-parse --short HEAD) -d :0 $file
+  if [ -d $(dirname "$file")/plots ] && [ -n "$RUSEFI_FTP_SERVER" ]; then
+    ncftpput -R -v -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" /diffs/plots_$(basename "$file") $(dirname "$file")/plots
+  fi
 done
