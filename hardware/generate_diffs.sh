a81e07f691299b86229da9185098eb0e00e1eb0a
David Holdeman <dave@holdemanenterprises.com>
2020-08-13 23:50:09 +0000

    use find exec

--- hardware/generate_diffs.sh
+++ hardware/generate_diffs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-for file in $(find . -name "*.kicad_pcb"); do
-  ../KiCad-Diff/kidiff_linux.py -w -s Git -b $(git rev-parse --short HEAD~1) -a $(git rev-parse --short HEAD) -d :0 $file
-  if [ -d $(dirname "$file")/plots ] && [ -n "$RUSEFI_FTP_SERVER" ]; then
-    echo "mkdir diffs/plots_$(basename "$file" .kicad_pcb)" | ncftp -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER"
-    ncftpput -R -v -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" /diffs/plots_$(basename "$file" .kicad_pcb) $(dirname "$file")/plots
+function gendiffs() {
+  ../KiCad-Diff/kidiff_linux.py -w -s Git -b $(git rev-parse --short HEAD~1) -a $(git rev-parse --short HEAD) -d :0 $1
+  if [ -d $(dirname "$1")/plots ] && [ -n "$RUSEFI_FTP_SERVER" ]; then
+    echo "mkdir diffs/plots_$(basename "$1" .kicad_pcb)" | ncftp -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER"
+    ncftpput -R -v -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" /diffs/plots_$(basename "$1" .kicad_pcb) $(dirname "$1")/plots
   fi
-done
+}
+
+find . -name "*.kicad_pcb" -exec gendiffs {} \;
