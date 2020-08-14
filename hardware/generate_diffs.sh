162d36a5b36836eacb01efa2a557d16cf0c7af28
David Holdeman <dave@holdemanenterprises.com>
2020-08-13 19:38:50 -0500

    Kicad diff action (#1691)
    * add kicad diff workflow
    
    * download KiCad-Diff and fix path
    
    * add wildcards
    
    * wat me
    
    * not capitalized as in the help menu grrr
    
    * run script instead of all in workflow
    
    * actually it is supposed to be capitalized
    
    * upload results
    
    * check for dir existence
    
    * check RUSEFI_FTP_SERVER
    
    * fix indentation
    
    * we dont actually need xvfb
    
    * make directory
    
    * remove extension from dir name
    
    * use find exec
    
    * export and use bash
    
    * fix quotes

---
+++ hardware/generate_diffs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function gendiffs() {
+  ../KiCad-Diff/kidiff_linux.py -w -s Git -b $(git rev-parse --short HEAD~1) -a $(git rev-parse --short HEAD) -d :0 $1
+  if [ -d $(dirname "$1")/plots ] && [ -n "$RUSEFI_FTP_SERVER" ]; then
+    echo "mkdir diffs/plots_$(basename "$1" .kicad_pcb)" | ncftp -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER"
+    ncftpput -R -v -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" /diffs/plots_$(basename "$1" .kicad_pcb) $(dirname "$1")/plots
+  fi
+}
+export -f gendiffs
+
+find . -name "*.kicad_pcb" -exec bash -c 'gendiffs "$0"' {} \;
