abb162411381765b1f90983e8b9b85c92761fdbc
David Holdeman <dave@holdemanenterprises.com>
2020-08-14 19:29:02 +0000

    remove release suffix

--- hardware/generate_diffs.sh
+++ hardware/generate_diffs.sh
@@ -10,15 +10,11 @@
 # which contains index.html. Open that in a web browser and you get a nice visual diff of each layer.
 #!/bin/bash
 
-if [ "$1" == "release" ]; then
-  SUFFIX="_release"
-fi
-
 function gendiffs() {
   ../KiCad-Diff/kidiff_linux.py -w -s Git -b $(git rev-parse --short HEAD~1) -a $(git rev-parse --short HEAD) -d :0 $1
   if [ -d $(dirname "$1")/plots ] && [ -n "$RUSEFI_FTP_SERVER" ]; then
     lftp -u "$RUSEFI_DOXYGEN_FTP_USER","$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" -c \
-      "mirror -Re $(dirname "$1")/plots/ /diffs/plots_$(basename "$1" .kicad_pcb)${SUFFIX}; exit"
+      "mirror -Re $(dirname "$1")/plots/ /diffs/plots_$(basename "$1" .kicad_pcb); exit"
   fi
 }
 export -f gendiffs
