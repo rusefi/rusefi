2d50bf4e257f1b63cf077fbfeecbc431bff10b44
David Holdeman <david@chuckwagoncomputing.com>
2020-08-14 07:38:39 -0500

    Add documentation to script

--- hardware/generate_diffs.sh
+++ hardware/generate_diffs.sh
@@ -1,3 +1,13 @@
+#####################################################################################
+#                                 generate_diffs.sh                                 #
+#                                    08/13/2020                                     #
+#                             Written By David Holdeman                             #
+#   Checks for changed .kicad_pcb files, generates visual diffs, and uploads them   #
+#                                      to FTP.                                      #
+#####################################################################################
+# The FTP location this uploads to is /diffs/plot_<name of kicad_pcb file, without the extension>
+# In that directory are subdirectorys containing generated SVGs, as well as a 'web' subdirectory,
+# which contains index.html. Open that in a web browser and you get a nice visual diff of each layer.
 #!/bin/bash
 
 function gendiffs() {
