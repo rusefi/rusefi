733008b9d042694acf6242feb4609512a088b931
Matthew Kennedy <matthewkennedy@outlook.com>
2020-08-21 16:47:12 -0700

    Analog input filtering  (#1680)
    * improve biquad
    
    * cleanup
    
    * add filtering to subscriptions
    
    * config sensors
    
    * comment
    
    * doesn't need to be that fast
    
    Co-authored-by: Matthew Kennedy <makenne@microsoft.com>

--- firmware/util/util.mk
+++ firmware/util/util.mk
@@ -11,6 +11,7 @@ UTILSRC_CPP = \
 	$(UTIL_DIR)/containers/counter64.cpp \
 	$(UTIL_DIR)/containers/local_version_holder.cpp \
 	$(UTIL_DIR)/containers/table_helper.cpp \
+	$(UTIL_DIR)/math/biquad.cpp \
 	$(UTIL_DIR)/math/pid.cpp \
 	$(UTIL_DIR)/math/interpolation.cpp \
 	$(PROJECT_DIR)/util/datalogging.cpp \
