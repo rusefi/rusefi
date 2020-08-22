57aaabd1c057f0734c21e3bdf6a359db4ef6ff55
David Holdeman <david@chuckwagoncomputing.com>
2020-08-21 19:21:13 -0500

    catch error

--- simulator/compile.sh
+++ simulator/compile.sh
@@ -1,4 +1,4 @@
 rm -f build/rusefi_simulator.exe
 make -j4 -r
-file build/rusefi_simulator
+file build/rusefi_simulator || { echo "Simulator compilation failed"; exit 1; }
 echo "TIMESTAMP $(date "+%D %T.%2N") just compiled rusefi simulator"
