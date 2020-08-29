997374f4b78b352d2be392132671963b368ee5cc
David Holdeman <david@chuckwagoncomputing.com>
2020-08-29 18:05:49 -0500

    semicolon stupid

--- firmware/controllers/buttonshift.cpp
+++ firmware/controllers/buttonshift.cpp
@@ -52,6 +52,6 @@ setDesiredGear(GEAR_1);
 
 
 void initButtonShift(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
-    buttonShiftController.init(PASS_ENGINE_PARAMETER_SIGNATURE)
+    buttonShiftController.init(PASS_ENGINE_PARAMETER_SIGNATURE);
     engine->gearController = &buttonShiftController;
 }
