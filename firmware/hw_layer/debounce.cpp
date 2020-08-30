208ded4af3f1bdca39784ebc4ae6b425ec11650e
David Holdeman <david@chuckwagoncomputing.com>
2020-08-30 16:53:10 -0500

    ifdefs

--- firmware/hw_layer/debounce.cpp
+++ firmware/hw_layer/debounce.cpp
@@ -4,10 +4,11 @@ ButtonDebounce::ButtonDebounce (int t, brain_pin_e p, pin_input_mode_e m) {
     threshold = t;
     timeLast = 0;
     pin = p;
+#ifdef PAL_MODE_INPUT_PULLDOWN
     mode = (m == PI_PULLUP) ? PAL_MODE_INPUT_PULLUP : PAL_MODE_INPUT_PULLDOWN;
 //    efiSetPadMode("Button", p, mode);
 efiSetPadMode("Button", p, PAL_HIGH);
-
+#endif
 }
 
 bool ButtonDebounce::readPinEvent() {
@@ -18,8 +19,10 @@ bool ButtonDebounce::readPinEvent() {
     timeLast = timeNow;
     readValue = false;
  //   readValue = efiReadPin(pin);
+#ifdef PAL_MODE_INPUT_PULLDOWN
     if (mode != PAL_MODE_INPUT_PULLDOWN) {
         return !readValue;
     }
+#endif
     return readValue;
 }
