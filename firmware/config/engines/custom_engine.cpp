d059cb46168dd6d55a353838b085ffb684e089ba
rusefi <rusefillc@gmail.com>
2020-08-31 14:26:34 -0400

    blinking more LEDs

--- firmware/config/engines/custom_engine.cpp
+++ firmware/config/engines/custom_engine.cpp
@@ -538,6 +538,7 @@ void mreBoardNewTest(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
 	engineConfiguration->ignitionPins[8 - 1] = TLE8888_PIN_12;
 
 	engineConfiguration->afr.hwChannel = EFI_ADC_NONE;
+	engineConfiguration->throttlePedalPositionAdcChannel = EFI_ADC_NONE;
 
 	engineConfiguration->ignitionPins[9 - 1] = GPIOB_1; // AV9
 	engineConfiguration->ignitionPins[10 - 1] = GPIOB_0; // AV8
