2e6ca2198ffc4dbd1da9dfc23dcca05b2d420651
David Holdeman <david@chuckwagoncomputing.com>
2020-08-31 17:26:32 -0500

    try switching order

--- firmware/controllers/buttonshift.cpp
+++ firmware/controllers/buttonshift.cpp
@@ -6,20 +6,20 @@ EXTERN_ENGINE;
 ButtonShiftController buttonShiftController;
 
 void ButtonShiftController::init (DECLARE_ENGINE_PARAMETER_SIGNATURE) {
-    ButtonDebounce u = ButtonDebounce(150, CONFIG(tcuUpshiftButtonPin), CONFIG(tcuUpshiftButtonPinMode));
-    debounceUp = &u;
-    ButtonDebounce d = ButtonDebounce(150, CONFIG(tcuDownshiftButtonPin), CONFIG(tcuDownshiftButtonPinMode));
+    ButtonDebounce d = ButtonDebounce(500, CONFIG(tcuDownshiftButtonPin), CONFIG(tcuDownshiftButtonPinMode));
     debounceDown = &d;
+    ButtonDebounce u = ButtonDebounce(500, CONFIG(tcuUpshiftButtonPin), CONFIG(tcuUpshiftButtonPinMode));
+    debounceUp = &u;
 }
 
 void ButtonShiftController::update() {
     bool upPinState = false;
     bool downPinState = false;
-    if (CONFIG(tcuDownshiftButtonPin) && CONFIG(tcuEnabled)) {
-        downPinState = debounceDown->readPinEvent();
-    }
     if (CONFIG(tcuUpshiftButtonPin) && CONFIG(tcuEnabled)) {
         upPinState = debounceUp->readPinEvent();
+    }
+    if (CONFIG(tcuDownshiftButtonPin) && CONFIG(tcuEnabled)) {
+        downPinState = debounceDown->readPinEvent();
     }
     gear_e gear = getDesiredGear();
     if (upPinState) {
