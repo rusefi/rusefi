a88db25ba03dcd051d2d637ba3e9ac1c7d8b150a
David Holdeman <david@chuckwagoncomputing.com>
2020-08-31 21:00:46 -0500

    try checking tcuEnabled

--- firmware/controllers/engine_controller.cpp
+++ firmware/controllers/engine_controller.cpp
@@ -265,7 +265,9 @@ static void doPeriodicSlowCallback(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 	engine->periodicSlowCallback(PASS_ENGINE_PARAMETER_SIGNATURE);
 #endif /* if EFI_ENGINE_CONTROL && EFI_SHAFT_POSITION_INPUT */
 
-	engine->gearController->update();
+	if (CONFIG(tcuEnabled)) {
+		engine->gearController->update();
+	}
 }
 
 void initPeriodicEvents(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
