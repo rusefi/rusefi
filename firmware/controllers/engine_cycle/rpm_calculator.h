eb46386c3615cc22e95fc27979fe6b106fe44530
rusefi <rusefillc@gmail.com>
2020-09-03 19:51:50 -0400

    refactoring

--- firmware/controllers/engine_cycle/rpm_calculator.h
+++ firmware/controllers/engine_cycle/rpm_calculator.h
@@ -102,11 +102,6 @@ public:
 	 */
 	float getRpmAcceleration() const;
 	/**
-	 * This is public because sometimes we cannot afford to call isRunning() and the value is good enough
-	 * Zero if engine is not running
-	 */
-	volatile int rpmValue = 0;
-	/**
 	 * this is RPM on previous engine cycle.
 	 */
 	int previousRpmValue = 0;
@@ -117,6 +112,11 @@ public:
 	volatile floatus_t oneDegreeUs = NAN;
 	volatile efitick_t lastRpmEventTimeNt = 0;
 private:
+	/**
+	 * Sometimes we cannot afford to call isRunning() and the value is good enough
+	 * Zero if engine is not running
+	 */
+	 int rpmValue = 0;
 	/**
 	 * Should be called once we've realized engine is not spinning any more.
 	 */
@@ -143,9 +143,6 @@ private:
 
 // Just a getter for rpmValue which also handles mockRpm if not EFI_PROD_CODE
 #define GET_RPM() ( ENGINE(rpmCalculator.getRpm(PASS_ENGINE_PARAMETER_SIGNATURE)) )
-
-// simple variable access, theoretically could be faster than getter method but that's a long stretch
-#define GET_RPM_VALUE ( ENGINE(rpmCalculator.rpmValue) )
 
 #define isValidRpm(rpm) ((rpm) > 0 && (rpm) < UNREALISTIC_RPM)
 
