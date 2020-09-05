5c01eb5ed98d219e12584cc9c5dbd1acd1c8bb59
David Holdeman <david@chuckwagoncomputing.com>
2020-09-05 15:49:08 -0500

    unit_tests macros

--- unit_tests/globalaccess.h
+++ unit_tests/globalaccess.h
@@ -23,4 +23,6 @@
 #define PASS_ENGINE_PARAMETER_SUFFIX , PASS_ENGINE_PARAMETER_SIGNATURE
 
 #define CONFIG(x) engineConfiguration->x
+#define CONFIG_POINTERS(x) &CONFIG(x), &activeConfiguration.x
+#define DECLARE_CONFIG_POINTERS(x, y) x *y, x *active_##y
 #define ENGINE(x) engine->x
