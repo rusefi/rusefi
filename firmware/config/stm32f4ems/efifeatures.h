41b5d5de0605e9e8580539ae4da485351cd0df7a
rusefi <rusefillc@gmail.com>
2020-09-07 12:24:50 -0400

    Revert "Hellen says efifeatures"
    This reverts commit 63eb179d

--- firmware/config/stm32f4ems/efifeatures.h
+++ firmware/config/stm32f4ems/efifeatures.h
@@ -69,10 +69,6 @@
 #define HAL_TRIGGER_USE_PAL FALSE
 #endif /* HAL_TRIGGER_USE_PAL */
 
-#ifndef HAL_TRIGGER_USE_ADC
-#define HAL_TRIGGER_USE_ADC FALSE
-#endif /* HAL_TRIGGER_USE_ADC */
-
 /**
  * TunerStudio support.
  */
@@ -148,12 +144,8 @@
 #define BOARD_TLE8888_COUNT 	1
 #endif
 
-#ifndef BOARD_DRV8860_COUNT
-#define BOARD_DRV8860_COUNT         0
-#endif
-
 // todo: move this outside of efifeatures.h
-#define BOARD_EXT_GPIOCHIPS			(BOARD_TLE6240_COUNT + BOARD_MC33972_COUNT + BOARD_TLE8888_COUNT + BOARD_DRV8860_COUNT)
+#define BOARD_EXT_GPIOCHIPS			(BOARD_TLE6240_COUNT + BOARD_MC33972_COUNT + BOARD_TLE8888_COUNT)
 
 // todo: move this outside of efifeatures.h
 #define BOARD_EXT_PINREPOPINS 24
@@ -249,8 +241,6 @@
 #ifndef EFI_USB_SERIAL
 #define EFI_USB_SERIAL TRUE
 #endif
-
-#define EFI_CONSOLE_USB_DEVICE (&SDU1)
 
 /**
  * While we embed multiple PnP configurations into the same firmware binary, these marcoses give us control
@@ -390,9 +380,6 @@
 
 #ifndef LED_ERROR_BRAIN_PIN
 #define LED_ERROR_BRAIN_PIN GPIOD_14
-#endif
-#ifndef LED_ERROR_BRAIN_PIN_MODE
-#define LED_ERROR_BRAIN_PIN_MODE DEFAULT_OUTPUT
 #endif
 
 // USART1 -> check defined STM32_SERIAL_USE_USART1
