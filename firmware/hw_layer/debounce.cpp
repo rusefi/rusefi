84880c3ae8bf6dd7827b692d6698a510f6377f3d
David Holdeman <david@chuckwagoncomputing.com>
2020-09-07 09:55:29 -0500

    fix few problemos

--- firmware/hw_layer/debounce.cpp
+++ firmware/hw_layer/debounce.cpp
@@ -30,14 +30,14 @@ void ButtonDebounce::init (int t, brain_pin_e p, pin_input_mode_e m) {
     active_mode = *m;
 #ifdef PAL_MODE_INPUT_PULLDOWN
     // getInputMode converts from pin_input_mode_e to iomode_t
-    efiSetPadMode("Button", active_pin, getInputMode(active_mode));
+    efiSetPadMode("Button", active_pin, getInputMode(&active_mode));
 #endif
     initialized = true;
 }
 
 void ButtonDebounce::updateConfigurationList () {
     ButtonDebounce *listItem = s_firstDebounce;
-    while (listItem != nulptr) {
+    while (listItem != nullptr) {
         listItem->updateConfiguration();
         if (listItem->nextDebounce != nullptr) {
             listItem = listItem->nextDebounce;
@@ -54,7 +54,7 @@ void ButtonDebounce::updateConfiguration () {
     if (*pin != active_pin || *mode != active_mode || (isActiveConfigurationVoid && (*pin != 0 || *mode != 0))) {
 #endif /* EFI_ACTIVE_CONFIGURATION_IN_FLASH */
         brain_pin_markUnused(active_pin);
-        efiSetPadMode("Button", *pin, getInputMode(*mode));
+        efiSetPadMode("Button", *pin, getInputMode(mode));
     }
 }
 
@@ -78,7 +78,7 @@ bool ButtonDebounce::readPinEvent() {
 #ifdef PAL_MODE_INPUT_PULLDOWN
     readValue = efiReadPin(oldPin);
     // Invert
-    if (getInputMode(*active_mode) == PAL_MODE_INPUT_PULLUP) {
+    if (getInputMode(&active_mode) == PAL_MODE_INPUT_PULLUP) {
         readValue = !readValue;
     }
 #endif
