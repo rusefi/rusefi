cab4cd0df2c82728d20d8659f32812d178bb4e26
David Holdeman <david@chuckwagoncomputing.com>
2020-09-08 10:29:38 -0500

    Debounce pin management (#1760)
    * add linked list
    
    * macros, and use in buttonshift
    
    * unit_tests macros
    
    * add extern
    
    * add parens
    
    * move extern
    
    * move extern
    
    * move buttonDebounceListHead
    
    * move buttonDebouncePointerHead
    
    * merge gore
    
    * undo
    
    * reduce unused size
    
    * don't store pointer if already initialized
    
    * few changes
    
    * remove oldPin
    
    * fix merge conflict
    
    * merge in changes commited to wrong branch
    
    * fix definition
    
    * out of class?
    
    * brute force programming
    
    * fix few problemos
    
    * am confuse
    
    * am confuse
    
    * am confuse
    
    * am confuse
    
    * oldPin snuck in
    
    * move to public?
    
    * define again
    
    * try constexpr
    
    * def in cpp
    
    * remove constexpr
    
    * fix def
    
    * fix?
    
    * update active
    
    * fix a few things

--- firmware/hw_layer/debounce.cpp
+++ firmware/hw_layer/debounce.cpp
@@ -6,16 +6,60 @@
  * @author David Holdeman, (c) 2020
  */
 #include "debounce.h"
+#include "pin_repository.h"
+#include "engine_configuration.h"
+#include "hardware.h"
 
-void ButtonDebounce::init (int t, brain_pin_e p, pin_input_mode_e m) {
+ButtonDebounce* ButtonDebounce::s_firstDebounce = nullptr;
+
+void ButtonDebounce::init (int t, brain_pin_e *pin, pin_input_mode_e *mode) {
+    if (!initialized) {
+        ButtonDebounce *listItem = s_firstDebounce;
+        if (listItem == nullptr) {
+            s_firstDebounce = this;
+        } else {
+            while (listItem->nextDebounce != nullptr) {
+                listItem = listItem->nextDebounce;
+            }
+            listItem->nextDebounce = this;
+        }
+    }
     threshold = MS2NT(t);
     timeLast = 0;
-    pin = p;
+    this->pin = pin;
+    active_pin = *pin;
+    this->mode = mode;
+    active_mode = *mode;
 #ifdef PAL_MODE_INPUT_PULLDOWN
     // getInputMode converts from pin_input_mode_e to iomode_t
-    mode = getInputMode(m);
-    efiSetPadMode("Button", p, mode);
+    efiSetPadMode("Button", active_pin, getInputMode(active_mode));
 #endif
+    initialized = true;
+}
+
+void ButtonDebounce::updateConfigurationList () {
+    ButtonDebounce *listItem = s_firstDebounce;
+    while (listItem != nullptr) {
+        listItem->updateConfiguration();
+        if (listItem->nextDebounce != nullptr) {
+            listItem = listItem->nextDebounce;
+        } else {
+            break;
+        }
+    }
+}
+
+void ButtonDebounce::updateConfiguration () {
+#ifndef EFI_ACTIVE_CONFIGURATION_IN_FLASH
+    if (*pin != active_pin || *mode != active_mode) {
+#else
+    if (*pin != active_pin || *mode != active_mode || (isActiveConfigurationVoid && (*pin != 0 || *mode != 0))) {
+#endif /* EFI_ACTIVE_CONFIGURATION_IN_FLASH */
+        brain_pin_markUnused(active_pin);
+        efiSetPadMode("Button", *pin, getInputMode(*mode));
+    }
+    active_pin = *pin;
+    active_mode = *mode;
 }
 
 /**
@@ -36,9 +80,9 @@ bool ButtonDebounce::readPinEvent() {
     //  for example to implement long button presses, it will be needed.
     readValue = false;
 #ifdef PAL_MODE_INPUT_PULLDOWN
-    readValue = efiReadPin(pin);
+    readValue = efiReadPin(active_pin);
     // Invert
-    if (mode != PAL_MODE_INPUT_PULLDOWN) {
+    if (getInputMode(active_mode) == PAL_MODE_INPUT_PULLUP) {
         readValue = !readValue;
     }
 #endif
