543eed1da94d9a4b0e1483ca36150fbbce59c886
Matthew Kennedy <matthewkennedy@outlook.com>
2020-09-09 12:04:20 -0700

    fiix

--- firmware/controllers/algo/rusefi_types.h
+++ firmware/controllers/algo/rusefi_types.h
@@ -103,6 +103,7 @@ typedef float fsio_table_8x8_f32t[FSIO_TABLE_8][FSIO_TABLE_8];
 typedef float tps_tps_table_t[TPS_TPS_ACCEL_TABLE][TPS_TPS_ACCEL_TABLE];
 typedef uint8_t fsio_table_8x8_u8t[FSIO_TABLE_8][FSIO_TABLE_8];
 typedef uint8_t boost_table_t[BOOST_LOAD_COUNT][BOOST_RPM_COUNT];
+typedef uint8_t boost_target_table_t[BOOST_LOAD_COUNT][BOOST_RPM_COUNT];
 typedef uint8_t gppwm_table_t[GPPWM_LOAD_COUNT][GPPWM_RPM_COUNT]; 
 
 
