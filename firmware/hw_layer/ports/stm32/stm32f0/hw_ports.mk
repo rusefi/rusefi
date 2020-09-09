729bf748eb708298ec52d9c73eedfc9d6dd9a480
rusefi <rusefillc@gmail.com>
2020-09-09 08:53:11 -0400

    Hellen says merge #1772 more ports

--- firmware/hw_layer/ports/stm32/stm32f0/hw_ports.mk
+++ firmware/hw_layer/ports/stm32/stm32f0/hw_ports.mk
@@ -1,4 +1,6 @@
+HW_LAYER_EGT = $(PROJECT_DIR)/hw_layer/ports/stm32/serial_over_usb/usbcfg.c \
+	$(PROJECT_DIR)/hw_layer/ports/stm32/serial_over_usb/usbconsole.c
 
-
-HW_STM32_SRC_CPP = $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f0/mpu_util.cpp
+HW_STM32_SRC_CPP = $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f0/mpu_util.cpp \
+	$(PROJECT_DIR)/hw_layer/ports/stm32/backup_ram.cpp
 	
