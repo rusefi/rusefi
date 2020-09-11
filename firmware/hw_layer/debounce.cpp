652bfbc97d72c94670d1a15f4b2adf9fce727f96
David Holdeman <david@chuckwagoncomputing.com>
2020-09-10 19:35:33 -0500

    add if

--- firmware/hw_layer/debounce.cpp
+++ firmware/hw_layer/debounce.cpp
@@ -86,7 +86,9 @@ bool ButtonDebounce::readPinState() {
     //  but when a method is implemented to actually get the pin's state,
     //  for example to implement long button presses, it will be needed.
     readValue = false;
+#if EFI_PROD_CODE || EFI_UNIT_TEST
     readValue = efiReadPin(active_pin);
+#endif
 #if EFI_PROD_CODE
     // Invert
     if (getInputMode(active_mode) == PAL_MODE_INPUT_PULLUP) {
