ad23b74bec13e4f6c38554149f2c4979a710fb62
David Holdeman <david@chuckwagoncomputing.com>
2020-09-15 19:04:31 -0500

    use efi::size

--- firmware/controllers/simple_tcu.cpp
+++ firmware/controllers/simple_tcu.cpp
@@ -1,15 +1,15 @@
 #include "simple_tcu.h"
 
-OutputPin tcuSolenoids[TCU_SOLENOID_COUNT];
+OutputPin tcuSolenoids[efi::size(tcu_solenoid)];
 
 void SimpleTransmissionController::SimpleTransmissionController() {
-    for (int i = 0; i < TCU_SOLENOID_COUNT; i++) {
+    for (int i = 0; i < efi::size(tcu_solenoid); i++) {
         tcuSolenoids[i].initPin("Transmission Solenoid", CONFIG(tcu_solenoid[i]), OM_DEFAULT);
     }
 }
 
 void SimpleTransmissionController::update(gear_e gear) {
-    for (int i = 0; i < TCU_SOLENOID_COUNT; i++) {
+    for (int i = 0; i < efi::size(tcu_solenoid); i++) {
         tcuSolenoids[i].setValue(CONFIG(tcuSolenoidTable)[i][static_cast<int>(gear)])
     }
     setCurrentGear(gear);
