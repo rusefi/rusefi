027937ac281b5c909156044c3aea12aab4448ec4
David Holdeman <david@chuckwagoncomputing.com>
2020-09-19 15:18:40 -0500

    comment

--- firmware/controllers/start_stop.cpp
+++ firmware/controllers/start_stop.cpp
@@ -6,5 +6,6 @@ EXTERN_ENGINE;
 ButtonDebounce startStopButtonDebounce;
 
 void initStartStopButton(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
+	/* startCrankingDuration is efitimesec_t, so we need to multiply by 1000 to get milliseconds */
 	startStopButtonDebounce.init((CONFIG(startCrankingDuration)*1000), &CONFIG(startStopButtonPin), &CONFIG(startStopButtonMode));
 }
