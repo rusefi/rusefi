99f2d5cadcbf444bf58acf9e57a6fed61355d5be
David Holdeman <david@chuckwagoncomputing.com>
2020-09-19 15:16:47 -0500

    Revert "comment"
    This reverts commit f7ce8ed48d015490ed82d692270198817569b5a3.

--- firmware/controllers/start_stop.cpp
+++ firmware/controllers/start_stop.cpp
@@ -6,6 +6,5 @@ EXTERN_ENGINE;
 ButtonDebounce startStopButtonDebounce;
 
 void initStartStopButton(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
-	/* startCrankingDuration is efitimesec_t, so we need to multiply it by 1000 to get milliseconds*/
 	startStopButtonDebounce.init((CONFIG(startCrankingDuration)*1000), &CONFIG(startStopButtonPin), &CONFIG(startStopButtonMode));
 }
