5944737cc8edee034fbde8056e79e9dc8686415f
Andrei <andreikagit@users.noreply.github.com>
2020-09-23 20:47:07 +0300

    fix compilation

--- firmware/config/boards/hellen/cypress/chconf.h
+++ firmware/config/boards/hellen/cypress/chconf.h
@@ -58,41 +58,7 @@
 #endif /* EFI_CLOCK_LOCKS */
 
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif /* __cplusplus */
- #ifndef __ASSEMBLER__
-  #if EFI_CLOCK_LOCKS
-    void irqEnterHook(void);
-    void irqExitHook(void);
-  #else /* EFI_CLOCK_LOCKS */
-    #define irqEnterHook() {}
-    #define irqExitHook() {}
-  #endif /*EFI_CLOCK_LOCKS */
- #endif /* __ASSEMBLER__ */
-#ifdef __cplusplus
-}
-#endif /* __cplusplus */
-
-#if EFI_CLOCK_LOCKS
-#ifdef __cplusplus
-extern "C"
-{
-#endif /* __cplusplus */
-#ifndef __ASSEMBLER__
-  void onLockHook(void);
-  void onUnlockHook(void);
-#endif /* __ASSEMBLER__ */
-#ifdef __cplusplus
-}
-#endif /* __cplusplus */
-  #define ON_LOCK_HOOK onLockHook()
-  #define ON_UNLOCK_HOOK onUnlockHook()
-#else /* EFI_CLOCK_LOCKS */
-  #define ON_LOCK_HOOK
-  #define ON_UNLOCK_HOOK
-#endif /* EFI_CLOCK_LOCKS */
+#include "chconf_common.h"
 
 /*===========================================================================*/
 /**
@@ -677,26 +643,6 @@ extern "C"
 }
 
 /**
- * @brief   Threads descriptor structure extension.
- * @details User fields added to the end of the @p thread_t structure.
- */
-#define CH_CFG_THREAD_EXTRA_FIELDS                                          \
-  void *activeStack; \
-  int remainingStack; \
-  /* Add threads custom fields here.*/
-
-/**
- * @brief   Threads initialization hook.
- * @details User initialization code added to the @p chThdInit() API.
- *
- * @note    It is invoked from within @p chThdInit() and implicitly from all
- *          the threads creation APIs.
- */
-#define CH_CFG_THREAD_INIT_HOOK(tp) {                                       \
-  /* Add threads initialization code here.*/                                \
-}
-
-/**
  * @brief   Threads finalization hook.
  * @details User finalization code added to the @p chThdExit() API.
  *
@@ -706,30 +652,6 @@ extern "C"
  */
 #define CH_CFG_THREAD_EXIT_HOOK(tp) {                                       \
   /* Add threads finalization code here.*/                                  \
-}
-
-/**
- * @brief   Context switch hook.
- * @details This hook is invoked just before switching between threads.
- */
-#define CH_CFG_CONTEXT_SWITCH_HOOK(ntp, otp) {                              \
-  /* Context switch code here.*/                                            \
-}
-
-/**
- * @brief   ISR enter hook.
- */
-#define CH_CFG_IRQ_PROLOGUE_HOOK() {                                        \
-  /* IRQ prologue code here.*/                                              \
-  irqEnterHook();                                                           \
-}
-
-/**
- * @brief   ISR exit hook.
- */
-#define CH_CFG_IRQ_EPILOGUE_HOOK() {                                        \
-  /* IRQ epilogue code here.*/                                              \
-  irqExitHook();                                                            \
 }
 
 /**
@@ -807,9 +729,9 @@ void chDbgPanic3(const char *msg, const char * file, int line);
  * so that it would not crash the error handler in case of stack issues
  */
 #if CH_DBG_SYSTEM_STATE_CHECK
-#define hasFatalError() (ch.dbg.panic_msg != NULL)
+#define hasOsPanicError() (ch.dbg.panic_msg != NULL)
 #else
-#define hasFatalError() (FALSE)
+#define hasOsPanicError() (FALSE)
 #endif
 
 
