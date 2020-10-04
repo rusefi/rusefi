7d8238e6976d5776c21d897bce825a34def836e6
rusefillc <sdfsdfqsf2334234234>
2020-10-03 21:36:09 -0400

    HW CI has failed 11 times since Oct 2 #1849
    Revert "configure all sample times - fix MRE knock sampling (#1841)"
    
    This reverts commit e02c86a7

--- firmware/config/boards/microrusefi/knock_config.h
+++ firmware/config/boards/microrusefi/knock_config.h
@@ -7,6 +7,14 @@
 #define KNOCK_ADC_CH1 ADC_CHANNEL_IN3
 #define KNOCK_PIN_CH1 GPIOA_3
 
+// knock 2 - pin PF5 NOT used! - not used for MRE
+#define KNOCK_ADC_CH2 ADC_CHANNEL_IN15
+#define KNOCK_PIN_CH2 GPIOF_5
+
 // Sample rate & time - depends on the exact MCU
 #define KNOCK_SAMPLE_TIME ADC_SAMPLE_84
 #define KNOCK_SAMPLE_RATE (STM32_PCLK2 / (4 * (84 + 12)))
+
+#ifndef MRE_SW_KNOCK_ADC
+    #define MRE_SW_KNOCK_ADC
+#endif
