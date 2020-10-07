bf5aded1d21bc7994c6cbb0dae7c2050dae53c34
David Holdeman <david@chuckwagoncomputing.com>
2020-10-07 16:21:24 -0500

    try

--- firmware/controllers/simple_tcu.cpp
+++ firmware/controllers/simple_tcu.cpp
@@ -7,15 +7,15 @@ EXTERN_CONFIG;
 OutputPin tcuSolenoids[efi::size(CONFIG(tcu_solenoid))];
 
 SimpleTransmissionController::SimpleTransmissionController() {
-/*    for (int i = 0; i < efi::size(CONFIG(tcu_solenoid)); i++) {
+    for (int i = 0; i < efi::size(CONFIG(tcu_solenoid)); i++) {
         tcuSolenoids[i].initPin("Transmission Solenoid", CONFIG(tcu_solenoid[i]), &CONFIG(tcu_solenoid_mode[i]));
-    }*/
+    }
 }
 
 void SimpleTransmissionController::update(gear_e gear) {
-/*    for (int i = 0; i < efi::size(CONFIG(tcu_solenoid)); i++) {
+    for (int i = 0; i < efi::size(CONFIG(tcu_solenoid)); i++) {
         tcuSolenoids[i].setValue(config->tcuSolenoidTable[i][static_cast<int>(gear)]);
-    }*/
+    }
     setCurrentGear(gear);
     postState();
 }
