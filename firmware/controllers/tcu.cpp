a6a0e8ba773f7a484616188ae140592aa8149505
David Holdeman <david@chuckwagoncomputing.com>
2020-10-09 22:34:45 -0500

    Simple transmission 2 (#1870)
    * simple trans controller
    
    * don't need check
    
    * we can loop!
    
    * use efi::size
    
    * pin mode configuration
    
    * set pin mode
    
    * switch to RegisteredOutputPin
    
    * size
    
    * add to makefile
    
    * fixes
    
    * fixes
    
    * wat
    
    * output pin, and ts stuff
    
    * use SimpleTransmissionController
    
    * include
    
    * grrr
    
    * grrr
    
    * move to separate file
    
    * fix includes
    
    * try
    
    * semicolon stupid
    
    * try pointers
    
    * try
    
    * add to makefile
    
    * wat
    
    * try
    
    * try
    
    * try
    
    * try
    
    * try
    
    * try
    
    * try
    
    * try
    
    * try
    
    * try
    
    * try
    
    * try
    
    * try
    
    * try
    
    * undo
    
    * more idiomatic maybe
    
    * remove logger
    
    * try init func
    
    * try
    
    * try
    
    * try init fn
    
    * try
    
    * switch
    
    * try
    
    * try
    
    * ifndef
    
    * try
    
    * try
    
    * ifndef
    
    * use def dug
    
    * INJECT FIRST
    
    * try
    
    * try
    
    * duh

--- firmware/controllers/tcu.cpp
+++ firmware/controllers/tcu.cpp
@@ -28,23 +28,3 @@ void TransmissionControllerBase::postState() {
     tsOutputChannels.tcuCurrentGear = getCurrentGear();
 #endif
 }
-
-void GearControllerBase::update() {
-    transmissionController.update(getDesiredGear());
-    postState();
-}
-
-gear_e GearControllerBase::getDesiredGear() const {
-    return desiredGear;
-}
-
-gear_e GearControllerBase::setDesiredGear(gear_e gear) {
-    desiredGear = gear;
-    return getDesiredGear();
-}
-
-void GearControllerBase::postState() {
-#if EFI_TUNER_STUDIO
-    tsOutputChannels.tcuDesiredGear = getDesiredGear();
-#endif
-}
