e0bb7ad5811c3966d9755bbe5b8dc7d88d3c6ecf
Matthew Kennedy <matthewkennedy@outlook.com>
2020-10-20 19:24:32 -0700

    disable i2c, other unused features (#1891)
    * dead
    
    * more

--- firmware/hw_layer/lcd/lcd_HD44780.cpp
+++ firmware/hw_layer/lcd/lcd_HD44780.cpp
@@ -106,19 +106,7 @@ static void lcd_HD44780_write(uint8_t data) {
 		//	LCD Pin RW -> P1
 		//	LCD Pin E  -> P2
 
-#if HAL_USE_I2C
 		// todo: finish all this stuff
-				i2cAcquireBus(&I2CD1);
-//
-//				txbuf[0] = 4;
-//				i2cMasterTransmit(&I2CD1, LCD_PORT_EXP_ADDR, txbuf, 1, NULL, 0);
-//				lcdSleep(10); // enable pulse must be >450ns
-//
-//				txbuf[0] = 0;
-//				i2cMasterTransmit(&I2CD1, LCD_PORT_EXP_ADDR, txbuf, 1, NULL, 0);
-//
-				i2cReleaseBus(&I2CD1);
-#endif /* HAL_USE_I2C */
 	}
 }
 
