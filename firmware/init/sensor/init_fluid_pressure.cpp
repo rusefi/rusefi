fb62c5708b5178b99d2d7033aaaba15294858499
Matthew Kennedy <matthewkennedy@outlook.com>
2020-10-23 02:45:34 -0700

    add

---
+++ firmware/init/sensor/init_fluid_pressure.cpp
@@ -0,0 +1,57 @@
+#include "init.h"
+#include "adc_subscription.h"
+#include "engine.h"
+#include "error_handling.h"
+#include "global.h"
+#include "functional_sensor.h"
+#include "linear_func.h"
+
+EXTERN_ENGINE;
+
+static LinearFunc oilpSensorFunc;
+static FunctionalSensor oilpSensor(SensorType::OilPressure, /* timeout = */ MS2NT(50));
+
+static LinearFunc fuelPressureFuncLow;
+static FunctionalSensor fuelPressureSensorLow(SensorType::FuelPressureLow, /* timeout = */ MS2NT(50));
+
+static LinearFunc fuelPressureFuncHigh;
+static FunctionalSensor fuelPressureSensorHigh(SensorType::FuelPressureHigh, /* timeout = */ MS2NT(50));
+
+void configureFluidPressure(LinearFunc& func, const linear_sensor_s& cfg) {
+	float val1 = cfg.value1;
+	float val2 = cfg.value2;
+
+	// Limit to max given pressure - val1 or val2 could be larger
+	// (sensor may be backwards, high voltage = low pressure)
+	float greaterOutput = val1 > val2 ? val1 : val2;
+
+	// Allow slightly negative output (-5kpa) so as to not fail the sensor when engine is off
+	func.configure(cfg.v1, val1, cfg.v2, val2, /*minOutput*/ -5, greaterOutput);
+}
+
+void initFluidPressure(LinearFunc& func, FunctionalSensor& sensor, const linear_sensor_s& cfg, float bandwidth) {
+	auto channel = cfg.channel;
+	// Only register if we have a sensor
+	if (channel == EFI_ADC_NONE) {
+		return;
+	}
+
+	configureFluidPressure(func, cfg);
+	sensor.setFunction(func);
+
+	AdcSubscription::SubscribeSensor(sensor, channel, bandwidth);
+
+	if (!sensor.Register()) {
+		firmwareError(CUSTOM_INVALID_TPS_SETTING, "Duplicate registration for sensor \"%s\"", sensor.getSensorName());
+	}
+}
+
+void initOilPressure(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
+	initFluidPressure(oilpSensorFunc, oilpSensor, CONFIG(oilPressure));
+	initFluidPressure(fuelPressureFuncLow, fuelPressureSensorLow, CONFIG(oilPressure));
+	initFluidPressure(fuelPressureFuncHigh, fuelPressureSensorHigh, CONFIG(oilPressure));
+}
+
+void reconfigureOilPressure(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
+	configureFluidPressure(oilpSensorFunc, CONFIG(oilPressure));
+}
