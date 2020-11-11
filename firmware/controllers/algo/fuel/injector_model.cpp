0fa06e2e2a2141adbb48d6ec93d5f346d1361b03
Matthew Kennedy <matthewkennedy@outlook.com>
2020-11-10 20:11:22 -0800

    compensate injector flow based on fuel pressure (#1931)
    * injector compensation
    
    * fix
    
    * ui
    
    * testability & comments
    
    * hella testing
    
    * debug channels
    
    * enum value
    
    * auto generated enums
    
    * update comment
    
    * oops too much
    
    * update config fields
    
    * handle failed pressure sensor
    
    * build
    
    * fix
    
    * add some todos for the future
    
    Co-authored-by: Matthew Kennedy <makenne@microsoft.com>

--- firmware/controllers/algo/fuel/injector_model.cpp
+++ firmware/controllers/algo/fuel/injector_model.cpp
@@ -1,4 +1,6 @@
 #include "injector_model.h"
+#include "tunerstudio_outputs.h"
+#include "map.h"
 
 EXTERN_ENGINE;
 
@@ -15,10 +17,58 @@ constexpr float convertToGramsPerSecond(float ccPerMinute) {
 	return ccPerSecond * 0.72f;	// 0.72g/cc fuel density
 }
 
+expected<float> InjectorModel::getAbsoluteRailPressure() const {
+	switch (CONFIG(injectorCompensationMode)) {
+		case ICM_FixedRailPressure:
+			// TODO: should this add baro pressure instead of 1atm?
+			return (CONFIG(fuelReferencePressure) + 101.325f);
+		case ICM_SensedRailPressure:
+			// TODO: what happens when the sensor fails?
+			return Sensor::get(SensorType::FuelPressureInjector);
+		default: return unexpected;
+	}
+}
+
+float InjectorModel::getInjectorFlowRatio() const {
+	// Compensation disabled, use reference flow.
+	if (CONFIG(injectorCompensationMode) == ICM_None) {
+		return 1.0f;
+	}
+
+	float referencePressure = CONFIG(fuelReferencePressure);
+	expected<float> absRailPressure = getAbsoluteRailPressure();
+
+	// If sensor failed, best we can do is disable correction
+	if (!absRailPressure) {
+		return 1.0f;
+	}
+
+	float map = getMap(PASS_ENGINE_PARAMETER_SIGNATURE);
+
+	// TODO: what to do when pressureDelta is less than 0?
+	float pressureDelta = absRailPressure.Value - map;
+	float pressureRatio = pressureDelta / referencePressure;
+	float flowRatio = sqrtf(pressureRatio);
+
+#if EFI_TUNER_STUDIO
+	if (CONFIG(debugMode) == DBG_INJECTOR_COMPENSATION) {
+		tsOutputChannels.debugFloatField1 = pressureDelta;
+		tsOutputChannels.debugFloatField2 = pressureRatio;
+		tsOutputChannels.debugFloatField3 = flowRatio;
+	}
+#endif // EFI_TUNER_STUDIO
+
+	// TODO: should the flow ratio be clamped?
+	return flowRatio;
+}
+
 float InjectorModel::getInjectorMassFlowRate() const {
-	// TODO: injector flow dependent upon rail pressure (and temperature/ethanol content?)
+	// TODO: injector flow dependent upon temperature/ethanol content?
 	auto injectorVolumeFlow = CONFIG(injector.flow);
-	return convertToGramsPerSecond(injectorVolumeFlow);
+
+	float flowRatio = getInjectorFlowRatio();
+
+	return flowRatio * convertToGramsPerSecond(injectorVolumeFlow);
 }
 
 float InjectorModel::getDeadtime() const {
