35754d298ad38825235611e45d4e9b1f26a36402
rusefillc <sdfsdfqsf2334234234>
2020-11-18 19:34:09 -0500

    Starter seems to be engaged forever #1965

--- unit_tests/tests/tests.mk
+++ unit_tests/tests/tests.mk
@@ -16,6 +16,7 @@ TESTS_SRC_CPP = \
 	tests/ignition_injection/test_injector_model.cpp \
 	tests/util/test_buffered_writer.cpp \
 	tests/test_util.cpp \
+	tests/test_start_stop.cpp \
 	tests/test_hardware_reinit.cpp \
 	tests/test_ion.cpp \
 	tests/test_aux_valves.cpp \
