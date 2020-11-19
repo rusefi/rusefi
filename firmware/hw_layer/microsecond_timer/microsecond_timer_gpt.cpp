bb010bc37ab061f255194e9c6ee351b956745227
Matthew Kennedy <matthewkennedy@outlook.com>
2020-11-18 16:08:20 -0800

    simulator build

--- firmware/hw_layer/microsecond_timer/microsecond_timer_gpt.cpp
+++ firmware/hw_layer/microsecond_timer/microsecond_timer_gpt.cpp
@@ -1,6 +1,8 @@
 #include "global.h"
 #include "port_microsecond_timer.h"
 
+#if EFI_PROD_CODE
+
 void portSetHardwareSchedulerTimer(efitick_t nowNt, efitick_t setTimeNt) {
 	int32_t deltaTimeUs = NT2US((int32_t)setTimeNt - (int32_t)nowNt);
 
@@ -34,6 +36,8 @@ void portInitMicrosecondTimer() {
 	gptStart(&GPTDEVICE, &gpt5cfg);
 	efiAssertVoid(CUSTOM_ERR_TIMER_STATE, GPTDEVICE.state == GPT_READY, "hw state");
 }
+
+#endif // EFI_PROD_CODE
 
 // This implementation just uses the generic port counter - this usually returns a count of CPU cycles since start
 uint32_t getTimeNowLowerNt() {
