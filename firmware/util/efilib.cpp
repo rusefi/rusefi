747b3d2fa862013b0b8fa6cbc462e11c20c34a33
Matthew Kennedy <matthewkennedy@outlook.com>
2020-11-22 05:26:17 -0800

    fix #1968 (#1976)
    * tanf
    
    * name
    
    * comment, add test

--- firmware/util/efilib.cpp
+++ firmware/util/efilib.cpp
@@ -371,3 +371,29 @@ float expf_taylor(float x)
 {
 	return expf_taylor_impl(x, 4);
 }
+
+float tanf_taylor(float x) {
+	// This exists because the "normal" implementation, tanf, pulls in like 6kb of
+	// code and loookup tables
+
+	// This is only specified from [0, pi/2 - 0.01)
+	// Inside that range it has an error of less than 0.1%, and it gets worse as theta -> pi/2
+
+	// Precompute some exponents of x
+	float x2 = x * x;
+	float x3 = x2 * x;
+	float x4 = x3 * x;
+	float x5 = x4 * x;
+	float x6 = x5 * x;
+	// x7 not used
+	float x8 = x6 * x2;
+
+	// 3-term Taylor Series for sin(theta)
+	float sin_val = x - (x3 / 6) + (x5 / 120);
+
+	// 5-term Taylor Series for cos(theta)
+	float cos_val = 1 - (x2 / 2) + (x4 / 24) - (x6 / 720) + (x8 / 40320);
+
+	// tan = sin / cos
+	return sin_val / cos_val;
+}
