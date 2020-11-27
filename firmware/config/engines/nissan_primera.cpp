334f0a5a8a65335898f5b52a564c7b00006078cd
rusefillc <sdfsdfqsf2334234234>
2020-11-26 21:07:34 -0500

    dead engine config

--- firmware/config/engines/nissan_primera.cpp
+++ firmware/config/engines/nissan_primera.cpp
@@ -14,20 +14,6 @@
 
 EXTERN_CONFIG;
 
-void setNissanPrimeraEngineConfiguration(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
-	setDefaultFrankensoConfiguration(PASS_CONFIG_PARAMETER_SIGNATURE);
-
-	engineConfiguration->trigger.type = TT_NISSAN_SR20VE;
-
-	engineConfiguration->ignitionPins[0] = GPIOD_7;
-	engineConfiguration->ignitionPins[1] = GPIO_UNASSIGNED;
-	engineConfiguration->ignitionPins[2] = GPIOD_6;
-
-
-	engineConfiguration->auxValves[0] = GPIOE_14;
-	engineConfiguration->auxValves[1] = GPIOE_12;
-}
-
 void setNissanPrimeraEngineConfiguration_360(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
 	engineConfiguration->trigger.type = TT_NISSAN_SR20VE_360;
 }
