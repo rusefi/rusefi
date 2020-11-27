f15efa7f1561a15c50c5467aac0b981ef4d0f570
rusefillc <sdfsdfqsf2334234234>
2020-11-26 20:53:44 -0500

    dead engine config

--- unit_tests/tests/trigger/test_trigger_decoder.cpp
+++ unit_tests/tests/trigger/test_trigger_decoder.cpp
@@ -525,7 +525,6 @@ TEST(misc, testTriggerDecoder) {
 
 	}
 	testTriggerDecoder2("miata 1990", MIATA_1990, 11, 0.2985, 0.3890);
-	testTriggerDecoder3("miata 1994", MIATA_1994_DEVIATOR, 11, 0.2985, 0.3890, MIATA_NA_GAP);
 	testTriggerDecoder3("citroen", CITROEN_TU3JP, 0, 0.4833, 0.0, 2.9994);
 
 	testTriggerDecoder2("CAMARO_4", CAMARO_4, 40, 0.5, 0);
