76342bab8b5f4719d07c02c9eccd5ddfddb96713
Matthew Kennedy <matthewkennedy@outlook.com>
2020-12-09 19:11:55 -0800

    fix

--- firmware/hw_layer/io_pins.h
+++ firmware/hw_layer/io_pins.h
@@ -29,5 +29,11 @@ EXTERNC iomode_t getInputMode(pin_input_mode_e mode);
 #if HAL_USE_ICU
 EXTERNC void efiIcuStart(const char *msg, ICUDriver *icup, const ICUConfig *config);
 #endif /* HAL_USE_ICU */
+
+#if !EFI_PROD_CODE
+#define BRAIN_PIN_COUNT (1 << 8 * sizeof(brain_pin_e))
+extern bool mockPinStates[BRAIN_PIN_COUNT];
+#endif
+
 #endif /* EFI_GPIO_HARDWARE */
 
