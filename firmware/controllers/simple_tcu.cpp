1b59375425455b3ea8692bac4ae53a21d2f8cee3
David Holdeman <david@chuckwagoncomputing.com>
2020-12-12 14:41:27 -0600

    Fix table (#2062)
    * try
    
    * try
    
    * swap labels
    
    * switch axes in code

--- firmware/controllers/simple_tcu.cpp
+++ firmware/controllers/simple_tcu.cpp
@@ -15,7 +15,7 @@ void SimpleTransmissionController::init() {
 void SimpleTransmissionController::update(gear_e gear) {
     for (size_t i = 0; i < efi::size(CONFIG(tcu_solenoid)); i++) {
 #ifndef EFI_UNIT_TEST
-        tcuSolenoids[i].setValue(config->tcuSolenoidTable[static_cast<int>(gear) + 1][i]);
+        tcuSolenoids[i].setValue(config->tcuSolenoidTable[i][static_cast<int>(gear) + 1]);
 #endif
     }
     setCurrentGear(gear);
