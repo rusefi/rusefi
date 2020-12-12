7851dec07b49f702568226e57aae473629bd7e48
rusefillc <sdfsdfqsf2334234234>
2020-12-12 17:28:27 -0500

    basic hw in the loop - pwm self validation #2028

--- java_console/autotest/src/com/rusefi/RealHwTest.java
+++ java_console/autotest/src/com/rusefi/RealHwTest.java
@@ -1,11 +1,7 @@
 package com.rusefi;
 
 import com.devexperts.logging.Logging;
-import com.rusefi.autodetect.PortDetector;
-import com.rusefi.config.generated.Fields;
-import com.rusefi.core.EngineState;
 import com.rusefi.io.LinkManager;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -27,7 +23,6 @@ import static com.rusefi.Timeouts.SECOND;
 public class RealHwTest {
     private static final Logging log = getLogging(RealHwTest.class);
     private static final int STARTUP_SLEEP = 20;
-    private volatile static String firmwareVersion;
 
     public static void main(String[] args) throws InterruptedException {
         log.info("Sleeping " + STARTUP_SLEEP + " seconds to give OS time to connect VCP driver");
@@ -46,21 +41,16 @@ public class RealHwTest {
      * @return true if test is a SUCCESS, false if a FAILURE
      */
     public static boolean runHardwareTest(String[] args) {
-        String port = startRealHardwareTest(args);
-        if (port == null) {
-            return false;
-        } else {
-            return runHardwareTest(port);
-        }
+        return runHardwareTest();
     }
 
     /**
      * @return true if test is a SUCCESS, false if a FAILURE
      */
-    public static boolean runHardwareTest(String port) {
+    public static boolean runHardwareTest() {
         long start = System.currentTimeMillis();
         try {
-            runRealHardwareTest(port);
+            runRealHardwareTest(ControllerConnectorState.getLinkManager());
         } catch (Throwable e) {
             StringWriter sw = new StringWriter();
             PrintWriter pw = new PrintWriter(sw);
@@ -74,38 +64,10 @@ public class RealHwTest {
         return true;
     }
 
-    static String startRealHardwareTest(@NotNull String[] args) {
-        /**
-         * with real hardware we have noise on all analog inputs which gives us random sensor data, we cannot really
-         * test exact numbers yet
-         */
-        TestingUtils.isRealHardware = true;
-        FileLog.MAIN.start();
-        String port;
-        if (args.length == 1 || args.length == 2) {
-            port = args[0];
-        } else if (args.length == 0) {
-            port = PortDetector.autoDetectPort(null);
-        } else {
-            System.out.println("Only one optional argument expected: port number");
-            port = null;
-        }
-        return port;
-    }
-
-    private static void runRealHardwareTest(String port) {
-        LinkManager linkManager = new LinkManager().setCompositeLogicEnabled(false);
-        linkManager.getEngineState().registerStringValueAction(Fields.PROTOCOL_VERSION_TAG, new EngineState.ValueCallback<String>() {
-            @Override
-            public void onUpdate(String firmwareVersion) {
-                RealHwTest.firmwareVersion = firmwareVersion;
-            }
-        });
-
-        IoUtil.realHardwareConnect(linkManager, port);
+    private static void runRealHardwareTest(LinkManager linkManager) {
         // first run tests which require real hardware
         new HardwareTests(linkManager.getCommandQueue()).runRealHardwareTests();
-        if (firmwareVersion == null)
+        if (ControllerConnectorState.firmwareVersion == null)
             throw new IllegalStateException("firmwareVersion has not arrived");
 
         // now run common part of the test which should be same on real hardware and simulator
