6d2b15949f9ab865bc193ba87c3fede6f3fd3ee0
Matthew Kennedy <matthewkennedy@outlook.com>
2020-12-17 23:56:22 -0800

    index

--- firmware/controllers/actuators/aux_pid.cpp
+++ firmware/controllers/actuators/aux_pid.cpp
@@ -133,7 +133,7 @@ void startAuxPins() {
 
 void stopAuxPins() {
 	for (int i = 0;i < AUX_PID_COUNT;i++) {
-		instances[index].auxOutputPin.unregister();
+		instances[i].auxOutputPin.unregister();
 	}
 }
 
