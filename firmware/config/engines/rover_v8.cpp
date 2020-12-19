1e5be46b4ce9761c66712fd4de7ad815c7ba24bd
rusefillc <sdfsdfqsf2334234234>
2020-12-19 15:18:23 -0500

    something ? HIP / SPI pin conflict does not fail gracefully #2108

--- firmware/config/engines/rover_v8.cpp
+++ firmware/config/engines/rover_v8.cpp
@@ -21,8 +21,8 @@ void setFrankenstein_01_LCD(engine_configuration_s *engineConfiguration) {
 	engineConfiguration->HD44780_e = GPIOE_11;
 	engineConfiguration->HD44780_db4 = GPIOE_13;
 	engineConfiguration->HD44780_db5 = GPIOE_15;
-	engineConfiguration->HD44780_db6 = GPIOB_11;
-	engineConfiguration->HD44780_db7 = GPIOB_13;
+	engineConfiguration->HD44780_db6 = GPIO_UNASSIGNED; // todo#2108 GPIOB_11;
+	engineConfiguration->HD44780_db7 = GPIO_UNASSIGNED; // todo#2108 GPIOB_13;
 }
 
 EXTERN_CONFIG;
@@ -61,8 +61,8 @@ void setRoverv8(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
 */
 
     // set_ignition_channels
-    engineConfiguration->ignitionPins[0] = GPIOE_8; // Frankenstein: low side - out #x (?)
-    engineConfiguration->ignitionPins[7] = GPIOE_10; // Frankenstein: low side - out #x (?)
+    engineConfiguration->ignitionPins[0] = GPIO_UNASSIGNED; // todo#2108 GPIOE_8; // Frankenstein: low side - out #x (?)
+    engineConfiguration->ignitionPins[7] = GPIO_UNASSIGNED; // todo#2108 GPIOE_10; // Frankenstein: low side - out #x (?)
     engineConfiguration->ignitionPins[3] = GPIOE_12; // Frankenstein: low side - out #x (?)
     engineConfiguration->ignitionPins[2] = GPIOE_14; // Frankenstein: low side - out #x (?)
 
@@ -79,10 +79,10 @@ void setRoverv8(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
 	// Frankenstein: low side - out #11: PB8
 	// Frankenstein: low side - out #12: PB9
 
-	engineConfiguration->injectionPins[0] = GPIOC_14; // Frankenstein: low side - out #1
-	engineConfiguration->injectionPins[1] = GPIOC_15; // Frankenstein: low side - out #2
+	engineConfiguration->injectionPins[0] = GPIO_UNASSIGNED; // todo#2108 GPIOC_14; // Frankenstein: low side - out #1
+	engineConfiguration->injectionPins[1] = GPIO_UNASSIGNED; // todo#2108 GPIOC_15; // Frankenstein: low side - out #2
 	engineConfiguration->injectionPins[2] = GPIOE_6; // Frankenstein: low side - out #3
-	engineConfiguration->injectionPins[3] = GPIOC_13; // Frankenstein: low side - out #4
+	engineConfiguration->injectionPins[3] = GPIO_UNASSIGNED; // todo#2108 GPIOC_13; // Frankenstein: low side - out #4
     engineConfiguration->injectionPins[4] = GPIOE_4; // Frankenstein: low side - out #5
     engineConfiguration->injectionPins[5] = GPIOE_5; // Frankenstein: low side - out #6
     engineConfiguration->injectionPins[6] = GPIOE_2; // Frankenstein: low side - out #7
@@ -94,9 +94,9 @@ void setRoverv8(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
 	//no malfunction indicator pin needed, since we use CAN_BUS_MAZDA_RX8
 	//engineConfiguration->fuelPumpPin = GPIOE_0; // Frankenstein: low side - out #9
 	//engineConfiguration->malfunctionIndicatorPin = GPIOE_1; // Frankenstein: low side - out #10
-	engineConfiguration->fuelPumpPin = GPIOB_8; // Frankenstein: low side - out #11
+	engineConfiguration->fuelPumpPin = GPIO_UNASSIGNED; // todo#2108 GPIOB_8; // Frankenstein: low side - out #11
 	engineConfiguration->fuelPumpPinMode = OM_DEFAULT;
-	engineConfiguration->mainRelayPin  = GPIOB_9; // Frankenstein: low side - out #12
+	engineConfiguration->mainRelayPin  = GPIO_UNASSIGNED; // todo#2108 GPIOB_9; // Frankenstein: low side - out #12
 
     engineConfiguration->triggerInputPins[0] = GPIOC_6; // 2G YEL/BLU
     engineConfiguration->triggerInputPins[1] = GPIOA_5; // 2E White CKP
@@ -117,9 +117,9 @@ void setRoverv8(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
     engineConfiguration->tpsMax = convertVoltageTo10bitADC(4.538);
 
     // Stepper logic:
-    engineConfiguration->idle.stepperDirectionPin = GPIOB_10;
-    engineConfiguration->idle.stepperStepPin = GPIOB_15;
-    engineConfiguration->stepperEnablePin = GPIOB_14;
+    engineConfiguration->idle.stepperDirectionPin = GPIO_UNASSIGNED; // todo#2108 GPIOB_10;
+    engineConfiguration->idle.stepperStepPin = GPIO_UNASSIGNED; // todo#2108 GPIOB_15;
+    engineConfiguration->stepperEnablePin = GPIO_UNASSIGNED; // todo#2108 GPIOB_14;
     engineConfiguration->idleStepperReactionTime = 10;
     engineConfiguration->idleStepperTotalSteps = 150;
 
@@ -138,10 +138,10 @@ void setRoverv8(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
     setAlgorithm(LM_SPEED_DENSITY PASS_CONFIG_PARAMETER_SUFFIX);
 
     // todo: make this official Frankenstein joystick?
-    engineConfiguration->joystickCenterPin = GPIOD_8;
-    engineConfiguration->joystickAPin = GPIOD_10;
+    engineConfiguration->joystickCenterPin = GPIO_UNASSIGNED; // todo#2108 GPIOD_8;
+    engineConfiguration->joystickAPin = GPIO_UNASSIGNED; // todo#2108 GPIOD_10;
     engineConfiguration->joystickBPin = GPIO_UNASSIGNED;
     engineConfiguration->joystickCPin = GPIO_UNASSIGNED;
-    engineConfiguration->joystickDPin = GPIOD_11;
+    engineConfiguration->joystickDPin = GPIO_UNASSIGNED; // todo#2108 GPIOD_11;
 
 }
