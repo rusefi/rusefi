4854f254f023966bf3cca3dcb48087afaedc1243
GitHub build-firmware Action <action@github.com>
2020-12-22 22:24:02 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen_cypress.h
+++ firmware/controllers/generated/signature_hellen_cypress.h
@@ -4,5 +4,5 @@
 
 #define SIGNATURE_BOARD hellen_cypress
 #define SIGNATURE_DATE 2020.12.22
-#define SIGNATURE_HASH 2013371550
-#define TS_SIGNATURE "rusEFI 2020.12.22.hellen_cypress.2013371550"
+#define SIGNATURE_HASH 3930481456
+#define TS_SIGNATURE "rusEFI 2020.12.22.hellen_cypress.3930481456"
