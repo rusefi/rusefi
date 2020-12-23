4c347026fff5056536ee865bbe3b4909e7d953fe
rusefi <rusefillc@gmail.com>
2020-12-22 21:28:51 -0500

    Proteus is compiled without VSS feature #2122

--- firmware/hw_layer/vehicle_speed.cpp
+++ firmware/hw_layer/vehicle_speed.cpp
@@ -90,6 +90,7 @@ void startVSSPins(void) {
 		return;
 	}
 
+// todo: refactor https://github.com/rusefi/rusefi/issues/2123
 #if HAL_VSS_USE_PAL
 	ioline_t pal_line = PAL_LINE(getHwPort("vss", CONFIG(vehicleSpeedSensorInputPin)), getHwPin("vss", CONFIG(vehicleSpeedSensorInputPin)));
 	efiExtiEnablePin("VSS", CONFIG(vehicleSpeedSensorInputPin), PAL_EVENT_MODE_BOTH_EDGES, vsExtiCallback, (void *)pal_line);
