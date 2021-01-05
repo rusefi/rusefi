9a340569615c8fbb631f97ffd1679ec04c0bca26
Matthew Kennedy <makenne@microsoft.com>
2021-01-05 01:36:50 -0800

    fix

--- firmware/init/sensor/init_flex.cpp
+++ firmware/init/sensor/init_flex.cpp
@@ -1,12 +1,15 @@
 #include "init.h"
+#include "engine.h"
 
 EXTERN_ENGINE;
 
 // https://rusefi.com/forum/viewtopic.php?p=37452&sid=829804c90d5b2e1fecd1b900cf1b1811#p37452
 
 void initFlexSensor(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
+	auto pin = CONFIG(flexSensorPin);
+
 	// Nothing to do if no sensor configured
-	if (CONFIG(flexSensorPin) == GPIO_UNASSIGNED) {
+	if (pin == GPIO_UNASSIGNED) {
 		return;
 	}
 
