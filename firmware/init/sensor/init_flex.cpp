54c10ed39563a5d8ba1f140779754b90e3acbb67
Matthew Kennedy <makenne@microsoft.com>
2021-01-05 01:14:40 -0800

    linky linky

--- firmware/init/sensor/init_flex.cpp
+++ firmware/init/sensor/init_flex.cpp
@@ -2,6 +2,8 @@
 
 EXTERN_ENGINE;
 
+// https://rusefi.com/forum/viewtopic.php?p=37452&sid=829804c90d5b2e1fecd1b900cf1b1811#p37452
+
 void initFlexSensor(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
 	// Nothing to do if no sensor configured
 	if (CONFIG(flexSensorPin) == GPIO_UNASSIGNED) {
