8ba672637ac1ec862df0ceb98f02713d6fdbce1e
Matthew Kennedy <makenne@microsoft.com>
2021-01-05 01:44:25 -0800

    add sensor & init

---
+++ firmware/controllers/sensors/flex_sensor.cpp
@@ -0,0 +1,37 @@
+#include "engine.h"
+#include "digital_input_exti.h"
+#include "flex_sensor.h"
+
+EXTERN_ENGINE;
+
+static void flexExtiCallback(void* arg) {
+	auto inst = reinterpret_cast<FlexFuelSensor*>(arg);
+	inst->onEdge(getTimeNowNt());
+}
+
+void FlexFuelSensor::init(brain_pin_e pin) {
+#if EFI_PROD_CODE
+	// todo: refactor https://github.com/rusefi/rusefi/issues/2123
+	efiExtiEnablePin("flex", pin, 
+		PAL_EVENT_MODE_FALLING_EDGE,
+		flexExtiCallback, reinterpret_cast<void*>(this));
+#endif // EFI_PROD_CODE
+}
+
+FlexFuelSensor::FlexFuelSensor()
+	// this long timeout is ok, ethanol content isn't going to change quickly
+	: StoredValueSensor(SensorType::FuelEthanolPercent, MS2NT(500))
+{
+}
+
+void FlexFuelSensor::onEdge(efitick_t nowNt) {
+	float frequency = 1 / m_edgeTimer.getElapsedSecondsAndReset(nowNt);
+
+	// TODO: are these limits reasonable?
+	if (frequency > 45 && frequency < 155) {
+		setValidValue(frequency - 50, nowNt);
+	}
+
+	// no explicit invalidation, we can tolerate a somewhat dodgy connection
+	// so long as we get a valid interval every now and then
+}
