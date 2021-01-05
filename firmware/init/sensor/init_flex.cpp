20a967edf1f48d92320f978d0d244051db141b9d
Matthew Kennedy <matthewkennedy@outlook.com>
2021-01-05 14:13:56 -0800

    Implement flex sensor (#2189)
    * config & ui
    
    * stub sensor
    
    * stub init
    
    * output channel, gauge, SD log
    
    * linky linky
    
    * fix
    
    * add sensor & init
    
    * clamp
    
    * update comment
    
    * filter the resultant signal
    
    * changelog
    
    Co-authored-by: Matthew Kennedy <makenne@microsoft.com>

--- firmware/init/sensor/init_flex.cpp
+++ firmware/init/sensor/init_flex.cpp
@@ -1,7 +1,10 @@
 #include "init.h"
 #include "engine.h"
+#include "flex_sensor.h"
 
 EXTERN_ENGINE;
+
+static FlexFuelSensor flexSensor;
 
 // https://rusefi.com/forum/viewtopic.php?p=37452&sid=829804c90d5b2e1fecd1b900cf1b1811#p37452
 
@@ -13,5 +16,6 @@ void initFlexSensor(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
 		return;
 	}
 
-	// TODO: init sensor
+	flexSensor.init(pin);
+	flexSensor.Register();
 }
