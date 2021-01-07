27e944f2bfa1efec38a5c82b7f02f0bd7da319c7
GitHub build-firmware Action <action@github.com>
2021-01-07 13:25:37 +0000

    Auto-generated configs and docs

--- firmware/config/boards/kinetis/config/controllers/algo/rusefi_generated.h
+++ firmware/config/boards/kinetis/config/controllers/algo/rusefi_generated.h
@@ -292,7 +292,6 @@
 #define CMD_TS_INJECTOR_CATEGORY 19
 #define CMD_TS_SOLENOID_CATEGORY 41
 #define CMD_TS_X14 20
-#define CMD_TS_X15 21
 #define CMD_TS_X17 23
 #define CMD_TS_X18 24
 #define CMD_TS_X31 49
@@ -1247,7 +1246,7 @@
 #define showSdCardWarning_offset 76
 #define SIGNATURE_BOARD kin
 #define SIGNATURE_DATE 2021.01.07
-#define SIGNATURE_HASH 2254716217
+#define SIGNATURE_HASH 2682341175
 #define silentTriggerError_offset 1464
 #define slowAdcAlpha_offset 2088
 #define sparkDwellRpmBins_offset 332
@@ -1579,7 +1578,7 @@
 #define ts_show_spi true
 #define ts_show_trigger_comparator true
 #define ts_show_tunerstudio_port true
-#define TS_SIGNATURE "rusEFI 2021.01.07.kin.2254716217"
+#define TS_SIGNATURE "rusEFI 2021.01.07.kin.2682341175"
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't' // 0x74
--- java_console/models/src/main/java/com/rusefi/config/generated/Fields.java
+++ java_console/models/src/main/java/com/rusefi/config/generated/Fields.java
@@ -1,6 +1,6 @@
 package com.rusefi.config.generated;
 
-// this file was generated automatically by rusEfi tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt Thu Jan 07 13:07:35 UTC 2021
+// this file was generated automatically by rusEfi tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt Thu Jan 07 13:24:50 UTC 2021
 
 // by class com.rusefi.output.FileJavaFieldsConsumer
 import com.rusefi.config.*;
@@ -282,7 +282,6 @@ public class Fields {
 	public static final int CMD_TS_INJECTOR_CATEGORY = 19;
 	public static final int CMD_TS_SOLENOID_CATEGORY = 41;
 	public static final int CMD_TS_X14 = 20;
-	public static final int CMD_TS_X15 = 21;
 	public static final int CMD_TS_X17 = 23;
 	public static final int CMD_TS_X18 = 24;
 	public static final int CMD_TS_X31 = 49;
@@ -1201,7 +1200,7 @@ public class Fields {
 	public static final int servoOutputPins8_offset = 3147;
 	public static final int showHumanReadableWarning_offset = 976;
 	public static final int showSdCardWarning_offset = 76;
-	public static final int SIGNATURE_HASH = 512336128;
+	public static final int SIGNATURE_HASH = 118283022;
 	public static final int silentTriggerError_offset = 1464;
 	public static final int slowAdcAlpha_offset = 2088;
 	public static final int sparkDwellRpmBins_offset = 332;
@@ -1483,7 +1482,7 @@ public class Fields {
 	public static final char TS_SD_R_COMMAND = 'r';
 	public static final char TS_SD_W_COMMAND = 'w';
 	public static final char TS_SET_LOGGER_SWITCH = 'l';
-	public static final String TS_SIGNATURE = "rusEFI 2021.01.07.all.512336128";
+	public static final String TS_SIGNATURE = "rusEFI 2021.01.07.all.118283022";
 	public static final char TS_SINGLE_WRITE_COMMAND = 'W';
 	public static final int TT_TT_1_16 = 50;
 	public static final int TT_TT_2JZ_1_12 = 29;
