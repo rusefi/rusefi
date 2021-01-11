2f9536ccaf8f3b9065ea4780bff3fe7c8a754a32
Matthew Kennedy <makenne@microsoft.com>
2021-01-11 04:01:39 -0800

    tests

--- unit_tests/test_basic_math/test_find_index.cpp
+++ unit_tests/test_basic_math/test_find_index.cpp
@@ -110,3 +110,86 @@ TEST(misc, testSetTableValue) {
 	ASSERT_FLOAT_EQ(1.4, config.cltFuelCorr[0]);
 
 }
+
+class TestTable2dSmall : public ::testing::Test
+{
+protected:
+	float bins[2];
+	float values[2];
+
+	void SetUp() override
+	{
+		// This test maps [20,30] -> [100,200]
+		copyArray(bins, { 20.0f, 30.0f });
+		copyArray(values, { 100.0f, 200.0f });
+	}
+};
+
+TEST_F(TestTable2dSmall, OffScaleLow)
+{
+    EXPECT_FLOAT_EQ(interpolate2d(10, bins, values), 100);
+}
+
+TEST_F(TestTable2dSmall, OffScaleHigh)
+{
+    EXPECT_FLOAT_EQ(interpolate2d(40, bins, values), 200);
+}
+
+TEST_F(TestTable2dSmall, EdgeLeft)
+{
+    EXPECT_FLOAT_EQ(interpolate2d(20, bins, values), 100);
+}
+
+TEST_F(TestTable2dSmall, EdgeRight)
+{
+    EXPECT_FLOAT_EQ(interpolate2d(30, bins, values), 200);
+}
+
+TEST_F(TestTable2dSmall, Middle)
+{
+    EXPECT_FLOAT_EQ(interpolate2d(25, bins, values), 150);
+}
+
+class Test2dTableMassive : public ::testing::Test
+{
+	static constexpr int Count = 2500;
+
+protected:
+	float bins[Count];
+	float values[Count];
+
+	void SetUp() override
+	{
+		float x = 0;
+
+		for (size_t i = 0; i < std::size(bins); i++)
+		{
+			x += 0.1f;
+			bins[i] = x;
+			values[i] = x * x;
+		}
+	}
+};
+
+TEST_F(Test2dTableMassive, t)
+{
+    float x = 0;
+    float maxErr = -1;
+
+    for (size_t i = 0; i < 25000; i++)
+    {
+        x += 0.01f;
+
+        float actual = x * x;
+        float lookup = interpolate2d(x, bins, values);
+
+        float err = std::abs(actual - lookup);
+
+        if (err > maxErr)
+        {
+            maxErr = err;
+        }
+    }
+
+    EXPECT_LT(maxErr, 0.01);
+}
