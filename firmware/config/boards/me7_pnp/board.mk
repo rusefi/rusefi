d7e71a526ef592420ca01f8b62c6d9ca5893222e
Andrey Gusakov <dron0gus@gmail.com>
2021-01-16 17:45:42 +0300

    board.mk: append sources and includes to ALLSRC, ALLCPPSRC, ALLINC

--- firmware/config/boards/me7_pnp/board.mk
+++ firmware/config/boards/me7_pnp/board.mk
@@ -1,6 +1,6 @@
 # List of all the board related files.
 BOARDSRC = $(PROJECT_DIR)/config/boards/nucleo_f767/board.c
-BOARDSRC_CPP = $(PROJECT_DIR)/config/boards/me7_pnp/board_configuration.cpp
+BOARDCPPSRC = $(PROJECT_DIR)/config/boards/me7_pnp/board_configuration.cpp
 
 # Required include directories
 BOARDINC = $(PROJECT_DIR)/config/boards/NUCLEO_F767 $(PROJECT_DIR)/config/stm32f7ems
@@ -11,3 +11,8 @@ LDSCRIPT= $(PROJECT_DIR)/config/boards/nucleo_f767/STM32F76xxI.ld
 
 # Override DEFAULT_ENGINE_TYPE
 DDEFS += -DDEFAULT_ENGINE_TYPE=VAG_18_TURBO -DSTM32F767xx
+
+# Shared variables
+ALLCSRC   += $(BOARDSRC)
+ALLCPPSRC += $(BOARDCPPSRC)
+ALLINC    += $(BOARDINC)
--- firmware/config/boards/st_stm32f4/board.mk
+++ firmware/config/boards/st_stm32f4/board.mk
@@ -1,7 +1,7 @@
 # List of all the board related files.
 BOARDSRC = $(CHIBIOS)/os/hal/boards/ST_STM32F4_DISCOVERY/board.c \
     $(PROJECT_DIR)/config/boards/st_stm32f4/board_extra.c
-BOARDSRC_CPP =
+BOARDCPPSRC =
 
 # Required include directories
 BOARDINC = $(PROJECT_DIR)/config/boards/st_stm32f4
@@ -11,3 +11,8 @@ DDEFS += -DSTM32F407xx
 
 # We are running on Frankenso hardware!
 DDEFS += -DHW_FRANKENSO=1
+
+# Shared variables
+ALLCSRC   += $(BOARDSRC)
+ALLCPPSRC += $(BOARDCPPSRC)
+ALLINC    += $(BOARDINC)
