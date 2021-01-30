baf93728e58e44d89909798b1e974839008ccaa1
David Holdeman <david@chuckwagoncomputing.com>
2021-01-30 17:13:08 -0600

    connectors subdir stupid

--- misc/jenkins/generate_doxygen/gen_upload_docs.sh
+++ misc/jenkins/generate_doxygen/gen_upload_docs.sh
@@ -28,15 +28,15 @@ cd ..
 mkdir pinouts
 mkdir pinouts/proteus
 bash misc/pinout-gen/gen.sh firmware/config/boards/proteus/black23.yaml > pinouts/proteus/black23.html
-cp firmware/config/boards/proteus/black23.jpg pinouts/proteus/black23.jpg
+cp firmware/config/boards/proteus/connectors/black23.jpg pinouts/proteus/black23.jpg
 bash misc/pinout-gen/gen.sh firmware/config/boards/proteus/black35.yaml > pinouts/proteus/black35.html
-cp firmware/config/boards/proteus/black35.jpg pinouts/proteus/black35.jpg
+cp firmware/config/boards/proteus/connectors/black35.jpg pinouts/proteus/black35.jpg
 bash misc/pinout-gen/gen.sh firmware/config/boards/proteus/white35.yaml > pinouts/proteus/white35.html
-cp firmware/config/boards/proteus/black35.jpg pinouts/proteus/white35.jpg
+cp firmware/config/boards/proteus/connectors/black35.jpg pinouts/proteus/white35.jpg
 mkdir pinouts/hellen
 mkdir pinouts/hellen/hellen72
 bash misc/pinout-gen/gen.sh firmware/config/boards/hellen/hellen72/main.yaml > pinouts/hellen/hellen72/main.html
-cp firmware/config/boards/hellen/hellen72/main.jpg pinouts/hellen/hellen72/main.jpg
+cp firmware/config/boards/hellen/hellen72/connectors/main.jpg pinouts/hellen/hellen72/main.jpg
 
 if [ -n "$RUSEFI_FTP_SERVER" ]; then
   echo "Uploading Pinouts"
