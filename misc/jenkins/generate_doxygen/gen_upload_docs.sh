d61f38f56021dad4423bef1ec38ed072205eab97
David Holdeman <david@chuckwagoncomputing.com>
2021-01-30 16:11:22 -0600

    white35

--- misc/jenkins/generate_doxygen/gen_upload_docs.sh
+++ misc/jenkins/generate_doxygen/gen_upload_docs.sh
@@ -30,6 +30,8 @@ bash misc/pinout-gen/gen.sh firmware/config/boards/proteus/black23.yaml > pinout
 cp firmware/config/boards/proteus/black23.jpg pinouts/black23.jpg
 bash misc/pinout-gen/gen.sh firmware/config/boards/proteus/black35.yaml > pinouts/black35.html
 cp firmware/config/boards/proteus/black35.jpg pinouts/black35.jpg
+bash misc/pinout-gen/gen.sh firmware/config/boards/proteus/white35.yaml > pinouts/white35.html
+cp firmware/config/boards/proteus/black35.jpg pinouts/white35.jpg
 
 if [ -n "$RUSEFI_FTP_SERVER" ]; then
   echo "Uploading Pinouts"
