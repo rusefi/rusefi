10c31c29553641672698e326cd545f425ae5ba92
Matthew Kennedy <matthewkennedy@outlook.com>
2021-01-31 11:13:55 -0800

    -g3 to -g (#2256)
    Co-authored-by: Matthew Kennedy <makenne@microsoft.com>

--- firmware/config/boards/microrusefi/compile_mre_f4_recovery.sh
+++ firmware/config/boards/microrusefi/compile_mre_f4_recovery.sh
@@ -4,6 +4,6 @@ export PROJECT_BOARD=microrusefi
 export PROJECT_CPU=ARCH_STM32F4
 export EXTRA_PARAMS="-DSHORT_BOARD_NAME=mre_f4 -DIGNORE_FLASH_CONFIGURATION=TRUE"
 
-# export DEBUG_LEVEL_OPT="-O0 -ggdb -g3"
+# export DEBUG_LEVEL_OPT="-O0 -ggdb -g"
 
 bash ../common_make.sh
