5030c7245673c294866063c32f86bd333047e25c
David Holdeman <david@chuckwagoncomputing.com>
2021-01-31 07:08:52 -0600

    fix paths

--- misc/pinout-gen/gen.sh
+++ misc/pinout-gen/gen.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-cd "${0%/*}"
-sed -e '/###CSS###/{r style.css' -e 'd}' -e '/###JS###/{r script.js' -e 'd}' pinout.html | sed -e "/###DATA###/{r $1" -e 'd}'
+DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)"/"
+sed -e "/###CSS###/{r ${DIR}style.css" -e 'd}' -e "/###JS###/{r ${DIR}script.js" -e 'd}' ${DIR}pinout.html | sed -e "/###DATA###/{r $1" -e 'd}'
