fb75b68a0559077a1f05d2bc3e1fcb6208000e9a
Matthew Kennedy <matthewkennedy@outlook.com>
2021-01-31 13:37:16 -0800

    less magic

--- firmware/controllers/algo/airmass/speed_density_airmass.cpp
+++ firmware/controllers/algo/airmass/speed_density_airmass.cpp
@@ -46,7 +46,7 @@ float SpeedDensityAirmass::getMap(int rpm) const {
 	float fallbackMap;
 	if (CONFIG(enableMapEstimationTableFallback)) {
 		// if the map estimation table is enabled, estimate map based on the TPS and RPM
-		fallbackMap = m_mapEstimationTable->getValue(rpm, 100 * Sensor::get(SensorType::Tps1).value_or(0));
+		fallbackMap = m_mapEstimationTable->getValue(rpm, TPS_2_BYTE_PACKING_MULT * Sensor::get(SensorType::Tps1).value_or(0));
 	} else {
 		fallbackMap = CONFIG(failedMapFallback);
 	}
