8ff61e9e680e9b5e7ec14c10ecddcb38fbeacf18
rusefillc <sdfsdfqsf2334234234>
2021-02-02 09:50:20 -0500

    GM/Daewoo 60/2/2/2 Trigger Pattern #2264

--- firmware/controllers/trigger/decoders/trigger_structure.cpp
+++ firmware/controllers/trigger/decoders/trigger_structure.cpp
@@ -541,6 +541,9 @@ void TriggerWaveform::initializeTriggerWaveform(Logging *logger, operation_mode_
 		break;
 
 	case TT_GM_60_2_2_2:
+		configureGm60_2_2_2(this);
+		break;
+
 	case TT_GM_7X:
 		configureGmTriggerWaveform(this);
 		break;
