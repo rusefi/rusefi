8bd6035829a748a818d71b27c5122a39273b5eba
David Holdeman <david@chuckwagoncomputing.com>
2021-02-02 15:26:36 -0600

    debug

--- misc/jenkins/generate_doxygen/gen_upload_docs.sh
+++ misc/jenkins/generate_doxygen/gen_upload_docs.sh
@@ -28,15 +28,22 @@ cd ..
 
 CONNECTORS=$(find -path "./firmware/config/boards/*/connectors/*.yaml")
 for c in $CONNECTORS; do
+  echo "processing "$c
   DIR="pinouts/"$(echo $c | tr '/' '\n' | tail -n +5 | head -n -2 | tr '\n' '/')
+  echo "DIR "$DIR
   NAME=$(basename $c .yaml)
+  echo "NAME "$NAME
   mkdir -p $DIR
   bash misc/pinout-gen/gen.sh $c > $DIR/$NAME.html
+  file $DIR/$NAME.html
   IMG=$(yq r $c 'info.image.file')
+  echo "IMG "$IMG
   if [ $IMG ]; then
     cp $(dirname $c)/$IMG $DIR
   fi
+  ls $DIR
   cp misc/pinout-gen/yaml.min.js $DIR
+  ls $DIR
 done
 
 if [ -n "$RUSEFI_FTP_SERVER" ]; then
