cf9f9ae8fcab5f52362c95e1342337b87666406e
David Holdeman <david@chuckwagoncomputing.com>
2021-02-03 21:26:47 -0600

    Fix pinouts maybe (#2276)
    * debug
    
    * change flags

--- misc/jenkins/generate_doxygen/gen_upload_docs.sh
+++ misc/jenkins/generate_doxygen/gen_upload_docs.sh
@@ -7,7 +7,7 @@ bash misc/jenkins/InteractiveHtmlBom/run.sh
 
 if [ -n "$RUSEFI_FTP_SERVER" ]; then
   echo "Uploading IBOMs"
-  ncftpput -R -v -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" /ibom hardware/ibom/*
+  ncftpput -R -m -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" /ibom hardware/ibom/*
 fi
 
 pwd
@@ -20,7 +20,7 @@ doxygen || { echo "doxygen run FAILED"; exit 1; }
 cd ../doxygen
 if [ -n "$RUSEFI_FTP_SERVER" ]; then
   echo "Uploading Doxygen"
-  ncftpput -R -v -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" /html html/
+  ncftpput -R -m -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" /html html/
 fi
 [ $? -eq 0 ] || { echo "upload FAILED"; exit 1; }
 
@@ -28,19 +28,26 @@ cd ..
 
 CONNECTORS=$(find -path "./firmware/config/boards/*/connectors/*.yaml")
 for c in $CONNECTORS; do
+  echo "processing "$c
   DIR="pinouts/"$(echo $c | tr '/' '\n' | tail -n +5 | head -n -2 | tr '\n' '/')
+  echo "DIR "$DIR
   NAME=$(basename $c .yaml)
+  echo "NAME "$NAME
   mkdir -p $DIR
   bash misc/pinout-gen/gen.sh $c > $DIR/$NAME.html
+  file $DIR/$NAME.html
   IMG=$(yq r $c 'info.image.file')
+  echo "IMG "$IMG
   if [ $IMG ]; then
     cp $(dirname $c)/$IMG $DIR
   fi
+  ls $DIR
   cp misc/pinout-gen/yaml.min.js $DIR
+  ls $DIR
 done
 
 if [ -n "$RUSEFI_FTP_SERVER" ]; then
   echo "Uploading Pinouts"
-  ncftpput -R -v -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" /pinouts pinouts/
+  ncftpput -R -m -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" /pinouts pinouts/
 fi
 [ $? -eq 0 ] || { echo "upload FAILED"; exit 1; }
