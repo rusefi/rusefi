660b8e319def4b98a415694b325b5b80c575d3bc
rusefillc <sdfsdfqsf2334234234>
2021-02-06 00:07:05 -0500

    H7 compilation #2284

--- firmware/config/boards/nucleo_h743/board.mk
+++ firmware/config/boards/nucleo_h743/board.mk
@@ -3,6 +3,9 @@ BOARDSRC = $(CHIBIOS)/os/hal/boards/ST_NUCLEO144_H743ZI/board.c
 
 # Required include directories
 BOARDINC = $(PROJECT_DIR)/config/boards/nucleo_h743 $(PROJECT_DIR)/config/stm32h7ems
+CONFDIR = config/stm32h7ems
+
+LDSCRIPT  = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/ld/STM32H743xI.ld
 
 # Shared variables
 ALLCSRC   += $(BOARDSRC)
