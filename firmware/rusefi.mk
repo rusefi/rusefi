f7b29fd42ebcb00a1a80764cc9fb7b5e3797ae9e
rusefillc <sdfsdfqsf2334234234>
2021-02-05 20:00:44 -0500

    let's build for h7

--- firmware/rusefi.mk
+++ firmware/rusefi.mk
@@ -22,12 +22,12 @@ ifeq ($(PROJECT_CPU),ARCH_STM32F7)
 CPU_STARTUP = startup_stm32f7xx.mk
 CPU_PLATFORM = STM32F7xx/platform.mk
 CPU_HWLAYER = ports/stm32/stm32f7
-endif
-
-ifeq ($(PROJECT_CPU),ARCH_STM32F4)
+else ifeq ($(PROJECT_CPU),ARCH_STM32F4)
 CPU_STARTUP = startup_stm32f4xx.mk
 CPU_PLATFORM = STM32F4xx/platform.mk
 CPU_HWLAYER = ports/stm32/stm32f4
+else
+$(error Unexpected PROJECT_CPU)
 endif
 
 
