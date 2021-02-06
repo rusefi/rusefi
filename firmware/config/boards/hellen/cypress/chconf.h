6915852a7a9ad5dbda6260168489d3bc2ed3d67e
rusefillc <sdfsdfqsf2334234234>
2021-02-05 23:41:26 -0500

    reducing code duplication

--- firmware/config/boards/hellen/cypress/chconf.h
+++ firmware/config/boards/hellen/cypress/chconf.h
@@ -748,16 +748,6 @@ extern "C"
 void chDbgPanic3(const char *msg, const char * file, int line);
 #endif
 
-/**
- * declared as a macro so that this code does not use stack
- * so that it would not crash the error handler in case of stack issues
- */
-#if CH_DBG_SYSTEM_STATE_CHECK
-#define hasOsPanicError() (ch.dbg.panic_msg != NULL)
-#else
-#define hasOsPanicError() (FALSE)
-#endif
-
 
 #define chDbgAssert(c, remark) do {                                              \
   if (CH_DBG_ENABLE_ASSERTS != FALSE) {                                     \
