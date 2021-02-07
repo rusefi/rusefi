fe688471ca1cfa315f1891a7d29543c2ebea62a6
GitHub build-firmware Action <action@github.com>
2021-02-07 01:56:30 +0000

    Auto-generated configs and docs

--- firmware/config/boards/hellen/cypress/config/controllers/algo/engine_configuration_generated_structures.h
+++ firmware/config/boards/hellen/cypress/config/controllers/algo/engine_configuration_generated_structures.h
@@ -1,4 +1,4 @@
-// this section was generated automatically by rusEfi tool ConfigDefinition.jar based on hellen_cypress_gen_config.bat integration/rusefi_config.txt Sun Feb 07 00:51:41 UTC 2021
+// this section was generated automatically by rusEfi tool ConfigDefinition.jar based on hellen_cypress_gen_config.bat integration/rusefi_config.txt Sun Feb 07 01:56:13 UTC 2021
 // by class com.rusefi.output.CHeaderConsumer
 // begin
 #pragma once
@@ -3803,4 +3803,4 @@ struct persistent_config_s {
 typedef struct persistent_config_s persistent_config_s;
 
 // end
-// this section was generated automatically by rusEfi tool ConfigDefinition.jar based on hellen_cypress_gen_config.bat integration/rusefi_config.txt Sun Feb 07 00:51:41 UTC 2021
+// this section was generated automatically by rusEfi tool ConfigDefinition.jar based on hellen_cypress_gen_config.bat integration/rusefi_config.txt Sun Feb 07 01:56:13 UTC 2021
