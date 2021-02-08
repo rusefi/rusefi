ad6fc5415d6d94672816713ca7784d4089ed2b99
Matthew Kennedy <matthewkennedy@outlook.com>
2021-02-08 12:46:48 -0800

    move board.h for f7/h7 (#2316)
    * f7 board.h
    
    * h7

--- firmware/config/boards/proteus/board.mk
+++ firmware/config/boards/proteus/board.mk
@@ -7,7 +7,6 @@ BOARDINC = $(PROJECT_DIR)/config/boards/proteus
 ifeq ($(PROJECT_CPU),ARCH_STM32F4)
   BOARDINC += $(PROJECT_DIR)/config/stm32f4ems	# For board.h
 else
-  BOARDINC += $(BOARDS_DIR)/nucleo_f767		# For board.h
   BOARDINC += $(PROJECT_DIR)/config/stm32f7ems	# efifeatures/halconf/chconf.h
   CONFDIR   = config/stm32f4ems
   PROTEUS_LEGACY = TRUE
