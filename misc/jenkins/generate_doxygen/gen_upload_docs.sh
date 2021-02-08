36e5816e1cda8759205c8ebefad9c87da6f818f5
David Holdeman <david@chuckwagoncomputing.com>
2021-02-08 07:51:24 -0600

    try removing -m and glob (#2312)

--- misc/jenkins/generate_doxygen/gen_upload_docs.sh
+++ misc/jenkins/generate_doxygen/gen_upload_docs.sh
@@ -10,6 +10,6 @@ doxygen || { echo "doxygen run FAILED"; exit 1; }
 cd ../doxygen
 if [ -n "$RUSEFI_FTP_SERVER" ]; then
   echo "Uploading Doxygen"
-  ncftpput -R -m -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" /html html/*
+  ncftpput -R -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" /html html/
 fi
 [ $? -eq 0 ] || { echo "upload FAILED"; exit 1; }
