37d26ef348da0ab8dbeffdda2bf0c1d35ba9b5c8
Matthew Kennedy <matthewkennedy@outlook.com>
2021-02-08 15:50:10 -0800

    don't need this

--- firmware/config/boards/st_stm32f4/board.mk
+++ firmware/config/boards/st_stm32f4/board.mk
@@ -1,6 +1,5 @@
 # List of all the board related files.
 BOARDSRC = $(PROJECT_DIR)/config/boards/st_stm32f4/board_extra.c
-BOARDCPPSRC =
 
 # MCU defines
 DDEFS += -DSTM32F407xx
@@ -10,5 +9,3 @@ DDEFS += -DHW_FRANKENSO=1
 
 # Shared variables
 ALLCSRC   += $(BOARDSRC)
-ALLCPPSRC += $(BOARDCPPSRC)
-ALLINC    += $(BOARDINC)
