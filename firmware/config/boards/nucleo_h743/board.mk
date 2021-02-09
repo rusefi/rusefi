6fba228dabdb4610b94356829f03705859748bf2
Matthew Kennedy <matthewkennedy@outlook.com>
2021-02-08 16:09:42 -0800

    h7

--- firmware/config/boards/nucleo_h743/board.mk
+++ firmware/config/boards/nucleo_h743/board.mk
@@ -1,9 +1,4 @@
 # List of all the board related files.
 
-# Required include directories
-BOARDINC = $(PROJECT_DIR)/config/stm32h7ems
-CONFDIR = config/stm32h7ems
-
 # Shared variables
 ALLCPPSRC += $(PROJECT_DIR)/config/boards/nucleo_h743/board_configuration.cpp
-ALLINC    += $(BOARDINC)
