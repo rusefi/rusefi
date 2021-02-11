4b5d9c7b5a566e547a46df038139e57fcf33a423
Matthew Kennedy <matthewkennedy@outlook.com>
2021-02-11 10:45:19 -0800

    needed that

--- firmware/hw_layer/ports/stm32/serial_over_usb/usbcfg.c
+++ firmware/hw_layer/ports/stm32/serial_over_usb/usbcfg.c
@@ -27,6 +27,7 @@ SerialUSBDriver SDU1;
 /*
  * Endpoints to be used for USBD1.
  */
+#define USB_MSD_DATA_EP                 1
 #define USBD1_DATA_REQUEST_EP           2
 #define USBD1_DATA_AVAILABLE_EP         2
 #define USBD1_INTERRUPT_REQUEST_EP      3
