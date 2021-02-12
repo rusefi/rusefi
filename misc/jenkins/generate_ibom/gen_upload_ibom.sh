7761ad779b88c01b4035b11049afb25e3ffc5c5f
David Holdeman <david@chuckwagoncomputing.com>
2021-02-12 14:56:49 -0600

    Fix doxygen 3 (#2337)
    * try adding E flag
    
    * found flags that work
    
    * add flags to ibom
    
    * add flags to pinouts

--- misc/jenkins/generate_ibom/gen_upload_ibom.sh
+++ misc/jenkins/generate_ibom/gen_upload_ibom.sh
@@ -7,6 +7,6 @@ bash misc/jenkins/InteractiveHtmlBom/run.sh
 
 if [ -n "$RUSEFI_FTP_SERVER" ]; then
   echo "Uploading IBOMs"
-  ncftpput -R -m -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" /ibom hardware/ibom/*
+  ncftpput -R -z -m -V -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" / hardware/ibom
 fi
 [ $? -eq 0 ] || { echo "upload FAILED"; exit 1; }
