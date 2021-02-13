c4c82442c44605018fcbfa5d0d928d8f719af00d
Matthew Kennedy <matthewkennedy@outlook.com>
2021-02-13 03:18:12 -0800

    this is also covered

--- firmware/hw_layer/mass_storage/mass_storage.mk
+++ firmware/hw_layer/mass_storage/mass_storage.mk
@@ -1,1 +1,2 @@
-HW_MASS_STORAGE_SRC_C = $(PROJECT_DIR)/ChibiOS-Contrib/os/various/lib_scsi.c
+HW_MASS_STORAGE_SRC_C = $(PROJECT_DIR)/hw_layer/mass_storage/usb_msd_cfg.c \
+  $(PROJECT_DIR)/ChibiOS-Contrib/os/various/lib_scsi.c
