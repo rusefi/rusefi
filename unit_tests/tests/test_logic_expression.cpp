e22dda8abdb0655252017907c27d7413beeea945
Matthew Kennedy <matthewkennedy@outlook.com>
2021-02-17 19:42:35 -0800

    save

--- unit_tests/tests/test_logic_expression.cpp
+++ unit_tests/tests/test_logic_expression.cpp
@@ -72,21 +72,20 @@ TEST(fsio, testParsing) {
 	element = pool.parseExpression("1 3 AND not");
 	ASSERT_TRUE(element != NULL);
 
-	ASSERT_EQ(element->action, LE_NUMERIC_VALUE);
-	ASSERT_EQ(element->fValue, 1.0);
+	ASSERT_EQ(element[0].action, LE_NUMERIC_VALUE);
+	ASSERT_EQ(element[0].fValue, 1.0);
 
-	element = element->next;
-	ASSERT_EQ(element->action, LE_NUMERIC_VALUE);
-	ASSERT_EQ(element->fValue, 3.0);
+	ASSERT_EQ(element[1].action, LE_NUMERIC_VALUE);
+	ASSERT_EQ(element[1].fValue, 3.0);
 
-	element = element->next;
-	ASSERT_EQ(element->action, LE_OPERATOR_AND);
+	ASSERT_EQ(element[2].action, LE_OPERATOR_AND);
 
-	element = element->next;
-	ASSERT_EQ(element->action, LE_OPERATOR_NOT);
+	ASSERT_EQ(element[3].action, LE_OPERATOR_NOT);
 
-	element = element->next;
-	ASSERT_TRUE(element == NULL);
+	// last should be a return instruction
+	ASSERT_EQ(element[4].action, LE_METHOD_RETURN);
+
+	ASSERT_EQ(pool.getSize(), 5);
 }
 
 static void testExpression2(float selfValue, const char *line, float expected, Engine *engine) {
@@ -201,60 +200,6 @@ TEST(fsio, invalidFunction) {
 }
 
 TEST(fsio, testLogicExpressions) {
-	{
-
-	WITH_ENGINE_TEST_HELPER(FORD_INLINE_6_1995);
-
-	LECalculator c;
-
-	LEElement value1;
-	value1.init(LE_NUMERIC_VALUE, 123.0f);
-	c.add(&value1);
-
-	assertEqualsM("123", 123.0, c.getValue(0 PASS_ENGINE_PARAMETER_SUFFIX));
-
-	LEElement value2;
-	value2.init(LE_NUMERIC_VALUE, 321.0f);
-	c.add(&value2);
-
-	LEElement value3;
-	value3.init(LE_OPERATOR_AND);
-	c.add(&value3);
-	assertEqualsM("123 and 321", 1.0, c.getValue(0 PASS_ENGINE_PARAMETER_SUFFIX));
-
-	/**
-	 * fuel_pump = (time_since_boot < 4 seconds) OR (rpm > 0)
-	 * fuel_pump = time_since_boot 4 less rpm 0 > OR
-	 */
-
-	c.reset();
-
-	LEElement thepool[TEST_POOL_SIZE];
-	LEElementPool pool(thepool, TEST_POOL_SIZE);
-	LEElement *e = pool.next();
-	e->init(LE_METHOD_TIME_SINCE_BOOT);
-
-	e = pool.next();
-	e->init(LE_NUMERIC_VALUE, 4.0f);
-
-	e = pool.next();
-	e->init(LE_OPERATOR_LESS);
-
-	e = pool.next();
-	e->init(LE_METHOD_RPM);
-
-	e = pool.next();
-	e->init(LE_NUMERIC_VALUE, 0.0f);
-
-	e = pool.next();
-	e->init(LE_OPERATOR_MORE);
-
-	e = pool.next();
-	e->init(LE_OPERATOR_OR);
-
-	pool.reset();
-	}
-
 	/**
 	 * fan = (not fan && coolant > 90) OR (fan && coolant > 85)
 	 * fan = fan NOT coolant 90 AND more fan coolant 85 more AND OR
