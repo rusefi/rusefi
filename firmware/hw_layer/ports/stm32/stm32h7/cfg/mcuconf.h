ff1d6b7f353c41c196a0fcd0f29437c811a103c0
Matthew Kennedy <matthewkennedy@outlook.com>
2021-02-18 12:22:46 -0800

    h7 gpt

--- firmware/hw_layer/ports/stm32/stm32h7/cfg/mcuconf.h
+++ firmware/hw_layer/ports/stm32/stm32h7/cfg/mcuconf.h
@@ -263,7 +263,7 @@
 #define STM32_GPT_USE_TIM3                  FALSE
 #define STM32_GPT_USE_TIM4                  FALSE
 #define STM32_GPT_USE_TIM5                  FALSE
-#define STM32_GPT_USE_TIM6                  FALSE
+#define STM32_GPT_USE_TIM6                  TRUE
 #define STM32_GPT_USE_TIM7                  FALSE
 #define STM32_GPT_USE_TIM8                  FALSE
 #define STM32_GPT_USE_TIM12                 FALSE
