0d853dba4a759950c86dd6dbaf36588ae5749b1d
GitHub build-firmware Action <action@github.com>
2021-02-23 21:59:18 +0000

    Auto-generated configs and docs

--- firmware/config/boards/kinetis/config/controllers/algo/rusefi_generated.h
+++ firmware/config/boards/kinetis/config/controllers/algo/rusefi_generated.h
@@ -1226,7 +1226,7 @@
 #define showHumanReadableWarning_offset 976
 #define showSdCardWarning_offset 76
 #define SIGNATURE_BOARD kin
-#define SIGNATURE_DATE 2021.02.22
+#define SIGNATURE_DATE 2021.02.23
 #define SIGNATURE_HASH 3259727249
 #define silentTriggerError_offset 1464
 #define slowAdcAlpha_offset 2088
@@ -1561,7 +1561,7 @@
 #define ts_show_spi true
 #define ts_show_trigger_comparator true
 #define ts_show_tunerstudio_port true
-#define TS_SIGNATURE "rusEFI 2021.02.22.kin.3259727249"
+#define TS_SIGNATURE "rusEFI 2021.02.23.kin.3259727249"
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't' // 0x74
