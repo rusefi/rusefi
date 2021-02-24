4fe0ceec61ff957486d945597b0f88a3f6625015
Matthew Kennedy <matthewkennedy@outlook.com>
2021-02-24 09:08:03 -1000

    deduplication in halconf (#2377)
    * rusefi_halconf
    
    * h7 gpt
    
    * don't redefine that
    
    * maybe this was necessary
    
    * format
    
    * no usb in cypress
    
    * flags for bl
    
    * cypress
    
    * maybe bootloader
    
    * no smart build for cypress/kinetis
    
    * no smart build
    
    * no really, disable smart build
    
    * don't need to clean that env var
    
    * cypress
    
    * format

--- firmware/config/boards/kinetis/OS/os/hal/ports/KINETIS/LLD/COMPv2/driver.mk
+++ firmware/config/boards/kinetis/OS/os/hal/ports/KINETIS/LLD/COMPv2/driver.mk
@@ -2,12 +2,6 @@ ifeq ($(KINETIS_CONTRIB),)
   KINETIS_CONTRIB = $(CHIBIOS_CONTRIB)
 endif
 
-ifeq ($(USE_SMART_BUILD),yes)
-ifneq ($(findstring HAL_USE_COMP TRUE,$(HALCONF)),)
 PLATFORMSRC_CONTRIB += ${KINETIS_CONTRIB}/os/hal/ports/KINETIS/LLD/COMPv2/hal_comp_lld.c
-endif
-else
-PLATFORMSRC_CONTRIB += ${KINETIS_CONTRIB}/os/hal/ports/KINETIS/LLD/COMPv2/hal_comp_lld.c
-endif
 
 PLATFORMINC_CONTRIB += ${KINETIS_CONTRIB}/os/hal/ports/KINETIS/LLD/COMPv2
