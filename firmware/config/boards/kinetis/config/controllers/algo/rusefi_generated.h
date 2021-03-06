eb733bf18398a95c059cd6f38c2579bc4f0326fe
SlowRiot <slowriot@voxelstorm.com>
2021-03-06 00:15:29 +0000

    reverting update of auto-generated files

--- firmware/config/boards/kinetis/config/controllers/algo/rusefi_generated.h
+++ firmware/config/boards/kinetis/config/controllers/algo/rusefi_generated.h
@@ -1227,7 +1227,7 @@
 #define showSdCardWarning_offset 76
 #define SIGNATURE_BOARD kin
 #define SIGNATURE_DATE 2021.03.05
-#define SIGNATURE_HASH 975354752
+#define SIGNATURE_HASH 3259727249
 #define silentTriggerError_offset 1464
 #define slowAdcAlpha_offset 2088
 #define sparkDwellRpmBins_offset 332
@@ -1561,7 +1561,7 @@
 #define ts_show_spi true
 #define ts_show_trigger_comparator true
 #define ts_show_tunerstudio_port true
-#define TS_SIGNATURE "rusEFI 2021.03.05.kin.975354752"
+#define TS_SIGNATURE "rusEFI 2021.03.05.kin.3259727249"
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't' // 0x74
