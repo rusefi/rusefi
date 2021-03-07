5a4c24f9a5fa22a234a32509c1b8d488998664cb
Matthew Kennedy <matthewkennedy@outlook.com>
2021-03-07 12:32:41 -0800

    virtual zero timer (#2426)
    * virtual zero timer
    
    * it would help to call the correct function
    
    Co-authored-by: Matthew Kennedy <makenne@microsoft.com>

--- firmware/util/timer.cpp
+++ firmware/util/timer.cpp
@@ -35,6 +35,10 @@ float Timer::getElapsedSeconds() const {
 }
 
 float Timer::getElapsedSeconds(efitick_t nowNt) const {
+	return 1e-6 * getElapsedUs(nowNt);
+}
+
+float Timer::getElapsedUs(efitick_t nowNt) const {
 	auto delta = nowNt - m_lastReset;
 
 	// Yes, things can happen slightly in the future if we get a lucky interrupt between
@@ -50,7 +54,7 @@ float Timer::getElapsedSeconds(efitick_t nowNt) const {
 
 	auto delta32 = (uint32_t)delta;
 
-	return 1e-6 * NT2US(delta32);
+	return NT2US(delta32);
 }
 
 float Timer::getElapsedSecondsAndReset(efitick_t nowNt) {
