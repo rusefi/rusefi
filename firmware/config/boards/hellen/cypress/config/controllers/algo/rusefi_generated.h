efeb3d928960e1f749f78eee8262ee3819dd0346
GitHub build-firmware Action <action@github.com>
2021-03-13 03:33:50 +0000

    Auto-generated configs and docs

--- firmware/config/boards/hellen/cypress/config/controllers/algo/rusefi_generated.h
+++ firmware/config/boards/hellen/cypress/config/controllers/algo/rusefi_generated.h
@@ -1243,8 +1243,8 @@
 #define showHumanReadableWarning_offset 976
 #define showSdCardWarning_offset 76
 #define SIGNATURE_BOARD hellen_cypress
-#define SIGNATURE_DATE 2021.03.12
-#define SIGNATURE_HASH 1617045491
+#define SIGNATURE_DATE 2021.03.13
+#define SIGNATURE_HASH 839525751
 #define silentTriggerError_offset 1464
 #define slowAdcAlpha_offset 2088
 #define sparkDwellRpmBins_offset 332
@@ -1579,7 +1579,7 @@
 #define ts_show_spi true
 #define ts_show_trigger_comparator false
 #define ts_show_tunerstudio_port true
-#define TS_SIGNATURE "rusEFI 2021.03.12.hellen_cypress.1617045491"
+#define TS_SIGNATURE "rusEFI 2021.03.13.hellen_cypress.839525751"
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't' // 0x74
--- firmware/config/boards/kinetis/config/controllers/algo/rusefi_generated.h
+++ firmware/config/boards/kinetis/config/controllers/algo/rusefi_generated.h
@@ -1227,8 +1227,8 @@
 #define showHumanReadableWarning_offset 976
 #define showSdCardWarning_offset 76
 #define SIGNATURE_BOARD kin
-#define SIGNATURE_DATE 2021.03.12
-#define SIGNATURE_HASH 2680679183
+#define SIGNATURE_DATE 2021.03.13
+#define SIGNATURE_HASH 3450857867
 #define silentTriggerError_offset 1464
 #define slowAdcAlpha_offset 2088
 #define sparkDwellRpmBins_offset 332
@@ -1563,7 +1563,7 @@
 #define ts_show_spi true
 #define ts_show_trigger_comparator true
 #define ts_show_tunerstudio_port true
-#define TS_SIGNATURE "rusEFI 2021.03.12.kin.2680679183"
+#define TS_SIGNATURE "rusEFI 2021.03.13.kin.3450857867"
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't' // 0x74
