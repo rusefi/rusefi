5cc2efc3e5ac5452e1f98e893a67a069759a4b21
Matthew Kennedy <matthewkennedy@outlook.com>
2021-03-13 04:26:58 -0800

    use full memory on STM32F42x (#2450)
    * linker script
    
    * hook it up

--- firmware/hw_layer/ports/stm32/stm32f4/hw_ports.mk
+++ firmware/hw_layer/ports/stm32/stm32f4/hw_ports.mk
@@ -8,9 +8,15 @@ HW_LAYER_EMS_CPP += $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f4/mpu_util.cpp \
 
 DDEFS += -DSTM32F407xx
 MCU = cortex-m4
-LDSCRIPT = $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f4/STM32F405xG.ld
+LDSCRIPT = $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f4/STM32F4.ld
 ALLCSRC += $(CHIBIOS)/os/hal/boards/ST_STM32F4_DISCOVERY/board.c
 CONFDIR = $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f4/cfg
+
+# STM32F42x has extra memory, so change some flags so we can use it.
+ifeq ($(IS_STM32F429),yes)
+	USE_OPT += -Wl,--defsym=STM32F4_HAS_SRAM3=1
+	DDEFS += -DEFI_IS_F42x
+endif
 
 # TODO: remove, for efifeatures.h
 ALLINC += $(PROJECT_DIR)/config/stm32f4ems
