ce699ef9ff51ad3e762032492eab06e46cf8aa38
Matthew Kennedy <matthewkennedy@outlook.com>
2021-03-18 15:23:06 -0700

    put serial ports in their own file

--- firmware/console/binary/tunerstudio.mk
+++ firmware/console/binary/tunerstudio.mk
@@ -1,5 +1,6 @@
 
 TUNERSTUDIO_SRC_CPP = $(PROJECT_DIR)/console/binary/tunerstudio_io.cpp \
+	$(PROJECT_DIR)/console/binary/tunerstudio_io_serial.cpp \
 	$(PROJECT_DIR)/console/binary/ts_can_channel.cpp \
 	$(PROJECT_DIR)/console/binary/tunerstudio.cpp \
 	$(PROJECT_DIR)/console/binary/tunerstudio_commands.cpp \
