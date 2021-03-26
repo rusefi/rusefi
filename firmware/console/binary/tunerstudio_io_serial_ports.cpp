fd3220d38af47211f309993f3768b7edb0766065
Matthew Kennedy <makenne@microsoft.com>
2021-03-26 14:45:36 -0700

    format

--- firmware/console/binary/tunerstudio_io_serial_ports.cpp
+++ firmware/console/binary/tunerstudio_io_serial_ports.cpp
@@ -6,67 +6,65 @@
 
 #include "engine.h"
 
-EXTERN_ENGINE;
-
 #if EFI_PROD_CODE
 #include "tunerstudio.h"
 #include "tunerstudio_io.h"
 #include "connector_uart_dma.h"
 
+EXTERN_ENGINE;
 
 #if defined(TS_PRIMARY_UART) || defined(TS_PRIMARY_SERIAL)
-#ifdef TS_PRIMARY_UART
-	#if EFI_USE_UART_DMA
-		UartDmaTsChannel primaryChannel(TS_PRIMARY_UART);
-	#else
-		UartTsChannel primaryChannel(TS_PRIMARY_UART);
+	#ifdef TS_PRIMARY_UART
+		#if EFI_USE_UART_DMA
+			UartDmaTsChannel primaryChannel(TS_PRIMARY_UART);
+		#else
+			UartTsChannel primaryChannel(TS_PRIMARY_UART);
+		#endif
+	#elif defined(TS_PRIMARY_SERIAL)
+	SerialTsChannel primaryChannel(TS_PRIMARY_SERIAL);
 	#endif
-#elif defined(TS_PRIMARY_SERIAL)
-SerialTsChannel primaryChannel(TS_PRIMARY_SERIAL);
-#endif
 
-struct PrimaryChannelThread : public TunerstudioThread {
-	PrimaryChannelThread() : TunerstudioThread("Primary TS Channel") { }
+	struct PrimaryChannelThread : public TunerstudioThread {
+		PrimaryChannelThread() : TunerstudioThread("Primary TS Channel") { }
 
-	TsChannelBase* setupChannel() {
-		efiSetPadMode("Primary Channel RX", EFI_CONSOLE_RX_BRAIN_PIN, PAL_MODE_ALTERNATE(EFI_CONSOLE_AF));
-		efiSetPadMode("Primary Channel TX", EFI_CONSOLE_TX_BRAIN_PIN, PAL_MODE_ALTERNATE(EFI_CONSOLE_AF));
+		TsChannelBase* setupChannel() {
+			efiSetPadMode("Primary Channel RX", EFI_CONSOLE_RX_BRAIN_PIN, PAL_MODE_ALTERNATE(EFI_CONSOLE_AF));
+			efiSetPadMode("Primary Channel TX", EFI_CONSOLE_TX_BRAIN_PIN, PAL_MODE_ALTERNATE(EFI_CONSOLE_AF));
 
-		primaryChannel.start(CONFIG(uartConsoleSerialSpeed));
+			primaryChannel.start(CONFIG(uartConsoleSerialSpeed));
 
-		return &primaryChannel;
-	}
-};
+			return &primaryChannel;
+		}
+	};
 
-static PrimaryChannelThread primaryChannelThread;
+	static PrimaryChannelThread primaryChannelThread;
 #endif // defined(TS_PRIMARY_UART) || defined(TS_PRIMARY_SERIAL)
 
 #if defined(TS_SECONDARY_UART) || defined(TS_SECONDARY_SERIAL)
-
-#ifdef TS_SECONDARY_UART
-	#if EFI_USE_UART_DMA
-		UartDmaTsChannel secondaryChannel(TS_SECONDARY_UART);
-	#else
-		UartTsChannel secondaryChannel(TS_SECONDARY_UART);
+	#ifdef TS_SECONDARY_UART
+		#if EFI_USE_UART_DMA
+			UartDmaTsChannel secondaryChannel(TS_SECONDARY_UART);
+		#else
+			UartTsChannel secondaryChannel(TS_SECONDARY_UART);
+		#endif
+	#elif defined(TS_SECONDARY_SERIAL)
+	SerialTsChannel secondaryChannel(TS_SECONDARY_SERIAL);
 	#endif
-#elif defined(TS_SECONDARY_SERIAL)
-SerialTsChannel secondaryChannel(TS_SECONDARY_SERIAL);
-#endif
 
-struct SecondaryChannelThread : public TunerstudioThread {
-	SecondaryChannelThread() : TunerstudioThread("Secondary TS Channel") { }
+	struct SecondaryChannelThread : public TunerstudioThread {
+		SecondaryChannelThread() : TunerstudioThread("Secondary TS Channel") { }
 
-	TsChannelBase* setupChannel() {
-		efiSetPadMode("Secondary Channel RX", engineConfiguration->binarySerialRxPin, PAL_MODE_ALTERNATE(TS_SERIAL_AF));
-		efiSetPadMode("Secondary Channel TX", engineConfiguration->binarySerialTxPin, PAL_MODE_ALTERNATE(TS_SERIAL_AF));
+		TsChannelBase* setupChannel() {
+			efiSetPadMode("Secondary Channel RX", engineConfiguration->binarySerialRxPin, PAL_MODE_ALTERNATE(TS_SERIAL_AF));
+			efiSetPadMode("Secondary Channel TX", engineConfiguration->binarySerialTxPin, PAL_MODE_ALTERNATE(TS_SERIAL_AF));
 
-		secondaryChannel.start(CONFIG(uartConsoleSerialSpeed));
+			secondaryChannel.start(CONFIG(uartConsoleSerialSpeed));
 
-		return &secondaryChannel;
-	}
-};
+			return &secondaryChannel;
+		}
+	};
 
-static SecondaryChannelThread secondaryChannelThread;
+	static SecondaryChannelThread secondaryChannelThread;
 #endif // defined(TS_SECONDARY_UART) || defined(TS_SECONDARY_SERIAL)
 
 void startSerialChannels() {
