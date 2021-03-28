6a92851e49600593e64fe11f37a2c144e64e061c
Matthew Kennedy <matthewkennedy@outlook.com>
2021-03-28 13:10:58 -0700

    enable serial on proteus

--- firmware/hw_layer/ports/stm32/mcuconf_common_f4_f7.h
+++ firmware/hw_layer/ports/stm32/mcuconf_common_f4_f7.h
@@ -200,7 +200,7 @@
  * SERIAL driver system settings.
  */
 #define STM32_SERIAL_USE_USART1             TRUE
-#define STM32_SERIAL_USE_USART2             TRUE
+#define STM32_SERIAL_USE_USART2             FALSE
 #define STM32_SERIAL_USE_USART3             FALSE
 #define STM32_SERIAL_USE_UART4              FALSE
 #define STM32_SERIAL_USE_UART5              FALSE
@@ -258,7 +258,9 @@
 #define STM32_UART_USE_USART1               FALSE
 #endif
 
+#ifndef STM32_UART_USE_USART2
 #define STM32_UART_USE_USART2               FALSE
+#endif
 
 #ifndef STM32_UART_USE_USART3
 #define STM32_UART_USE_USART3               TRUE
