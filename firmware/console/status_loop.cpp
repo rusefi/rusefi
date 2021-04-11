96e655ff4e0e5440850b43555d08192caf308d23
Matthew Kennedy <matthewkennedy@outlook.com>
2021-04-11 07:15:54 -0700

    sd buffer (#2540)

--- firmware/console/status_loop.cpp
+++ firmware/console/status_loop.cpp
@@ -134,7 +134,7 @@ static void setWarningEnabled(int value) {
 
 #if EFI_FILE_LOGGING
 // this one needs to be in main ram so that SD card SPI DMA works fine
-static NO_CACHE char sdLogBuffer[100];
+static NO_CACHE char sdLogBuffer[150];
 static uint64_t binaryLogCount = 0;
 
 #endif /* EFI_FILE_LOGGING */
