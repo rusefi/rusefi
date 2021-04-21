82b821ce834297713a2e6f94894df9c17bb16dc7
rusefillc <sdfsdfqsf2334234234>
2021-04-21 09:56:46 -0400

    EFI_SOFTWARE_KNOCK for hellen

--- firmware/config/boards/hellen/hellen64_miataNA6_94/knock_config.h
+++ firmware/config/boards/hellen/hellen64_miataNA6_94/knock_config.h
@@ -7,9 +7,9 @@
 // Knock is on ADC3
 #define KNOCK_ADC ADCD3
 
-// knock 1 - pin PA3
-#define KNOCK_ADC_CH1 ADC_CHANNEL_IN3
-#define KNOCK_PIN_CH1 GPIOA_3
+// knock 1 - pin PF7
+#define KNOCK_ADC_CH1 ADC_CHANNEL_IN5
+#define KNOCK_PIN_CH1 GPIOF_7
 
 // Sample rate & time - depends on the exact MCU
 #define KNOCK_SAMPLE_TIME ADC_SAMPLE_84
