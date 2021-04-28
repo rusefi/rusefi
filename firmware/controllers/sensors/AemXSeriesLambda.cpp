1bd789a5a5213a5d1372e2f5183a69942f23d4de
Matthew Kennedy <matthewkennedy@outlook.com>
2021-04-28 15:28:29 -0700

    make wideband log fields match what it's actually sending (#2593)
    * make log fields match what it's actually sending
    
    * update wb firmware
    
    Co-authored-by: Matthew Kennedy <makenne@microsoft.com>

--- firmware/controllers/sensors/AemXSeriesLambda.cpp
+++ firmware/controllers/sensors/AemXSeriesLambda.cpp
@@ -37,11 +37,11 @@ void AemXSeriesWideband::decodeFrame(const CANRxFrame& frame, efitick_t nowNt) {
 	if (isRusefiController && CONFIG(debugMode) == DBG_RUSEFI_WIDEBAND) {
 		float pumpDuty = frame.data8[2] / 255.0f;
 		float sensorEsr = frame.data8[3] * 4;
-		float heaterDuty = frame.data8[4] / 255.0f;
+		float nernstVoltage = frame.data8[4] / 200.0f;
 
 		tsOutputChannels.debugFloatField1 = pumpDuty;
 		tsOutputChannels.debugFloatField2 = sensorEsr;
-		tsOutputChannels.debugFloatField3 = heaterDuty;
+		tsOutputChannels.debugFloatField3 = nernstVoltage;
 		tsOutputChannels.debugFloatField4 = lambdaFloat;
 	}
 #endif
