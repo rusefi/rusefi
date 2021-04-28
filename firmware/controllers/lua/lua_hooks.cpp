d50a5303f86c147bed346f5ccb87a04f35ec204a
Matthew Kennedy <matthewkennedy@outlook.com>
2021-04-28 01:18:57 -0700

    add a useful hook

--- firmware/controllers/lua/lua_hooks.cpp
+++ firmware/controllers/lua/lua_hooks.cpp
@@ -1,17 +1,34 @@
 #include "lua.hpp"
 #include "lua_hooks.h"
+
 #include "loggingcentral.h"
+#include "sensor.h"
 
 static int lua_efi_print(lua_State* l) {
-	luaL_checkstring(l, 1);
-
-	auto msg = lua_tostring(l, 1);
+	auto msg = luaL_checkstring(l, 1);
 
 	efiPrintf("LUA: %s", msg);
 
 	return 0;
 }
 
+static int lua_get_sensor(lua_State* l) {
+	auto sensorIndex = luaL_checkinteger(l, 1);
+
+	auto result = Sensor::get(static_cast<SensorType>(sensorIndex));
+
+	if (result) {
+		// return value if valid
+		lua_pushnumber(l, result.Value);
+	} else {
+		// return nil if invalid
+		lua_pushnil(l);
+	}
+
+	return 1;
+}
+
 void configureRusefiLuaHooks(lua_State* l) {
 	lua_register(l, "print", lua_efi_print);
+	lua_register(l, "getSensor", lua_get_sensor);
 }
