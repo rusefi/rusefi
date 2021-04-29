2dd6905d5ca9038df5cca7d14af068d3b22fe273
Matthew Kennedy <makenne@microsoft.com>
2021-04-29 11:12:36 -0700

    test table hook

--- unit_tests/tests/lua/test_lua_hooks.cpp
+++ unit_tests/tests/lua/test_lua_hooks.cpp
@@ -1,5 +1,6 @@
 #include "rusefi_lua.h"
 #include <gtest/gtest.h>
+#include "engine_test_helper.h"
 #include "sensor.h"
 
 static const char* getSensorTest = R"(
@@ -18,4 +19,16 @@ TEST(LuaHooks, TestGetSensor) {
 	// Now test with a value, returns value
 	Sensor::setMockValue(10, 33);
 	EXPECT_EQ(testLuaReturnsNumberOrNil(getSensorTest).value_or(0), 33);
+}
+
+static const char* tableTest = R"(
+function testFunc()
+	return table3d(1, 1000, 40)
+end
+)";
+
+TEST(LuaHooks, Table3d) {
+	WITH_ENGINE_TEST_HELPER(TEST_ENGINE);
+
+	EXPECT_EQ(testLuaReturnsNumber(tableTest), 33);
 }
