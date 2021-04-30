e3c459083102da99b409564bb7b7d384d6231186
rusefillc <sdfsdfqsf2334234234>
2021-04-29 23:33:30 -0400

    dead files?

--- firmware/controllers/generated/signature_kin.h
+++
@@ -1,8 +0,0 @@
-//
-// was generated automatically by rusEFI tool ConfigDefinition.jar based on kinetis_gen_config.bat integration/rusefi_config.txt
-//
-
-#define SIGNATURE_BOARD kin
-#define SIGNATURE_DATE 2021.04.30
-#define SIGNATURE_HASH 3513353544
-#define TS_SIGNATURE "rusEFI 2021.04.30.kin.3513353544"
