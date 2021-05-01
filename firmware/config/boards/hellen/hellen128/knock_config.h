2517f5fc635a15e9b8fc562c2ead11784341965f
rusefillc <sdfsdfqsf2334234234>
2021-05-01 19:15:23 -0400

    code duplication between hellen boards #2611
    it has to get worse before it gets better

---
+++ firmware/config/boards/hellen/hellen128/knock_config.h
@@ -0,0 +1,16 @@
+/**
+ * @file knock_config.h
+ */
+
+#pragma once
+
+// Knock is on ADC3
+#define KNOCK_ADC ADCD3
+
+// knock 1 - pin PF7
+#define KNOCK_ADC_CH1 ADC_CHANNEL_IN5
+#define KNOCK_PIN_CH1 GPIOF_7
+
+// Sample rate & time - depends on the exact MCU
+#define KNOCK_SAMPLE_TIME ADC_SAMPLE_84
+#define KNOCK_SAMPLE_RATE (STM32_PCLK2 / (4 * (84 + 12)))
