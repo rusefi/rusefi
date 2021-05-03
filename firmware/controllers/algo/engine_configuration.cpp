bf223aaacfbdb5b5dc8de07ba54009a0925801ec
rusefillc <sdfsdfqsf2334234234>
2021-05-02 23:47:47 -0400

    50% duty cycle is the default for tach signal

--- firmware/controllers/algo/engine_configuration.cpp
+++ firmware/controllers/algo/engine_configuration.cpp
@@ -792,8 +792,6 @@ static void setDefaultEngineConfiguration(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 
 	initTemperatureCurve(IAT_FUEL_CORRECTION_CURVE, 1);
 
-	engineConfiguration->tachPulseDuractionMs = 4;
-
 	engineConfiguration->auxPid[0].minValue = 10;
 	engineConfiguration->auxPid[0].maxValue = 90;
 
