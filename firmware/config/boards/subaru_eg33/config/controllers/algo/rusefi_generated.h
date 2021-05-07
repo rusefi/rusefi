a47afe873e5dd06c6cb238570cb7f23638e139ad
GitHub build-firmware Action <action@github.com>
2021-05-07 03:16:46 +0000

    Auto-generated configs and docs

--- firmware/config/boards/subaru_eg33/config/controllers/algo/rusefi_generated.h
+++ firmware/config/boards/subaru_eg33/config/controllers/algo/rusefi_generated.h
@@ -1258,8 +1258,8 @@
 #define showHumanReadableWarning_offset 976
 #define showSdCardWarning_offset 76
 #define SIGNATURE_BOARD subaru_eg33_f7
-#define SIGNATURE_DATE 2021.05.05
-#define SIGNATURE_HASH 3886082453
+#define SIGNATURE_DATE 2021.05.07
+#define SIGNATURE_HASH 2180359154
 #define silentTriggerError_offset 1464
 #define slowAdcAlpha_offset 2088
 #define sparkDwellRpmBins_offset 332
@@ -1594,7 +1594,7 @@
 #define ts_show_spi true
 #define ts_show_trigger_comparator false
 #define ts_show_tunerstudio_port false
-#define TS_SIGNATURE "rusEFI 2021.05.05.subaru_eg33_f7.3886082453"
+#define TS_SIGNATURE "rusEFI 2021.05.07.subaru_eg33_f7.2180359154"
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't' // 0x74
