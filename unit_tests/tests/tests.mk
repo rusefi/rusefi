77c28c69699a58f09f0b9646207d725b1ecd9b21
rusefillc <sdfsdfqsf2334234234>
2021-05-07 22:14:22 -0400

    Break out unit tests #2627

--- unit_tests/tests/tests.mk
+++ unit_tests/tests/tests.mk
@@ -18,8 +18,6 @@ TESTS_SRC_CPP = \
 	tests/lua/test_lua_basic.cpp \
 	tests/lua/test_lua_hooks.cpp \
 	tests/sensor/test_cj125.cpp \
-	tests/util/test_buffered_writer.cpp \
-	tests/util/test_error_accumulator.cpp \
 	tests/util/test_timer.cpp \
 	tests/test_util.cpp \
 	tests/test_start_stop.cpp \
