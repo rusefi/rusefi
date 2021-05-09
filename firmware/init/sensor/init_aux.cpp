3dd6b2efc9c71c1a81b85a44785a432f74f12ebc
Matthew Kennedy <matthewkennedy@outlook.com>
2021-05-09 16:47:37 -0700

    move FSIO aux analog inputs to sensor model (#2669)
    * sensor entries
    
    * init
    
    * non-explicit for list init to work
    
    * switch fsio
    
    * no longer need this lua hook
    
    * init

---
+++ firmware/init/sensor/init_aux.cpp
@@ -0,0 +1,43 @@
+#include "init.h"
+#include "adc_inputs.h"
+#include "adc_subscription.h"
+#include "engine.h"
+#include "global.h"
+#include "functional_sensor.h"
+
+EXTERN_ENGINE;
+
+// These aux sensors just read voltage - so the converter function has nothing to do
+struct IdentityFunction : public SensorConverter {
+	SensorResult convert(float raw) const {
+		return raw;
+	}
+};
+
+static IdentityFunction func;
+
+static FunctionalSensor auxSensors[] = {
+	{ SensorType::Aux1, MS2NT(50) },
+	{ SensorType::Aux2, MS2NT(50) },
+	{ SensorType::Aux3, MS2NT(50) },
+	{ SensorType::Aux4, MS2NT(50) },
+};
+
+static_assert(efi::size(auxSensors) == FSIO_ANALOG_INPUT_COUNT);
+
+void initAuxSensors(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
+	for (size_t i = 0; i < efi::size(CONFIG(fsioAdc)); i++) {
+		auto channel = CONFIG(fsioAdc)[i];
+
+		// Skip unconfigured channels
+		if (!isAdcChannelValid(channel)) {
+			continue;
+		}
+
+		auto& sensor = auxSensors[i];
+		sensor.setFunction(func);
+		sensor.Register();
+
+		AdcSubscription::SubscribeSensor(sensor, channel, 10);
+	}
+}
