1577d4307715e216a07773b515999ea87dcc2064
Matthew Kennedy <matthewkennedy@outlook.com>
2021-05-14 11:43:32 -0700

    ui

---
+++ java_console/ui/src/main/java/com/rusefi/ui/lua/LuaScriptPanel.java
@@ -0,0 +1,109 @@
+package com.rusefi.ui.lua;
+
+import com.opensr5.ConfigurationImage;
+import com.rusefi.CommandControl;
+import com.rusefi.FixedCommandControl;
+import com.rusefi.binaryprotocol.BinaryProtocol;
+import com.rusefi.config.generated.Fields;
+import com.rusefi.ui.MessagesPanel;
+import com.rusefi.ui.UIContext;
+import com.rusefi.ui.storage.Node;
+import com.rusefi.ui.widgets.AnyCommand;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import static com.rusefi.CommandControl.TEST;
+
+public class LuaScriptPanel {
+    private final UIContext context;
+    private final JPanel mainPanel = new JPanel(new BorderLayout());
+    private final AnyCommand command;
+    private final JTextArea scriptText;
+
+    public LuaScriptPanel(UIContext context, Node config) {
+        this.context = context;
+        this.command = AnyCommand.createField(context, config, true, true);
+
+        // Upper panel: command entry, etc
+        JPanel upperPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 0));
+
+        JButton readButton = new JButton("Read from ECU");
+        JButton writeButton = new JButton("Write to ECU");
+        JButton resetButton = new JButton("Reset/Reload Lua");
+
+        readButton.addActionListener(e -> read());
+        writeButton.addActionListener(e -> write());
+        resetButton.addActionListener(e -> resetLua());
+
+        upperPanel.add(readButton);
+        upperPanel.add(writeButton);
+        upperPanel.add(resetButton);
+        upperPanel.add(this.command.getContent());
+
+        // Center panel - script editor and log
+        JPanel scriptPanel = new JPanel(new BorderLayout());
+        this.scriptText = new JTextArea("hello");
+        scriptPanel.add(this.scriptText, BorderLayout.CENTER);
+
+        //centerPanel.add(, BorderLayout.WEST);
+        JPanel messagesPanel = new JPanel(new BorderLayout());
+        MessagesPanel mp = new MessagesPanel(null);
+        messagesPanel.add(BorderLayout.NORTH, mp.getButtonPanel());
+        messagesPanel.add(BorderLayout.CENTER, mp.getMessagesScroll());
+
+        JSplitPane centerPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scriptPanel, messagesPanel);
+
+        this.mainPanel.add(upperPanel, BorderLayout.NORTH);
+        this.mainPanel.add(centerPanel, BorderLayout.CENTER);
+    }
+
+    public JPanel getPanel() {
+        return mainPanel;
+    }
+
+    public ActionListener getTabSelectedListener() {
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (command != null)
+                    command.requestFocus();
+            }
+        };
+    }
+
+    void read() {
+        BinaryProtocol bp = this.context.getLinkManager().getCurrentStreamState();
+
+        if (bp == null) {
+            // TODO: Handle missing ECU
+            return;
+        }
+
+        ConfigurationImage image = bp.getControllerConfiguration();
+        ByteBuffer luaScriptBuffer = image.getByteBuffer(Fields.luaScript_offset, Fields.LUA_SCRIPT_SIZE);
+
+        scriptText.setText(new String(luaScriptBuffer.array(), Charset.forName("ASCII")));
+    }
+
+    void write() {
+        BinaryProtocol bp = this.context.getLinkManager().getCurrentStreamState();
+
+        String script = scriptText.getText();
+        bp.writeData(script.getBytes(StandardCharsets.US_ASCII), Fields.luaScript_offset, Fields.LUA_SCRIPT_SIZE);
+        bp.burn();
+
+        // Burning doesn't reload lua script, so we have to do it manually
+        resetLua();
+    }
+
+    void resetLua() {
+        this.context.getCommandQueue().write("luareset");
+    }
+}
