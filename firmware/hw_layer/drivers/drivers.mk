3bace7c33d25938acadc67de750b59454a2658f0
Matthew Kennedy <matthewkennedy@outlook.com>
2021-05-15 02:11:59 -0700

    makefile

--- firmware/hw_layer/drivers/drivers.mk
+++ firmware/hw_layer/drivers/drivers.mk
@@ -7,18 +7,18 @@ HW_LAYER_DRIVERS_INC = \
 	$(DRIVERS_DIR)/serial \
 
 HW_LAYER_DRIVERS_CORE = \
-	$(DRIVERS_DIR)/gpio/core.c \
 
-HW_LAYER_DRIVERS_CORE_CPP =
+HW_LAYER_DRIVERS_CORE_CPP = \
+	$(DRIVERS_DIR)/gpio/core.cpp \
 
-HW_LAYER_DRIVERS = \
-	$(DRIVERS_DIR)/gpio/tle6240.c \
-	$(DRIVERS_DIR)/gpio/tle8888.c \
-	$(DRIVERS_DIR)/gpio/mc33972.c \
-	$(DRIVERS_DIR)/gpio/mc33810.c \
-	$(DRIVERS_DIR)/gpio/drv8860.c \
+HW_LAYER_DRIVERS =
 
 HW_LAYER_DRIVERS_CPP = \
 	$(DRIVERS_DIR)/can/can_hw.cpp \
 	$(DRIVERS_DIR)/can/can_msg_tx.cpp \
-	$(DRIVERS_DIR)/serial/serial_hw.cpp
+	$(DRIVERS_DIR)/serial/serial_hw.cpp \
+	$(DRIVERS_DIR)/gpio/tle6240.cpp \
+	$(DRIVERS_DIR)/gpio/tle8888.cpp \
+	$(DRIVERS_DIR)/gpio/mc33972.cpp \
+	$(DRIVERS_DIR)/gpio/mc33810.cpp \
+	$(DRIVERS_DIR)/gpio/drv8860.cpp \
