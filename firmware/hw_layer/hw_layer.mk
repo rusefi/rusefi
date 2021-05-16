d2225a5d2240e1c2a97175c9d5408d3aec800756
Andrey Gusakov <dron0gus@gmail.com>
2021-05-16 13:53:42 +0300

    hw_layer.mk: HW_LAYER_DIR

--- firmware/hw_layer/hw_layer.mk
+++ firmware/hw_layer/hw_layer.mk
@@ -1,47 +1,52 @@
+HW_LAYER_DIR=$(PROJECT_DIR)/hw_layer
 
-HW_LAYER_INC=	$(PROJECT_DIR)/hw_layer $(PROJECT_DIR)/hw_layer/adc \
-	$(PROJECT_DIR)/hw_layer/digital_input \
-	$(PROJECT_DIR)/hw_layer/digital_input/trigger \
-	$(PROJECT_DIR)/hw_layer/microsecond_timer \
+# this var is used in bootloader
+HW_INC = \
+	$(HW_LAYER_DIR)/$(CPU_HWLAYER) \
+	$(HW_LAYER_DIR)/ports
 
-HW_INC = hw_layer/$(CPU_HWLAYER) \
-	$(PROJECT_DIR)/hw_layer/ports
+HW_LAYER_INC = \
+	$(HW_LAYER_DIR) \
+	$(HW_LAYER_DIR)/adc \
+	$(HW_LAYER_DIR)/digital_input \
+	$(HW_LAYER_DIR)/digital_input/trigger \
+	$(HW_LAYER_DIR)/microsecond_timer \
 
-HW_LAYER_EMS = $(HW_LAYER_EGT) \
-	$(PROJECT_DIR)/hw_layer/adc/mcp3208.c \
-	$(PROJECT_DIR)/hw_layer/mc33816_data.c
+HW_LAYER_EMS = \
+	$(HW_LAYER_EGT) \
+	$(HW_LAYER_DIR)/adc/mcp3208.c \
+	$(HW_LAYER_DIR)/mc33816_data.c
 
 HW_LAYER_EMS_CPP = \
-	$(PROJECT_DIR)/hw_layer/pin_repository.cpp \
-	$(PROJECT_DIR)/hw_layer/microsecond_timer/microsecond_timer.cpp \
-	$(PROJECT_DIR)/hw_layer/digital_input/digital_input.cpp \
-	$(PROJECT_DIR)/hw_layer/digital_input/digital_input_icu.cpp \
-	$(PROJECT_DIR)/hw_layer/digital_input/digital_input_exti.cpp \
-	$(PROJECT_DIR)/hw_layer/digital_input/trigger/trigger_input.cpp \
-	$(PROJECT_DIR)/hw_layer/digital_input/trigger/trigger_input_icu.cpp \
-	$(PROJECT_DIR)/hw_layer/digital_input/trigger/trigger_input_exti.cpp \
-	$(PROJECT_DIR)/hw_layer/hardware.cpp \
-	$(PROJECT_DIR)/hw_layer/smart_gpio.cpp \
-	$(PROJECT_DIR)/hw_layer/mmc_card.cpp \
-	$(PROJECT_DIR)/hw_layer/mmc_card_access.cpp \
-	$(PROJECT_DIR)/hw_layer/adc/adc_inputs.cpp \
-	$(PROJECT_DIR)/hw_layer/adc/adc_subscription.cpp \
-	$(PROJECT_DIR)/hw_layer/adc/ads1015.cpp \
-	$(PROJECT_DIR)/hw_layer/sensors/hip9011.cpp \
-	$(PROJECT_DIR)/hw_layer/sensors/hip9011_logic.cpp \
-	$(PROJECT_DIR)/hw_layer/mc33816.cpp \
-	$(PROJECT_DIR)/hw_layer/vehicle_speed.cpp \
-	$(PROJECT_DIR)/hw_layer/stepper.cpp \
-	$(PROJECT_DIR)/hw_layer/stepper_dual_hbridge.cpp \
-	$(PROJECT_DIR)/hw_layer/servo.cpp \
-	$(PROJECT_DIR)/hw_layer/io_pins.cpp \
-	$(PROJECT_DIR)/hw_layer/rtc_helper.cpp \
-	$(PROJECT_DIR)/hw_layer/cdm_ion_sense.cpp \
-	$(PROJECT_DIR)/hw_layer/debounce.cpp \
-
+	$(HW_LAYER_DIR)/pin_repository.cpp \
+	$(HW_LAYER_DIR)/microsecond_timer/microsecond_timer.cpp \
+	$(HW_LAYER_DIR)/digital_input/digital_input.cpp \
+	$(HW_LAYER_DIR)/digital_input/digital_input_icu.cpp \
+	$(HW_LAYER_DIR)/digital_input/digital_input_exti.cpp \
+	$(HW_LAYER_DIR)/digital_input/trigger/trigger_input.cpp \
+	$(HW_LAYER_DIR)/digital_input/trigger/trigger_input_icu.cpp \
+	$(HW_LAYER_DIR)/digital_input/trigger/trigger_input_exti.cpp \
+	$(HW_LAYER_DIR)/hardware.cpp \
+	$(HW_LAYER_DIR)/smart_gpio.cpp \
+	$(HW_LAYER_DIR)/mmc_card.cpp \
+	$(HW_LAYER_DIR)/mmc_card_access.cpp \
+	$(HW_LAYER_DIR)/adc/adc_inputs.cpp \
+	$(HW_LAYER_DIR)/adc/adc_subscription.cpp \
+	$(HW_LAYER_DIR)/adc/ads1015.cpp \
+	$(HW_LAYER_DIR)/sensors/hip9011.cpp \
+	$(HW_LAYER_DIR)/sensors/hip9011_logic.cpp \
+	$(HW_LAYER_DIR)/mc33816.cpp \
+	$(HW_LAYER_DIR)/vehicle_speed.cpp \
+	$(HW_LAYER_DIR)/stepper.cpp \
+	$(HW_LAYER_DIR)/stepper_dual_hbridge.cpp \
+	$(HW_LAYER_DIR)/servo.cpp \
+	$(HW_LAYER_DIR)/io_pins.cpp \
+	$(HW_LAYER_DIR)/rtc_helper.cpp \
+	$(HW_LAYER_DIR)/cdm_ion_sense.cpp \
+	$(HW_LAYER_DIR)/debounce.cpp \
 
 #
 # '-include' is a magic kind of 'include' which would survive if file to be included is not found
 #	
--include $(PROJECT_DIR)/hw_layer/$(CPU_HWLAYER)/hw_ports.mk
+-include $(HW_LAYER_DIR)/$(CPU_HWLAYER)/hw_ports.mk
 
