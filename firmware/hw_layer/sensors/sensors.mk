eb6fb6e849a60b7bfc118cd430a418a109055901
Andrey Gusakov <dron0gus@gmail.com>
2021-05-16 13:18:23 +0300

    HW_SENSORS_SRC -> HW_SENSORS_CPP

--- firmware/hw_layer/sensors/sensors.mk
+++ firmware/hw_layer/sensors/sensors.mk
@@ -2,7 +2,7 @@ HW_SENSORS_DIR=$(PROJECT_DIR)/hw_layer/sensors
 
 HW_SENSORS_INC=$(HW_SENSORS_DIR)
 
-HW_SENSORS_SRC = \
+HW_SENSORS_CPP = \
 	$(HW_SENSORS_DIR)/cj125.cpp \
 	$(HW_SENSORS_DIR)/cj125_logic.cpp \
 	$(HW_SENSORS_DIR)/yaw_rate_sensor.cpp \
@@ -10,5 +10,4 @@ HW_SENSORS_SRC = \
 	$(HW_SENSORS_DIR)/joystick.cpp \
 	$(HW_SENSORS_DIR)/lps25.cpp \
 	$(HW_SENSORS_DIR)/max31855.cpp \
-	$(HW_SENSORS_DIR)/gps_uart.c
-	
+	$(HW_SENSORS_DIR)/gps_uart.cpp
