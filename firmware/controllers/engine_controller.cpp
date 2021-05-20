5cb659a7b005a07674bdc9be39b41219457620db
Matthew Kennedy <matthewkennedy@outlook.com>
2021-05-20 16:01:17 -0700

    lua pwm hooks (#2687)
    * pwm hook
    
    * dedicated pins
    
    * write hook
    
    * UI
    
    * more info
    
    * zero based, clamp
    
    * memory
    
    * don't compile lua in bootloader at all

--- firmware/controllers/engine_controller.cpp
+++ firmware/controllers/engine_controller.cpp
@@ -702,7 +702,7 @@ void initEngineContoller(DECLARE_ENGINE_PARAMETER_SUFFIX) {
  * UNUSED_SIZE constants.
  */
 #ifndef RAM_UNUSED_SIZE
-#define RAM_UNUSED_SIZE 1800
+#define RAM_UNUSED_SIZE 1500
 #endif
 #ifndef CCM_UNUSED_SIZE
 #define CCM_UNUSED_SIZE 300
