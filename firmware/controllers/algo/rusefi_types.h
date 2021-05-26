12f329bd2707f8a9bb306749a49211e7918680d5
Matthew Kennedy <matthewkennedy@outlook.com>
2021-05-25 20:43:18 -0700

    fix

--- firmware/controllers/algo/rusefi_types.h
+++ firmware/controllers/algo/rusefi_types.h
@@ -121,6 +121,10 @@ typedef ignition_table_t angle_table_t;
 
 typedef uint32_t cylinders_count_t;
 
+typedef int16_t fsio_pwm_freq_t;
+
+typedef float fsio_setting_t;
+
 typedef brain_pin_e brain_input_pin_e;
 typedef brain_pin_e switch_input_pin_e;
 
