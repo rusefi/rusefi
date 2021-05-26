24d35b22c574e1c804b6469b79a934df26644be0
Matthew Kennedy <matthewkennedy@outlook.com>
2021-05-25 20:26:52 -0700

    cleanup 2

--- firmware/controllers/algo/rusefi_types.h
+++ firmware/controllers/algo/rusefi_types.h
@@ -40,8 +40,6 @@ typedef time_t efitimesec_t;
  */
 typedef uint32_t efitimems_t;
 
-typedef int pid_dt;
-
 /**
  * We use a signed type here so that subtraction result is a proper negative value.
  * A typical use-case negative result is when we do 'timeNow() - timeOfEvent' where timeOfEvent
@@ -122,12 +120,6 @@ typedef uint8_t gppwm_table_t[GPPWM_LOAD_COUNT][GPPWM_RPM_COUNT];
 typedef ignition_table_t angle_table_t;
 
 typedef uint32_t cylinders_count_t;
-
-typedef int32_t bool32_t;
-
-typedef int16_t fsio_pwm_freq_t;
-
-typedef float fsio_setting_t;
 
 typedef brain_pin_e brain_input_pin_e;
 typedef brain_pin_e switch_input_pin_e;
