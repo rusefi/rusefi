65f8dbab9a1b1c4cb8eb0f92640d5be3e051152a
Matthew Kennedy <matthewkennedy@outlook.com>
2021-05-29 12:23:57 -0700

    I don't typing good

--- firmware/controllers/actuators/electronic_throttle.cpp
+++ firmware/controllers/actuators/electronic_throttle.cpp
@@ -201,7 +201,7 @@ bool EtbController::init(etb_function_e function, DcMotor *motor, pid_s *pidPara
 			return false;
 		}
 
-		if (!Sensor.isRedundant(SensorType::AcceleratorPedal)) {
+		if (!Sensor::isRedundant(SensorType::AcceleratorPedal)) {
 			firmwareError(
 				OBD_Throttle_Position_Sensor_Circuit_Malfunction,
 				"Use of electronic throttle requires accelerator pedal to be redundant."
