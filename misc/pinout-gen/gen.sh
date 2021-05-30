f47fd70a41fa46f769e4ae9b2f0a350bf5d1300f
David Holdeman <david@chuckwagoncomputing.com>
2021-05-30 14:40:47 -0500

    try

--- misc/pinout-gen/gen.sh
+++ misc/pinout-gen/gen.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)"/"
-TEXT=$(sed -e "/###CSS###/{r ${DIR}style.css" -e 'd}' -e "/###JS###/{r ${DIR}script.js" -e 'd}' ${DIR}pinout.html | sed -e "s/\/\/\/DATA\/\/\//`${1//\//\\/}`,\n\/\/\/DATA\/\/\//")
+TEXT=$(sed -e "/###CSS###/{r ${DIR}style.css" -e 'd}' -e "/###JS###/{r ${DIR}script.js" -e 'd}' ${DIR}pinout.html | sed -e "s/\/\/\/DATA\/\/\//`$(echo ${1//\//\\/} | tr -d '\n')`,\n\/\/\/DATA\/\/\//")
 echo "$TEXT" > $2
