1a55e563ef1d0a3a77a063b7879af10423302147
David Holdeman <david@chuckwagoncomputing.com>
2021-05-30 18:19:26 -0500

    Convert pinout info to JSON while building pages (#2775)
    * try converting to json first
    
    * fix command
    
    * try
    
    * try
    
    * convert to json in js

--- misc/pinout-gen/append.sh
+++ misc/pinout-gen/append.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)"/"
-TEXT=$(sed -e "/\/\/\/DATA\/\/\//{a \ \`" -e "r $1"  -e "a \ \`,\n///DATA///" -e "d}" $2)
+TEXT=$(sed -e "/\/\/\/DATA\/\/\//{a \ \`$1\ \`,\n///DATA///" -e "d}" $2)
 echo "$TEXT" > $2
