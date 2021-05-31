baa529e9dd9f5f24de272b6ce4182e3416dc5ea6
GitHub build-firmware Action <action@github.com>
2021-05-31 12:21:13 +0000

    Auto-generated configs and docs

--- firmware/config/boards/hellen/cypress/config/controllers/algo/rusefi_generated.h
+++ firmware/config/boards/hellen/cypress/config/controllers/algo/rusefi_generated.h
@@ -1277,7 +1277,7 @@
 #define showSdCardWarning_offset 76
 #define SIGNATURE_BOARD hellen_cypress
 #define SIGNATURE_DATE 2021.05.31
-#define SIGNATURE_HASH 2874388014
+#define SIGNATURE_HASH 2886136611
 #define silentTriggerError_offset 1464
 #define slowAdcAlpha_offset 2088
 #define sparkDwellRpmBins_offset 332
@@ -1630,7 +1630,7 @@
 #define ts_show_spi true
 #define ts_show_trigger_comparator false
 #define ts_show_tunerstudio_port true
-#define TS_SIGNATURE "rusEFI 2021.05.31.hellen_cypress.2874388014"
+#define TS_SIGNATURE "rusEFI 2021.05.31.hellen_cypress.2886136611"
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't' // 0x74
--- firmware/controllers/generated/engine_configuration_generated_structures.h
+++ firmware/controllers/generated/engine_configuration_generated_structures.h
@@ -1,4 +1,4 @@
-// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt Mon May 31 08:20:52 UTC 2021
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt Mon May 31 12:20:03 UTC 2021
 // by class com.rusefi.output.CHeaderConsumer
 // begin
 #pragma once
@@ -4205,4 +4205,4 @@ struct persistent_config_s {
 };
 
 // end
-// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt Mon May 31 08:20:52 UTC 2021
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt Mon May 31 12:20:03 UTC 2021
