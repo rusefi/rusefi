201f332e76339a7ca9007fc2decd5e3eb0b088f9
GitHub build-firmware Action <action@github.com>
2021-05-31 08:29:46 +0000

    Auto-generated configs and docs

--- firmware/config/boards/subaru_eg33/config/controllers/algo/engine_configuration_generated_structures.h
+++ firmware/config/boards/subaru_eg33/config/controllers/algo/engine_configuration_generated_structures.h
@@ -1,4 +1,4 @@
-// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on config/boards/subaru_eg33/config/gen_config.sh integration/rusefi_config.txt Mon May 31 08:21:17 UTC 2021
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on config/boards/subaru_eg33/config/gen_config.sh integration/rusefi_config.txt Mon May 31 08:29:03 UTC 2021
 // by class com.rusefi.output.CHeaderConsumer
 // begin
 #pragma once
@@ -4205,4 +4205,4 @@ struct persistent_config_s {
 };
 
 // end
-// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on config/boards/subaru_eg33/config/gen_config.sh integration/rusefi_config.txt Mon May 31 08:21:17 UTC 2021
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on config/boards/subaru_eg33/config/gen_config.sh integration/rusefi_config.txt Mon May 31 08:29:03 UTC 2021
