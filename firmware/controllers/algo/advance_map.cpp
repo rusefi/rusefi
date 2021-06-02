de00c7fef79e75cfdb50458ba0a8171d4b820483
Matthew Kennedy <matthewkennedy@outlook.com>
2021-06-02 02:02:52 -0700

    Bandaid for 2735 (#2788)
    * bandaid
    
    * fix test
    
    Co-authored-by: Matthew Kennedy <makenne@microsoft.com>

--- firmware/controllers/algo/advance_map.cpp
+++ firmware/controllers/algo/advance_map.cpp
@@ -38,7 +38,7 @@ static ign_Map3D_t advanceMap("advance");
 static ign_Map3D_t iatAdvanceCorrectionMap("iat corr");
 
 // todo: reset this between cranking attempts?! #2735
-static int minCrankingRpm = 0;
+int minCrankingRpm = 0;
 
 #if IGN_LOAD_COUNT == DEFAULT_IGN_LOAD_COUNT
 static const float iatTimingRpmBins[IGN_LOAD_COUNT] = {880,	1260,	1640,	2020,	2400,	2780,	3000,	3380,	3760,	4140,	4520,	5000,	5700,	6500,	7200,	8000};
