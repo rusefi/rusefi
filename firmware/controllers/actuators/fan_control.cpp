02d1f61dff20f891689d7c2de0ba82aff8216169
Matthew Kennedy <matthewkennedy@outlook.com>
2021-06-10 11:43:19 -0700

    enable fan with AC

--- firmware/controllers/actuators/fan_control.cpp
+++ firmware/controllers/actuators/fan_control.cpp
@@ -1,14 +1,19 @@
 #include "fan_control.h"
+#include "engine.h"
 
 #include "bench_test.h"
 #include "efi_gpio.h"
 #include "sensor.h"
 
-static void fanControl(OutputPin& pin, int8_t fanOnTemp, int8_t fanOffTemp) {
+EXTERN_ENGINE;
+
+static void fanControl(OutputPin& pin, int8_t fanOnTemp, int8_t fanOffTemp, bool enableWithAc DECLARE_ENGINE_PARAMETER_SUFFIX) {
 	auto [cltValid, clt] = Sensor::get(SensorType::Clt);
 
 	if (!cltValid) {
 		// If CLT is broken, turn the fan on
+		pin.setValue(true);
+	} else if (enableWithAc && ENGINE(acSwitchState)) {
 		pin.setValue(true);
 	} else if (clt > fanOnTemp) {
 		// If hot, turn the fan on
@@ -21,8 +26,6 @@ static void fanControl(OutputPin& pin, int8_t fanOnTemp, int8_t fanOffTemp) {
 	}
 }
 
-EXTERN_ENGINE;
-
 void updateFans(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 #if EFI_PROD_CODE
 	if (isRunningBenchTest()) {
@@ -30,6 +33,6 @@ void updateFans(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 	}
 #endif
 
-	fanControl(enginePins.fanRelay, CONFIG(fanOnTemperature), CONFIG(fanOffTemperature));
+	fanControl(enginePins.fanRelay, CONFIG(fanOnTemperature), CONFIG(fanOffTemperature), CONFIG(enableFan1WithAc) PASS_ENGINE_PARAMETER_SUFFIX);
 	//fanControl(enginePins.fanRelay2, CONFIG(fan2OnTemperature), CONFIG(fan2OffTemperature));
 }
--- unit_tests/tests/test_fan_control.cpp
+++ unit_tests/tests/test_fan_control.cpp
@@ -8,6 +8,8 @@ TEST(FanControl, fan1) {
 
 	engineConfiguration->fanOnTemperature = 90;
 	engineConfiguration->fanOffTemperature = 80;
+	engineConfiguration->enableFan1WithAc = false;
+	engine->acSwitchState = false;
 
 	// Cold, fan should be off
 	Sensor::setMockValue(SensorType::Clt, 75);
@@ -31,6 +33,17 @@ TEST(FanControl, fan1) {
 
 	// Below threshold, should turn off
 	Sensor::setMockValue(SensorType::Clt, 75);
+	updateFans(PASS_ENGINE_PARAMETER_SIGNATURE);
+	EXPECT_EQ(false, enginePins.fanRelay.getLogicValue());
+
+	engineConfiguration->enableFan1WithAc = true;
+	// Now AC is on, fan should turn on!
+	engine->acSwitchState = true;
+	updateFans(PASS_ENGINE_PARAMETER_SIGNATURE);
+	EXPECT_EQ(true, enginePins.fanRelay.getLogicValue());
+
+	// Turn off AC, fan should turn off too.
+	engine->acSwitchState = false;
 	updateFans(PASS_ENGINE_PARAMETER_SIGNATURE);
 	EXPECT_EQ(false, enginePins.fanRelay.getLogicValue());
 }
