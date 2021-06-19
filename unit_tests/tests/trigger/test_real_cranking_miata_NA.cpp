2c66d74c7e7c256d465237c59f47bd5631481170
Andrey <rusefillc@gmail.com>
2021-06-18 22:52:01 -0400

    Trigger: warning code on normal engine start up #669

--- unit_tests/tests/trigger/test_real_cranking_miata_NA.cpp
+++ unit_tests/tests/trigger/test_real_cranking_miata_NA.cpp
@@ -8,7 +8,34 @@
 #include "engine_test_helper.h"
 
 TEST(cranking, realNA) {
-	FILE * fp = fopen("tests/trigger/recourses/cranking_na_3.csv", "r");
+	FILE *fp = fopen("tests/trigger/recourses/cranking_na_3.csv", "r");
+
+	char *line = nullptr;
+	size_t len = 0;
+	ssize_t read;
+
+	int index = -1;
+	while ((read = getline(&line, &len, fp)) != -1) {
+		index++;
+		if (index == 0) {
+			// skip header
+			continue;
+		}
+		const char s[2] = ",";
+
+		char *token;
+
+		/* get the first token */
+		token = strtok(line, s);
+
+		/* walk through other tokens */
+		while (token != NULL) {
+//			printf(" %s\n", token);
+
+			token = strtok(NULL, s);
+		}
+
+	}
 
 	ASSERT_TRUE(fp != nullptr);
 }
