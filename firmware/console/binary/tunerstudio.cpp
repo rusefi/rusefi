498358247e945211a2cdffdbda2f9225dd01afa6
Andrey <rusefillc@gmail.com>
2021-06-21 09:27:09 -0400

    performance optimization by dron0gus

--- firmware/console/binary/tunerstudio.cpp
+++ firmware/console/binary/tunerstudio.cpp
@@ -323,7 +323,9 @@ static void handleCrc32Check(TsChannelBase *tsChannel, ts_response_format_e mode
 
 	const char* start = getWorkingPageAddr() + offset;
 
-	uint32_t crc = SWAP_UINT32(crc32(start, count));
+	uint32_t crc = crc32(start, count);
+	// do not pass function as macro parameter that would cause same code to be invoked 4 times!
+	crc = SWAP_UINT32(crc);
 	tsChannel->sendResponse(mode, (const uint8_t *) &crc, 4);
 }
 
