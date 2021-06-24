448a1156ce55fb9d678b310a8e5c9bb2a559ec40
Matthew Kennedy <matthewkennedy@outlook.com>
2021-06-23 18:38:53 -0700

    ts writer copies all lines input -> output

--- java_tools/configuration_definition/src/com/rusefi/newparse/outputs/TsWriter.java
+++ java_tools/configuration_definition/src/com/rusefi/newparse/outputs/TsWriter.java
@@ -3,17 +3,26 @@ package com.rusefi.newparse.outputs;
 import com.rusefi.newparse.ParseState;
 import com.rusefi.newparse.layout.StructLayout;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
+import java.io.*;
 
 public class TsWriter {
-    public void writeTunerstudio(ParseState parser, String inputFile, String outputFile) throws FileNotFoundException {
-        // Write to file!
+    public void writeTunerstudio(ParseState parser, String inputFile, String outputFile) throws FileNotFoundException, IOException {
+        BufferedReader is = new BufferedReader(new FileReader(inputFile));
         PrintStream ps = new PrintStream(new FileOutputStream(outputFile));
 
-        writeLayoutAndComments(parser, ps);
+        while (is.ready()) {
+            String line = is.readLine();
 
+            if (line.contains("CONFIG_DEFINITION_START")) {
+                writeLayoutAndComments(parser, ps);
+                continue;
+            }
+
+            // Copy the line to the output stream
+            ps.println(line);
+        }
+
+        is.close();
         ps.close();
     }
 
@@ -22,8 +31,11 @@ public class TsWriter {
         TsMetadata meta = new TsMetadata();
 
         // Print configuration layout
+        int size = root.getSize();
+        ps.println("pageSize            = " + size);
+        ps.println("page = 1");
         root.writeTunerstudioLayout(ps, meta);
-        ps.println("; total TS size = " + root.getSize());
+        ps.println("; total TS size = " + size);
 
         // Print context help
         ps.println("[SettingContextHelp]");
