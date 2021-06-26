04096e57b8365a7d3071b6d929626dfdcd32a607
rusefillc <sdfsdfqsf2334234234>
2021-06-26 01:31:50 -0400

    VW warnings on cranking #2861

--- unit_tests/tests/trigger/test_real_volkswagen.cpp
+++ unit_tests/tests/trigger/test_real_volkswagen.cpp
@@ -11,15 +11,16 @@
 
 TEST(crankingVW, vwRealCrankingFromFile) {
 	CsvReader reader;
+	reader.triggerCount = 1;
 	int indeces[1] = {0};
 
 	reader.open("tests/trigger/recourses/nick_1.csv", indeces);
 	WITH_ENGINE_TEST_HELPER (VW_ABA);
-	/*
 
 	while (reader.haveMore()) {
 		reader.processLine(&eth);
 	}
+	/*
 	ASSERT_EQ( 0, eth.recentWarnings()->getCount())<< "warningCounter#vwRealCranking";
 	ASSERT_EQ( 560, GET_RPM())<< reader.lineIndex;
 */
