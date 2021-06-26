c1eda5dec859275e961635f43df75293ff745e34
Andrey <rusefillc@gmail.com>
2021-06-26 16:05:43 -0400

    VW warnings on cranking #2861

--- unit_tests/tests/trigger/test_real_volkswagen.cpp
+++ unit_tests/tests/trigger/test_real_volkswagen.cpp
@@ -13,15 +13,17 @@ TEST(crankingVW, vwRealCrankingFromFile) {
 	CsvReader reader;
 	reader.triggerCount = 1;
 	int indeces[1] = {0};
+//	setVerboseTrigger(true);
 
 	reader.open("tests/trigger/recourses/nick_1.csv", indeces);
 	WITH_ENGINE_TEST_HELPER (VW_ABA);
+	eth.setTriggerType(TT_60_2_VW PASS_ENGINE_PARAMETER_SUFFIX);
 
 	while (reader.haveMore()) {
 		reader.processLine(&eth);
 	}
 
-	ASSERT_EQ( 0, eth.recentWarnings()->getCount())<< "warningCounter#vwRealCranking";
-	ASSERT_EQ( 0, GET_RPM())<< reader.lineIndex;
+	ASSERT_EQ( 3, eth.recentWarnings()->getCount())<< "warningCounter#vwRealCranking";
+	ASSERT_EQ( 1683, GET_RPM())<< reader.lineIndex;
 
 }
