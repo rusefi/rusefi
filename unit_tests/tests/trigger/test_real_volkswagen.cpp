ec1c2fd50ba7895f905af70b50983455d983bb7f
Andrey <rusefillc@gmail.com>
2021-06-26 00:51:09 -0400

    VW warnings on cranking #2861

---
+++ unit_tests/tests/trigger/test_real_volkswagen.cpp
@@ -0,0 +1,26 @@
+/*
+ * @file test_real_volkswagen.cpp
+ *
+ * @date Jun 26, 2021
+ * @author Andrey Belomutskiy, (c) 2012-2021
+ */
+
+
+#include "engine_test_helper.h"
+#include "logicdata_csv_reader.h"
+
+TEST(crankingVW, vwRealCrankingFromFile) {
+	CsvReader reader;
+	int indeces[1] = {0};
+
+	reader.open("tests/trigger/recourses/nick_1.csv", indeces);
+	WITH_ENGINE_TEST_HELPER (VW_ABA);
+	/*
+
+	while (reader.haveMore()) {
+		reader.processLine(&eth);
+	}
+	ASSERT_EQ( 0, eth.recentWarnings()->getCount())<< "warningCounter#vwRealCranking";
+	ASSERT_EQ( 560, GET_RPM())<< reader.lineIndex;
+*/
+}
