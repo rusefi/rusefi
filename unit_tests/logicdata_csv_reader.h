f2db7755861b8493e6b0dcd69af9edc9376efa2a
Andrey <rusefillc@gmail.com>
2021-06-26 00:15:55 -0400

    progress

---
+++ unit_tests/logicdata_csv_reader.h
@@ -0,0 +1,14 @@
+/*
+ * @file logicdata_csv_reader.h
+ *
+ * @date Jun 26, 2021
+ * @author Andrey Belomutskiy, (c) 2012-2021
+ */
+
+
+class CsvReader {
+public:
+	void open(char *fileName, int * columnIndeces);
+
+};
+
