5c95fba778e3fd5d12cf5f4d06550ad1c3e63b1a
GitHub build-firmware Action <action@github.com>
2021-06-27 16:16:51 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_f429-discovery.h
+++ firmware/controllers/generated/signature_f429-discovery.h
@@ -2,7 +2,7 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_BOARD all
-#define SIGNATURE_DATE 2021.06.23
-#define SIGNATURE_HASH 3466233957
-#define TS_SIGNATURE "rusEFI 2021.06.23.all.3466233957"
+#define SIGNATURE_BOARD f429-discovery
+#define SIGNATURE_DATE 2021.06.27
+#define SIGNATURE_HASH 2363904949
+#define TS_SIGNATURE "rusEFI 2021.06.27.f429-discovery.2363904949"
