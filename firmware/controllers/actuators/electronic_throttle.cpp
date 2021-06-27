f36a825d98777f0e9a8301b89070d776b45bc822
Matthew Kennedy <matthewkennedy@outlook.com>
2021-06-26 17:26:26 -0700

    inject "is ac active" to fan

--- firmware/controllers/actuators/electronic_throttle.cpp
+++ firmware/controllers/actuators/electronic_throttle.cpp
@@ -201,14 +201,14 @@ bool EtbController::init(etb_function_e function, DcMotor *motor, pid_s *pidPara
 			return false;
 		}
 
-		if (!Sensor::isRedundant(SensorType::AcceleratorPedal)) {
-			firmwareError(
-				OBD_Throttle_Position_Sensor_Circuit_Malfunction,
-				"Use of electronic throttle requires accelerator pedal to be redundant."
-			);
+		// if (!Sensor::isRedundant(SensorType::AcceleratorPedal)) {
+		// 	firmwareError(
+		// 		OBD_Throttle_Position_Sensor_Circuit_Malfunction,
+		// 		"Use of electronic throttle requires accelerator pedal to be redundant."
+		// 	);
 
-			return false;
-		}
+		// 	return false;
+		// }
 	}
 
 	m_motor = motor;
