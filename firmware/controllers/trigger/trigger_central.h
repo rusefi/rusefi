8185eb2de332e555cae9bad6415a9b093e33010a
Andrey <rusefillc@gmail.com>
2021-06-27 13:00:01 -0400

    refactoring maybe making code reuse more possible in the future

--- firmware/controllers/trigger/trigger_central.h
+++ firmware/controllers/trigger/trigger_central.h
@@ -87,7 +87,7 @@ public:
 
 void triggerInfo(void);
 void handleShaftSignal2(trigger_event_e signal, efitick_t timestamp DECLARE_ENGINE_PARAMETER_SUFFIX);
-void hwHandleShaftSignal(trigger_event_e signal, efitick_t timestamp);
+void hwHandleShaftSignal(trigger_event_e signal, efitick_t timestamp DECLARE_ENGINE_PARAMETER_SUFFIX);
 void hwHandleVvtCamSignal(trigger_value_e front, efitick_t timestamp, int index DECLARE_ENGINE_PARAMETER_SUFFIX);
 
 void initTriggerCentral();
