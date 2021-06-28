70bc5b2375648562386a9eb399dc42c304aa7e20
Andrey <rusefillc@gmail.com>
2021-06-28 19:47:11 -0400

    Review default H64 tune #2815

--- firmware/config/boards/hellen/hellen64_miataNA6_94/board_configuration.cpp
+++ firmware/config/boards/hellen/hellen64_miataNA6_94/board_configuration.cpp
@@ -151,6 +151,7 @@ void setBoardDefaultConfiguration(void) {
 	engineConfiguration->canTxPin = GPIOD_1;
 	engineConfiguration->canRxPin = GPIOD_0;
 
+	engineConfiguration->acRelayPin = GPIOH_15; // 1J - AC Relay
 	engineConfiguration->fuelPumpPin = GPIOG_2;	// OUT_IO9
 	engineConfiguration->idle.solenoidPin = GPIOD_14;	// OUT_PWM5
 	engineConfiguration->fanPin = GPIOD_12;	// OUT_PWM8
