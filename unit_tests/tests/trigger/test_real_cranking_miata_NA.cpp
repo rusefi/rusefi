025c32de004c37fa1f48543991f94a87610d9a9d
Matthew Kennedy <matthewkennedy@outlook.com>
2021-07-01 18:29:04 -0700

    Stricter miata sync (#2891)
    * strict mazda trigger shape
    
    * this is an even better gap
    
    * sync point moved so things are different now
    
    * fix
    
    * update instantrpm in test
    
    * s
    
    * s
    
    * this is safer
    
    * test
    
    * tests
    
    * instant rpm
    
    * cleanup

--- unit_tests/tests/trigger/test_real_cranking_miata_NA.cpp
+++ unit_tests/tests/trigger/test_real_cranking_miata_NA.cpp
@@ -18,32 +18,33 @@ TEST(cranking, realCrankingFromFile) {
 
 	ssize_t read;
 
-	for (int i = 0; i < 18; i++) {
+	for (int i = 0; i < 23; i++) {
 		reader.readLine(&eth);
 	}
 
-	ASSERT_EQ( 229, GET_RPM())<< reader.lineIndex() << " @ 0";
-	ASSERT_EQ( 0, eth.recentWarnings()->getCount())<< "warningCounter#got synch";
+	EXPECT_EQ(0, GET_RPM())<< reader.lineIndex();
+	EXPECT_EQ( 0, eth.recentWarnings()->getCount());
 
+	// This tooth should be first sync point
 	reader.readLine(&eth);
 
-	ASSERT_EQ( 213, GET_RPM())<< reader.lineIndex() << " @ 1";
+	EXPECT_EQ(232, GET_RPM())<< reader.lineIndex() << " @ 1";
 
 	for (int i = 0; i < 30; i++) {
 		reader.readLine(&eth);
 	}
-	ASSERT_EQ( 223, GET_RPM())<< reader.lineIndex();
+	ASSERT_EQ(224, GET_RPM())<< reader.lineIndex();
 
 
 	for (int i = 0; i < 30; i++) {
 		reader.readLine(&eth);
 	}
-	ASSERT_EQ( 297, GET_RPM())<< reader.lineIndex() << " @ 2";
+	ASSERT_EQ(456, GET_RPM())<< reader.lineIndex() << " @ 2";
 
 	while (reader.haveMore()) {
 		reader.processLine(&eth);
 	}
 
 	ASSERT_EQ(0, eth.recentWarnings()->getCount())<< "warningCounter#realCranking";
-	ASSERT_EQ(560, GET_RPM())<< reader.lineIndex();
+	ASSERT_EQ(407, GET_RPM())<< reader.lineIndex();
 }
