fb7639f34740a2962853f37302f67736f09f401b
Andrey <rusefillc@gmail.com>
2021-07-03 12:26:31 -0400

    Nissan VVT trigger decoder #2887

--- firmware/controllers/trigger/trigger_central.cpp
+++ firmware/controllers/trigger/trigger_central.cpp
@@ -710,8 +710,14 @@ static void resetRunningTriggerCounters() {
 
 void onConfigurationChangeTriggerCallback(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 	bool changed = false;
-	for (size_t i = 0; i < efi::size(CONFIG(camInputs)); i++) {
-		changed |= isConfigurationChanged(camInputs[i]);
+	// todo: how do we static_assert here?
+	efiAssertVoid(OBD_PCM_Processor_Fault, efi::size(CONFIG(camInputs)) == efi::size(CONFIG(vvtMode)), "sizes");
+	efiAssertVoid(OBD_PCM_Processor_Fault, efi::size(CONFIG(camInputs)) == efi::size(CONFIG(vvtOffsets)), "sizes");
+
+	for (size_t camIndex = 0; camIndex < efi::size(CONFIG(camInputs)); camIndex++) {
+		changed |= isConfigurationChanged(camInputs[camIndex]);
+		changed |= isConfigurationChanged(vvtOffsets[camIndex]);
+		changed |= isConfigurationChanged(vvtMode[camIndex]);
 	}
 
 	for (size_t i = 0; i < efi::size(CONFIG(triggerGapOverride)); i++) {
@@ -722,10 +728,6 @@ void onConfigurationChangeTriggerCallback(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 		changed |= isConfigurationChanged(triggerInputPins[i]);
 	}
 
-	for (size_t i = 0; i < efi::size(CONFIG(vvtMode)); i++) {
-		changed |= isConfigurationChanged(vvtMode[i]);
-	}
-
 	changed |= isConfigurationChanged(trigger.type);
 	changed |= isConfigurationChanged(ambiguousOperationMode);
 	changed |= isConfigurationChanged(useOnlyRisingEdgeForTrigger);
@@ -734,7 +736,6 @@ void onConfigurationChangeTriggerCallback(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 	changed |= isConfigurationChanged(trigger.customSkippedToothCount);
 	changed |= isConfigurationChanged(vvtCamSensorUseRise);
 	changed |= isConfigurationChanged(overrideTriggerGaps);
-	changed |= isConfigurationChanged(vvtOffset);
 
 	if (changed) {
 		assertEngineReference();
