88da5c09b02ca88d9e8481399ceecd4d6c43b83b
rusefillc <sdfsdfqsf2334234234>
2021-07-05 10:57:40 -0400

    helping build?

--- firmware/rusefi.mk
+++ firmware/rusefi.mk
@@ -1,6 +1,14 @@
 
 ifeq ("$(wildcard $(RULESFILE))","")
-$(info Invoking "git submodule update --init")
+$(info Chibios: Invoking "git submodule update --init")
+$(shell git submodule update --init)
+$(info Invoked "git submodule update --init")
+# make is not happy about newly checked out module for some reason but next invocation would work
+$(error Please run 'make' again. Please make sure you have 'git' command in PATH)
+endif
+
+ifeq ("$(wildcard $(CHIBIOS_CONTRIB)/os/hal/hal.mk)","")
+$(info Contrib: Invoking "git submodule update --init")
 $(shell git submodule update --init)
 $(info Invoked "git submodule update --init")
 # make is not happy about newly checked out module for some reason but next invocation would work
