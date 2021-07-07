8112de5324cae5b60b43acb21374430904587e30
Matthew Kennedy <matthewkennedy@outlook.com>
2021-07-07 00:25:56 -0700

    fix

--- firmware/controllers/engine_cycle/spark_logic.cpp
+++ firmware/controllers/engine_cycle/spark_logic.cpp
@@ -97,7 +97,12 @@ static void prepareCylinderIgnitionSchedule(angle_t dwellAngleDuration, floatms_
 	IgnitionOutputPin *output = &enginePins.coils[coilIndex];
 
 	IgnitionOutputPin *secondOutput;
-	if (getCurrentIgnitionMode(PASS_ENGINE_PARAMETER_SIGNATURE) == IM_WASTED_SPARK && CONFIG(twoWireBatchIgnition)) {
+
+	// We need two outputs if:
+	//  - we are running wasted spark, and have "two wire" mode enabled
+	//  - We are running sequential mode, but we're cranking, so we should run in two wire wasted mode (not one wire wasted)
+	bool isTwoWireWasted = CONFIG(twoWireBatchIgnition) || (CONFIG(ignitionMode) == IM_SEQUENTIAL);
+	if (getCurrentIgnitionMode(PASS_ENGINE_PARAMETER_SIGNATURE) == IM_WASTED_SPARK && isTwoWireWasted) {
 		int secondIndex = index + CONFIG(specs.cylindersCount) / 2;
 		int secondCoilIndex = ID2INDEX(getCylinderId(secondIndex PASS_ENGINE_PARAMETER_SUFFIX));
 		secondOutput = &enginePins.coils[secondCoilIndex];
