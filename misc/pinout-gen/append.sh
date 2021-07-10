724127f4660a4856d792d430cbc01f4325935c10
Andrey <rusefillc@gmail.com>
2021-07-10 10:12:22 -0400

    pinouts process is broken but also green #2942

--- misc/pinout-gen/append.sh
+++ misc/pinout-gen/append.sh
@@ -2,4 +2,8 @@
 
 DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)"/"
 TEXT=$(sed -e "/\/\/\/DATA\/\/\//{a \ \`$1\ \`,\n///DATA///" -e "d}" $2)
+if [ $? -ne 0 ]; then
+  echo "Error in append.sh"
+  exit 1;
+fi
 echo "$TEXT" > $2
