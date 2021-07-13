e604edce34ca2818b1cd09e5557caf5074016419
Matthew Kennedy <makenne@microsoft.com>
2021-07-13 01:23:04 -0700

    chibios uses HSI

--- firmware/hw_layer/ports/stm32/mcuconf_common_f4_f7.h
+++ firmware/hw_layer/ports/stm32/mcuconf_common_f4_f7.h
@@ -324,11 +324,9 @@
  */
 #define STM32_WDG_USE_IWDG                  FALSE
 
-// We auto detect the value of HSE, so set the default PLLM value to the maximum,
-// so we don't accidentially overclock to processor before we know how fast HSE is
-#define STM32_PLLM_VALUE 25
-
-// This also means we have to pretend (for now) we have a 25MHz HSE fitted
+// Pretend we have a 25MHz external crystal.  This value isn't actually used since we
+// configure the PLL to start on the HSI oscillator, then compute HSE's speed at runtime
+// and reconfigure the PLL appropriately.
 #define STM32_HSECLK 25000000
 
 // After boot, we will detect the real frequency, and adjust the PLL M value to suit
--- firmware/hw_layer/ports/stm32/stm32f4/cfg/mcuconf.h
+++ firmware/hw_layer/ports/stm32/stm32f4/cfg/mcuconf.h
@@ -51,8 +51,8 @@
 
 #define STM32_CLOCK48_REQUIRED              TRUE
 #define STM32_SW                            STM32_SW_PLL
-#define STM32_PLLSRC                        STM32_PLLSRC_HSE
-
+#define STM32_PLLSRC                        STM32_PLLSRC_HSI
+#define STM32_PLLM_VALUE                    16
 #define STM32_PLLN_VALUE                    336
 #define STM32_PLLP_VALUE                    2
 #define STM32_PLLQ_VALUE                    7
