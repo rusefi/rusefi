7fd2e8275f9dc93550a1577e66aa087327fcecc1
Andrey <rusefillc@gmail.com>
2021-07-13 14:39:21 -0400

    NB2 hellen-72-rev-d something is off #2958

--- unit_tests/tests/trigger/test_real_nb2_cranking.cpp
+++ unit_tests/tests/trigger/test_real_nb2_cranking.cpp
@@ -10,6 +10,17 @@
 #include "logicdata_csv_reader.h"
 
 TEST(crankingNB2, nb2RealCrankingFromFile) {
+	CsvReader reader(1, /* vvtCount */ 1);
+	int indeces[1] = {0};
+
+	reader.open("tests/trigger/resources/nb2_rev-d-4.csv", indeces);
+	WITH_ENGINE_TEST_HELPER (ET_HELLEN_NB2);
+
+	while (eth.getTimeNowUs() < 3'028'987) {
+		reader.processLine(&eth);
+		ASSERT_EQ(0, GET_RPM()) << "At line " << reader.lineIndex() << " time " << eth.getTimeNowUs();
+	}
+	ASSERT_EQ(243, GET_RPM()) << "At line " << reader.lineIndex() << " time " << eth.getTimeNowUs();
 
 
 }
