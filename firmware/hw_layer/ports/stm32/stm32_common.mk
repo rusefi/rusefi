c30064cd15814b523e0e4ce94b78419db31ed249
Andrey <rusefillc@gmail.com>
2021-07-15 01:17:42 -0400

    progress

--- firmware/hw_layer/ports/stm32/stm32_common.mk
+++ firmware/hw_layer/ports/stm32/stm32_common.mk
@@ -1,8 +1,8 @@
 HW_LAYER_EMS += $(PROJECT_DIR)/hw_layer/ports/stm32/serial_over_usb/usbcfg.c \
-	            $(PROJECT_DIR)/hw_layer/ports/stm32/serial_over_usb/usbconsole.c \
 				$(PROJECT_DIR)/hw_layer/ports/stm32/flash_int.c \
 
 HW_LAYER_EMS_CPP += \
+    $(PROJECT_DIR)/hw_layer/ports/stm32/serial_over_usb/usbconsole.cpp \
 	$(PROJECT_DIR)/hw_layer/ports/stm32/stm32_pins.cpp \
 	$(PROJECT_DIR)/hw_layer/ports/stm32/stm32_common.cpp \
 	$(PROJECT_DIR)/hw_layer/ports/stm32/backup_ram.cpp \
