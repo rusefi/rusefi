45d185b09d1cdab5196e8f82a80cd6cafd28ac7b
Matthew Kennedy <matthewkennedy@outlook.com>
2021-07-16 11:06:26 -0700

    lua hooks for CAN tx (#2967)
    * CanTxMessage usable from unit test
    
    * testLuaReturnsNil
    
    * CAN hook and tests
    
    * it helps if parameter names exist
    
    * nameless params
    
    * s
    
    * redefinition of default argument
    
    Co-authored-by: Matthew Kennedy <makenne@microsoft.com>

--- firmware/hw_layer/drivers/drivers.mk
+++ firmware/hw_layer/drivers/drivers.mk
@@ -13,12 +13,12 @@ HW_LAYER_DRIVERS_CORE = \
 HW_LAYER_DRIVERS_CORE_CPP = \
 	$(DRIVERS_DIR)/gpio/core.cpp \
 	$(DRIVERS_DIR)/i2c/i2c_bb.cpp \
+	$(DRIVERS_DIR)/can/can_msg_tx.cpp \
 
 HW_LAYER_DRIVERS =
 
 HW_LAYER_DRIVERS_CPP = \
 	$(DRIVERS_DIR)/can/can_hw.cpp \
-	$(DRIVERS_DIR)/can/can_msg_tx.cpp \
 	$(DRIVERS_DIR)/serial/serial_hw.cpp \
 	$(DRIVERS_DIR)/gpio/tle6240.cpp \
 	$(DRIVERS_DIR)/gpio/tle8888.cpp \
