6a12627f3916fa9dcebfb59e15efb0d79c30d169
Matthew Kennedy <matthewkennedy@outlook.com>
2021-07-16 11:19:44 -0700

    put this where it belongs

--- firmware/controllers/algo/engine_configuration.h
+++ firmware/controllers/algo/engine_configuration.h
@@ -64,6 +64,7 @@ void setBoardConfigOverrides(void);
 extern engine_configuration_s *engineConfiguration;
 extern persistent_config_container_s persistentState;
 extern persistent_config_s *config;
+#endif // EFI_UNIT_TEST
 
 /**
  * & is reference in C++ (not C)
@@ -73,4 +74,16 @@ extern persistent_config_s *config;
  * This is about EFI_ACTIVE_CONFIGURATION_IN_FLASH
  */
 extern engine_configuration_s & activeConfiguration;
-#endif // EFI_UNIT_TEST
+
+#if ! EFI_ACTIVE_CONFIGURATION_IN_FLASH
+// We store a special changeable copy of configuration is RAM, so we can just compare them
+#define isConfigurationChanged(x) (engineConfiguration->x != activeConfiguration.x)
+#else
+// We cannot call prepareVoidConfiguration() for activeConfiguration if it's stored in flash,
+// so we need to tell the firmware that it's "void" (i.e. zeroed, invalid) by setting a special flag variable,
+// and then we consider 'x' as changed if it's just non-zero.
+extern bool isActiveConfigurationVoid;
+#define isConfigurationChanged(x) ((engineConfiguration->x != activeConfiguration.x) || (isActiveConfigurationVoid && engineConfiguration->x != 0))
+#endif /* EFI_ACTIVE_CONFIGURATION_IN_FLASH */
+
+#define isPinOrModeChanged(pin, mode) (isConfigurationChanged(pin) || isConfigurationChanged(mode))
