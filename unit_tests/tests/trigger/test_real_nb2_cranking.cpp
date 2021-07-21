328be1d14e16117301520b6671f18513336e1025
Matthew Kennedy <matthewkennedy@outlook.com>
2021-07-21 10:46:31 -0700

    test a second log too

--- unit_tests/tests/trigger/test_real_nb2_cranking.cpp
+++ unit_tests/tests/trigger/test_real_nb2_cranking.cpp
@@ -28,3 +28,23 @@ TEST(realCrankingNB2, normalCranking) {
 	// TODO: why warnings?
 	ASSERT_EQ(3, eth.recentWarnings()->getCount());
 }
+
+TEST(realCrankingNB2, crankingMissingInjector) {
+	CsvReader reader(1, /* vvtCount */ 1);
+	int indeces[] = {0};
+
+	reader.open("tests/trigger/resources/nb2-cranking-good-missing-injector-1.csv", indeces);
+	WITH_ENGINE_TEST_HELPER (HELLEN_NB2);
+
+	while (reader.haveMore()) {
+		reader.processLine(&eth);
+	}
+
+	// VVT position nearly zero!
+	EXPECT_NEAR(engine->triggerCentral.getVVTPosition(0, 0), -7.1926f, 1e-4);
+
+	ASSERT_EQ(668, GET_RPM());
+
+	// TODO: why warnings?
+	ASSERT_EQ(2, eth.recentWarnings()->getCount());
+}
