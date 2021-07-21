0f0b84a6dbc81977b3ff5dbbcc84ea7ec2fd5b55
Matthew Kennedy <matthewkennedy@outlook.com>
2021-07-21 11:24:53 -0700

    remove TS file read (#3004)
    * remove
    
    * more defines
    
    * test
    
    * lots of java
    
    * s

--- java_console/io/src/main/java/com/rusefi/binaryprotocol/BinaryProtocol.java
+++ java_console/io/src/main/java/com/rusefi/binaryprotocol/BinaryProtocol.java
@@ -88,10 +88,6 @@ public class BinaryProtocol {
 
     public static String findCommand(byte command) {
         switch (command) {
-            case Fields.TS_SD_R_COMMAND:
-                return "SD_R_COMMAND";
-            case Fields.TS_SD_W_COMMAND:
-                return "SD_W_COMMAND";
             case Fields.TS_PAGE_COMMAND:
                 return "PAGE";
             case Fields.TS_COMMAND_F:
