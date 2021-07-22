2a07c3df27f9e165f061478a8a47d360069d0092
Andrey <rusefillc@gmail.com>
2021-07-21 23:08:56 -0400

    Nissan VVT trigger decoder #2887

---
+++ unit_tests/tests/trigger/test_real_cranking_nissan_vq40.cpp
@@ -0,0 +1,27 @@
+/*
+ * @file test_real_cranking_nissan_vq40.cpp
+ *
+ * @date Jul 21, 2021
+ * @author Andrey Belomutskiy, (c) 2012-2021
+ */
+
+#include "engine_test_helper.h"
+#include "logicdata_csv_reader.h"
+
+TEST(realCrankingVQ40, normalCranking) {
+	CsvReader reader(1, /* vvtCount */ 2);
+	int indeces[] = {0};
+
+	reader.open("tests/trigger/resources/nissan_vq40_cranking-1.csv", indeces);
+	WITH_ENGINE_TEST_HELPER (ET_HELLEN_121_NISSAN);
+
+	while (reader.haveMore()) {
+		reader.processLine(&eth);
+	}
+
+	EXPECT_NEAR(engine->triggerCentral.getVVTPosition(/*bankIndex*/0, /*camIndex*/0), -46.817, 1e-2);
+	// todo: read second channel
+	EXPECT_NEAR(engine->triggerCentral.getVVTPosition(/*bankIndex*/1, /*camIndex*/0), 0, 1e-2);
+	ASSERT_EQ(241, GET_RPM())<< reader.lineIndex();
+
+}
