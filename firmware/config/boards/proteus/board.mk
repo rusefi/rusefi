a907170e504af9aed3157a0d9eeac39aae531eb4
Matthew Kennedy <matthewkennedy@outlook.com>
2021-07-25 16:03:38 -0700

    also unnecessary now

--- firmware/config/boards/proteus/board.mk
+++ firmware/config/boards/proteus/board.mk
@@ -29,7 +29,9 @@ ifneq ($(PROJECT_CPU),ARCH_STM32H7)
 endif
 
 # disable hardware serial ports on H7
-ifneq ($(PROJECT_CPU),ARCH_STM32H7)
+ifeq ($(PROJECT_CPU),ARCH_STM32H7)
+	DDEFS += -DTS_NO_PRIMARY -DTS_NO_SECONDARY
+else
 	# Hardware serial port on UART 2 -> PD5/PD6
 	DDEFS += -DSTM32_UART_USE_USART2=TRUE
 	DDEFS += -DTS_PRIMARY_UART=UARTD2
