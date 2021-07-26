9a2c23aea8dbad72e49141912defeecf66738d81
Matthew Kennedy <matthewkennedy@outlook.com>
2021-07-25 19:28:30 -0700

    apparently board.mk can't set cpu

--- firmware/config/boards/atlas/board.mk
+++ firmware/config/boards/atlas/board.mk
@@ -3,8 +3,6 @@ BOARDCPPSRC =  $(PROJECT_DIR)/config/boards/atlas/board_configuration.cpp
 
 BOARDINC = $(PROJECT_DIR)/config/boards/atlas
 
-PROJECT_CPU = ARCH_STM32H7
-
 #DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=GPIOE_3
 DDEFS += -DFIRMWARE_ID=\"atlas\" -DDEFAULT_ENGINE_TYPE=MINIMAL_PINS
 
--- firmware/config/boards/atlas/compile_atlas.sh
+++ firmware/config/boards/atlas/compile_atlas.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export PROJECT_BOARD=atlas
+export PROJECT_CPU = ARCH_STM32H7
 
 bash ../common_make.sh
