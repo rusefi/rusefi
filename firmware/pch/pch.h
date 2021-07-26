7239f43bea44337c9ce08a6bc5fe00a1c17a7fc4
Matthew Kennedy <matthewkennedy@outlook.com>
2021-07-25 18:05:41 -0700

    comment

--- firmware/pch/pch.h
+++ firmware/pch/pch.h
@@ -1,3 +1,14 @@
+/**
+ * @file	pch.h
+ * 
+ * This file is rusEFI's precompiled header. Most cpp files should include
+ * this file in lieu of any of the files below.
+ * 
+ * This helps compilation performance by parsing/analyzing these files only
+ * once, then re-loading that precompiled data for every file that includes
+ * this file.
+ */
+
 #include "efifeatures.h"
 #include "engine_ptr.h"
 #include "global.h"
