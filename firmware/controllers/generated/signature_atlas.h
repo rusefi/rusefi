0401fd0625251bbfac9e214d2c2cde140d5118fd
GitHub build-firmware Action <action@github.com>
2021-07-26 16:27:03 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_atlas.h
+++ firmware/controllers/generated/signature_atlas.h
@@ -1,2 +1,8 @@
-hello Atlas
-see https://github.com/rusefi/rusefi/blob/master/firmware/config/boards/readme.md
+//
+// was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
+//
+
+#define SIGNATURE_BOARD atlas
+#define SIGNATURE_DATE 2021.07.26
+#define SIGNATURE_HASH 656786215
+#define TS_SIGNATURE "rusEFI 2021.07.26.atlas.656786215"
