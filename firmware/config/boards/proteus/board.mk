f44a84b40c78863d8d42f7f4c8d29c645780d4cd
Matthew Kennedy <matthewkennedy@outlook.com>
2021-07-25 18:23:23 -0700

    set up precompiled header (#2971)
    * ignore
    
    * set up precompiled header
    
    * makefile
    
    * fixes
    
    * cleanup
    
    * only depend cpp on pch
    
    * efifeatures in pch
    
    * f7 efifeatures
    
    * consolidate
    
    * simulator
    
    * proteus build
    
    * error handling is a good one
    
    * bootloader pch
    
    * don't auto include pch
    
    * dirs
    
    * delete on clean
    
    * bootloader cleans
    
    * clean.sh deletes pch
    
    * disable in bl for now
    
    * there was a typo so maybe it'll work now
    
    * remove todo
    
    * don't need that any more
    
    * don't need these parts
    
    * don't need this part
    
    * undo changes
    
    * use a different cpp as example
    
    * s
    
    * Revert "cleanup"
    
    This reverts commit 8de8698490f5d09c2df753f00a89751aa898be9a.
    
    * also unnecessary now
    
    * buh
    
    * comment
    
    Co-authored-by: Matthew Kennedy <makenne@microsoft.com>

--- firmware/config/boards/proteus/board.mk
+++ firmware/config/boards/proteus/board.mk
@@ -30,7 +30,7 @@ endif
 
 # disable hardware serial ports on H7
 ifeq ($(PROJECT_CPU),ARCH_STM32H7)
-  DDEFS += -DTS_NO_PRIMARY -DTS_NO_SECONDARY
+	DDEFS += -DTS_NO_PRIMARY -DTS_NO_SECONDARY
 else
 	# Hardware serial port on UART 2 -> PD5/PD6
 	DDEFS += -DSTM32_UART_USE_USART2=TRUE
