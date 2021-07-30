555d9dfe4e7a6a3f8368309c3540ea79ac83afe1
Lenar Khannanov <55162739+Come1LLF00@users.noreply.github.com>
2021-07-30 15:15:32 +0300

    Feature 3014 migrating to gradle (#3079)
    * feat: restructured autotest for migrating to gradle
    
    * feat: restructured enum_to_string for migrating to gradle
    
    * feat: restructured configuration_definition for migrating to gradle; fix: corrected build.xml
    
    * feat: restructured logging for migrating to gradle; fix: corrected build.xml's
    
    * feat: restructured romraider for migrating to gradle; fix: corrected build.xml's
    
    * feat: restructured autotest for migrating to gradle
    
    * feat: restructured enum_to_string for migrating to gradle
    
    * feat: restructured configuration_definition for migrating to gradle; fix: corrected build.xml
    
    * feat: restructured logging for migrating to gradle; fix: corrected build.xml's
    
    * feat: restructured romraider for migrating to gradle; fix: corrected build.xml's
    
    * feat: restructured autotest for migrating to gradle
    
    * feat: restructured enum_to_string for migrating to gradle
    
    * feat: restructured configuration_definition for migrating to gradle; fix: corrected build.xml
    
    * feat: restructured logging for migrating to gradle; fix: corrected build.xml's
    
    * feat: restructured romraider for migrating to gradle; fix: corrected build.xml's
    
    * feat: restructured enum_to_string for migrating to gradle
    
    * fix: corrected main build.xml
    
    * fix: corrected main build.xml for romraider
    
    * fix: corrected main build.xml for romraider
    
    * feat: restructured autotest for migrating to gradle
    
    * feat: restructured enum_to_string for migrating to gradle
    
    * feat: restructured configuration_definition for migrating to gradle; fix: corrected build.xml
    
    * feat: restructured logging for migrating to gradle; fix: corrected build.xml's
    
    * feat: restructured romraider for migrating to gradle; fix: corrected build.xml's
    
    * feat: restructured enum_to_string for migrating to gradle
    
    * fix: corrected main build.xml
    
    * fix: corrected main build.xml for romraider
    
    * fix: corrected main build.xml for romraider
    
    * feat: restructured enum_to_string for migrating to gradle
    
    * feat: restructured configuration_definition for migrating to gradle; fix: corrected build.xml
    
    * upd: get changes from master; fix: rebasing conflicts
    
    * feat: restructured logging for migrating to gradle; fix: corrected build.xml's
    
    * upd: up-to-master; fix: conflicts in build.xml's
    
    * feat: restructured logging for migrating to gradle; fix: corrected build.xml's
    
    * fix: conflicts from master
    
    * feat: restructured shared_ui module for gradle; fix: main build.xml
    
    * feat: restructured tools module; fix: main build.xml according to new structure
    
    * feat: restructured modules in java_tools with build.xml; fix: corrected paths in build.xml's and .iml's according to the new structure; corrected path in gen_ptrace_enums.bat

--- firmware/gen_ptrace_enums.bat
+++ firmware/gen_ptrace_enums.bat
@@ -1,5 +1,5 @@
 #
 # this tool might have enough flexibility to generate both C# and java class depending on specified "top line" and "stringClassName" parameters?
 #
-java -jar ../java_console_binary/rusefi_console.jar ptrace_enums development/perf_trace.h ../java_console/models/src/com/rusefi/tracing/EnumNames.java "package com.rusefi.tracing;" "public static final String"
+java -jar ../java_console_binary/rusefi_console.jar ptrace_enums development/perf_trace.h ../java_console/models/src/main/java/com/rusefi/tracing/EnumNames.java "package com.rusefi.tracing;" "public static final String"
 
