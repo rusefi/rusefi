4f9b3cc4d0a4a60227161841cd1b31bbe874a96c
Andrey <rusefillc@gmail.com>
2021-07-30 16:07:22 -0400

    RUSEFI MSD to contain bundle-specific URL #2848
    refactoring and progress

--- firmware/hw_layer/mass_storage/create_ini_image.sh
+++ firmware/hw_layer/mass_storage/create_ini_image.sh
@@ -3,17 +3,25 @@
 # fail on error
 set -e
 
+FULL_INI=$1
+H_OUTPUT=$2
+FS_SIZE=$3
+SHORT_BOARDNAME=$4
+
+echo "ini $FULL_INI to $H_OUTPUT size $FS_SIZE for $SHORT_BOARDNAME"
+
+
 rm -f rusefi.zip ramdisk_image.h
 
 # copy *count*KB of zeroes
-dd if=/dev/zero of=ramdisk.image bs=1024 count=112
+dd if=/dev/zero of=ramdisk.image bs=1024 count=$FS_SIZE
 
 # create a FAT filesystem inside, name it RUSEFI
 mkfs.fat ramdisk.image
-fatlabel ramdisk.image RUSEFI
+fatlabel ramdisk.image "RUSEFI $SHORT_BOARDNAME"
 
 # -j option dumps all files in the root of the zip (instead of inside directories)
-zip -j rusefi.ini.zip $1
+zip -j rusefi.ini.zip $FULL_INI
 
 # Put the zip inside the filesystem
 mcopy -i ramdisk.image rusefi.ini.zip ::
@@ -22,9 +30,12 @@ mcopy -i ramdisk.image hw_layer/mass_storage/filesystem_contents/README.txt ::
 mcopy -i ramdisk.image hw_layer/mass_storage/filesystem_contents/rusEFI\ Forum.url ::
 mcopy -i ramdisk.image hw_layer/mass_storage/filesystem_contents/rusEFI\ Wiki.url ::
 
+
+
+
 # write out as a C array, with "static const" tacked on the front
 xxd -i ramdisk.image \
     | cat <(echo -n "static const ") - \
-    > $2
+    > $H_OUTPUT
 
 rm ramdisk.image rusefi.ini.zip
