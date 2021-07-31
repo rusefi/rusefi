c601659dc704fd31026a6de74aa19c55b38a24d2
Andrey <rusefillc@gmail.com>
2021-07-30 21:45:55 -0400

    RUSEFI MSD to contain bundle-specific URL #2848

--- firmware/hw_layer/mass_storage/create_ini_image.sh
+++ firmware/hw_layer/mass_storage/create_ini_image.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# from firmware folder:
+# hw_layer/mass_storage/create_ini_image.sh tunerstudio/generated/rusefi.ini hw_layer/mass_storage/ramdisk_image.h  112 test https://rusefi.com/s/test
+
 # fail on error
 set -e
 
@@ -24,15 +27,15 @@ fatlabel ramdisk.image RUSEFI
 # -j option dumps all files in the root of the zip (instead of inside directories)
 zip -j rusefi.ini.zip $FULL_INI
 
-cp hw_layer/rusEFI_Wiki_template.url wiki.temp
-echo "URL=${BOARD_SPECIFIC_URL}" >>  wiki.temp
+cp hw_layer/mass_storage/filesystem_contents/rusEFI_Wiki_template.url hw_layer/mass_storage/wiki.temp
+echo "URL=${BOARD_SPECIFIC_URL}" >>  hw_layer/mass_storage/wiki.temp
 
 # Put the zip inside the filesystem
 mcopy -i ramdisk.image rusefi.ini.zip ::
 # Put a readme text file in there too
 mcopy -i ramdisk.image hw_layer/mass_storage/filesystem_contents/README.txt ::
 mcopy -i ramdisk.image hw_layer/mass_storage/filesystem_contents/rusEFI\ Forum.url ::
-mcopy -i ramdisk.image wiki.temp ::rusEFI\ ${SHORT_BOARDNAME}\ Wiki.url
+mcopy -i ramdisk.image hw_layer/mass_storage/wiki.temp ::rusEFI\ ${SHORT_BOARDNAME}\ Wiki.url
 
 
 
