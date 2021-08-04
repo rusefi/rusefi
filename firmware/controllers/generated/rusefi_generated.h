5866348d53c5dbf05aee7c2ee82560543fc0d086
GitHub build-firmware Action <action@github.com>
2021-08-04 02:55:38 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated.h
+++ firmware/controllers/generated/rusefi_generated.h
@@ -1324,7 +1324,7 @@
 #define showSdCardWarning_offset 76
 #define SIGNATURE_BOARD hellen154hyundai
 #define SIGNATURE_DATE 2021.08.04
-#define SIGNATURE_HASH 833144580
+#define SIGNATURE_HASH 2246561538
 #define silentTriggerError_offset 1464
 #define slowAdcAlpha_offset 2088
 #define sparkDwellRpmBins_offset 332
@@ -1680,7 +1680,7 @@
 #define ts_show_spi true
 #define ts_show_trigger_comparator false
 #define ts_show_tunerstudio_port false
-#define TS_SIGNATURE "rusEFI 2021.08.04.hellen154hyundai.833144580"
+#define TS_SIGNATURE "rusEFI 2021.08.04.hellen154hyundai.2246561538"
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't'
