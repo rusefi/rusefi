bb076e25786db19a8889d413338ebfe455299a11
Andrey <rusefillc@gmail.com>
2021-08-07 16:43:31 -0400

    NB1

--- firmware/config/boards/hellen/hellen-nb1/compile_hellen-nb1.sh
+++ firmware/config/boards/hellen/hellen-nb1/compile_hellen-nb1.sh
@@ -2,7 +2,7 @@
 
 export PROJECT_BOARD=hellen/hellen-nb1
 export PROJECT_CPU=ARCH_STM32F4
-export EXTRA_PARAMS="-DSHORT_BOARD_NAME=hellenNB1"
+export EXTRA_PARAMS="-DSHORT_BOARD_NAME=hellen-nb1"
 
 cd ..
 bash ../common_make.sh
