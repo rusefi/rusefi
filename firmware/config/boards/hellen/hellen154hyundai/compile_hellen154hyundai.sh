242156801f9dbd8bc0081476e37db99d529494f7
rusefillc <sdfsdfqsf2334234234>
2021-08-07 02:44:17 -0400

    154hyundai

--- firmware/config/boards/hellen/hellen154hyundai/compile_hellen154hyundai.sh
+++ firmware/config/boards/hellen/hellen154hyundai/compile_hellen154hyundai.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export PROJECT_BOARD=hellen/hellen121nissan
+export PROJECT_BOARD=hellen/hellen154hyundai
 export PROJECT_CPU=ARCH_STM32F4
-export EXTRA_PARAMS="-DSHORT_BOARD_NAME=hellen121nissan"
+export EXTRA_PARAMS="-DSHORT_BOARD_NAME=hellen154hyundai"
 
 cd ..
 bash ../common_make.sh
