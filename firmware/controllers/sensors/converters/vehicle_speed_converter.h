e4c1d0c0de0f91a566f43819b7179155855489d9
alxrMironov <330OMcorporative>
2021-08-13 00:49:47 +0300

    Add vehicle_speed_converter.h with realization.

---
+++ firmware/controllers/sensors/converters/vehicle_speed_converter.h
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "sensor_converter_func.h"
+
+class VehicleSpeedConverter : public SensorConverter {
+public:
+	DECLARE_ENGINE_PTR;
+
+	VehicleSpeedConverter() {
+
+	}
+
+	SensorResult 
+	convert(float frequency) const override {
+		auto speed = frequency * engineConfiguration->vehicleSpeedCoef;
+		return speed;
+	}
+};
