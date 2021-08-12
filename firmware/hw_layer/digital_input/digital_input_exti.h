5bf52023e77881dee67f27f36a84cd5728936d2f
rusefillc <sdfsdfqsf2334234234>
2021-08-12 13:43:38 -0400

    Hellen Nissan defaults are no longer valid #3143

--- firmware/hw_layer/digital_input/digital_input_exti.h
+++ firmware/hw_layer/digital_input/digital_input_exti.h
@@ -1,5 +1,5 @@
 /*
- * digital_input_exti.h
+ * @file digital_input_exti.h
  *
  *  Created on: Dec 18, 2018
  * @author Andrey Belomutskiy, (c) 2012-2020
@@ -10,6 +10,7 @@
 #include "digital_input.h"
 
 #if HAL_USE_PAL
+void efiExtiInit();
 void efiExtiEnablePin(const char *msg, brain_pin_e pin, uint32_t mode, palcallback_t cb, void *cb_data);
 void efiExtiDisablePin(brain_pin_e brainPin);
 #endif /* HAL_USE_PAL */
