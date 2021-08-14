9dca5e53136c6a4af94659c76f351ea2ffb5a283
Andrey Gusakov <dron0gus@gmail.com>
2021-08-14 15:21:06 +0300

    board: subaru: enable WSPI and NOR flash drivers

--- firmware/config/boards/subaru_eg33/halconf.h
+++ firmware/config/boards/subaru_eg33/halconf.h
@@ -13,6 +13,8 @@
 /* this file is exist just to include mcuconf.h from THIS directory */
 #include "mcuconf.h"
 
+#define HAL_USE_WSPI                    TRUE
+
 #include "../../../hw_layer/ports/stm32/stm32f7/cfg/halconf.h"
 
 #endif /* _HALCONF_SUBARUEG33_H_ */
