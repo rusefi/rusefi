839ae6152911b72b5a366d518b0046967b5ce8e7
alxrMironov <330OMcorporative>
2021-08-28 01:04:20 +0300

    Add turbocharger speed sensor initialization functions

---
+++ firmware/init/sensor/init_turbocharger_speed_sensor.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+
+#include "init.h"
+#include "frequency_sensor.h"
+#include "turbocharger_speed_converter.h"
+
+static FrequencySensor turbochargerSpeedSensor(SensorType::VehicleSpeed, MS2NT(500));
+static TurbochargerSpeedConverter turbochargerSpeedConverter;
+
+
+void initTurbochargerSpeedSensor(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
+	INJECT_ENGINE_REFERENCE(&turbochargerSpeedConverter);
+
+	auto pin = CONFIG(turboSpeedSensorInputPin);
+
+	// Nothing to do if no sensor configured
+	if (!isBrainPinValid(pin)) {
+		return;
+	}
+
+	turbochargerSpeedSensor.setFunction(turbochargerSpeedConverter);
+	turbochargerSpeedSensor.init(pin, "tcss");
+	turbochargerSpeedSensor.Register();
+}
+
+void deinitTurbochargerSpeedSensor() {
+	turbochargerSpeedSensor.deInit();
+}
