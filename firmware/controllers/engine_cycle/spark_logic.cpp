160d7810a46c984a9ceeafb62443a59806805b43
rusefillc <sdfsdfqsf2334234234>
2021-09-06 10:35:26 -0400

    Smart ECU: misfire detection #936

--- firmware/controllers/engine_cycle/spark_logic.cpp
+++ firmware/controllers/engine_cycle/spark_logic.cpp
@@ -533,6 +533,13 @@ void onTriggerEventSparkLogic(bool limitedSpark, uint32_t trgEventIndex, int rpm
 			IgnitionEvent *event = &ENGINE(ignitionEvents.elements[i]);
 			if (event->dwellPosition.triggerEventIndex != trgEventIndex)
 				continue;
+
+			if (i == 0 && CONFIG(artificialTestMisfire) && (engine->globalSparkIdCounter % ((int)engineConfiguration->fsio_setting[5]) == 0)) {
+				// artificial misfire on cylinder #1 for testing purposes
+				warning(CUSTOM_ERR_6729, "artificial misfire on cylinder #1 for testing purposes %d", engine->globalSparkIdCounter);
+				continue;
+			}
+
 			handleSparkEvent(limitedSpark, trgEventIndex, event, rpm, edgeTimestamp PASS_ENGINE_PARAMETER_SUFFIX);
 		}
 	}
