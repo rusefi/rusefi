8c9ff949fe75e66e911578692ae713a210bae6a0
rusefillc <sdfsdfqsf2334234234>
2021-09-06 08:09:14 -0400

    thank you Nik - dealing with false positive CUSTOM_OBD_LOW_FREQUENCY

--- firmware/controllers/actuators/gppwm/gppwm.cpp
+++ firmware/controllers/actuators/gppwm/gppwm.cpp
@@ -34,7 +34,9 @@ void initGpPwm(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 
 		// Setup pin & pwm
 		pins[i].initPin("gp pwm", cfg.pin);
-		startSimplePwm(&outputs[i], "gp pwm", &engine->executor, &pins[i], freq, 0);
+		if (usePwm) {
+			startSimplePwm(&outputs[i], "gp pwm", &engine->executor, &pins[i], freq, 0);
+		}
 
 		// Set up this channel's lookup table
 		tables[i]->init(cfg.table, cfg.loadBins, cfg.rpmBins);
