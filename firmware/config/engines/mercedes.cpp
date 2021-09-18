96dca359a91d81190558949ec0e93720d5813936
rusefillc <sdfsdfqsf2334234234>
2021-09-18 00:17:52 -0400

    merc128: engine config defaults #3236

--- firmware/config/engines/mercedes.cpp
+++ firmware/config/engines/mercedes.cpp
@@ -7,10 +7,15 @@
 
 #include "pch.h"
 
+// is this M104 or M112 or both?
 void setHellenMercedes128_6_cyl(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
-
+	engineConfiguration->specs.cylindersCount = 6;
+	// 1-4-2-5-3-6 M104
+	engineConfiguration->specs.firingOrder = FO_1_4_3_6_2_5; // M112
 }
 
+// M113
 void setHellenMercedes128_8_cyl(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
-
+	engineConfiguration->specs.cylindersCount = 8;
+	engineConfiguration->specs.firingOrder = FO_1_5_4_2_6_3_7_8;
 }
