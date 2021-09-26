b602f1b1580ef5a99d8052d3e506286efe8c96c1
rusefillc <sdfsdfqsf2334234234>
2021-09-25 21:49:23 -0400

    rusEFI console to compare current bundle against auto-DFU bundle #3266
    one step back

--- java_console/io/src/main/java/com/rusefi/io/DfuHelper.java
+++ java_console/io/src/main/java/com/rusefi/io/DfuHelper.java
@@ -7,13 +7,7 @@ import java.io.IOException;
 
 public class DfuHelper {
     public static void sendDfuRebootCommand(IoStream stream, StringBuilder messages) {
-
-
-
-        if (1==1)
-            return;
-
-        byte[] command = BinaryProtocol.getTextCommandBytes(Fields.CMD_REBOOT_DFU);
+       byte[] command = BinaryProtocol.getTextCommandBytes(Fields.CMD_REBOOT_DFU);
         try {
             stream.sendPacket(command);
             stream.close();
