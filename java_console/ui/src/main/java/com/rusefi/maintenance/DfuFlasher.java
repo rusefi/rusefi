8e60a0216b313b2bf31431e5a51eb2850daa332b
rusefillc <sdfsdfqsf2334234234>
2021-09-26 01:21:40 -0400

    rusEFI console to compare current bundle against auto-DFU bundle fix #3266

--- java_console/ui/src/main/java/com/rusefi/maintenance/DfuFlasher.java
+++ java_console/ui/src/main/java/com/rusefi/maintenance/DfuFlasher.java
@@ -5,6 +5,7 @@ import com.rusefi.ConsoleUI;
 import com.rusefi.Launcher;
 import com.rusefi.Timeouts;
 import com.rusefi.autodetect.PortDetector;
+import com.rusefi.autodetect.SerialAutoChecker;
 import com.rusefi.io.DfuHelper;
 import com.rusefi.io.IoStream;
 import com.rusefi.io.serial.SerialIoStreamJSerialComm;
@@ -16,7 +17,10 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
 
 import static com.rusefi.StartupFrame.appendBundleName;
 
@@ -51,16 +55,27 @@ public class DfuFlasher {
         String port = selectedItem.toString();
         StringBuilder messages = new StringBuilder();
 
+        AtomicBoolean isSignatureValidated = new AtomicBoolean(true);
         if (!PortDetector.isAutoPort(port)) {
             messages.append("Using selected " + port + "\n");
             IoStream stream = SerialIoStreamJSerialComm.openPort(port);
-            DfuHelper.sendDfuRebootCommand(stream, messages);
+            AtomicReference<String> result = new AtomicReference<>();
+            new SerialAutoChecker(port, new CountDownLatch(1)).checkResponse(stream, new Function<SerialAutoChecker.CallbackContext, Void>() {
+                @Override
+                public Void apply(SerialAutoChecker.CallbackContext callbackContext) {
+                    result.set(callbackContext.getSignature());
+                    return null;
+                }
+            });
+            boolean isSignatureValidatedLocal = DfuHelper.sendDfuRebootCommand(parent, result.get(), stream, messages);
+            isSignatureValidated.set(isSignatureValidatedLocal);
         } else {
             messages.append("Auto-detecting port...\n");
             // instead of opening the just-detected port we execute the command using the same stream we used to discover port
             // it's more reliable this way
             port = PortDetector.autoDetectSerial(callbackContext -> {
-                DfuHelper.sendDfuRebootCommand(callbackContext.getStream(), messages);
+                boolean isSignatureValidatedLocal = DfuHelper.sendDfuRebootCommand(parent, callbackContext.getSignature(), callbackContext.getStream(), messages);
+                isSignatureValidated.set(isSignatureValidatedLocal);
                 return null;
             }).getSerialPort();
             if (port == null) {
@@ -73,7 +88,11 @@ public class DfuFlasher {
         StatusWindow wnd = new StatusWindow();
         wnd.showFrame(appendBundleName("DFU status " + Launcher.CONSOLE_VERSION));
         wnd.appendMsg(messages.toString());
-        ExecHelper.submitAction(() -> executeDFU(wnd), DfuFlasher.class + " thread");
+        if (isSignatureValidated.get()) {
+            ExecHelper.submitAction(() -> executeDFU(wnd), DfuFlasher.class + " thread");
+        } else {
+            wnd.appendMsg("Please use manual DFU to change bundle type.");
+        }
     }
 
     public static void runDfuProgramming() {
