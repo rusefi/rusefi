27d612fc970f66666faec57407f982d2cfbc483a
rusefillc <sdfsdfqsf2334234234>
2021-09-26 01:21:40 -0400

    rusEFI console to compare current bundle against auto-DFU bundle fix #3266

--- java_console/io/src/main/java/com/rusefi/io/DfuHelper.java
+++ java_console/io/src/main/java/com/rusefi/io/DfuHelper.java
@@ -1,13 +1,17 @@
 package com.rusefi.io;
 
+import com.rusefi.RusEfiSignature;
+import com.rusefi.SignatureHelper;
+import com.rusefi.autoupdate.Autoupdate;
 import com.rusefi.binaryprotocol.BinaryProtocol;
 import com.rusefi.config.generated.Fields;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class DfuHelper {
     public static void sendDfuRebootCommand(IoStream stream, StringBuilder messages) {
-       byte[] command = BinaryProtocol.getTextCommandBytes(Fields.CMD_REBOOT_DFU);
+        byte[] command = BinaryProtocol.getTextCommandBytes(Fields.CMD_REBOOT_DFU);
         try {
             stream.sendPacket(command);
             stream.close();
@@ -15,5 +19,23 @@ public class DfuHelper {
         } catch (IOException e) {
             messages.append("Error " + e);
         }
+    }
+
+    public static boolean sendDfuRebootCommand(JComponent parent, String signature, IoStream stream, StringBuilder messages) {
+        RusEfiSignature s = SignatureHelper.parse(signature);
+        String bundleName = Autoupdate.readBundleFullName();
+        if (bundleName != null && s != null) {
+            if (!bundleName.equalsIgnoreCase(s.getBundle())) {
+                JOptionPane.showMessageDialog(parent, String.format("You have \"%s\" controller does not look right to program it with \"%s\"", s.getBundle(), bundleName));
+                // in case of mismatched bundle type we are supposed do close connection
+                // and properly handle the case of user hitting "Update Firmware" again
+                // closing connection is a mess on Windows so it's simpler to just exit
+                System.exit(-5);
+                return false;
+            }
+        }
+
+        sendDfuRebootCommand(stream, messages);
+        return true;
     }
 }
