c89b06fa07c6cac914a3579dafe6a252a8b10c0f
rusefillc <sdfsdfqsf2334234234>
2021-09-29 14:31:27 -0400

    miata NB1 is a weird half-VR/half-Hall sensor

--- firmware/controllers/trigger/decoders/trigger_mazda.cpp
+++ firmware/controllers/trigger/decoders/trigger_mazda.cpp
@@ -91,7 +91,6 @@ static void initializeMazdaMiataNb1ShapeWithOffset(TriggerWaveform *s, float off
 	s->initialize(FOUR_STROKE_CAM_SENSOR);
 	s->setTriggerSynchronizationGap3(0, 0.065, 0.17f);
 	s->useRiseEdge = false;
-	s->bothFrontsRequired = true;
 	s->useOnlyPrimaryForSync = true;
 	efiAssertVoid(OBD_PCM_Processor_Fault, s->gapBothDirections == false, "NB1 trigger measures on FALL events");
 
