106db49e291b5a531a94de6ac177c6584d5337f6
Matthew Kennedy <matthewkennedy@outlook.com>
2021-10-01 21:53:29 -0700

    undo test changes

--- unit_tests/tests/trigger/test_real_cranking_nissan_vq40.cpp
+++ unit_tests/tests/trigger/test_real_cranking_nissan_vq40.cpp
@@ -32,8 +32,7 @@ TEST(realCrankingVQ40, normalCranking) {
 	ASSERT_EQ(241, GET_RPM())<< reader.lineIndex();
 
 	// TODO: why warnings?
-	ASSERT_EQ(3, eth.recentWarnings()->getCount());
+	ASSERT_EQ(2, eth.recentWarnings()->getCount());
 	ASSERT_EQ(CUSTOM_SYNC_COUNT_MISMATCH, eth.recentWarnings()->get(0));
-	ASSERT_EQ(CUSTOM_OUT_OF_ORDER_COIL, eth.recentWarnings()->get(1));	// this is from a coil being protected by overdwell protection
-	ASSERT_EQ(CUSTOM_SYNC_ERROR, eth.recentWarnings()->get(2));
+	ASSERT_EQ(CUSTOM_SYNC_ERROR, eth.recentWarnings()->get(1));
 }
