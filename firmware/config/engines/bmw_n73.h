8778c1467b33dd4a7e64212c6b3bfb8d50929feb
rusefillc <sdfsdfqsf2334234234>
2021-10-02 13:25:40 -0400

    proteus adapters

---
+++ firmware/config/engines/bmw_n73.h
@@ -0,0 +1,12 @@
+/*
+ * bmw_n73.h
+ *
+ * @date Oct 2, 2021
+ * @author Andrey Belomutskiy, (c) 2012-2021
+ */
+
+#pragma once
+
+#include "engine_configuration.h"
+
+void setEngineProteusBMW_N73_GDI(DECLARE_CONFIG_PARAMETER_SIGNATURE);
