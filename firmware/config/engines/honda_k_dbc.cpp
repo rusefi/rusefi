a8058a6ea4c4991af096c8527a858abc8ec9c790
rusefillc <sdfsdfqsf2334234234>
2021-10-02 13:25:40 -0400

    proteus adapters

---
+++ firmware/config/engines/honda_k_dbc.cpp
@@ -0,0 +1,12 @@
+/*
+ * honda_k_dbc.cpp
+ *
+ * @date Oct 2, 2021
+ * @author Andrey Belomutskiy, (c) 2012-2021
+ */
+
+#include "honda_k_dbc.h"
+
+void setProteusHondaElement2003(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
+
+}
