1cae5384fa4c6a1fc6949dd9115edc79779402b4
Matthew Kennedy <matthewkennedy@outlook.com>
2021-10-04 13:42:19 -0700

    comments

--- firmware/controllers/sensors/sensor_type.h
+++ firmware/controllers/sensors/sensor_type.h
@@ -77,7 +77,9 @@ enum class SensorType : unsigned char {
 
 	TurbochargerSpeed = 35,
 
+	// Fast MAP is synchronous to crank angle - user selectable phase/window
 	MapFast = 36,
+	// Slow MAP is asynchronous - not synced to anything, normal analog sampling
 	MapSlow = 37,
 
 	// Leave me at the end!
