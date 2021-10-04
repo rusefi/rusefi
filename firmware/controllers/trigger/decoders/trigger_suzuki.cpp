b7f6c408473fd292c0213454f70e95d22b8c0983
rusefillc <sdfsdfqsf2334234234>
2021-10-04 19:59:51 -0400

    https://rusefi.com/forum/viewtopic.php?f=5&t=2143 Suzuki G13B tooth pattern

--- firmware/controllers/trigger/decoders/trigger_suzuki.cpp
+++ firmware/controllers/trigger/decoders/trigger_suzuki.cpp
@@ -11,12 +11,13 @@ void initializeSuzukiG13B(TriggerWaveform *s) {
 	s->initialize(FOUR_STROKE_CAM_SENSOR);
 
 	float w = 5;
+	float specialTooth = 20;
 
 	s->addEvent720(180 - w, T_PRIMARY, TV_RISE);
 	s->addEvent720(180, T_PRIMARY, TV_FALL);
 
-	s->addEvent720(30 + 180 - w, T_PRIMARY, TV_RISE);
-	s->addEvent720(30 + 180, T_PRIMARY, TV_FALL);
+	s->addEvent720(2 * specialTooth + 180 - w, T_PRIMARY, TV_RISE);
+	s->addEvent720(2 * specialTooth + 180, T_PRIMARY, TV_FALL);
 
 	s->addEvent720(360 - w, T_PRIMARY, TV_RISE);
 	s->addEvent720(360, T_PRIMARY, TV_FALL);
@@ -27,7 +28,7 @@ void initializeSuzukiG13B(TriggerWaveform *s) {
 	s->addEvent720(720 - w, T_PRIMARY, TV_RISE);
 	s->addEvent720(720, T_PRIMARY, TV_FALL);
 
-	s->setTriggerSynchronizationGap(0.14);
+	s->setTriggerSynchronizationGap(0.22);
 	s->setSecondTriggerSynchronizationGap(1);
 }
 
