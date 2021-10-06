f2d01ab96963d2ba84bbe8edbb593e22a143aaae
Matthew Kennedy <matthewkennedy@outlook.com>
2021-10-06 12:16:14 -0700

    oooooh map avg on hh7

--- firmware/config/stm32h7ems/efifeatures.h
+++ firmware/config/stm32h7ems/efifeatures.h
@@ -2,9 +2,6 @@
 
 #pragma once
 
-#undef EFI_MAP_AVERAGING
-#define EFI_MAP_AVERAGING FALSE
-
 #undef EFI_USE_FAST_ADC
 // https://github.com/rusefi/rusefi/issues/3301 "H7 is currently actually using fast ADC exclusively - it just needs a bit of plumbing to make it work."
 #define EFI_USE_FAST_ADC FALSE
