a8f0187b5a93d2e887c7238daf14c77e1eadffaa
rusefillc <sdfsdfqsf2334234234>
2021-10-06 11:36:02 -0400

    flash_reboot_dfu.bat feedback (#3300)

--- firmware/flash_reboot_dfu.bat
+++ firmware/flash_reboot_dfu.bat
@@ -6,6 +6,6 @@ rem
 echo Sending rusEFI DFU request
 java -jar ../java_console_binary/rusefi_console.jar reboot_dfu
 echo Now sleeping before DFU
-sleep 10
+sleep 5
 echo Invoking DFU process
 ..\misc\install\DfuSe\DfuSeCommand.exe -c -d --fn deliver/rusefi.dfu
