6806410f9754e5c29224b35b0547abf6399ae850
rusefi <rusefillc@gmail.com>
2021-10-07 17:35:27 -0400

    export board-specific macro for stm32 pins based on board yaml #3298

--- firmware/config/boards/hellen/hellen_meta.h
+++ firmware/config/boards/hellen/hellen_meta.h
@@ -15,7 +15,17 @@
 #define H144_LS_6 GPIOF_12
 
 #define H176_LS_1 GPIOG_7
+#define H176_LS_2 GPIOG_8
+#define H176_LS_3 GPIOD_11
+#define H176_LS_4 GPIOD_10
+#define H176_LS_5 GPIOD_9
+#define H176_LS_6 GPIOF_12
+#define H176_LS_7 GPIOF_13
+#define H176_LS_8 GPIOF_14
 
 #define H176_OUT_PWM1 GPIOD_13
 #define H176_OUT_PWM2 GPIOC_6
 #define H176_OUT_PWM3 GPIOC_7
+
+#define H176_CAN_RX GPIOD_0
+#define H176_CAN_TX GPIOD_1
