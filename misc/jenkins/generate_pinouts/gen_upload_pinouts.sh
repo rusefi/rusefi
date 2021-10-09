f064f2c83b30d958cbf128f72f2736dd6b83e95a
rusefillc <sdfsdfqsf2334234234>
2021-10-09 09:44:11 -0400

    https://github.com/rusefi/rusefi_documentation/issues/164

--- misc/jenkins/generate_pinouts/gen_upload_pinouts.sh
+++ misc/jenkins/generate_pinouts/gen_upload_pinouts.sh
@@ -42,7 +42,7 @@ done
 
 if [ -n "$RUSEFI_FTP_SERVER" ]; then
   echo "Uploading Pinouts..."
-  ncftpput -R -z -m -v -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" / pinouts
+  ncftpput -r 999 -m -R -z -v -u "$RUSEFI_DOXYGEN_FTP_USER" -p "$RUSEFI_DOXYGEN_FTP_PASS" "$RUSEFI_FTP_SERVER" / pinouts
   echo "Uploaded!"
 fi
 [ $? -eq 0 ] || { echo "upload FAILED"; exit 1; }
