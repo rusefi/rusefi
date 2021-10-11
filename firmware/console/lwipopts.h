8ca619144d364cb7e9442bce0f244f15897ff477
Matthew Kennedy <matthewkennedy@outlook.com>
2021-10-11 12:35:09 -0700

    misc ethernet config parameters (#3343)
    * various cfg
    
    * this has to be 1522
    
    Co-authored-by: Matthew Kennedy <makenne@microsoft.com>

--- firmware/console/lwipopts.h
+++ firmware/console/lwipopts.h
@@ -55,7 +55,7 @@
 #endif
 
 #define LWIP_LINK_POLL_INTERVAL   TIME_S2I(1)
-#define LWIP_IPADDR(p)            IP4_ADDR(p, 192, 168, 10, 14)
+#define LWIP_IPADDR(p)            IP4_ADDR(p, 192, 168, 10, 1)
 #define LWIP_GATEWAY(p)           IP4_ADDR(p, 192, 168, 10, 1)
 
 #define LWIP_COMPAT_SOCKETS 0
