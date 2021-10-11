58f9f6380e759859ab978c9dda66c4af04d57aa6
rusefillc <sdfsdfqsf2334234234>
2021-10-11 14:22:35 -0400

    hyundai

--- firmware/config/boards/hellen/hellen154hyundai/board_configuration.cpp
+++ firmware/config/boards/hellen/hellen154hyundai/board_configuration.cpp
@@ -159,17 +159,6 @@ void setBoardDefaultConfiguration(void) {
 	setAlgorithm(LM_SPEED_DENSITY PASS_CONFIG_PARAMETER_SUFFIX);
 
 
-	// 2010-2012 315cc at 43.5psi
-	// fuel system is fixed pressure 55psi
-	// 2013+ 450cc at 43.5
-	// fuel system is fixed pressure 85psi
-	// flow rate P2 = flow rate P1 * sqrt(P2/P1)
-	engineConfiguration->injector.flow = 354.19; // https://www.google.com/search?q=315*sqrt%2855%2F43.5%29
-	engineConfiguration->fuelReferencePressure = PSI2KPA(55);
-	// todo: split engine generations
-	engineConfiguration->injector.flow = 629.03; // https://www.google.com/search?q=450*sqrt%2885%2F43.5%29
-	engineConfiguration->fuelReferencePressure = PSI2KPA(85);
-
 	engineConfiguration->injectorCompensationMode = ICM_FixedRailPressure;
 
 	engineConfiguration->specs.cylindersCount = 4;
