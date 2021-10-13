6121124f6dd36c4b62d48b5dd6a21761c7d51602
rusefillc <sdfsdfqsf2334234234>
2021-10-12 20:53:00 -0400

    progress

--- firmware/config/boards/hellen/hellen88bmw/board_configuration.cpp
+++ firmware/config/boards/hellen/hellen88bmw/board_configuration.cpp
@@ -72,9 +72,6 @@ static void setupDefaultSensorInputs() {
 	engineConfiguration->triggerInputPins[2] = GPIO_UNASSIGNED;
 	// Direct hall-only cam input
 	engineConfiguration->camInputs[0] = H144_IN_CAM;
-	// todo: remove from default since 4 cylinder does not use it
-	// todo: this requires unit test change
-	engineConfiguration->camInputs[1 * CAMS_PER_BANK] = H144_IN_D_AUX4;
 
 	engineConfiguration->tps1_1AdcChannel = H144_IN_TPS;
 	engineConfiguration->tps1_2AdcChannel = H144_IN_AUX1;
