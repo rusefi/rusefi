a5f5ec40293c86ab64ed08aef7157127ac89f6ff
rusefillc <sdfsdfqsf2334234234>
2021-10-14 18:43:26 -0400

    Whatever we call it, no matter how we do it - we need live data / remote view into rusEFI actual state #3353
    progress

--- firmware/gen_enum_to_string.sh
+++ firmware/gen_enum_to_string.sh
@@ -4,7 +4,7 @@ echo "This batch files reads rusefi_enums.h and produces auto_generated_enums.* 
 
 rm gen_enum_to_string.log
 
-java -DSystemOut.name=gen_java_enum -cp ../java_tools/enum2string.jar com.rusefi.ToJavaEnum -enumInputFile controllers/algo/live_data_ids.h -outputPath ../java_console/ui/src/main/java/com/rusefi/enums
+java -DSystemOut.name=gen_java_enum -cp ../java_tools/enum2string.jar com.rusefi.ToJavaEnum -enumInputFile controllers/algo/live_data_ids.h -outputPath ../java_console/io/src/main/java/com/rusefi/enums
 
 java -DSystemOut.name=gen_enum_to_string \
 	-jar ../java_tools/enum2string.jar \
