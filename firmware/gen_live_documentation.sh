04a4189ed6970ee5573b786789e9d61876d1291b
rusefillc <sdfsdfqsf2334234234>
2021-10-14 00:30:54 -0400

    Whatever we call it, whatever we implement it - we need live data / remote view into rusEFI actual state #3353

--- firmware/gen_live_documentation.sh
+++ firmware/gen_live_documentation.sh
@@ -2,15 +2,17 @@
 
 rm gen_live_documentation.log
 
-bash gen_line_documentation_one_file.sh pid_state.txt PidState.java
+bash gen_live_documentation_one_file.sh ac_control AcControl.java
 
-bash gen_line_documentation_one_file.sh engine_state EngineState.java
+bash gen_live_documentation_one_file.sh pid_state PidState.java
 
-bash gen_line_documentation_one_file.sh trigger_centralTriggerCentral.java
+bash gen_live_documentation_one_file.sh engine_state EngineState.java
 
-bash gen_line_documentation_one_file.sh trigger_state.txt TriggerState.java
+bash gen_live_documentation_one_file.sh trigger_central TriggerCentral.java
 
-bash gen_line_documentation_one_file.sh wall_fuel_state.txtWallFuelState.java
+bash gen_live_documentation_one_file.sh trigger_state TriggerState.java
+
+bash gen_live_documentation_one_file.sh wall_fuel_state WallFuelState.java
 
 java -DSystemOut.name=gen_live_documentation \
  -cp ../java_tools/ConfigDefinition.jar \
