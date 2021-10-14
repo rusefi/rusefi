75900071e46f30ca51e0131045d09f2990cada0a
rusefillc <sdfsdfqsf2334234234>
2021-10-13 21:39:01 -0400

    https://github.com/rusefi/rusefi_documentation/issues/170

--- misc/actions/pinouts-push-to-pages.sh
+++ misc/actions/pinouts-push-to-pages.sh
@@ -4,7 +4,18 @@ rm -rf workarea
 mkdir workarea
 git clone https://github.com/rusefi/rusefi.github.io.git workarea/pages
 
-mv -u pinouts/ workarea/pages/
+
+#
+# drama: we have two kinds of index files:
+#  1) we have proper pinouts
+#  2) we need to explicitly generate directory index files since github actions does not provide directory listing
+# at the moment 'gen_index.py' overrides pinouts files
+# TODO: make gen_index.py skip existing file? or create pinouts, then gen_index.py, and then create pinouts AGAIN to save those while enjoying additions to index files?!
+#
+
+rsync -a -v pinouts/ workarea/pages/pinouts/
+
+
 cd workarea/pages/
 git add *
 git status
