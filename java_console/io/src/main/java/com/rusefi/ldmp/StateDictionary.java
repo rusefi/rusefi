01547e58497125039bce3642c505ee66477669d0
rusefillc <sdfsdfqsf2334234234>
2021-10-14 18:43:26 -0400

    Whatever we call it, no matter how we do it - we need live data / remote view into rusEFI actual state #3353
    progress

--- java_console/io/src/main/java/com/rusefi/ldmp/StateDictionary.java
+++ java_console/io/src/main/java/com/rusefi/ldmp/StateDictionary.java
@@ -1,72 +1,45 @@
 package com.rusefi.ldmp;
 
 import com.rusefi.config.Field;
-import com.rusefi.config.generated.*;
-import com.rusefi.ui.livedocs.LiveDataContext;
+import com.rusefi.config.generated.EngineState;
+import com.rusefi.config.generated.PidState;
+import com.rusefi.config.generated.TriggerCentral;
+import com.rusefi.config.generated.TriggerState;
+import com.rusefi.enums.live_data_e;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 public enum StateDictionary {
     INSTANCE;
 
     public static final int NONE = -1;
-    private Map<Integer, Field[]> map = new HashMap<>();
+    private Map<live_data_e, Field[]> map = new HashMap<>();
 
     StateDictionary() {
-        register(Fields.LDS_SPEED_DENSITY_STATE_INDEX, EngineState.VALUES); // 2
-        register(Fields.LDS_ENGINE_STATE_INDEX, EngineState.VALUES); // 3
-        register(Fields.LDS_FUEL_TRIM_STATE_INDEX, EngineState.VALUES);
-        register(Fields.LDS_TPS_TPS_ENRICHMENT_STATE_INDEX, EngineState.VALUES); // 5
-        register(Fields.LDS_TRIGGER_CENTRAL_STATE_INDEX, TriggerCentral.VALUES);
-        register(Fields.LDS_ETB_PID_STATE_INDEX, PidState.VALUES); // 7
-        register(Fields.LDS_IDLE_PID_STATE_INDEX, PidState.VALUES);
-        // LDS_ALTERNATOR_PID_STATE_INDEX
-        // LDS_CJ125_PID_STATE_INDEX
-        register(Fields.LDS_TRIGGER_STATE_STATE_INDEX, TriggerState.VALUES); // 11
+        register(live_data_e.LDS_SPEED_DENSITY, EngineState.VALUES); // 2
+        register(live_data_e.LDS_ENGINE, EngineState.VALUES); // 3
+        register(live_data_e.LDS_FUEL_TRIM, EngineState.VALUES);
+        register(live_data_e.LDS_TPS_TPS_ENRICHMENT, EngineState.VALUES); // 5
+        register(live_data_e.LDS_TRIGGER_CENTRAL, TriggerCentral.VALUES);
+        register(live_data_e.LDS_ETB_PID, PidState.VALUES); // 7
+        register(live_data_e.LDS_IDLE_PID, PidState.VALUES);
+        // LDS_ALTERNATOR_PID
+        // LDS_CJ125_PID
+        register(live_data_e.LDS_TRIGGER_STATE, TriggerState.VALUES); // 11
     }
 
-    private void register(int ldsIndex, Field[] values) {
+    private void register(live_data_e ldsIndex, Field[] values) {
         map.put(ldsIndex, values);
     }
 
-    public Field[] getValue(String state) {
-        String indexFieldName = getContextIndexFieldName(state);
-        LiveDataContext indexValue = getStateContext(indexFieldName);
-        return getFields(indexFieldName, indexValue);
-    }
-
     @NotNull
-    public Field[] getFields(String indexFieldName, LiveDataContext indexValue) {
-        Field[] result = map.get(indexValue.getId());
+    public Field[] getFields(live_data_e indexFieldName) {
+        Field[] result = map.get(indexFieldName);
         if (result == null) {
-            throw new IllegalStateException("Nothing for " + indexFieldName + "/" + indexValue);
+            throw new IllegalStateException("Nothing for " + indexFieldName);
         }
         return result;
     }
-
-    @NotNull
-    public static String getContextIndexFieldName(String state) {
-        return "LDS_" + state.toUpperCase() + "_STATE_INDEX";
-    }
-
-    public static LiveDataContext getStateContext(String contextIndexFieldName) {
-        java.lang.reflect.Field field;
-        try {
-            field = Fields.class.getField(contextIndexFieldName);
-        } catch (NoSuchFieldException e) {
-            throw new IllegalStateException(e);
-        }
-        Objects.requireNonNull(field, "Field " + contextIndexFieldName);
-        int indexValue;
-        try {
-            indexValue = (int) field.get(null);
-        } catch (IllegalAccessException e) {
-            throw new IllegalStateException(e);
-        }
-        return new LiveDataContext(indexValue);
-    }
-
 }
