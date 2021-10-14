b24ea754f22c8d0e9a15414d408b2dc37b7c3643
rusefillc <sdfsdfqsf2334234234>
2021-10-13 21:04:47 -0400

    Move pinouts to pages.rusefi.com #170

--- misc/actions/pinouts-create.sh
+++ misc/actions/pinouts-create.sh
@@ -13,7 +13,7 @@ else
 fi
 # yq 3 and 4 have incompatible syntax. We use yq 4.
 echo ${I_AM} invoking yq version
-yq -V
+yqdir/yq -V
 for c in $CONNECTORS; do
   echo "${I_AM} processing "$c
   DIR="pinouts/"$(echo $c | tr '/' '\n' | tail -n +5 | head -n -2 | tr '\n' '/')
@@ -22,16 +22,16 @@ for c in $CONNECTORS; do
   echo "${I_AM} NAME "$NAME
   mkdir -p $DIR
   if [ -f $DIR/index.html ]; then
-    bash misc/pinout-gen/append.sh "$(yq -j e $c)" $DIR/index.html
+    bash misc/pinout-gen/append.sh "$(yqdir/yq -j e $c)" $DIR/index.html
   else
-    bash misc/pinout-gen/gen.sh "$(yq -j e $c)" $DIR/index.html
+    bash misc/pinout-gen/gen.sh "$(yqdir/yq -j e $c)" $DIR/index.html
   fi
   if [ $? -ne 0 ]; then
     echo "${I_AM} something is broken"
     exit 1;
   fi
   file $DIR/index.html
-  IMG=$(yq e '.info.image.file' $c)
+  IMG=$(yqdir/yq e '.info.image.file' $c)
   if [ $? -ne 0 ]; then
     exit 1;
   fi
