875602d1fc7692a178b5ac357409516763b5ab61
rusefillc <sdfsdfqsf2334234234>
2021-10-13 21:04:02 -0400

    Move pinouts to pages.rusefi.com #170

--- misc/actions/pinouts-install.sh
+++ misc/actions/pinouts-install.sh
@@ -4,5 +4,6 @@
         sudo apt-get install ncftp
         wget https://github.com/mikefarah/yq/releases/download/v4.9.6/yq_linux_amd64
         chmod a+x yq_linux_amd64
+        rm -rf yqdir
         mkdir yqdir
         mv yq_linux_amd64 yqdir/yq
