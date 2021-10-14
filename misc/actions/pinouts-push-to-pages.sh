6cf5cf2e2447d79aae37f6e8a8d408f4810f3ca0
rusefillc <sdfsdfqsf2334234234>
2021-10-13 21:25:29 -0400

    Move pinouts to pages.rusefi.com #170

---
+++ misc/actions/pinouts-push-to-pages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf workarea
+mkdir workarea
+git clone https://github.com/rusefi/rusefi.github.io.git workarea/pages
+
+mv pinouts/ workarea/pages/
+cd workarea/pages/
+git add *
+git status
+
+        git config --local user.email "action@github.com"
+        git config --local user.name "GitHub build-firmware Action"
+
+git commit -m 'pinouts'
+git push
