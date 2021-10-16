0a850402e0db08b3162d90fe389330c35499070f
rusefillc <sdfsdfqsf2334234234>
2021-10-15 20:57:45 -0400

    Better Windows build-in DFU #3338

--- misc/install_st/make_package2.bat
+++ misc/install_st/make_package2.bat
@@ -7,5 +7,7 @@ cp -r "C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK_U
 rem https://github.com/rusefi/rusefi_external_utils/blob/master/stsw-stm32102_1_4_0.zip
 cp -r "C:\Program Files (x86)\STMicroelectronics\Software\Virtual comport driver" silent_st_drivers
 
+cp -r "C:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\Drivers\DFU_Driver" silent_st_drivers
+
 rem compress 'silent_st_drivers' folder
-"C:\Program Files\7-Zip\7z.exe" a silent_st_drivers.exe -mmt -mx5 -sfx silent_st_drivers
+"C:\Program Files\7-Zip\7z.exe" a silent_st_drivers2.exe -mmt -mx5 -sfx silent_st_drivers
