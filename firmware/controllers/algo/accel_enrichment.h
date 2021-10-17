d84cf5eea81e524ff4e6afdf48ac9238bbafc3f1
rusefillc <sdfsdfqsf2334234234>
2021-10-16 20:16:40 -0400

    splitting accelerations

--- firmware/controllers/algo/accel_enrichment.h
+++ firmware/controllers/algo/accel_enrichment.h
@@ -60,22 +60,6 @@ private:
 	int cycleCnt = 0;
 };
 
-/**
- * Wall wetting, also known as fuel film
- * See https://github.com/rusefi/rusefi/issues/151 for the theory
- */
-class WallFuel : public wall_fuel_state {
-public:
-	/**
-	 * @param target desired squirt duration
-	 * @return total adjusted fuel squirt duration once wall wetting is taken into effect
-	 */
-	floatms_t adjust(floatms_t target DECLARE_ENGINE_PARAMETER_SUFFIX);
-	floatms_t getWallFuel() const;
-	void resetWF();
-	int invocationCounter = 0;
-};
-
 void initAccelEnrichment(DECLARE_ENGINE_PARAMETER_SIGNATURE);
 
 void setEngineLoadAccelLen(int len);
