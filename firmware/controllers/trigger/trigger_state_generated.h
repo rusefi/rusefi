981fc5829791d55ea3b8f1d0a3e3f37d0a39adc6
GitHub build-firmware Action <action@github.com>
2021-10-17 01:13:47 +0000

    Auto-generated configs and docs

--- firmware/controllers/trigger/trigger_state_generated.h
+++ firmware/controllers/trigger/trigger_state_generated.h
@@ -1,4 +1,4 @@
-// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/trigger/trigger_state.txt Sat Oct 16 20:21:25 EDT 2021
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/trigger/trigger_state.txt Sun Oct 17 01:13:41 UTC 2021
 // by class com.rusefi.output.CHeaderConsumer
 // begin
 #pragma once
@@ -13,4 +13,4 @@ struct trigger_state_s {
 };
 
 // end
-// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/trigger/trigger_state.txt Sat Oct 16 20:21:25 EDT 2021
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/trigger/trigger_state.txt Sun Oct 17 01:13:41 UTC 2021
