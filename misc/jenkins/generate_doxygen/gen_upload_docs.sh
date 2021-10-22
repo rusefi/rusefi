478a25d0b4e22b95caf8401829c6d9e2cf56f3b9
David Holdeman <david@chuckwagoncomputing.com>
2021-10-22 08:07:35 -0500

    Change vars and use docs dir (#3390)

--- misc/jenkins/generate_doxygen/gen_upload_docs.sh
+++ misc/jenkins/generate_doxygen/gen_upload_docs.sh
@@ -9,6 +9,6 @@ doxygen || { echo "doxygen run FAILED"; exit 1; }
 cd ../doxygen
 if [ -n "$RUSEFI_SSH_SERVER" ]; then
   echo "Uploading Doxygen"
-  tar -czf - html | sshpass -p "$RUSEFI_DOXYGEN_SSH_PASS" ssh -o StrictHostKeyChecking=no "$RUSEFI_DOXYGEN_SSH_USER"@"$RUSEFI_SSH_SERVER" "tar -xzf -"
+  tar -czf - html | sshpass -p "$RUSEFI_SSH_PASS" ssh -o StrictHostKeyChecking=no "$RUSEFI_SSH_USER"@"$RUSEFI_SSH_SERVER" "tar -xzf - -C docs"
 fi
 [ $? -eq 0 ] || { echo "upload FAILED"; exit 1; }
--- misc/jenkins/generate_pinouts/gen_upload_pinouts.sh
+++ misc/jenkins/generate_pinouts/gen_upload_pinouts.sh
@@ -4,7 +4,7 @@ misc/actions/pinouts-create.sh
 
 if [ -n "$RUSEFI_SSH_SERVER" ]; then
   echo "Uploading Pinouts..."
-  tar -czf - pinouts | sshpass -p "$RUSEFI_DOXYGEN_SSH_PASS" ssh -o StrictHostKeyChecking=no "$RUSEFI_DOXYGEN_SSH_USER"@"$RUSEFI_SSH_SERVER" "tar -xzf -"
+  tar -czf - pinouts | sshpass -p "$RUSEFI_SSH_PASS" ssh -o StrictHostKeyChecking=no "$RUSEFI_SSH_USER"@"$RUSEFI_SSH_SERVER" "tar -xzf - -C docs"
 fi
 [ $? -eq 0 ] || { echo "upload FAILED"; exit 1; }
 
