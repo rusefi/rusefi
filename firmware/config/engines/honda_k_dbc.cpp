7b68d0d226e5c15e0382240b4b8f0a7be12600bf
rusefi <rusefillc@gmail.com>
2021-10-23 15:49:02 -0400

    Honda K

--- firmware/config/engines/honda_k_dbc.cpp
+++ firmware/config/engines/honda_k_dbc.cpp
@@ -11,10 +11,23 @@
 #include "proteus_meta.h"
 #endif
 
+/**
+ * PROTEUS_HONDA_ELEMENT_2003
+ */
 void setProteusHondaElement2003(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
 	engineConfiguration->specs.cylindersCount = 4;
 	engineConfiguration->specs.displacement = 2.4;
 	engineConfiguration->trigger.type = TT_HONDA_K_12_1;
+
+	strcpy(CONFIG(engineMake), ENGINE_MAKE_HONDA);
+	strcpy(CONFIG(engineCode), "K24");
+	strcpy(CONFIG(vehicleName), "test");
+
+#if HW_PROTEUS & EFI_PROD_CODE
+	engineConfiguration->triggerInputPins[0] = PROTEUS_DIGITAL_2;
+	engineConfiguration->camInputs[0] = PROTEUS_DIGITAL_4;
+	engineConfiguration->camInputs[1 * CAMS_PER_BANK] = PROTEUS_DIGITAL_1;
+#endif
 }
 
 void setProteusHondaOBD2A(DECLARE_CONFIG_PARAMETER_SIGNATURE) {
