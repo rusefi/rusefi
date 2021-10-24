3a30c50b789a18c27a2c5f88c0ac28bea61f1387
rusefillc <sdfsdfqsf2334234234>
2021-10-23 21:56:00 -0400

    generate java enum from C enum? generate both C and java from yaml? #2102

--- firmware/gen_config_common.sh
+++ firmware/gen_config_common.sh
@@ -1,4 +1,5 @@
 COMMON_GEN_CONFIG="-jar ../java_tools/ConfigDefinition.jar \
+ -enumInputFile controllers/algo/engine_types.h \
  -enumInputFile controllers/algo/rusefi_enums.h \
  -cache_zip_file tunerstudio/generated/cache.zip \
  -ts_destination tunerstudio \
