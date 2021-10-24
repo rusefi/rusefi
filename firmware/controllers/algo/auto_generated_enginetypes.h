27e6c5d682e964dfb38246dff2eec58baa44a55c
GitHub build-firmware Action <action@github.com>
2021-10-24 19:28:08 +0000

    Auto-generated configs and docs

--- firmware/controllers/algo/auto_generated_enginetypes.h
+++ firmware/controllers/algo/auto_generated_enginetypes.h
@@ -1,4 +1,4 @@
-// was generated automatically by rusEFI tool  from engine_types.h // by enum2string.jar tool on Sat Oct 23 23:39:23 EDT 2021
+// was generated automatically by rusEFI tool  from engine_types.h // by enum2string.jar tool on Sun Oct 24 19:26:39 UTC 2021
 // see also gen_config_and_enums.bat
 
 
@@ -7,3 +7,4 @@
 #pragma once
 const char *getDebug_mode_e(debug_mode_e value);
 const char *getEngine_type_e(engine_type_e value);
+const char *getTrigger_type_e(trigger_type_e value);
