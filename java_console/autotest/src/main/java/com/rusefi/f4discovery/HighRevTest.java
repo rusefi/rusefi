1b54962f63f22cd97af68d1307414fd6060f9e62
rusefillc <sdfsdfqsf2334234234>
2021-10-24 08:54:37 -0400

    generate java enum from C enum? generate both C and java from yaml? #2102

--- java_console/autotest/src/main/java/com/rusefi/f4discovery/HighRevTest.java
+++ java_console/autotest/src/main/java/com/rusefi/f4discovery/HighRevTest.java
@@ -1,17 +1,17 @@
 package com.rusefi.f4discovery;
 
 import com.rusefi.RusefiTestBase;
+import com.rusefi.enums.engine_type_e;
 import com.rusefi.functional_tests.EcuTestHelper;
-import org.junit.Ignore;
 import org.junit.Test;
 
+import static com.rusefi.config.generated.Fields.CMD_ENGINESNIFFERRPMTHRESHOLD;
 import static com.rusefi.functional_tests.EcuTestHelper.FAIL;
-import static com.rusefi.config.generated.Fields.*;
 
 public class HighRevTest extends RusefiTestBase {
     @Test
     public void testVW() {
-        ecu.setEngineType(ET_VW_ABA);
+        ecu.setEngineType(engine_type_e.VW_ABA);
         // trying to disable engine sniffer to help https://github.com/rusefi/rusefi/issues/1849
         ecu.sendCommand("set " + CMD_ENGINESNIFFERRPMTHRESHOLD + " 100");
         ecu.changeRpm(900);
@@ -21,7 +21,7 @@ public class HighRevTest extends RusefiTestBase {
 
     @Test
     public void testV12() {
-        ecu.setEngineType(ET_BMW_M73_F);
+        ecu.setEngineType(engine_type_e.BMW_M73_F);
         ecu.changeRpm(700);
         // first let's get to expected RPM
         EcuTestHelper.assertRpmDoesNotJump(6000, 5, 40, FAIL, ecu.commandQueue);
