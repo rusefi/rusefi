8ac4341cfa8d5ef71cf39c055cff3393aec5c10b
GitHub build-firmware Action <action@github.com>
2021-10-31 01:38:27 +0000

    Auto-generated configs and docs

--- java_console/models/src/main/java/com/rusefi/config/generated/IdleState.java
+++ java_console/models/src/main/java/com/rusefi/config/generated/IdleState.java
@@ -1,6 +1,6 @@
 package com.rusefi.config.generated;
 
-// this file was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/actuators/idle_state.txt Sun Oct 17 04:35:23 UTC 2021
+// this file was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/actuators/idle_state.txt Sun Oct 31 01:38:22 UTC 2021
 
 // by class com.rusefi.output.FileJavaFieldsConsumer
 import com.rusefi.config.*;
@@ -10,76 +10,74 @@ public class IdleState {
 	public static final Field CURRENTIDLEPOSITION = Field.create("CURRENTIDLEPOSITION", 4, FieldType.FLOAT);
 	public static final Field BASEIDLEPOSITION = Field.create("BASEIDLEPOSITION", 8, FieldType.FLOAT);
 	public static final Field THROTTLEPEDALUPSTATE = Field.create("THROTTLEPEDALUPSTATE", 12, FieldType.INT);
-	public static final Field ETBIDLEADDITION = Field.create("ETBIDLEADDITION", 16, FieldType.FLOAT);
-	public static final Field MIGHTRESETPID = Field.create("MIGHTRESETPID", 20, FieldType.BIT, 0);
-	public static final Field SHOULDRESETPID = Field.create("SHOULDRESETPID", 20, FieldType.BIT, 1);
-	public static final Field WASRESETPID = Field.create("WASRESETPID", 20, FieldType.BIT, 2);
-	public static final Field MUSTRESETPID = Field.create("MUSTRESETPID", 20, FieldType.BIT, 3);
-	public static final Field UNUSEDBIT_9_4 = Field.create("UNUSEDBIT_9_4", 20, FieldType.BIT, 4);
-	public static final Field UNUSEDBIT_9_5 = Field.create("UNUSEDBIT_9_5", 20, FieldType.BIT, 5);
-	public static final Field UNUSEDBIT_9_6 = Field.create("UNUSEDBIT_9_6", 20, FieldType.BIT, 6);
-	public static final Field UNUSEDBIT_9_7 = Field.create("UNUSEDBIT_9_7", 20, FieldType.BIT, 7);
-	public static final Field UNUSEDBIT_9_8 = Field.create("UNUSEDBIT_9_8", 20, FieldType.BIT, 8);
-	public static final Field UNUSEDBIT_9_9 = Field.create("UNUSEDBIT_9_9", 20, FieldType.BIT, 9);
-	public static final Field UNUSEDBIT_9_10 = Field.create("UNUSEDBIT_9_10", 20, FieldType.BIT, 10);
-	public static final Field UNUSEDBIT_9_11 = Field.create("UNUSEDBIT_9_11", 20, FieldType.BIT, 11);
-	public static final Field UNUSEDBIT_9_12 = Field.create("UNUSEDBIT_9_12", 20, FieldType.BIT, 12);
-	public static final Field UNUSEDBIT_9_13 = Field.create("UNUSEDBIT_9_13", 20, FieldType.BIT, 13);
-	public static final Field UNUSEDBIT_9_14 = Field.create("UNUSEDBIT_9_14", 20, FieldType.BIT, 14);
-	public static final Field UNUSEDBIT_9_15 = Field.create("UNUSEDBIT_9_15", 20, FieldType.BIT, 15);
-	public static final Field UNUSEDBIT_9_16 = Field.create("UNUSEDBIT_9_16", 20, FieldType.BIT, 16);
-	public static final Field UNUSEDBIT_9_17 = Field.create("UNUSEDBIT_9_17", 20, FieldType.BIT, 17);
-	public static final Field UNUSEDBIT_9_18 = Field.create("UNUSEDBIT_9_18", 20, FieldType.BIT, 18);
-	public static final Field UNUSEDBIT_9_19 = Field.create("UNUSEDBIT_9_19", 20, FieldType.BIT, 19);
-	public static final Field UNUSEDBIT_9_20 = Field.create("UNUSEDBIT_9_20", 20, FieldType.BIT, 20);
-	public static final Field UNUSEDBIT_9_21 = Field.create("UNUSEDBIT_9_21", 20, FieldType.BIT, 21);
-	public static final Field UNUSEDBIT_9_22 = Field.create("UNUSEDBIT_9_22", 20, FieldType.BIT, 22);
-	public static final Field UNUSEDBIT_9_23 = Field.create("UNUSEDBIT_9_23", 20, FieldType.BIT, 23);
-	public static final Field UNUSEDBIT_9_24 = Field.create("UNUSEDBIT_9_24", 20, FieldType.BIT, 24);
-	public static final Field UNUSEDBIT_9_25 = Field.create("UNUSEDBIT_9_25", 20, FieldType.BIT, 25);
-	public static final Field UNUSEDBIT_9_26 = Field.create("UNUSEDBIT_9_26", 20, FieldType.BIT, 26);
-	public static final Field UNUSEDBIT_9_27 = Field.create("UNUSEDBIT_9_27", 20, FieldType.BIT, 27);
-	public static final Field UNUSEDBIT_9_28 = Field.create("UNUSEDBIT_9_28", 20, FieldType.BIT, 28);
-	public static final Field UNUSEDBIT_9_29 = Field.create("UNUSEDBIT_9_29", 20, FieldType.BIT, 29);
-	public static final Field UNUSEDBIT_9_30 = Field.create("UNUSEDBIT_9_30", 20, FieldType.BIT, 30);
-	public static final Field UNUSEDBIT_9_31 = Field.create("UNUSEDBIT_9_31", 20, FieldType.BIT, 31);
+	public static final Field MIGHTRESETPID = Field.create("MIGHTRESETPID", 16, FieldType.BIT, 0);
+	public static final Field SHOULDRESETPID = Field.create("SHOULDRESETPID", 16, FieldType.BIT, 1);
+	public static final Field WASRESETPID = Field.create("WASRESETPID", 16, FieldType.BIT, 2);
+	public static final Field MUSTRESETPID = Field.create("MUSTRESETPID", 16, FieldType.BIT, 3);
+	public static final Field UNUSEDBIT_8_4 = Field.create("UNUSEDBIT_8_4", 16, FieldType.BIT, 4);
+	public static final Field UNUSEDBIT_8_5 = Field.create("UNUSEDBIT_8_5", 16, FieldType.BIT, 5);
+	public static final Field UNUSEDBIT_8_6 = Field.create("UNUSEDBIT_8_6", 16, FieldType.BIT, 6);
+	public static final Field UNUSEDBIT_8_7 = Field.create("UNUSEDBIT_8_7", 16, FieldType.BIT, 7);
+	public static final Field UNUSEDBIT_8_8 = Field.create("UNUSEDBIT_8_8", 16, FieldType.BIT, 8);
+	public static final Field UNUSEDBIT_8_9 = Field.create("UNUSEDBIT_8_9", 16, FieldType.BIT, 9);
+	public static final Field UNUSEDBIT_8_10 = Field.create("UNUSEDBIT_8_10", 16, FieldType.BIT, 10);
+	public static final Field UNUSEDBIT_8_11 = Field.create("UNUSEDBIT_8_11", 16, FieldType.BIT, 11);
+	public static final Field UNUSEDBIT_8_12 = Field.create("UNUSEDBIT_8_12", 16, FieldType.BIT, 12);
+	public static final Field UNUSEDBIT_8_13 = Field.create("UNUSEDBIT_8_13", 16, FieldType.BIT, 13);
+	public static final Field UNUSEDBIT_8_14 = Field.create("UNUSEDBIT_8_14", 16, FieldType.BIT, 14);
+	public static final Field UNUSEDBIT_8_15 = Field.create("UNUSEDBIT_8_15", 16, FieldType.BIT, 15);
+	public static final Field UNUSEDBIT_8_16 = Field.create("UNUSEDBIT_8_16", 16, FieldType.BIT, 16);
+	public static final Field UNUSEDBIT_8_17 = Field.create("UNUSEDBIT_8_17", 16, FieldType.BIT, 17);
+	public static final Field UNUSEDBIT_8_18 = Field.create("UNUSEDBIT_8_18", 16, FieldType.BIT, 18);
+	public static final Field UNUSEDBIT_8_19 = Field.create("UNUSEDBIT_8_19", 16, FieldType.BIT, 19);
+	public static final Field UNUSEDBIT_8_20 = Field.create("UNUSEDBIT_8_20", 16, FieldType.BIT, 20);
+	public static final Field UNUSEDBIT_8_21 = Field.create("UNUSEDBIT_8_21", 16, FieldType.BIT, 21);
+	public static final Field UNUSEDBIT_8_22 = Field.create("UNUSEDBIT_8_22", 16, FieldType.BIT, 22);
+	public static final Field UNUSEDBIT_8_23 = Field.create("UNUSEDBIT_8_23", 16, FieldType.BIT, 23);
+	public static final Field UNUSEDBIT_8_24 = Field.create("UNUSEDBIT_8_24", 16, FieldType.BIT, 24);
+	public static final Field UNUSEDBIT_8_25 = Field.create("UNUSEDBIT_8_25", 16, FieldType.BIT, 25);
+	public static final Field UNUSEDBIT_8_26 = Field.create("UNUSEDBIT_8_26", 16, FieldType.BIT, 26);
+	public static final Field UNUSEDBIT_8_27 = Field.create("UNUSEDBIT_8_27", 16, FieldType.BIT, 27);
+	public static final Field UNUSEDBIT_8_28 = Field.create("UNUSEDBIT_8_28", 16, FieldType.BIT, 28);
+	public static final Field UNUSEDBIT_8_29 = Field.create("UNUSEDBIT_8_29", 16, FieldType.BIT, 29);
+	public static final Field UNUSEDBIT_8_30 = Field.create("UNUSEDBIT_8_30", 16, FieldType.BIT, 30);
+	public static final Field UNUSEDBIT_8_31 = Field.create("UNUSEDBIT_8_31", 16, FieldType.BIT, 31);
 	public static final Field[] VALUES = {
 	IDLESTATE,
 	CURRENTIDLEPOSITION,
 	BASEIDLEPOSITION,
 	THROTTLEPEDALUPSTATE,
-	ETBIDLEADDITION,
 	MIGHTRESETPID,
 	SHOULDRESETPID,
 	WASRESETPID,
 	MUSTRESETPID,
-	UNUSEDBIT_9_4,
-	UNUSEDBIT_9_5,
-	UNUSEDBIT_9_6,
-	UNUSEDBIT_9_7,
-	UNUSEDBIT_9_8,
-	UNUSEDBIT_9_9,
-	UNUSEDBIT_9_10,
-	UNUSEDBIT_9_11,
-	UNUSEDBIT_9_12,
-	UNUSEDBIT_9_13,
-	UNUSEDBIT_9_14,
-	UNUSEDBIT_9_15,
-	UNUSEDBIT_9_16,
-	UNUSEDBIT_9_17,
-	UNUSEDBIT_9_18,
-	UNUSEDBIT_9_19,
-	UNUSEDBIT_9_20,
-	UNUSEDBIT_9_21,
-	UNUSEDBIT_9_22,
-	UNUSEDBIT_9_23,
-	UNUSEDBIT_9_24,
-	UNUSEDBIT_9_25,
-	UNUSEDBIT_9_26,
-	UNUSEDBIT_9_27,
-	UNUSEDBIT_9_28,
-	UNUSEDBIT_9_29,
-	UNUSEDBIT_9_30,
-	UNUSEDBIT_9_31,
+	UNUSEDBIT_8_4,
+	UNUSEDBIT_8_5,
+	UNUSEDBIT_8_6,
+	UNUSEDBIT_8_7,
+	UNUSEDBIT_8_8,
+	UNUSEDBIT_8_9,
+	UNUSEDBIT_8_10,
+	UNUSEDBIT_8_11,
+	UNUSEDBIT_8_12,
+	UNUSEDBIT_8_13,
+	UNUSEDBIT_8_14,
+	UNUSEDBIT_8_15,
+	UNUSEDBIT_8_16,
+	UNUSEDBIT_8_17,
+	UNUSEDBIT_8_18,
+	UNUSEDBIT_8_19,
+	UNUSEDBIT_8_20,
+	UNUSEDBIT_8_21,
+	UNUSEDBIT_8_22,
+	UNUSEDBIT_8_23,
+	UNUSEDBIT_8_24,
+	UNUSEDBIT_8_25,
+	UNUSEDBIT_8_26,
+	UNUSEDBIT_8_27,
+	UNUSEDBIT_8_28,
+	UNUSEDBIT_8_29,
+	UNUSEDBIT_8_30,
+	UNUSEDBIT_8_31,
 	};
 }
