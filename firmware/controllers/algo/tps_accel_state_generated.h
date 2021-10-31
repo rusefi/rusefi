d0b2ce56548c68f5a970fdfe3701d029d82b3637
Andrey <rusefillc@gmail.com>
2021-10-30 22:20:29 -0400

    Whatever we call it, no matter how we do it - we need live data / remote view into rusEFI actual state #3353

--- firmware/controllers/algo/tps_accel_state_generated.h
+++ firmware/controllers/algo/tps_accel_state_generated.h
@@ -39,88 +39,88 @@ struct tps_accel_state_s {
 	bool isFractionalEnrichment : 1;
 	/**
 	offset 20 bit 4 */
-	bool unusedBit_9_4 : 1;
+	bool belowEpsilon : 1;
 	/**
 	offset 20 bit 5 */
-	bool unusedBit_9_5 : 1;
+	bool unusedBit_10_5 : 1;
 	/**
 	offset 20 bit 6 */
-	bool unusedBit_9_6 : 1;
+	bool unusedBit_10_6 : 1;
 	/**
 	offset 20 bit 7 */
-	bool unusedBit_9_7 : 1;
+	bool unusedBit_10_7 : 1;
 	/**
 	offset 20 bit 8 */
-	bool unusedBit_9_8 : 1;
+	bool unusedBit_10_8 : 1;
 	/**
 	offset 20 bit 9 */
-	bool unusedBit_9_9 : 1;
+	bool unusedBit_10_9 : 1;
 	/**
 	offset 20 bit 10 */
-	bool unusedBit_9_10 : 1;
+	bool unusedBit_10_10 : 1;
 	/**
 	offset 20 bit 11 */
-	bool unusedBit_9_11 : 1;
+	bool unusedBit_10_11 : 1;
 	/**
 	offset 20 bit 12 */
-	bool unusedBit_9_12 : 1;
+	bool unusedBit_10_12 : 1;
 	/**
 	offset 20 bit 13 */
-	bool unusedBit_9_13 : 1;
+	bool unusedBit_10_13 : 1;
 	/**
 	offset 20 bit 14 */
-	bool unusedBit_9_14 : 1;
+	bool unusedBit_10_14 : 1;
 	/**
 	offset 20 bit 15 */
-	bool unusedBit_9_15 : 1;
+	bool unusedBit_10_15 : 1;
 	/**
 	offset 20 bit 16 */
-	bool unusedBit_9_16 : 1;
+	bool unusedBit_10_16 : 1;
 	/**
 	offset 20 bit 17 */
-	bool unusedBit_9_17 : 1;
+	bool unusedBit_10_17 : 1;
 	/**
 	offset 20 bit 18 */
-	bool unusedBit_9_18 : 1;
+	bool unusedBit_10_18 : 1;
 	/**
 	offset 20 bit 19 */
-	bool unusedBit_9_19 : 1;
+	bool unusedBit_10_19 : 1;
 	/**
 	offset 20 bit 20 */
-	bool unusedBit_9_20 : 1;
+	bool unusedBit_10_20 : 1;
 	/**
 	offset 20 bit 21 */
-	bool unusedBit_9_21 : 1;
+	bool unusedBit_10_21 : 1;
 	/**
 	offset 20 bit 22 */
-	bool unusedBit_9_22 : 1;
+	bool unusedBit_10_22 : 1;
 	/**
 	offset 20 bit 23 */
-	bool unusedBit_9_23 : 1;
+	bool unusedBit_10_23 : 1;
 	/**
 	offset 20 bit 24 */
-	bool unusedBit_9_24 : 1;
+	bool unusedBit_10_24 : 1;
 	/**
 	offset 20 bit 25 */
-	bool unusedBit_9_25 : 1;
+	bool unusedBit_10_25 : 1;
 	/**
 	offset 20 bit 26 */
-	bool unusedBit_9_26 : 1;
+	bool unusedBit_10_26 : 1;
 	/**
 	offset 20 bit 27 */
-	bool unusedBit_9_27 : 1;
+	bool unusedBit_10_27 : 1;
 	/**
 	offset 20 bit 28 */
-	bool unusedBit_9_28 : 1;
+	bool unusedBit_10_28 : 1;
 	/**
 	offset 20 bit 29 */
-	bool unusedBit_9_29 : 1;
+	bool unusedBit_10_29 : 1;
 	/**
 	offset 20 bit 30 */
-	bool unusedBit_9_30 : 1;
+	bool unusedBit_10_30 : 1;
 	/**
 	offset 20 bit 31 */
-	bool unusedBit_9_31 : 1;
+	bool unusedBit_10_31 : 1;
 	/**
 	 * offset 24
 	 */
