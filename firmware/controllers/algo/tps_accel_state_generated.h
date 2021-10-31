432bad64edc44ae763e40428ba6981997a614528
Andrey <rusefillc@gmail.com>
2021-10-30 21:45:52 -0400

    Whatever we call it, no matter how we do it - we need live data / remote view into rusEFI actual state #3353

---
+++ firmware/controllers/algo/tps_accel_state_generated.h
@@ -0,0 +1,12 @@
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/algo/tps_accel_state.txt Sat Oct 30 21:35:17 EDT 2021
+// by class com.rusefi.output.CHeaderConsumer
+// begin
+#pragma once
+#include "rusefi_types.h"
+// start of tps_accel_state_s
+struct tps_accel_state_s {
+	/** total size 0*/
+};
+
+// end
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/algo/tps_accel_state.txt Sat Oct 30 21:35:17 EDT 2021
