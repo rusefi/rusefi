2ffcc2bb4532c81cf55b323b80b61cce057b63c5
Scott Smith <scott.smith@gmail.com>
2021-11-05 16:26:25 -0700

    Determine the correct mul/div for autoscale from the TS config line instead of specifying it twice. (#3470)

--- java_tools/configuration_definition/src/main/java/com/rusefi/ConfigField.java
+++ java_tools/configuration_definition/src/main/java/com/rusefi/ConfigField.java
@@ -14,11 +14,11 @@ import java.util.regex.Pattern;
  * 1/15/15
  */
 public class ConfigField {
-    public static final ConfigField VOID = new ConfigField(null, "", null, null, null, new int[0], null, false, false, null,null, -1, null, null);
+    public static final ConfigField VOID = new ConfigField(null, "", null, null, null, new int[0], null, false, false, false, null, -1, null, null);
 
     private static final String typePattern = "([\\w\\d_]+)(\\[([\\w\\d]+)(\\sx\\s([\\w\\d]+))?(\\s([\\w\\d]+))?\\])?";
 
-    private static final String namePattern = "[[\\w\\d\\s<>_]]+";
+    private static final String namePattern = "[[\\w\\d\\s_]]+";
     private static final String commentPattern = ";([^;]*)";
 
     private static final Pattern FIELD = Pattern.compile(typePattern + "\\s(" + namePattern + ")(" + commentPattern + ")?(;(.*))?");
@@ -42,7 +42,7 @@ public class ConfigField {
     private final boolean isIterate;
     private final ReaderState state;
     private boolean fsioVisible;
-    private final String autoscaleSpec;
+    private final boolean hasAutoscale;
     private final String individualName;
     private final int indexWithinArray;
     private final String trueName;
@@ -60,11 +60,11 @@ public class ConfigField {
                        String tsInfo,
                        boolean isIterate,
                        boolean fsioVisible,
-                       String autoscaleSpec,
+                       boolean hasAutoscale,
                        String individualName,
                        int indexWithinArray, String trueName, String falseName) {
         this.fsioVisible = fsioVisible;
-        this.autoscaleSpec = autoscaleSpec;
+        this.hasAutoscale = hasAutoscale;
         this.individualName = individualName;
         this.indexWithinArray = indexWithinArray;
         this.trueName = trueName == null ? "true" : trueName;
@@ -143,14 +143,12 @@ public class ConfigField {
 
         boolean isFsioVisible = Arrays.stream(nameTokens).anyMatch(s -> s.equalsIgnoreCase("fsio_visible"));
 
-        String autoscaleSpec = null;
+        boolean hasAutoscale = false;
         for (String autoscaler : nameTokens) {
-            if (!autoscaler.startsWith("autoscale")) {
-                continue;
+            if (autoscaler.equals("autoscale")) {
+                hasAutoscale = true;
+                break;
             }
-
-            autoscaleSpec = autoscaler.split("[<>]")[1];
-            break;
         }
 
         String comment = matcher.group(10);
@@ -176,7 +174,7 @@ public class ConfigField {
 
 
         ConfigField field = new ConfigField(state, name, comment, arraySizeAsText, type, arraySizes,
-                tsInfo, isIterate, isFsioVisible, autoscaleSpec, null, -1, null, null);
+                tsInfo, isIterate, isFsioVisible, hasAutoscale, null, -1, null, null);
         SystemOut.println("type " + type);
         SystemOut.println("name " + name);
         SystemOut.println("comment " + comment);
@@ -261,7 +259,39 @@ public class ConfigField {
         return fsioVisible;
     }
 
-    public String autoscaleSpec() { return this.autoscaleSpec; }
+    public String autoscaleSpec() {
+        if (!hasAutoscale) {
+            return null;
+        }
+        String[] tokens = tsInfo.split("\\,");
+        String scale = tokens[1].trim();
+        double factor;
+        if (scale.startsWith("{") && scale.endsWith("}")) {
+            // Handle just basic division, not a full fledged eval loop
+            scale = scale.substring(1, scale.length() - 1);
+            String[] parts = scale.split("/");
+            factor = Double.parseDouble(parts[0]) / Double.parseDouble(parts[1]);
+        } else {
+            factor = Double.parseDouble(scale);
+        }
+        int mul, div;
+        if (factor < 1.d) {
+            mul = (int)Math.round(1. / factor);
+            div = 1;
+        } else {
+            mul = 1;
+            div = (int)factor;
+        }
+        // Verify accuracy
+        double factor2 = ((double)div) / mul;
+        double accuracy = Math.abs((factor2 / factor) - 1.);
+        if (accuracy > 0.0000001) {
+            // Don't want to deal with exception propogation; this should adequately not compile
+            return "$*@#$* Cannot accurately represent autoscale for " + tokens[1];
+        }
+        
+        return mul + ", " + div;
+    }
 
     public String getUnits() {
         if (tsInfo == null)
--- java_tools/configuration_definition/src/main/java/com/rusefi/ConfigStructure.java
+++ java_tools/configuration_definition/src/main/java/com/rusefi/ConfigStructure.java
@@ -82,7 +82,7 @@ public class ConfigStructure {
 	    }
             ConfigField fill = new ConfigField(state, ALIGNMENT_FILL_AT + totalSize, "need 4 byte alignment",
                     "" + fillSize,
-		    TypesHelper.UINT8_T, fillSizeArray, "\"units\", 1, 0, -20, 100, 0", false, false, null, null, -1, null, null);
+		    TypesHelper.UINT8_T, fillSizeArray, "\"units\", 1, 0, -20, 100, 0", false, false, false, null, -1, null, null);
             addBoth(fill);
         }
         totalSize += fillSize;
@@ -116,7 +116,7 @@ public class ConfigStructure {
             return;
         int sizeAtStartOfPadding = cFields.size();
         while (readingBitState.get() < 32) {
-            ConfigField bitField = new ConfigField(readerState, "unusedBit_" + sizeAtStartOfPadding + "_" + readingBitState.get(), "", null, BOOLEAN_T, new int[0], null, false, false, null, null, -1, null, null);
+            ConfigField bitField = new ConfigField(readerState, "unusedBit_" + sizeAtStartOfPadding + "_" + readingBitState.get(), "", null, BOOLEAN_T, new int[0], null, false, false, false, null, -1, null, null);
             addBitField(bitField);
         }
         readingBitState.reset();
