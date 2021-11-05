c05376ed5f22659532642c576a05decc094498a7
Andrey <rusefillc@gmail.com>
2021-11-05 15:25:29 -0400

    lua curve progress

--- firmware/controllers/core/fsio_impl.h
+++ firmware/controllers/core/fsio_impl.h
@@ -39,5 +39,6 @@ void onConfigurationChangeFsioCallback(engine_configuration_s *previousConfigura
 void runHardcodedFsio(DECLARE_ENGINE_PARAMETER_SIGNATURE);
 
 float getCurveValue(int index, float key DECLARE_ENGINE_PARAMETER_SUFFIX);
+int getCurveIndexByName(const char *name DECLARE_ENGINE_PARAMETER_SUFFIX);
 ValueProvider3D *getFSIOTable(int index);
 
