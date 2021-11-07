fdc32277ead35b6c0f1e7490fcfee852e023e6ec
GitHub build-firmware Action <action@github.com>
2021-11-07 04:44:16 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen72.h
+++ firmware/controllers/generated/signature_hellen72.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 2051143393
-#define TS_SIGNATURE "rusEFI 2021.11.07.hellen72.2051143393"
+#define SIGNATURE_HASH 4220556938
+#define TS_SIGNATURE "rusEFI 2021.11.07.hellen72.4220556938"
--- firmware/controllers/generated/signature_hellenNA6.h
+++ firmware/controllers/generated/signature_hellenNA6.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 2911674312
-#define TS_SIGNATURE "rusEFI 2021.11.07.hellenNA6.2911674312"
+#define SIGNATURE_HASH 744357795
+#define TS_SIGNATURE "rusEFI 2021.11.07.hellenNA6.744357795"
