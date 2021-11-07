678f5db3b9bfdc6c655d1c435b765c5bff3f7fbc
GitHub build-firmware Action <action@github.com>
2021-11-07 15:30:54 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen128.h
+++ firmware/controllers/generated/signature_hellen128.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 2024136565
-#define TS_SIGNATURE "rusEFI 2021.11.07.hellen128.2024136565"
+#define SIGNATURE_HASH 1120375612
+#define TS_SIGNATURE "rusEFI 2021.11.07.hellen128.1120375612"
