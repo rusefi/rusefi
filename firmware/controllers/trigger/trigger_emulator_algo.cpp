38ea0d183566a2159bbdda76787aef67eea06533
rusefillc <sdfsdfqsf2334234234>
2021-11-09 20:42:02 -0500

    Revert "Don't keep a separate MultiChannelStateSequence for the trigger emulator. (#3513)"
    This reverts commit 4e220dc163256f6ed35b424a6ab7275020eaa567.

--- firmware/controllers/trigger/trigger_emulator_algo.cpp
+++ firmware/controllers/trigger/trigger_emulator_algo.cpp
@@ -53,7 +53,20 @@ void TriggerEmulatorHelper::handleEmulatorCallback(const int size, const MultiCh
 	}
 }
 
-PwmConfig triggerSignal;
+/*
+ * todo: should we simply re-use instances used by trigger_decoder?
+ * todo: since we are emulating same shape we are decoding
+ */
+static pin_state_t pinStates1[PWM_PHASE_MAX_COUNT];
+static pin_state_t pinStates2[PWM_PHASE_MAX_COUNT];
+static pin_state_t pinStates3[PWM_PHASE_MAX_COUNT];
+static SingleChannelStateSequence waves[PWM_PHASE_MAX_WAVE_PER_PWM] = { SingleChannelStateSequence(pinStates1), SingleChannelStateSequence(pinStates2),
+		SingleChannelStateSequence(pinStates3) };
+static SingleChannelStateSequence sr[PWM_PHASE_MAX_WAVE_PER_PWM] = { waves[0], waves[1], waves[2] };
+
+static float pwmSwitchTimesBuffer[PWM_PHASE_MAX_COUNT];
+
+PwmConfig triggerSignal(pwmSwitchTimesBuffer, sr);
 
 static int atTriggerVersion = 0;
 
@@ -106,7 +119,11 @@ static void updateTriggerWaveformIfNeeded(PwmConfig *state DECLARE_ENGINE_PARAME
 
 
 		TriggerWaveform *s = &engine->triggerCentral.triggerShape;
-		copyPwmParameters(state, s->getSize(), &s->wave);
+		pin_state_t *pinStates[PWM_PHASE_MAX_WAVE_PER_PWM] = {
+				s->wave.channels[0].pinStates,
+				s->wave.channels[1].pinStates,
+				s->wave.channels[2].pinStates };
+		copyPwmParameters(state, s->getSize(), s->wave.switchTimes, PWM_PHASE_MAX_WAVE_PER_PWM, pinStates);
 		state->safe.periodNt = -1; // this would cause loop re-initialization
 	}
 }
@@ -124,7 +141,7 @@ static void emulatorApplyPinState(int stateIndex, PwmConfig *state) /* pwm_gen_c
 		 * this callback would invoke the input signal handlers directly
 		 */
 		helper.handleEmulatorCallback(state->phaseCount,
-				*state->multiChannelStateSequence,
+				state->multiChannelStateSequence,
 				stateIndex);
 	}
 
@@ -144,11 +161,16 @@ static void initTriggerPwm(DECLARE_ENGINE_PARAMETER_SIGNATURE) {
 
 	TriggerWaveform *s = &engine->triggerCentral.triggerShape;
 	setTriggerEmulatorRPM(engineConfiguration->triggerSimulatorFrequency PASS_ENGINE_PARAMETER_SUFFIX);
+	pin_state_t *pinStates[PWM_PHASE_MAX_WAVE_PER_PWM] = {
+			s->wave.channels[0].pinStates,
+			s->wave.channels[1].pinStates,
+			s->wave.channels[2].pinStates };
 	int phaseCount = s->getSize();
+	assertIsInBounds(phaseCount - 1, pwmSwitchTimesBuffer, "pwmSwitchTimesBuffer");
 	triggerSignal.weComplexInit("position sensor",
 			&engine->executor,
-			phaseCount, &s->wave,
-			updateTriggerWaveformIfNeeded, (pwm_gen_callback*)emulatorApplyPinState);
+			phaseCount, s->wave.switchTimes, PWM_PHASE_MAX_WAVE_PER_PWM,
+			pinStates, updateTriggerWaveformIfNeeded, (pwm_gen_callback*)emulatorApplyPinState);
 
 	hasInitTriggerEmulator = true;
 }
