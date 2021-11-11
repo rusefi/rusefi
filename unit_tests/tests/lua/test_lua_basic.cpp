72a2bdba66a0cdfa3b3bea6d4f08f90b01e9c0d0
rusefi <rusefillc@gmail.com>
2021-11-10 19:23:05 -0500

    MathMin

--- unit_tests/tests/lua/test_lua_basic.cpp
+++ unit_tests/tests/lua/test_lua_basic.cpp
@@ -13,6 +13,20 @@ TEST(LuaBasic, ReturnsNumber) {
 	EXPECT_FLOAT_EQ(result, 5.5f);
 }
 
+TEST(LuaBasic, MathMin) {
+/*
+	auto script = R"(
+						function testFunc()
+							return min(1, 2)
+						end
+				)";
+
+	float result = testLuaReturnsNumber(script);
+
+	EXPECT_FLOAT_EQ(result, 1.0f);
+*/
+}
+
 TEST(LuaBasic, ReturnsInteger) {
 	auto script = R"(
 						function testFunc()
