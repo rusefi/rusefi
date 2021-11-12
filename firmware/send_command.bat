cdf069ff1694be0a0c1a0701c7a8c6786faf6b84
rusefi <rusefillc@gmail.com>
2021-11-12 12:45:41 -0500

    maybe lua freezes proteus h7 after 7 seconds of run #3539

---
+++ firmware/send_command.bat
@@ -0,0 +1,8 @@
+rem
+rem auto-detects connected running rusEfi serial port and send text 'reboot' command
+rem 
+
+set command=%1
+echo "Command: [%command%]"
+
+java -jar ../java_console_binary/rusefi_console.jar %command%
