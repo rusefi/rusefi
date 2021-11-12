e4c8fce68fefb16839786939f077739140cf2590
Matthew Kennedy <makenne@microsoft.com>
2021-11-12 11:35:07 -0800

    vss uses real values

--- firmware/controllers/sensors/converters/vehicle_speed_converter.h
+++ firmware/controllers/sensors/converters/vehicle_speed_converter.h
@@ -6,7 +6,20 @@ public:
 	DECLARE_ENGINE_PTR;
 
 	SensorResult convert(float frequency) const override {
-		auto speed = frequency * engineConfiguration->vehicleSpeedCoef;
-		return speed;
+		auto vssRevPerKm = CONFIG(driveWheelRevPerKm) * CONFIG(vssGearRatio);
+
+		auto pulsePerKm = (vssRevPerKm * CONFIG(vssToothCount));
+
+		if (pulsePerKm == 0) {
+			// avoid div by 0
+			return 0;
+		}
+
+		auto kmPerPulse = 1 / pulsePerKm;
+
+		//     1 pulse       3600 sec      1 km       km
+		//    ---------  *  ---------- * --------- = -----
+		//       sec           1 hr       1 pulse     sec
+		return frequency *     3600    * pulsePerKm;
 	}
 };
