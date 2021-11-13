dffab7abff74ca54e4b4faef2f0898ab538c27f3
GitHub build-firmware Action <action@github.com>
2021-11-13 03:01:55 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_prometheus_405.h
+++ firmware/controllers/generated/signature_prometheus_405.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 1051654634
-#define TS_SIGNATURE "rusEFI 2021.11.12.prometheus_405.1051654634"
+#define SIGNATURE_HASH 3976473171
+#define TS_SIGNATURE "rusEFI 2021.11.13.prometheus_405.3976473171"
