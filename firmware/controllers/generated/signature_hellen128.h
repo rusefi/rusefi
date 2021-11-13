625eda6d09009973fa0bd8eca4b9acd19412acea
GitHub build-firmware Action <action@github.com>
2021-11-13 15:38:11 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen128.h
+++ firmware/controllers/generated/signature_hellen128.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 1603322692
-#define TS_SIGNATURE "rusEFI 2021.11.13.hellen128.1603322692"
+#define SIGNATURE_HASH 4023326348
+#define TS_SIGNATURE "rusEFI 2021.11.13.hellen128.4023326348"
--- firmware/controllers/generated/signature_hellen_cypress.h
+++ firmware/controllers/generated/signature_hellen_cypress.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on hellen_cypress_gen_config.bat integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 2293829748
-#define TS_SIGNATURE "rusEFI 2021.11.13.hellen_cypress.2293829748"
+#define SIGNATURE_HASH 954647996
+#define TS_SIGNATURE "rusEFI 2021.11.13.hellen_cypress.954647996"
