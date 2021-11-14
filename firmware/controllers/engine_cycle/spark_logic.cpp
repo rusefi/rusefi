6210eaacf777092c7358be22e2d35569dab7cb56
rusefillc <sdfsdfqsf2334234234>
2021-11-13 23:10:38 -0500

    script settings need names!

--- firmware/controllers/engine_cycle/spark_logic.cpp
+++ firmware/controllers/engine_cycle/spark_logic.cpp
@@ -556,7 +556,7 @@ void onTriggerEventSparkLogic(bool limitedSpark, uint32_t trgEventIndex, int rpm
 			if (event->dwellPosition.triggerEventIndex != trgEventIndex)
 				continue;
 
-			if (i == 0 && CONFIG(artificialTestMisfire) && (getRevolutionCounter() % ((int)engineConfiguration->fsio_setting[5]) == 0)) {
+			if (i == 0 && CONFIG(artificialTestMisfire) && (getRevolutionCounter() % ((int)engineConfiguration->scriptSetting[5]) == 0)) {
 				// artificial misfire on cylinder #1 for testing purposes
 				// enable artificialMisfire
 				// set_fsio_setting 6 20
