aa3fc55c8e1dbd67a33b0e6a4cef0106400f6089
GitHub build-firmware Action <action@github.com>
2021-11-14 15:07:50 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen_cypress.h
+++ firmware/controllers/generated/signature_hellen_cypress.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on hellen_cypress_gen_config.bat integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 3933246252
-#define TS_SIGNATURE "rusEFI 2021.11.14.hellen_cypress.3933246252"
+#define SIGNATURE_HASH 223845907
+#define TS_SIGNATURE "rusEFI 2021.11.14.hellen_cypress.223845907"
--- firmware/controllers/generated/signature_subaru_eg33_f7.h
+++ firmware/controllers/generated/signature_subaru_eg33_f7.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on config/boards/subaru_eg33/config/gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 322921152
-#define TS_SIGNATURE "rusEFI 2021.11.14.subaru_eg33_f7.322921152"
+#define SIGNATURE_HASH 4095242239
+#define TS_SIGNATURE "rusEFI 2021.11.14.subaru_eg33_f7.4095242239"
