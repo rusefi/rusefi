756b9e2b3d212d446010c742608a4508ae106faf
GitHub build-firmware Action <action@github.com>
2021-11-14 14:27:16 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_kin.h
+++ firmware/controllers/generated/signature_kin.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on kinetis_gen_config.bat integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 1902586993
-#define TS_SIGNATURE "rusEFI 2021.11.14.kin.1902586993"
+#define SIGNATURE_HASH 930756429
+#define TS_SIGNATURE "rusEFI 2021.11.14.kin.930756429"
--- firmware/controllers/generated/signature_subaru_eg33_f7.h
+++ firmware/controllers/generated/signature_subaru_eg33_f7.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on config/boards/subaru_eg33/config/gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 2005733729
-#define TS_SIGNATURE "rusEFI 2021.11.14.subaru_eg33_f7.2005733729"
+#define SIGNATURE_HASH 831523421
+#define TS_SIGNATURE "rusEFI 2021.11.14.subaru_eg33_f7.831523421"
