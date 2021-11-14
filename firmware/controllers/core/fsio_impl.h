1a3fb4a0dbc4d4c01a741356c505d9ee674e91d2
rusefillc <sdfsdfqsf2334234234>
2021-11-14 11:04:51 -0500

    Epic: Remove FSIO #2928

--- firmware/controllers/core/fsio_impl.h
+++ firmware/controllers/core/fsio_impl.h
@@ -17,8 +17,8 @@
 // see useFSIO5ForCriticalIssueEngineStop
 #define MAGIC_OFFSET_FOR_CRITICAL_ENGINE 5
 
-typedef Map3D<SCRIPT_TABLE__8, SCRIPT_TABLE__8, float, float, float> fsio8_Map3D_f32t;
-typedef Map3D<SCRIPT_TABLE__8, SCRIPT_TABLE__8, uint8_t, float, float> fsio8_Map3D_u8t;
+typedef Map3D<SCRIPT_TABLE_8, SCRIPT_TABLE_8, float, float, float> fsio8_Map3D_f32t;
+typedef Map3D<SCRIPT_TABLE_8, SCRIPT_TABLE_8, uint8_t, float, float> fsio8_Map3D_u8t;
 
 expected<float> getEngineValue(le_action_e action DECLARE_ENGINE_PARAMETER_SUFFIX);
 
