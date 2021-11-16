b477365e7cd8b25aa696fa91c6a21b8062f17e4c
GitHub build-firmware Action <action@github.com>
2021-11-16 02:53:15 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen154hyundai.h
+++ firmware/controllers/generated/signature_hellen154hyundai.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 3722014070
-#define TS_SIGNATURE "rusEFI 2021.11.15.hellen154hyundai.3722014070"
+#define SIGNATURE_HASH 3158337385
+#define TS_SIGNATURE "rusEFI 2021.11.16.hellen154hyundai.3158337385"
