6fdb98e84f76a41e384792b3057eab667dd561f8
rusefillc <sdfsdfqsf2334234234>
2021-11-16 16:45:14 -0500

    helping launch enter simulator

--- firmware/controllers/algo/launch_control.cpp
+++ firmware/controllers/algo/launch_control.cpp
@@ -22,7 +22,7 @@
 bool LaunchControlBase::isInsideSwitchCondition() {
 	switch (CONFIG(launchActivationMode)) {
 	case SWITCH_INPUT_LAUNCH:
-#if EFI_PROD_CODE
+#if !EFI_SIMULATOR
 		if (isBrainPinValid(CONFIG(launchActivatePin))) {
 			//todo: we should take into consideration if this sw is pulled high or low!
 			launchActivatePinState = efiReadPin(CONFIG(launchActivatePin));
