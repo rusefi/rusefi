674afb070f1049cd9c1927d3e6e050a93d9f54fb
Matthew Kennedy <matthewkennedy@outlook.com>
2021-11-20 02:40:04 -0800

    test

--- unit_tests/tests/trigger/test_real_gm_24x.cpp
+++ unit_tests/tests/trigger/test_real_gm_24x.cpp
@@ -6,14 +6,19 @@ TEST(crankingGm24x, gmRealCrankingFromFile) {
 	CsvReader reader(1, /* vvtCount */ 0);
 	int indeces[1] = {0};
 
-	reader.open("tests/trigger/resources/nick_1.csv", indeces);
-	EngineTestHelper eth (TEST_ENGINE);
+	reader.open("tests/trigger/resources/gm_24x_cranking.csv", indeces);
+	EngineTestHelper eth(TEST_ENGINE);
 	engineConfiguration->isFasterEngineSpinUpEnabled = true;
 
 	eth.setTriggerType(TT_GM_LS_24);
 
+	// extern bool verboseMode;
+	// verboseMode = true;
+
 	while (reader.haveMore()) {
 		reader.processLine(&eth);
+
+		engine->rpmCalculator.onSlowCallback();
 	}
 
 	//ASSERT_EQ( 0, eth.recentWarnings()->getCount())<< "warningCounter#vwRealCranking";
