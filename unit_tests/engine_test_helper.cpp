f1900b4887041cde0816dbff6bdf88d26c6f5148
rusefillc <sdfsdfqsf2334234234>
2021-11-20 00:23:27 -0500

    On -fpermissive in Windows in unit tests #3581

--- unit_tests/engine_test_helper.cpp
+++ unit_tests/engine_test_helper.cpp
@@ -329,7 +329,7 @@ void EngineTestHelper::assertEvent(const char *msg, int index, void *callback, e
 
 	InjectionEvent *actualEvent = (InjectionEvent *)event->action.getArgument();
 
-	assertEqualsLM(msg, (long)expectedEvent->outputs[0], (long)actualEvent->outputs[0]);
+	assertEqualsLM(msg, (uintptr_t)expectedEvent->outputs[0], (uintptr_t)actualEvent->outputs[0]);
 // but this would not work	assertEqualsLM(msg, expectedPair, (long)eventPair);
 }
 
