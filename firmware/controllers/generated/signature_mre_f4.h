34c2fcc60d64c50635fda6939dba4f25c25000ed
GitHub build-firmware Action <action@github.com>
2021-11-21 13:16:25 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_mre_f4.h
+++ firmware/controllers/generated/signature_mre_f4.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 3115368262
-#define TS_SIGNATURE "rusEFI 2021.11.20.mre_f4.3115368262"
+#define SIGNATURE_HASH 2177674835
+#define TS_SIGNATURE "rusEFI 2021.11.21.mre_f4.2177674835"
