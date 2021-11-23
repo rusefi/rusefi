ca1e42007380257df4fde22c7b5201bfaea7c2de
rusefillc <sdfsdfqsf2334234234>
2021-11-23 15:49:55 -0500

    live data for wastegate #3588

--- java_console/models/src/main/java/com/rusefi/config/generated/BoostControl.java
+++ java_console/models/src/main/java/com/rusefi/config/generated/BoostControl.java
@@ -8,71 +8,71 @@ import com.rusefi.config.*;
 public class BoostControl {
 	public static final Field ISTPSVALID = Field.create("ISTPSVALID", 0, FieldType.BIT, 0);
 	public static final Field M_SHOULDRESETPID = Field.create("M_SHOULDRESETPID", 0, FieldType.BIT, 1);
-	public static final Field UNUSEDBIT_2_2 = Field.create("UNUSEDBIT_2_2", 0, FieldType.BIT, 2);
-	public static final Field UNUSEDBIT_2_3 = Field.create("UNUSEDBIT_2_3", 0, FieldType.BIT, 3);
-	public static final Field UNUSEDBIT_2_4 = Field.create("UNUSEDBIT_2_4", 0, FieldType.BIT, 4);
-	public static final Field UNUSEDBIT_2_5 = Field.create("UNUSEDBIT_2_5", 0, FieldType.BIT, 5);
-	public static final Field UNUSEDBIT_2_6 = Field.create("UNUSEDBIT_2_6", 0, FieldType.BIT, 6);
-	public static final Field UNUSEDBIT_2_7 = Field.create("UNUSEDBIT_2_7", 0, FieldType.BIT, 7);
-	public static final Field UNUSEDBIT_2_8 = Field.create("UNUSEDBIT_2_8", 0, FieldType.BIT, 8);
-	public static final Field UNUSEDBIT_2_9 = Field.create("UNUSEDBIT_2_9", 0, FieldType.BIT, 9);
-	public static final Field UNUSEDBIT_2_10 = Field.create("UNUSEDBIT_2_10", 0, FieldType.BIT, 10);
-	public static final Field UNUSEDBIT_2_11 = Field.create("UNUSEDBIT_2_11", 0, FieldType.BIT, 11);
-	public static final Field UNUSEDBIT_2_12 = Field.create("UNUSEDBIT_2_12", 0, FieldType.BIT, 12);
-	public static final Field UNUSEDBIT_2_13 = Field.create("UNUSEDBIT_2_13", 0, FieldType.BIT, 13);
-	public static final Field UNUSEDBIT_2_14 = Field.create("UNUSEDBIT_2_14", 0, FieldType.BIT, 14);
-	public static final Field UNUSEDBIT_2_15 = Field.create("UNUSEDBIT_2_15", 0, FieldType.BIT, 15);
-	public static final Field UNUSEDBIT_2_16 = Field.create("UNUSEDBIT_2_16", 0, FieldType.BIT, 16);
-	public static final Field UNUSEDBIT_2_17 = Field.create("UNUSEDBIT_2_17", 0, FieldType.BIT, 17);
-	public static final Field UNUSEDBIT_2_18 = Field.create("UNUSEDBIT_2_18", 0, FieldType.BIT, 18);
-	public static final Field UNUSEDBIT_2_19 = Field.create("UNUSEDBIT_2_19", 0, FieldType.BIT, 19);
-	public static final Field UNUSEDBIT_2_20 = Field.create("UNUSEDBIT_2_20", 0, FieldType.BIT, 20);
-	public static final Field UNUSEDBIT_2_21 = Field.create("UNUSEDBIT_2_21", 0, FieldType.BIT, 21);
-	public static final Field UNUSEDBIT_2_22 = Field.create("UNUSEDBIT_2_22", 0, FieldType.BIT, 22);
-	public static final Field UNUSEDBIT_2_23 = Field.create("UNUSEDBIT_2_23", 0, FieldType.BIT, 23);
-	public static final Field UNUSEDBIT_2_24 = Field.create("UNUSEDBIT_2_24", 0, FieldType.BIT, 24);
-	public static final Field UNUSEDBIT_2_25 = Field.create("UNUSEDBIT_2_25", 0, FieldType.BIT, 25);
-	public static final Field UNUSEDBIT_2_26 = Field.create("UNUSEDBIT_2_26", 0, FieldType.BIT, 26);
-	public static final Field UNUSEDBIT_2_27 = Field.create("UNUSEDBIT_2_27", 0, FieldType.BIT, 27);
-	public static final Field UNUSEDBIT_2_28 = Field.create("UNUSEDBIT_2_28", 0, FieldType.BIT, 28);
-	public static final Field UNUSEDBIT_2_29 = Field.create("UNUSEDBIT_2_29", 0, FieldType.BIT, 29);
-	public static final Field UNUSEDBIT_2_30 = Field.create("UNUSEDBIT_2_30", 0, FieldType.BIT, 30);
-	public static final Field UNUSEDBIT_2_31 = Field.create("UNUSEDBIT_2_31", 0, FieldType.BIT, 31);
+	public static final Field ISBELOWCLOSEDLOOPTHRESHOLD = Field.create("ISBELOWCLOSEDLOOPTHRESHOLD", 0, FieldType.BIT, 2);
+	public static final Field UNUSEDBIT_3_3 = Field.create("UNUSEDBIT_3_3", 0, FieldType.BIT, 3);
+	public static final Field UNUSEDBIT_3_4 = Field.create("UNUSEDBIT_3_4", 0, FieldType.BIT, 4);
+	public static final Field UNUSEDBIT_3_5 = Field.create("UNUSEDBIT_3_5", 0, FieldType.BIT, 5);
+	public static final Field UNUSEDBIT_3_6 = Field.create("UNUSEDBIT_3_6", 0, FieldType.BIT, 6);
+	public static final Field UNUSEDBIT_3_7 = Field.create("UNUSEDBIT_3_7", 0, FieldType.BIT, 7);
+	public static final Field UNUSEDBIT_3_8 = Field.create("UNUSEDBIT_3_8", 0, FieldType.BIT, 8);
+	public static final Field UNUSEDBIT_3_9 = Field.create("UNUSEDBIT_3_9", 0, FieldType.BIT, 9);
+	public static final Field UNUSEDBIT_3_10 = Field.create("UNUSEDBIT_3_10", 0, FieldType.BIT, 10);
+	public static final Field UNUSEDBIT_3_11 = Field.create("UNUSEDBIT_3_11", 0, FieldType.BIT, 11);
+	public static final Field UNUSEDBIT_3_12 = Field.create("UNUSEDBIT_3_12", 0, FieldType.BIT, 12);
+	public static final Field UNUSEDBIT_3_13 = Field.create("UNUSEDBIT_3_13", 0, FieldType.BIT, 13);
+	public static final Field UNUSEDBIT_3_14 = Field.create("UNUSEDBIT_3_14", 0, FieldType.BIT, 14);
+	public static final Field UNUSEDBIT_3_15 = Field.create("UNUSEDBIT_3_15", 0, FieldType.BIT, 15);
+	public static final Field UNUSEDBIT_3_16 = Field.create("UNUSEDBIT_3_16", 0, FieldType.BIT, 16);
+	public static final Field UNUSEDBIT_3_17 = Field.create("UNUSEDBIT_3_17", 0, FieldType.BIT, 17);
+	public static final Field UNUSEDBIT_3_18 = Field.create("UNUSEDBIT_3_18", 0, FieldType.BIT, 18);
+	public static final Field UNUSEDBIT_3_19 = Field.create("UNUSEDBIT_3_19", 0, FieldType.BIT, 19);
+	public static final Field UNUSEDBIT_3_20 = Field.create("UNUSEDBIT_3_20", 0, FieldType.BIT, 20);
+	public static final Field UNUSEDBIT_3_21 = Field.create("UNUSEDBIT_3_21", 0, FieldType.BIT, 21);
+	public static final Field UNUSEDBIT_3_22 = Field.create("UNUSEDBIT_3_22", 0, FieldType.BIT, 22);
+	public static final Field UNUSEDBIT_3_23 = Field.create("UNUSEDBIT_3_23", 0, FieldType.BIT, 23);
+	public static final Field UNUSEDBIT_3_24 = Field.create("UNUSEDBIT_3_24", 0, FieldType.BIT, 24);
+	public static final Field UNUSEDBIT_3_25 = Field.create("UNUSEDBIT_3_25", 0, FieldType.BIT, 25);
+	public static final Field UNUSEDBIT_3_26 = Field.create("UNUSEDBIT_3_26", 0, FieldType.BIT, 26);
+	public static final Field UNUSEDBIT_3_27 = Field.create("UNUSEDBIT_3_27", 0, FieldType.BIT, 27);
+	public static final Field UNUSEDBIT_3_28 = Field.create("UNUSEDBIT_3_28", 0, FieldType.BIT, 28);
+	public static final Field UNUSEDBIT_3_29 = Field.create("UNUSEDBIT_3_29", 0, FieldType.BIT, 29);
+	public static final Field UNUSEDBIT_3_30 = Field.create("UNUSEDBIT_3_30", 0, FieldType.BIT, 30);
+	public static final Field UNUSEDBIT_3_31 = Field.create("UNUSEDBIT_3_31", 0, FieldType.BIT, 31);
 	public static final Field OPENLOOPPART = Field.create("OPENLOOPPART", 4, FieldType.FLOAT);
 	public static final Field CLOSEDLOOPPART = Field.create("CLOSEDLOOPPART", 8, FieldType.FLOAT);
 	public static final Field[] VALUES = {
 	ISTPSVALID,
 	M_SHOULDRESETPID,
-	UNUSEDBIT_2_2,
-	UNUSEDBIT_2_3,
-	UNUSEDBIT_2_4,
-	UNUSEDBIT_2_5,
-	UNUSEDBIT_2_6,
-	UNUSEDBIT_2_7,
-	UNUSEDBIT_2_8,
-	UNUSEDBIT_2_9,
-	UNUSEDBIT_2_10,
-	UNUSEDBIT_2_11,
-	UNUSEDBIT_2_12,
-	UNUSEDBIT_2_13,
-	UNUSEDBIT_2_14,
-	UNUSEDBIT_2_15,
-	UNUSEDBIT_2_16,
-	UNUSEDBIT_2_17,
-	UNUSEDBIT_2_18,
-	UNUSEDBIT_2_19,
-	UNUSEDBIT_2_20,
-	UNUSEDBIT_2_21,
-	UNUSEDBIT_2_22,
-	UNUSEDBIT_2_23,
-	UNUSEDBIT_2_24,
-	UNUSEDBIT_2_25,
-	UNUSEDBIT_2_26,
-	UNUSEDBIT_2_27,
-	UNUSEDBIT_2_28,
-	UNUSEDBIT_2_29,
-	UNUSEDBIT_2_30,
-	UNUSEDBIT_2_31,
+	ISBELOWCLOSEDLOOPTHRESHOLD,
+	UNUSEDBIT_3_3,
+	UNUSEDBIT_3_4,
+	UNUSEDBIT_3_5,
+	UNUSEDBIT_3_6,
+	UNUSEDBIT_3_7,
+	UNUSEDBIT_3_8,
+	UNUSEDBIT_3_9,
+	UNUSEDBIT_3_10,
+	UNUSEDBIT_3_11,
+	UNUSEDBIT_3_12,
+	UNUSEDBIT_3_13,
+	UNUSEDBIT_3_14,
+	UNUSEDBIT_3_15,
+	UNUSEDBIT_3_16,
+	UNUSEDBIT_3_17,
+	UNUSEDBIT_3_18,
+	UNUSEDBIT_3_19,
+	UNUSEDBIT_3_20,
+	UNUSEDBIT_3_21,
+	UNUSEDBIT_3_22,
+	UNUSEDBIT_3_23,
+	UNUSEDBIT_3_24,
+	UNUSEDBIT_3_25,
+	UNUSEDBIT_3_26,
+	UNUSEDBIT_3_27,
+	UNUSEDBIT_3_28,
+	UNUSEDBIT_3_29,
+	UNUSEDBIT_3_30,
+	UNUSEDBIT_3_31,
 	OPENLOOPPART,
 	CLOSEDLOOPPART,
 	};
