07efe271a447aafd86b9e471171245af67cd7aef
rusefillc <sdfsdfqsf2334234234>
2021-11-24 13:30:50 -0500

    explicit unit tests for config tokens in real source code

--- java_console/ui/src/main/java/com/rusefi/ui/livedata/SourceCodePainter.java
+++ java_console/ui/src/main/java/com/rusefi/ui/livedata/SourceCodePainter.java
@@ -3,6 +3,16 @@ package com.rusefi.ui.livedata;
 import java.awt.*;
 
 public interface SourceCodePainter {
+    SourceCodePainter VOID = new SourceCodePainter() {
+        @Override
+        public void paintBackground(Color color, Range range) {
+        }
+
+        @Override
+        public void paintForeground(Color color, Range range) {
+        }
+    };
+
     void paintBackground(Color color, Range range);
 
     void paintForeground(Color color, Range range);
