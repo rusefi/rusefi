fbdcca54a169ff90a58e6126173e55ccc502bede
GitHub build-firmware Action <action@github.com>
2021-11-24 19:48:08 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_proteus_f4.h
+++ firmware/controllers/generated/signature_proteus_f4.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 914793901
-#define TS_SIGNATURE "rusEFI 2021.11.24.proteus_f4.914793901"
+#define SIGNATURE_HASH 1056583892
+#define TS_SIGNATURE "rusEFI 2021.11.24.proteus_f4.1056583892"
