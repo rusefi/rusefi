ca5ba2f8e88430a65cc8e956e802b44a733b1228
GitHub build-firmware Action <action@github.com>
2021-11-26 02:06:11 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_all.h
+++ firmware/controllers/generated/signature_all.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 2682339545
-#define TS_SIGNATURE "rusEFI 2021.11.26.all.2682339545"
+#define SIGNATURE_HASH 2609070538
+#define TS_SIGNATURE "rusEFI 2021.11.26.all.2609070538"
--- firmware/controllers/generated/signature_hellen-nb1.h
+++ firmware/controllers/generated/signature_hellen-nb1.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 976449616
-#define TS_SIGNATURE "rusEFI 2021.11.26.hellen-nb1.976449616"
+#define SIGNATURE_HASH 1045524803
+#define TS_SIGNATURE "rusEFI 2021.11.26.hellen-nb1.1045524803"
