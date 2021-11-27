bdd77f971be7c7638fc2d0827b14a3589f56039b
GitHub build-firmware Action <action@github.com>
2021-11-27 23:40:21 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen72.h
+++ firmware/controllers/generated/signature_hellen72.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 2885768592
-#define TS_SIGNATURE "rusEFI 2021.11.27.hellen72.2885768592"
+#define SIGNATURE_HASH 4065244341
+#define TS_SIGNATURE "rusEFI 2021.11.27.hellen72.4065244341"
--- firmware/controllers/generated/signature_prometheus_405.h
+++ firmware/controllers/generated/signature_prometheus_405.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 139856838
-#define TS_SIGNATURE "rusEFI 2021.11.27.prometheus_405.139856838"
+#define SIGNATURE_HASH 1444539107
+#define TS_SIGNATURE "rusEFI 2021.11.27.prometheus_405.1444539107"
