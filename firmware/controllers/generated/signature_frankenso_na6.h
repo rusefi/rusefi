1edbb8d29e56622f54ec72dc8dc27b7c070b65d9
GitHub build-firmware Action <action@github.com>
2021-11-27 16:00:49 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_frankenso_na6.h
+++ firmware/controllers/generated/signature_frankenso_na6.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 1785094030
-#define TS_SIGNATURE "rusEFI 2021.11.27.frankenso_na6.1785094030"
+#define SIGNATURE_HASH 1952215867
+#define TS_SIGNATURE "rusEFI 2021.11.27.frankenso_na6.1952215867"
--- firmware/controllers/generated/signature_hellenNA6.h
+++ firmware/controllers/generated/signature_hellenNA6.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 1718176975
-#define TS_SIGNATURE "rusEFI 2021.11.27.hellenNA6.1718176975"
+#define SIGNATURE_HASH 2018730106
+#define TS_SIGNATURE "rusEFI 2021.11.27.hellenNA6.2018730106"
