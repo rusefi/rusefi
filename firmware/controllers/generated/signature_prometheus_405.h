3a68ddf9047e2dccba4597dd53fec93b5e8cda1c
GitHub build-firmware Action <action@github.com>
2021-11-27 18:42:03 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_prometheus_405.h
+++ firmware/controllers/generated/signature_prometheus_405.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 2188602719
-#define TS_SIGNATURE "rusEFI 2021.11.27.prometheus_405.2188602719"
+#define SIGNATURE_HASH 500245376
+#define TS_SIGNATURE "rusEFI 2021.11.27.prometheus_405.500245376"
--- firmware/controllers/generated/signature_subaru_eg33_f7.h
+++ firmware/controllers/generated/signature_subaru_eg33_f7.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on config/boards/subaru_eg33/config/gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 287548137
-#define TS_SIGNATURE "rusEFI 2021.11.27.subaru_eg33_f7.287548137"
+#define SIGNATURE_HASH 2390879286
+#define TS_SIGNATURE "rusEFI 2021.11.27.subaru_eg33_f7.2390879286"
