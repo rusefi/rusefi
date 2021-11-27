bcf4a8c67c688ad81c0005207582a67a439cc9ff
GitHub build-firmware Action <action@github.com>
2021-11-27 17:44:20 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_kin.h
+++ firmware/controllers/generated/signature_kin.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on kinetis_gen_config.bat integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 3409841554
-#define TS_SIGNATURE "rusEFI 2021.11.27.kin.3409841554"
+#define SIGNATURE_HASH 3864088298
+#define TS_SIGNATURE "rusEFI 2021.11.27.kin.3864088298"
