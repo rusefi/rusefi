8674a8b830ac08685d19e83642c67749a7d7d167
rusefillc <sdfsdfqsf2334234234>
2021-11-28 13:57:56 -0500

    automation around outputs section #197
    datalog toolset

--- java_tools/configuration_definition/src/main/java/com/rusefi/output/BaseCHeaderConsumer.java
+++ java_tools/configuration_definition/src/main/java/com/rusefi/output/BaseCHeaderConsumer.java
@@ -8,7 +8,7 @@ public abstract class BaseCHeaderConsumer implements ConfigurationConsumer {
     private static final String BOOLEAN_TYPE = "bool";
     private final StringBuilder content = new StringBuilder();
 
-    public static String getHeaderText(ConfigField configField, int currentOffset, int bitIndex) {
+    private static String getHeaderText(ConfigField configField, int currentOffset, int bitIndex) {
         if (configField.isBit()) {
             String comment = "\t/**" + EOL + ConfigDefinition.packComment(configField.getCommentContent(), "\t") + "\toffset " + currentOffset + " bit " + bitIndex + " */" + EOL;
             return comment + "\t" + BOOLEAN_TYPE + " " + configField.getName() + " : 1 {};" + EOL;
