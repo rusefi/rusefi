b8660db35efce882fc250c6398fcefc433b6c917
Matthew Kennedy <matthewkennedy@outlook.com>
2021-11-27 21:40:12 -0800

    make fan control live-data-friendly (#3626)
    * make fan live-data-friendly
    
    * script
    
    * generated for happy simulator

--- firmware/controllers/actuators/fan_control.h
+++ firmware/controllers/actuators/fan_control.h
@@ -1,4 +1,19 @@
 #pragma once
 
+#include "fan_control_generated.h"
+
+struct FanController : public fan_control_s {
+	void update(bool acActive);
+
+private:
+	bool getState(bool acActive, bool lastState);
+
+protected:
+	virtual OutputPin& getPin() = 0;
+	virtual float getFanOnTemp() = 0;
+	virtual float getFanOffTemp() = 0;
+	virtual bool enableWithAc() = 0;
+	virtual bool disableWhenStopped() = 0;
+};
 
 void updateFans(bool acActive);
