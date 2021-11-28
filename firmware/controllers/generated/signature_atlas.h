1e58ab65d193af8bc0ea2693909e28520faa1e2b
GitHub build-firmware Action <action@github.com>
2021-11-28 22:26:50 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_atlas.h
+++ firmware/controllers/generated/signature_atlas.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 2679144889
-#define TS_SIGNATURE "rusEFI 2021.11.28.atlas.2679144889"
+#define SIGNATURE_HASH 3358366148
+#define TS_SIGNATURE "rusEFI 2021.11.28.atlas.3358366148"
--- firmware/controllers/generated/signature_hellen121nissan.h
+++ firmware/controllers/generated/signature_hellen121nissan.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 956175935
-#define TS_SIGNATURE "rusEFI 2021.11.28.hellen121nissan.956175935"
+#define SIGNATURE_HASH 1868697154
+#define TS_SIGNATURE "rusEFI 2021.11.28.hellen121nissan.1868697154"
