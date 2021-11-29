8913746065574922703bd6cd94aadc0b68c5a4d3
GitHub build-firmware Action <action@github.com>
2021-11-29 16:10:44 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_atlas.h
+++ firmware/controllers/generated/signature_atlas.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 938241596
-#define TS_SIGNATURE "rusEFI 2021.11.29.atlas.938241596"
+#define SIGNATURE_HASH 67127238
+#define TS_SIGNATURE "rusEFI 2021.11.29.atlas.67127238"
--- firmware/controllers/generated/signature_hellen128.h
+++ firmware/controllers/generated/signature_hellen128.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 2984308924
-#define TS_SIGNATURE "rusEFI 2021.11.29.hellen128.2984308924"
+#define SIGNATURE_HASH 2181880134
+#define TS_SIGNATURE "rusEFI 2021.11.29.hellen128.2181880134"
