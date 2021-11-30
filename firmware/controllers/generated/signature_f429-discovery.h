d08be922ea928e8a763145a45fe9109947dd406f
GitHub build-firmware Action <action@github.com>
2021-11-30 21:40:56 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_f429-discovery.h
+++ firmware/controllers/generated/signature_f429-discovery.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 415937249
-#define TS_SIGNATURE "rusEFI 2021.11.29.f429-discovery.415937249"
+#define TS_SIGNATURE "rusEFI 2021.11.30.f429-discovery.415937249"
--- firmware/controllers/generated/signature_frankenso_na6.h
+++ firmware/controllers/generated/signature_frankenso_na6.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 1660479240
-#define TS_SIGNATURE "rusEFI 2021.11.29.frankenso_na6.1660479240"
+#define TS_SIGNATURE "rusEFI 2021.11.30.frankenso_na6.1660479240"
