22962809f88da4ec6dff4262edbd44e62f268da3
rusefillc <sdfsdfqsf2334234234>
2021-12-01 00:07:04 -0500

    proteus to have enlarged Lua

--- firmware/controllers/engine_controller.cpp
+++ firmware/controllers/engine_controller.cpp
@@ -741,7 +741,7 @@ void initEngineContoller() {
  * UNUSED_SIZE constants.
  */
 #ifndef RAM_UNUSED_SIZE
-#define RAM_UNUSED_SIZE 23000
+#define RAM_UNUSED_SIZE 21000
 #endif
 #ifndef CCM_UNUSED_SIZE
 #define CCM_UNUSED_SIZE 16
