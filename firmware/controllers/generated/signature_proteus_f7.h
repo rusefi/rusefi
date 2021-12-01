f767500ef1c2692ec408c5d957f7c4b89fe8d2d2
GitHub build-firmware Action <action@github.com>
2021-12-01 04:23:33 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_proteus_f7.h
+++ firmware/controllers/generated/signature_proteus_f7.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 2803138081
-#define TS_SIGNATURE "rusEFI 2021.12.01.proteus_f7.2803138081"
+#define SIGNATURE_HASH 4173296285
+#define TS_SIGNATURE "rusEFI 2021.12.01.proteus_f7.4173296285"
