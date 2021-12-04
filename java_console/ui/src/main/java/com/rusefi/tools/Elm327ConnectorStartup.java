bdbf74789899bdbeef9071bf4a2e5a79a56fb6c9
rusefillc <sdfsdfqsf2334234234>
2021-12-04 14:02:54 -0500

    ELM327 bugfix: port was opened twice

--- java_console/ui/src/main/java/com/rusefi/tools/Elm327ConnectorStartup.java
+++ java_console/ui/src/main/java/com/rusefi/tools/Elm327ConnectorStartup.java
@@ -2,14 +2,16 @@ package com.rusefi.tools;
 
 import com.devexperts.logging.Logging;
 import com.rusefi.autodetect.PortDetector;
+import com.rusefi.autodetect.SerialAutoChecker;
 import com.rusefi.io.can.Elm327Connector;
 
 public class Elm327ConnectorStartup {
     private final static Logging log = Logging.getLogging(Elm327ConnectorStartup.class);
 
     public static void start() {
-        //String autoDetectedPort = PortDetector.autoDetectSerial(null, PortDetector.DetectorMode.DETECT_ELM327);
-        String autoDetectedPort = "COM73";
+        SerialAutoChecker.AutoDetectResult detectResult = PortDetector.autoDetectSerial(null, PortDetector.DetectorMode.DETECT_ELM327);
+        String autoDetectedPort = detectResult.getSerialPort();
+        //String autoDetectedPort = "COM73";
         if (autoDetectedPort == null) {
             System.err.println(ConsoleTools.RUS_EFI_NOT_DETECTED);
             return;
