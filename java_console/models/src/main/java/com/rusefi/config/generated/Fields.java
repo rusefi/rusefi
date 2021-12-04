2afae264c601e06c3e5cfe7b0f80cd0bc8cc2f66
GitHub build-firmware Action <action@github.com>
2021-12-04 02:51:21 +0000

    Auto-generated configs and docs

--- java_console/models/src/main/java/com/rusefi/config/generated/Fields.java
+++ java_console/models/src/main/java/com/rusefi/config/generated/Fields.java
@@ -1,6 +1,6 @@
 package com.rusefi.config.generated;
 
-// this file was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt Sat Dec 04 02:13:12 UTC 2021
+// this file was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt Sat Dec 04 02:49:51 UTC 2021
 
 // by class com.rusefi.output.FileJavaFieldsConsumer
 import com.rusefi.config.*;
