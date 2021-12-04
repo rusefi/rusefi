c4d8c1b72e5119d13e858ddaa9e84665936e762a
GitHub build-firmware Action <action@github.com>
2021-12-04 23:06:32 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellenNA6.h
+++ firmware/controllers/generated/signature_hellenNA6.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 2045900493
-#define TS_SIGNATURE "rusEFI 2021.12.04.hellenNA6.2045900493"
+#define SIGNATURE_HASH 2637770989
+#define TS_SIGNATURE "rusEFI 2021.12.04.hellenNA6.2637770989"
--- firmware/controllers/generated/signature_subaru_eg33_f7.h
+++ firmware/controllers/generated/signature_subaru_eg33_f7.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on config/boards/subaru_eg33/config/gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 3430327861
-#define TS_SIGNATURE "rusEFI 2021.12.04.subaru_eg33_f7.3430327861"
+#define SIGNATURE_HASH 683565077
+#define TS_SIGNATURE "rusEFI 2021.12.04.subaru_eg33_f7.683565077"
