b9b169f4fcf7b13a2f2ab31601aa52b9c1d1a397
GitHub build-firmware Action <action@github.com>
2021-12-07 03:13:32 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen154hyundai.h
+++ firmware/controllers/generated/signature_hellen154hyundai.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 627913955
-#define TS_SIGNATURE "rusEFI 2021.12.07.hellen154hyundai.627913955"
+#define SIGNATURE_HASH 662888674
+#define TS_SIGNATURE "rusEFI 2021.12.07.hellen154hyundai.662888674"
--- firmware/controllers/generated/signature_proteus_f7.h
+++ firmware/controllers/generated/signature_proteus_f7.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 3146305002
-#define TS_SIGNATURE "rusEFI 2021.12.07.proteus_f7.3146305002"
+#define SIGNATURE_HASH 3110541803
+#define TS_SIGNATURE "rusEFI 2021.12.07.proteus_f7.3110541803"
