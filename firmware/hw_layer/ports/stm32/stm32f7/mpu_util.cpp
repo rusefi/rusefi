da83b0a12affdb844700ff0ab91af11ec80cea00
Matthew Kennedy <matthewkennedy@outlook.com>
2021-12-06 21:25:30 -0800

    f7

--- firmware/hw_layer/ports/stm32/stm32f7/mpu_util.cpp
+++ firmware/hw_layer/ports/stm32/stm32f7/mpu_util.cpp
@@ -176,15 +176,20 @@ void sys_dual_bank(void) {
 
 
 void stm32_stop() {
+	SysTick->CTRL = 0;
 	__disable_irq();
+	RCC->AHB1RSTR = RCC_AHB1RSTR_GPIOERST;
 
 	// configure mode bits
 	PWR->CR1 &= ~PWR_CR1_PDDS;	// cleared PDDS means stop mode (not standby) 
+	PWR->CR1 |= PWR_CR1_FPDS;	// turn off flash in stop mode
+	PWR->CR1 |= PWR_CR1_UDEN;	// regulator underdrive in stop mode
+	PWR->CR1 |= PWR_CR1_LPUDS;	// low power regulator in under drive mode
+	PWR->CR1 |= PWR_CR1_LPDS;	// regulator in low power mode
 
 	// enable Deepsleep mode
 	SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
 
-	// Wait for event - this will return when stop mode is done
 	__WFE();
 
 	// Lastly, reboot
@@ -192,7 +197,9 @@ void stm32_stop() {
 }
 
 void stm32_standby() {
+	SysTick->CTRL = 0;
 	__disable_irq();
+	RCC->AHB1RSTR = RCC_AHB1RSTR_GPIOERST;
 
 	// configure mode bits
 	PWR->CR1 |= PWR_CR1_PDDS;		// PDDS = use standby mode (not stop mode)
