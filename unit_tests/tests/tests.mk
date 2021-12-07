6e49d6638553d5d3055eb55f2e315e2141191642
Andrey <rusefillc@gmail.com>
2021-12-07 16:58:10 -0500

    MAP phase sensing #3544
    look new unit test!

--- unit_tests/tests/tests.mk
+++ unit_tests/tests/tests.mk
@@ -19,6 +19,7 @@ TESTS_SRC_CPP = \
 	tests/trigger/test_nissan_vq_vvt.cpp \
 	tests/trigger/test_override_gaps.cpp \
 	tests/trigger/test_injection_scheduling.cpp \
+	tests/trigger/test_instant_map.cpp \
 	tests/ignition_injection/injection_mode_transition.cpp \
 	tests/ignition_injection/test_startOfCrankingPrimingPulse.cpp \
 	tests/ignition_injection/test_multispark.cpp \
