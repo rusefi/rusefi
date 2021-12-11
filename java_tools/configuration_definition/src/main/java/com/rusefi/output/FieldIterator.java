2656f4b7b7f61cb4022f155ded96722ded80c573
rusefillc <sdfsdfqsf2334234234>
2021-12-11 04:31:54 -0500

    generator refactoring

--- java_tools/configuration_definition/src/main/java/com/rusefi/output/FieldIterator.java
+++ java_tools/configuration_definition/src/main/java/com/rusefi/output/FieldIterator.java
@@ -9,14 +9,14 @@ import java.util.List;
  * custom iterator with references to previous and next elements
  */
 public class FieldIterator {
-    private final List<ConfigField> tsFields;
+    private final List<ConfigField> fields;
     BitState bitState = new BitState();
     private ConfigField prev = ConfigField.VOID;
     ConfigField next;
     ConfigField cf;
 
-    public FieldIterator(List<ConfigField> tsFields) {
-        this.tsFields = tsFields;
+    public FieldIterator(List<ConfigField> fields) {
+        this.fields = fields;
     }
 
     /**
@@ -28,11 +28,18 @@ public class FieldIterator {
 
     public void start(int index) {
         int nextIndex = index + 1;
-        while (nextIndex < tsFields.size() && tsFields.get(nextIndex).isDirective())
+        while (nextIndex < fields.size() && fields.get(nextIndex).isDirective())
             nextIndex++;
 
-        next = nextIndex >= tsFields.size() ? ConfigField.VOID : tsFields.get(nextIndex);
-        cf = tsFields.get(index);
+        next = nextIndex >= fields.size() ? ConfigField.VOID : fields.get(nextIndex);
+        cf = fields.get(index);
+    }
+
+    public void loop() {
+        for (int i = 0; i < fields.size(); i++) {
+            start(i);
+            end();
+        }
     }
 
     public void end() {
