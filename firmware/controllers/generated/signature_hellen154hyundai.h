2788284170066182428416c722f67681d6aba437
GitHub build-firmware Action <action@github.com>
2021-12-17 03:14:27 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen154hyundai.h
+++ firmware/controllers/generated/signature_hellen154hyundai.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 2633643800
-#define TS_SIGNATURE "rusEFI 2021.12.17.hellen154hyundai.2633643800"
+#define SIGNATURE_HASH 2665798621
+#define TS_SIGNATURE "rusEFI 2021.12.17.hellen154hyundai.2665798621"
--- firmware/controllers/generated/signature_subaru_eg33_f7.h
+++ firmware/controllers/generated/signature_subaru_eg33_f7.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on config/boards/subaru_eg33/config/gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 2675012811
-#define TS_SIGNATURE "rusEFI 2021.12.17.subaru_eg33_f7.2675012811"
+#define SIGNATURE_HASH 2641336334
+#define TS_SIGNATURE "rusEFI 2021.12.17.subaru_eg33_f7.2641336334"
