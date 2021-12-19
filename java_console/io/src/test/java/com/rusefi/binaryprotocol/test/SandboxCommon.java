4b9157d8046aedfea1526422117a8f453d8a6e1d
rusefillc <sdfsdfqsf2334234234>
2021-12-18 20:21:30 -0500

    logging improvement

--- java_console/io/src/test/java/com/rusefi/binaryprotocol/test/SandboxCommon.java
+++ java_console/io/src/test/java/com/rusefi/binaryprotocol/test/SandboxCommon.java
@@ -1,5 +1,6 @@
 package com.rusefi.binaryprotocol.test;
 
+import com.devexperts.logging.Logging;
 import com.opensr5.ConfigurationImage;
 import com.rusefi.binaryprotocol.BinaryProtocol;
 import com.rusefi.binaryprotocol.BinaryProtocolState;
@@ -16,7 +17,11 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
+import static com.devexperts.logging.Logging.getLogging;
+
 public class SandboxCommon {
+    private static final Logging log = getLogging(SandboxCommon.class);
+
     static ConfigurationImage readImage(IoStream tsStream, LinkManager linkManager) throws InterruptedException {
         AtomicReference<ConfigurationImage> configurationImageAtomicReference = new AtomicReference<>();
         CountDownLatch imageLatch = new CountDownLatch(1);
@@ -26,11 +31,11 @@ public class SandboxCommon {
         streamConnector.connectAndReadConfiguration(new BinaryProtocol.Arguments(false), new ConnectionStateListener() {
             @Override
             public void onConnectionEstablished() {
-                System.out.println("onConnectionEstablished");
+                log.info("onConnectionEstablished");
 
                 BinaryProtocol currentStreamState = linkManager.getCurrentStreamState();
                 if (currentStreamState == null) {
-                    System.out.println("No BinaryProtocol");
+                    log.info("No BinaryProtocol");
                 } else {
                     BinaryProtocolState binaryProtocolState = currentStreamState.getBinaryProtocolState();
                     ConfigurationImage ci = binaryProtocolState.getControllerConfiguration();
@@ -41,13 +46,13 @@ public class SandboxCommon {
 
             @Override
             public void onConnectionFailed() {
-                System.out.println("onConnectionFailed");
+                log.info("onConnectionFailed");
             }
         });
 
         imageLatch.await(1, TimeUnit.MINUTES);
         ConfigurationImage ci = configurationImageAtomicReference.get();
-        System.out.println("Got ConfigurationImage " + ci + ", " + ci.getSize());
+        log.info("Got ConfigurationImage " + ci + ", " + ci.getSize());
         return ci;
     }
 
@@ -55,7 +60,7 @@ public class SandboxCommon {
         BinaryProtocol bp = new BinaryProtocol(linkManager, tsStream);
         linkManager.COMMUNICATION_EXECUTOR.submit(() -> {
             if (tsStream.getDataBuffer().dropPending() != 0)
-                System.out.println("ERROR Extra data before CRC");
+                log.info("ERROR Extra data before CRC");
             bp.getCrcFromController(Fields.TOTAL_CONFIG_SIZE);
 //            bp.getCrcFromController(Fields.TOTAL_CONFIG_SIZE);
 //            bp.getCrcFromController(Fields.TOTAL_CONFIG_SIZE);
@@ -66,7 +71,7 @@ public class SandboxCommon {
 
     static void verifySignature(IoStream tsStream, String prefix, String suffix) throws IOException {
         String signature = BinaryProtocol.getSignature(tsStream);
-        System.out.println(prefix + "Got " + signature + " signature via " + suffix);
+        log.info(prefix + "Got " + signature + " signature via " + suffix);
         if (signature == null || !signature.startsWith(Fields.PROTOCOL_SIGNATURE_PREFIX))
             throw new IllegalStateException("Unexpected S " + signature);
     }
@@ -78,7 +83,7 @@ public class SandboxCommon {
         byte[] fResponse = new byte[3];
         dataBuffer.waitForBytes("hello", System.currentTimeMillis(), fResponse.length);
         dataBuffer.getData(fResponse);
-        System.out.println(prefix + " Got F response " + IoStream.printByteArray(fResponse));
+        log.info(prefix + " Got F response " + IoStream.printByteArray(fResponse));
         if (fResponse[0] != '0' || fResponse[1] != '0' || fResponse[2] != '1')
             throw new IllegalStateException("Unexpected TS_COMMAND_F response " + Arrays.toString(fResponse));
     }
