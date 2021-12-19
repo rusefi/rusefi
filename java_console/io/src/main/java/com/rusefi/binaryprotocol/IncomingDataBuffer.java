5da5c2d15f5b3c753d7f727a87ab66916faebc40
rusefi <rusefillc@gmail.com>
2021-12-18 19:33:50 -0500

    better logging

--- java_console/io/src/main/java/com/rusefi/binaryprotocol/IncomingDataBuffer.java
+++ java_console/io/src/main/java/com/rusefi/binaryprotocol/IncomingDataBuffer.java
@@ -131,7 +131,7 @@ public class IncomingDataBuffer {
             while (cbb.length() < count) {
                 int timeout = (int) (startTimestamp + timeoutMs - System.currentTimeMillis());
                 if (timeout <= 0) {
-                    log.info(loggingMessage + ": timeout. Got only " + cbb.length() + " while expecting " + count);
+                    log.info(loggingMessage + ": timeout " + timeoutMs + "ms. Got only " + cbb.length() + " while expecting " + count);
                     return true; // timeout. Sad face.
                 }
                 try {
