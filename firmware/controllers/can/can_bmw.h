b9fd67881641f32c67acf1bcb7e86b5aa1a8c316
rusefillc <sdfsdfqsf2334234234>
2021-12-19 22:08:35 -0500

    BMW CAN

--- firmware/controllers/can/can_bmw.h
+++ firmware/controllers/can/can_bmw.h
@@ -29,9 +29,12 @@
 // 10ms period
 #define CAN_BMW_E90_RPM_THROTTLE      0x0AA
 
-// ??? 0x0B5
+// MECH
+#define CAN_BMW_E90_TORQUE_DEMAND2    0x0B5
 // 20ms period
 #define CAN_BMW_E90_TORQUE_DEMAND     0x0B6
+// MECH
+#define CAN_BMW_E90_TRANSMISSION_DATA 0x0BA
 // Requirement wheel torque 0x0BF
 
 #define CAN_BMW_E90_STEERING_WHEEL    0x0C4
@@ -39,15 +42,25 @@
 // Terminal Status, 100ms period
 #define CAN_BMW_E90_IGNITION_KEY      0x130
 
+
+#define CAN_BMW_E65_GEAR_SELECTOR     0x192
 #define CAN_BMW_E90_BRAKING           0x19E
+// MECH Getriebedaten 2
+#define CAN_BMW_E90_GEARBOX_DATA_2    0x1A2
+
 // Engine data
 #define CAN_BMW_E90_COOLANT           0x1D0
+// MECH Anzeige Getriebedaten
+#define CAN_BMW_E90_TRANSMISSION_DISP 0x1D2
 
 // SEAT_BELT 2FA
 // DOOR_STATUS 2FC
+
+// MECH Status Gang 0x304
 
 // EXTERNAL_TEMP_TIME 0x310
 
 // MILEAGE_RANGE 0x310
 //#define CAN_BMW_E90_HAND_BRAKE        0x34F
-// WHEEL_TOLERANCE 374
+// WHEEL_TOLERANCE 0x374
+// MECH Getriebedaten 3 0x3B1
