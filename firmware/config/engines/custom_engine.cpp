47cde8801c63a75ee0370d644c57ce9e756f9617
rusefillc <sdfsdfqsf2334234234>
2021-12-20 02:07:03 -0500

    2 byte table axis #3045

--- firmware/config/engines/custom_engine.cpp
+++ firmware/config/engines/custom_engine.cpp
@@ -793,8 +793,8 @@ void setHellen72etb() {
 
 void setHellenDefaultVrThresholds() {
 	for (int i = 0;i<VR_THRESHOLD_COUNT;i++) {
-		setLinearCurve(engineConfiguration->vrThreshold[i].rpmBins, 600 / RPM_1_BYTE_PACKING_MULT, 7000 / RPM_1_BYTE_PACKING_MULT, 100 / RPM_1_BYTE_PACKING_MULT);
-		setLinearCurve(engineConfiguration->vrThreshold[i].values, PACK_PERCENT_BYTE_MULT * 0.6, PACK_PERCENT_BYTE_MULT * 1.2, PACK_PERCENT_BYTE_MULT * 0.1);
+		setLinearCurve(engineConfiguration->vrThreshold[i].rpmBins, 600, 7000, 100);
+		setLinearCurve(engineConfiguration->vrThreshold[i].values, 0.6, 1.2, 0.1);
 	}
 }
 
