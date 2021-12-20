4ebb17c57718c522c3b9348539ed61489f709657
Matthew Kennedy <matthewkennedy@outlook.com>
2021-12-20 02:34:42 -0800

    why are these defaults insane

--- firmware/config/engines/ford_aspire.cpp
+++ firmware/config/engines/ford_aspire.cpp
@@ -38,9 +38,6 @@ static const int8_t default_aspire_timing_table[16][16] = {
 #endif
 
 static void setDefaultAspireMaps() {
-	setTimingLoadBin(1.2, 4.4);
-	setTimingRpmBin(800, 7000);
-
 #if IGN_LOAD_COUNT == DEFAULT_IGN_LOAD_COUNT
 	copyTable(config->ignitionTable, default_aspire_timing_table);
 #endif
