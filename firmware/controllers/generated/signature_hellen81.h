b9ea7e3bb54140a16812990b4676a4e293e90cfc
GitHub build-firmware Action <action@github.com>
2021-12-20 03:41:09 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen81.h
+++ firmware/controllers/generated/signature_hellen81.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 69003082
-#define TS_SIGNATURE "rusEFI 2021.12.20.hellen81.69003082"
+#define SIGNATURE_HASH 4042664782
+#define TS_SIGNATURE "rusEFI 2021.12.20.hellen81.4042664782"
--- firmware/controllers/generated/signature_hellen88bmw.h
+++ firmware/controllers/generated/signature_hellen88bmw.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 3849088086
-#define TS_SIGNATURE "rusEFI 2021.12.20.hellen88bmw.3849088086"
+#define SIGNATURE_HASH 294035538
+#define TS_SIGNATURE "rusEFI 2021.12.20.hellen88bmw.294035538"
