81e9513f4bc1b610c5fbf9ede5b944e2044f5609
GitHub build-firmware Action <action@github.com>
2021-12-20 07:21:57 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_atlas.h
+++ firmware/controllers/generated/signature_atlas.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 4070236290
-#define TS_SIGNATURE "rusEFI 2021.12.20.atlas.4070236290"
+#define SIGNATURE_HASH 563421671
+#define TS_SIGNATURE "rusEFI 2021.12.20.atlas.563421671"
