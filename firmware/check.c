52d11962aa97ba32eeb585636b607a3d931e31b4
Andrey Gusakov <dron0gus@gmail.com>
2021-12-20 22:42:10 +0300

    Do compiler check first

---
+++ firmware/check.c
@@ -0,0 +1,24 @@
+// This file asserts that the compiler is appropriate for rusEFI use.
+
+// non-MCU builds are significantly more tolerant
+#if EFI_PROD_CODE
+
+#define GCC_VERSION ((__GNUC__ * 100) + (__GNUC_MINOR__ * 10) + ( __GNUC_PATCHLEVEL__ ))
+
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+
+// Firmware builds require at least GCC 9.3.1
+#if (GCC_VERSION < 931)
+	#pragma message("GCC is " STR(__GNUC__)"."STR(__GNUC_MINOR__)"."STR(__GNUC_PATCHLEVEL__))
+	#error "GCC compiler >= 9.3.1 required"
+#endif
+
+#if (GCC_VERSION > 1000)
+	#pragma message("GCC is " STR(__GNUC__)"."STR(__GNUC_MINOR__)"."STR(__GNUC_PATCHLEVEL__))
+	#error "GCC10 not supported see https://github.com/rusefi/rusefi/labels/Gcc10"
+#endif
+
+
+
+#endif
