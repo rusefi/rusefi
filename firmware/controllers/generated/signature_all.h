949d3ea7062660b0141e2e427647edc9dabc7cf6
GitHub build-firmware Action <action@github.com>
2021-12-22 13:15:15 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_all.h
+++ firmware/controllers/generated/signature_all.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 241264427
-#define TS_SIGNATURE "rusEFI 2021.12.22.all.241264427"
+#define SIGNATURE_HASH 111802612
+#define TS_SIGNATURE "rusEFI 2021.12.22.all.111802612"
--- firmware/controllers/generated/signature_prometheus_469.h
+++ firmware/controllers/generated/signature_prometheus_469.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh integration/rusefi_config.txt
 //
 
-#define SIGNATURE_HASH 1586216653
-#define TS_SIGNATURE "rusEFI 2021.12.22.prometheus_469.1586216653"
+#define SIGNATURE_HASH 1447239954
+#define TS_SIGNATURE "rusEFI 2021.12.22.prometheus_469.1447239954"
