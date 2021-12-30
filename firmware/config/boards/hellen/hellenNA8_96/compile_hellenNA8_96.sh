35b8eb70d5dd06dd5759ef218ae7cf3ae89c3e3a
rusefillc <sdfsdfqsf2334234234>
2021-12-30 00:42:52 -0500

    HellenNA8_96 config folder #3724

--- firmware/config/boards/hellen/hellenNA8_96/compile_hellenNA8_96.sh
+++ firmware/config/boards/hellen/hellenNA8_96/compile_hellenNA8_96.sh
@@ -2,7 +2,7 @@
 
 export PROJECT_BOARD=hellen/hellen-nb1
 export PROJECT_CPU=ARCH_STM32F4
-export EXTRA_PARAMS="-DSHORT_BOARD_NAME=hellen-na8-96"
+export EXTRA_PARAMS="-DSHORT_BOARD_NAME=hellenNA8_96"
 
 cd ..
 bash ../common_make.sh
