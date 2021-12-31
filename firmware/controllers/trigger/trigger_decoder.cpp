b3de217c1bc2cc4803e7da47bdda49e423311e0a
Matthew Kennedy <matthewkennedy@outlook.com>
2021-12-31 00:21:21 -0600

    adjustment to #3722 (#3723)
    * s
    
    * behold, another bug a test caught
    
    * test
    
    * test generates usable trigger pattern so it works correctly
    
    * comment
    
    * don't need that

--- firmware/controllers/trigger/trigger_decoder.cpp
+++ firmware/controllers/trigger/trigger_decoder.cpp
@@ -80,6 +80,8 @@ void TriggerState::resetTriggerState() {
 
 	totalEventCountBase = 0;
 	isFirstEvent = true;
+
+	m_hasSynchronizedSymmetrical = false;
 }
 
 void TriggerState::setTriggerErrorState() {
@@ -404,6 +406,10 @@ angle_t TriggerState::syncSymmetricalCrank(int divider, int remainder, angle_t e
 		incrementTotalEventCounter();
 		totalShift += engineCycle / divider;
 	}
+
+	// Allow injection/ignition to happen, we've now fully sync'd the crank based on new cam information
+	m_hasSynchronizedSymmetrical = true;
+
 	return totalShift;
 }
 
