e03d46e006e9c5a2892dc36a9f4cdf9faea1596f
Matthew Kennedy <matthewkennedy@outlook.com>
2022-01-04 11:34:55 -0800

    comment

--- firmware/controllers/can/can_listener.h
+++ firmware/controllers/can/can_listener.h
@@ -39,6 +39,8 @@ public:
 protected:
 	virtual void decodeFrame(const CANRxFrame& frame, efitick_t nowNt) = 0;
 
+	// Return true if the provided frame should be accepted for processing by the listener.
+	// Override if you need more complex logic than comparing to a single ID.
 	virtual bool acceptFrame(const CANRxFrame& frame) const {
 		return CAN_ID(frame) == m_id;
 	}
