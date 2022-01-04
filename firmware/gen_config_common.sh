9e0042cd21e915b3b2a17e873d3c5f36822cf64b
rusefillc <sdfsdfqsf2334234234>
2022-01-03 23:28:13 -0500

    dead code?

--- firmware/gen_config_common.sh
+++ firmware/gen_config_common.sh
@@ -1,7 +1,6 @@
 COMMON_GEN_CONFIG="-jar ../java_tools/ConfigDefinition.jar \
  -enumInputFile controllers/algo/engine_types.h \
  -enumInputFile controllers/algo/rusefi_enums.h \
- -cache_zip_file tunerstudio/generated/cache.zip \
  -readfile OUTPUTS_SECTION console/binary/generated/output_channels.ini \
  -readfile DATALOG_SECTION console/binary/generated/data_logs.ini \
  -ts_destination tunerstudio \
