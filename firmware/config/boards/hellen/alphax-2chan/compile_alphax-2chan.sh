2065299abcaec262d64077441ca2280aa8252d11
rusefillc <sdfsdfqsf2334234234>
2022-01-05 16:08:10 -0500

    AlphaX

--- firmware/config/boards/hellen/alphax-2chan/compile_alphax-2chan.sh
+++ firmware/config/boards/hellen/alphax-2chan/compile_alphax-2chan.sh
@@ -2,7 +2,7 @@
 
 export PROJECT_BOARD=hellen/alphax-2chan
 export PROJECT_CPU=ARCH_STM32F4
-export EXTRA_PARAMS="-DSHORT_BOARD_NAME=AlphaX-2chan"
+export EXTRA_PARAMS="-DSHORT_BOARD_NAME=alphax-2chan"
 
 cd ..
 bash ../common_make.sh
