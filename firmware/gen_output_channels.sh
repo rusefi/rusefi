dd5303362cef8dcc702b2cc8c8e9e482fcb8a680
rusefillc <sdfsdfqsf2334234234>
2022-01-05 00:32:14 -0500

    dead code

--- firmware/gen_output_channels.sh
+++ firmware/gen_output_channels.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-
-source gen_config_common.sh
-echo "Using COMMON_GEN_CONFIG [$COMMON_GEN_CONFIG]"
-
 java -DSystemOut.name=logs/gen_output_channels \
  -jar ../java_tools/ConfigDefinition.jar \
  -prepend integration/rusefi_config.txt \
