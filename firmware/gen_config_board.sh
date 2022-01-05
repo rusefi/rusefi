7f91b54aa32afe5cb7a994114c4c5b871cee7606
rusefillc <sdfsdfqsf2334234234>
2022-01-05 00:58:57 -0500

    this just makes no sense. it must be useless.

--- firmware/gen_config_board.sh
+++ firmware/gen_config_board.sh
@@ -24,11 +24,6 @@ else
 fi
 
 echo "BOARDNAME=${BOARDNAME} SHORT_BOARDNAME=${SHORT_BOARDNAME}"
-if [ "${SHORT_BOARDNAME}" = "all" ]; then
-  SOMETHINGSOMETHING=false
-else
-  SOMETHINGSOMETHING=true
-fi
 
 bash gen_signature.sh ${SHORT_BOARDNAME}
 
@@ -53,8 +48,8 @@ java -DSystemOut.name=logs/gen_config_board \
  -field_lookup_file controllers/lua/generated/value_lookup_generated.cpp \
 	-board ${BOARDNAME} \
 	-ts_output_name generated/${INI} \
-  -with_c_defines $SOMETHINGSOMETHING \
-  -initialize_to_zero $SOMETHINGSOMETHING \
+  -with_c_defines false \
+  -initialize_to_zero false \
 	-signature tunerstudio/generated/signature_${SHORT_BOARDNAME}.txt \
 	-signature_destination controllers/generated/signature_${SHORT_BOARDNAME}.h \
   -java_destination ../java_console/models/src/main/java/com/rusefi/config/generated/Fields.java \
