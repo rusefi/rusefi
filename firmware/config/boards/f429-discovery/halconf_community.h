ffef831163a30435065b88dda1ad185f66007db2
rusefillc <sdfsdfqsf2334234234>
2022-01-04 20:55:42 -0500

    stm32f429-discovery: mostly copy-paste from ChibiOS-Contrib

--- firmware/config/boards/f429-discovery/halconf_community.h
+++ firmware/config/boards/f429-discovery/halconf_community.h
@@ -1,112 +1,20 @@
-/*
-    ChibiOS - Copyright (C) 2014 Uladzimir Pylinsky aka barthess
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
-
-#ifndef HALCONF_COMMUNITY_H
-#define HALCONF_COMMUNITY_H
-
 /**
- * @brief   Enables the community overlay.
+ * @file boards/f429-discovery/halconf_community.h
+ *
+ * @brief In this header we can override defaults from halconf_community.h.
+ *
+ * @date Jan 08, 2022
+ * @author Andrey Gusakov, 2022
  */
-#if !defined(HAL_USE_COMMUNITY) || defined(__DOXYGEN__)
-#define HAL_USE_COMMUNITY           TRUE
-#endif
 
-/**
- * @brief   Enables the FSMC subsystem.
- */
-#if !defined(HAL_USE_FSMC) || defined(__DOXYGEN__)
+#ifndef HALCONF_COMMUNITY_F429_H
+#define HALCONF_COMMUNITY_F429_H
+
+/* Enable SDRAM support */
 #define HAL_USE_FSMC                TRUE
-#endif
-
-/**
- * @brief   Enables the SDRAM subsystem.
- */
-#if !defined(HAL_USE_SDRAM) || defined(__DOXYGEN__)
 #define HAL_USE_SDRAM               TRUE
-#endif
 
-/**
- * @brief   Enables the SRAM subsystem.
- */
-#if !defined(HAL_USE_SRAM) || defined(__DOXYGEN__)
-#define HAL_USE_SRAM                FALSE
-#endif
+/* all other options are default */
+#include "../../../hw_layer/ports/stm32/cfg/halconf_community.h"
 
-/**
- * @brief   Enables the NAND subsystem.
- */
-#if !defined(HAL_USE_NAND) || defined(__DOXYGEN__)
-#define HAL_USE_NAND                FALSE
-#endif
-
-/**
- * @brief   Enables the 1-wire subsystem.
- */
-#if !defined(HAL_USE_ONEWIRE) || defined(__DOXYGEN__)
-#define HAL_USE_ONEWIRE             FALSE
-#endif
-
-/**
- * @brief   Enables the EICU subsystem.
- */
-#if !defined(HAL_USE_EICU) || defined(__DOXYGEN__)
-#define HAL_USE_EICU                FALSE
-#endif
-
-/**
- * @brief   Enables the CRC subsystem.
- */
-#if !defined(HAL_USE_CRC) || defined(__DOXYGEN__)
-#define HAL_USE_CRC                 FALSE
-#endif
-
-/**
- * @brief   Enables the RNG subsystem.
- */
-#if !defined(HAL_USE_RNG) || defined(__DOXYGEN__)
-#define HAL_USE_RNG                 FALSE
-#endif
-
-/*===========================================================================*/
-/* FSMCNAND driver related settings.                                         */
-/*===========================================================================*/
-
-/**
- * @brief   Enables the @p nandAcquireBus() and @p nanReleaseBus() APIs.
- * @note    Disabling this option saves both code and data space.
- */
-#if !defined(NAND_USE_MUTUAL_EXCLUSION) || defined(__DOXYGEN__)
-#define NAND_USE_MUTUAL_EXCLUSION   TRUE
-#endif
-
-/*===========================================================================*/
-/* 1-wire driver related settings.                                           */
-/*===========================================================================*/
-/**
- * @brief   Enables strong pull up feature.
- * @note    Disabling this option saves both code and data space.
- */
-#define ONEWIRE_USE_STRONG_PULLUP   FALSE
-
-/**
- * @brief   Enables search ROM feature.
- * @note    Disabling this option saves both code and data space.
- */
-#define ONEWIRE_USE_SEARCH_ROM      TRUE
-
-#endif /* HALCONF_COMMUNITY_H */
-
-/** @} */
+#endif /* HALCONF_COMMUNITY_F429_H */
