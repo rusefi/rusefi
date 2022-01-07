7c0fd37dfe5c0dfd143d85a60d1bfc2581572a85
rusefillc <sdfsdfqsf2334234234>
2022-01-06 19:02:35 -0500

    Live Data progress

---
+++ firmware/common.mk
@@ -0,0 +1,6 @@
+include $(PROJECT_DIR)/init/init.mk
+include $(PROJECT_DIR)/util/util.mk
+include $(PROJECT_DIR)/hw_layer/hw_layer.mk
+include $(PROJECT_DIR)/hw_layer/sensors/sensors.mk
+
+ALLINC += $(PROJECT_DIR)/console/binary/generated
