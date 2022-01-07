d9175696a22b5936242d656b155626092fd5ce67
rusefillc <sdfsdfqsf2334234234>
2022-01-06 21:40:48 -0500

    mk files refactoring

--- firmware/config/engines/engines.mk
+++ firmware/config/engines/engines.mk
@@ -1,6 +1,4 @@
 
-ENGINES_SRC = 
-
 ENGINES_SRC_CPP = $(PROJECT_DIR)/config/engines/ford_aspire.cpp \
 	$(PROJECT_DIR)/config/engines/custom_engine.cpp \
 	$(PROJECT_DIR)/config/engines/bmw_m73.cpp \
