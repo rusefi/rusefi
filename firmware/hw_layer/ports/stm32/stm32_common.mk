f4730421eda31f7282e47e3a6fd6af0771d3b319
Andrey G <dron0gus@gmail.com>
2022-01-09 00:33:34 +0300

    mk files random cleanup (#3759)

--- firmware/hw_layer/ports/stm32/stm32_common.mk
+++ firmware/hw_layer/ports/stm32/stm32_common.mk
@@ -1,5 +1,5 @@
 HW_LAYER_EMS += $(PROJECT_DIR)/hw_layer/ports/stm32/serial_over_usb/usbcfg.c \
-				$(PROJECT_DIR)/hw_layer/ports/stm32/flash_int.c \
+				$(PROJECT_DIR)/hw_layer/ports/stm32/flash_int.c
 
 HW_LAYER_EMS_CPP += \
     $(PROJECT_DIR)/hw_layer/ports/stm32/serial_over_usb/usbconsole.cpp \
@@ -7,8 +7,10 @@ HW_LAYER_EMS_CPP += \
 	$(PROJECT_DIR)/hw_layer/ports/stm32/stm32_common.cpp \
 	$(PROJECT_DIR)/hw_layer/ports/stm32/backup_ram.cpp \
 	$(PROJECT_DIR)/hw_layer/ports/stm32/microsecond_timer_stm32.cpp \
-	$(PROJECT_DIR)/hw_layer/ports/stm32/osc_detector.cpp \
+	$(PROJECT_DIR)/hw_layer/ports/stm32/osc_detector.cpp
 
 RUSEFIASM = $(PROJECT_DIR)/hw_layer/ports/stm32/rusEfiStartup.S
 
-HW_INC += $(PROJECT_DIR)/hw_layer/ports/stm32 $(PROJECT_DIR)/hw_layer/ports/stm32/serial_over_usb
+HW_INC += \
+	$(PROJECT_DIR)/hw_layer/ports/stm32 \
+	$(PROJECT_DIR)/hw_layer/ports/stm32/serial_over_usb
