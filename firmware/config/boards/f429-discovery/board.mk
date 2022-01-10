fc32af43bbf1c3d46779f4e7d72b7c91ba316ead
Andrey G <dron0gus@gmail.com>
2022-01-11 00:14:39 +0300

    F429 discovery ram dev (#3761)
    * stm32f429-discovery: mostly copy-paste from ChibiOS-Contrib
    
    * STM32F429-discovery has external SDRAM
    
    * F429-Discovery: add simple memory test for SDRAM (disabled)
    
    * Revert "random piece of https://github.com/dron0gus/rusefi/tree/f429-discovery-ram-dev"
    
    This reverts commit 96258202721c179c0bb11bf953a81126ed3db26d.
    
    Co-authored-by: rusefillc <sdfsdfqsf2334234234>

--- firmware/config/boards/f429-discovery/board.mk
+++ firmware/config/boards/f429-discovery/board.mk
@@ -1,5 +1,24 @@
 BOARD_DIR = $(PROJECT_DIR)/config/boards/$(PROJECT_BOARD)
 
-BOARDCPPSRC = $(BOARDS_DIR)/f429-discovery/board_configuration.cpp
+HALCONFDIR = $(BOARD_DIR)
 
+# List of all the board related files.
+BOARDCPPSRC = $(BOARD_DIR)/board_configuration.cpp
+
+# Required include directories
 BOARDINC = $(BOARD_DIR)
+
+# STM32F429 has FSMC with SDRAM support
+IS_STM32F429 = yes
+EFI_HAS_EXT_SDRAM = yes
+
+# avoid any engine setup
+DDEFS += -DDEFAULT_ENGINE_TYPE=MINIMAL_PINS
+
+#LED
+DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=GPIOG_14
+
+DDEFS += -DSTM32_FSMC_USE_FSMC1=TRUE -DSTM32_SDRAM_USE_SDRAM2=TRUE
+
+# Shared variables
+ALLINC += $(BOARDINC)
