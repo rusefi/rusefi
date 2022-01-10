4e7d6e23646d640e635b888bc7d4390c30979670
rusefillc <sdfsdfqsf2334234234>
2022-01-09 22:44:22 -0500

    random piece of https://github.com/dron0gus/rusefi/tree/f429-discovery-ram-dev

--- firmware/config/stm32f4ems/efifeatures.h
+++ firmware/config/stm32f4ems/efifeatures.h
@@ -257,10 +257,14 @@
 
 #define EFI_CONSOLE_USB_DEVICE SDU1
 
-// F42x has more memory, so we can:
-//  - use compressed USB MSD image (requires 32k of memory)
-//  - use perf trace (requires ~16k of memory)
-#ifdef EFI_IS_F42x
+#if defined(EFI_HAS_EXT_SDRAM)
+    #define ENABLE_PERF_TRACE TRUE
+    #define LUA_USER_HEAP (1 * 1024 * 1024)
+    #define LUA_SYSTEM_HEAP (1 * 1024 * 1024)
+#elif defined(EFI_IS_F42x)
+    // F42x has more memory, so we can:
+    //  - use compressed USB MSD image (requires 32k of memory)
+    //  - use perf trace (requires ~16k of memory)
 	#define EFI_USE_COMPRESSED_INI_MSD
 	#define ENABLE_PERF_TRACE TRUE
 
