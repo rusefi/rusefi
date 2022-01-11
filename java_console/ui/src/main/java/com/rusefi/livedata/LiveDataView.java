cca765ebe6a36adcc6affe4c83b129a41188e075
rusefillc <sdfsdfqsf2334234234>
2022-01-10 21:56:27 -0500

    RE usability: live data for idle controller

--- java_console/ui/src/main/java/com/rusefi/livedata/LiveDataView.java
+++ java_console/ui/src/main/java/com/rusefi/livedata/LiveDataView.java
@@ -10,23 +10,23 @@ import com.rusefi.ldmp.StateDictionary;
  */
 public enum LiveDataView {
     // todo: code generate this part of the enum with some BEFORE/AFTER tag?
-    AC_CONTROL(live_data_e.LDS_ac_control, AcControl.VALUES, "ac_control.cpp"),
-    FUEL_PUMP(live_data_e.LDS_fuel_pump, FuelPump.VALUES, "fuel_pump.cpp"),
-    MAIN_RELAY(live_data_e.LDS_main_relay, MainRelay.VALUES, "main_relay.cpp"),
-    TPS_ACCEL(live_data_e.LDS_tps_accel_state, TpsAccelState.VALUES, "accel_enrichment.cpp"),
-    BOOST_CONTROL(live_data_e.LDS_boost_control, BoostControl.VALUES, "boost_control.cpp"),
-    LAUNCH_CONTROL(live_data_e.LDS_launch_control_state, LaunchControl.VALUES, "launch_control.cpp"),
-    HIGH_PRESSURE(live_data_e.LDS_high_pressure_fuel_pump, HighPressureFuelPump.VALUES, "high_pressure_fuel_pump.cpp"),
+    AC_CONTROL(live_data_e.LDS_ac_control, AcControl.VALUES),
+    FUEL_PUMP(live_data_e.LDS_fuel_pump, FuelPump.VALUES),
+    MAIN_RELAY(live_data_e.LDS_main_relay, MainRelay.VALUES),
+    TPS_ACCEL(live_data_e.LDS_tps_accel_state, TpsAccelState.VALUES),
+    BOOST_CONTROL(live_data_e.LDS_boost_control, BoostControl.VALUES),
+    LAUNCH_CONTROL(live_data_e.LDS_launch_control_state, LaunchControl.VALUES),
+    HIGH_PRESSURE(live_data_e.LDS_high_pressure_fuel_pump, HighPressureFuelPump.VALUES),
     ;
 
     private final live_data_e liveDataE;
     private final Field[] values;
     private final String fileName;
 
-    LiveDataView(live_data_e liveDataE, Field[] values, String fileName) {
+    LiveDataView(live_data_e liveDataE, Field[] values) {
         this.liveDataE = liveDataE;
         this.values = values;
-        this.fileName = fileName;
+        this.fileName = StateDictionary.INSTANCE.getFileName(liveDataE);
     }
 
     public live_data_e getLiveDataE() {
