2ec0924d985934ad4b93ec56e6bef761824df06e
Matthew Kennedy <matthewkennedy@outlook.com>
2022-01-15 23:26:44 -0800

    only firmware

--- firmware/util/efilib.cpp
+++ firmware/util/efilib.cpp
@@ -401,6 +401,8 @@ float tanf_taylor(float x) {
 	return sin_val / cos_val;
 }
 
+#if EFI_PROD_CODE
 extern "C" void __cxa_pure_virtual(void) {
-  osalSysHalt("Pure virtual function call.");
+	osalSysHalt("Pure virtual function call.");
 }
+#endif
