0a31061da707c73143f3c93fe5553caa578cb223
rusefillc <rusefillc@gmail.com>
2022-01-15 23:02:11 -0500

    Revert "support better wbo format (#3740)"
    This reverts commit cc9fdd97

--- firmware/controllers/sensors/AemXSeriesLambda.h
+++ firmware/controllers/sensors/AemXSeriesLambda.h
@@ -7,17 +7,7 @@ public:
 	AemXSeriesWideband(uint8_t sensorIndex, SensorType type);
 
 protected:
-	bool acceptFrame(const CANRxFrame& frame) const override;
-
-	// Dispatches to one of the three decoders below
 	void decodeFrame(const CANRxFrame& frame, efitick_t nowNt) override;
-
-	// Decode an actual AEM controller, or a rusEFI controller sending AEM format
-	void decodeAemXSeries(const CANRxFrame& frame, efitick_t nowNt);
-
-	// Decode rusEFI custom format
-	void decodeRusefiStandard(const CANRxFrame& frame, efitick_t nowNt);
-	void decodeRusefiDiag(const CANRxFrame& frame);
 
 private:
 	const uint8_t m_sensorIndex;
