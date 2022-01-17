d1fb89ad3d7224bb1d73872fb2c4b56b0e23fe7b
rusefillc <sdfsdfqsf2334234234>
2022-01-16 20:50:55 -0500

    Openblt proteus #3797
    rollback

--- firmware/config/boards/proteus/compile_proteus_f4.sh
+++ firmware/config/boards/proteus/compile_proteus_f4.sh
@@ -4,6 +4,6 @@ export PROJECT_BOARD=proteus
 export PROJECT_CPU=ARCH_STM32F4
 export EXTRA_PARAMS=-DSHORT_BOARD_NAME=proteus_f4
 
-export USE_OPENBLT=yes
+# export USE_OPENBLT=yes
 
 bash ../common_make.sh
