f59aaa36edf8ec2355cd61075927cd6e4c56415f
rusefillc <sdfsdfqsf2334234234>
2022-01-23 21:15:45 -0500

    https://github.com/mck1117/wideband/issues/44
    https://rusefi.com/forum/viewtopic.php?p=44163#p44163

--- misc/stm32f1_test_project/can.cpp
+++ misc/stm32f1_test_project/can.cpp
@@ -8,9 +8,10 @@ const CANConfig canConfig500 =
 {
     CAN_MCR_ABOM | CAN_MCR_AWUM | CAN_MCR_TXFP,
     /*
-     For 36MHz http://www.bittiming.can-wiki.info/ gives us Pre-scaler=4, Seq 1=15 and Seq 2=2. Subtract '1' for register values
+    Settings from https://forum.chibios.org/viewtopic.php?t=2990
+     For 36MHz http://www.bittiming.can-wiki.info/ does not give that option :( Subtract '1' for register values
     */
-    CAN_BTR_SJW(0) | CAN_BTR_BRP(3)  | CAN_BTR_TS1(13) | CAN_BTR_TS2(1),
+    CAN_BTR_SJW(0) | CAN_BTR_BRP(5)  | CAN_BTR_TS1(5) | CAN_BTR_TS2(4),
 };
 
 
