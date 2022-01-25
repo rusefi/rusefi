c59cde8be1f854bc0029aea917222e70fad8d749
GitHub build-firmware Action <action@github.com>
2022-01-25 02:04:47 +0000

    Auto-generated configs and docs

--- java_console/models/src/main/java/com/rusefi/config/generated/LaunchControl.java
+++ java_console/models/src/main/java/com/rusefi/config/generated/LaunchControl.java
@@ -1,6 +1,6 @@
 package com.rusefi.config.generated;
 
-// this file was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/algo/launch_control_state.txt Mon Jan 24 02:53:31 UTC 2022
+// this file was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/algo/launch_control_state.txt Tue Jan 25 02:02:07 UTC 2022
 
 // by class com.rusefi.output.FileJavaFieldsConsumer
 import com.rusefi.config.*;
@@ -12,33 +12,33 @@ public class LaunchControl {
 	public static final Field ISLAUNCHCONDITION = Field.create("ISLAUNCHCONDITION", 4, FieldType.BIT, 2);
 	public static final Field ISSWITCHACTIVATED = Field.create("ISSWITCHACTIVATED", 4, FieldType.BIT, 3);
 	public static final Field ISCLUTCHACTIVATED = Field.create("ISCLUTCHACTIVATED", 4, FieldType.BIT, 4);
-	public static final Field ACTIVATESWITCHCONDITION = Field.create("ACTIVATESWITCHCONDITION", 4, FieldType.BIT, 5);
-	public static final Field RPMCONDITION = Field.create("RPMCONDITION", 4, FieldType.BIT, 6);
-	public static final Field SPEEDCONDITION = Field.create("SPEEDCONDITION", 4, FieldType.BIT, 7);
-	public static final Field TPSCONDITION = Field.create("TPSCONDITION", 4, FieldType.BIT, 8);
-	public static final Field UNUSEDBIT_10_9 = Field.create("UNUSEDBIT_10_9", 4, FieldType.BIT, 9);
-	public static final Field UNUSEDBIT_10_10 = Field.create("UNUSEDBIT_10_10", 4, FieldType.BIT, 10);
-	public static final Field UNUSEDBIT_10_11 = Field.create("UNUSEDBIT_10_11", 4, FieldType.BIT, 11);
-	public static final Field UNUSEDBIT_10_12 = Field.create("UNUSEDBIT_10_12", 4, FieldType.BIT, 12);
-	public static final Field UNUSEDBIT_10_13 = Field.create("UNUSEDBIT_10_13", 4, FieldType.BIT, 13);
-	public static final Field UNUSEDBIT_10_14 = Field.create("UNUSEDBIT_10_14", 4, FieldType.BIT, 14);
-	public static final Field UNUSEDBIT_10_15 = Field.create("UNUSEDBIT_10_15", 4, FieldType.BIT, 15);
-	public static final Field UNUSEDBIT_10_16 = Field.create("UNUSEDBIT_10_16", 4, FieldType.BIT, 16);
-	public static final Field UNUSEDBIT_10_17 = Field.create("UNUSEDBIT_10_17", 4, FieldType.BIT, 17);
-	public static final Field UNUSEDBIT_10_18 = Field.create("UNUSEDBIT_10_18", 4, FieldType.BIT, 18);
-	public static final Field UNUSEDBIT_10_19 = Field.create("UNUSEDBIT_10_19", 4, FieldType.BIT, 19);
-	public static final Field UNUSEDBIT_10_20 = Field.create("UNUSEDBIT_10_20", 4, FieldType.BIT, 20);
-	public static final Field UNUSEDBIT_10_21 = Field.create("UNUSEDBIT_10_21", 4, FieldType.BIT, 21);
-	public static final Field UNUSEDBIT_10_22 = Field.create("UNUSEDBIT_10_22", 4, FieldType.BIT, 22);
-	public static final Field UNUSEDBIT_10_23 = Field.create("UNUSEDBIT_10_23", 4, FieldType.BIT, 23);
-	public static final Field UNUSEDBIT_10_24 = Field.create("UNUSEDBIT_10_24", 4, FieldType.BIT, 24);
-	public static final Field UNUSEDBIT_10_25 = Field.create("UNUSEDBIT_10_25", 4, FieldType.BIT, 25);
-	public static final Field UNUSEDBIT_10_26 = Field.create("UNUSEDBIT_10_26", 4, FieldType.BIT, 26);
-	public static final Field UNUSEDBIT_10_27 = Field.create("UNUSEDBIT_10_27", 4, FieldType.BIT, 27);
-	public static final Field UNUSEDBIT_10_28 = Field.create("UNUSEDBIT_10_28", 4, FieldType.BIT, 28);
-	public static final Field UNUSEDBIT_10_29 = Field.create("UNUSEDBIT_10_29", 4, FieldType.BIT, 29);
-	public static final Field UNUSEDBIT_10_30 = Field.create("UNUSEDBIT_10_30", 4, FieldType.BIT, 30);
-	public static final Field UNUSEDBIT_10_31 = Field.create("UNUSEDBIT_10_31", 4, FieldType.BIT, 31);
+	public static final Field ISVALIDINPUTPIN = Field.create("ISVALIDINPUTPIN", 4, FieldType.BIT, 5);
+	public static final Field ACTIVATESWITCHCONDITION = Field.create("ACTIVATESWITCHCONDITION", 4, FieldType.BIT, 6);
+	public static final Field RPMCONDITION = Field.create("RPMCONDITION", 4, FieldType.BIT, 7);
+	public static final Field SPEEDCONDITION = Field.create("SPEEDCONDITION", 4, FieldType.BIT, 8);
+	public static final Field TPSCONDITION = Field.create("TPSCONDITION", 4, FieldType.BIT, 9);
+	public static final Field UNUSEDBIT_11_10 = Field.create("UNUSEDBIT_11_10", 4, FieldType.BIT, 10);
+	public static final Field UNUSEDBIT_11_11 = Field.create("UNUSEDBIT_11_11", 4, FieldType.BIT, 11);
+	public static final Field UNUSEDBIT_11_12 = Field.create("UNUSEDBIT_11_12", 4, FieldType.BIT, 12);
+	public static final Field UNUSEDBIT_11_13 = Field.create("UNUSEDBIT_11_13", 4, FieldType.BIT, 13);
+	public static final Field UNUSEDBIT_11_14 = Field.create("UNUSEDBIT_11_14", 4, FieldType.BIT, 14);
+	public static final Field UNUSEDBIT_11_15 = Field.create("UNUSEDBIT_11_15", 4, FieldType.BIT, 15);
+	public static final Field UNUSEDBIT_11_16 = Field.create("UNUSEDBIT_11_16", 4, FieldType.BIT, 16);
+	public static final Field UNUSEDBIT_11_17 = Field.create("UNUSEDBIT_11_17", 4, FieldType.BIT, 17);
+	public static final Field UNUSEDBIT_11_18 = Field.create("UNUSEDBIT_11_18", 4, FieldType.BIT, 18);
+	public static final Field UNUSEDBIT_11_19 = Field.create("UNUSEDBIT_11_19", 4, FieldType.BIT, 19);
+	public static final Field UNUSEDBIT_11_20 = Field.create("UNUSEDBIT_11_20", 4, FieldType.BIT, 20);
+	public static final Field UNUSEDBIT_11_21 = Field.create("UNUSEDBIT_11_21", 4, FieldType.BIT, 21);
+	public static final Field UNUSEDBIT_11_22 = Field.create("UNUSEDBIT_11_22", 4, FieldType.BIT, 22);
+	public static final Field UNUSEDBIT_11_23 = Field.create("UNUSEDBIT_11_23", 4, FieldType.BIT, 23);
+	public static final Field UNUSEDBIT_11_24 = Field.create("UNUSEDBIT_11_24", 4, FieldType.BIT, 24);
+	public static final Field UNUSEDBIT_11_25 = Field.create("UNUSEDBIT_11_25", 4, FieldType.BIT, 25);
+	public static final Field UNUSEDBIT_11_26 = Field.create("UNUSEDBIT_11_26", 4, FieldType.BIT, 26);
+	public static final Field UNUSEDBIT_11_27 = Field.create("UNUSEDBIT_11_27", 4, FieldType.BIT, 27);
+	public static final Field UNUSEDBIT_11_28 = Field.create("UNUSEDBIT_11_28", 4, FieldType.BIT, 28);
+	public static final Field UNUSEDBIT_11_29 = Field.create("UNUSEDBIT_11_29", 4, FieldType.BIT, 29);
+	public static final Field UNUSEDBIT_11_30 = Field.create("UNUSEDBIT_11_30", 4, FieldType.BIT, 30);
+	public static final Field UNUSEDBIT_11_31 = Field.create("UNUSEDBIT_11_31", 4, FieldType.BIT, 31);
 	public static final Field[] VALUES = {
 	RETARDTHRESHOLDRPM,
 	COMBINEDCONDITIONS,
@@ -46,32 +46,32 @@ public class LaunchControl {
 	ISLAUNCHCONDITION,
 	ISSWITCHACTIVATED,
 	ISCLUTCHACTIVATED,
+	ISVALIDINPUTPIN,
 	ACTIVATESWITCHCONDITION,
 	RPMCONDITION,
 	SPEEDCONDITION,
 	TPSCONDITION,
-	UNUSEDBIT_10_9,
-	UNUSEDBIT_10_10,
-	UNUSEDBIT_10_11,
-	UNUSEDBIT_10_12,
-	UNUSEDBIT_10_13,
-	UNUSEDBIT_10_14,
-	UNUSEDBIT_10_15,
-	UNUSEDBIT_10_16,
-	UNUSEDBIT_10_17,
-	UNUSEDBIT_10_18,
-	UNUSEDBIT_10_19,
-	UNUSEDBIT_10_20,
-	UNUSEDBIT_10_21,
-	UNUSEDBIT_10_22,
-	UNUSEDBIT_10_23,
-	UNUSEDBIT_10_24,
-	UNUSEDBIT_10_25,
-	UNUSEDBIT_10_26,
-	UNUSEDBIT_10_27,
-	UNUSEDBIT_10_28,
-	UNUSEDBIT_10_29,
-	UNUSEDBIT_10_30,
-	UNUSEDBIT_10_31,
+	UNUSEDBIT_11_10,
+	UNUSEDBIT_11_11,
+	UNUSEDBIT_11_12,
+	UNUSEDBIT_11_13,
+	UNUSEDBIT_11_14,
+	UNUSEDBIT_11_15,
+	UNUSEDBIT_11_16,
+	UNUSEDBIT_11_17,
+	UNUSEDBIT_11_18,
+	UNUSEDBIT_11_19,
+	UNUSEDBIT_11_20,
+	UNUSEDBIT_11_21,
+	UNUSEDBIT_11_22,
+	UNUSEDBIT_11_23,
+	UNUSEDBIT_11_24,
+	UNUSEDBIT_11_25,
+	UNUSEDBIT_11_26,
+	UNUSEDBIT_11_27,
+	UNUSEDBIT_11_28,
+	UNUSEDBIT_11_29,
+	UNUSEDBIT_11_30,
+	UNUSEDBIT_11_31,
 	};
 }
