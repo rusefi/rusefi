4804a736f08e7a68f72071ec98d4bbf33810cabb
Matthew Kennedy <matthewkennedy@outlook.com>
2022-01-26 14:33:04 -0800

    vss and flex are filtered (#3839)
    * vss and flex are filtered
    
    * turbo
    
    * freq sensor test

--- firmware/init/sensor/init_vehicle_speed_sensor.cpp
+++ firmware/init/sensor/init_vehicle_speed_sensor.cpp
@@ -4,7 +4,8 @@
 #include "frequency_sensor.h"
 #include "vehicle_speed_converter.h"
 
-static FrequencySensor vehicleSpeedSensor(SensorType::VehicleSpeed, MS2NT(500));
+// 0.05 filter parameter means averaging over ~20 sensor teeth
+static FrequencySensor vehicleSpeedSensor(SensorType::VehicleSpeed, MS2NT(500), 0.05f);
 static VehicleSpeedConverter vehicleSpeedConverter;
 
 void initVehicleSpeedSensor() {
