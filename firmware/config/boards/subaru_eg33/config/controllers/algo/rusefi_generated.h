20d5f2432963bfe5411a67330d8377e094201a7f
GitHub build-firmware Action <action@github.com>
2022-01-26 04:02:08 +0000

    Auto-generated configs and docs

--- firmware/config/boards/subaru_eg33/config/controllers/algo/rusefi_generated.h
+++ firmware/config/boards/subaru_eg33/config/controllers/algo/rusefi_generated.h
@@ -2432,7 +2432,7 @@
 #define ts_show_spi true
 #define ts_show_trigger_comparator false
 #define ts_show_tunerstudio_port false
-#define TS_SIGNATURE "rusEFI 2022.01.25.subaru_eg33_f7.1872146882"
+#define TS_SIGNATURE "rusEFI 2022.01.26.subaru_eg33_f7.1872146882"
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/signature_hellen_cypress.h
+++ firmware/controllers/generated/signature_hellen_cypress.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 2530820142
-#define TS_SIGNATURE "rusEFI 2022.01.25.hellen_cypress.2530820142"
+#define TS_SIGNATURE "rusEFI 2022.01.26.hellen_cypress.2530820142"
