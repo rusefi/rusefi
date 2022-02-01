45d39906bd17b0791b25a322aa85c330501dbe7c
rusefillc <sdfsdfqsf2334234234>
2022-01-31 19:48:38 -0500

    I2C EEPROM support #3860

--- firmware/hw_layer/ports/stm32/cfg/halconf_community.h
+++ firmware/hw_layer/ports/stm32/cfg/halconf_community.h
@@ -70,7 +70,7 @@
  * @brief   Enables the EEPROM subsystem.
  */
 #if !defined(HAL_USE_EEPROM) || defined(__DOXYGEN__)
-#define HAL_USE_EEPROM            FALSE
+#define HAL_USE_EEPROM            TRUE
 #endif
 
 /**
@@ -126,8 +126,8 @@
 /* EEProm driver related settings.                                           */
 /*===========================================================================*/
 
-#define EEPROM_USE_EE24XX FALSE
-#define EEPROM_USE_EE25XX TRUE
+#define EEPROM_USE_EE24XX TRUE
+#define EEPROM_USE_EE25XX FALSE
 
 
 
