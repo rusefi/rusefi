7048d744e2422fa548ec0dfbf0728ec811488d8c
Matthew Kennedy <matthewkennedy@outlook.com>
2022-02-02 12:42:13 -0800

    this test can be useful

--- unit_tests/tests/test_engine_math.cpp
+++ unit_tests/tests/test_engine_math.cpp
@@ -17,9 +17,7 @@ TEST(misc, structSize) {
 	ASSERT_EQ(1, sizeof(pin_output_mode_e)) << "small enum size";
 	ASSERT_EQ(1, sizeof(brain_pin_e)) << "small enum size";
 	ASSERT_EQ(16, sizeof(air_pressure_sensor_config_s));
-/* no longer the case at least for Proteus
-	ASSERT_EQ(20000, sizeof(persistent_config_s));
-*/
+	ASSERT_EQ(TOTAL_CONFIG_SIZE, sizeof(persistent_config_s));
 }
 
 TEST(misc, testIgnitionPlanning) {
