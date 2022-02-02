aae7d380d027d4d76d4e7018f93f62f48a662374
GitHub build-firmware Action <action@github.com>
2022-02-02 06:04:12 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen154hyundai.h
+++ firmware/controllers/generated/signature_hellen154hyundai.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
 //
 
-#define SIGNATURE_HASH 771776236
-#define TS_SIGNATURE "rusEFI 2022.02.02.hellen154hyundai.771776236"
+#define SIGNATURE_HASH 1281292990
+#define TS_SIGNATURE "rusEFI 2022.02.02.hellen154hyundai.1281292990"
--- firmware/controllers/generated/signature_hellen88bmw.h
+++ firmware/controllers/generated/signature_hellen88bmw.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
 //
 
-#define SIGNATURE_HASH 1232734898
-#define TS_SIGNATURE "rusEFI 2022.02.02.hellen88bmw.1232734898"
+#define SIGNATURE_HASH 723824352
+#define TS_SIGNATURE "rusEFI 2022.02.02.hellen88bmw.723824352"
