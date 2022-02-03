373288ed9b9bef71caf5e496d380a6f400f90368
Benas <bbrazdziunas@gavea.co.uk>
2022-02-03 17:44:32 +0000

    working with smd sd card

--- firmware/config/boards/core8/board_configuration.cpp
+++ firmware/config/boards/core8/board_configuration.cpp
@@ -30,10 +30,10 @@ void setSerialConfigurationOverrides(void) {
  */
 static void setupVbatt() {
 	// 5.6k high side/10k low side = 1.56 ratio divider
-	engineConfiguration->analogInputDividerCoefficient = 1.56f;
+	engineConfiguration->analogInputDividerCoefficient = 1.47f;
 
 	// 6.34k high side/1k low side = 9.2
-	engineConfiguration->vbattDividerCoeff = (7.38f / 1.0f);
+	engineConfiguration->vbattDividerCoeff = (6.34f / 1.0f);
 
 	// Battery sense on PA7
 	engineConfiguration->vbattAdcChannel = EFI_ADC_0;
@@ -49,6 +49,29 @@ void setBoardDefaultConfiguration(void) {
 	
 	setupVbatt();
 	
+	engineConfiguration->sdCardPeriodMs = 50;
 	engineConfiguration->isSdCardEnabled = true;
+
+
+	engineConfiguration->canWriteEnabled = true;
+	engineConfiguration->canReadEnabled = true;
+	engineConfiguration->canSleepPeriodMs = 50;
+	
+	engineConfiguration->canTxPin = GPIOD_0;
+	engineConfiguration->canRxPin = GPIOD_1;
+	engineConfiguration->canBaudRate = B500KBPS;
+
+	
+	engineConfiguration->can2RxPin = GPIOB_5;
+	engineConfiguration->can2TxPin = GPIOB_6;
+	engineConfiguration->can2BaudRate = B500KBPS;
+
+	
+	engineConfiguration->clt.config.bias_resistor = 2490;
+	engineConfiguration->iat.config.bias_resistor = 2490;
+
+	
+	engineConfiguration->lps25BaroSensorScl = GPIOB_10;
+	engineConfiguration->lps25BaroSensorSda = GPIOB_11;
 
 }
--- java_console/models/src/main/java/com/rusefi/config/generated/Fields.java
+++ java_console/models/src/main/java/com/rusefi/config/generated/Fields.java
@@ -1,6 +1,6 @@
 package com.rusefi.config.generated;
 
-// this file was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) integration/rusefi_config.txt Wed Feb 02 09:07:37 GMT 2022
+// this file was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) integration/rusefi_config.txt Thu Feb 03 10:04:42 GMT 2022
 
 // by class com.rusefi.output.FileJavaFieldsConsumer
 import com.rusefi.config.*;
@@ -2314,7 +2314,7 @@ public class Fields {
 	public static final int TS_RESPONSE_UNDERRUN = 0x80;
 	public static final int TS_RESPONSE_UNRECOGNIZED_COMMAND = 0x83;
 	public static final char TS_SET_LOGGER_SWITCH = 'l';
-	public static final String TS_SIGNATURE = "rusEFI 2022.02.02.core8.2189003722";
+	public static final String TS_SIGNATURE = "rusEFI 2022.02.03.core8.2189003722";
 	public static final char TS_SINGLE_WRITE_COMMAND = 'W';
 	public static final char TS_TEST_COMMAND = 't';
 	public static final int TsCalMode_EtbKd = 5;
