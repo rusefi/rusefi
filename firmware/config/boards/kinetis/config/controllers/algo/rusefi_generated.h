37532f600c682cceba45ee0b19a69b6df2512d06
GitHub build-firmware Action <action@github.com>
2022-02-07 22:17:51 +0000

    Auto-generated configs and docs

--- firmware/config/boards/kinetis/config/controllers/algo/rusefi_generated.h
+++ firmware/config/boards/kinetis/config/controllers/algo/rusefi_generated.h
@@ -1894,7 +1894,7 @@
 #define show_Frankenso_presets true
 #define show_test_presets true
 #define showHumanReadableWarning_offset 896
-#define SIGNATURE_HASH 1160407560
+#define SIGNATURE_HASH 562429891
 #define silentTriggerError_offset 1260
 #define slowAdcAlpha_offset 1712
 #define sparkDwellRpmBins_offset 54
@@ -2425,7 +2425,7 @@
 #define ts_show_spi true
 #define ts_show_trigger_comparator true
 #define ts_show_tunerstudio_port true
-#define TS_SIGNATURE "rusEFI 2022.02.07.kin.1160407560"
+#define TS_SIGNATURE "rusEFI 2022.02.07.kin.562429891"
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/signature_hellen-nb1.h
+++ firmware/controllers/generated/signature_hellen-nb1.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
 //
 
-#define SIGNATURE_HASH 2274248021
-#define TS_SIGNATURE "rusEFI 2022.02.07.hellen-nb1.2274248021"
+#define SIGNATURE_HASH 3810644126
+#define TS_SIGNATURE "rusEFI 2022.02.07.hellen-nb1.3810644126"
