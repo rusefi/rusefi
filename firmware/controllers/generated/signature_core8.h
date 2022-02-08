119191b3816a169ddea52a6ae95c2c0216c0940e
Benas <bbrazdziunas@gavea.co.uk>
2022-02-08 07:54:26 +0000

    no message

--- firmware/controllers/generated/signature_core8.h
+++ firmware/controllers/generated/signature_core8.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
 //
 
-#define SIGNATURE_HASH 2049101931
-#define TS_SIGNATURE "rusEFI 2022.02.07.core8.2049101931"
+#define SIGNATURE_HASH 512574880
+#define TS_SIGNATURE "rusEFI 2022.02.08.core8.512574880"
