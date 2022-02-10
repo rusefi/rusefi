40e75657bdf1c64325cab5ef576dce7a102c3eda
GitHub build-firmware Action <action@github.com>
2022-02-10 03:28:45 +0000

    Auto-generated configs and docs

---
+++ firmware/controllers/generated/signature_harley81.h
@@ -0,0 +1,6 @@
+//
+// was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
+//
+
+#define SIGNATURE_HASH 808538647
+#define TS_SIGNATURE "rusEFI 2022.02.10.harley81.808538647"
