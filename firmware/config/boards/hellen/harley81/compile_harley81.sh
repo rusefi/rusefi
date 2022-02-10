8309c14a7d455d50f67cacff0c0ad3b221cb7e80
rusefillc <sdfsdfqsf2334234234>
2022-02-09 22:52:19 -0500

    fixing harley81

--- firmware/config/boards/hellen/harley81/compile_harley81.sh
+++ firmware/config/boards/hellen/harley81/compile_harley81.sh
@@ -2,7 +2,7 @@
 
 export PROJECT_BOARD=hellen/harley81
 export PROJECT_CPU=ARCH_STM32F4
-export EXTRA_PARAMS="-DSHORT_BOARD_NAME=hellen81hd"
+export EXTRA_PARAMS="-DSHORT_BOARD_NAME=harley81"
 
 cd ..
 bash ../common_make.sh
