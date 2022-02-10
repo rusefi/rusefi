84c1b0992790c12f344249a8441da5c3c291b208
Benas Brazdziunas <b.brazdziunas@gmail.com>
2022-02-10 19:12:27 +0000

    merge core8 board config to master

---
+++ firmware/controllers/generated/signature_core8.h
@@ -0,0 +1,6 @@
+//
+// was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
+//
+
+#define SIGNATURE_HASH 3810719315
+#define TS_SIGNATURE "rusEFI 2022.02.10.core8.3810719315"
