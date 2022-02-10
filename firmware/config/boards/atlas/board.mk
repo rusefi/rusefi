8f17fdc502c38f09e84ab16bd17116c78b54e3fa
rusefillc <sdfsdfqsf2334234234>
2022-02-09 19:55:04 -0500

    better default DEFAULT_ENGINE_TYPE

--- firmware/config/boards/atlas/board.mk
+++ firmware/config/boards/atlas/board.mk
@@ -4,7 +4,7 @@ BOARDCPPSRC =  $(PROJECT_DIR)/config/boards/atlas/board_configuration.cpp
 BOARDINC = $(PROJECT_DIR)/config/boards/atlas
 
 #DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=GPIOE_3
-DDEFS += -DFIRMWARE_ID=\"atlas\" -DDEFAULT_ENGINE_TYPE=MINIMAL_PINS
+DDEFS += -DFIRMWARE_ID=\"atlas\"
 
 # This stuff doesn't work on H7 yet
 # DDEFS += -DSTM32_ADC_USE_ADC3=TRUE
