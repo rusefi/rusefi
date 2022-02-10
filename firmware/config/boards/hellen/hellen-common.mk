4781ebe11ecd0ca3379509310df9ecf32b39f5c1
rusefillc <sdfsdfqsf2334234234>
2022-02-09 22:46:13 -0500

    reducing code duplication

--- firmware/config/boards/hellen/hellen-common.mk
+++ firmware/config/boards/hellen/hellen-common.mk
@@ -11,3 +11,6 @@ DDEFS += -DLED_COMMUNICATION_BRAIN_PIN_MODE=INVERTED_OUTPUT
 
 # We are running on Hellen-One hardware!
 DDEFS += -DHW_HELLEN=1
+
+# todo: is it broken?
+DDEFS += -DEFI_LOGIC_ANALYZER=FALSE
