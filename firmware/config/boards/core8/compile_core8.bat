d6cb66ff73de7280ffc219e9d9c590d1554eed7d
rusefillc <sdfsdfqsf2334234234>
2022-02-11 17:18:50 -0500

    core8

--- firmware/config/boards/core8/compile_core8.bat
+++ firmware/config/boards/core8/compile_core8.bat
@@ -1,10 +1,3 @@
 @echo off
 
-cd firmware
-make clean -j8 PROJECT_BOARD=core8
-make -j8 PROJECT_BOARD=core8 PROJECT_CPU=ARCH_STM32F4
-bash.exe gen_config_board.sh core8 core8 rusefi_core8.ini
-
-
-
-
+bash.exe compile_core8.sh
---
+++ firmware/config/boards/core8/compile_core8.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export PROJECT_BOARD=core8
+export PROJECT_CPU=ARCH_STM32F4
+
+bash ../common_make.sh
