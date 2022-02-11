8f85aea3bd7d2ad283c738073b2c372249c0fe50
Matthew Kennedy <matthewkennedy@outlook.com>
2022-02-11 04:03:15 -0800

    executeCommand knows about opcodes (#3915)
    * executeCommand knows about opcodes
    
    * kick
    
    * extra line
    
    * gross duplication
    
    * ugh more

--- java_console/io/src/main/java/com/rusefi/io/commands/GetOutputsCommand.java
+++ java_console/io/src/main/java/com/rusefi/io/commands/GetOutputsCommand.java
@@ -11,10 +11,9 @@ import static com.rusefi.binaryprotocol.IoHelper.swap16;
 
 public class GetOutputsCommand {
     public static byte[] createRequest() {
-        byte[] packet = new byte[5];
-        packet[0] = Fields.TS_OUTPUT_COMMAND;
-        putShort(packet, 1, 0); // offset
-        putShort(packet, 3, swap16(Fields.TS_OUTPUT_SIZE));
+        byte[] packet = new byte[4];
+        putShort(packet, 0, 0); // offset
+        putShort(packet, 2, swap16(Fields.TS_OUTPUT_SIZE));
         return packet;
     }
 
