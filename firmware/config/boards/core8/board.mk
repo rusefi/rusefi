a9089fec946cae1d1c093aec2195da724c46b50d
Benas Brazdziunas <b.brazdziunas@gmail.com>
2022-02-11 20:04:02 +0000

    Connector setup(image and yaml) and applied recommendations (#3921)
    * no message
    
    * removed unnecessary as they are in defaults
    
    Co-authored-by: Benas <bbrazdziunas@gavea.co.uk>

--- firmware/config/boards/core8/board.mk
+++ firmware/config/boards/core8/board.mk
@@ -4,7 +4,6 @@ BOARDCPPSRC =  $(PROJECT_DIR)/config/boards/core8/board_configuration.cpp
 BOARDINC = $(PROJECT_DIR)/config/boards/core8
 
 # Override DEFAULT_ENGINE_TYPE
-DDEFS += -DSTM32F407xx
 DDEFS += -DSHORT_BOARD_NAME=core8
 DDEFS += -DFIRMWARE_ID=\"core8\"
 DDEFS += -DDEFAULT_ENGINE_TYPE=MINIMAL_PINS
