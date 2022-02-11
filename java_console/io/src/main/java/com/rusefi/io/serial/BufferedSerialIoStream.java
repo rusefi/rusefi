08774c4149a7ddedb986c2fbaba6f2373b8578b8
rusefillc <sdfsdfqsf2334234234>
2022-02-10 22:45:30 -0500

    just refactoring

--- java_console/io/src/main/java/com/rusefi/io/serial/BufferedSerialIoStream.java
+++ java_console/io/src/main/java/com/rusefi/io/serial/BufferedSerialIoStream.java
@@ -22,7 +22,7 @@ public class BufferedSerialIoStream extends SerialIoStream {
      */
     private BufferedSerialIoStream(SerialPort sp, String port) {
         super(sp, port);
-        this.dataBuffer = IncomingDataBuffer.createDataBuffer("[serial] ", this);
+        this.dataBuffer = this.createDataBuffer("[serial] ");
     }
 
     @Override
--- java_console/io/src/main/java/com/rusefi/io/stream/PCanIoStream.java
+++ java_console/io/src/main/java/com/rusefi/io/stream/PCanIoStream.java
@@ -79,7 +79,7 @@ public class PCanIoStream extends AbstractIoStream {
 
     public PCanIoStream(PCANBasic can) {
         this.can = can;
-        this.dataBuffer = IncomingDataBuffer.createDataBuffer("", this);
+        this.dataBuffer = this.createDataBuffer("");
     }
 
     @Override
