9d3218b5e900317a321cc5e6214720c1878563f4
GitHub build-firmware Action <action@github.com>
2022-02-16 18:10:04 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_alphax-2chan.h
+++ firmware/controllers/generated/signature_alphax-2chan.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
 //
 
-#define SIGNATURE_HASH 890731926
-#define TS_SIGNATURE "rusEFI 2022.02.16.alphax-2chan.890731926"
+#define SIGNATURE_HASH 3806301238
+#define TS_SIGNATURE "rusEFI 2022.02.16.alphax-2chan.3806301238"
--- firmware/controllers/generated/signature_alphax-4chan.h
+++ firmware/controllers/generated/signature_alphax-4chan.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
 //
 
-#define SIGNATURE_HASH 569639500
-#define TS_SIGNATURE "rusEFI 2022.02.16.alphax-4chan.569639500"
+#define SIGNATURE_HASH 4161589463
+#define TS_SIGNATURE "rusEFI 2022.02.16.alphax-4chan.4161589463"
