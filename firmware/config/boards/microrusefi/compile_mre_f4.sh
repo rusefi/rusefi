f0de921850822d92e41db266268f1f73cffa3a09
rusefillc <sdfsdfqsf2334234234>
2022-02-21 16:59:09 -0500

    openblt doesn't work proteus f7 #3483
    one step back

--- firmware/config/boards/microrusefi/compile_mre_f4.sh
+++ firmware/config/boards/microrusefi/compile_mre_f4.sh
@@ -4,6 +4,6 @@ export PROJECT_BOARD=microrusefi
 export PROJECT_CPU=ARCH_STM32F4
 export EXTRA_PARAMS="-DSHORT_BOARD_NAME=mre_f4"
 
-export USE_OPENBLT=yes
+# export USE_OPENBLT=yes
 
 bash ../common_make.sh
