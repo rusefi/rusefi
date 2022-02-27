84fb26b984a5b5678e7de16097c75911add2ed0a
rusefillc <sdfsdfqsf2334234234>
2022-02-26 22:23:49 -0500

    explicit FW ID not so easy

--- firmware/config/boards/frankenso/compile_frankenso_na6.sh
+++ firmware/config/boards/frankenso/compile_frankenso_na6.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DEFAULT_ENGINE_TYPE=-DDEFAULT_ENGINE_TYPE=FRANKENSO_MIATA_NA6_VAF
+FW_ID_ENV=frankensoNA6
 
 export EXTRA_PARAMS="-DDUMMY \
  -DSHORT_BOARD_NAME=frankenso_na6 \
@@ -8,7 +9,7 @@ export EXTRA_PARAMS="-DDUMMY \
  \
  \
  \
- -DFIRMWARE_ID=\\\"frankensoNA6\\\""
+ "
 
 bash ../common_make.sh
 
