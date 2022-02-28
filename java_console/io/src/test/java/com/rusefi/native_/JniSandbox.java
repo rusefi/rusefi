1f005073d965ad7b689f1d722b8752e6b6bba21b
rusefillc <sdfsdfqsf2334234234>
2022-02-28 12:29:02 -0500

    JNI for test coverage #3965

--- java_console/io/src/test/java/com/rusefi/native_/JniSandbox.java
+++ java_console/io/src/test/java/com/rusefi/native_/JniSandbox.java
@@ -1,13 +1,23 @@
 package com.rusefi.native_;
 
+import java.io.File;
+import java.util.Arrays;
+
 /**
  * Following VM option is needed to launch:
  * -Djava.library.path=../unit_tests/build
  */
 public class JniSandbox {
+
+    private static final String LIBNAME = "lib_rusefi_test";
+
     public static void main(String[] args) {
-        System.loadLibrary("lib_rusefi_test");
+        String[] list = new File(".").list((dir, name) -> name.startsWith(LIBNAME));
+        System.out.println(" " + Arrays.toString(list));
+        System.loadLibrary(LIBNAME);
+
         EngineLogic engineLogic = new EngineLogic();
         System.out.println(engineLogic.getVersion());
+        engineLogic.setConfiguration(new byte[4], 24, 14);
     }
 }
