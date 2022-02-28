82686cfb9f48326ec0932ee0a7873df727c209b7
rusefillc <sdfsdfqsf2334234234>
2022-02-28 12:29:02 -0500

    JNI for test coverage #3965

--- unit_tests/native/native_impl.cpp
+++ unit_tests/native/native_impl.cpp
@@ -8,10 +8,24 @@
 #include "pch.h"
 #include "com_rusefi_native__EngineLogic.h"
 
+//static EngineTestHelper eth(TEST_ENGINE);
+
 JNIEXPORT jstring JNICALL Java_com_rusefi_native_1_EngineLogic_getVersion(JNIEnv * env, jobject) {
 	const char msg[60] = "Hello from unit tests";
 	const char* fileName = msg;
 	jstring result = env->NewStringUTF(msg);
+	printf("[native] hello\n");
 	return result;
 }
 
+JNIEXPORT jbyteArray JNICALL Java_com_rusefi_native_1_EngineLogic_getConfiguration(JNIEnv *env, jobject) {
+	return nullptr;
+}
+
+
+JNIEXPORT void JNICALL Java_com_rusefi_native_1_EngineLogic_setConfiguration(JNIEnv *env, jobject instance,
+		jbyteArray data, jint offset, jint size) {
+
+	printf("[native] setConfiguration %d %d\n", offset, size);
+//	printf("[native] engine %d %d\n", engineConfiguration->engineType);
+}
