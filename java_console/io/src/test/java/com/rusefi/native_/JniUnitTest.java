1a2d5a3500eb6c69948f21987dedf7d7275cd9fe
rusefillc <sdfsdfqsf2334234234>
2022-03-01 00:37:22 -0500

    JNI for test coverage #3965

---
+++ java_console/io/src/test/java/com/rusefi/native_/JniUnitTest.java
@@ -0,0 +1,16 @@
+package com.rusefi.native_;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+public class JniUnitTest {
+    @Test
+    public void run() {
+        JniSandbox.loadLibrary();
+
+        EngineLogic engineLogic = new EngineLogic();
+        String version = engineLogic.getVersion();
+        assertTrue("Got " + version, version.contains("Hello"));
+    }
+}
