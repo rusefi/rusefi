b594905660c820a80735e42b13a8c32b97f44ecb
Matthew Kennedy <matthewkennedy@outlook.com>
2022-03-02 15:46:12 -0800

    allow disabling serial

--- firmware/hw_layer/ports/stm32/mcuconf_common_f4_f7.h
+++ firmware/hw_layer/ports/stm32/mcuconf_common_f4_f7.h
@@ -225,9 +225,18 @@
 /*
  * SERIAL driver system settings.
  */
+#ifndef STM32_SERIAL_USE_USART1
 #define STM32_SERIAL_USE_USART1             TRUE
+#endif
+
+#ifndef STM32_SERIAL_USE_USART2
 #define STM32_SERIAL_USE_USART2             FALSE
+#endif
+
+#ifndef STM32_SERIAL_USE_USART3
 #define STM32_SERIAL_USE_USART3             FALSE
+#endif
+
 #define STM32_SERIAL_USE_UART4              FALSE
 #define STM32_SERIAL_USE_UART5              FALSE
 #define STM32_SERIAL_USE_USART6             TRUE
