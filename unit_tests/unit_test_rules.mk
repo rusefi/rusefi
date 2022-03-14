9d213b658426767051c34209a4f90623355a8757
Matthew Kennedy <matthewkennedy@outlook.com>
2022-03-14 12:00:02 -0700

    things are happy without gtest (#3999)
    * things are happy without gtest
    
    * this is not a static lib

--- unit_tests/unit_test_rules.mk
+++ unit_tests/unit_test_rules.mk
@@ -15,7 +15,7 @@ PCHSUB = unit_tests
 include $(PROJECT_DIR)/rusefi_rules.mk
 
 ifneq ($(OS),Windows_NT)
-# at the moment lib asan breaks JNI static library
+# at the moment lib asan breaks JNI library
 	SANITIZE = no
 else
 	SANITIZE = no
