c74a1d17bf3928a78c9f4fcb3359c7b798f478f7
Matthew Kennedy <matthewkennedy@outlook.com>
2022-03-22 11:21:07 -0700

    track which wheel/value was last

--- firmware/controllers/trigger/trigger_decoder.cpp
+++ firmware/controllers/trigger/trigger_decoder.cpp
@@ -468,6 +468,9 @@ void TriggerState::onShaftSynchronization(
 #endif /* EFI_UNIT_TEST */
 }
 
+static trigger_wheel_e lastTriggerWheel;
+static trigger_value_e lastType;
+
 /**
  * @brief Trigger decoding happens here
  * VR falls are filtered out and some VR noise detection happens prior to invoking this method, for
@@ -517,8 +520,11 @@ void TriggerState::decodeTriggerEvent(
 		int prevTooth = toothed_previous_time;
 		int now2 = nowNt;
 		int delta = prevTooth - now2;
-		firmwareError(CUSTOM_OBD_93, "[%s] toothed_previous_time after nowNt prev=%d now=%d delta=%d", msg, prevTooth, now2, delta);
+		firmwareError(CUSTOM_OBD_93, "[%s] toothed_previous_time after nowNt prev=%d now=%d delta=%d wheel=%d value=%d lastWheel=%d lastValue=%d", msg, prevTooth, now2, delta, triggerWheel, type, lastTriggerWheel, lastType);
 	}
+
+	lastTriggerWheel = triggerWheel;
+	lastType = type;
 
 	efitick_t currentDurationLong = isFirstEvent ? 0 : nowNt - toothed_previous_time;
 
