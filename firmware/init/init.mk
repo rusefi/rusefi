125b882a82a35fb32e33b0e8f9900c7c3e1d0aa4
rusefillc <sdfsdfqsf2334234234>
2022-03-28 23:31:03 -0400

    [EPIC] [Feature Request] Transmission Control #1454

--- firmware/init/init.mk
+++ firmware/init/init.mk
@@ -13,4 +13,5 @@ INIT_SRC_CPP =	$(PROJECT_DIR)/init/sensor/init_sensors.cpp \
 				$(PROJECT_DIR)/init/sensor/init_fuel_level.cpp \
 				$(PROJECT_DIR)/init/sensor/init_aux.cpp \
 				$(PROJECT_DIR)/init/sensor/init_vehicle_speed_sensor.cpp \
+				$(PROJECT_DIR)/init/sensor/init_aux_speed_sensor.cpp \
 				$(PROJECT_DIR)/init/sensor/init_turbocharger_speed_sensor.cpp \
