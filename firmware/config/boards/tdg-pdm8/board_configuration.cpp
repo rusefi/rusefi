baaaa5633c76a8d7168ff40d26bd594c853cbd32
Matthew Kennedy <matthewkennedy@outlook.com>
2022-03-29 15:33:07 -0700

    some actual config

--- firmware/config/boards/tdg-pdm8/board_configuration.cpp
+++ firmware/config/boards/tdg-pdm8/board_configuration.cpp
@@ -2,3 +2,12 @@
 
 void setSdCardConfigurationOverrides() {
 }
+
+void setBoardConfigOverrides() {
+	// No divider, just direct connected analog inputs (they're all internal, so why divide?)
+	engineConfiguration->analogInputDividerCoefficient = 1;
+
+	// 10k/100k divider
+	engineConfiguration->vbattDividerCoeff = 11;
+	engineConfiguration->vbattAdcChannel = EFI_ADC_4;
+}
