f3da42627207c45a7b8c5ade46730fc25878b221
Matthew Kennedy <matthewkennedy@outlook.com>
2022-03-30 16:18:18 -0700

    stub tdg pdm (#4025)
    * stub tdg pdm
    
    * mk
    
    * happy build
    
    * some actual config
    
    * CAN
    
    * script
    
    * chmod
    
    * skip rate now that we know it builds

---
+++ firmware/config/boards/tdg-pdm8/board_configuration.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+
+void setSdCardConfigurationOverrides() {
+}
+
+void setBoardConfigOverrides() {
+	// No divider, just direct connected analog inputs (they're all internal, so why divide?)
+	engineConfiguration->analogInputDividerCoefficient = 1;
+
+	// 10k/100k divider
+	engineConfiguration->vbattDividerCoeff = 11;
+	engineConfiguration->vbattAdcChannel = EFI_ADC_4;
+
+	engineConfiguration->canTxPin = GPIOA_12;
+	engineConfiguration->canRxPin = GPIOA_11;
+}
