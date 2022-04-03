748939f354cdd0af2492df958ea118258c6fe1e1
Matthew Kennedy <matthewkennedy@outlook.com>
2022-04-03 12:22:42 -0700

    fix

--- firmware/config/boards/hellen/hellenNA8_96/board.mk
+++ firmware/config/boards/hellen/hellenNA8_96/board.mk
@@ -1,18 +1,15 @@
 # Combine the related files for a specific platform and MCU.
 
 # Target ECU board design
-BOARDCPPSRC = $(BOARDS_DIR)/hellen/hellen-nb1/board_configuration.cpp
-BOARDINC = $(BOARDS_DIR)/hellen/hellen-nb1
+BOARDCPPSRC = $(BOARDS_DIR)/hellen/hellenNA8_96/board_configuration.cpp
+BOARDINC = $(BOARDS_DIR)/hellen/hellenNA8_96
 
 # Set this if you want a default engine type other than normal hellen-nb1
 ifeq ($(DEFAULT_ENGINE_TYPE),)
   DEFAULT_ENGINE_TYPE = -DDEFAULT_ENGINE_TYPE=HELLEN_NA8_96
 endif
 
-
 DDEFS += -DEFI_MAIN_RELAY_CONTROL=TRUE
-
-
 
 # Add them all together
 DDEFS += -DFIRMWARE_ID=\"hellenNB1\" $(DEFAULT_ENGINE_TYPE)
