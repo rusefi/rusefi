4b6d69520e31acfb4d660912d9cea54bd625d2b8
GitHub build-firmware Action <action@github.com>
2022-04-03 14:22:17 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_alphax-4chan.h
+++ firmware/controllers/generated/signature_alphax-4chan.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
 //
 
-#define SIGNATURE_HASH 2640322895
-#define TS_SIGNATURE "rusEFI 2022.04.03.alphax-4chan.2640322895"
+#define SIGNATURE_HASH 570745706
+#define TS_SIGNATURE "rusEFI 2022.04.03.alphax-4chan.570745706"
