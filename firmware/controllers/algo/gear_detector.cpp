f0b810a81ced184b30ca907a46933cdea661b018
Matthew Kennedy <matthewkennedy@outlook.com>
2022-04-07 06:21:11 -0700

    Gear detection implementation (#4052)
    * gear detect improvements
    
    * ui
    
    * ui for gear ratios
    
    * ui
    
    * output channel
    
    * output channels etc
    
    * implement gear detection
    
    * name
    
    * s
    
    * status loop
    
    * missing function
    
    * ui tweaking
    
    * s

--- firmware/controllers/algo/gear_detector.cpp
+++ firmware/controllers/algo/gear_detector.cpp
@@ -1,9 +1,55 @@
 #include "pch.h"
 
-void GearDetector::onSlowCallback() {
-	m_gearboxRatio = computeGearboxRatio();
+static constexpr float geometricMean(float x, float y) {
+	return sqrtf(x * y);
+}
 
-	// TODO: solve for which gear this is
+void GearDetector::onConfigurationChange(engine_configuration_s const * /*previousConfig*/) {
+	// Compute gear thresholds between gears
+
+	for (size_t i = 0; i < efi::size(m_gearThresholds); i++) {
+		// Threshold i is the threshold between gears i and i+1
+
+		m_gearThresholds[i] = geometricMean(
+			engineConfiguration->gearRatio[i],
+			engineConfiguration->gearRatio[i + 1]
+		);
+
+		// TODO: validate gears are in correct order
+	}
+}
+
+void GearDetector::onSlowCallback() {
+	float ratio = computeGearboxRatio();
+	m_gearboxRatio = ratio;
+
+	m_currentGear = determineGearFromRatio(ratio);
+}
+
+size_t GearDetector::determineGearFromRatio(float ratio) const {
+	// 1.5x first gear is neutral or clutch slip or something
+	if (ratio > engineConfiguration->gearRatio[0] * 1.5f) {
+		return 0;
+	}
+
+	auto gearCount = engineConfiguration->totalGearsCount;
+
+	// 0.66x top gear is coasting with engine off or something
+	if (ratio < engineConfiguration->gearRatio[gearCount - 1] * 0.66f) {
+		return 0;
+	}
+
+	size_t currentGear = gearCount;
+
+	while (currentGear > 1) {
+		if (ratio < m_gearThresholds[currentGear - 2]) {
+			break;
+		}
+
+		currentGear--;
+	}
+
+	return currentGear;
 }
 
 float GearDetector::computeGearboxRatio() const {
@@ -30,4 +76,8 @@ float GearDetector::computeGearboxRatio() const {
 
 float GearDetector::getGearboxRatio() const {
 	return m_gearboxRatio;
+}
+
+size_t GearDetector::getCurrentGear() const {
+	return m_currentGear;
 }
--- unit_tests/tests/test_gear_detector.cpp
+++ unit_tests/tests/test_gear_detector.cpp
@@ -26,3 +26,110 @@ TEST(GearDetector, ComputeGearRatio) {
 	// Idling, car stopped, check no div/0
 	EXPECT_EQ(0, GetGearRatioFor(507, 4.1, 0, 800));
 }
+
+TEST(GearDetector, DetermineGearSingleSpeed) {
+	EngineTestHelper eth(TEST_ENGINE);
+	GearDetector dut;
+
+	engineConfiguration->totalGearsCount = 1;
+	engineConfiguration->gearRatio[0] = 2;
+
+	dut.onConfigurationChange(nullptr);
+
+	// Super high ratios indicate clutch slip or idling in neutral or something
+	EXPECT_EQ(0, dut.determineGearFromRatio(100));
+	EXPECT_EQ(0, dut.determineGearFromRatio(4));
+
+	// Check exactly on the gear
+	EXPECT_EQ(1, dut.determineGearFromRatio(2));
+
+	// Check near the gear
+	EXPECT_EQ(1, dut.determineGearFromRatio(2.1));
+	EXPECT_EQ(1, dut.determineGearFromRatio(1.9));
+
+	// Extremely low ratio suggests stopped engine at speed?
+	EXPECT_EQ(0, dut.determineGearFromRatio(1.0));
+}
+
+TEST(GearDetector, DetermineGear5Speed) {
+	EngineTestHelper eth(TEST_ENGINE);
+	GearDetector dut;
+
+	engineConfiguration->totalGearsCount = 5;
+	engineConfiguration->gearRatio[0] = 3.35;
+	engineConfiguration->gearRatio[1] = 1.99;
+	engineConfiguration->gearRatio[2] = 1.33;
+	engineConfiguration->gearRatio[3] = 1.00;
+	engineConfiguration->gearRatio[4] = 0.72;
+
+	dut.onConfigurationChange(nullptr);
+
+	// Super high ratios indicate clutch slip or idling in neutral or something
+	EXPECT_EQ(0, dut.determineGearFromRatio(100));
+	EXPECT_EQ(0, dut.determineGearFromRatio(6));
+
+	// Check exactly on gears
+	EXPECT_EQ(1, dut.determineGearFromRatio(3.35));
+	EXPECT_EQ(2, dut.determineGearFromRatio(1.99));
+	EXPECT_EQ(3, dut.determineGearFromRatio(1.33));
+	EXPECT_EQ(4, dut.determineGearFromRatio(1.00));
+	EXPECT_EQ(5, dut.determineGearFromRatio(0.72));
+
+	// Check near each gear
+	EXPECT_EQ(1, dut.determineGearFromRatio(3.45));
+	EXPECT_EQ(1, dut.determineGearFromRatio(3.25));
+
+	EXPECT_EQ(2, dut.determineGearFromRatio(2.2));
+	EXPECT_EQ(2, dut.determineGearFromRatio(1.8));
+
+	EXPECT_EQ(3, dut.determineGearFromRatio(1.45));
+	EXPECT_EQ(3, dut.determineGearFromRatio(1.25));
+
+	EXPECT_EQ(4, dut.determineGearFromRatio(1.1));
+	EXPECT_EQ(4, dut.determineGearFromRatio(0.9));
+
+	EXPECT_EQ(5, dut.determineGearFromRatio(0.8));
+	EXPECT_EQ(5, dut.determineGearFromRatio(0.6));
+
+	// Extremely low ratio suggests stopped engine at speed?
+	EXPECT_EQ(0, dut.determineGearFromRatio(0.1));
+}
+
+TEST(GearDetector, DetermineGear8Speed) {
+	EngineTestHelper eth(TEST_ENGINE);
+	GearDetector dut;
+
+	// ZF 8HP 70
+	engineConfiguration->totalGearsCount = 8;
+	engineConfiguration->gearRatio[0] = 4.69;
+	engineConfiguration->gearRatio[1] = 3.13;
+	engineConfiguration->gearRatio[2] = 2.10;
+	engineConfiguration->gearRatio[3] = 1.67;
+	engineConfiguration->gearRatio[4] = 1.28;
+	engineConfiguration->gearRatio[5] = 1;
+	engineConfiguration->gearRatio[6] = 0.84;
+	engineConfiguration->gearRatio[7] = 0.67;
+
+	dut.onConfigurationChange(nullptr);
+
+	// Super high ratios indicate clutch slip or idling in neutral or something
+	EXPECT_EQ(0, dut.determineGearFromRatio(100));
+	EXPECT_EQ(0, dut.determineGearFromRatio(8));
+
+	// Check exactly on gears - only test the ends, the middle works
+	EXPECT_EQ(1, dut.determineGearFromRatio(4.69));
+	EXPECT_EQ(2, dut.determineGearFromRatio(3.13));
+
+	EXPECT_EQ(7, dut.determineGearFromRatio(0.84));
+	EXPECT_EQ(8, dut.determineGearFromRatio(0.67));
+
+	// Check near each gear - only test the ends, the middle works
+	EXPECT_EQ(1, dut.determineGearFromRatio(4.75));
+	EXPECT_EQ(1, dut.determineGearFromRatio(4.3));
+
+	EXPECT_EQ(8, dut.determineGearFromRatio(0.71));
+	EXPECT_EQ(8, dut.determineGearFromRatio(0.6));
+
+	// Extremely low ratio suggests stopped engine at speed?
+	EXPECT_EQ(0, dut.determineGearFromRatio(0.1));
+}
