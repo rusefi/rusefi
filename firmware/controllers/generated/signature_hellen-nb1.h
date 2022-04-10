a671a7fca92a36c1e6b4cb9000d77cf7474a0415
GitHub build-firmware Action <action@github.com>
2022-04-10 19:21:02 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen-nb1.h
+++ firmware/controllers/generated/signature_hellen-nb1.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
 //
 
-#define SIGNATURE_HASH 60423411
-#define TS_SIGNATURE "rusEFI 2022.04.10.hellen-nb1.60423411"
+#define SIGNATURE_HASH 1756047907
+#define TS_SIGNATURE "rusEFI 2022.04.10.hellen-nb1.1756047907"
--- firmware/controllers/generated/signature_hellen154hyundai.h
+++ firmware/controllers/generated/signature_hellen154hyundai.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
 //
 
-#define SIGNATURE_HASH 2713001279
-#define TS_SIGNATURE "rusEFI 2022.04.10.hellen154hyundai.2713001279"
+#define SIGNATURE_HASH 4220826014
+#define TS_SIGNATURE "rusEFI 2022.04.10.hellen154hyundai.4220826014"
