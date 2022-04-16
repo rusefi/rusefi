b5d45a9e269eb2d98387f464184cab10c01e615d
GitHub build-firmware Action <action@github.com>
2022-04-16 18:05:15 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen121vag.h
+++ firmware/controllers/generated/signature_hellen121vag.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
 //
 
-#define SIGNATURE_HASH 4175867210
-#define TS_SIGNATURE "rusEFI 2022.04.16.hellen121vag.4175867210"
+#define SIGNATURE_HASH 171871788
+#define TS_SIGNATURE "rusEFI 2022.04.16.hellen121vag.171871788"
