6977a74f113253487a76a4c3e0d9edc0e46c1c04
Matthew Kennedy <matthewkennedy@outlook.com>
2022-04-18 11:57:29 -0700

    some hellens

--- firmware/config/boards/hellen/hellen128/compile_hellen128.sh
+++ firmware/config/boards/hellen/hellen128/compile_hellen128.sh
@@ -2,7 +2,6 @@
 
 export PROJECT_BOARD=hellen/hellen128
 export PROJECT_CPU=ARCH_STM32F4
-export EXTRA_PARAMS="-DSHORT_BOARD_NAME=hellen128"
 
 cd ..
 bash ../common_make.sh
--- firmware/config/boards/hellen/hellen154hyundai/compile_hellen154hyundai.sh
+++ firmware/config/boards/hellen/hellen154hyundai/compile_hellen154hyundai.sh
@@ -2,7 +2,6 @@
 
 export PROJECT_BOARD=hellen/hellen154hyundai
 export PROJECT_CPU=ARCH_STM32F4
-export EXTRA_PARAMS="-DSHORT_BOARD_NAME=hellen154hyundai"
 
 cd ..
 bash ../common_make.sh
