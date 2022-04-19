7c7ac99bfbefbdb503a63f011071e0b4975ae3b7
rusefillc <sdfsdfqsf2334234234>
2022-04-19 15:03:38 -0400

    https://github.com/rusefi/alphax-2chan/issues/50
    more progress

--- firmware/controllers/actuators/idle_thread_io.cpp
+++ firmware/controllers/actuators/idle_thread_io.cpp
@@ -90,13 +90,15 @@ void setManualIdleValvePosition(int positionPercent) {
 	engineConfiguration->manIdlePosition = positionPercent;
 }
 
+#endif /* EFI_UNIT_TEST */
+
+#if EFI_PROD_CODE
 static void startInputPinIfValid(const char *msg, brain_pin_e pin, pin_input_mode_e mode) {
 	if (isBrainPinValid(pin)) {
 		efiSetPadMode(msg, pin, getInputMode(mode));
 	}
 }
-
-#endif /* EFI_UNIT_TEST */
+#endif // EFI_PROD_CODE
 
 percent_t getIdlePosition() {
 	return engine->module<IdleController>().unmock().currentIdlePosition;
