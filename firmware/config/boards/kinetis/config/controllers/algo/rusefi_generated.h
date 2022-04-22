275803b6c12cb3aef2564f4417effc6afd1ec435
GitHub build-firmware Action <action@github.com>
2022-04-22 04:51:49 +0000

    Auto-generated configs and docs

--- firmware/config/boards/kinetis/config/controllers/algo/rusefi_generated.h
+++ firmware/config/boards/kinetis/config/controllers/algo/rusefi_generated.h
@@ -2518,7 +2518,7 @@
 #define ts_show_spi true
 #define ts_show_trigger_comparator true
 #define ts_show_tunerstudio_port true
-#define TS_SIGNATURE "rusEFI 2022.04.21.kin.2327713320"
+#define TS_SIGNATURE "rusEFI 2022.04.22.kin.2327713320"
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't'
