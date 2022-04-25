3ad63077068aa8ab9d168fadb48d0e326c9adaa0
Matthew Kennedy <matthewkennedy@outlook.com>
2022-04-25 12:42:28 -0700

    cypress

--- firmware/hw_layer/ports/cypress/hw_ports.mk
+++ firmware/hw_layer/ports/cypress/hw_ports.mk
@@ -7,7 +7,7 @@ HW_LAYER_EMS_CPP += $(PROJECT_DIR)/hw_layer/ports/cypress/mpu_util.cpp \
 	$(PROJECT_DIR)/hw_layer/ports/cypress/cypress_common.cpp \
 	$(PROJECT_DIR)/hw_layer/ports/cypress/backup_ram.cpp \
 	$(PROJECT_DIR)/hw_layer/microsecond_timer/microsecond_timer_gpt.cpp \
-	$(PROJECT_DIR)/hw_layer/ports/cypress/flash_int.c \
+	$(PROJECT_DIR)/hw_layer/ports/cypress/flash_int.cpp \
 	$(PROJECT_DIR)/hw_layer/ports/cypress/serial_over_usb/usbconsole.cpp \
 	$(PROJECT_DIR)/hw_layer/ports/cypress/serial_over_usb/usbconfig.cpp \
 
