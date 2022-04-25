91ccb359c95da59b630efa97a79dc14712bbb744
Matthew Kennedy <matthewkennedy@outlook.com>
2022-04-25 12:11:43 -0700

    cypress/kinetis

--- firmware/hw_layer/ports/cypress/hw_ports.mk
+++ firmware/hw_layer/ports/cypress/hw_ports.mk
@@ -5,13 +5,12 @@ endif
 HW_LAYER_EGT = $(PROJECT_DIR)/hw_layer/ports/cypress/serial_over_usb/usbconfig.c \
 	$(PROJECT_DIR)/hw_layer/ports/cypress/serial_over_usb/usbconsole.c
 
-HW_LAYER_EMS += 	$(PROJECT_DIR)/hw_layer/ports/cypress/flash_int.c
-
 HW_LAYER_EMS_CPP += $(PROJECT_DIR)/hw_layer/ports/cypress/mpu_util.cpp \
 	$(PROJECT_DIR)/hw_layer/ports/cypress/cypress_pins.cpp \
 	$(PROJECT_DIR)/hw_layer/ports/cypress/cypress_common.cpp \
 	$(PROJECT_DIR)/hw_layer/ports/cypress/backup_ram.cpp \
 	$(PROJECT_DIR)/hw_layer/microsecond_timer/microsecond_timer_gpt.cpp \
+	$(PROJECT_DIR)/hw_layer/ports/cypress/flash_int.c \
 
 HW_INC += $(PROJECT_DIR)/hw_layer/ports/cypress/serial_over_usb
 
