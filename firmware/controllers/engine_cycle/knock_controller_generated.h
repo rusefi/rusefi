ebf9e8ab47cb860cc002ab48efa98f7ab379effe
rusefillc <sdfsdfqsf2334234234>
2022-04-29 15:23:22 -0400

    knock reaction #202
    LiveData

--- firmware/controllers/engine_cycle/knock_controller_generated.h
+++ firmware/controllers/engine_cycle/knock_controller_generated.h
@@ -1,4 +1,4 @@
-// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/engine_cycle/knock_controller.txt Fri Apr 29 16:01:47 UTC 2022
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/engine_cycle\knock_controller.txt Fri Apr 29 15:13:26 EDT 2022
 // by class com.rusefi.output.CHeaderConsumer
 // begin
 #pragma once
@@ -10,8 +10,16 @@ struct knock_controller_s {
 	 * offset 0
 	 */
 	angle_t m_knockRetard = (angle_t)0;
+	/**
+	 * offset 4
+	 */
+	float m_knockThreshold = (float)0;
+	/**
+	 * offset 8
+	 */
+	uint32_t m_knockCount = (uint32_t)0;
 };
-static_assert(sizeof(knock_controller_s) == 4);
+static_assert(sizeof(knock_controller_s) == 12);
 
 // end
-// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/engine_cycle/knock_controller.txt Fri Apr 29 16:01:47 UTC 2022
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/engine_cycle\knock_controller.txt Fri Apr 29 15:13:26 EDT 2022
