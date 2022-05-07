b2815eb33c8513bd313f93d572ded29fea89a38a
rusefillc <sdfsdfqsf2334234234>
2022-05-07 05:50:14 -0400

    improving scripts?

--- firmware/config/boards/common_make.sh
+++ firmware/config/boards/common_make.sh
@@ -14,7 +14,7 @@ make -j$(nproc) -r PROJECT_BOARD=$PROJECT_BOARD PROJECT_CPU=$PROJECT_CPU
 [ -e build/rusefi.hex ] || { echo "FAILED to compile by $SCRIPT_NAME with $PROJECT_BOARD $DEBUG_LEVEL_OPT and $EXTRA_PARAMS"; exit 1; }
 if [ $USE_OPENBLT = "yes" ]; then
   make openblt
-  [ -e build-openblt/openblt_$PROJECT_BOARD.hex ] || { echo "FAILED to compile OpneBLT by $SCRIPT_NAME with $PROJECT_BOARD"; exit 1; }
+  [ -e build-openblt/openblt_$PROJECT_BOARD.hex ] || { echo "FAILED to compile OpenBLT by $SCRIPT_NAME with $PROJECT_BOARD"; exit 1; }
 fi
 
 if uname | grep "NT"; then
