e487ac80b9cb66e979ee67fb489a17ac6223730d
Andrey Gusakov <dron0gus@gmail.com>
2022-05-07 12:55:26 +0300

    Export PROJECT_BOARD, we need it while compiling OpenBLT with separate make call

--- firmware/config/boards/clean_env_variables.sh
+++ firmware/config/boards/clean_env_variables.sh
@@ -3,6 +3,8 @@
 echo "clean_env_variables.sh: Cleaning up so that no one inherits any of these values"
 export DEBUG_LEVEL_OPT=""
 export EXTRA_PARAMS=""
+export PROJECT_BOARD=""
+export PROJECT_CPU=""
 export USE_BOOTLOADER=""
 export VAR_DEF_ENGINE_TYPE=""
 export LED_CRITICAL_ERROR_BRAIN_PIN=""
