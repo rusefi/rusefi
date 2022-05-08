5bbb401f85aafbc4148f755f8e6d2cff2630d3f3
GitHub build-firmware Action <action@github.com>
2022-05-08 13:06:44 +0000

    Auto-generated configs and docs

--- firmware/controllers/trigger/trigger_state_generated.h
+++ firmware/controllers/trigger/trigger_state_generated.h
@@ -1,4 +1,4 @@
-// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/trigger/trigger_state.txt Sun May 08 12:02:50 UTC 2022
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/trigger/trigger_state.txt Sun May 08 13:03:46 UTC 2022
 // by class com.rusefi.output.CHeaderConsumer
 // begin
 #pragma once
@@ -6,40 +6,45 @@
 // start of trigger_state_s
 struct trigger_state_s {
 	/**
-	 * offset 0
-	 */
-	float triggerSyncGapRatio = (float)0;
-	/**
 	 * Crank revolution counter
-	 * offset 4
+	 * offset 0
 	 */
 	uint32_t totalRevolutionCounter = (uint32_t)0;
 	/**
-	 * offset 8
+	 * offset 4
 	 */
 	scaled_channel<float, 1, 1> vvtSyncGapRatio = (float)0;
 	/**
-	 * offset 12
+	 * offset 8
 	 */
 	scaled_channel<float, 1, 1> vvtCurrentPosition = (float)0;
 	/**
+	 * @@GAUGE_NAME_TRG_GAP@@
+	 * offset 12
+	 */
+	scaled_channel<float, 1, 1> triggerSyncGapRatio = (float)0;
+	/**
 	 * offset 16
+	 */
+	float triggerActualSyncGapRatio = (float)0;
+	/**
+	 * offset 20
 	 */
 	uint8_t triggerStateIndex = (uint8_t)0;
 	/**
-	 * offset 17
+	 * offset 21
 	 */
 	uint8_t vvtCounter = (uint8_t)0;
 	/**
-	 * offset 18
+	 * offset 22
 	 */
 	uint8_t vvtSyncCounter = (uint8_t)0;
 	/**
-	 * offset 19
+	 * offset 23
 	 */
 	uint8_t vvtStateIndex = (uint8_t)0;
 };
-static_assert(sizeof(trigger_state_s) == 20);
+static_assert(sizeof(trigger_state_s) == 24);
 
 // end
-// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/trigger/trigger_state.txt Sun May 08 12:02:50 UTC 2022
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/trigger/trigger_state.txt Sun May 08 13:03:46 UTC 2022
