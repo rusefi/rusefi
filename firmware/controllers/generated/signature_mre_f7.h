01e09cab697000460ba47626a7a41da65d799fda
GitHub build-firmware Action <action@github.com>
2022-05-11 10:51:10 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_mre_f7.h
+++ firmware/controllers/generated/signature_mre_f7.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
 //
 
-#define SIGNATURE_HASH 265171808
-#define TS_SIGNATURE "rusEFI 2022.05.10.mre_f7.265171808"
+#define SIGNATURE_HASH 3743348276
+#define TS_SIGNATURE "rusEFI 2022.05.11.mre_f7.3743348276"
