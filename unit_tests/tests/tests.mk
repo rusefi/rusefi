90f0ae605788f7a38581372434f814ff69050521
Andrey <rusefillc@gmail.com>
2022-05-15 10:07:02 -0400

    reduce flash footprint by smarter code generation #4163

--- unit_tests/tests/tests.mk
+++ unit_tests/tests/tests.mk
@@ -29,6 +29,7 @@ TESTS_SRC_CPP = \
 	tests/ignition_injection/test_fuel_computer.cpp \
 	tests/ignition_injection/test_injector_model.cpp \
 	tests/lua/test_lua_basic.cpp \
+	tests/lua/test_lookup.cpp \
 	tests/lua/test_lua_e65.cpp \
 	tests/lua/test_lua_with_engine.cpp \
 	tests/lua/test_lua_hooks.cpp \
