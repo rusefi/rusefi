f19d9348f8c2efc48fcf112c43d721959e4afdda
Matthew Kennedy <matthewkennedy@outlook.com>
2022-05-17 18:19:30 -0700

    s

--- firmware/config/boards/tdg-pdm8/board_configuration.cpp
+++ firmware/config/boards/tdg-pdm8/board_configuration.cpp
@@ -25,17 +25,17 @@ static constexpr float AmpsPerVolt = 10;
 static constexpr float MaxCurrent = 100;
 
 static const ProtectedGpioConfig cfgs[] = {
-	{ GPIOA_0,  EFI_ADC_10, AmpsPerVolt, MaxCurrent },
-	{ GPIOA_1,  EFI_ADC_11, AmpsPerVolt, MaxCurrent },
-	{ GPIOA_2,  EFI_ADC_12, AmpsPerVolt, MaxCurrent },
-	{ GPIOA_3,  EFI_ADC_13, AmpsPerVolt, MaxCurrent },
-	{ GPIOC_9,  EFI_ADC_14, AmpsPerVolt, MaxCurrent },
-	{ GPIOA_8,  EFI_ADC_15, AmpsPerVolt, MaxCurrent },
-	{ GPIOA_9,  EFI_ADC_8,  AmpsPerVolt, MaxCurrent },
-	{ GPIOA_10, EFI_ADC_9,  AmpsPerVolt, MaxCurrent },
+	{ Gpio::A0,  EFI_ADC_10, AmpsPerVolt, MaxCurrent },
+	{ Gpio::A1,  EFI_ADC_11, AmpsPerVolt, MaxCurrent },
+	{ Gpio::A2,  EFI_ADC_12, AmpsPerVolt, MaxCurrent },
+	{ Gpio::A3,  EFI_ADC_13, AmpsPerVolt, MaxCurrent },
+	{ Gpio::C9,  EFI_ADC_14, AmpsPerVolt, MaxCurrent },
+	{ Gpio::A8,  EFI_ADC_15, AmpsPerVolt, MaxCurrent },
+	{ Gpio::A9,  EFI_ADC_8,  AmpsPerVolt, MaxCurrent },
+	{ Gpio::A10, EFI_ADC_9,  AmpsPerVolt, MaxCurrent },
 };
 
 void boardInitHardware() {
 	// We recycle GPIOI_0..7 for protected outputs since this is a 64-pin STM32 which has no port I
-	protectedGpio_add(GPIOI_0, cfgs);
+	protectedGpio_add(Gpio::I0, cfgs);
 }
