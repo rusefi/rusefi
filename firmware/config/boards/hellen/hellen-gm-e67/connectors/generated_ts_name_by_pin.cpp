b0c656b3633f7f126ebb1924ef8259fa821c6aa5
GitHub build-firmware Action <action@github.com>
2022-05-23 04:47:17 +0000

    Auto-generated configs and docs

--- firmware/config/boards/hellen/hellen-gm-e67/connectors/generated_ts_name_by_pin.cpp
+++ firmware/config/boards/hellen/hellen-gm-e67/connectors/generated_ts_name_by_pin.cpp
@@ -19,6 +19,9 @@ const char * getBoardSpecificPinName(brain_pin_e brainPin) {
 		case Gpio::F12: return "C2-52 INJ 6";
 		case Gpio::F13: return "C2-71 INJ 7";
 		case Gpio::F14: return "C2-48 INJ 8";
+		case Gpio::F4: return "C1-63 - Crank";
+		case Gpio::F6: return "C2-59 - Cam";
+		case Gpio::G14: return "C1-40 - MAIN";
 		case Gpio::G7: return "C2-72 INJ 1";
 		case Gpio::G8: return "C2-32 INJ 2";
 		default: return nullptr;
