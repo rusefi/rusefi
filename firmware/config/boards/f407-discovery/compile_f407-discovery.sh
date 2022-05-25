c9863f40c67cb2ef74bafb3490d717997ab60032
rusefillc <sdfsdfqsf2334234234>
2022-05-25 14:25:30 -0400

    console uses .hex Looks like I broke MRE and Proteus with OpenBLT #4199
    HW CI to use common build script

--- firmware/config/boards/f407-discovery/compile_f407-discovery.sh
+++ firmware/config/boards/f407-discovery/compile_f407-discovery.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export EXTRA_PARAMS="-DDUMMY -DSHORT_BOARD_NAME=f4-discovery"
+export EXTRA_PARAMS="-DDUMMY -DSHORT_BOARD_NAME=f407-discovery"
 
 bash ../common_make.sh f407-discovery ARCH_STM32F4
