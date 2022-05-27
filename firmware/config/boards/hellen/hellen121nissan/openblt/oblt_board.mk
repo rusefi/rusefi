f0bc24c8d620c6cc7029d77383e8bfa5d649df01
rusefillc <sdfsdfqsf2334234234>
2022-05-27 13:18:51 -0400

    first hellen OpenBLT?

--- firmware/config/boards/hellen/hellen121nissan/openblt/oblt_board.mk
+++ firmware/config/boards/hellen/hellen121nissan/openblt/oblt_board.mk
@@ -8,13 +8,22 @@ PROJ_FILES += $(filter-out $(OPENBLT_BOARD_DIR)/flash_layout.c, $(call rwildcard
 #|--------------------------------------------------------------------------------------|
 #| Options for toolchain binaries                                                       |
 #|--------------------------------------------------------------------------------------|
-BRDFLAGS    = -DSTM32F429xx
+ifeq ($(PROJECT_CPU),ARCH_STM32F4)
+	BRDFLAGS     = -DSTM32F429xx
+else ifeq ($(PROJECT_CPU),ARCH_STM32F7)
+	#TODO: check what CPU is on proteus f7
+	BRDFLAGS     = -DSTM32F767xx
+else ifeq ($(PROJECT_CPU),ARCH_STM32H7)
+	#TODO: check what CPU is on proteus h7
+	BRDFLAGS     = -DSTM32H743xx
+endif
 
 #|--------------------------------------------------------------------------------------|
 #| Specify library files                                                                |
 #|--------------------------------------------------------------------------------------|
 LIBS =
 
+# We use HSI (internal) on proteus. We define HSE (external) only to have compiler happy.
 BRDFLAGS    += -DHSE_VALUE=8000000
 
 BRDFLAGS += -DSTATUS_LED_PORT=GPIOE -DSTATUS_LED_PIN=GPIO_PIN_3
@@ -25,4 +34,6 @@ BRDFLAGS += -DBOOT_CPU_SYSTEM_SPEED_KHZ=168000
 # Select the desired UART peripheral as a zero based index.
 BRDFLAGS += -DBOOT_COM_RS232_CHANNEL_INDEX=2
 # \brief Select the desired CAN peripheral as a zero based index.
-BRDFLAGS += -DBOOT_COM_CAN_CHANNEL_INDEX=1
+BRDFLAGS += -DBOOT_COM_CAN_CHANNEL_INDEX=0
+
+PROJ_FILES += $(PROJECT_DIR)/hw_layer/openblt/main_internal_osc.c
