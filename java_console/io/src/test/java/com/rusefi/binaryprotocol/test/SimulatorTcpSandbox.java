17d6cfc8d2c1d13c3d57bf8eb70958db7bf04b1c
rusefillc <sdfsdfqsf2334234234>
2022-05-28 17:24:09 -0400

    simulator TCP data rate #4203
    java to java TCP/IP baseline test

--- java_console/io/src/test/java/com/rusefi/binaryprotocol/test/SimulatorTcpSandbox.java
+++ java_console/io/src/test/java/com/rusefi/binaryprotocol/test/SimulatorTcpSandbox.java
@@ -1,6 +1,5 @@
 package com.rusefi.binaryprotocol.test;
 
-import com.opensr5.ConfigurationImage;
 import com.rusefi.binaryprotocol.BinaryProtocol;
 import com.rusefi.io.LinkManager;
 import com.rusefi.io.tcp.TcpIoStream;
@@ -19,9 +18,46 @@ public class SimulatorTcpSandbox {
         TcpIoStream tsStream = new TcpIoStream("sandbox", s);
 
         LinkManager linkManager = new LinkManager();
-        SandboxCommon.verifyCrcNoPending(tsStream, linkManager);
+//        SandboxCommon.verifyCrcNoPending(tsStream, linkManager);
 
-        ConfigurationImage ci = SandboxCommon.readImage(tsStream, linkManager);
+        for (int i = 0; i < 3; i++) {
+            // warm-up cycles just for fun
+            String signature = BinaryProtocol.getSignature(tsStream);
+        }
+
+
+        {
+            int count = 10000;
+            long startMs = System.currentTimeMillis();
+            for (int i = 0; i < count; i++) {
+                // warm-up cycles just for fun
+                String signature = BinaryProtocol.getSignature(tsStream);
+            }
+            long time = System.currentTimeMillis() - startMs;
+            double timePerCommand = 1.0 * time / count;
+            System.out.println("Executed " + count + " getSignature in " + time + "ms\n" + "Per-signature: " + timePerCommand + "ms");
+
+//        linkManager.submit(() -> {
+
+        }
+
+        {
+            BinaryProtocol bp = new BinaryProtocol(linkManager, tsStream);
+            int count = 10000;
+            long startMs = System.currentTimeMillis();
+            for (int i = 0; i < count; i++) {
+                //  boolean response = bp.requestOutputChannels();
+                SandboxCommon.runGetProtocolCommand("dd", tsStream);
+            }
+            long time = System.currentTimeMillis() - startMs;
+            double timePerCommand = 1.0 * time / count;
+            System.out.println("Executed " + count + " GetProtocol in " + time + "ms\n" + "Per-GetProtocol: " + timePerCommand + "ms");
+            //          log.info("requestOutputChannels " + response);
+            //     });
+
+
+        }
+        //        ConfigurationImage ci = SandboxCommon.readImage(tsStream, linkManager);
 
         System.exit(0);
     }
