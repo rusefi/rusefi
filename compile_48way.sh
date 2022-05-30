54e8c95731d2c28e095e92ffd1735b1b276c9fb4
sbtoonz <sbtoonz@gmail.com>
2022-05-30 09:29:50 -0700

    This shouldn't exist in master
    Signed-off-by: sbtoonz <sbtoonz@gmail.com>

--- compile_48way.sh
+++
@@ -1,8 +0,0 @@
-@echo off
-cd firmware
-make -j8 clean
-cd ..
-bash "misc/jenkins/compile_other_versions/compile.sh" 48way 48way rusefi_48way.ini
-cd firmware
-bash gen_config_board.sh 48way 48way rusefi_48way.ini
-cd ..
