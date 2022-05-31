289a01fd05fa6e8e984da8f15e846d19ec53798b
Matthew Kennedy <matthewkennedy@outlook.com>
2022-05-31 05:38:23 -0700

    actually run the new parser (#4218)
    * let the new parser run
    
    * s
    
    * ant
    
    * minor cleanups
    
    * allow passing string
    
    * tests found a genuine bug
    
    * test definitions
    
    * helper
    
    * test lots of parsing
    
    * test missing define
    
    * test typedefs, bits
    
    * fix bug with more than 32 consecutive bits overflowing a single field
    
    * s
    
    * test extra hex definitions
    
    * jar

---
+++ java_tools/configuration_definition/src/test/java/com/rusefi/test/newParse/ParseDefinitionsTest.java
@@ -0,0 +1,147 @@
+package com.rusefi.test.newParse;
+
+import com.rusefi.RusefiParseErrorStrategy;
+import com.rusefi.newparse.ParseState;
+import com.rusefi.newparse.parsing.Definition;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static com.rusefi.test.newParse.NewParseHelper.parse;
+
+
+public class ParseDefinitionsTest {
+    @Test
+    public void basicIntegerDefinition() throws IOException {
+        ParseState state = parse("#define foo 123");
+
+        Assert.assertNull(state.findDefinition("notFoo"));
+
+        Definition def = state.findDefinition("foo");
+        Assert.assertTrue(def.isNumeric());
+        Assert.assertEquals(123, def.asDouble(), 1e-5);
+
+        Definition defHex = state.findDefinition("foo_16_hex");
+        Assert.assertFalse(defHex.isNumeric());
+        Assert.assertEquals("\\x00\\x7b", defHex.value);
+    }
+
+    @Test
+    public void basicDoubleDefinition() throws IOException {
+        ParseState state = parse("#define foo 123.5");
+
+        Definition def = state.findDefinition("foo");
+
+        Assert.assertTrue(def.isNumeric());
+        Assert.assertEquals(123.5, def.asDouble(), 1e-5);
+    }
+
+    @Test
+    public void basicStringDefinition() throws IOException {
+        ParseState state = parse("#define foo \"fooValue\"");
+
+        Definition def = state.findDefinition("foo");
+
+        Assert.assertFalse(def.isNumeric());
+        Assert.assertTrue(def.value instanceof String);
+        Assert.assertEquals("\"fooValue\"", def.value);
+    }
+
+    @Test
+    public void definitionWithNumexprValue() throws IOException {
+        ParseState state = parse("#define foo 5 + 8 * 2 + 3");
+
+        Definition def = state.findDefinition("foo");
+
+        Assert.assertTrue(def.isNumeric());
+        Assert.assertEquals(24, def.asDouble(), 1e-5);
+    }
+
+    @Test
+    public void definitionWithAnotherDefinition() throws IOException {
+        ParseState state = parse(
+            "#define val1 20\n" +
+            "#define val2 val1 + 1\n" +
+            "#define val3 3 * @@val2@@");
+
+        Definition def1 = state.findDefinition("val1");
+        Assert.assertTrue(def1.isNumeric());
+        Assert.assertEquals(20, def1.asDouble(), 1e-5);
+
+        Definition def2 = state.findDefinition("val2");
+        Assert.assertTrue(def2.isNumeric());
+        Assert.assertEquals(21, def2.asDouble(), 1e-5);
+
+        Definition def3 = state.findDefinition("val3");
+        Assert.assertTrue(def3.isNumeric());
+        Assert.assertEquals(63, def3.asDouble(), 1e-5);
+    }
+
+
+    @Test(expected = RuntimeException.class)
+    public void definitionEvalMissingDefinition() throws IOException {
+        parse(
+            "#define val1 20\n" +
+            "#define val2 val1 + 1\n" +
+            "#define val3 3 * valgggggg"
+        );
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void definitionEvalNonNumeric() throws IOException {
+        parse(
+                "#define val1 \"foo\"\n" +
+                        "#define val2 val1 + 1"
+        );
+    }
+
+    @Test
+    public void definitionOverwritePolicyReplace() throws IOException {
+        ParseState state = new ParseState();
+        state.setDefinitionPolicy(Definition.OverwritePolicy.Replace);
+        RusefiParseErrorStrategy.parseDefinitionString(state.getListener(), "#define val 20");
+
+        Definition def = state.findDefinition("val");
+        Assert.assertEquals(20, def.asDouble(), 1e-5);
+
+        // Now parse another definition with the same name
+        RusefiParseErrorStrategy.parseDefinitionString(state.getListener(), "#define val 40");
+
+        // Should get back the new definition, not the old one
+        Definition def2 = state.findDefinition("val");
+        Assert.assertTrue(def != def2);
+        Assert.assertEquals(40, def2.asDouble(), 1e-5);
+    }
+
+    @Test
+    public void definitionOverwritePolicyIgnoreNew() throws IOException {
+        ParseState state = new ParseState();
+        state.setDefinitionPolicy(Definition.OverwritePolicy.IgnoreNew);
+        RusefiParseErrorStrategy.parseDefinitionString(state.getListener(), "#define val 20");
+
+        Definition def = state.findDefinition("val");
+        Assert.assertEquals(20, def.asDouble(), 1e-5);
+
+        // Now parse another definition with the same name
+        state.setDefinitionPolicy(Definition.OverwritePolicy.IgnoreNew);
+        RusefiParseErrorStrategy.parseDefinitionString(state.getListener(), "#define val 40");
+
+        // New one ignored, still returns the same old one.
+        Assert.assertEquals(def, state.findDefinition("val"));
+        Assert.assertEquals(20, def.asDouble(), 1e-5);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void definitionOverwritePolicyNotAllowed() throws IOException {
+        ParseState state = new ParseState();
+        state.setDefinitionPolicy(Definition.OverwritePolicy.NotAllowed);
+        RusefiParseErrorStrategy.parseDefinitionString(state.getListener(), "#define val 20");
+
+        Definition def = state.findDefinition("val");
+        Assert.assertEquals(20, def.asDouble(), 1e-5);
+
+        // Now parse another definition with the same name, this should throw
+        RusefiParseErrorStrategy.parseDefinitionString(state.getListener(), "#define val 40");
+    }
+}
