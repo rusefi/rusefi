cf293b22c3d9e296a81df021ad50b734e0499c09
rusefillc <sdfsdfqsf2334234234>
2022-06-03 14:04:16 -0400

    sorry you have to go for now

--- firmware/config/boards/skeleton/efifeatures.h
+++ firmware/config/boards/skeleton/efifeatures.h
@@ -30,7 +30,7 @@
 
 // Build the logic analyzer support.
 // A logic analyzer viewer is included in the java console.
-#define EFI_LOGIC_ANALYZER TRUE
+#define EFI_LOGIC_ANALYZER FALSE
 
 // A development feature to test output jitter and consistency
 #define EFI_PWM_TESTER FALSE
