2389d0d4d9356a7ac5ac9b84e1a1f031427a7e5b
GitHub build-firmware Action <action@github.com>
2022-06-08 19:22:11 +0000

    Auto-generated configs and docs

--- firmware/config/boards/hellen/cypress/config/controllers/algo/rusefi_generated.h
+++ firmware/config/boards/hellen/cypress/config/controllers/algo/rusefi_generated.h
@@ -1925,7 +1925,7 @@
 #define show_Frankenso_presets true
 #define show_test_presets true
 #define showHumanReadableWarning_offset 940
-#define SIGNATURE_HASH 1289224818
+#define SIGNATURE_HASH 627263422
 #define silentTriggerError_offset 1340
 #define skippedWheelOnCam_offset 1340
 #define slowAdcAlpha_offset 1580
@@ -2477,7 +2477,7 @@
 #define ts_show_tunerstudio_port true
 #define ts_show_vr_threshold_2 true
 #define ts_show_vr_threshold_all true
-#define TS_SIGNATURE "rusEFI 2022.06.08.hellen_cypress.1289224818"
+#define TS_SIGNATURE "rusEFI 2022.06.08.hellen_cypress.627263422"
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/signature_hellen-nb1.h
+++ firmware/controllers/generated/signature_hellen-nb1.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
 //
 
-#define SIGNATURE_HASH 3498016180
-#define TS_SIGNATURE "rusEFI 2022.06.08.hellen-nb1.3498016180"
+#define SIGNATURE_HASH 3116644472
+#define TS_SIGNATURE "rusEFI 2022.06.08.hellen-nb1.3116644472"
