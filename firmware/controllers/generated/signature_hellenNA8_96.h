b69c7196159b399f7d7bcb5509ed649565367d1f
GitHub build-firmware Action <action@github.com>
2022-06-23 18:22:18 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellenNA8_96.h
+++ firmware/controllers/generated/signature_hellenNA8_96.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
 //
 
-#define SIGNATURE_HASH 1840322044
-#define TS_SIGNATURE "rusEFI 2022.06.23.hellenNA8_96.1840322044"
+#define SIGNATURE_HASH 218621285
+#define TS_SIGNATURE "rusEFI 2022.06.23.hellenNA8_96.218621285"
