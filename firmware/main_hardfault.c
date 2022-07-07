cbdfb7ac5c1493cd6777512fa17a084f0f942506
Matthew Kennedy <matthewkennedy@outlook.com>
2022-07-07 12:14:31 -0700

    Write hard faults to backup ram, print on next boot (#4324)
    * use backup ram, hard fault handler improvement
    
    * wire it up
    
    * upload elf
    
    * s
    
    * I guess I'll just use strncpy
    
    * s
    
    * what is it even complaining about?
    
    * neat, we don't need a cast
    
    * cypress and kinetis
    
    * cleanup
    
    * ci
    
    * guard for tests

--- firmware/main_hardfault.c
+++ firmware/main_hardfault.c
@@ -17,102 +17,110 @@
 #define bkpt() __asm volatile("BKPT #0\n")
 
 void NMI_Handler(void) {
-    //TODO
-    while(1);
+	NVIC_SystemReset();
 }
 
 //See http://infocenter.arm.com/help/topic/com.arm.doc.dui0552a/BABBGBEC.html
 typedef enum  {
-    Reset = 1,
-    NMI = 2,
-    HardFault = 3,
-    MemManage = 4,
-    BusFault = 5,
-    UsageFault = 6,
+	Reset = 1,
+	NMI = 2,
+	HardFault = 3,
+	MemManage = 4,
+	BusFault = 5,
+	UsageFault = 6,
 } FaultType;
 
-void HardFault_Handler(void) {
-    //Copy to local variables (not pointers) to allow GDB "i loc" to directly show the info
-    //Get thread context. Contains main registers including PC and LR
-    struct port_extctx ctx;
-    memcpy(&ctx, (void*)__get_PSP(), sizeof(struct port_extctx));
-    (void)ctx;
-    //Interrupt status register: Which interrupt have we encountered, e.g. HardFault?
-    FaultType faultType = (FaultType)__get_IPSR();
-    (void)faultType;
-    //For HardFault/BusFault this is the address that was accessed causing the error
-    uint32_t faultAddress = SCB->BFAR;
-    (void)faultAddress;
-    //Flags about hardfault / busfault
-    //See http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0552a/Cihdjcfc.html for reference
-    bool isFaultPrecise = ((SCB->CFSR >> SCB_CFSR_BUSFAULTSR_Pos) & (1 << 1) ? true : false);
-    bool isFaultImprecise = ((SCB->CFSR >> SCB_CFSR_BUSFAULTSR_Pos) & (1 << 2) ? true : false);
-    bool isFaultOnUnstacking = ((SCB->CFSR >> SCB_CFSR_BUSFAULTSR_Pos) & (1 << 3) ? true : false);
-    bool isFaultOnStacking = ((SCB->CFSR >> SCB_CFSR_BUSFAULTSR_Pos) & (1 << 4) ? true : false);
-    bool isFaultAddressValid = ((SCB->CFSR >> SCB_CFSR_BUSFAULTSR_Pos) & (1 << 7) ? true : false);
-    (void)isFaultPrecise;
-    (void)isFaultImprecise;
-    (void)isFaultOnUnstacking;
-    (void)isFaultOnStacking;
-    (void)isFaultAddressValid;
-    //Cause debugger to stop. Ignored if no debugger is attached
-    bkpt();
-    NVIC_SystemReset();
+void logHardFault(uint32_t type, uintptr_t faultAddress, struct port_extctx* ctx, uint32_t csfr);
+
+void HardFault_Handler_C(void* sp) {
+	//Copy to local variables (not pointers) to allow GDB "i loc" to directly show the info
+	//Get thread context. Contains main registers including PC and LR
+	struct port_extctx ctx;
+	memcpy(&ctx, sp, sizeof(struct port_extctx));
+
+	//Interrupt status register: Which interrupt have we encountered, e.g. HardFault?
+	FaultType faultType = (FaultType)__get_IPSR();
+	(void)faultType;
+	//For HardFault/BusFault this is the address that was accessed causing the error
+	uint32_t faultAddress = SCB->BFAR;
+
+	//Flags about hardfault / busfault
+	//See http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0552a/Cihdjcfc.html for reference
+	bool isFaultPrecise = ((SCB->CFSR >> SCB_CFSR_BUSFAULTSR_Pos) & (1 << 1) ? true : false);
+	bool isFaultImprecise = ((SCB->CFSR >> SCB_CFSR_BUSFAULTSR_Pos) & (1 << 2) ? true : false);
+	bool isFaultOnUnstacking = ((SCB->CFSR >> SCB_CFSR_BUSFAULTSR_Pos) & (1 << 3) ? true : false);
+	bool isFaultOnStacking = ((SCB->CFSR >> SCB_CFSR_BUSFAULTSR_Pos) & (1 << 4) ? true : false);
+	bool isFaultAddressValid = ((SCB->CFSR >> SCB_CFSR_BUSFAULTSR_Pos) & (1 << 7) ? true : false);
+	(void)isFaultPrecise;
+	(void)isFaultImprecise;
+	(void)isFaultOnUnstacking;
+	(void)isFaultOnStacking;
+	(void)isFaultAddressValid;
+
+	logHardFault(faultType, faultAddress, &ctx, SCB->CFSR >> SCB_CFSR_BUSFAULTSR_Pos);
+
+	//Cause debugger to stop. Ignored if no debugger is attached
+	bkpt();
+	NVIC_SystemReset();
 }
 
-void BusFault_Handler(void) __attribute__((alias("HardFault_Handler")));
+void UsageFault_Handler_C(void* sp) {
+	//Copy to local variables (not pointers) to allow GDB "i loc" to directly show the info
+	//Get thread context. Contains main registers including PC and LR
+	struct port_extctx ctx;
+	memcpy(&ctx, sp, sizeof(struct port_extctx));
 
-void UsageFault_Handler(void) {
-    //Copy to local variables (not pointers) to allow GDB "i loc" to directly show the info
-    //Get thread context. Contains main registers including PC and LR
-    struct port_extctx ctx;
-    memcpy(&ctx, (void*)__get_PSP(), sizeof(struct port_extctx));
-    (void)ctx;
-    //Interrupt status register: Which interrupt have we encountered, e.g. HardFault?
-    FaultType faultType = (FaultType)__get_IPSR();
-    (void)faultType;
-    //Flags about hardfault / busfault
-    //See http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0552a/Cihdjcfc.html for reference
-    bool isUndefinedInstructionFault = ((SCB->CFSR >> SCB_CFSR_USGFAULTSR_Pos) & (1 << 0) ? true : false);
-    bool isEPSRUsageFault = ((SCB->CFSR >> SCB_CFSR_USGFAULTSR_Pos) & (1 << 1) ? true : false);
-    bool isInvalidPCFault = ((SCB->CFSR >> SCB_CFSR_USGFAULTSR_Pos) & (1 << 2) ? true : false);
-    bool isNoCoprocessorFault = ((SCB->CFSR >> SCB_CFSR_USGFAULTSR_Pos) & (1 << 3) ? true : false);
-    bool isUnalignedAccessFault = ((SCB->CFSR >> SCB_CFSR_USGFAULTSR_Pos) & (1 << 8) ? true : false);
-    bool isDivideByZeroFault = ((SCB->CFSR >> SCB_CFSR_USGFAULTSR_Pos) & (1 << 9) ? true : false);
-    (void)isUndefinedInstructionFault;
-    (void)isEPSRUsageFault;
-    (void)isInvalidPCFault;
-    (void)isNoCoprocessorFault;
-    (void)isUnalignedAccessFault;
-    (void)isDivideByZeroFault;
-    bkpt();
-    NVIC_SystemReset();
+	//Interrupt status register: Which interrupt have we encountered, e.g. HardFault?
+	FaultType faultType = (FaultType)__get_IPSR();
+	(void)faultType;
+	//Flags about hardfault / busfault
+	//See http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0552a/Cihdjcfc.html for reference
+	bool isUndefinedInstructionFault = ((SCB->CFSR >> SCB_CFSR_USGFAULTSR_Pos) & (1 << 0) ? true : false);
+	bool isEPSRUsageFault = ((SCB->CFSR >> SCB_CFSR_USGFAULTSR_Pos) & (1 << 1) ? true : false);
+	bool isInvalidPCFault = ((SCB->CFSR >> SCB_CFSR_USGFAULTSR_Pos) & (1 << 2) ? true : false);
+	bool isNoCoprocessorFault = ((SCB->CFSR >> SCB_CFSR_USGFAULTSR_Pos) & (1 << 3) ? true : false);
+	bool isUnalignedAccessFault = ((SCB->CFSR >> SCB_CFSR_USGFAULTSR_Pos) & (1 << 8) ? true : false);
+	bool isDivideByZeroFault = ((SCB->CFSR >> SCB_CFSR_USGFAULTSR_Pos) & (1 << 9) ? true : false);
+	(void)isUndefinedInstructionFault;
+	(void)isEPSRUsageFault;
+	(void)isInvalidPCFault;
+	(void)isNoCoprocessorFault;
+	(void)isUnalignedAccessFault;
+	(void)isDivideByZeroFault;
+
+	logHardFault(faultType, 0, &ctx, SCB->CFSR);
+
+	bkpt();
+	NVIC_SystemReset();
 }
 
-void MemManage_Handler(void) {
-    //Copy to local variables (not pointers) to allow GDB "i loc" to directly show the info
-    //Get thread context. Contains main registers including PC and LR
-    struct port_extctx ctx;
-    memcpy(&ctx, (void*)__get_PSP(), sizeof(struct port_extctx));
-    (void)ctx;
-    //Interrupt status register: Which interrupt have we encountered, e.g. HardFault?
-    FaultType faultType = (FaultType)__get_IPSR();
-    (void)faultType;
-    //For HardFault/BusFault this is the address that was accessed causing the error
-    uint32_t faultAddress = SCB->MMFAR;
-    (void)faultAddress;
-    //Flags about hardfault / busfault
-    //See http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0552a/Cihdjcfc.html for reference
-    bool isInstructionAccessViolation = ((SCB->CFSR >> SCB_CFSR_MEMFAULTSR_Pos) & (1 << 0) ? true : false);
-    bool isDataAccessViolation = ((SCB->CFSR >> SCB_CFSR_MEMFAULTSR_Pos) & (1 << 1) ? true : false);
-    bool isExceptionUnstackingFault = ((SCB->CFSR >> SCB_CFSR_MEMFAULTSR_Pos) & (1 << 3) ? true : false);
-    bool isExceptionStackingFault = ((SCB->CFSR >> SCB_CFSR_MEMFAULTSR_Pos) & (1 << 4) ? true : false);
-    bool isFaultAddressValid = ((SCB->CFSR >> SCB_CFSR_MEMFAULTSR_Pos) & (1 << 7) ? true : false);
-    (void)isInstructionAccessViolation;
-    (void)isDataAccessViolation;
-    (void)isExceptionUnstackingFault;
-    (void)isExceptionStackingFault;
-    (void)isFaultAddressValid;
-    bkpt();
-    NVIC_SystemReset();
+void MemManage_Handler_C(void* sp) {
+	//Copy to local variables (not pointers) to allow GDB "i loc" to directly show the info
+	//Get thread context. Contains main registers including PC and LR
+	struct port_extctx ctx;
+	memcpy(&ctx, sp, sizeof(struct port_extctx));
+
+	//Interrupt status register: Which interrupt have we encountered, e.g. HardFault?
+	FaultType faultType = (FaultType)__get_IPSR();
+	(void)faultType;
+	//For HardFault/BusFault this is the address that was accessed causing the error
+	uint32_t faultAddress = SCB->MMFAR;
+
+	//Flags about hardfault / busfault
+	//See http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0552a/Cihdjcfc.html for reference
+	bool isInstructionAccessViolation = ((SCB->CFSR >> SCB_CFSR_MEMFAULTSR_Pos) & (1 << 0) ? true : false);
+	bool isDataAccessViolation = ((SCB->CFSR >> SCB_CFSR_MEMFAULTSR_Pos) & (1 << 1) ? true : false);
+	bool isExceptionUnstackingFault = ((SCB->CFSR >> SCB_CFSR_MEMFAULTSR_Pos) & (1 << 3) ? true : false);
+	bool isExceptionStackingFault = ((SCB->CFSR >> SCB_CFSR_MEMFAULTSR_Pos) & (1 << 4) ? true : false);
+	bool isFaultAddressValid = ((SCB->CFSR >> SCB_CFSR_MEMFAULTSR_Pos) & (1 << 7) ? true : false);
+	(void)isInstructionAccessViolation;
+	(void)isDataAccessViolation;
+	(void)isExceptionUnstackingFault;
+	(void)isExceptionStackingFault;
+	(void)isFaultAddressValid;
+
+	logHardFault(faultType, faultAddress, &ctx, SCB->CFSR);
+
+	bkpt();
+	NVIC_SystemReset();
 }
