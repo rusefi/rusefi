578faa0d41e3b7e6dbb8cb95b91acafdf860b535
rusefillc <sdfsdfqsf2334234234>
2022-07-08 19:33:49 -0400

    ETB to overrides, fixing 88

--- firmware/config/boards/hellen/hellen88bmw/board_configuration.cpp
+++ firmware/config/boards/hellen/hellen88bmw/board_configuration.cpp
@@ -89,6 +89,12 @@ void setBoardConfigOverrides() {
 	setHellen144LedPins();
 	setupVbatt();
 
+    // hellen88bmw uses L6205PD
+	engineConfiguration->etbIo[0].directionPin1 = H144_OUT_PWM2; // ETB+
+	engineConfiguration->etbIo[0].directionPin2 = H144_OUT_PWM3; // ETB-
+	engineConfiguration->etbIo[0].controlPin = H144_OUT_PWM1; // ETB_EN
+	engineConfiguration->etb_use_two_wires = true;
+
 	setHellenSdCardSpi3();
 
 	engineConfiguration->clt.config.bias_resistor = 4700;
