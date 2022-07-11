25cc8ed0dfc1ec067c0b642e1b68725493f418f9
rusefillc <48498823+rusefillc@users.noreply.github.com>
2022-07-11 04:30:49 -0400

    STM32_HSE_BYPASS where we need it (#4338)
    Co-authored-by: rusefillc <sdfsdfqsf2334234234>

--- firmware/config/boards/nucleo_f767/compile_stm32f767_nucleo.sh
+++ firmware/config/boards/nucleo_f767/compile_stm32f767_nucleo.sh
@@ -5,8 +5,11 @@
 SCRIPT_NAME="compile_nucleo_f767.sh"
 echo "Entering $SCRIPT_NAME"
 
+# Nucleo boards use MCO signal from St-Link and NOT oscillator - these need STM32_HSE_BYPASS
+
 export EXTRA_PARAMS="-DDUMMY -DSTM32F767xx \
  -DEFI_INJECTOR_PIN3=Gpio::Unassigned \
+ -DSTM32_HSE_BYPASS=TRUE \
  -DFIRMWARE_ID=\\\"nucleo767\\\" \
  -DEFI_COMMUNICATION_PIN=Gpio::B7 \
  -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::B14 \
