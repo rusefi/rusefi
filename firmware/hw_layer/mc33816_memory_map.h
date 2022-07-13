5e929f027d6d837bff93231a0248723ce40ace35
Matthew Kennedy <matthewkennedy@outlook.com>
2022-07-13 16:25:21 -0700

    switch to use generated C source instead of copy/paste

--- firmware/hw_layer/mc33816_memory_map.h
+++ firmware/hw_layer/mc33816_memory_map.h
@@ -2,19 +2,21 @@
  * see mc33816/rusefi/readme.md
 */
 
+#include "mc33816/rusefi/sample_code/PT2001_dram.h"
+
 typedef enum {
     // see dram1.def values
-    Iboost = 0,
-    Ipeak = 1,
-    Ihold = 2,
-    Tpeak_off = 3,
-    Tpeak_tot = 4,
-    Tbypass = 5,
-    Thold_off = 6,
-    Thold_tot = 7,
+    Iboost = PT2001_D1_Iboost,
+    Ipeak = PT2001_D1_Ipeak,
+    Ihold = PT2001_D1_Ihold,
+    Tpeak_off = PT2001_D1_Tpeak_off,
+    Tpeak_tot = PT2001_D1_Tpeak_tot,
+    Tbypass = PT2001_D1_Tbypass,
+    Thold_off = PT2001_D1_Thold_off,
+    Thold_tot = PT2001_D1_Thold_tot,
     // see dram2.def values, base 64 for channel 2
-    Vboost_high = 64,
-    Vboost_low = 65,
+    Vboost_high = PT2001_D2_Vboost_high,
+    Vboost_low = PT2001_D2_Vboost_low,
     Isense4_high = 66,
     Isense4_low = 67,
     HPFP_Ipeak = 69,
