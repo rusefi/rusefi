edee5e77d43700f11920b75c9ebd778b0b430952
Matthew Kennedy <matthewkennedy@outlook.com>
2022-07-13 16:34:03 -0700

    more memory map

--- firmware/hw_layer/mc33816_memory_map.h
+++ firmware/hw_layer/mc33816_memory_map.h
@@ -17,10 +17,10 @@ typedef enum {
     // see dram2.def values, base 64 for channel 2
     Vboost_high = PT2001_D2_Vboost_high,
     Vboost_low = PT2001_D2_Vboost_low,
-    Isense4_high = 66,
-    Isense4_low = 67,
-    HPFP_Ipeak = 69,
-    HPFP_Ihold = 70,
-    HPFP_Thold_off = 71,
-    HPFP_Thold_tot = 72,
+    Isense4_high = PT2001_D2_Isense4_high,
+    Isense4_low = PT2001_D2_Isense4_low,
+    HPFP_Ipeak = PT2001_D2_PCV_Ipeak,
+    HPFP_Ihold = PT2001_D2_PCV_Ihold,
+    HPFP_Thold_off = PT2001_D2_PCV_Thold_off,
+    HPFP_Thold_tot = PT2001_D2_PCV_Thold_tot,
 } MC33816Mem;
