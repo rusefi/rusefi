f9f33da641a0f36172e65bc05627be6dc8a64150
GitHub build-firmware Action <action@github.com>
2022-07-19 08:17:08 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_alphax-2chan.h
+++ firmware/controllers/generated/signature_alphax-2chan.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 3925271431
-#define TS_SIGNATURE "rusEFI 2022.07.18.alphax-2chan.3925271431"
+#define TS_SIGNATURE "rusEFI 2022.07.19.alphax-2chan.3925271431"
--- firmware/controllers/generated/signature_frankenso_na6.h
+++ firmware/controllers/generated/signature_frankenso_na6.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 2575860271
-#define TS_SIGNATURE "rusEFI 2022.07.18.frankenso_na6.2575860271"
+#define TS_SIGNATURE "rusEFI 2022.07.19.frankenso_na6.2575860271"
