64a5536d41d286e801e11540b3dad3f9cb927be6
GitHub build-firmware Action <action@github.com>
2022-07-20 21:19:50 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen-nb1.h
+++ firmware/controllers/generated/signature_hellen-nb1.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 40849191
-#define TS_SIGNATURE "rusEFI 2022.07.19.hellen-nb1.40849191"
+#define TS_SIGNATURE "rusEFI 2022.07.20.hellen-nb1.40849191"
--- firmware/controllers/generated/signature_hellen121nissan.h
+++ firmware/controllers/generated/signature_hellen121nissan.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 1396360705
-#define TS_SIGNATURE "rusEFI 2022.07.19.hellen121nissan.1396360705"
+#define TS_SIGNATURE "rusEFI 2022.07.20.hellen121nissan.1396360705"
