2d1fb25ab44712cc4763d55284064543dddb6f17
GitHub build-firmware Action <action@github.com>
2022-07-21 19:34:19 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_48way.h
+++ firmware/controllers/generated/signature_48way.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
 //
 
-#define SIGNATURE_HASH 2318799270
-#define TS_SIGNATURE "rusEFI 2022.07.21.48way.2318799270"
+#define SIGNATURE_HASH 4082769834
+#define TS_SIGNATURE "rusEFI 2022.07.21.48way.4082769834"
--- firmware/controllers/generated/signature_all.h
+++ firmware/controllers/generated/signature_all.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
 //
 
-#define SIGNATURE_HASH 2635143657
-#define TS_SIGNATURE "rusEFI 2022.07.21.all.2635143657"
+#define SIGNATURE_HASH 3833403365
+#define TS_SIGNATURE "rusEFI 2022.07.21.all.3833403365"
