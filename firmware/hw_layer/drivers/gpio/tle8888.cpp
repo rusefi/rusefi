eb442998dc60a6da9144de34b54b6a793c4a583e
Matthew Kennedy <matthewkennedy@outlook.com>
2022-08-02 09:19:50 -0700

    misc fixes (#4411)
    * fix warnings
    
    * break to debugger in case of panic
    
    * move the check
    
    * s

--- firmware/hw_layer/drivers/gpio/tle8888.cpp
+++ firmware/hw_layer/drivers/gpio/tle8888.cpp
@@ -399,6 +399,10 @@ int Tle8888::spi_rw_array(const uint16_t *tx, uint16_t *rx, int n)
 	uint16_t rxdata;
 	SPIDriver *spi = cfg->spi_bus;
 
+	if (n <= 0) {
+		return -2;
+	}
+
 	/**
 	 * 15.1 SPI Protocol
 	 *
