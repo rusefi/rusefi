5fc700083497c5454c3449c627f85d0cd769b0dc
Matthew Kennedy <matthewkennedy@outlook.com>
2022-08-08 05:53:08 -0700

    Pt2001 module (#4396)
    * pt2001 uses module
    
    * read flag0
    
    * c
    
    * libfirmware
    
    * makefiles
    
    * are unit tests happy?
    
    * bootloader

--- firmware/hw_layer/mc33816.h
+++ firmware/hw_layer/mc33816.h
@@ -7,21 +7,4 @@
 
 #pragma once
 
-const int MC_CK = 6; // PLL x24 / CLK_DIV 4 = 6Mhz
-
-const int MAX_SPI_MODE_A_TRANSFER_SIZE = 31;  //max size for register config transfer
-
-enum {
-	CODE_RAM1,
-	CODE_RAM2,
-	DATA_RAM
-};
-enum {
-	REG_MAIN,
-	REG_CH1,
-	REG_CH2,
-	REG_IO,
-	REG_DIAG
-};
-
 void initMc33816();
