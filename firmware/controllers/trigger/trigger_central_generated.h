006190aa152f5658b858a5d34deba8c05d9e5259
GitHub build-firmware Action <action@github.com>
2022-08-15 19:03:46 +0000

    Auto-generated configs and docs

--- firmware/controllers/trigger/trigger_central_generated.h
+++ firmware/controllers/trigger/trigger_central_generated.h
@@ -1,4 +1,4 @@
-// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/trigger/trigger_central.txt Mon Aug 15 18:43:34 UTC 2022
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/trigger/trigger_central.txt Mon Aug 15 19:03:34 UTC 2022
 // by class com.rusefi.output.CHeaderConsumer
 // begin
 #pragma once
@@ -51,4 +51,4 @@ struct trigger_central_s {
 static_assert(sizeof(trigger_central_s) == 40);
 
 // end
-// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/trigger/trigger_central.txt Mon Aug 15 18:43:34 UTC 2022
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/trigger/trigger_central.txt Mon Aug 15 19:03:34 UTC 2022
