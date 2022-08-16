02fe0e6e142cc440d6989049db9ed58d26d0a059
rusefillc <sdfsdfqsf2334234234>
2022-08-16 10:25:40 -0400

    reducing inconsistency, renaming .ini file

--- firmware/config/boards/kinetis/config/gen_kinetis_config.sh
+++ firmware/config/boards/kinetis/config/gen_kinetis_config.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # This script files reads rusefi_config.txt and produces firmware persistent configuration headers
 # the storage section of rusefi.ini is updated as well
-#
-# many of the files generated here require time consuming full compilation thus we have an aggressive caching mechanism
-# to make sure that we do not regenerate for no reason
-# the primary input files are rusefi_config.txt and rusefi.input, also mapping.yaml etc
-# see inside cache.zip for all input files
-#
 
-pwd
+BOARDNAME=kinetis
+SHORT_BOARDNAME=kin
+BOARD_DIR=config/boards/${BOARDNAME}
 
-bash gen_signature.sh kin
+
+
+
+
+
+
+bash gen_signature.sh ${SHORT_BOARDNAME}
 
 source gen_config_common.sh
 echo "Using COMMON_GEN_CONFIG [$COMMON_GEN_CONFIG]"
@@ -20,13 +22,13 @@ java \
  $COMMON_GEN_CONFIG_PREFIX \
  -tool kinetis_gen_config.bat \
  $COMMON_GEN_CONFIG \
- -ts_output_name generated/rusefi_kinetis.ini \
- -c_defines config/boards/kinetis/config/controllers/algo/rusefi_generated.h \
- -c_destination config/boards/kinetis/config/controllers/algo/engine_configuration_generated_structures.h \
- -signature tunerstudio/generated/signature_kin.txt \
- -signature_destination controllers/generated/signature_kin.h \
+ -ts_output_name generated/rusefi_${SHORT_BOARDNAME}.ini \
+ -c_defines ${BOARD_DIR}/config/controllers/algo/rusefi_generated.h \
+ -c_destination ${BOARD_DIR}/config/controllers/algo/engine_configuration_generated_structures.h \
+ -signature tunerstudio/generated/signature_${SHORT_BOARDNAME}.txt \
+ -signature_destination controllers/generated/signature_${SHORT_BOARDNAME}.h \
  -enumInputFile controllers/algo/rusefi_hw_enums.h \
- -board kinetis \
+ -board ${BOARDNAME} \
  -prepend config/boards/kinetis/config/rusefi_config_kinetis.txt
 
-[ $? -eq 0 ] || { echo "ERROR generating TunerStudio config for kinetis"; exit 1; }
+[ $? -eq 0 ] || { echo "ERROR generating TunerStudio config for ${BOARDNAME}"; exit 1; }
