88a501a11fe81e4c583c0297ca1ef3f68e71c0e5
GitHub build-firmware Action <action@github.com>
2022-08-16 21:17:44 +0000

    Auto-generated configs and docs

--- firmware/config/boards/subaru_eg33/config/controllers/algo/rusefi_generated.h
+++ firmware/config/boards/subaru_eg33/config/controllers/algo/rusefi_generated.h
@@ -1912,7 +1912,7 @@
 #define show_Proteus_presets false
 #define show_test_presets false
 #define showHumanReadableWarning_offset 960
-#define SIGNATURE_HASH 4114004825
+#define SIGNATURE_HASH 2954556755
 #define silentTriggerError_offset 1360
 #define skippedWheelOnCam_offset 1360
 #define slowAdcAlpha_offset 1600
@@ -2468,7 +2468,7 @@
 #define ts_show_vbatt true
 #define ts_show_vr_threshold_2 true
 #define ts_show_vr_threshold_all true
-#define TS_SIGNATURE "rusEFI 2022.08.16.subaru_eg33_f7.4114004825"
+#define TS_SIGNATURE "rusEFI 2022.08.16.subaru_eg33_f7.2954556755"
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/signature_cypress.h
+++ firmware/controllers/generated/signature_cypress.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on hellen_cypress_gen_config.bat null
 //
 
-#define SIGNATURE_HASH 3138063585
-#define TS_SIGNATURE "rusEFI 2022.08.16.cypress.3138063585"
+#define SIGNATURE_HASH 4263985899
+#define TS_SIGNATURE "rusEFI 2022.08.16.cypress.4263985899"
