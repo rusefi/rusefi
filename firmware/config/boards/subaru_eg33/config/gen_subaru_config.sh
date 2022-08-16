e2c1793e08cba70aad91b3e70662977ca66dfd28
rusefillc <sdfsdfqsf2334234234>
2022-08-16 16:41:33 -0400

    reducing inconsistency between gen scripts

--- firmware/config/boards/subaru_eg33/config/gen_subaru_config.sh
+++ firmware/config/boards/subaru_eg33/config/gen_subaru_config.sh
@@ -37,8 +37,7 @@ java \
  $COMMON_GEN_CONFIG \
  -c_defines ${BOARD_DIR}/config/controllers/algo/rusefi_generated.h \
  -c_destination ${BOARD_DIR}/config/controllers/algo/engine_configuration_generated_structures.h \
- -enumInputFile ${BOARD_DIR}/rusefi_hw_enums.h \
- -prepend ${BOARD_DIR}/prepend.txt
+ -enumInputFile ${BOARD_DIR}/rusefi_hw_enums.h
 
 [ $? -eq 0 ] || { echo "ERROR generating TunerStudio config for ${BOARDNAME}"; exit 1; }
 
