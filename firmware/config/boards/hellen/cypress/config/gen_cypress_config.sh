adadb330c83a8a35c4e369c555efbfdfefe6bf76
rusefillc <sdfsdfqsf2334234234>
2022-08-16 10:23:52 -0400

    reducing inconsistency, renaming signature header

--- firmware/config/boards/hellen/cypress/config/gen_cypress_config.sh
+++ firmware/config/boards/hellen/cypress/config/gen_cypress_config.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # This script files reads rusefi_config.txt and produces firmware persistent configuration headers
 # the storage section of rusefi.ini is updated as well
-#
-# many of the files generated here require time consuming full compilation thus we have an aggressive caching mechanism
-# to make sure that we do not regenerate for no reason
-# the primary input files are rusefi_config.txt and rusefi.input, also mapping.yaml etc
-# see inside cache.zip for all input files
-#
 
-pwd
+BOARDNAME=cypress
+SHORT_BOARDNAME=cypress
+BOARD_DIR=config/boards/hellen/${BOARDNAME}
 
-bash gen_signature.sh hellen_cypress
+
+
+
+
+
+
+bash gen_signature.sh ${SHORT_BOARDNAME}
 
 source gen_config_common.sh
 echo "Using COMMON_GEN_CONFIG [$COMMON_GEN_CONFIG]"
@@ -21,12 +23,12 @@ java \
  -tool hellen_cypress_gen_config.bat \
  $COMMON_GEN_CONFIG \
  -ts_output_name generated/rusefi_hellen_cypress.ini \
- -c_defines config/boards/hellen/cypress/config/controllers/algo/rusefi_generated.h \
- -c_destination config/boards/hellen/cypress/config/controllers/algo/engine_configuration_generated_structures.h \
- -signature tunerstudio/generated/signature_hellen_cypress.txt \
- -signature_destination controllers/generated/signature_hellen_cypress.h \
+ -c_defines ${BOARD_DIR}/config/controllers/algo/rusefi_generated.h \
+ -c_destination ${BOARD_DIR}/config/controllers/algo/engine_configuration_generated_structures.h \
+ -signature tunerstudio/generated/signature_${SHORT_BOARDNAME}.txt \
+ -signature_destination controllers/generated/signature_${SHORT_BOARDNAME}.h \
  -enumInputFile controllers/algo/rusefi_hw_enums.h \
  -board hellen_cypress \
- -prepend config/boards/hellen/cypress/config/tunerstudio/generated/hellen_cypress_prefix.txt
+ -prepend ${BOARD_DIR}/config/tunerstudio/generated/hellen_cypress_prefix.txt
 
-[ $? -eq 0 ] || { echo "ERROR generating TunerStudio config for hellen_cypress"; exit 1; }
+[ $? -eq 0 ] || { echo "ERROR generating TunerStudio config for ${BOARDNAME}"; exit 1; }
