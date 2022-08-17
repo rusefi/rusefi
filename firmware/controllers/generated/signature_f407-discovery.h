4a415e5f136de2965da85bb3f2ebc5483273cd81
GitHub gen-configs Action <action@github.com>
2022-08-17 01:17:01 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_f407-discovery.h
+++ firmware/controllers/generated/signature_f407-discovery.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
 //
 
-#define SIGNATURE_HASH 4263985899
-#define TS_SIGNATURE "rusEFI 2022.08.17.f407-discovery.4263985899"
+#define SIGNATURE_HASH 1028802779
+#define TS_SIGNATURE "rusEFI 2022.08.17.f407-discovery.1028802779"
