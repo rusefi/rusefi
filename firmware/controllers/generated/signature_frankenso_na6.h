36ce8220666574dfdb4712dd5360ba6538864155
GitHub gen-configs Action <action@github.com>
2022-08-23 02:57:26 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_frankenso_na6.h
+++ firmware/controllers/generated/signature_frankenso_na6.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
 //
 
-#define SIGNATURE_HASH 1915760821
-#define TS_SIGNATURE "rusEFI 2022.08.23.frankenso_na6.1915760821"
+#define SIGNATURE_HASH 3772245908
+#define TS_SIGNATURE "rusEFI 2022.08.23.frankenso_na6.3772245908"
--- firmware/controllers/generated/signature_proteus_f7.h
+++ firmware/controllers/generated/signature_proteus_f7.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh null
 //
 
-#define SIGNATURE_HASH 2509310727
-#define TS_SIGNATURE "rusEFI 2022.08.23.proteus_f7.2509310727"
+#define SIGNATURE_HASH 125230118
+#define TS_SIGNATURE "rusEFI 2022.08.23.proteus_f7.125230118"
