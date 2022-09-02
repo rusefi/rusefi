48f960b5812aca5cceb978b751309bd84cbc3b12
Andrey <rusefillc@gmail.com>
2022-09-01 20:40:38 -0400

    Revert "tear down Engine god object #4511"
    This reverts commit dc60837e4c658bfae82df8923422c50a415537bc.

--- firmware/controllers/algo/fuel/dfco.h
+++ firmware/controllers/algo/fuel/dfco.h
@@ -3,6 +3,8 @@
  */
 
 #pragma once
+#include "engine_module.h"
+#include "timer.h"
 
 // DFCO = deceleration fuel cut off, ie, save gas when your foot is off the pedal
 class DfcoController : public EngineModule {
