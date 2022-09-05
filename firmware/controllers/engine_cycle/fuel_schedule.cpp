9a9b33401caa2b35a6c4deb2b89001db113bbca1
Andrey <rusefillc@gmail.com>
2022-09-05 00:48:38 -0400

    refactoring: splitting header where cpp was already split

--- firmware/controllers/engine_cycle/fuel_schedule.cpp
+++ firmware/controllers/engine_cycle/fuel_schedule.cpp
@@ -48,6 +48,10 @@ static float getInjectionAngleCorrection(float fuelMs, float oneDegreeUs) {
 	}
 }
 
+InjectionEvent::InjectionEvent() {
+	memset(outputs, 0, sizeof(outputs));
+}
+
 // Returns the start angle of this injector in engine coordinates (0-720 for a 4 stroke),
 // or unexpected if unable to calculate the start angle due to missing information.
 expected<float> InjectionEvent::computeInjectionAngle(int cylinderIndex) const {
