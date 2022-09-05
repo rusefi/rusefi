a7af8ecec9e4358ddea7949b2ff3e5a33fb72b55
rusefillc <48498823+rusefillc@users.noreply.github.com>
2022-09-05 19:49:27 -0400

    Engine Sniffer into TS data logs fix #4535 (#4537)

--- firmware/controllers/system/efi_gpio.cpp
+++ firmware/controllers/system/efi_gpio.cpp
@@ -130,6 +130,7 @@ EnginePins::EnginePins() :
 	static_assert(efi::size(trailNames) >= MAX_CYLINDER_COUNT, "Too many ignition pins");
 	static_assert(efi::size(injectorNames) >= MAX_CYLINDER_COUNT, "Too many injection pins");
 	for (int i = 0; i < MAX_CYLINDER_COUNT;i++) {
+		enginePins.coils[i].coilIndex = i;
 		enginePins.coils[i].name = sparkNames[i];
 		enginePins.coils[i].shortName = sparkShortNames[i];
 
@@ -374,6 +375,44 @@ void InjectorOutputPin::reset() {
 
 IgnitionOutputPin::IgnitionOutputPin() {
 	reset();
+}
+
+void IgnitionOutputPin::setHigh() {
+	NamedOutputPin::setHigh();
+	// this is NASTY but what's the better option? bytes? At cost of 22 extra bytes in output status packet?
+	switch (coilIndex) {
+	case 0:
+		engine->outputChannels.coilState1 = true;
+		break;
+	case 1:
+		engine->outputChannels.coilState2 = true;
+		break;
+	case 2:
+		engine->outputChannels.coilState3 = true;
+		break;
+	case 3:
+		engine->outputChannels.coilState4 = true;
+		break;
+	}
+}
+
+void IgnitionOutputPin::setLow() {
+	NamedOutputPin::setLow();
+	// this is NASTY but what's the better option? bytes? At cost of 22 extra bytes in output status packet?
+	switch (coilIndex) {
+	case 0:
+		engine->outputChannels.coilState1 = false;
+		break;
+	case 1:
+		engine->outputChannels.coilState2 = false;
+		break;
+	case 2:
+		engine->outputChannels.coilState3 = false;
+		break;
+	case 3:
+		engine->outputChannels.coilState4 = false;
+		break;
+	}
 }
 
 void IgnitionOutputPin::reset() {
