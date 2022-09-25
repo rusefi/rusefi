cff779dd78d514dcadd90a6f96cd928e9da9da0b
Andrey <rusefillc@gmail.com>
2022-09-24 23:18:57 -0400

    K progress

--- unit_tests/tests/trigger/test_real_k20.cpp
+++ unit_tests/tests/trigger/test_real_k20.cpp
@@ -5,13 +5,15 @@
 TEST(realk20, cranking) {
 	CsvReader reader(/* triggerCount */ 1, /* vvtCount */ 2);
 
-	reader.open("tests/trigger/resources/civic-K20-cranking.csv");
+	reader.open("tests/trigger/resources/civic-K20-cranking.csv", NORMAL_ORDER, REVERSE_ORDER);
 
 	EngineTestHelper eth (PROTEUS_HONDA_ELEMENT_2003);
 
 	while (reader.haveMore()) {
-// todo		reader.processLine(&eth);
+		reader.processLine(&eth);
 	}
+
+	ASSERT_EQ(84, round(Sensor::getOrZero(SensorType::Rpm)));
 
 
 }
