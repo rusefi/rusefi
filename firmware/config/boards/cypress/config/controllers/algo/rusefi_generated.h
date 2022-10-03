6aa8d4cb0bbd860f4283f964f01396d5040c743a
GitHub gen-configs Action <action@github.com>
2022-10-03 04:25:05 +0000

    Auto-generated configs and docs

--- firmware/config/boards/cypress/config/controllers/algo/rusefi_generated.h
+++ firmware/config/boards/cypress/config/controllers/algo/rusefi_generated.h
@@ -2526,7 +2526,7 @@
 #define ts_show_vbatt true
 #define ts_show_vr_threshold_2 true
 #define ts_show_vr_threshold_all true
-#define TS_SIGNATURE "rusEFI 2022.10.02.cypress.2717408623"
+#define TS_SIGNATURE "rusEFI 2022.10.03.cypress.2717408623"
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't'
