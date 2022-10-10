9d132676fdaf0a3f6517a7a6b0585f8ed94a48a2
Matthew Kennedy <matthewkennedy@outlook.com>
2022-10-10 04:44:20 -0700

    consolidate analog vs. temp info commands (#4646)
    * consolidate analog vs. temp info commands
    
    * cleanup
    
    * guard

--- java_console/ui/src/main/java/com/rusefi/ui/RecentCommands.java
+++ java_console/ui/src/main/java/com/rusefi/ui/RecentCommands.java
@@ -36,7 +36,7 @@ public class RecentCommands {
     private static final String STOPENGINE = "stopengine";
     private static final String SHOWCONFIG = "showconfig";
     private static final String HELP = "help";
-    private static final String ANALOGINFO = "analoginfo";
+    private static final String SENSORINFO = "sensorinfo";
     private static final String IDLEINFO = "idleinfo";
     private static final String ALTINFO = "altinfo";
     private static final String TPSINFO = "tpsinfo";
@@ -47,7 +47,6 @@ public class RecentCommands {
     private static final String SPEEDINFO = "speedinfo";
     private static final String joystickINFO = "joystickinfo";
     private static final String FUELINFO = "fuelinfo";
-    private static final String TEMPINFO = "tempinfo";
     private static final String HIPINFO = "hipinfo";
     private static final String SDINFO = "sdinfo";
     private static final String FSIOINFO = "fsioinfo";
@@ -60,7 +59,7 @@ public class RecentCommands {
         ImageIcon infoIcon = AutoupdateUtil.loadIcon("info.png");
         COMMAND_ICONS.put(SHOWCONFIG, infoIcon);
         COMMAND_ICONS.put(HELP, AutoupdateUtil.loadIcon("help.jpg"));
-        COMMAND_ICONS.put(ANALOGINFO, infoIcon);
+        COMMAND_ICONS.put(SENSORINFO, infoIcon);
         COMMAND_ICONS.put(CMD_TRIGGERINFO, AutoupdateUtil.loadIcon("trigger.jpg"));
         COMMAND_ICONS.put(IDLEINFO, infoIcon);
         COMMAND_ICONS.put(ALTINFO, AutoupdateUtil.loadIcon("alternator.jpg"));
@@ -71,7 +70,6 @@ public class RecentCommands {
         COMMAND_ICONS.put(joystickINFO, AutoupdateUtil.loadIcon("joystick.png"));
         COMMAND_ICONS.put(CANINFO, infoIcon);
         COMMAND_ICONS.put(FUELINFO, infoIcon);
-        COMMAND_ICONS.put(TEMPINFO, infoIcon);
         COMMAND_ICONS.put(HIPINFO, AutoupdateUtil.loadIcon("knock.jpg"));
         COMMAND_ICONS.put(SDINFO, AutoupdateUtil.loadIcon("sdinfo.jpg"));
         COMMAND_ICONS.put(FSIOINFO, infoIcon);
@@ -121,7 +119,7 @@ public class RecentCommands {
         add(HELP);
         add(SHOWCONFIG);
         add(STOPENGINE);
-        add(ANALOGINFO);
+        add(SENSORINFO);
         add(CMD_TRIGGERINFO);
         add(TSINFO);
         add(SPEEDINFO);
@@ -149,7 +147,6 @@ public class RecentCommands {
         add(MAPINFO);
         add(ACCELINFO);
         add(FUELINFO);
-        add(TEMPINFO);
         add(HIPINFO);
         add(SDINFO);
         add(FSIOINFO);
