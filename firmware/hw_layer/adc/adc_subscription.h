a13445478f497affc56a4c63574e4a7b825c1582
Matthew Kennedy <matthewkennedy@outlook.com>
2022-10-10 04:44:20 -0700

    consolidate analog vs. temp info commands (#4646)
    * consolidate analog vs. temp info commands
    
    * cleanup
    
    * guard

--- firmware/hw_layer/adc/adc_subscription.h
+++ firmware/hw_layer/adc/adc_subscription.h
@@ -12,4 +12,6 @@ public:
 	static void SubscribeSensor(FunctionalSensor &sensor, adc_channel_e channel, float lowpassCutoff, float voltsPerAdcVolt = 0.0f);
 	static void UnsubscribeSensor(FunctionalSensor& sensor);
 	static void UpdateSubscribers(efitick_t nowNt);
+
+	static void PrintInfo();
 };
