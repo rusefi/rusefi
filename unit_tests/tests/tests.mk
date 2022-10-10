aa881efe8956bdc41baef76246b59c1903461c25
Matthew Kennedy <matthewkennedy@outlook.com>
2022-10-09 18:22:05 -0700

    fix 36-2-1 trigger (#4645)
    * fix
    
    * test files
    
    * test real 4b11
    
    * test.mk
    
    * better angle error math
    
    * test GM too
    
    * changelog

--- unit_tests/tests/tests.mk
+++ unit_tests/tests/tests.mk
@@ -13,6 +13,7 @@ TESTS_SRC_CPP = \
 	tests/trigger/test_real_cranking_miata_na6.cpp \
 	tests/trigger/test_real_cranking_nissan_vq40.cpp \
 	tests/trigger/test_trigger_skipped_wheel.cpp \
+	tests/trigger/test_real_4b11.cpp \
 	tests/trigger/test_real_volkswagen.cpp \
 	tests/trigger/test_real_nb2_cranking.cpp \
 	tests/trigger/test_real_gm_24x.cpp \
