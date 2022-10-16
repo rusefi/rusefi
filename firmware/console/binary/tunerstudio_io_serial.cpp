d4d2aaea058695051d5c0711f3d1cd7584c3cebd
rusefillc <sdfsdfqsf2334234234>
2022-10-16 01:21:44 -0400

    /*isEndOfPacket*/

--- firmware/console/binary/tunerstudio_io_serial.cpp
+++ firmware/console/binary/tunerstudio_io_serial.cpp
@@ -23,7 +23,7 @@ void SerialTsChannel::stop() {
 	sdStop(m_driver);
 }
 
-void SerialTsChannel::write(const uint8_t* buffer, size_t size, bool) {
+void SerialTsChannel::write(const uint8_t* buffer, size_t size, bool /*isEndOfPacket*/) {
 	chnWriteTimeout(m_driver, buffer, size, BINARY_IO_TIMEOUT);
 }
 
