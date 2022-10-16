8389d35821d20f2135987fd6f1cceedce3e6c9d7
rusefillc <sdfsdfqsf2334234234>
2022-10-16 00:12:19 -0400

    only refactoring

--- java_console/io/src/test/java/com/rusefi/binaryprotocol/test/TcpServerSandbox.java
+++ java_console/io/src/test/java/com/rusefi/binaryprotocol/test/TcpServerSandbox.java
@@ -25,62 +25,64 @@ import static com.rusefi.io.tcp.BinaryProtocolServer.TS_OK;
  */
 public class TcpServerSandbox {
     public static void main(String[] args) throws IOException {
-        BinaryProtocolServer.tcpServerSocket(29001,
-                "server",
-                new CompatibleFunction<Socket, Runnable>() {
+        Listener serverSocketCreationCallback = parameter -> System.out.println("serverSocketCreationCallback");
+        CompatibleFunction<Socket, Runnable> socketRunnableFactory = new CompatibleFunction<Socket, Runnable>() {
+            @Override
+            public Runnable apply(Socket socket) {
+                System.out.println("New connection!");
+
+                return new Runnable() {
                     @Override
-                    public Runnable apply(Socket socket) {
-                        System.out.println("apply");
+                    public void run() {
+                        System.out.println("Run server socket: " + socket);
 
-                        return new Runnable() {
-                            @Override
-                            public void run() {
-                                System.out.println("Run server socket: " + socket);
+                        try {
+                            IoStream stream = new TcpIoStream(TcpServerSandbox.class.getSimpleName(), socket);
+                            IncomingDataBuffer in = stream.getDataBuffer();
 
-                                try {
-                                    IoStream stream = new TcpIoStream(TcpServerSandbox.class.getSimpleName(), socket);
-                                    IncomingDataBuffer in = stream.getDataBuffer();
+                            while (!socket.isClosed()) {
+                                handleCommand(stream, in, socket);
 
-                                    while (!socket.isClosed()) {
-                                        Integer length = BinaryProtocolServer.getPendingPacketLengthOrHandleProtocolCommand(socket, new BinaryProtocolServer.Context(), in);
-                                        if (length == null)
-                                            continue;
-                                        byte[] payload = BinaryProtocolServer.getPacketContent(in, length);
-
-                                        byte command = payload[0];
-
-                                        if (command == Fields.TS_HELLO_COMMAND) {
-                                            new HelloCommand(Fields.TS_SIGNATURE).handle(stream);
-                                        } else if (command == Fields.TS_GET_PROTOCOL_VERSION_COMMAND_F) {
-                                            stream.sendPacket((TS_OK + TS_PROTOCOL).getBytes());
-                                        } else if (command == Fields.TS_PAGE_COMMAND) {
-                                            stream.sendPacket(TS_OK.getBytes());
-                                        } else if (command == Fields.TS_READ_COMMAND) {
-                                            DataInputStream dis = new DataInputStream(new ByteArrayInputStream(payload, 1, payload.length - 1));
-                                            int offset = swap16(dis.readShort());
-                                            int count = swap16(dis.readShort());
-                                            byte[] response = new byte[1 + count];
-                                            response[0] = (byte) TS_OK.charAt(0);
-                                            stream.sendPacket(response);
-                                        } else if (command == Fields.TS_GET_FIRMWARE_VERSION) {
-                                            stream.sendPacket((TS_OK + "rusEFI proxy").getBytes());
-                                        } else
-                                            throw new UnsupportedOperationException("Unsupported command " + command);
-                                    }
-
-                                } catch (Exception e) {
-                                    System.out.println(e);
-                                }
                             }
-                        };
-                    }
-                },
-                new Listener() {
-                    @Override
-                    public void onResult(Object parameter) {
-                        System.out.println("onResult");
 
+                        } catch (Exception e) {
+                            System.out.println(e);
+                        }
                     }
-                }, StatusConsumer.ANONYMOUS);
+                };
+            }
+        };
+        BinaryProtocolServer.tcpServerSocket(29001, "server", socketRunnableFactory,
+                serverSocketCreationCallback, StatusConsumer.ANONYMOUS);
+    }
+
+    private static void handleCommand(IoStream stream, IncomingDataBuffer in, Socket socket) throws IOException {
+        Integer length = BinaryProtocolServer.getPendingPacketLengthOrHandleProtocolCommand(socket, new BinaryProtocolServer.Context(), in);
+        if (length == null)
+            return;
+        byte[] payload = BinaryProtocolServer.getPacketContent(in, length);
+
+        byte command = payload[0];
+
+        if (command == Fields.TS_HELLO_COMMAND) {
+            new HelloCommand(Fields.TS_SIGNATURE).handle(stream);
+        } else if (command == Fields.TS_GET_PROTOCOL_VERSION_COMMAND_F) {
+            stream.sendPacket((TS_OK + TS_PROTOCOL).getBytes());
+        } else if (command == Fields.TS_PAGE_COMMAND) {
+            stream.sendPacket(TS_OK.getBytes());
+        } else if (command == Fields.TS_CRC_CHECK_COMMAND) {
+            throw new IllegalStateException("Need CRC");
+        } else if (command == Fields.TS_READ_COMMAND) {
+            DataInputStream dis = new DataInputStream(new ByteArrayInputStream(payload, 1, payload.length - 1));
+            int offset = swap16(dis.readShort());
+            int count = swap16(dis.readShort());
+            // always all zero response
+            byte[] response = new byte[1 + count];
+            response[0] = (byte) TS_OK.charAt(0);
+            stream.sendPacket(response);
+        } else if (command == Fields.TS_GET_FIRMWARE_VERSION) {
+            stream.sendPacket((TS_OK + "rusEFI proxy").getBytes());
+        } else
+            throw new UnsupportedOperationException("Unsupported command " + command);
     }
 }
