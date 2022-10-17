f4214fe393fed82521f3e1871e4f4cfceef0661d
GitHub gen-configs Action <action@github.com>
2022-10-17 02:17:10 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen-gm-e67.h
+++ firmware/controllers/generated/signature_hellen-gm-e67.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 4260571948
-#define TS_SIGNATURE "rusEFI 2022.10.16.hellen-gm-e67.4260571948"
+#define TS_SIGNATURE "rusEFI 2022.10.17.hellen-gm-e67.4260571948"
--- firmware/controllers/generated/signature_prometheus_469.h
+++ firmware/controllers/generated/signature_prometheus_469.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 427718524
-#define TS_SIGNATURE "rusEFI 2022.10.16.prometheus_469.427718524"
+#define TS_SIGNATURE "rusEFI 2022.10.17.prometheus_469.427718524"
