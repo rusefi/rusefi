b67ebee1fd13df3e9d4cc91b42a8c67905f34036
Matthew Kennedy <matthewkennedy@outlook.com>
2022-10-18 19:45:07 -0700

    correct pins

--- firmware/config/boards/nucleo_f429/board_configuration.cpp
+++ firmware/config/boards/nucleo_f429/board_configuration.cpp
@@ -14,13 +14,12 @@ void preHalInit() {
 	efiSetPadMode("Ethernet",  Gpio::A2, PAL_MODE_ALTERNATE(0xb));
 	efiSetPadMode("Ethernet",  Gpio::A7, PAL_MODE_ALTERNATE(0xb));
 
+	efiSetPadMode("Ethernet", Gpio::B13, PAL_MODE_ALTERNATE(0xb));
+
 	efiSetPadMode("Ethernet",  Gpio::C1, PAL_MODE_ALTERNATE(0xb));
 	efiSetPadMode("Ethernet",  Gpio::C4, PAL_MODE_ALTERNATE(0xb));
 	efiSetPadMode("Ethernet",  Gpio::C5, PAL_MODE_ALTERNATE(0xb));
 
-	efiSetPadMode("Ethernet",  Gpio::D5, PAL_MODE_ALTERNATE(0xb));
-
 	efiSetPadMode("Ethernet", Gpio::G11, PAL_MODE_ALTERNATE(0xb));
 	efiSetPadMode("Ethernet", Gpio::G13, PAL_MODE_ALTERNATE(0xb));
-	efiSetPadMode("Ethernet", Gpio::G14, PAL_MODE_ALTERNATE(0xb));
 }
