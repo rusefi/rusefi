c16d2e82e451548d5ac3a0a1c871bb85a0c1f7f1
Matthew Kennedy <matthewkennedy@outlook.com>
2022-10-19 21:59:39 -0700

    more f429 business (#4679)
    * 429 boots and LEDs work
    
    * oops missed a comma
    
    * enable on f767 too, since it should be the same board
    
    * why not just completely duplicate it
    
    * script should not be fancy
    
    * script should not be fancy
    
    * correct pins
    
    * h743 while we're at it

--- firmware/config/boards/nucleo_f767/board.mk
+++ firmware/config/boards/nucleo_f767/board.mk
@@ -1,11 +1,16 @@
 # List of all the board related files.
-BOARDCPPSRC = $(PROJECT_DIR)/config/boards/nucleo_f767/board_configuration.cpp
+
+# F429 and F767 Nucleo are indeed the same board with a different chip fitted - so recycle the F429 config
+BOARDCPPSRC = $(PROJECT_DIR)/config/boards/nucleo_f429/board_configuration.cpp
 
 # reducing flash consumption for EFI_ETHERNET to fit
 DDEFS += -DEFI_FILE_LOGGING=FALSE -DEFI_ALTERNATOR_CONTROL=FALSE -DEFI_LOGIC_ANALYZER=FALSE -DEFI_ENABLE_ASSERTS=FALSE
 
+DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::B14
+
+# Enable ethernet
 LWIP = yes
 DDEFS += -DEFI_ETHERNET=TRUE
 
-DDEFS += -DSTM32F767xx
-
+DDEFS += -DFIRMWARE_ID=\"nucleo_f767\"
+DDEFS += -DDEFAULT_ENGINE_TYPE=MINIMAL_PINS
--- firmware/config/boards/nucleo_h743/board.mk
+++ firmware/config/boards/nucleo_h743/board.mk
@@ -1,15 +1,12 @@
 # List of all the board related files.
 
-DDEFS = -DTS_NO_PRIMARY=0 \
-	-DTS_PRIMARY_PORT=SD3 \
-	-DEFI_CONSOLE_TX_BRAIN_PIN=Gpio::D8 \
-	-DEFI_CONSOLE_RX_BRAIN_PIN=Gpio::D9 \
+BOARDCPPSRC = $(PROJECT_DIR)/config/boards/nucleo_h743/board_configuration.cpp
 
-DDEFS += -DEFI_FILE_LOGGING=FALSE
+DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::B14
 
+# Enable ethernet
 LWIP = yes
 DDEFS += -DEFI_ETHERNET=TRUE
 
-# Shared variables
-ALLCPPSRC += $(PROJECT_DIR)/config/boards/nucleo_h743/board_configuration.cpp
-
+DDEFS += -DFIRMWARE_ID=\"nucleo_h743\"
+DDEFS += -DDEFAULT_ENGINE_TYPE=MINIMAL_PINS
