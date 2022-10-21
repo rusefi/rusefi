24650f64606493c3ea4a94b352e14e278a9674c4
Matthew Kennedy <matthewkennedy@outlook.com>
2022-10-20 19:25:39 -0700

    implement "ford mode" injector correction (#4686)
    * implement "ford mode"
    
    * missed the enum value

--- firmware/controllers/algo/fuel/injector_model.h
+++ firmware/controllers/algo/fuel/injector_model.h
@@ -17,26 +17,48 @@ public:
 	floatms_t getInjectionDuration(float fuelMassGram) const override;
 	float getFuelMassForDuration(floatms_t duration) const override;
 
-	virtual float getInjectorMassFlowRate() = 0;
 	virtual float getInjectorFlowRatio() = 0;
 	virtual expected<float> getAbsoluteRailPressure() const = 0;
-	virtual float correctShortPulse(float baseDuration) const = 0;
+
+	virtual float getBaseFlowRate() const = 0;
+
+	virtual InjectorNonlinearMode getNonlinearMode() const = 0;
+	float getBaseDurationImpl(float baseDuration) const;
+	virtual float correctInjectionPolynomial(float baseDuration) const;
+
+	// Ford small pulse model
+	virtual float getSmallPulseFlowRate() const = 0;
+	virtual float getSmallPulseBreakPoint() const = 0;
 
 private:
+	// Mass flow rate for large-pulse flow, g/s
 	float m_massFlowRate = 0;
+
+	// Break point below which the "small pulse" slope is used, grams
+	float m_smallPulseBreakPoint = 0;
+
+	// Flow rate for small pulses, g/s
+	float m_smallPulseFlowRate = 0;
+
+	// Correction adder for small pulses to correct for small/large pulse kink, ms
+	float m_smallPulseOffset = 0;
 };
 
 class InjectorModel : public InjectorModelBase {
 public:
 
 	floatms_t getDeadtime() const override;
-	float getInjectorMassFlowRate() override;
+	float getBaseFlowRate() const override;
 	float getInjectorFlowRatio() override;
 	expected<float> getAbsoluteRailPressure() const override;
 
+	InjectorNonlinearMode getNonlinearMode() const override;
+
+	// Ford small pulse model
+	float getSmallPulseFlowRate() const override;
+	float getSmallPulseBreakPoint() const override;
+
 	// Small pulse correction logic
-	float correctShortPulse(float baseDuration) const override;
-	virtual float correctInjectionPolynomial(float baseDuration) const;
 
 	using interface_t = IInjectorModel; // Mock interface
 };
