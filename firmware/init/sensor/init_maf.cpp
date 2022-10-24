f8a4d35d25d0d746a978296bb63d7f822a07d32c
Matthew Kennedy <matthewkennedy@outlook.com>
2022-10-24 09:41:05 -0700

    dual MAF sensors (#4697)
    * add second MAF
    
    * output channesl
    
    * move them around
    
    * ok let's try at the very end
    
    * is the logic wrong, or something else?
    
    * ooh it's the sensor order

--- firmware/init/sensor/init_maf.cpp
+++ firmware/init/sensor/init_maf.cpp
@@ -5,7 +5,8 @@
 #include "functional_sensor.h"
 #include "table_func.h"
 
-static FunctionalSensor maf(SensorType::Maf, /* timeout = */ MS2NT(50));
+static FunctionalSensor maf (SensorType::Maf , /* timeout = */ MS2NT(50));
+static FunctionalSensor maf2(SensorType::Maf2, /* timeout = */ MS2NT(50));
 
 #if !EFI_UNIT_TEST
 // extract the type of the elements in the bin/value arrays
@@ -15,16 +16,19 @@ using ValueType = std::remove_extent_t<decltype(config->mafDecoding)>;
 // This function converts volts -> kg/h
 static TableFunc mafCurve(config->mafDecodingBins, config->mafDecoding);
 
-void initMaf() {
-	adc_channel_e channel = engineConfiguration->mafAdcChannel;
-
+static void initMaf(adc_channel_e channel, FunctionalSensor& m) {
 	if (!isAdcChannelValid(channel)) {
 		return;
 	}
 
-	maf.setFunction(mafCurve);
+	m.setFunction(mafCurve);
 
-	AdcSubscription::SubscribeSensor(maf, channel, /*lowpassCutoff =*/ 50);
-	maf.Register();
+	AdcSubscription::SubscribeSensor(m, channel, /*lowpassCutoff =*/ 50);
+	m.Register();
+}
+
+void initMaf() {
+	initMaf(engineConfiguration->mafAdcChannel, maf);
+	initMaf(engineConfiguration->maf2AdcChannel, maf2);
 }
 #endif // ! EFI_UNIT_TEST
