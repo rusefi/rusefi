f47c36c92f19469aaa6123ef59a6e7106768497b
rusefillc <sdfsdfqsf2334234234>
2022-10-28 03:44:39 -0400

    renaming tool

--- firmware/gen_live_documentation.sh
+++ firmware/gen_live_documentation.sh
@@ -4,7 +4,7 @@ rm gen_live_documentation.log
 
 java -DSystemOut.name=logs/gen_live_documentation \
  -cp ../java_tools/ConfigDefinition.jar \
- com.rusefi.ldmp.UsagesReader integration/LiveData.yaml
+ com.rusefi.ldmp.LiveDataProcessor integration/LiveData.yaml
 [ $? -eq 0 ] || { echo "ERROR generating"; exit 1; }
 
 bash gen_output_channels.sh
