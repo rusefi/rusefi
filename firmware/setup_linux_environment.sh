9cf40c58d5a95e6b3db48c48967fff3d90433620
Nathan Schulte <nmschulte@desmas.net>
2022-11-07 20:50:18 -0600

    setup_linux_environment.sh tar uses --auto-compress

--- firmware/setup_linux_environment.sh
+++ firmware/setup_linux_environment.sh
@@ -27,7 +27,7 @@ rm -rf arm-none-eabi-gcc.tar.bz2
 
 # Download and extract GCC compiler
 curl -L -o arm-none-eabi-gcc.tar.xz https://developer.arm.com/-/media/Files/downloads/gnu/11.2-2022.02/binrel/gcc-arm-11.2-2022.02-x86_64-arm-none-eabi.tar.xz
-tar -xjvf arm-none-eabi-gcc.tar.xz
+tar -xavf arm-none-eabi-gcc.tar.xz
 
 # Delete downloaded image
 rm arm-none-eabi-gcc.tar.xz
