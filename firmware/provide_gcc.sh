00e43f9a46263bd92b9a4e889e1fa334a693ff0d
Nathan Schulte <8540239+nmschulte@users.noreply.github.com>
2022-11-09 06:18:40 -0600

    use rusEFI pruned ARM GCC 11.3 (#4763)
    * use rusEFI pruned ARM GCC 11.3
    
    * fix Firmware at GHA: avoid dash
    
    * avoid double decompress in provide_gcc.sh
    
    also ensure TMP_DIR, avoiding strange woes about GHA CI: ./firmware/provide_gcc.sh: 23: pushd: not found

--- firmware/provide_gcc.sh
+++ firmware/provide_gcc.sh
@@ -4,20 +4,31 @@
 
 set -e
 
-URL="https://github.com/rusefi/build_support/raw/master/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi.tar.xz"
-ARCHIVE="${URL##*/}"
-DIR="gcc-arm-none-eabi"
+# URL to download original toolchain from
+URL="https://github.com/rusefi/build_support/raw/master/rusefi-arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi.tar.xz"
+# colloquial directory name, to afford re-use of script
+COLLOQUIAL="gcc-arm-none-eabi"
+# temporary working directory
+TMP_DIR="/tmp/rusefi-provide_gcc"
 
-# Delete existing archive
-rm -rf ${ARCHIVE}
+archive="${URL##*/}"
+
+# Cleanup prior [failed] runs
+rm -rf "${TMP_DIR}"
 
 # Download and extract archive
-curl -L -o ${ARCHIVE} ${URL}
-tar -xavf ${ARCHIVE}
+echo Downloading and extracting ${archive}
+mkdir -p "${TMP_DIR}"
+cd "${TMP_DIR}"
+curl -L -o "${archive}" "${URL}"
+tar -xaf "${archive}"
+rm "${archive}"
 
 # Create colloquially named link
-ARCHIVE_DIR=$(tar --exclude="*/*" -tf ${ARCHIVE})
-ln -s ${ARCHIVE_DIR%/} ${DIR}
+archive_dir="$(echo *)"
+cd - >/dev/null
+mv "${TMP_DIR}/${archive_dir}" "$(pwd)"
+ln -s "${archive_dir%/}" "${COLLOQUIAL}"
 
 # Delete downloaded archive
-rm ${ARCHIVE}
+rm -rf "${TMP_DIR}"
