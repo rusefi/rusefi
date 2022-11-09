800b6b7a95947ce3fc59dd60b35c7caed0239e1d
rusefillc <sdfsdfqsf2334234234>
2022-11-09 14:41:21 -0500

    https://github.com/rusefi/hellenNA6_issues/issues/35

--- firmware/controllers/system/efi_gpio.cpp
+++ firmware/controllers/system/efi_gpio.cpp
@@ -625,11 +625,13 @@ void OutputPin::initPin(const char *msg, brain_pin_e brainPin, const pin_output_
 				? !actualValue 
 				: actualValue;
 
+#ifndef DISABLE_PIN_STATE_VALIDATION
 			// if the pin was set to logical 1, then set an error and disable the pin so that things don't catch fire
 			if (logicalValue) {
 				firmwareError(OBD_PCM_Processor_Fault, "HARDWARE VALIDATION FAILED %s: unexpected startup pin state %s actual value=%d logical value=%d mode=%s", msg, hwPortname(brainPin), actualValue, logicalValue, getPin_output_mode_e(*outputMode));
 				OutputPin::deInit();
 			}
+#endif
 		}
 	}
 #endif /* EFI_GPIO_HARDWARE */
