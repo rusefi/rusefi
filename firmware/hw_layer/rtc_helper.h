ce4db8170bee37004a1cfa2c467e87e5f5962db8
Nathan Schulte <nmschulte@desmas.net>
2022-11-10 00:40:27 -0600

    ChibiOS RTC avoids ctime

--- firmware/hw_layer/rtc_helper.h
+++ firmware/hw_layer/rtc_helper.h
@@ -4,14 +4,22 @@
  *
  * @date Feb 5, 2014
  * @author Andrey Belomutskiy, (c) 2012-2020
+ * @author Nathan Schulte, (c) 2022
  */
 
 #pragma once
 
-void printDateTime();
-void setDateTime(const char *strDate);
+#include "rusefi_types.h"
+
+#if EFI_RTC
 void initRtc();
-void date_set_tm(struct tm *);
-void date_get_tm(struct tm *);
+void printRtcDateTime();
+efidatetime_t getRtcDateTime();
+void setRtcDateTime(const efidatetime_t * const dateTime);
+
+efidatetime_t convertRtcDateTimeToEfi(const RTCDateTime * const timespec);
+RTCDateTime convertRtcDateTimeFromEfi(const efidatetime_t * const dateTime);
+#endif // EFI_RTC
+
 void dateToString(char *buffer);
 bool dateToStringShort(char *lcd_str);
