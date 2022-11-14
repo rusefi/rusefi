cbd737fd39913f08140ca52ade9cd33209690fe6
GitHub gen-configs Action <action@github.com>
2022-11-14 22:57:45 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen-nb1.h
+++ firmware/controllers/generated/signature_hellen-nb1.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH snap_12176
-#define TS_SIGNATURE "rusEFI 2022.11.14.hellen-nb1.snap_12176"
+#define SIGNATURE_HASH snap_46596
+#define TS_SIGNATURE "rusEFI 2022.11.14.hellen-nb1.snap_46596"
--- firmware/controllers/generated/signature_subaru_eg33_f7.h
+++ firmware/controllers/generated/signature_subaru_eg33_f7.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on config/boards/subaru_eg33/config/gen_subaru_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH snap_51078
-#define TS_SIGNATURE "rusEFI 2022.11.14.subaru_eg33_f7.snap_51078"
+#define SIGNATURE_HASH snap_24082
+#define TS_SIGNATURE "rusEFI 2022.11.14.subaru_eg33_f7.snap_24082"
