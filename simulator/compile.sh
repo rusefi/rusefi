946a50dd6877b1f477c2666c237bf459a4f12d21
Nathan Schulte <nmschulte@desmas.net>
2022-11-18 01:04:39 -0600

    simulator: tweak case of rusEFI in compile.sh log

--- simulator/compile.sh
+++ simulator/compile.sh
@@ -2,4 +2,4 @@ rm -f build/rusefi_simulator.exe
 make -j$(nproc) -r
 [ $? -eq 0 ] || { echo "Simulator compilation failed"; exit 1; }
 file build/rusefi_simulator
-echo "TIMESTAMP $(date "+%D %T.%2N") just compiled rusefi simulator"
+echo "TIMESTAMP $(date "+%D %T.%2N") just compiled rusEFI simulator"
