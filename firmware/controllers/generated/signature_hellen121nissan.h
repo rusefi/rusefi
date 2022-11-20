3083d9bb5155ef1ef5f5d52f3ca2f039bf57afe8
GitHub gen-configs Action <action@github.com>
2022-11-20 06:14:28 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen121nissan.h
+++ firmware/controllers/generated/signature_hellen121nissan.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH snap_17869
-#define TS_SIGNATURE "rusEFI 2022.11.20.hellen121nissan.snap_17869"
+#define SIGNATURE_HASH snap_42150
+#define TS_SIGNATURE "rusEFI 2022.11.20.hellen121nissan.snap_42150"
--- firmware/controllers/generated/signature_prometheus_405.h
+++ firmware/controllers/generated/signature_prometheus_405.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH snap_37207
-#define TS_SIGNATURE "rusEFI 2022.11.20.prometheus_405.snap_37207"
+#define SIGNATURE_HASH snap_28732
+#define TS_SIGNATURE "rusEFI 2022.11.20.prometheus_405.snap_28732"
