98c67d978928bc53862c11490bb4b07bbc1b0416
rusefillc <sdfsdfqsf2334234234>
2022-11-20 23:13:38 -0500

    better API

--- java_console/models/src/main/java/com/rusefi/stream/LogicdataOutputStream.java
+++ java_console/models/src/main/java/com/rusefi/stream/LogicdataOutputStream.java
@@ -2,12 +2,30 @@ package com.rusefi.stream;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class LogicdataOutputStream {
     private final OutputStream outputStream;
 
     public LogicdataOutputStream(OutputStream outputStream) {
         this.outputStream = outputStream;
+    }
+
+    public void writeDouble(double value) throws IOException {
+        if (value == 0.0) {
+            writeByte(0);
+        } else {
+            writeByte(8);
+            // doubles are saved little-endian, sorry Java :)
+            ByteBuffer bb = ByteBuffer.allocate(8);
+            bb.order(ByteOrder.LITTLE_ENDIAN);
+            bb.putDouble(value);
+            bb.rewind();
+            for (int i = 0; i < 8; i++) {
+                writeByte(bb.get());
+            }
+        }
     }
 
     // This is the main secret of this format! :)
@@ -28,14 +46,14 @@ public class LogicdataOutputStream {
     }
 
     private void writeAs(long value, int numBytes) throws IOException {
-    	if (value == 0) {
-   			writeByte(0);
-   		} else {
-			writeByte(numBytes);
-			for (int i = 0; i < numBytes; i++) {
-				writeByte((byte)((value >> (i * 8)) & 0xff));
-			}
-		}
+        if (value == 0) {
+            writeByte(0);
+        } else {
+            writeByte(numBytes);
+            for (int i = 0; i < numBytes; i++) {
+                writeByte((byte) ((value >> (i * 8)) & 0xff));
+            }
+        }
     }
 
     public void close() throws IOException {
@@ -48,5 +66,12 @@ public class LogicdataOutputStream {
 
     public void writeByte(int i) throws IOException {
         outputStream.write(i);
+    }
+
+    void writeString(String str) throws IOException {
+        writeVarLength(str.length());
+        for (char c : str.toCharArray()) {
+            writeByte(c);
+        }
     }
 }
