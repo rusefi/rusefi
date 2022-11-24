49320f10f6bd96e7a0849a66cec20e1e71ad2d5b
rusefillc <sdfsdfqsf2334234234>
2022-11-23 21:10:32 -0500

    Full Optimized – High Speed #4818
    refactoring - nicer API

--- java_console/io/src/main/java/com/rusefi/io/commands/WriteChunkCommand.java
+++ java_console/io/src/main/java/com/rusefi/io/commands/WriteChunkCommand.java
@@ -1,13 +1,4 @@
 package com.rusefi.io.commands;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-
 public class WriteChunkCommand {
-    @NotNull
-    public static DataInputStream createPayLoadStream(byte[] payload) {
-        return new DataInputStream(new ByteArrayInputStream(payload, 1, payload.length - 1));
-    }
 }
