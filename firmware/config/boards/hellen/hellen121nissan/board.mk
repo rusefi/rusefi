c8e3582c4a3eb8e055e0c7cd5adfce99bd30ba62
rusefillc <sdfsdfqsf2334234234>
2022-11-25 10:13:57 -0500

    Nissan: probably hardware issue but maybe we need firmware progress as well #4821

--- firmware/config/boards/hellen/hellen121nissan/board.mk
+++ firmware/config/boards/hellen/hellen121nissan/board.mk
@@ -18,6 +18,9 @@ DDEFS += -DTS_NO_PRIMARY=1
 DDEFS += -DFIRMWARE_ID=\"hellen121nissan\" $(VAR_DEF_ENGINE_TYPE)
 DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE -DSTM32_ADC_USE_ADC3=TRUE
 
+# bad temporary solution of hardware issue https://github.com/rusefi/rusefi/issues/4821
+DDEFS += -DMAX_TPS_PPS_DISCREPANCY=10.0f
+
 DDEFS += -DSHORT_BOARD_NAME=hellen121nissan
 
 include $(BOARDS_DIR)/hellen/hellen-common144.mk
