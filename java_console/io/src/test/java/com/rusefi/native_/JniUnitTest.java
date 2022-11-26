704f6a8318410e6e5e3786178e41959c331dce73
Andrey <rusefillc@gmail.com>
2022-11-26 17:40:09 -0500

    Target AFR table Y axis stuck at 0 #4826
    not working but shows promise?

--- java_console/io/src/test/java/com/rusefi/native_/JniUnitTest.java
+++ java_console/io/src/test/java/com/rusefi/native_/JniUnitTest.java
@@ -27,6 +27,9 @@ public class JniUnitTest {
 
         assertEquals(TS_FILE_VERSION, (int) getValue(engineLogic.getOutputs(), Sensor.FIRMWARE_VERSION));
 
+        // huh? assertEquals(14.0, getValue(engineLogic.getOutputs(), Sensor.afrTarget));
+        assertEquals(0.0, getValue(engineLogic.getOutputs(), Sensor.afrTarget));
+
         double veValue = getValue(engineLogic.getOutputs(), Sensor.veValue);
         assertTrue("veValue", veValue > 40 && veValue < 90);
 
