cbccb013f261a3dcdaaec5bea6e7dd0bd60225a3
GitHub gen-configs Action <action@github.com>
2022-11-27 14:13:54 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen88bmw.h
+++ firmware/controllers/generated/signature_hellen88bmw.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH snap_9106
-#define TS_SIGNATURE "rusEFI 2022.11.27.hellen88bmw.snap_9106"
+#define SIGNATURE_HASH snap_36195
+#define TS_SIGNATURE "rusEFI 2022.11.27.hellen88bmw.snap_36195"
--- firmware/controllers/generated/signature_prometheus_469.h
+++ firmware/controllers/generated/signature_prometheus_469.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH snap_59832
-#define TS_SIGNATURE "rusEFI 2022.11.27.prometheus_469.snap_59832"
+#define SIGNATURE_HASH snap_18249
+#define TS_SIGNATURE "rusEFI 2022.11.27.prometheus_469.snap_18249"
