a14fb947c26400e2bdf68abbc3e767ad64452447
Andrey <rusefillc@gmail.com>
2022-11-28 19:22:45 -0500

    exp average is pretty cool

---
+++ firmware/util/math/exp_average.h
@@ -0,0 +1,21 @@
+/**
+ * @file exp_average.h
+ */
+
+#pragma once
+
+class ExpAverage {
+public:
+    float average(float value) {
+        current = smoothingFactor * value + (1 - smoothingFactor) * current;
+        return current;
+    }
+
+    void init(int length) {
+        smoothingFactor = 2 / (length + 1.0);
+    }
+
+private:
+    float current = 0;
+    float smoothingFactor = 0.5;
+};
