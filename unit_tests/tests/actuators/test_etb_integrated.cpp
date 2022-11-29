73e309da03218b05ba925e2e92560110c690aa45
Andrey <rusefillc@gmail.com>
2022-11-29 14:02:44 -0500

    ETB duty cycle jitter #4833

--- unit_tests/tests/actuators/test_etb_integrated.cpp
+++ unit_tests/tests/actuators/test_etb_integrated.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "init.h"
 #include "electronic_throttle_impl.h"
+#include "live_data.h"
 
 TEST(etb, integrated) {
 	EngineTestHelper eth(TEST_ENGINE);
@@ -27,5 +28,15 @@ TEST(etb, integrated) {
 
 	ASSERT_EQ(engine->outputChannels.etbTarget, 40);
 	ASSERT_EQ(etb->prevOutput, 100);
+	ASSERT_EQ(etb->etbDutyAverage, 50);
 
+	Sensor::setMockValue(SensorType::AcceleratorPedal, 10, true);
+	etb->update();
+	ASSERT_EQ(etb->etbDutyAverage, -25);
+	ASSERT_EQ(etb->etbDutyRateOfChange, -75);
+
+	float destination;
+	int offset = ELECTRONIC_THROTTLE_BASE_ADDRESS + offsetof(electronic_throttle_s, etbDutyRateOfChange);
+	copyRange((uint8_t*)&destination, getLiveDataFragments(), offset, sizeof(destination));
+	ASSERT_EQ(destination, 0); // huh? what is broken here?
 }
