b3a3c97b87f88f74f81d3e8051e5775ee9d6afa2
rusefi <rusefillc@gmail.com>
2022-11-29 22:00:28 -0500

    ETB duty cycle jitter #4833
    still totally dead on real hardware and I've already fixed two defects! time for an indicator

--- firmware/controllers/limp_manager.h
+++ firmware/controllers/limp_manager.h
@@ -26,6 +26,18 @@ enum class ClearReason : uint8_t {
 	// todo: add a code generator between ClearReason and fuelIgnCutCodeList in rusefi.input
 };
 
+
+enum class TpsState : uint8_t {
+	None, // 0
+	Setting,
+	TpsError,
+	PpsError, // 3
+	InputJitter,
+	PidJitter,
+
+	// keep this list in sync with etbCutCodeList
+};
+
 // Only allows clearing the value, but never resetting it.
 class Clearable {
 public:
