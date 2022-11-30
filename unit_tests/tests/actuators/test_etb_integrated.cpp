7547edb566090ee9405c47a5599d566321859214
Andrey <rusefillc@gmail.com>
2022-11-29 20:36:03 -0500

    mock invalid & current state of ETB error counter

--- unit_tests/tests/actuators/test_etb_integrated.cpp
+++ unit_tests/tests/actuators/test_etb_integrated.cpp
@@ -12,8 +12,6 @@ static EtbController * initEtbIntegratedTest() {
 	engineConfiguration->throttlePedalPositionAdcChannel = EFI_ADC_3;
 	engineConfiguration->throttlePedalPositionSecondAdcChannel = EFI_ADC_3;
 
-	Sensor::setMockValue(SensorType::AcceleratorPedalPrimary, 40);
-	Sensor::setMockValue(SensorType::AcceleratorPedalSecondary, 40);
 
 	Sensor::setMockValue(SensorType::Tps1, 25.0f, true);
 
@@ -25,6 +23,9 @@ static EtbController * initEtbIntegratedTest() {
 TEST(etb, integrated) {
 	EngineTestHelper eth(TEST_ENGINE); // we have a distractor so cannot move EngineTestHelper into utility method
 	EtbController *etb = initEtbIntegratedTest();
+
+	Sensor::setMockValue(SensorType::AcceleratorPedalPrimary, 40);
+	Sensor::setMockValue(SensorType::AcceleratorPedalSecondary, 40);
 
 	etb->update();
 
@@ -51,8 +52,18 @@ TEST(etb, integratedTpsJitter) {
 
 	ASSERT_FALSE(isTps1Error());
 	ASSERT_FALSE(isTps2Error());
-	ASSERT_FALSE(isPedalError());
+	ASSERT_TRUE(isPedalError());
 
 	etb->update();
 
+	Sensor::setInvalidMockValue(SensorType::AcceleratorPedal);
+	ASSERT_TRUE(isPedalError());
+	etb->update();
+
+	ASSERT_EQ(1, etb->etbInputErrorCounter);
+
+	Sensor::setInvalidMockValue(SensorType::Tps1);
+	etb->update();
+
+	ASSERT_EQ(1, etb->etbInputErrorCounter);
 }
