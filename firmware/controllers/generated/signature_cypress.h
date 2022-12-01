0edf719e774c322fd0b943301c167fd4e39adafc
GitHub gen-configs Action <action@github.com>
2022-12-01 00:55:15 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_cypress.h
+++ firmware/controllers/generated/signature_cypress.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on hellen_cypress_gen_config.bat by SignatureConsumer
 //
 
-#define SIGNATURE_HASH snap_51449
-#define TS_SIGNATURE "rusEFI 2022.12.01.cypress.snap_51449"
+#define SIGNATURE_HASH snap_36020
+#define TS_SIGNATURE "rusEFI 2022.12.01.cypress.snap_36020"
--- firmware/controllers/generated/signature_prometheus_405.h
+++ firmware/controllers/generated/signature_prometheus_405.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH snap_4383
-#define TS_SIGNATURE "rusEFI 2022.12.01.prometheus_405.snap_4383"
+#define SIGNATURE_HASH snap_21842
+#define TS_SIGNATURE "rusEFI 2022.12.01.prometheus_405.snap_21842"
