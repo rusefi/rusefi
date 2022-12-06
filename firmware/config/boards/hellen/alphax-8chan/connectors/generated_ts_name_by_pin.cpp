2174ed31d285c0500ca9390d3e7b3c0b17dad5ec
GitHub gen-configs Action <action@github.com>
2022-12-06 04:43:03 +0000

    Auto-generated configs and docs

--- firmware/config/boards/hellen/alphax-8chan/connectors/generated_ts_name_by_pin.cpp
+++ firmware/config/boards/hellen/alphax-8chan/connectors/generated_ts_name_by_pin.cpp
@@ -8,6 +8,14 @@
 // see comments at declaration in pin_repository.h
 const char * getBoardSpecificPinName(brain_pin_e brainPin) {
 	switch(brainPin) {
+		case Gpio::C13: return "13A - Ignition 1";
+		case Gpio::D10: return "1A - Injector 4";
+		case Gpio::D11: return "2A - Injector 3";
+		case Gpio::E3: return "10A - Ignition 4";
+		case Gpio::E4: return "11A - Ignition 3";
+		case Gpio::E5: return "12A - Ignition 2";
+		case Gpio::G7: return "4A - Injector 1";
+		case Gpio::G8: return "3A - Injector 2";
 		default: return nullptr;
 	}
 	return nullptr;
