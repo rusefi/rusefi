3dd1015109af43cb99806a186ebd0c3e27281119
Matthew Kennedy <matthewkennedy@outlook.com>
2022-12-07 11:38:03 -0800

    I still don't java good

--- java_console/autotest/src/main/java/com/rusefi/common/MiscTest.java
+++ java_console/autotest/src/main/java/com/rusefi/common/MiscTest.java
@@ -7,6 +7,7 @@ import com.rusefi.core.SensorCentral;
 import org.junit.Test;
 
 import static com.devexperts.util.TimeUtil.SECOND;
+import static com.rusefi.IoUtil.sleepSeconds;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
