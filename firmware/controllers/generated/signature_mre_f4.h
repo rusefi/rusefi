1c014a165a9977546da615944bde356fbe0d59a3
GitHub gen-configs Action <action@github.com>
2022-12-07 15:10:52 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_mre_f4.h
+++ firmware/controllers/generated/signature_mre_f4.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH snap_64186
-#define TS_SIGNATURE "rusEFI 2022.12.07.mre_f4.snap_64186"
+#define SIGNATURE_HASH snap_42399
+#define TS_SIGNATURE "rusEFI 2022.12.07.mre_f4.snap_42399"
