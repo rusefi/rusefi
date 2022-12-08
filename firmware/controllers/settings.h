702c538f864efd7c4ffe77e47ca4c8eeb6df3b6b
Nathan Schulte <nmschulte@desmas.net>
2022-12-07 22:18:10 -0600

    avoid sscanf use (for atoi)
    to avoid bloating the stack

--- firmware/controllers/settings.h
+++ firmware/controllers/settings.h
@@ -20,4 +20,4 @@ void setEngineType(int value);
 void readPin(const char *pinName);
 
 void printDateTime();
-void setDateTime(const char * const isoDateTime);
+void setDateTime(char const * const isoDateTime);
