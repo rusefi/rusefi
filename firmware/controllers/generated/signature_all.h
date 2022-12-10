e4d602c35e5a3b061a60ccdc39705c02a8c163d7
GitHub gen-configs Action <action@github.com>
2022-12-10 22:22:53 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_all.h
+++ firmware/controllers/generated/signature_all.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH snap_59789
-#define TS_SIGNATURE "rusEFI 2022.12.10.all.snap_59789"
+#define SIGNATURE_HASH snap_57900
+#define TS_SIGNATURE "rusEFI 2022.12.10.all.snap_57900"
--- firmware/controllers/generated/signature_f407-discovery.h
+++ firmware/controllers/generated/signature_f407-discovery.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH snap_59789
-#define TS_SIGNATURE "rusEFI 2022.12.10.f407-discovery.snap_59789"
+#define SIGNATURE_HASH snap_57900
+#define TS_SIGNATURE "rusEFI 2022.12.10.f407-discovery.snap_57900"
