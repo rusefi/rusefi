e189d6517b9b29672c43a19b9f069482937ecc65
rusefillc <sdfsdfqsf2334234234>
2022-12-14 19:28:28 -0500

    random Frankenso clean-up

--- unit_tests/tests/sensor/test_sensor_init.cpp
+++ unit_tests/tests/sensor/test_sensor_init.cpp
@@ -192,6 +192,7 @@ TEST(SensorInit, Clt) {
 
 	// 2003 neon sensor
 	engineConfiguration->clt.config = {0, 30, 100, 32500, 7550, 700, 2700};
+	engineConfiguration->clt.adcChannel = EFI_ADC_6;
 
 	initThermistors();
 
@@ -220,6 +221,7 @@ TEST(SensorInit, Lambda) {
 
 TEST(SensorInit, Map) {
 	EngineTestHelper eth(TEST_ENGINE);
+	engineConfiguration->map.sensor.hwChannel = EFI_ADC_4;
 
 	initMap();
 
