3754d8657712cc73fa2a5d8df60f3c22247e89ae
rusefi <rusefillc@gmail.com>
2022-12-18 17:23:12 -0500

    https://github.com/rusefi/rusefi/issues/4909

--- firmware/config/boards/hellen/alphax-2chan/trigger_scope_config.h
+++ firmware/config/boards/hellen/alphax-2chan/trigger_scope_config.h
@@ -8,3 +8,5 @@
 #define TRIGGER_SCOPE_ADC_CH1 H144_ADC_IN_RES3
 
 #define TRIGGER_SCOPE_HAS_CH2 false
+// todo: fix https://github.com/rusefi/rusefi/issues/4909
+#define TRIGGER_SCOPE_ADC_CH2 H144_ADC_IN_RES2
