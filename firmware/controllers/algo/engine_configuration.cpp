08cdec228d00067f7462d603e11476aaad7eea7a
rusefillc <sdfsdfqsf2334234234>
2022-12-20 08:39:55 -0500

    docs

--- firmware/controllers/algo/engine_configuration.cpp
+++ firmware/controllers/algo/engine_configuration.cpp
@@ -112,7 +112,7 @@
 /**
  * Current engine configuration. On firmware start we assign empty configuration, then
  * we copy actual configuration after reading settings.
- * This is useful to compare old and new configurations in order to apply new settings.
+ * This is useful to compare old (activeConfiguration) and new (engineConfiguration) configurations in order to apply new settings.
  *
  * todo: place this field next to 'engineConfiguration'?
  */
