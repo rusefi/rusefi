7932a64c8bef021eeaf2a7b402640d36bc5d2eed
GitHub gen-configs Action <action@github.com>
2022-12-27 04:49:54 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_alphax-4chan.h
+++ firmware/controllers/generated/signature_alphax-4chan.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH snap_47985
-#define TS_SIGNATURE "rusEFI 2022.12.27.alphax-4chan.snap_47985"
+#define SIGNATURE_HASH snap_45782
+#define TS_SIGNATURE "rusEFI 2022.12.27.alphax-4chan.snap_45782"
--- firmware/controllers/generated/signature_proteus_f7.h
+++ firmware/controllers/generated/signature_proteus_f7.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH snap_27950
-#define TS_SIGNATURE "rusEFI 2022.12.27.proteus_f7.snap_27950"
+#define SIGNATURE_HASH snap_25737
+#define TS_SIGNATURE "rusEFI 2022.12.27.proteus_f7.snap_25737"
