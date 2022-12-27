6a42a1de77de24eea4fcb7499361136ab054c688
rusefillc <sdfsdfqsf2334234234>
2022-12-27 00:23:04 -0500

    Sometimes while editing Lua we get "Fuel pressure compensation is set to use a pressure sensor, but none is configured. 20221226@proteus" on Passat config fix #4916

--- firmware/init/sensor/init_fluid_pressure.cpp
+++ firmware/init/sensor/init_fluid_pressure.cpp
@@ -54,7 +54,9 @@ static void initFluidPressure(LinearFunc& func, FunctionalSensor& sensor, const 
 void initFluidPressure() {
 	initFluidPressure(oilpSensorFunc, oilpSensor, engineConfiguration->oilPressure, 10);
 	initFluidPressure(fuelPressureFuncLow, fuelPressureSensorLow, engineConfiguration->lowPressureFuel, 10);
-	initFluidPressure(fuelPressureFuncHigh, fuelPressureSensorHigh, engineConfiguration->highPressureFuel, 100);
+	if (isConfigurationChanged(highPressureFuel.hwChannel)) {
+	    initFluidPressure(fuelPressureFuncHigh, fuelPressureSensorHigh, engineConfiguration->highPressureFuel, 100);
+	}
 	initFluidPressure(auxLinear1Func, auxLinear1Sensor, engineConfiguration->auxLinear1, 10);
 	initFluidPressure(auxLinear2Func, auxLinear2Sensor, engineConfiguration->auxLinear2, 10);
 
@@ -70,7 +72,9 @@ void initFluidPressure() {
 void deinitFluidPressure() {
 	AdcSubscription::UnsubscribeSensor(oilpSensor);
 	AdcSubscription::UnsubscribeSensor(fuelPressureSensorLow);
-	AdcSubscription::UnsubscribeSensor(fuelPressureSensorHigh);
+	if (isConfigurationChanged(highPressureFuel.hwChannel)) {
+	    AdcSubscription::UnsubscribeSensor(fuelPressureSensorHigh);
+	}
 	AdcSubscription::UnsubscribeSensor(auxLinear1Sensor);
 	AdcSubscription::UnsubscribeSensor(auxLinear2Sensor);
 }
