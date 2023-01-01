92545e38ff30a6fea44754d0f125b5b54436d244
GitHub gen-configs Action <action@github.com>
2023-01-01 16:40:26 +0000

    Auto-generated configs and docs

---
+++ firmware/controllers/generated/signature_m74_9.h
@@ -0,0 +1,6 @@
+//
+// was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
+//
+
+#define SIGNATURE_HASH snap_11522
+#define TS_SIGNATURE "rusEFI 2023.01.01.m74_9.snap_11522"
