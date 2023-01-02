2c97ccb2a8a8199cfe51f73bd223d1d76d0c3140
GitHub gen-configs Action <action@github.com>
2023-01-02 16:18:44 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_s105.h
+++ firmware/controllers/generated/signature_s105.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH snap_44381
-#define TS_SIGNATURE "rusEFI 2023.01.02.s105.snap_44381"
+#define SIGNATURE_HASH snap_64393
+#define TS_SIGNATURE "rusEFI 2023.01.02.s105.snap_64393"
