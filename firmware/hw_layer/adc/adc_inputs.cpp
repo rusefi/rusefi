cc7ea202d55110e9858b28cc37cd1ac7fd1571c9
Andrey Gusakov <dron0gus@gmail.com>
2023-01-08 18:32:08 +0300

    adc: fix emulator compilation

--- firmware/hw_layer/adc/adc_inputs.cpp
+++ firmware/hw_layer/adc/adc_inputs.cpp
@@ -21,8 +21,11 @@
 
 #include "pch.h"
 
-#if HAL_USE_ADC
+float __attribute__((weak)) getAnalogInputDividerCoefficient(adc_channel_e) {
+    return engineConfiguration->analogInputDividerCoefficient;
+}
 
+#if HAL_USE_ADC
 
 #include "adc_subscription.h"
 #include "AdcConfiguration.h"
@@ -39,10 +42,6 @@ static NO_CACHE adcsample_t slowAdcSamples[SLOW_ADC_CHANNEL_COUNT];
 static NO_CACHE adcsample_t fastAdcSampleBuf[ADC_BUF_DEPTH_FAST * ADC_MAX_CHANNELS_COUNT];
 
 static adc_channel_mode_e adcHwChannelEnabled[HW_MAX_ADC_INDEX];
-
-float __attribute__((weak)) getAnalogInputDividerCoefficient(adc_channel_e) {
-    return engineConfiguration->analogInputDividerCoefficient;
-}
 
 // Board voltage, with divider coefficient accounted for
 float getVoltageDivided(const char *msg, adc_channel_e hwChannel) {
