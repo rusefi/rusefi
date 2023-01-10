ade3647e04992515d21255fd8188889d2b662bd8
Matthew Kennedy <matthewkennedy@outlook.com>
2023-01-10 12:02:01 -0800

    other tests

--- java_tools/ts_plugin/src/test/java/com/rusefi/SignatureHelperTest.java
+++ java_tools/ts_plugin/src/test/java/com/rusefi/SignatureHelperTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertEquals;
 public class SignatureHelperTest {
     @Test
     public void test() {
-        String url = SignatureHelper.getUrl("rusEFI 2020.07.06.frankenso_na6.2468827536").first;
-        assertEquals("https://rusefi.com/online/ini/rusefi/2020/07/06/frankenso_na6/2468827536.ini", url);
+        String url = SignatureHelper.getUrl("rusEFI master.2020.07.06.frankenso_na6.2468827536").first;
+        assertEquals("https://rusefi.com/online/ini/master/rusefi/2020/07/06/frankenso_na6/2468827536.ini", url);
     }
 }
