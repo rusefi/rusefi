a83eb3dcae31e0e380e5270d07c94c00566647ee
mi-hol <22799428+mi-hol@users.noreply.github.com>
2023-01-12 17:53:32 +0100

    WIP: add Benelli Walbro

---
+++ firmware/config/boards/hellen/Benelli_Walbro/board.mk
@@ -0,0 +1,20 @@
+# Combine the related files for a specific platform and MCU.
+
+# Target ECU board design
+BOARDCPPSRC = $(BOARDS_DIR)/hellen/alphax-4chan/board_configuration.cpp
+BOARDINC = $(BOARDS_DIR)/hellen/alphax-4chan
+
+DDEFS += -DEFI_MAIN_RELAY_CONTROL=TRUE
+
+# This board has trigger scope hardware!
+DDEFS += -DTRIGGER_SCOPE
+
+# Add them all together
+DDEFS += -DFIRMWARE_ID=\"AlphaX-4chan\"
+DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE -DSTM32_ADC_USE_ADC3=TRUE
+
+include $(BOARDS_DIR)/hellen/hellen-common144.mk
+
+DDEFS += $(PRIMARY_COMMUNICATION_PORT_USART2)
+
+DDEFS += -DSHORT_BOARD_NAME=alphax-4chan
---
+++ firmware/config/boards/hellen/Benelli_Walbro/compile_Benelli_Walbro.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ..
+bash ../common_make.sh hellen/Benelli_Walbro ARCH_STM32F4
