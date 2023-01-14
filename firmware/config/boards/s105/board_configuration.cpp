d51be43f009a8195cac40c11ea8ab95ad6be21e4
Andrey Gusakov <dron0gus@gmail.com>
2023-01-14 22:07:18 +0300

    S105: add more ADC dividers

--- firmware/config/boards/s105/board_configuration.cpp
+++ firmware/config/boards/s105/board_configuration.cpp
@@ -86,13 +86,42 @@ void setBoardConfigOverrides() {
 
 float getAnalogInputDividerCoefficient(adc_channel_e hwChannel)
 {
-	/* MAP or TPS */
+	/* MAP, TPS, MAF (na), AC press (na) */
 	if ((hwChannel == EFI_ADC_0) ||
-		(hwChannel == EFI_ADC_1)) {
-		// 22K (high) + 41.2K (low) on TPS and MAP
-		return (22.0 + 41.2) / 22.0;
+		(hwChannel == EFI_ADC_1) ||
+		(hwChannel == EFI_ADC_11) ||
+		(hwChannel == EFI_ADC_6)) {
+		return (22.0 + 41.2) / 41.2;
 	}
 
-	// 22K (high) + 22K (low) on IAT and CLT
-	return (22.0 + 22.0) / 22.0;
+	/* IAT, CLT, AFR 1, AFR 2 (na), knock */
+	if ((hwChannel == EFI_ADC_3) ||
+		(hwChannel == EFI_ADC_14) ||
+		(hwChannel == EFI_ADC_7) ||
+		(hwChannel == EFI_ADC_5) ||
+		(hwChannel == EFI_ADC_2)) {
+		/* no divider, 1.5K pull-up to 3.3V */
+		return 1.0;
+	}
+
+	/* +12 sense, Ignition switch */
+	if ((hwChannel == EFI_ADC_9) ||
+		(hwChannel == EFI_ADC_8)) {
+		return (68.0 + 6.8) / 6.8;
+	}
+
+	/* 5V feedback */
+	if (hwChannel == EFI_ADC_10) {
+		return (10.0 + 10.0) / 10.0;
+	}
+
+	/* Ignition feedback */
+	if ((hwChannel == EFI_ADC_12) ||
+		(hwChannel == EFI_ADC_13)) {
+		/* TBD */
+		return 1.0;
+	}
+
+	/* for all others */
+	return engineConfiguration->analogInputDividerCoefficient;
 }
