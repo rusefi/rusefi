0b820e8dd312e2a65ee7d4a8bfd0f33ebc053136
rusefillc <sdfsdfqsf2334234234>
2023-01-16 21:08:52 -0500

    happy NY

--- java_console/ui/src/main/java/com/rusefi/StartupFrame.java
+++ java_console/ui/src/main/java/com/rusefi/StartupFrame.java
@@ -49,7 +49,7 @@ public class StartupFrame {
 
     public static final String LOGO_PATH = "/com/rusefi/";
     private static final String LOGO = LOGO_PATH + "logo.png";
-    public static final String LINK_TEXT = "rusEFI (c) 2012-2022";
+    public static final String LINK_TEXT = "rusEFI (c) 2012-2023";
     private static final String URI = "http://rusefi.com/?java_console";
     // private static final int RUSEFI_ORANGE = 0xff7d03;
 
