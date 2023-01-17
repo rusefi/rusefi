292b699f9af6af0862e3f29bb5f375dad2be61f1
rusefillc <sdfsdfqsf2334234234>
2023-01-16 22:47:06 -0500

    helping kinetis

---
+++ java_tools/configuration_definition/src/main/java/com/rusefi/output/variables/VariableRecord.java
@@ -0,0 +1,30 @@
+package com.rusefi.output.variables;
+
+import com.rusefi.core.Pair;
+
+public class VariableRecord {
+    public final String type;
+    private final String humanName;
+    private final String fullName;
+    private final String conditional;
+
+
+    public VariableRecord(String humanName, String fullName, String type, String conditional) {
+        this.humanName = humanName;
+        this.fullName = fullName;
+        this.type = type;
+        this.conditional = conditional;
+    }
+
+    public String getUserName() {
+        return humanName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getConditional() {
+        return conditional;
+    }
+}
