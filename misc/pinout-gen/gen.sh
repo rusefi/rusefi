be3c8d50d88e497368a3ebe7a2d20ec1efbd8727
David Holdeman <david@chuckwagoncomputing.com>
2023-01-17 16:48:32 -0600

    multiply backslashes so they don't get swallowed

--- misc/pinout-gen/gen.sh
+++ misc/pinout-gen/gen.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)"/"
-TEXT=$(sed -e "/###CSS###/{r ${DIR}style.css" -e 'd}' -e "/###JS###/{r ${DIR}script.js" -e 'd}' ${DIR}pinout.html | sed -e "s/\/\/\/DATA\/\/\//\`$(echo ${1//\//\\/} | tr -d '\n')\`,\n\/\/\/DATA\/\/\//")
+JSON="$(echo $1 | sed 's/\\/\\\\\\\\/g')"
+TEXT=$(sed -e "/###CSS###/{r ${DIR}style.css" -e 'd}' -e "/###JS###/{r ${DIR}script.js" -e 'd}' ${DIR}pinout.html | sed -e "s/\/\/\/DATA\/\/\//\`$(echo ${JSON//\//\\/} | tr -d '\n')\`,\n\/\/\/DATA\/\/\//")
 if [ $? -ne 0 ]; then
   echo "Error in gen.sh"
   exit 1;
