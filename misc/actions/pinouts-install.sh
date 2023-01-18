69b8eb327a55a26207a8dedb5814264040f271b8
rusefillc <sdfsdfqsf2334234234>
2023-01-18 01:26:50 -0500

    Update yq, fix bug in pinout generation #4977
    revert

--- misc/actions/pinouts-install.sh
+++ misc/actions/pinouts-install.sh
@@ -2,7 +2,7 @@
 
         sudo apt-get update
         sudo apt-get install sshpass
-        wget https://github.com/mikefarah/yq/releases/download/v4.30.8/yq_linux_amd64
+        wget https://github.com/mikefarah/yq/releases/download/v4.9.6/yq_linux_amd64
         chmod a+x yq_linux_amd64
         rm -rf yqdir
         mkdir yqdir
