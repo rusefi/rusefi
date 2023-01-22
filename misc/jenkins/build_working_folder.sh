d99af48d2109ca20bb03c31d529c1cb106c5d9a4
David Holdeman <david@chuckwagoncomputing.com>
2023-01-21 19:51:39 -0600

    put in subdir named after branch

--- misc/jenkins/build_working_folder.sh
+++ misc/jenkins/build_working_folder.sh
@@ -116,8 +116,8 @@ ls -l $FULL_BUNDLE_FILE
 if [ -n "$RUSEFI_SSH_USER" ]; then
  echo "$SCRIPT_NAME: Uploading full bundle"
  retVal=0
- if [ "$1" = "true" ]; then
-   tar -czf - $FULL_BUNDLE_FILE  | sshpass -p $RUSEFI_SSH_PASS ssh -o StrictHostKeyChecking=no $RUSEFI_SSH_USER@$RUSEFI_SSH_SERVER "tar -xzf - -C build_server/lts"
+ if [ -n "$1" ]; then
+   tar -czf - $FULL_BUNDLE_FILE  | sshpass -p $RUSEFI_SSH_PASS ssh -o StrictHostKeyChecking=no $RUSEFI_SSH_USER@$RUSEFI_SSH_SERVER "mkdir -p build_server/lts/$1; tar -xzf - -C build_server/lts/$1"
    retVal=$?
  else
    tar -czf - $FULL_BUNDLE_FILE  | sshpass -p $RUSEFI_SSH_PASS ssh -o StrictHostKeyChecking=no $RUSEFI_SSH_USER@$RUSEFI_SSH_SERVER "tar -xzf - -C build_server"
