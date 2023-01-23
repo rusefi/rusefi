8d52aa3e1cfd82c35c2e2ffc81f5c8d27cc393ce
David Holdeman <david@chuckwagoncomputing.com>
2023-01-23 17:37:20 -0600

    check for downloaded toolkit

--- firmware/provide_gcc.sh
+++ firmware/provide_gcc.sh
@@ -6,6 +6,7 @@ set -e
 
 # URL to download original toolchain from
 URL="https://github.com/rusefi/build_support/raw/master/rusefi-arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi.tar.xz"
+MANIFEST_SUM="e4b25a623445c9f02303eba63b1d2806"
 # colloquial directory name, to afford re-use of script
 COLLOQUIAL="gcc-arm-none-eabi"
 # temporary working directory
@@ -13,8 +14,11 @@ TMP_DIR="/tmp/rusefi-provide_gcc"
 
 archive="${URL##*/}"
 
-# Cleanup prior [failed] runs
-rm -rf "${TMP_DIR}"
+if [ -d "${TMP_DIR}" ]; then
+	if [ "$(md5sum ${TMP_DIR}/*manifest.txt | cut -d ' ' -f 1)" = "$MANIFEST_SUM" ]; then
+		exit 0
+	fi
+fi
 
 # Download and extract archive
 echo Downloading and extracting ${archive}
@@ -29,6 +33,3 @@ archive_dir="$(echo *)"
 cd - >/dev/null
 mv "${TMP_DIR}/${archive_dir}" "$(pwd)"
 ln -s "${archive_dir%/}" "${COLLOQUIAL}"
-
-# Delete downloaded archive
-rm -rf "${TMP_DIR}"
