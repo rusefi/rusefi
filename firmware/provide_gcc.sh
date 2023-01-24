ab72e778d43ce72f8769a09fdebaae303451d3fa
David Holdeman <david@chuckwagoncomputing.com>
2023-01-24 16:38:10 -0600

    maybe the path is right this time

--- firmware/provide_gcc.sh
+++ firmware/provide_gcc.sh
@@ -6,7 +6,7 @@ set -e
 
 # URL to download original toolchain from
 URL="https://github.com/rusefi/build_support/raw/master/rusefi-arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi.tar.xz"
-# This is the md5sum of the /bin/ld executable within the archive, used for verifying we have the proper version.
+# This is the md5sum of the /bin/arm-none-eabi-ld executable within the archive, used for verifying we have the proper version.
 # If you change the above URL, you will need to update this checksum as well.
 MANIFEST_SUM="8e50ee1adb41acfd56fc38d74d6bb18e"
 # colloquial directory name, to afford re-use of script
@@ -19,8 +19,8 @@ archive="${URL##*/}"
 SWD="$PWD"
 
 # If the checksum file doesn't exist, or if its checksum doesn't match, then download and install the archive.
-if [ ! -f "${TMP_DIR}"/*/bin/ld ] ||\
-   [ "$MANIFEST_SUM" != "$(md5sum ${TMP_DIR}/*/bin/ld | cut -d ' ' -f 1)" ]; then
+if [ ! -f "${TMP_DIR}"/*/bin/arm-none-eabi-ld ] ||\
+   [ "$MANIFEST_SUM" != "$(md5sum ${TMP_DIR}/*/bin/arm-none-eabi-ld | cut -d ' ' -f 1)" ]; then
 	rm -rf "${TMP_DIR}"
 	# Download and extract archive
 	echo Downloading and extracting ${archive}
