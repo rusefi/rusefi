28ab2d6f8a4a7bd7d6d6d556155ee7086f350188
benas-gavea <bbrazdziunas@gavea.co.uk>
2023-01-27 12:41:26 +0000

    Initial work on dual ADC wideband

--- firmware/init/sensor/init_lambda.cpp
+++ firmware/init/sensor/init_lambda.cpp
@@ -7,7 +7,10 @@
 
 struct GetAfrWrapper {
 	float getLambda() {
-		return getAfr() / 14.7f;
+		return getAfr(SensorType::Lambda1) / 14.7f;
+	};
+	float getLambda2() {
+		return getAfr(SensorType::Lambda2) / 14.7f;
 	}
 };
 
