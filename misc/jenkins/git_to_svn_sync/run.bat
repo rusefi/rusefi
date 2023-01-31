d36479d260fa6a4b379fc1992ec687f0da8ece30
rusefillc <sdfsdfqsf2334234234>
2023-01-31 08:07:10 -0500

    we definitely do not sync from git into SVN anymore

--- misc/jenkins/git_to_svn_sync/run.bat
+++
@@ -1,7 +0,0 @@
-rm -rf C:\stuff\rusefi.git2svn
-mkdir C:\stuff\rusefi.git2svn
-mkdir C:\stuff\rusefi.git2svn\git
-cd C:\stuff\rusefi.git2svn\git
-git clone https://github.com/rusefi/rusefi
-cd C:\stuff\rusefi.git2svn\git\rusefi
-call misc/jenkins/git_to_svn_sync/git2svn.bat
