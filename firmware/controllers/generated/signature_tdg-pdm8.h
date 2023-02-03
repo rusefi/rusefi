836144913deeb7b5e09e88da02dbff0cc7505af2
GitHub gen-configs Action <action@github.com>
2023-02-03 22:16:06 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_tdg-pdm8.h
+++ firmware/controllers/generated/signature_tdg-pdm8.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2848356575
-#define TS_SIGNATURE "rusEFI master.2023.02.03.tdg-pdm8.2848356575"
+#define SIGNATURE_HASH 3330845672
+#define TS_SIGNATURE "rusEFI master.2023.02.03.tdg-pdm8.3330845672"
