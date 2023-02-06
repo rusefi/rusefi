9ae89ee49705f7336fcc76c8456ec6bee2d3fc92
rusefillc <48498823+rusefillc@users.noreply.github.com>
2023-02-06 19:05:38 -0400

    Als timer smaller (#5041)
    * Update default_base_engine.cpp
    
    (cherry picked from commit 8ac64e2a16f18b7497c3fa1993a4d3e31a161332)
    
    * als timer meta
    
    ---------
    
    Co-authored-by: TurboMarian <podgajnymariusz@gmail.com>
    Co-authored-by: rusefillc <sdfsdfqsf2334234234>

--- firmware/controllers/algo/defaults/default_base_engine.cpp
+++ firmware/controllers/algo/defaults/default_base_engine.cpp
@@ -44,7 +44,7 @@ void setDefaultBaseEngine() {
 
 	engineConfiguration->ALSMinRPM = 400;
 	engineConfiguration->ALSMaxRPM = 3200;
-	engineConfiguration->alsMaxDuration = 3.5;
+	engineConfiguration->ALSMaxDuration = 3.5;
 	engineConfiguration->ALSMaxCLT = 105;
 //	engineConfiguration->alsMinPps = 10;
 	engineConfiguration->alsMinTimeBetween = 5;
