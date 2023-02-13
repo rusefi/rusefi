e776a39ecc1eb4e77d7da6ae305374287175bce3
rusefillc <sdfsdfqsf2334234234>
2023-02-13 18:39:57 -0500

    Nick wonders if DC wastegate is still FUBAR #5075

--- java_console/io/src/main/java/com/rusefi/ldmp/StateDictionary.java
+++ java_console/io/src/main/java/com/rusefi/ldmp/StateDictionary.java
@@ -45,6 +45,7 @@ public enum StateDictionary {
         register(live_data_e.LDS_trigger_state_primary, TriggerStatePrimary.VALUES, "trigger_decoder");
         register(live_data_e.LDS_fuel_computer, FuelComputer.VALUES, "fuel_computer");
         register(live_data_e.LDS_antilag_system_state, AntilagSystem.VALUES, "antilag_system");
+        register(live_data_e.LDS_dc_motors, DcMotors.VALUES, "dc_motors");
         if (map.size() != live_data_e.values().length) {
             Set<live_data_e> missing = new HashSet<>(Arrays.asList(live_data_e.values()));
             missing.removeAll(map.keySet());
