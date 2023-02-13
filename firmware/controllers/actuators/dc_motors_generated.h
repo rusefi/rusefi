102e984efa7995427e92b3e2ded53587ed239292
Andrey <rusefillc@gmail.com>
2023-02-13 17:59:45 -0500

    Nick wonders if DC wastegate is still FUBAR #5075

---
+++ firmware/controllers/actuators/dc_motors_generated.h
@@ -0,0 +1,114 @@
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/actuators/dc_motors.txt Mon Feb 13 17:54:48 EST 2023
+// by class com.rusefi.output.CHeaderConsumer
+// begin
+#pragma once
+#include "rusefi_types.h"
+// start of dc_motors_s
+struct dc_motors_s {
+	/**
+	 * "DC: output0"
+	per
+	 * offset 0
+	 */
+	float dcOutput0 = (float)0;
+	/**
+	offset 4 bit 0 */
+	bool isEnabled0 : 1 {};
+	/**
+	offset 4 bit 1 */
+	bool unusedBit_2_1 : 1 {};
+	/**
+	offset 4 bit 2 */
+	bool unusedBit_2_2 : 1 {};
+	/**
+	offset 4 bit 3 */
+	bool unusedBit_2_3 : 1 {};
+	/**
+	offset 4 bit 4 */
+	bool unusedBit_2_4 : 1 {};
+	/**
+	offset 4 bit 5 */
+	bool unusedBit_2_5 : 1 {};
+	/**
+	offset 4 bit 6 */
+	bool unusedBit_2_6 : 1 {};
+	/**
+	offset 4 bit 7 */
+	bool unusedBit_2_7 : 1 {};
+	/**
+	offset 4 bit 8 */
+	bool unusedBit_2_8 : 1 {};
+	/**
+	offset 4 bit 9 */
+	bool unusedBit_2_9 : 1 {};
+	/**
+	offset 4 bit 10 */
+	bool unusedBit_2_10 : 1 {};
+	/**
+	offset 4 bit 11 */
+	bool unusedBit_2_11 : 1 {};
+	/**
+	offset 4 bit 12 */
+	bool unusedBit_2_12 : 1 {};
+	/**
+	offset 4 bit 13 */
+	bool unusedBit_2_13 : 1 {};
+	/**
+	offset 4 bit 14 */
+	bool unusedBit_2_14 : 1 {};
+	/**
+	offset 4 bit 15 */
+	bool unusedBit_2_15 : 1 {};
+	/**
+	offset 4 bit 16 */
+	bool unusedBit_2_16 : 1 {};
+	/**
+	offset 4 bit 17 */
+	bool unusedBit_2_17 : 1 {};
+	/**
+	offset 4 bit 18 */
+	bool unusedBit_2_18 : 1 {};
+	/**
+	offset 4 bit 19 */
+	bool unusedBit_2_19 : 1 {};
+	/**
+	offset 4 bit 20 */
+	bool unusedBit_2_20 : 1 {};
+	/**
+	offset 4 bit 21 */
+	bool unusedBit_2_21 : 1 {};
+	/**
+	offset 4 bit 22 */
+	bool unusedBit_2_22 : 1 {};
+	/**
+	offset 4 bit 23 */
+	bool unusedBit_2_23 : 1 {};
+	/**
+	offset 4 bit 24 */
+	bool unusedBit_2_24 : 1 {};
+	/**
+	offset 4 bit 25 */
+	bool unusedBit_2_25 : 1 {};
+	/**
+	offset 4 bit 26 */
+	bool unusedBit_2_26 : 1 {};
+	/**
+	offset 4 bit 27 */
+	bool unusedBit_2_27 : 1 {};
+	/**
+	offset 4 bit 28 */
+	bool unusedBit_2_28 : 1 {};
+	/**
+	offset 4 bit 29 */
+	bool unusedBit_2_29 : 1 {};
+	/**
+	offset 4 bit 30 */
+	bool unusedBit_2_30 : 1 {};
+	/**
+	offset 4 bit 31 */
+	bool unusedBit_2_31 : 1 {};
+};
+static_assert(sizeof(dc_motors_s) == 8);
+
+// end
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/actuators/dc_motors.txt Mon Feb 13 17:54:48 EST 2023
