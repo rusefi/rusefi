97077487f3701c0e848d7d476d1b71f1e4e877d4
Nathan Schulte <nmschulte@desmas.net>
2023-02-16 00:44:01 -0600

    meld RedundantSensor unit tests

--- unit_tests/tests/sensor/redundant.cpp
+++ unit_tests/tests/sensor/redundant.cpp
@@ -24,7 +24,7 @@ protected:
 		ASSERT_TRUE(m1.Register());
 		ASSERT_TRUE(m2.Register());
 
-		dut.configure(5.0f, false);
+		dut.configure(5.0f, false, 100);
 	}
 
 	void TearDown() override
@@ -157,7 +157,7 @@ protected:
 		ASSERT_TRUE(m1.Register());
 		ASSERT_TRUE(m2.Register());
 
-		dut.configure(5.0f, true);
+		dut.configure(5.0f, true, 100);
 	}
 
 	void TearDown() override
@@ -230,13 +230,13 @@ TEST_F(SensorRedundantIgnoreSecond, SetBothIgnoreSecond)
 	}
 }
 
-class SensorFordRedundantTps : public ::testing::Test
+class SensorRedundantPartialSecond : public ::testing::Test
 {
 protected:
-	RedundantFordTps dut;
+	RedundantSensor dut;
 	MockSensor m1, m2;
 
-	SensorFordRedundantTps()
+	SensorRedundantPartialSecond()
 		: dut(SensorType::Tps1, SensorType::Tps1Primary, SensorType::Tps1Secondary)
 		, m1(SensorType::Tps1Primary)
 		, m2(SensorType::Tps1Secondary)
@@ -252,7 +252,7 @@ protected:
 		ASSERT_TRUE(m1.Register());
 		ASSERT_TRUE(m2.Register());
 
-		dut.configure(5.0f, 50);
+		dut.configure(5.0f, false, 50);
 	}
 
 	void TearDown() override
@@ -261,7 +261,14 @@ protected:
 	}
 };
 
-TEST_F(SensorFordRedundantTps, SetOnlyOneSensor)
+TEST_F(SensorRedundantPartialSecond, CheckIsRedundant)
+{
+	{
+		EXPECT_TRUE(dut.isRedundant());
+	}
+}
+
+TEST_F(SensorRedundantPartialSecond, SetOnlyOneSensor)
 {
 	// Don't set any sensors - expect invalid
 	{
@@ -269,7 +276,7 @@ TEST_F(SensorFordRedundantTps, SetOnlyOneSensor)
 		EXPECT_FALSE(result.Valid);
 	}
 
-	// Set one sensor
+	// Set first sensor
 	m1.set(24.0f);
 
 	// Should still be invalid - only one is set!
@@ -279,7 +286,7 @@ TEST_F(SensorFordRedundantTps, SetOnlyOneSensor)
 	}
 }
 
-TEST_F(SensorFordRedundantTps, SetTwoSensors)
+TEST_F(SensorRedundantPartialSecond, SetTwoSensors)
 {
 	// Don't set any sensors - expect invalid
 	{
@@ -287,9 +294,9 @@ TEST_F(SensorFordRedundantTps, SetTwoSensors)
 		EXPECT_FALSE(result.Valid);
 	}
 
-	// Set one sensor
+	// Set first sensor
 	m1.set(12.0f);
-	// Set the other sensor at double the first
+	// Set second sensor at double the first
 	m2.set(28.0f);
 
 	// Should now be valid - and the average of the two input
@@ -297,12 +304,10 @@ TEST_F(SensorFordRedundantTps, SetTwoSensors)
 		auto result = dut.get();
 		EXPECT_TRUE(result.Valid);
 		EXPECT_FLOAT_EQ(result.Value, 13.0f);
-
-		EXPECT_TRUE(dut.isRedundant());
 	}
 }
 
-TEST_F(SensorFordRedundantTps, DifferenceNone)
+TEST_F(SensorRedundantPartialSecond, DifferenceNone)
 {
 	// Set both sensors to the same value
 	m1.set(10);
@@ -316,7 +321,7 @@ TEST_F(SensorFordRedundantTps, DifferenceNone)
 	}
 }
 
-TEST_F(SensorFordRedundantTps, DifferenceNearLimit)
+TEST_F(SensorRedundantPartialSecond, DifferenceNearLimit)
 {
 	// Set both sensors to nearly the limit (4.998 apart)
 	m1.set(7.501f);
@@ -330,7 +335,7 @@ TEST_F(SensorFordRedundantTps, DifferenceNearLimit)
 	}
 }
 
-TEST_F(SensorFordRedundantTps, DifferenceOverLimit)
+TEST_F(SensorRedundantPartialSecond, DifferenceOverLimit)
 {
 	// Set both sensors barely over the limit (5.002 apart)
 	m1.set(7.499f);
@@ -343,7 +348,7 @@ TEST_F(SensorFordRedundantTps, DifferenceOverLimit)
 	}
 }
 
-TEST_F(SensorFordRedundantTps, DifferenceOverLimitSwapped)
+TEST_F(SensorRedundantPartialSecond, DifferenceOverLimitSwapped)
 {
 	// Now try it the other way (m1 > m2)
 	m1.set(12.501f);
@@ -356,9 +361,9 @@ TEST_F(SensorFordRedundantTps, DifferenceOverLimitSwapped)
 	}
 }
 
-TEST_F(SensorFordRedundantTps, HighRange)
+TEST_F(SensorRedundantPartialSecond, PartialRedundancyRange)
 {
-	// Set the throttle like it's at 75%
+	// Set the value like it's at 75%
 	m1.set(75);
 	m2.set(100);
 
