fa08a8cb6fb753d391a6bd48b26da8192bd3775d
andreika-git <andrei.home@gmail.com>
2023-02-18 15:45:40 +0200

    https://github.com/rusefi/hellen125honda-issues/issues/27

--- firmware/hw_layer/kline.h
+++ firmware/hw_layer/kline.h
@@ -2,6 +2,8 @@
 
 
 #define KLINE_SERIAL_DEVICE (&SD2)
+#define KLINE_SERIAL_DEVICE_RX H144_UART2_RX
+#define KLINE_SERIAL_DEVICE_TX H144_UART2_TX
 
 // The standard transmission rate
 #define KLINE_BAUD_RATE 10400
