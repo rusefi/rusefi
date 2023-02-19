27a0e9005b6e45345f6d22819205a399657f0e3e
GitHub gen-configs Action <action@github.com>
2023-02-19 23:33:02 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen-gm-e67.h
+++ firmware/controllers/generated/signature_hellen-gm-e67.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1512545983
-#define TS_SIGNATURE "rusEFI master.2023.02.19.hellen-gm-e67.1512545983"
+#define SIGNATURE_HASH 2114665978
+#define TS_SIGNATURE "rusEFI master.2023.02.19.hellen-gm-e67.2114665978"
