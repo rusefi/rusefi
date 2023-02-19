b9ad8e77890d0b1d53a94af20b3b24ab385e3b99
GitHub gen-configs Action <action@github.com>
2023-02-19 23:33:02 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen121nissan.h
+++ firmware/controllers/generated/signature_hellen121nissan.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2612553381
-#define TS_SIGNATURE "rusEFI master.2023.02.19.hellen121nissan.2612553381"
+#define SIGNATURE_HASH 869604388
+#define TS_SIGNATURE "rusEFI master.2023.02.19.hellen121nissan.869604388"
--- firmware/controllers/generated/signature_hellen154hyundai.h
+++ firmware/controllers/generated/signature_hellen154hyundai.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3611644593
-#define TS_SIGNATURE "rusEFI master.2023.02.19.hellen154hyundai.3611644593"
+#define SIGNATURE_HASH 2069902044
+#define TS_SIGNATURE "rusEFI master.2023.02.19.hellen154hyundai.2069902044"
