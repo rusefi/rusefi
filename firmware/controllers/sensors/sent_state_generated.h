3752038667b89905d23007a4dc18170a49baefae
GitHub gen-configs Action <action@github.com>
2023-02-20 16:40:01 +0000

    Auto-generated configs and docs

--- firmware/controllers/sensors/sent_state_generated.h
+++ firmware/controllers/sensors/sent_state_generated.h
@@ -1,4 +1,4 @@
-// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/sensors//sent_state.txt Wed Feb 15 00:55:51 UTC 2023
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/sensors//sent_state.txt Mon Feb 20 16:39:15 UTC 2023
 // by class com.rusefi.output.CHeaderConsumer
 // begin
 #pragma once
@@ -6,15 +6,17 @@
 // start of sent_state_s
 struct sent_state_s {
 	/**
+	 * "ETB: SENT value0"
+	value
 	 * offset 0
 	 */
-	uint16_t value1 = (uint16_t)0;
+	uint16_t value0 = (uint16_t)0;
 	/**
-	 * need 4 byte alignment
-	units
+	 * "ETB: SENT value1"
+	value
 	 * offset 2
 	 */
-	uint8_t alignmentFill_at_2[2];
+	uint16_t value1 = (uint16_t)0;
 	/**
 	 * "ETB: SENT error rate"
 	ratio
@@ -25,4 +27,4 @@ struct sent_state_s {
 static_assert(sizeof(sent_state_s) == 8);
 
 // end
-// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/sensors//sent_state.txt Wed Feb 15 00:55:51 UTC 2023
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/sensors//sent_state.txt Mon Feb 20 16:39:15 UTC 2023
