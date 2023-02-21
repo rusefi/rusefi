6bcdbf303ffec7f9486d5156d5aa154d08f70444
Matthew Kennedy <matthewkennedy@outlook.com>
2023-02-20 20:28:16 -0800

    cleanup

--- firmware/gen_config.sh
+++ firmware/gen_config.sh
@@ -11,12 +11,6 @@ mkdir build
 
 
 # todo: we have a bit of code duplication with build-firmware.yaml here :(
-config/boards/kinetis/config/gen_kinetis_config.sh
-[ $? -eq 0 ] || { echo "ERROR generating board kinetis kin"; exit 1; }
-
-config/boards/cypress/config/gen_cypress_config.sh
-[ $? -eq 0 ] || { echo "ERROR generating board cypress cypress"; exit 1; }
-
 config/boards/subaru_eg33/config/gen_subaru_config.sh
 [ $? -eq 0 ] || { echo "ERROR generating board subaru_eg33 subaru_eg33_f7"; exit 1; }
 
