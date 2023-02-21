2881bfa0f798feed984206d8df66bb30ff457146
Andrey <rusefillc@gmail.com>
2023-02-20 21:21:03 -0500

    moving sensors files a bit

--- firmware/controllers/sensors/sensors.mk
+++ firmware/controllers/sensors/sensors.mk
@@ -1,23 +1,31 @@
 
-CONTROLLERS_SENSORS_SRC_CPP = 	$(PROJECT_DIR)/controllers/sensors/thermistors.cpp \
+CONTROLLERS_SENSORS_SRC_CPP = \
+	$(PROJECT_DIR)/controllers/sensors/core/functional_sensor.cpp \
+	$(PROJECT_DIR)/controllers/sensors/core/sensor.cpp \
+ 	$(PROJECT_DIR)/controllers/sensors/thermistors.cpp \
 	$(PROJECT_DIR)/controllers/sensors/allsensors.cpp \
 	$(PROJECT_DIR)/controllers/sensors/auto_generated_sensor.cpp \
-	$(PROJECT_DIR)/controllers/sensors/map.cpp \
-	$(PROJECT_DIR)/controllers/sensors/maf.cpp \
+	$(PROJECT_DIR)/controllers/sensors/impl/AemXSeriesLambda.cpp \
+	$(PROJECT_DIR)/controllers/sensors/impl/map.cpp \
+	$(PROJECT_DIR)/controllers/sensors/impl/maf.cpp \
+	$(PROJECT_DIR)/controllers/sensors/impl/Lps25Sensor.cpp \
 	$(PROJECT_DIR)/controllers/sensors/tps.cpp \
-	$(PROJECT_DIR)/controllers/sensors/ego.cpp \
-	$(PROJECT_DIR)/controllers/sensors/sensor.cpp \
+	$(PROJECT_DIR)/controllers/sensors/impl/ego.cpp \
 	$(PROJECT_DIR)/controllers/sensors/sensor_info_printing.cpp \
 	$(PROJECT_DIR)/controllers/sensors/sensor_checker.cpp \
-	$(PROJECT_DIR)/controllers/sensors/functional_sensor.cpp \
 	$(PROJECT_DIR)/controllers/sensors/redundant_sensor.cpp \
 	$(PROJECT_DIR)/controllers/sensors/redundant_ford_tps.cpp \
-	$(PROJECT_DIR)/controllers/sensors/AemXSeriesLambda.cpp \
 	$(PROJECT_DIR)/controllers/sensors/frequency_sensor.cpp \
-	$(PROJECT_DIR)/controllers/sensors/software_knock.cpp \
-	$(PROJECT_DIR)/controllers/sensors/Lps25Sensor.cpp \
+	$(PROJECT_DIR)/controllers/sensors/impl/software_knock.cpp \
 	$(PROJECT_DIR)/controllers/sensors/converters/linear_func.cpp \
 	$(PROJECT_DIR)/controllers/sensors/converters/resistance_func.cpp \
 	$(PROJECT_DIR)/controllers/sensors/converters/thermistor_func.cpp \
 	$(PROJECT_DIR)/controllers/sensors/converters/identity_func.cpp \
 	$(PROJECT_DIR)/controllers/sensors/vr_pwm.cpp
+
+
+CONTROLLERS_SENSORS_INC=\
+    $(CONTROLLERS_DIR)/sensors \
+    $(CONTROLLERS_DIR)/sensors/core \
+    $(CONTROLLERS_DIR)/sensors/impl \
+	$(CONTROLLERS_DIR)/sensors/converters
