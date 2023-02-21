d269139aa429a57e66b24d4c3a5f385725a74383
Matthew Kennedy <matthewkennedy@outlook.com>
2023-02-21 01:11:34 -0800

    cleanups

--- misc/jenkins/build_java_console.sh
+++ misc/jenkins/build_java_console.sh
@@ -16,11 +16,3 @@ cd ..
 
 [ -e java_console_binary/rusefi_console.jar ] || { echo "CONSOLE COMPILATION FAILED"; exit 1; }
 echo "java console looks good"
-
-echo "Building TS plugin"
-cd java_tools
-./gradlew :ts_plugin_launcher:shadowJar
-cd ..
-
-[ -e java_tools/ts_plugin_launcher/build/jar/rusefi_ts_plugin_launcher.jar ] || { echo "PLUGIN COMPILATION FAILED"; exit 1; }
-echo "TS plugin looks good"
