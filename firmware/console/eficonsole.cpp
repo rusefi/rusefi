3fe5e8f3e3d50976e04bbb2e9cb3dc7980fd6900
Matthew Kennedy <matthewkennedy@outlook.com>
2023-02-22 13:57:31 -0800

    unnecessary printouts at boot

--- firmware/console/eficonsole.cpp
+++ firmware/console/eficonsole.cpp
@@ -84,58 +84,6 @@ static void sayHello() {
 	efiPrintf("MCU rev=%x flashSize=%d", mcuRevision, flashSize);
 #endif
 
-
-#ifdef CH_FREQUENCY
-	efiPrintf("CH_FREQUENCY=%d", CH_FREQUENCY);
-#endif
-
-#ifdef CORTEX_MAX_KERNEL_PRIORITY
-	efiPrintf("CORTEX_MAX_KERNEL_PRIORITY=%d", CORTEX_MAX_KERNEL_PRIORITY);
-#endif
-
-#ifdef STM32_ADCCLK
-	efiPrintf("STM32_ADCCLK=%d", STM32_ADCCLK);
-	efiPrintf("STM32_TIMCLK1=%d", STM32_TIMCLK1);
-	efiPrintf("STM32_TIMCLK2=%d", STM32_TIMCLK2);
-#endif
-#ifdef STM32_PCLK1
-	efiPrintf("STM32_PCLK1=%d", STM32_PCLK1);
-	efiPrintf("STM32_PCLK2=%d", STM32_PCLK2);
-#endif
-
-	efiPrintf("PORT_IDLE_THREAD_STACK_SIZE=%d", PORT_IDLE_THREAD_STACK_SIZE);
-
-	efiPrintf("CH_DBG_ENABLE_ASSERTS=%d", CH_DBG_ENABLE_ASSERTS);
-#ifdef CH_DBG_ENABLED
-	efiPrintf("CH_DBG_ENABLED=%d", CH_DBG_ENABLED);
-#endif
-	efiPrintf("CH_DBG_SYSTEM_STATE_CHECK=%d", CH_DBG_SYSTEM_STATE_CHECK);
-	efiPrintf("CH_DBG_ENABLE_STACK_CHECK=%d", CH_DBG_ENABLE_STACK_CHECK);
-
-#ifdef EFI_LOGIC_ANALYZER
-	efiPrintf("EFI_LOGIC_ANALYZER=%d", EFI_LOGIC_ANALYZER);
-#endif
-#ifdef EFI_TUNER_STUDIO
-	efiPrintf("EFI_TUNER_STUDIO=%d", EFI_TUNER_STUDIO);
-#else
-	efiPrintf("EFI_TUNER_STUDIO=%d", 0);
-#endif
-
-#ifdef EFI_SIGNAL_EXECUTOR_SLEEP
-	efiPrintf("EFI_SIGNAL_EXECUTOR_SLEEP=%d", EFI_SIGNAL_EXECUTOR_SLEEP);
-#endif
-
-#ifdef EFI_SIGNAL_EXECUTOR_HW_TIMER
-	efiPrintf("EFI_SIGNAL_EXECUTOR_HW_TIMER=%d", EFI_SIGNAL_EXECUTOR_HW_TIMER);
-#endif
-
-#if defined(EFI_SHAFT_POSITION_INPUT)
-	efiPrintf("EFI_SHAFT_POSITION_INPUT=%d", EFI_SHAFT_POSITION_INPUT);
-#endif
-#ifdef EFI_INTERNAL_ADC
-	efiPrintf("EFI_INTERNAL_ADC=%d", EFI_INTERNAL_ADC);
-#endif
-
 	/**
 	 * Time to finish output. This is needed to avoid mix-up of this methods output and console command confirmation
 	 */
