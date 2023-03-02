93fc5b2cebe1e11e73def67a8a6ea2ad4b72defe
Matthew Kennedy <matthewkennedy@outlook.com>
2023-03-02 14:52:36 -0500

    use shared txt instead of parsing full rusefi_config (#59)
    (cherry picked from commit fbb90f2c040442bc10787b9d722e477a7572d75c)

--- firmware/gen_config_common.sh
+++ firmware/gen_config_common.sh
@@ -21,6 +21,7 @@ COMMON_GEN_CONFIG="
  -signature tunerstudio/generated/signature_${SHORT_BOARDNAME}.txt \
  -signature_destination controllers/generated/signature_${SHORT_BOARDNAME}.h \
  -ts_output_name generated/${INI} \
+ -prepend integration/rusefi_config_shared.txt \
  -prepend ${BOARD_DIR}/prepend.txt \
  -board ${BOARD_DIR} \
  -definition integration/rusefi_config.txt"
