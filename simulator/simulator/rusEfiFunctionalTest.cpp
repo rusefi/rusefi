f0bacdf5c4b4d2fa43f52723ee04236bd7c50eb0
Andrey <rusefillc@gmail.com>
2023-03-05 00:57:38 -0500

    reducing weird? only:hellen-honda-k

--- simulator/simulator/rusEfiFunctionalTest.cpp
+++ simulator/simulator/rusEfiFunctionalTest.cpp
@@ -21,6 +21,7 @@
 #include "bench_test.h"
 #include "tunerstudio.h"
 #include "map_averaging.h"
+#include "mmc_card.h"
 #include "memstreams.h"
 #include <chprintf.h>
 #include "rusefi_lua.h"
@@ -116,7 +117,6 @@ void rusEfiFunctionalTest(void) {
 
 	startLoggingProcessor();
 
-	void initMmcCard();
 	initMmcCard();
 
     /**
