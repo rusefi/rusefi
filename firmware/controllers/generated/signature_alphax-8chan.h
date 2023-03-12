8ac28e245035f5d68814376c5fb4ff6dac711c05
GitHub gen-configs Action <action@github.com>
2023-03-12 03:55:46 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_alphax-8chan.h
+++ firmware/controllers/generated/signature_alphax-8chan.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2900596091
-#define TS_SIGNATURE "rusEFI master.2023.03.12.alphax-8chan.2900596091"
+#define SIGNATURE_HASH 1558203733
+#define TS_SIGNATURE "rusEFI master.2023.03.12.alphax-8chan.1558203733"
--- firmware/controllers/generated/signature_hellen-honda-k.h
+++ firmware/controllers/generated/signature_hellen-honda-k.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2170257803
-#define TS_SIGNATURE "rusEFI master.2023.03.12.hellen-honda-k.2170257803"
+#define SIGNATURE_HASH 1262238002
+#define TS_SIGNATURE "rusEFI master.2023.03.12.hellen-honda-k.1262238002"
