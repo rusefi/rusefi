cfce02f151402e517ee0c7322c0f130e44215974
David Holdeman <dave@holdemanenterprises.com>
2023-03-13 15:40:24 -0500

    Upgrade to Kicad 7 (#5178)
    * update to kicad 7
    
    * update rusefi_lib
    
    * update InteractiveHtmlBom
    
    * use my branch for now

--- misc/jenkins/InteractiveHtmlBom/run.sh
+++ misc/jenkins/InteractiveHtmlBom/run.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 INTERACTIVE_HTML_BOM_NO_DISPLAY="true"
-IBOM_CMD="python3 hardware/InteractiveHtmlBom/InteractiveHtmlBom/generate_interactive_bom.py --no-browser --name-format \"%f_latest\" --dest-dir ../ibom --include-nets"
-echo "IBOM_CMD=$IBOM_CMD"
+ibom() {
+  python3 hardware/InteractiveHtmlBom/InteractiveHtmlBom/generate_interactive_bom.py --no-browser --name-format "%f_latest" --dest-dir ../ibom --include-nets $@
+}
 
-pwd
-[ -e hardware/frankenso/frankenso.kicad_pcb ] || { echo "hardware/frankenso/frankenso.kicad_pcb not found. Was this invoked from wrong folder?"; exit -1; }
+echo "Current directory: $(pwd)"
+[ $(basename $(pwd)) = 'rusefi' ] || { echo "Not in 'rusefi' directory. Was this invoked from the wrong directory?"; exit 1; }
 
 for f in $(ls hardware/*/*.kicad_pcb); do
   if ls $(dirname $f)/$(basename $f .kicad_pcb).net 2>/dev/null; then
-    $IBOM_CMD --netlist-file $(ls $(dirname $f)/$(basename $f .kicad_pcb).net) $f
+    ibom --netlist-file $(ls $(dirname $f)/$(basename $f .kicad_pcb).net) $f
   else
-    $IBOM_CMD $f
+    ibom $f
   fi
 done
