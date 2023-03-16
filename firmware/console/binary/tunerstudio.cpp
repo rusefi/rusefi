00ea3e56e964dfbfbea49c06e5d9c64f309090d1
rusefillc <sdfsdfqsf2334234234>
2023-03-16 13:02:31 -0400

    dead code

--- firmware/console/binary/tunerstudio.cpp
+++ firmware/console/binary/tunerstudio.cpp
@@ -118,13 +118,6 @@ static void printTsStats(void) {
 	efiPrintf("Primary UART TX %s", hwPortname(EFI_CONSOLE_TX_BRAIN_PIN));
 #endif
 
-	if (false) {
-		// todo: is this code needed somewhere else?
-		efiPrintf("TS RX on %s", hwPortname(engineConfiguration->binarySerialRxPin));
-
-		efiPrintf("TS TX on %s @%d", hwPortname(engineConfiguration->binarySerialTxPin),
-				engineConfiguration->tunerStudioSerialSpeed);
-	}
 #endif /* EFI_PROD_CODE */
 
 	printErrorCounters();
