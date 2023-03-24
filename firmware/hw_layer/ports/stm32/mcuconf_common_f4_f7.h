11b16d34da396c05c243bf7ea79fa31accf73a4c
Matthew Kennedy <matthewkennedy@outlook.com>
2023-03-24 14:37:04 -0700

    everybody gets pwm timer 3

--- firmware/hw_layer/ports/stm32/mcuconf_common_f4_f7.h
+++ firmware/hw_layer/ports/stm32/mcuconf_common_f4_f7.h
@@ -209,11 +209,7 @@
 #define STM32_PWM_USE_ADVANCED              FALSE
 #define STM32_PWM_USE_TIM1                  FALSE
 #define STM32_PWM_USE_TIM2                  FALSE
-
-#ifndef STM32_PWM_USE_TIM3
-#define STM32_PWM_USE_TIM3                  FALSE
-#endif
-
+#define STM32_PWM_USE_TIM3                  TRUE
 #define STM32_PWM_USE_TIM4                  TRUE
 #define STM32_PWM_USE_TIM5                  TRUE
 #define STM32_PWM_USE_TIM8                  TRUE
