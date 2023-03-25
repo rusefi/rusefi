58b5764024696172b9ec6ec3f469e63ea3827e59
rusefillc <sdfsdfqsf2334234234>
2023-03-24 21:07:56 -0400

    hellen81 does not approve
    https://github.com/rusefi/rusefi/pull/5196
    
    https://github.com/rusefi/alphax-4chan/issues/100

--- firmware/hw_layer/ports/stm32/stm32h7/cfg/mcuconf.h
+++ firmware/hw_layer/ports/stm32/stm32h7/cfg/mcuconf.h
@@ -356,7 +356,7 @@
 #define STM32_PWM_USE_ADVANCED              FALSE
 #define STM32_PWM_USE_TIM1                  FALSE
 #define STM32_PWM_USE_TIM2                  FALSE
-#define STM32_PWM_USE_TIM3                  TRUE
+#define STM32_PWM_USE_TIM3                  FALSE
 #define STM32_PWM_USE_TIM4                  TRUE
 #define STM32_PWM_USE_TIM5                  TRUE
 #define STM32_PWM_USE_TIM8                  TRUE
