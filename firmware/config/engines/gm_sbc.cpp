0a4939d8643ce07c5aadfab40cd25f2faa2ed991
Andreika <andreika-git@users.noreply.github.com>
2023-03-26 22:41:07 +0300

    8chan Official well known idle stepper configuration #5197 (#5204)

--- firmware/config/engines/gm_sbc.cpp
+++ firmware/config/engines/gm_sbc.cpp
@@ -1,12 +1,38 @@
 #include "pch.h"
 #include "gm_sbc.h"
 
+#if HW_HELLEN
+#include "hellen_meta.h"
+#endif // HW_HELLEN
+
+
+void setStepperHw() {
+	engineConfiguration->useStepperIdle = true;
+	engineConfiguration->useHbridgesToDriveIdleStepper = true;
+	engineConfiguration->stepper_dc_use_two_wires = false;
+	engineConfiguration->stepperDcInvertedPins = false; // or true?
+
+#if HW_HELLEN
+	// using 8chan pinout for DC1: A26 (OUT_DC1+) and A27 (OUT_DC1-)
+	engineConfiguration->stepperDcIo[0].controlPin = H144_GP4; // DC1_PWM
+	engineConfiguration->stepperDcIo[0].directionPin1 = H144_GP3; // DC1_DIR
+	engineConfiguration->stepperDcIo[0].directionPin2 = Gpio::Unassigned;
+	engineConfiguration->stepperDcIo[0].disablePin = H144_GP5; // ETB_DIS
+	// using 8chan pinout for DC2: A28 (OUT_DC2+) and A29 (OUT_DC2-)
+	engineConfiguration->stepperDcIo[1].controlPin = H144_UART1_TX; // DC2_PWM
+	engineConfiguration->stepperDcIo[1].directionPin1 = H_SPI3_CS; // DC2_DIR
+	engineConfiguration->stepperDcIo[1].directionPin2 = Gpio::Unassigned;
+	engineConfiguration->stepperDcIo[1].disablePin = H144_GP5; // ETB_DIS
+#endif // HW_HELLEN
+}
+
 void setSbc() {
 	engineConfiguration->specs.cylindersCount = 8;
 	engineConfiguration->specs.firingOrder = FO_1_8_4_3_6_5_7_2;
 	engineConfiguration->specs.displacement = 5.2;
 
-	engineConfiguration->useStepperIdle = true;
+ 	setStepperHw();
+
 	// 8 cylinders one coil HEI trigger
 }
 
