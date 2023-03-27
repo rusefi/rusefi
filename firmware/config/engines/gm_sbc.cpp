dce88c34e4b6d284a1e8485fa8a6ae51180bec70
rusefillc <sdfsdfqsf2334234234>
2023-03-27 10:33:49 -0400

    only:alphax-8chan

--- firmware/config/engines/gm_sbc.cpp
+++ firmware/config/engines/gm_sbc.cpp
@@ -31,6 +31,9 @@ void setSbc() {
 	engineConfiguration->specs.firingOrder = FO_1_8_4_3_6_5_7_2;
 	engineConfiguration->specs.displacement = 5.2;
 
+	strcpy(engineConfiguration->engineMake, ENGINE_MAKE_GM);
+	strcpy(engineConfiguration->engineCode, "SBC");
+
  	setStepperHw();
 
 	// 8 cylinders one coil HEI trigger
