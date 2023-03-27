9f0bb410dd027af2e38051815cdf62a9b836e8bb
Matthew Kennedy <matthewkennedy@outlook.com>
2023-03-27 01:55:18 -0700

    tests

--- java_tools/configuration_definition/src/test/java/com/rusefi/test/ConfigDefinitionOutputTest.java
+++ java_tools/configuration_definition/src/test/java/com/rusefi/test/ConfigDefinitionOutputTest.java
@@ -8,9 +8,9 @@ import static org.junit.Assert.assertEquals;
 public class ConfigDefinitionOutputTest {
     @Test
     public void testComment() {
-        assertEquals("", BaseCHeaderConsumer.packComment("", "\t"));
-        assertEquals("\t * abc\n", BaseCHeaderConsumer.packComment("abc", "\t"));
-        assertEquals("\t * abc\n" +
-                "\t * vbn\n", BaseCHeaderConsumer.packComment("abc\\nvbn", "\t"));
+        assertEquals("", BaseCHeaderConsumer.packComment("", "\t// "));
+        assertEquals("\t// abc\n", BaseCHeaderConsumer.packComment("abc", "\t// "));
+        assertEquals("\t// abc\n" +
+                "\t// vbn\n", BaseCHeaderConsumer.packComment("abc\\nvbn", "\t// "));
     }
 }
