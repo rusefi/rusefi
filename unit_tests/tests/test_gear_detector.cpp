c682c49d4608cb071e512f926b2729e6927e1d7d
Matthew Kennedy <matthewkennedy@outlook.com>
2023-04-01 23:17:50 -0700

    register/unregister

--- unit_tests/tests/test_gear_detector.cpp
+++ unit_tests/tests/test_gear_detector.cpp
@@ -12,7 +12,7 @@ float GetGearRatioFor(float revPerKm, float axle, float kph, float rpm) {
 	GearDetector dut;
 	dut.onSlowCallback();
 
-	return dut.get().value_or(0);
+	return Sensor::getOrZero(SensorType::DetectedGear);
 }
 
 TEST(GearDetector, ComputeGearRatio) {
