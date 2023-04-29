bf81fc41a06203752a43e33d3b4cc76d18385564
Andrey <rusefillc@gmail.com>
2023-04-29 11:13:50 -0400

    only:it's going to be painful

--- firmware/controllers/generated/signature_stm32h743_nucleo.h
+++
@@ -1,6 +0,0 @@
-//
-// was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
-//
-
-#define SIGNATURE_HASH 2593219155
-#define TS_SIGNATURE "rusEFI master.2023.04.29.stm32h743_nucleo.2593219155"
