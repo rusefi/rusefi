c400bb1d116d27ac0cded53ab33c42545a1d2ba5
GitHub gen-configs Action <action@github.com>
2023-04-29 13:18:11 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_s105.h
+++ firmware/controllers/generated/signature_s105.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3051882253
-#define TS_SIGNATURE "rusEFI master.2023.04.29.s105.3051882253"
+#define SIGNATURE_HASH 2724042730
+#define TS_SIGNATURE "rusEFI master.2023.04.29.s105.2724042730"
