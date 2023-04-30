37c60a326769c021a99e212847dd854af51266cb
GitHub gen-configs Action <action@github.com>
2023-04-30 02:10:53 +0000

    Auto-generated configs and docs

--- firmware/controllers/math/throttle_model_generated.h
+++ firmware/controllers/math/throttle_model_generated.h
@@ -1,4 +1,4 @@
-// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/math/throttle_model.txt Tue Mar 14 20:09:59 PDT 2023
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/math/throttle_model.txt Sun Apr 30 02:09:52 UTC 2023
 // by class com.rusefi.output.CHeaderConsumer
 // begin
 #pragma once
@@ -6,7 +6,7 @@
 // start of throttle_model_s
 struct throttle_model_s {
 };
-static_assert(sizeof(throttle_model_s) == 0);
+static_assert(sizeof(throttle_model_s) == 1);
 
 // end
-// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/math/throttle_model.txt Tue Mar 14 20:09:59 PDT 2023
+// this section was generated automatically by rusEFI tool ConfigDefinition.jar based on (unknown script) controllers/math/throttle_model.txt Sun Apr 30 02:09:52 UTC 2023
