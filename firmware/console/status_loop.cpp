fea92524e4125ba2fd437d0e27615bb13aec0861
Matthew Kennedy <matthewkennedy@outlook.com>
2023-05-05 05:09:42 -0400

    more log fields cleanup, lots of duplicates around fuel logic #98
    (cherry picked from commit a8fef3634202c721f4dac12e7612444b7936cdc0)

--- firmware/console/status_loop.cpp
+++ firmware/console/status_loop.cpp
@@ -540,12 +540,6 @@ static void updateFuelCorrections() {
 }
 
 static void updateFuelResults() {
-	// todo: kill outputChannel while taking care of gauge name and scale!
-	engine->outputChannels.chargeAirMass = engine->fuelComputer.sdAirMassInOneCylinder;
-
-	engine->outputChannels.baseFuel = engine->engineState.baseFuel * 1000;	// Convert grams to mg
-	engine->outputChannels.fuelRunning = engine->fuelComputer.running.fuel;
-
 	engine->outputChannels.fuelFlowRate = engine->engineState.fuelConsumption.getConsumptionGramPerSecond();
 	engine->outputChannels.totalFuelConsumption = engine->engineState.fuelConsumption.getConsumedGrams();
 }
@@ -561,8 +555,6 @@ static void updateFuelInfo() {
 	engine->outputChannels.injectionOffset = engine->engineState.injectionOffset;
 
 	engine->outputChannels.veValue = engine->engineState.currentVe;
-
-	engine->outputChannels.crankingFuelMs = engine->engineState.crankingFuel.fuel;
 }
 
 static void updateIgnition(int rpm) {
