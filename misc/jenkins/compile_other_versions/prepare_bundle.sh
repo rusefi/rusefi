8f6c81dbac19614d385ca9e5bf8777d823b2c80c
Matthew Kennedy <matthewkennedy@outlook.com>
2023-05-12 00:18:52 -0700

    build script tweaks -> date in folder name #91

--- misc/jenkins/compile_other_versions/prepare_bundle.sh
+++ misc/jenkins/compile_other_versions/prepare_bundle.sh
@@ -8,15 +8,15 @@ echo "RUSEFI_BUILD_FTP_USER=$RUSEFI_BUILD_FTP_USER"
 
 TIMESTAMP=$(date "+%Y%m%d_%H%M%S")
 
-if [ "$4" = "true" ]; then
-  export FOLDER="temp/fome.${3}.${BUNDLE_NAME}"
+if [ "$3" = "master" ]; then
+  export FOLDER="temp/fome.snapshot.${TIMESTAMP}.${BUNDLE_NAME}"
 else
-  export FOLDER="temp/fome.snapshot.${BUNDLE_NAME}"
+  export FOLDER="temp/fome.${3}.${BUNDLE_NAME}"
 fi
 
 export BUNDLE_FULL_NAME="fome_bundle_${BUNDLE_NAME}"
 
-bash misc/jenkins/build_working_folder.sh "$3" "$4"
+bash misc/jenkins/build_working_folder.sh
 
 pwd
 echo "exiting $SCRIPT_NAME"
