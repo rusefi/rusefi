6767720927506688603c74762a6c7ae112e5b4dd
Matthew Kennedy <matthewkennedy@outlook.com>
2023-05-12 12:02:10 -0700

    check for empty or null

--- misc/jenkins/compile_other_versions/prepare_bundle.sh
+++ misc/jenkins/compile_other_versions/prepare_bundle.sh
@@ -9,7 +9,7 @@ echo "RUSEFI_BUILD_FTP_USER=$RUSEFI_BUILD_FTP_USER"
 TIMESTAMP=$(date "+%Y%m%d_%H%M%S")
 
 # No arg 3 means this is a snapshot "no name", just use the timestamp
-if [ -z $3 ]; then
+if [ -z $3 ] || [ $3 = "null" ]; then
   export FOLDER="temp/fome.snapshot.${TIMESTAMP}.${BUNDLE_NAME}"
 else
   export FOLDER="temp/fome.${3}.${BUNDLE_NAME}"
