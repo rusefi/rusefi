725f45c82826821eb76867414abe7c049ab08128
GitHub gen-configs Action <action@github.com>
2023-05-14 14:18:02 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_proteus_h7.h
+++ firmware/controllers/generated/signature_proteus_h7.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3246506218
-#define TS_SIGNATURE "rusEFI master.2023.05.14.proteus_h7.3246506218"
+#define SIGNATURE_HASH 1245506942
+#define TS_SIGNATURE "rusEFI master.2023.05.14.proteus_h7.1245506942"
