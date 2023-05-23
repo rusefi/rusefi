6b5d6d00fc42696a625d81bfdb867cf4a96b4e2f
rusefillc <sdfsdfqsf2334234234>
2023-05-23 17:01:34 -0400

    only:kinetis dead

--- firmware/config/boards/kinetis/compile_kinetis.sh
+++ firmware/config/boards/kinetis/compile_kinetis.sh
@@ -2,11 +2,4 @@
 
 echo "Compiling for Kinetis MKE16F512"
 
-# set BUILDDIR=build_kinetis
-export BUILDDIR=build
-
-# rm -f ${BUILDDIR}/rusefi_kinetis.*
-
-# make -r -j4
-
 bash ../common_make.sh kinetis kinetis
