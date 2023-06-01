c8df28df880f5ed4f8ecc0311cd6946aa69c24cd
Andrey <rusefillc@gmail.com>
2023-06-01 18:46:04 -0400

    test-build-guards

---
+++ firmware/controllers/generated/signature_t-b-g.h
@@ -0,0 +1,6 @@
+//
+// was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
+//
+
+#define SIGNATURE_HASH 756779283
+#define TS_SIGNATURE "rusEFI tbg.2023.06.01.t-b-g.756779283"
