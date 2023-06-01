7f37c54648a0c64123df85138597a755c9966c4c
GitHub gen-configs Action <action@github.com>
2023-06-01 14:30:42 +0000

    Auto-generated configs and docs

--- firmware/controllers/algo/auto_generated_enginetypes.h
+++ firmware/controllers/algo/auto_generated_enginetypes.h
@@ -1,4 +1,4 @@
-// was generated automatically by rusEFI tool  from engine_types.h // by enum2string.jar tool on Thu Jun 01 05:46:34 UTC 2023
+// was generated automatically by rusEFI tool  from engine_types.h // by enum2string.jar tool on Thu Jun 01 14:29:50 UTC 2023
 // see also gen_config_and_enums.bat
 
 
@@ -10,6 +10,8 @@ const char *getDebug_mode_e(debug_mode_e value);
 #if __cplusplus
 const char *getEngine_type_e(engine_type_e value);
 #endif //__cplusplus
+#if __cplusplus
 const char *getTrigger_type_e(trigger_type_e value);
+#endif //__cplusplus
 const char *getTs_14_command(ts_14_command value);
 const char *getTs_command_e(ts_command_e value);
