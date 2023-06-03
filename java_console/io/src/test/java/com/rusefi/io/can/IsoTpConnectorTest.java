36999591d3725d972de3ce5f5c5d6d776a511e19
rusefillc <rusefillc@gmail.com>
2023-06-03 01:42:05 -0400

    things were weird here

--- java_console/io/src/test/java/com/rusefi/io/can/IsoTpConnectorTest.java
+++ java_console/io/src/test/java/com/rusefi/io/can/IsoTpConnectorTest.java
@@ -11,10 +11,8 @@ import static org.junit.Assert.assertEquals;
 public class IsoTpConnectorTest {
     @Test
     public void testConnector() {
-
         byte[] crcWrappedCrcRequest = new byte[]{
                 0, 5, 107, 0, 0, 80, 95, 105, -81, -96, 112};
-
 
         List<String> packets = new ArrayList<>();
 
@@ -23,9 +21,8 @@ public class IsoTpConnectorTest {
             public void sendCanData(byte[] hdr, byte[] data, int dataOffset, int dataLength) {
                 byte[] total = combineArrays(hdr, data, dataOffset, dataLength);
 
-                String packetAsString = HexBinary.printHexBinary(Elm327Connector.byteToString(total));
+                String packetAsString = HexBinary.printHexBinary(total);
                 packets.add(packetAsString);
-
             }
 
             @Override
@@ -36,9 +33,7 @@ public class IsoTpConnectorTest {
         IsoTpConnector.sendStrategy(crcWrappedCrcRequest, testConnector);
 
         assertEquals(2, packets.size());
-        //                       10    0B                                  50
-        assertEquals("31 30 30 42 30 30 30 35 36 42 30 30 30 30 35 30 0D ", packets.get(0));
-        //                       21    5F    69    AF    A0    70
-        assertEquals("32 31 35 46 36 39 41 46 41 30 37 30 0D ", packets.get(1));
+        assertEquals("10 0B 00 05 6B 00 00 50 ", packets.get(0));
+        assertEquals("21 5F 69 AF A0 70 ", packets.get(1));
     }
 }
