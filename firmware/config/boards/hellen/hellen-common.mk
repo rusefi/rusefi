21f547b04af86808c70c904169b8873a4a263f77
Matthew Kennedy <matthewkennedy@outlook.com>
2023-06-05 00:06:08 -0700

    include dirs

--- firmware/config/boards/hellen/hellen-common.mk
+++ firmware/config/boards/hellen/hellen-common.mk
@@ -4,6 +4,8 @@
 BOARDCPPSRC += $(BOARDS_DIR)/hellen/hellen_common.cpp \
     $(BOARDS_DIR)/hellen/hellen_board_id.cpp
 
+BOARDINC += $(BOARDS_DIR)/hellen
+
 DDEFS += -DLED_PIN_MODE=OM_INVERTED
 
 # We are running on Hellen-One hardware!
