73563a619dc205e63b8180041793b384f6cc6743
Andrey <rusefillc@gmail.com>
2023-06-06 00:17:59 -0400

    small-can-board

---
+++ firmware/controllers/generated/signature_small-can-board.h
@@ -0,0 +1,6 @@
+//
+// was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
+//
+
+#define SIGNATURE_HASH 459850031
+#define TS_SIGNATURE "rusEFI master.2023.06.06.small-can-board.459850031"
