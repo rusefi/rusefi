0e1fe9b26ef990823a65250acd120cdcaa980991
rusefillc <sdfsdfqsf2334234234>
2023-06-06 20:48:13 -0400

    small can board

--- firmware/controllers/generated/signature_small-can-board.h
+++
@@ -1,6 +0,0 @@
-//
-// was generated automatically by rusEFI tool ConfigDefinition.jar based on gen_config.sh by SignatureConsumer
-//
-
-#define SIGNATURE_HASH 459850031
-#define TS_SIGNATURE "rusEFI master.2023.06.07.small-can-board.459850031"
