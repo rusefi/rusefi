a5a629d1e20cf6c00eb5871f778772bf89d99329
Andrey <rusefillc@gmail.com>
2023-06-15 15:10:01 -0400

    make unit tests compatible with latest googletest #2064

--- unit_tests/gtest-all.cpp
+++ unit_tests/gtest-all.cpp
@@ -2,8 +2,10 @@
 // todo: adjust the makefile so that we can remove this
 
 #include "src/gtest.cc"
+#include "src/gtest-assertion-result.cc"
 #include "src/gtest-death-test.cc"
 #include "src/gtest-filepath.cc"
+#include "src/gtest-matchers.cc"
 #include "src/gtest-port.cc"
 #include "src/gtest-printers.cc"
 #include "src/gtest-test-part.cc"
