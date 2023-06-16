73239d161673732a335eeb71ceb423fb46604d4a
Andrey <rusefillc@gmail.com>
2023-06-16 19:54:02 -0400

    only:Improve toolset for default tune canned tune generation #4871

--- java_console/ui/src/main/java/com/rusefi/tools/WriteSimulatorConfiguration.java
+++ java_console/ui/src/main/java/com/rusefi/tools/WriteSimulatorConfiguration.java
@@ -9,12 +9,14 @@ import com.rusefi.io.LinkManager;
 import com.rusefi.tune.xml.Msq;
 
 import javax.xml.bind.JAXBException;
+import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
 public class WriteSimulatorConfiguration {
     // f407-discovery is historically the most inclusive .ini file
     private static final String INI_FILE_FOR_SIMULATOR = "../firmware/tunerstudio/generated/rusefi_f407-discovery.ini";
+    private static final String FOLDER = "generated";
 
     public static void main(String[] args) throws IOException, InterruptedException, JAXBException {
         // SimulatorExecHelper
@@ -27,7 +29,8 @@ public class WriteSimulatorConfiguration {
         if (ini == null)
             throw new IllegalStateException("Not found " + INI_FILE_FOR_SIMULATOR);
         Msq m = MsqFactory.valueOf(configuration, ini);
-        m.writeXmlFile("simulator_tune.xml");
+        new File(FOLDER).mkdirs();
+        m.writeXmlFile(FOLDER + File.separator + "simulator_tune.xml");
         System.exit(0);
     }
 }
