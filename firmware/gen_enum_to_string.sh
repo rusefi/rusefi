0ec196b88fa9ac7890b624e985ded0199cc4e8d3
Andrey <rusefillc@gmail.com>
2023-06-17 16:22:04 -0400

    CI to use tools from source code, also time to not have jar files in repo in the first place?

--- firmware/gen_enum_to_string.sh
+++ firmware/gen_enum_to_string.sh
@@ -8,14 +8,16 @@ cd ../firmware
 
 rm gen_enum_to_string.log
 
-java -DSystemOut.name=logs/gen_java_enum -cp ../java_tools/enum2string.jar com.rusefi.ToJavaEnum -enumInputFile controllers/sensors/sensor_type.h -outputPath ../java_console/io/src/main/java/com/rusefi/enums
+ENUM_JAR=../java_tools/enum2string.jar
+
+java -DSystemOut.name=logs/gen_java_enum -cp ${ENUM_JAR} com.rusefi.ToJavaEnum -enumInputFile controllers/sensors/sensor_type.h -outputPath ../java_console/io/src/main/java/com/rusefi/enums
 [ $? -eq 0 ] || { echo "ERROR generating sensors"; exit 1; }
 
-java -DSystemOut.name=logs/gen_java_enum -cp ../java_tools/enum2string.jar com.rusefi.ToJavaEnum -enumInputFile controllers/algo/engine_types.h   -outputPath ../java_console/io/src/main/java/com/rusefi/enums -definition integration/rusefi_config.txt
+java -DSystemOut.name=logs/gen_java_enum -cp ${ENUM_JAR} com.rusefi.ToJavaEnum -enumInputFile controllers/algo/engine_types.h   -outputPath ../java_console/io/src/main/java/com/rusefi/enums -definition integration/rusefi_config.txt
 [ $? -eq 0 ] || { echo "ERROR generating types"; exit 1; }
 
 java -DSystemOut.name=logs/gen_enum_to_string \
-	-jar ../java_tools/enum2string.jar \
+	-jar ${ENUM_JAR} \
 	-outputPath controllers/algo \
 	-generatedFile commonenum \
 	-enumInputFile controllers/algo/rusefi_enums.h
@@ -23,7 +25,7 @@ java -DSystemOut.name=logs/gen_enum_to_string \
 [ $? -eq 0 ] || { echo "ERROR generating enums"; exit 1; }
 
 java -DSystemOut.name=logs/gen_enum_to_string \
-	-jar ../java_tools/enum2string.jar \
+	-jar ${ENUM_JAR} \
 	-outputPath hw_layer/drivers/can \
 	-generatedFile can_category \
 	-enumInputFile hw_layer/drivers/can/can_category.h
@@ -31,7 +33,7 @@ java -DSystemOut.name=logs/gen_enum_to_string \
 [ $? -eq 0 ] || { echo "ERROR generating enums"; exit 1; }
 
 java -DSystemOut.name=logs/gen_enum_to_string \
-	-jar ../java_tools/enum2string.jar \
+	-jar ${ENUM_JAR} \
 	-outputPath controllers/trigger/decoders \
 	-generatedFile sync_edge \
 	-enumInputFile controllers/trigger/decoders/sync_edge.h
@@ -39,7 +41,7 @@ java -DSystemOut.name=logs/gen_enum_to_string \
 [ $? -eq 0 ] || { echo "ERROR generating enums"; exit 1; }
 
 java -DSystemOut.name=logs/gen_enum_to_string \
-	-jar ../java_tools/enum2string.jar \
+	-jar ${ENUM_JAR} \
 	-outputPath controllers/algo \
 	-generatedFile enginetypes \
 	-enumInputFile controllers/algo/engine_types.h
@@ -49,14 +51,14 @@ java -DSystemOut.name=logs/gen_enum_to_string \
 # TODO: rearrange enums so that we have WAY less duplicated generated code? at the moment too many enums are generated 4 times
 
 java -DSystemOut.name=logs/gen_enum_to_string \
-	-jar ../java_tools/enum2string.jar \
+	-jar ${ENUM_JAR} \
 	-outputPath controllers/algo \
 	-enumInputFile controllers/algo/rusefi_hw_enums.h \
 
 [ $? -eq 0 ] || { echo "ERROR generating hw_enums"; exit 1; }
 
 java -DSystemOut.name=logs/gen_enum_to_string \
-	-jar ../java_tools/enum2string.jar \
+	-jar ${ENUM_JAR} \
 	-outputPath controllers/sensors \
 	-generatedFile sensor \
 	-enumInputFile controllers/sensors/sensor_type.h
