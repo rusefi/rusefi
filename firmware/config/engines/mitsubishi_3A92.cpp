d135c5cf301cb7e616a6755633d93f904f9437a9
Andrey <rusefillc@gmail.com>
2023-06-18 21:19:59 -0400

    only:three bytes of canned

--- firmware/config/engines/mitsubishi_3A92.cpp
+++ firmware/config/engines/mitsubishi_3A92.cpp
@@ -1,4 +1,11 @@
 #include "pch.h"
 
 void setMitsubishi3A92() {
+	engineConfiguration->ignitionMode = IM_INDIVIDUAL_COILS;
+	engineConfiguration->cylindersCount = 3.0;
+	engineConfiguration->displacement = 1.2;
+	engineConfiguration->vvtMode[0] = VVT_MITSUBISHI_3A92;
+
+	engineConfiguration->maxCamPhaseResolveRpm = 1500.0;
+	engineConfiguration->crankingInjectionMode = IM_SEQUENTIAL;
 }
--- firmware/config/engines/toyota_1NZ_FE.cpp
+++ firmware/config/engines/toyota_1NZ_FE.cpp
@@ -1,4 +1,9 @@
 #include "pch.h"
 
 void setToyota1NZFE() {
+    engineConfiguration->ignitionMode = IM_INDIVIDUAL_COILS;
+    engineConfiguration->displacement = 1.5;
+    engineConfiguration->globalTriggerAngleOffset = 230.0;
+    engineConfiguration->vvtMode[0] = VVT_TOYOTA_4_1;
+    engineConfiguration->vvtOffsets[0] = 316.0;
 }
