91b4c4907dcf9f015af6fedd1e8eacb14045c5b3
rusefillc <sdfsdfqsf2334234234>
2023-06-20 19:09:44 -0500

    F7

--- firmware/config/boards/hellen/alphax-8chan/board.mk
+++ firmware/config/boards/hellen/alphax-8chan/board.mk
@@ -15,6 +15,9 @@ DDEFS += -DADC_MUX_PIN=Gpio::B3
 
 include $(BOARDS_DIR)/hellen/hellen-common144.mk
 
-DDEFS += $(PRIMARY_COMMUNICATION_PORT_USART2)
+# serial ports only on F4
+ifeq ($(PROJECT_CPU),ARCH_STM32F4)
+	DDEFS += $(PRIMARY_COMMUNICATION_PORT_USART2)
+endif
 
 DDEFS += -DSHORT_BOARD_NAME=alphax-8chan
