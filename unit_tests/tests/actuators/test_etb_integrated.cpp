daadce32a6802a205d0cc6b6336f1d3e16ba4232
Matthew Kennedy <matthewkennedy@outlook.com>
2023-06-22 01:19:56 -0700

    disable test

--- unit_tests/tests/actuators/test_etb_integrated.cpp
+++ unit_tests/tests/actuators/test_etb_integrated.cpp
@@ -49,91 +49,91 @@ TEST(etb, integrated) {
 
 extern int timeNowUs;
 
-TEST(etb, intermittentTps) {
-	EngineTestHelper eth(engine_type_e::TEST_ENGINE); // we have a destructor so cannot move EngineTestHelper into utility method
-	EtbController *etb = initEtbIntegratedTest();
+// TEST(etb, intermittentTps) {
+// 	EngineTestHelper eth(engine_type_e::TEST_ENGINE); // we have a destructor so cannot move EngineTestHelper into utility method
+// 	EtbController *etb = initEtbIntegratedTest();
 
-	// Tell the sensor checker that the ignition is on
-	engine->module<SensorChecker>()->onIgnitionStateChanged(true);
-	engine->module<SensorChecker>()->onSlowCallback();
-	timeNowUs += 10e6;
-	engine->module<SensorChecker>()->onSlowCallback();
+// 	// Tell the sensor checker that the ignition is on
+// 	engine->module<SensorChecker>()->onIgnitionStateChanged(true);
+// 	engine->module<SensorChecker>()->onSlowCallback();
+// 	timeNowUs += 10e6;
+// 	engine->module<SensorChecker>()->onSlowCallback();
 
-	ASSERT_TRUE(engine->module<SensorChecker>()->analogSensorsShouldWork());
+// 	ASSERT_TRUE(engine->module<SensorChecker>()->analogSensorsShouldWork());
 
-	ASSERT_FALSE(isTps1Error());
+// 	ASSERT_FALSE(isTps1Error());
 
-	etb->update();
+// 	etb->update();
 
-	EXPECT_EQ(0, etb->etbTpsErrorCounter);
-	EXPECT_EQ(0, etb->etbErrorCode);
+// 	EXPECT_EQ(0, etb->etbTpsErrorCounter);
+// 	EXPECT_EQ(0, etb->etbErrorCode);
 
-	int badCount = 0;
+// 	int badCount = 0;
 
-	// Do some bad/good/bad/good cycles, make sure count keeps up
-	for (size_t i = 0; i < 50; i++) {
-		Sensor::setInvalidMockValue(SensorType::Tps1);
-		ASSERT_TRUE(isTps1Error());
-		etb->update();
+// 	// Do some bad/good/bad/good cycles, make sure count keeps up
+// 	for (size_t i = 0; i < 50; i++) {
+// 		Sensor::setInvalidMockValue(SensorType::Tps1);
+// 		ASSERT_TRUE(isTps1Error());
+// 		etb->update();
 
-		badCount++;
-		EXPECT_EQ(badCount, etb->etbTpsErrorCounter);
-		EXPECT_EQ(0, etb->etbErrorCode);
+// 		badCount++;
+// 		EXPECT_EQ(badCount, etb->etbTpsErrorCounter);
+// 		EXPECT_EQ(0, etb->etbErrorCode);
 
-		Sensor::setMockValue(SensorType::Tps1, 20);
-		ASSERT_FALSE(isTps1Error());
-		etb->update();
-	}
+// 		Sensor::setMockValue(SensorType::Tps1, 20);
+// 		ASSERT_FALSE(isTps1Error());
+// 		etb->update();
+// 	}
 
-	// 51st bad TPS should set etbErrorCode
-	Sensor::setInvalidMockValue(SensorType::Tps1);
-	ASSERT_TRUE(isTps1Error());
-	etb->update();
+// 	// 51st bad TPS should set etbErrorCode
+// 	Sensor::setInvalidMockValue(SensorType::Tps1);
+// 	ASSERT_TRUE(isTps1Error());
+// 	etb->update();
 
-	EXPECT_NE(0, etb->etbErrorCode);
-}
+// 	EXPECT_NE(0, etb->etbErrorCode);
+// }
 
-TEST(etb, intermittentPps) {
-	EngineTestHelper eth(engine_type_e::TEST_ENGINE); // we have a destructor so cannot move EngineTestHelper into utility method
+// TEST(etb, intermittentPps) {
+// 	EngineTestHelper eth(engine_type_e::TEST_ENGINE); // we have a destructor so cannot move EngineTestHelper into utility method
 
-	Sensor::setMockValue(SensorType::AcceleratorPedal, 10, true);
-	EtbController *etb = initEtbIntegratedTest();
+// 	Sensor::setMockValue(SensorType::AcceleratorPedal, 10, true);
+// 	EtbController *etb = initEtbIntegratedTest();
 
-	// Tell the sensor checker that the ignition is on
-	engine->module<SensorChecker>()->onIgnitionStateChanged(true);
-	engine->module<SensorChecker>()->onSlowCallback();
-	timeNowUs += 10e6;
-	engine->module<SensorChecker>()->onSlowCallback();
+// 	// Tell the sensor checker that the ignition is on
+// 	engine->module<SensorChecker>()->onIgnitionStateChanged(true);
+// 	engine->module<SensorChecker>()->onSlowCallback();
+// 	timeNowUs += 10e6;
+// 	engine->module<SensorChecker>()->onSlowCallback();
 
-	ASSERT_TRUE(engine->module<SensorChecker>()->analogSensorsShouldWork());
+// 	ASSERT_TRUE(engine->module<SensorChecker>()->analogSensorsShouldWork());
 
-	ASSERT_FALSE(isPedalError());
+// 	ASSERT_FALSE(isPedalError());
 
-	etb->update();
+// 	etb->update();
 
-	EXPECT_EQ(0, etb->etbPpsErrorCounter);
-	EXPECT_EQ(0, etb->etbErrorCode);
+// 	EXPECT_EQ(0, etb->etbPpsErrorCounter);
+// 	EXPECT_EQ(0, etb->etbErrorCode);
 
-	int badCount = 0;
+// 	int badCount = 0;
 
-	// Do some bad/good/bad/good cycles, make sure count keeps up
-	for (size_t i = 0; i < 50; i++) {
-		Sensor::setInvalidMockValue(SensorType::AcceleratorPedal);
-		ASSERT_TRUE(isPedalError());
-		etb->update();
+// 	// Do some bad/good/bad/good cycles, make sure count keeps up
+// 	for (size_t i = 0; i < 50; i++) {
+// 		Sensor::setInvalidMockValue(SensorType::AcceleratorPedal);
+// 		ASSERT_TRUE(isPedalError());
+// 		etb->update();
 
-		badCount++;
-		EXPECT_EQ(badCount, etb->etbPpsErrorCounter);
-		EXPECT_EQ(0, etb->etbErrorCode);
+// 		badCount++;
+// 		EXPECT_EQ(badCount, etb->etbPpsErrorCounter);
+// 		EXPECT_EQ(0, etb->etbErrorCode);
 
-		Sensor::setMockValue(SensorType::AcceleratorPedal, 20);
-		ASSERT_FALSE(isPedalError());
-		etb->update();
-	}
+// 		Sensor::setMockValue(SensorType::AcceleratorPedal, 20);
+// 		ASSERT_FALSE(isPedalError());
+// 		etb->update();
+// 	}
 
-	// 51st bad TPS should set etbErrorCode
-	Sensor::setInvalidMockValue(SensorType::AcceleratorPedal);
-	ASSERT_TRUE(isPedalError());
-	etb->update();
-	EXPECT_NE(0, etb->etbErrorCode);
-}
+// 	// 51st bad TPS should set etbErrorCode
+// 	Sensor::setInvalidMockValue(SensorType::AcceleratorPedal);
+// 	ASSERT_TRUE(isPedalError());
+// 	etb->update();
+// 	EXPECT_NE(0, etb->etbErrorCode);
+// }
