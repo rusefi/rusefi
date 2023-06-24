7774fc3ae229e8bc2ec28d726b971ba6ad99b842
rusefi <rusefillc@gmail.com>
2023-06-24 02:01:49 -0400

    only:docs

--- firmware/config/engines/gm_sbc.cpp
+++ firmware/config/engines/gm_sbc.cpp
@@ -18,15 +18,13 @@ void setStepperHw() {
 	engineConfiguration->injector.flow = 482.5;
 
 #if HW_HELLEN
-    engineConfiguration->stepperDcInvertedPins = true; // todo: fix test harness not today
-
     setPPSInputs(EFI_ADC_NONE, EFI_ADC_NONE);
-	// using 8chan pinout for DC1: A26 (OUT_DC1+) and A27 (OUT_DC1-)
+	// using 8chan pinout for DC1: A26 (OUT_DC1+ AH pin "D") and A27 (OUT_DC1- AL pin "C")
 	engineConfiguration->stepperDcIo[0].controlPin = H144_GP4; // DC1_PWM
 	engineConfiguration->stepperDcIo[0].directionPin1 = H144_GP3; // DC1_DIR
 	engineConfiguration->stepperDcIo[0].directionPin2 = Gpio::Unassigned;
 	engineConfiguration->stepperDcIo[0].disablePin = H144_GP5; // ETB_DIS
-	// using 8chan pinout for DC2: A28 (OUT_DC2+) and A29 (OUT_DC2-)
+	// using 8chan pinout for DC2: A28 (OUT_DC2+ BL pin "A") and A29 (OUT_DC2- BH pin "B")
 	engineConfiguration->stepperDcIo[1].controlPin = H144_UART1_TX; // DC2_PWM
 	engineConfiguration->stepperDcIo[1].directionPin1 = H_SPI3_CS; // DC2_DIR
 	engineConfiguration->stepperDcIo[1].directionPin2 = Gpio::Unassigned;
@@ -34,7 +32,7 @@ void setStepperHw() {
 #endif // HW_HELLEN
 
 #if HW_PROTEUS
-	// coil #1
+	// coil #1 - proteus pin 7 to AH pin "D"
 	// PWM pin
 	engineConfiguration->stepperDcIo[0].controlPin = Gpio::D12;
 	// DIR pin
@@ -44,7 +42,7 @@ void setStepperHw() {
 	// Unused
 	engineConfiguration->stepperDcIo[0].directionPin2 = Gpio::Unassigned;
 
-	// coil #2
+	// coil #2 = proteus pin 15 to BL pin "A"
 	// PWM pin
 	engineConfiguration->stepperDcIo[1].controlPin = Gpio::D13;
 	// DIR pin
