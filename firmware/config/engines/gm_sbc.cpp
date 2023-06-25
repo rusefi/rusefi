ed4c25e3dcbcb964fe94daa251f32240200cfde9
rusefillc <sdfsdfqsf2334234234>
2023-06-24 23:37:23 -0400

    SBC

--- firmware/config/engines/gm_sbc.cpp
+++ firmware/config/engines/gm_sbc.cpp
@@ -59,6 +59,9 @@ void setSbc() {
 	engineConfiguration->firingOrder = FO_1_8_4_3_6_5_7_2;
 	engineConfiguration->displacement = 5.2;
 
+    // see engine sniffer at https://github.com/rusefi/rusefi/issues/5353
+    // if injectors are wired in groups of four best to use channels 1 and 4
+    engineConfiguration->twoWireBatchInjection = true;
     engineConfiguration->crankingInjectionMode = IM_BATCH;
     engineConfiguration->injectionMode = IM_BATCH;
 
