beb72ea15d17323cdb0a9f1fd156ee73aec8f0c4
GitHub gen-default-tune Action <action@github.com>
2023-06-28 00:34:24 +0000

    Auto-generated default tune

--- firmware/controllers/generated/rusefi_generated.h
+++ firmware/controllers/generated/rusefi_generated.h
@@ -1359,7 +1359,7 @@
 #define ts_show_vbatt true
 #define ts_show_vr_threshold_2 true
 #define ts_show_vr_threshold_all true
-#define TS_SIGNATURE "rusEFI master.2023.06.27.f407-discovery.1041064078"
+#define TS_SIGNATURE "rusEFI master.2023.06.28.f407-discovery.1041064078"
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't'
