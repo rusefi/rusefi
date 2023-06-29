f7d5a1c4d348e0deda3726b2584b4eeb2796003c
rusefillc <sdfsdfqsf2334234234>
2023-06-29 19:24:11 -0400

    unit test to validate fresh .ini file

---
+++ java_tools/tune-tools/src/test/java/com/rusefi/tune/ReadCurrentIniTest.java
@@ -0,0 +1,15 @@
+package com.rusefi.tune;
+
+import com.opensr5.ini.IniFileModel;
+import com.rusefi.tools.tune.WriteSimulatorConfiguration;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class ReadCurrentIniTest {
+    @Test
+    public void test() {
+        IniFileModel ini = new IniFileModel().readIniFile("../" + WriteSimulatorConfiguration.INI_FILE_FOR_SIMULATOR);
+        assertNotNull(ini);
+    }
+}
