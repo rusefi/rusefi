bdfb4ae25e6c7886fd7bec7ff022a5b533b189e7
rusefi <rusefillc@gmail.com>
2023-06-29 23:09:43 -0400

    only:dead code comment

--- firmware/controllers/bench_test.cpp
+++ firmware/controllers/bench_test.cpp
@@ -456,7 +456,6 @@ void executeTSCommand(uint16_t subsystem, uint16_t index) {
 
 	case TS_IGNITION_CATEGORY:
 		if (!running) {
-			/* WARN: fixed charge time */
 			doRunSparkBench(index, 300.0, engineConfiguration->benchTestOnTime,
 				engineConfiguration->benchTestOffTime, engineConfiguration->benchTestCount);
 		}
