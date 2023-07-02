f182fafe00f16b09bbc342950ca1b6f0a1a47643
rusefillc <sdfsdfqsf2334234234>
2023-07-02 17:00:52 -0400

    only:antlr script

---
+++ java_console/ui/src/main/java/com/rusefi/livedata/generate.bat
@@ -0,0 +1,3 @@
+java -jar ../.././../../../../../../java_tools/configuration_definition/lib/antlr-4.10-complete.jar -Dlanguage=Java -package com.rusefi.livedata.generated CPP14Lexer.g4
+java -jar ../.././../../../../../../java_tools/configuration_definition/lib/antlr-4.10-complete.jar -Dlanguage=Java -package com.rusefi.livedata.generated CPP14Parser.g4
+
