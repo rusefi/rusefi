04acd2f09221c9e13217410adc0ba04979bf792c
GitHub gen-configs Action <action@github.com>
2023-07-02 04:59:13 +0000

    Auto-generated configs and docs

--- java_console/models/src/main/java/com/rusefi/config/generated/VvtState.java
+++ java_console/models/src/main/java/com/rusefi/config/generated/VvtState.java
@@ -3,8 +3,12 @@ package com.rusefi.config.generated;
 import com.rusefi.config.*;
 
 public class VvtState {
-	public static final Field VVT_PLACEHOLDER = Field.create("VVT_PLACEHOLDER", 0, FieldType.INT).setScale(1.0).setBaseOffset(1372);
+	public static final Field VVTTARGET = Field.create("VVTTARGET", 0, FieldType.INT16).setScale(0.1).setBaseOffset(1372);
+	public static final Field VVTOUTPUT = Field.create("VVTOUTPUT", 2, FieldType.INT8).setScale(0.5).setBaseOffset(1372);
+	public static final Field ALIGNMENTFILL_AT_3 = Field.create("ALIGNMENTFILL_AT_3", 3, FieldType.INT8).setScale(1.0).setBaseOffset(1372);
 	public static final Field[] VALUES = {
-	VVT_PLACEHOLDER,
+	VVTTARGET,
+	VVTOUTPUT,
+	ALIGNMENTFILL_AT_3,
 	};
 }
