89dee4d71a4825510c05a46430c465d673625c82
Andrey Gusakov <dron0gus@gmail.com>
2023-07-02 14:35:57 +0300

    OpenBLT: simple flash over USB script

---
+++ firmware/flash_usb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script will try to flash/update RusEFI part of firmware over USB interface.
+
+echo This script may need root privileges for accessing USB device or special udev rules (TODO)
+
+BootCommander -t=xcp_usb deliver/rusefi_update.srec
+
+# OR
+# You can build it from sources with:
+# (cd ext/openblt/Host/Source/LibOpenBLT/ ; mkdir build ; cd build ; cmake .. ; make -j )
+# and
+# (cd ext/openblt/Host/Source/BootCommander/ ; mkdir build ; cd build ; cmake .. ; make -j )
+# And run:
+# ext/openblt/Host/BootCommander -t=xcp_usb deliver/rusefi_update.srec
