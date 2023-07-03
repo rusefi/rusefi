d7fcb651bdc7e574112898e980eb42a964d512c8
rusefillc <sdfsdfqsf2334234234>
2023-07-03 08:16:37 -0400

    only:helping build

--- java_tools/configuration_definition_base/src/main/java/com/rusefi/TriggerMetaGenerator.java
+++ java_tools/configuration_definition_base/src/main/java/com/rusefi/TriggerMetaGenerator.java
@@ -1,12 +1,10 @@
 package com.rusefi;
 
-import com.rusefi.util.LazyFile;
-
 import java.io.IOException;
 
 public class TriggerMetaGenerator {
     public static void main(String[] a) throws IOException {
-        ReaderStateImpl reader = new ReaderStateImpl(null, LazyFile.REAL);
+        ReaderStateImpl reader = new ReaderStateImpl();
         reader.setDefinitionInputFile("integration/rusefi_config_trigger.txt");
         reader.addCHeaderDestination("../unit_tests/TriggerMeta.h");
         reader.addJavaDestination("../java_tools/trigger-image/src/main/java/com/rusefi/config/generated/TriggerMeta.java");
