54c4756fc2fa9d69b7069b26468247bf5edd68fb
Andrey <rusefillc@gmail.com>
2023-07-04 00:05:54 -0400

    only:reusable HW CI build script

---
+++ .github/workflows/hw-ci/build_for_hw_ci.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+HW_FOLDER=$1
+HW_TARGET=$2
+
+# for instance
+# .github/workflows/hw-ci/build_for_hw_ci.sh config/boards/f407-discovery f407-discovery
+# .github/workflows/hw-ci/build_for_hw_ci.sh config/boards/proteus        proteus_f4
+
+set -e
+echo "HW CI build [$HW_FOLDER][$HW_TARGET]"
+
+cd firmware
+
+./gen_live_documentation.sh
+./gen_config_board.sh $HW_FOLDER $HW_TARGET
+
+echo "We aren't guaranteed a clean machine every time, so manually clean the output."
+make clean
+cd ..
+
+export EXTRA_2_PARAMS=-DHARDWARE_CI
+
+echo Build Firmware
+misc/jenkins/compile_other_versions/compile.sh $HW_FOLDER $HW_TARGET
