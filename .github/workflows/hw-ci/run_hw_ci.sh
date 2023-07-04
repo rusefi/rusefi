18fcb53086d91fac682563f64d0d9c3f2bddec65
Andrey <rusefillc@gmail.com>
2023-07-04 12:29:07 -0400

    only:extracing reusable HW CI scripts

---
+++ .github/workflows/hw-ci/run_hw_ci.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# for instance
+# .github/workflows/hw-ci/run_hw_ci.sh com.rusefi.HwCiF4Proteus
+
+HW_SUITE=$1
+
+set -e
+
+cd java_tools
+./gradlew :autotest:shadowJar
+cd ..
+
+java -cp java_console/autotest/build/libs/autotest-all.jar $HW_SUITE
