8f517c22e85b7f771a1d1b5dd26d543cb55fe23b
Andrey <rusefillc@gmail.com>
2023-07-04 10:04:03 -0400

    only:extracting reusable script

--- .github/workflows/hw-ci/openocd_wipe_and_flash.sh
+++ .github/workflows/hw-ci/openocd_wipe_and_flash.sh
@@ -6,7 +6,9 @@ set -e
 
 # for instance
 #
-# .github/workflows/hw-ci/ ../.github/workflows/openocd_ci_f4_discovery.cfg
-# .github/workflows/hw-ci/ ../.github/workflows/openocd_ci_proteus.cfg
+# .github/workflows/hw-ci/openocd_wipe_and_flash.sh ../.github/workflows/openocd_ci_f4_discovery.cfg
+# .github/workflows/hw-ci/openocd_wipe_and_flash.sh ../.github/workflows/openocd_ci_proteus.cfg
+
+cd firmware
 
 openocd -f "$HW_SCRIPT" -c init -c targets -c "reset halt" -c "flash erase_sector 0 0 11" -c "flash write_image "deliver/rusefi.bin" 0x08000000" -c "reset run" -c "shutdown"
