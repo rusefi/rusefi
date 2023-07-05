6133d2504afd8a8f1cf2b6afd063755024b208ff
rusefillc <sdfsdfqsf2334234234>
2023-07-04 22:07:00 -0400

    global_port.h

---
+++ firmware/hw_layer/ports/cypress/global_port.h
@@ -0,0 +1,3 @@
+#else /* this MCU doesn't need these */
+#define CCM_OPTIONAL
+#define NO_CACHE
---
+++ firmware/hw_layer/ports/stm32/stm32h7/global_port.h
@@ -0,0 +1,6 @@
+// DTCM memory is 128k
+#define CCM_OPTIONAL __attribute__((section(".ram5")))
+//TODO: update LD file!
+#define SDRAM_OPTIONAL __attribute__((section(".ram8")))
+// SRAM3 is 32k and set to disable dcache
+#define NO_CACHE __attribute__((section(".ram3")))
