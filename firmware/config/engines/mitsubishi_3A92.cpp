f6b40a0922f81f69e41d05777f3c5e49ab37783a
rusefillc <sdfsdfqsf2334234234>
2023-07-10 19:49:38 -0400

    only:stuff related to #5401

--- firmware/config/engines/mitsubishi_3A92.cpp
+++ firmware/config/engines/mitsubishi_3A92.cpp
@@ -1,10 +1,14 @@
 #include "pch.h"
 
 void setMitsubishi3A92() {
+	engineConfiguration->trigger.type = trigger_type_e::TT_36_2_1_1;
+
 	engineConfiguration->ignitionMode = IM_INDIVIDUAL_COILS;
 	engineConfiguration->cylindersCount = 3.0;
 	engineConfiguration->displacement = 1.2;
+    engineConfiguration->globalTriggerAngleOffset = -360.0;
 	engineConfiguration->vvtMode[0] = VVT_MITSUBISHI_3A92;
+    engineConfiguration->vvtOffsets1 = 316.0;
 
 	engineConfiguration->maxCamPhaseResolveRpm = 1500.0;
 	engineConfiguration->crankingInjectionMode = IM_SEQUENTIAL;
