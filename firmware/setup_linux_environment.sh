6909f5a7a4f74b8bfb6e4e0cd2373ce4cf90a087
Matthew Kennedy <matthewkennedy@outlook.com>
2023-07-17 22:54:07 -0700

    ant can go

--- firmware/setup_linux_environment.sh
+++ firmware/setup_linux_environment.sh
@@ -15,7 +15,7 @@
 sudo apt-get update
 
 # install dependencies
-sudo apt-get install -y build-essential gcc gdb gcc-multilib g++-multilib make openjdk-8-jdk-headless ant mtools zip xxd libncurses5 libncursesw5
+sudo apt-get install -y build-essential gcc gdb gcc-multilib g++-multilib make openjdk-8-jdk-headless mtools zip xxd libncurses5 libncursesw5
 
 # delete any old tools, create a new folder, and go there
 rm -rf ~/.rusefi-tools
