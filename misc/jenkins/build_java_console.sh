c346abed6c5514e8f806aca3a574a7d21aa01cec
Matthew Kennedy <matthewkennedy@outlook.com>
2023-07-18 03:27:41 -0700

    console script uses gradle

--- misc/jenkins/build_java_console.sh
+++ misc/jenkins/build_java_console.sh
@@ -3,15 +3,10 @@
 echo "java version"
 java -version
 
-echo "Generating Java (Antlr)"
-cd java_tools
-./gradlew :config_definition:generateGrammarSource
-cd ..
-
 echo "Building java console"
 pwd
-cd java_console
-ant clean clean_out_folder jar
+cd java_tools
+./gradlew :ui:shadowJar
 cd ..
 
 [ -e java_console_binary/fome_console.jar ] || { echo "CONSOLE COMPILATION FAILED"; exit 1; }
