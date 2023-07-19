6615b604d4742211c71ceee1b010ce717e760ab7
andreyb <rusefillc@gmail.com>
2023-07-19 09:27:52 -0700

    sling

--- firmware/config/engines/slingshot.cpp
+++ firmware/config/engines/slingshot.cpp
@@ -8,4 +8,15 @@ https://rusefi.com/docs/pinouts/hellen/hellen-112-17/
 void setSlingshot() {
 	strcpy(engineConfiguration->engineMake, "Polaris");
     setInline4();
+    engineConfiguration->injectionMode = IM_SEQUENTIAL;
+
+    engineConfiguration->ignitionMode = IM_WASTED_SPARK; // just for now
+    engineConfiguration->twoWireBatchIgnition = true;
+
+	engineConfiguration->map.sensor.type = MT_CUSTOM;
+	// GM TMAP is recommended
+	engineConfiguration->map.sensor.lowValue = 20;
+   	engineConfiguration->mapLowValueVoltage = 0.3;
+  	engineConfiguration->map.sensor.highValue = 250;
+   	engineConfiguration->mapHighValueVoltage = 4.65;
 }
