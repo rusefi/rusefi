86ff1603c5a7b81238c428553412f05f0c7a10ed
andreika-git <andrei.home@gmail.com>
2023-07-24 19:19:17 +0300

    set 768k flash for proteus_f7_debug

--- firmware/config/boards/proteus/compile_proteus_f7_debug.sh
+++ firmware/config/boards/proteus/compile_proteus_f7_debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DEBUG_LEVEL_OPT="-O0 -ggdb -g"
+export DEBUG_LEVEL_OPT="-O0 -ggdb -g -Wl,--defsym=FLASH_SIZE=768k"
 export EFI_LUA_LOOKUP="FALSE"
 
 bash ../common_make.sh proteus ARCH_STM32F7
