7fb3f31c61586f3e0f6118abb3b694cffda02989
rusefillc <48498823+rusefillc@users.noreply.github.com>
2023-07-25 20:06:39 -0400

    proteus F7 hard fault on "enable self stimulation" #5354 (#5454)
    only:coverage, that's easy!
    
    Co-authored-by: rusefillc <sdfsdfqsf2334234234>

--- java_console/autotest/src/main/java/com/rusefi/HwCiProteus.java
+++ java_console/autotest/src/main/java/com/rusefi/HwCiProteus.java
@@ -1,12 +1,14 @@
 package com.rusefi;
 
 import com.rusefi.common.MiscTest;
+import com.rusefi.f4discovery.CompositeLoggerTest;
 import com.rusefi.f4discovery.HighRevTest;
 import com.rusefi.proteus.ProteusAnalogTest;
 
 public class HwCiProteus {
     public static void main(String[] args) {
         CmdJUnitRunner.runHardwareTestAndExit(new Class[]{
+                CompositeLoggerTest.class,
                 HighRevTest.class,
                 MiscTest.class,
                 ProteusAnalogTest.class,
