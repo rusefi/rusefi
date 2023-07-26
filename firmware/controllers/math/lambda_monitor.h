30f85245ecead1719edf1d98c865a77239d4bffb
Matthew Kennedy <matthewkennedy@outlook.com>
2023-07-26 00:35:28 -0700

    test lambda monitor response #75

--- firmware/controllers/math/lambda_monitor.h
+++ firmware/controllers/math/lambda_monitor.h
@@ -12,12 +12,14 @@ protected:
 
 	// Returns false if the current lambda reading is leaner than allowable.
 	// Returns true in any other case (rich enough, bad sensor, recent fuel cut, rpm to low, load too low, etc)
-	bool isCurrentlyGood(float rpm, float load) const;
+	virtual bool isCurrentlyGood(float rpm, float load) const;
+
 	virtual float getMaxAllowedLambda(float rpm, float load) const = 0;
+	virtual float getTimeout() const = 0;
 
 	// Determine whether fuel should be restored after a cut occurs
 	// Returns true if OK to leave the "cut" state
-	bool restoreConditionsMet(float rpm, float load) const;
+	virtual bool restoreConditionsMet(float rpm, float load) const;
 
 private:
 	Timer m_timeSinceGoodLambda;
@@ -25,4 +27,5 @@ private:
 
 class LambdaMonitor : public LambdaMonitorBase {
 	float getMaxAllowedLambda(float rpm, float load) const override;
+	float getTimeout() const override;
 };
--- unit_tests/tests/test_lambda_monitor.cpp
+++ unit_tests/tests/test_lambda_monitor.cpp
@@ -1,5 +1,63 @@
 #include "pch.h"
 
-TEST(LambdaMonitor, x) {
+struct MockLambdaMonitor : public LambdaMonitorBase {
+	bool isGood = true;
+	bool isCurrentlyGood(float /*rpm*/, float /*load*/) const override {
+		return isGood;
+	}
 
+	bool isRestore = false;
+	bool restoreConditionsMet(float /*rpm*/, float /*load*/) const override {
+		return isRestore;
+	}
+
+	float getTimeout() const override {
+		// Timeout of 1 second
+		return 1;
+	}
+
+	MOCK_METHOD(float, getMaxAllowedLambda, (float rpm, float load), (const, override));
+};
+
+TEST(LambdaMonitor, Response) {
+	MockLambdaMonitor mlm;
+
+	extern int timeNowUs;
+	int startTime = 1e6;
+	timeNowUs = startTime;
+
+	mlm.isGood = true;
+	mlm.isRestore = false;
+	mlm.update(2000, 50);
+	EXPECT_TRUE(mlm.lambdaCurrentlyGood);
+	EXPECT_FALSE(mlm.isCut());
+
+	// now lambda will be bad, but we don't cut yet
+	mlm.isGood = false;
+	mlm.update(2000, 50);
+	EXPECT_FALSE(mlm.lambdaCurrentlyGood);
+	EXPECT_FALSE(mlm.isCut());
+
+	// 0.9 second later, still not cut
+	timeNowUs = startTime + 0.9e6;
+	mlm.update(2000, 50);
+	EXPECT_FALSE(mlm.lambdaCurrentlyGood);
+	EXPECT_FALSE(mlm.isCut());
+
+	// 1.1 second later, cut!
+	timeNowUs = startTime + 1.1e6;
+	mlm.update(2000, 50);
+	EXPECT_FALSE(mlm.lambdaCurrentlyGood);
+	EXPECT_TRUE(mlm.isCut());
+
+	// Lambda goes back to normal, but restore conditions not met so we should stay cut
+	mlm.isGood = true;
+	mlm.update(2000, 50);
+	EXPECT_TRUE(mlm.lambdaCurrentlyGood);
+	EXPECT_TRUE(mlm.isCut());
+
+	mlm.isRestore = true;
+	mlm.update(2000, 50);
+	EXPECT_TRUE(mlm.lambdaCurrentlyGood);
+	EXPECT_FALSE(mlm.isCut());
 }
