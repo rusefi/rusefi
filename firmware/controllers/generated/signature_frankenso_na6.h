bbeca0189fe92cf456758f4c5a86eab54c98206a
GitHub gen-configs Action <action@github.com>
2023-07-26 17:41:42 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_frankenso_na6.h
+++ firmware/controllers/generated/signature_frankenso_na6.h
@@ -1,2 +1,2 @@
-#define SIGNATURE_HASH 1426450181
-#define TS_SIGNATURE "rusEFI (FOME) master.2023.07.26.frankenso_na6.1426450181"
+#define SIGNATURE_HASH 1831092890
+#define TS_SIGNATURE "rusEFI (FOME) master.2023.07.26.frankenso_na6.1831092890"
--- firmware/controllers/generated/signature_hellen-gm-e67.h
+++ firmware/controllers/generated/signature_hellen-gm-e67.h
@@ -1,2 +1,2 @@
-#define SIGNATURE_HASH 507390236
-#define TS_SIGNATURE "rusEFI (FOME) master.2023.07.26.hellen-gm-e67.507390236"
+#define SIGNATURE_HASH 639601795
+#define TS_SIGNATURE "rusEFI (FOME) master.2023.07.26.hellen-gm-e67.639601795"
