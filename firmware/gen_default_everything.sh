ab8d3235aa65cae5c2ea2ddc90e3c9a20ca6ee46
Matthew Kennedy <matthewkennedy@outlook.com>
2023-08-03 11:50:00 -0700

    gen_enum_to_string is part of gen_live_documentation

--- firmware/gen_default_everything.sh
+++ firmware/gen_default_everything.sh
@@ -2,9 +2,6 @@
 
 # is order of operations important here or not really?
 
-echo "gen_enum_to_string.sh as part of everything"
-./gen_enum_to_string.sh
-
 echo "gen_live_documentation.sh as part of everything"
 ./gen_live_documentation.sh
 
--- firmware/gen_live_documentation.sh
+++ firmware/gen_live_documentation.sh
@@ -9,3 +9,6 @@ java -DSystemOut.name=logs/gen_live_documentation \
 
 java -DSystemOut.name=logs/gen_java_enum -cp ../java_tools/enum2string.jar com.rusefi.ToJavaEnum -enumInputFile console/binary/generated/live_data_ids.h -outputPath ../java_console/io/src/main/java/com/rusefi/enums
 [ $? -eq 0 ] || { echo "ERROR generating live data ids"; exit 1; }
+
+# live_data_ids.h is generated above, so order of operations is important here!
+./gen_enum_to_string.sh
