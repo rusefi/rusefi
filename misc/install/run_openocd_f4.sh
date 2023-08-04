eea5f8293f11c70f697c1b28640e404c4e833c4a
Andrey <rusefillc@gmail.com>
2023-08-04 12:42:11 -0400

    only:mailboxes unit test

---
+++ misc/install/run_openocd_f4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+STLINK=$(lsusb | grep 'ST-LINK\|STLINK')
+if [ "$STLINK" ]; then
+  echo "I see $STLINK make sure your board file accepts this specific kind of ST-LINK"
+else
+  echo "Do not see any ST-LINK. If you are on WSL make sure you have bound USB"
+  exit -1
+fi
+
+sudo openocd -f board/stm32f4discovery.cfg -c init -c reset
