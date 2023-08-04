7af9f79562c565dc9b453ed9ff5112a8b9a7ef20
Andrey <rusefillc@gmail.com>
2023-08-04 12:34:24 -0400

    only:linux openocd

--- firmware/flash_openocd407.sh
+++ firmware/flash_openocd407.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-echo "I am flash_openocd.sh"
+echo "I am flash_openocd407.sh"
 
+STLINK=$(lsusb | grep 'ST-LINK\|STLINK')
+if [ "$STLINK" ]; then
+  echo "I see $STLINK make sure your board file accepts this specific kind of ST-LINK"
+else
+  echo "Do not see any ST-LINK. If you are on WSL make sure you have binded USB"
+  exit -1
+fi
 
-openocd -f board/stm32f4discovery.cfg  -c "program build/rusefi.bin verify reset exit 0x08000000"
-
+sudo openocd -f board/stm32f4discovery.cfg  -c "program build/rusefi.bin verify reset exit 0x08000000"
