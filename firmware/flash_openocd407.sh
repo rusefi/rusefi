55bf511ad432a24c4b1230ed22b11d1e745fdd36
rusefillc <rusefillc@gmail.com>
2023-08-04 09:01:11 -0400

    only:linux openocd

--- firmware/flash_openocd407.sh
+++ firmware/flash_openocd407.sh
@@ -3,5 +3,5 @@
 echo "I am flash_openocd.sh"
 
 
-openocd -f board/stm32f4discovery.cfg  -c "program ../../firmware/build/rusefi.bin verify reset exit 0x08000000"
+openocd -f board/stm32f4discovery.cfg  -c "program build/rusefi.bin verify reset exit 0x08000000"
 
