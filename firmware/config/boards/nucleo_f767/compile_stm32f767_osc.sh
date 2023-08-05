9bea3a0ca66c3604b2a2c455c7d3836b5ed0dfe5
rusefillc <48498823+rusefillc@users.noreply.github.com>
2023-08-05 11:33:29 -0400

    DUMMY no longer needed (#5485)
    * DUMMY no longer needed
    
    * one step back
    
    ---------
    
    Co-authored-by: rusefillc <sdfsdfqsf2334234234>

--- firmware/config/boards/nucleo_f767/compile_stm32f767_osc.sh
+++ firmware/config/boards/nucleo_f767/compile_stm32f767_osc.sh
@@ -2,7 +2,7 @@
 
 # STM32F767 version of the firmware for F767ZI chip NOT on Nucleo board, with a real oscilattor
 
-export EXTRA_PARAMS="-DDUMMY -DEFI_ENABLE_ASSERTS=FALSE \
+export EXTRA_PARAMS="-DEFI_ENABLE_ASSERTS=FALSE \
  -DCH_DBG_ENABLE_CHECKS=FALSE -DCH_DBG_ENABLE_ASSERTS=FALSE -DCH_DBG_ENABLE_STACK_CHECK=FALSE -DCH_DBG_FILL_THREADS=FALSE -DCH_DBG_THREADS_PROFILING=FALSE"
 
 # Do not forget to comment out following line if looking to debug!
