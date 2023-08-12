a16ad983fbb36354c3df3ed19c4dfee40ad2eb85
andreika-git <andrei.home@gmail.com>
2023-08-12 23:18:59 +0300

    CAN bench test: broadcast event counters #5436

--- firmware/controllers/can/can_bench_test.cpp
+++ firmware/controllers/can/can_bench_test.cpp
@@ -21,6 +21,29 @@ static void setPin(const CANRxFrame& frame, int value) {
 }
 
 void processCanBenchTest(const CANRxFrame& frame) {
+	if (CAN_EID(frame) == BENCH_TEST_EVENT_COUNTERS) {
+		CanTxMessage msg(CanCategory::BENCH_TEST, BENCH_TEST_RAW_ANALOG, 8);
+
+		int primaryFall = engine->triggerCentral.getHwEventCounter((int)SHAFT_PRIMARY_FALLING);
+		int primaryRise = engine->triggerCentral.getHwEventCounter((int)SHAFT_PRIMARY_RISING);
+		int secondaryFall = engine->triggerCentral.getHwEventCounter((int)SHAFT_SECONDARY_FALLING);
+		int secondaryRise = engine->triggerCentral.getHwEventCounter((int)SHAFT_SECONDARY_RISING);
+
+		msg[0] = (primaryRise + primaryFall) & 0xff;
+		msg[1] = (secondaryRise + secondaryFall) & 0xff;
+
+		for (int camIdx = 0; camIdx < 4; camIdx++) {
+			int vvtRise = 0, vvtFall = 0;
+			if (camIdx < CAM_INPUTS_COUNT) {
+				vvtRise = engine->triggerCentral.vvtEventRiseCounter[camIdx];
+				vvtFall = engine->triggerCentral.vvtEventFallCounter[camIdx];
+			}
+
+			msg[2 + camIdx] = (vvtRise + vvtFall) & 0xff;
+		}
+		
+		return;
+	}
 	if (CAN_EID(frame) != CAN_ECU_HW_META) {
 		return;
 	}
