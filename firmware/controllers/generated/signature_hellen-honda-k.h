ed5dbd72f2843812d0fd584f9e27668fe39f819d
GitHub gen-configs Action <action@github.com>
2023-08-20 02:28:25 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen-honda-k.h
+++ firmware/controllers/generated/signature_hellen-honda-k.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2970052162
-#define TS_SIGNATURE "rusEFI master.2023.08.20.hellen-honda-k.2970052162"
+#define SIGNATURE_HASH 2119878641
+#define TS_SIGNATURE "rusEFI master.2023.08.20.hellen-honda-k.2119878641"
