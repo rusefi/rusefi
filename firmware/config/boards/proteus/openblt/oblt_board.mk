16d3c5d8e78f3450d7bcf892ac4b7263750cdcfa
Matthew Kennedy <matthewkennedy@outlook.com>
2023-08-25 15:54:10 -0700

    no need to check

--- firmware/config/boards/proteus/openblt/oblt_board.mk
+++ firmware/config/boards/proteus/openblt/oblt_board.mk
@@ -11,10 +11,8 @@ PROJ_FILES += $(filter-out $(OPENBLT_BOARD_DIR)/flash_layout.c, $(call rwildcard
 ifeq ($(PROJECT_CPU),ARCH_STM32F4)
 	BRDFLAGS     = -DSTM32F429xx
 else ifeq ($(PROJECT_CPU),ARCH_STM32F7)
-	#TODO: check what CPU is on proteus f7
 	BRDFLAGS     = -DSTM32F767xx
 else ifeq ($(PROJECT_CPU),ARCH_STM32H7)
-	#TODO: check what CPU is on proteus h7
 	BRDFLAGS     = -DSTM32H743xx
 endif
 
