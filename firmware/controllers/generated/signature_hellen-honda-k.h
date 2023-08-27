8b450caad27cb8d4b39b26d1d4d61afd8fb009a6
GitHub gen-configs Action <action@github.com>
2023-08-27 06:31:12 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen-honda-k.h
+++ firmware/controllers/generated/signature_hellen-honda-k.h
@@ -1,2 +1,2 @@
-#define SIGNATURE_HASH 2759807294
-#define TS_SIGNATURE "rusEFI (FOME) master.2023.08.27.hellen-honda-k.2759807294"
+#define SIGNATURE_HASH 150925813
+#define TS_SIGNATURE "rusEFI (FOME) master.2023.08.27.hellen-honda-k.150925813"
