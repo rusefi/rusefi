4b5542123e31158b965404aa00610ca5fa4385f1
Andrey <rusefillc@gmail.com>
2023-08-28 23:16:23 -0400

    Need a one line build script: at least cheap build simplification? keyword: lazy #5225

--- firmware/config/boards/microrusefi/compile_mre_f4.sh
+++ firmware/config/boards/microrusefi/compile_mre_f4.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#
+# in order to get fresh .ini and ramdisk_image one still has to manually invoke
+# ./gen_config_board.sh config/boards/microrusefi mre_f4
+#
+
 export USE_OPENBLT=yes
 
 bash ../common_make.sh microrusefi ARCH_STM32F4
