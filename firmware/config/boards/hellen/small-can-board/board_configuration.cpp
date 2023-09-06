001cac3da57a33649a540aa200882706e43948a4
Matthew Kennedy <matthewkennedy@outlook.com>
2023-09-06 14:49:08 -0700

    small can pinout (?)

--- firmware/config/boards/hellen/small-can-board/board_configuration.cpp
+++ firmware/config/boards/hellen/small-can-board/board_configuration.cpp
@@ -7,4 +7,25 @@ void setBoardConfigOverrides() {
 	setHellen64Can();
 	setHellen64MegaEnPin();
 	setHellen64SdCardSpi1();
+
+	// 33k / 6.8k = 5.835
+	engineConfiguration->vbattDividerCoeff = (33 + 6.8) / 6.8;
+	engineConfiguration->vbattAdcChannel = EFI_ADC_5;
+
+	// Force I/O for Lua - so no TS is required
+	engineConfiguration->luaOutputPins[0] = Gpio::B7;
+	engineConfiguration->luaOutputPins[1] = Gpio::C8;
+	engineConfiguration->luaOutputPins[2] = Gpio::C9;
+	engineConfiguration->luaOutputPins[3] = Gpio::A8;
+
+	// TODO: are analog correct?
+	engineConfiguration->auxAnalogInputs[0] = EFI_ADC_0;
+	engineConfiguration->auxAnalogInputs[1] = EFI_ADC_1;
+	engineConfiguration->auxAnalogInputs[2] = EFI_ADC_2;
+	engineConfiguration->auxAnalogInputs[3] = EFI_ADC_3;
+
+	// TODO: are digital input correct?
+	engineConfiguration->luaDigitalInputPins[0] = Gpio::C2;
+	engineConfiguration->luaDigitalInputPins[1] = Gpio::C3;
+	engineConfiguration->luaDigitalInputPins[2] = Gpio::C4;
 }
