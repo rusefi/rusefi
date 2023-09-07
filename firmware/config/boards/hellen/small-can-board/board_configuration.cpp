d8edc9f61fad016e203b350940b6c988f10ff075
Matthew Kennedy <matthewkennedy@outlook.com>
2023-09-07 12:23:28 -0700

    small can defaults

--- firmware/config/boards/hellen/small-can-board/board_configuration.cpp
+++ firmware/config/boards/hellen/small-can-board/board_configuration.cpp
@@ -8,24 +8,25 @@ void setBoardConfigOverrides() {
 	setHellen64MegaEnPin();
 	setHellen64SdCardSpi1();
 
+	// 4.7k high side/4.7k low side = 2.0 ratio divider
+	engineConfiguration->analogInputDividerCoefficient = 2.0f;
+
 	// 33k / 6.8k = 5.835
 	engineConfiguration->vbattDividerCoeff = (33 + 6.8) / 6.8;
-	engineConfiguration->vbattAdcChannel = EFI_ADC_5;
+	engineConfiguration->vbattAdcChannel = EFI_ADC_11;
 
 	// Force I/O for Lua - so no TS is required
-	engineConfiguration->luaOutputPins[0] = Gpio::B7;
-	engineConfiguration->luaOutputPins[1] = Gpio::C8;
-	engineConfiguration->luaOutputPins[2] = Gpio::C9;
-	engineConfiguration->luaOutputPins[3] = Gpio::A8;
+	engineConfiguration->luaOutputPins[0] = H64_OUT_PWM1;
+	engineConfiguration->luaOutputPins[1] = H64_OUT_PWM2;
+	engineConfiguration->luaOutputPins[2] = H64_OUT_PWM3;
+	engineConfiguration->luaOutputPins[3] = H64_OUT_PWM4;
 
-	// TODO: are analog correct?
-	engineConfiguration->auxAnalogInputs[0] = EFI_ADC_0;
-	engineConfiguration->auxAnalogInputs[1] = EFI_ADC_1;
-	engineConfiguration->auxAnalogInputs[2] = EFI_ADC_2;
-	engineConfiguration->auxAnalogInputs[3] = EFI_ADC_3;
-
-	// TODO: are digital input correct?
-	engineConfiguration->luaDigitalInputPins[0] = Gpio::C2;
-	engineConfiguration->luaDigitalInputPins[1] = Gpio::C3;
-	engineConfiguration->luaDigitalInputPins[2] = Gpio::C4;
+	engineConfiguration->auxAnalogInputs[0] = EFI_ADC_2;
+	engineConfiguration->auxAnalogInputs[1] = EFI_ADC_3;
+	engineConfiguration->auxAnalogInputs[2] = EFI_ADC_4;
+	engineConfiguration->auxAnalogInputs[3] = EFI_ADC_5;
+	engineConfiguration->auxAnalogInputs[4] = EFI_ADC_6;
+	engineConfiguration->auxAnalogInputs[5] = EFI_ADC_7;
+	engineConfiguration->auxAnalogInputs[6] = EFI_ADC_14;
+	engineConfiguration->auxAnalogInputs[7] = EFI_ADC_15;
 }
