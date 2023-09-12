8bb137f464eb9ac9b7defb6efae37bb81338564c
mi-hol <22799428+mi-hol@users.noreply.github.com>
2023-09-12 19:46:29 +0200

    Update git_add_upstream.bat for FOME (#240)
    * Update git_add_upstream.bat for FOME
    
    hope this is correct. Please verify and update
    
    * Update misc/git_scripts/git_add_upstream.bat
    
    Co-authored-by: Nathan Schulte <8540239+nmschulte@users.noreply.github.com>
    
    * Update misc/git_scripts/git_add_upstream.bat
    
    Co-authored-by: Nathan Schulte <8540239+nmschulte@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Nathan Schulte <8540239+nmschulte@users.noreply.github.com>

--- misc/git_scripts/git_add_upstream.bat
+++ misc/git_scripts/git_add_upstream.bat
@@ -1,12 +1,8 @@
 git remote -v
-git remote add upstream https://github.com/rusefi/rusefi.git
+git remote add upstream https://github.com/FOME-Tech/fome-fw.git
 
 rem The heros, in alpabetical order
-git remote add andreika-git https://github.com/andreika-git/rusefi
-git remote add DonaldBecker https://github.com/DonaldBecker/rusefi
-git remote add dron0gus     https://github.com/dron0gus/rusefi 
-git remote add mck1117      https://github.com/mck1117/rusefi 
-git remote add NOx-z        https://github.com/NOx-z/rusefi 
-git remote add chuckwagoncomputing https://github.com/chuckwagoncomputing/rusefi 
+git remote add mck1117      https://github.com/mck1117/fome-fw 
+git remote add nmschulte    https://github.com/nmschulte/fome-fw.git
 
 git remote -v
