828715d47abb3652dbdffe715476bc88acaa6d3e
GitHub gen-configs Action <action@github.com>
2023-09-13 19:25:28 +0000

    Auto-generated configs and docs

--- firmware/controllers/algo/auto_generated_enginetypes.cpp
+++ firmware/controllers/algo/auto_generated_enginetypes.cpp
@@ -47,6 +47,10 @@ case DBG_10:
   return "DBG_10";
 case DBG_11:
   return "DBG_11";
+case DBG_12:
+  return "DBG_12";
+case DBG_13:
+  return "DBG_13";
 case DBG_15:
   return "DBG_15";
 case DBG_16:
@@ -57,6 +61,8 @@ case DBG_2:
   return "DBG_2";
 case DBG_20:
   return "DBG_20";
+case DBG_21:
+  return "DBG_21";
 case DBG_24:
   return "DBG_24";
 case DBG_25:
@@ -65,6 +71,8 @@ case DBG_26:
   return "DBG_26";
 case DBG_29:
   return "DBG_29";
+case DBG_32:
+  return "DBG_32";
 case DBG_34:
   return "DBG_34";
 case DBG_35:
@@ -79,16 +87,14 @@ case DBG_43:
   return "DBG_43";
 case DBG_46:
   return "DBG_46";
+case DBG_6:
+  return "DBG_6";
 case DBG_7:
   return "DBG_7";
 case DBG_8:
   return "DBG_8";
 case DBG_9:
   return "DBG_9";
-case DBG_ANALOG_INPUTS:
-  return "DBG_ANALOG_INPUTS";
-case DBG_ANALOG_INPUTS2:
-  return "DBG_ANALOG_INPUTS2";
 case DBG_COMPOSITE_LOG:
   return "DBG_COMPOSITE_LOG";
 case DBG_DWELL_METRIC:
@@ -115,10 +121,6 @@ case DBG_MAP:
   return "DBG_MAP";
 case DBG_METRICS:
   return "DBG_METRICS";
-case DBG_SD_CARD:
-  return "DBG_SD_CARD";
-case DBG_SOFT_SPARK_CUT:
-  return "DBG_SOFT_SPARK_CUT";
 case DBG_SR5_PROTOCOL:
   return "DBG_SR5_PROTOCOL";
 case DBG_STEPPER_IDLE_CONTROL:
@@ -131,8 +133,6 @@ case DBG_TPS_ACCEL:
   return "DBG_TPS_ACCEL";
 case DBG_TRIGGER_COUNTERS:
   return "DBG_TRIGGER_COUNTERS";
-case DBG_UNUSED12:
-  return "DBG_UNUSED12";
 case DBG_UNUSED41:
   return "DBG_UNUSED41";
 case DBG_UNUSED_42:
