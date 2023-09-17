2f37d788fff5bb5770c7391a715d3e40f5d95ae5
Matthew Kennedy <matthewkennedy@outlook.com>
2023-09-17 02:21:03 -0400

    add option to temperature compensate alpha-n

--- firmware/controllers/algo/airmass/alphan_airmass.cpp
+++ firmware/controllers/algo/airmass/alphan_airmass.cpp
@@ -13,11 +13,17 @@ AirmassResult AlphaNAirmass::getAirmass(int rpm, bool postState) {
 	// In this case, VE directly describes the cylinder filling relative to the ideal
 	float ve = getVe(rpm, tps.Value, postState);
 
+	// optionally use real IAT instead of fixed air temperature
+	constexpr float standardIat = 273.0f + 20.0f;	// std atmosphere temperature
+	float iat = engineConfiguration->alphaNUseIat
+		? Sensor::get(SensorType::Iat).value_or(standardIat)
+		: standardIat;
+
 	// TODO: should this be barometric pressure and/or temperature compensated?
 	mass_t airmass = getAirmassImpl(
 		ve,
 		101.325f,		// std atmosphere pressure
-		273.0f + 20.0f	// std atmosphere pressure
+		iat
 	);
 
 	return {
