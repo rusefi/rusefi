823fee6c4551f5fe94afbcd21a943c48c2c65709
rusefillc <sdfsdfqsf2334234234>
2023-09-18 15:39:19 -0400

    only: remove SID

--- firmware/init/sensor/init_flex.cpp
+++ firmware/init/sensor/init_flex.cpp
@@ -7,7 +7,7 @@
 static FrequencySensor flexSensor(SensorType::FuelEthanolPercent, MS2NT(500));
 static FlexConverter converter;
 
-// https://rusefi.com/forum/viewtopic.php?p=37452&sid=829804c90d5b2e1fecd1b900cf1b1811#p37452
+// https://rusefi.com/forum/viewtopic.php?p=37452#p37452
 
 void initFlexSensor() {
 	// 0.01 means filter bandwidth of ~1hz with ~100hz sensor
