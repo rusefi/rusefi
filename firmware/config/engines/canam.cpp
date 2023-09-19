817f35941a8066a8c97a70095be6fe91a98fa604
rusefi <rusefillc@gmail.com>
2023-09-19 17:01:57 -0400

    canam only:proteus_f7

--- firmware/config/engines/canam.cpp
+++ firmware/config/engines/canam.cpp
@@ -5,9 +5,13 @@
 #include "canam.h"
 
 // set engine_type 54
+// https://www.youtube.com/watch?v=j8DOFp02QDY
 void setMaverickX3() {
+    strcpy(engineConfiguration->engineMake, "Rotax");
+    strcpy(engineConfiguration->engineCode, "900 ACE");
+
     engineConfiguration->cylindersCount = 3;
-    // engineConfiguration->firingOrder = FO_;
+    engineConfiguration->firingOrder = FO_1_3_2;
     engineConfiguration->injectionMode = IM_SEQUENTIAL;
 
 #if HW_PROTEUS
