7645bfe0e7fd048bd6bdbb03c0dc5076896901c7
Andrey <rusefillc@gmail.com>
2023-09-21 22:06:44 -0400

    only:canam only:proteus_f7

--- firmware/config/engines/canam.cpp
+++ firmware/config/engines/canam.cpp
@@ -27,29 +27,7 @@ void setMaverickX3() {
 #endif // HW_PROTEUS
 
 #if HW_PROTEUS
-	strncpy(config->luaScript, GET_BIT_RANGE_LSB TWO_BYTES_LSB PRINT_ARRAY SET_TWO_BYTES_LSB R"(
-
-
-isUseful = Timer.new()
-isUseful : reset()
-
-function onTick()
-
-rpm = getSensor("RPM")
-vbat = getSensor("BatteryVoltage")
-
---		print (vbat .. " " .. rpm)
-if (vbat < 8) or (rpm > 200) then
--- keep alive if USB hooked up
-isUseful : reset()
-end
-
-	if (isUseful : getElapsedSeconds() > 17) then
-		mcu_standby()
-	end
-end
-
-)", efi::size(config->luaScript));
+    #include "canam_2021.lua"
 #endif // HW_PROTEUS
 
 }
---
+++ firmware/config/engines/canam_2021.lua
@@ -0,0 +1,23 @@
+	strncpy(config->luaScript, GET_BIT_RANGE_LSB TWO_BYTES_LSB PRINT_ARRAY SET_TWO_BYTES_LSB R"(
+
+
+isUseful = Timer.new()
+isUseful : reset()
+
+function onTick()
+
+rpm = getSensor("RPM")
+vbat = getSensor("BatteryVoltage")
+
+--		print (vbat .. " " .. rpm)
+if (vbat < 8) or (rpm > 200) then
+-- keep alive if USB hooked up
+isUseful : reset()
+end
+
+	if (isUseful : getElapsedSeconds() > 17) then
+		mcu_standby()
+	end
+end
+
+)", efi::size(config->luaScript));
