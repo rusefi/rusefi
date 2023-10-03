085415868ff366f4df424b846c402cfae4140d76
Matthew Kennedy <matthewkennedy@outlook.com>
2023-10-02 22:14:03 -0700

    trip odometer run time

--- firmware/controllers/algo/trip_odometer.h
+++ firmware/controllers/algo/trip_odometer.h
@@ -12,12 +12,19 @@ public:
 	uint32_t getDistanceMeters() const;
 
 private:
+	// Fuel consumed
 	uint32_t m_consumedGrams = 0;
 	float m_consumedRemainder = 0;
 	float m_rate = 0;
 
+	// Distance travelled
 	uint32_t m_distanceMeters = 0;
 	float m_distanceRemainder = 0;
+
+	// Engine run time and ign on time
+	uint32_t m_slowCallbackCounter = 0;
+	uint32_t m_ignitionOnSeconds = 0;
+	uint32_t m_engineRunningSeconds = 0;
 
 	Timer m_timer;
 };
