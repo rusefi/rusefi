701c54713e5fcd96b42c732cc31e60517f21afda
GitHub gen-configs Action <action@github.com>
2023-10-13 01:45:29 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen-112-17.h
+++ firmware/controllers/generated/signature_hellen-112-17.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1917649567
-#define TS_SIGNATURE "rusEFI master.2023.10.13.hellen-112-17.1917649567"
+#define SIGNATURE_HASH 3104806563
+#define TS_SIGNATURE "rusEFI master.2023.10.13.hellen-112-17.3104806563"
