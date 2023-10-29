b004948262652f6c37797895c389eab95c27ae92
GitHub gen-configs Action <action@github.com>
2023-10-29 17:30:50 +0000

    Auto-generated configs and docs

---
+++ firmware/controllers/generated/signature_at_start_f435.h
@@ -0,0 +1,6 @@
+//
+// was generated automatically by rusEFI tool config_definition.jar based on gen_config.sh by SignatureConsumer
+//
+
+#define SIGNATURE_HASH 998272346
+#define TS_SIGNATURE "rusEFI master.2023.10.29.at_start_f435.998272346"
