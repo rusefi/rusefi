85167c25109aa34f9bda0934ec3b8543ad96ad25
GitHub gen-configs Action <action@github.com>
2023-10-31 15:24:31 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_m74_9.h
+++ firmware/controllers/generated/signature_m74_9.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1016282064
-#define TS_SIGNATURE "rusEFI master.2023.10.31.m74_9.1016282064"
+#define SIGNATURE_HASH 1288750686
+#define TS_SIGNATURE "rusEFI master.2023.10.31.m74_9.1288750686"
