8d1bdacafbc64afc89d233855978d06863e8a5e2
rusefi <rusefillc@gmail.com>
2023-10-31 22:47:57 -0400

    fuelTempGauge only:small-can-board

--- firmware/console/status_loop.cpp
+++ firmware/console/status_loop.cpp
@@ -386,6 +386,7 @@ static void updateTempSensors() {
 	SensorResult oilTemp = Sensor::get(SensorType::OilTemperature);
 	engine->outputChannels.oilTemp = oilTemp.value_or(0);
 
+    // see also updateFuelSensors()
 	SensorResult fuelTemp = Sensor::get(SensorType::FuelTemperature);
 	engine->outputChannels.fuelTemp = fuelTemp.value_or(0);
 
