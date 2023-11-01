bb2fe87a1b0be98ca6ec8f4ce00859e34bf74c54
Matthew Kennedy <matthewkennedy@outlook.com>
2023-11-01 16:46:31 -0700

    no error for undefined preprocessor definition

--- firmware/rusefi_rules.mk
+++ firmware/rusefi_rules.mk
@@ -2,4 +2,4 @@
 RUSEFI_OPT = -Werror
 
 # ...except these few
-RUSEFI_OPT += -Wno-error=unused-function -Wno-error=unused-variable -Wno-error=sign-compare -Wno-error=unused-parameter
+RUSEFI_OPT += -Wno-error=unused-function -Wno-error=unused-variable -Wno-error=sign-compare -Wno-error=unused-parameter -Wno-error=undef
