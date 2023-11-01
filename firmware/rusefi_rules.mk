dd7a49a00deb380cecb853ab2083fa6554cd60b6
rusefillc <sdfsdfqsf2334234234>
2023-11-01 13:18:22 -0400

    only:variable shadowing should be avoided fix #5676

--- firmware/rusefi_rules.mk
+++ firmware/rusefi_rules.mk
@@ -1,5 +1,5 @@
 # Warnings-as-errors...
-RUSEFI_OPT = -Werror
+RUSEFI_OPT = -Werror -Werror=shadow
 
 # ...except these few
 RUSEFI_OPT += -Wno-error=unused-function -Wno-error=unused-variable -Wno-error=sign-compare -Wno-error=unused-parameter
