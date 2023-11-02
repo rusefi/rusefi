7b022b07c6a10d8f5dc9257ae6bac99dd2590dc6
Matthew Kennedy <matthewkennedy@outlook.com>
2023-11-01 17:02:07 -0700

    enable check for shadowing!

--- firmware/rusefi_rules.mk
+++ firmware/rusefi_rules.mk
@@ -1,5 +1,6 @@
 # Warnings-as-errors...
 RUSEFI_OPT = -Werror
+RUSEFI_CPPOPT = -Werror=shadow
 
 # ...except these few
 RUSEFI_OPT += -Wno-error=unused-function -Wno-error=unused-variable -Wno-error=sign-compare -Wno-error=unused-parameter -Wno-error=undef -Wno-error=stringop-overflow
