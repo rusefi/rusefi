f031007b16c0f2c9da0928f7e44bd04d3cd96ceb
Andrey <rusefillc@gmail.com>
2023-11-01 22:34:53 -0400

    variable shadowing should be avoided #5676 helping kinetis

---
+++ firmware/controllers/core/log_hard_fault.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+#if EFI_PROD_CODE
+#include <hal.h>
+
+void logHardFault(uint32_t type, uintptr_t faultAddress, port_extctx* ctx, uint32_t csfr);
+#endif // EFI_PROD_CODE
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
