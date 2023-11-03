f5251e50cf4915d3a14a0d93f1fb4179b7e490f3
Andrey Gusakov <dron0gus@gmail.com>
2023-11-03 14:07:12 +0300

    at_start_f435: enable MFS for settings storing in internal flash

---
+++ firmware/config/boards/at_start_f435/board_storage.cpp
@@ -0,0 +1,39 @@
+/**
+ * @file boards/subaru_eg33/board_storage.h
+ *
+ * @brief Storage configuration file
+ *
+ * @date Nov 03, 2023
+ * @author Andrey Gusakov, 2023
+ */
+
+#include "pch.h"
+
+/* This board stores settings in internal flash bank 2 */
+#if EFI_STORAGE_MFS == TRUE
+
+#include "hal_mfs.h"
+
+/* use 128K + 128K on bank 2 for settings */
+const MFSConfig mfscfg1 = {
+  .flashp           = (BaseFlash *)&EFLD2,
+  .erased           = 0xFFFFFFFFU,
+  .bank_size        = 128U * 1024U,
+  .bank0_start      = 0U,
+  .bank0_sectors    = 32U,
+  .bank1_start      = 32U,
+  .bank1_sectors    = 32U
+};
+
+void boardInitMfs()
+{
+  /* Starting EFL driver.*/
+  eflStart(&EFLD2, NULL);
+}
+
+const MFSConfig *boardGetMfsConfig()
+{
+	return &mfscfg1;
+}
+
+#endif /* EFI_STORAGE_MFS == TRUE */
