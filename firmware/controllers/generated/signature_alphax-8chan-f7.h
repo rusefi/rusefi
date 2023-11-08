d9bd5e9c5bfc8c8373d15672c8c7197efe5efc60
rusefillc <sdfsdfqsf2334234234>
2023-11-08 11:16:49 -0500

    typos & human errors

--- firmware/controllers/generated/signature_alphax-8chan-f7.h
+++
@@ -1,6 +0,0 @@
-//
-// was generated automatically by rusEFI tool config_definition.jar based on gen_config.sh by SignatureConsumer
-//
-
-#define SIGNATURE_HASH 819766844
-#define TS_SIGNATURE "rusEFI master.2023.06.20.alphax-8chan-f7.819766844"
