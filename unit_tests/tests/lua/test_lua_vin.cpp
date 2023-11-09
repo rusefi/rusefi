950f0d4525a7ce93c26957b4f0b4cff9bcc12f0a
Andrey <rusefillc@gmail.com>
2023-11-08 21:52:43 -0500

    only:vincpy

--- unit_tests/tests/lua/test_lua_vin.cpp
+++ unit_tests/tests/lua/test_lua_vin.cpp
@@ -13,11 +13,29 @@ TEST(LuaVin, Test) {
 
 	strcpy(engineConfiguration->vinNumber, "GM123");
 
-	const char* realdata = TWO_BYTES_MSB R"(
+	const char* realdata = R"(
 
 	function testFunc()
 		return vin(0)
 	end)";
 
 	EXPECT_NEAR_M3(testLuaReturnsNumberOrNil(realdata).value_or(0), 0x47);
+}
+
+TEST(LuaVinCpy, Test) {
+	EngineTestHelper eth(engine_type_e::TEST_CRANK_ENGINE);
+
+	strcpy(engineConfiguration->vinNumber, "GM123");
+
+	const char* realdata = ARRAY_EQUALS PRINT_ARRAY R"(
+
+	function testFunc()
+	    data = {0, 0, 0, 0, 0}
+	    vincpy(data, 2, 1, 3);
+		print(arrayToString(data))
+	    expected = { 0x31, 0x32, 0x33, 0x00, 0x00 }
+		return equals(data, expected)
+	end)";
+
+	EXPECT_NEAR_M3(testLuaReturnsNumberOrNil(realdata).value_or(0), 0);
 }
