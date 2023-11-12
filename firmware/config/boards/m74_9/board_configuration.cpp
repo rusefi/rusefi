0f19de2108e3949f37ab8b1c39aacb81c9bf35c2
Andrey Gusakov <dron0gus@gmail.com>
2023-11-12 19:24:17 +0300

    m74.9: L9779 spi interface

--- firmware/config/boards/m74_9/board_configuration.cpp
+++ firmware/config/boards/m74_9/board_configuration.cpp
@@ -19,13 +19,29 @@ Gpio getRunningLedPin() {
 	return Gpio::Unassigned;
 }
 
+/**
+ * @brief   Board-specific configuration defaults.
+ * @todo    Add your board-specific code, if any.
+ */
+void setBoardDefaultConfiguration() {
+	/* SPI1 is used for communication with L9779 */
+	engineConfiguration->is_enabled_spi_1 = true;
+
+	engineConfiguration->spi1mosiPin = Gpio::E15;
+	engineConfiguration->spi1MosiMode = PO_DEFAULT;
+	engineConfiguration->spi1misoPin = Gpio::E14;
+	engineConfiguration->spi1MisoMode = PO_DEFAULT;
+	engineConfiguration->spi1sckPin = Gpio::E13;
+	engineConfiguration->spi1SckMode = PO_DEFAULT;
+}
+
 static struct l9779_config l9779_cfg = {
 	.spi_bus = &SPID1,
 	.spi_config = {
 		.circular = false,
 		.end_cb = NULL,
-		.ssport = NULL,
-		.sspad = 0,
+		.ssport = GPIOE,
+		.sspad = 11,
 		.cr1 =
 			SPI_CR1_16BIT_MODE |
 			SPI_CR1_SSM |
