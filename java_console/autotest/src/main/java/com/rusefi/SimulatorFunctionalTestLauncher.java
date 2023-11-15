461926ef20626e56809e80c60d85ba53f4184aa1
Andrey <rusefillc@gmail.com>
2023-11-15 00:16:29 -0500

    maybe probably not

--- java_console/autotest/src/main/java/com/rusefi/SimulatorFunctionalTestLauncher.java
+++ java_console/autotest/src/main/java/com/rusefi/SimulatorFunctionalTestLauncher.java
@@ -3,17 +3,24 @@ package com.rusefi;
 import com.rusefi.io.LinkManager;
 import com.rusefi.simulator.SimulatorFunctionalTest;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * this class runs rusEFI functional tests against rusEFI simulator
  */
 public class SimulatorFunctionalTestLauncher {
     static volatile boolean isHappy;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
             e.printStackTrace();
             System.exit(66);
         });
         boolean startSimulator = args.length == 1 && args[0].equalsIgnoreCase("start");
+
+//        if (startSimulator) {
+//            buildSimulator();
+//        }
 
         long start = System.currentTimeMillis();
         FileLog.SIMULATOR_CONSOLE.start();
@@ -40,5 +47,11 @@ public class SimulatorFunctionalTestLauncher {
         long time = (System.currentTimeMillis() - start) / 1000;
         FileLog.MAIN.logLine("Done in " + time + "secs");
         System.exit(0); // this is a safer method eliminating the issue of non-daemon threads
+    }
+
+    private static void buildSimulator() throws IOException, InterruptedException {
+        Process makeProcess = Runtime.getRuntime().exec("make -j8", null, new File("../simulator"));
+        SimulatorExecHelper.dumpProcessOutput(makeProcess, null);
+        makeProcess.waitFor();
     }
 }
