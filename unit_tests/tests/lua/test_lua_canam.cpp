cd19753a7d5acd37398f5313a71d9c07ccfac8d3
Andrey <rusefillc@gmail.com>
2023-11-18 01:45:34 -0500

    only: canam

--- unit_tests/tests/lua/test_lua_canam.cpp
+++ unit_tests/tests/lua/test_lua_canam.cpp
@@ -14,6 +14,30 @@ TEST(canam, ecu102_1) {
     EXPECT_NEAR_M3(testLuaReturnsNumberOrNil(realdata).value_or(0), 0x45);
 }
 
+TEST(canam, bytes) {
+	const char* realdata = TWO_BYTES_MSB R"(
+
+	function testFunc()
+		data = {0x00, 0x00, 0x00, 0x00, 0x01, 0xc7, 0x00, 0xc6}
+		return getTwoBytesMSB(data, 4, 1)
+	end
+	)";
+
+    EXPECT_NEAR_M3(testLuaReturnsNumberOrNil(realdata).value_or(0), 455);
+}
+
+TEST(canam, bytes2) {
+	const char* realdata = TWO_BYTES_MSB R"(
+
+	function testFunc()
+		data = {0x00, 0x00, 0x04, 0xa2, 0x02, 0x6c, 0x00, 0x00}
+		return getTwoBytesMSB(data, 2, 1)
+	end
+	)";
+
+    EXPECT_NEAR_M3(testLuaReturnsNumberOrNil(realdata).value_or(0), 1186);
+}
+
 TEST(canam, ecu102_4) {
 	const char* realdata = VAG_CHECKSUM R"(
 
