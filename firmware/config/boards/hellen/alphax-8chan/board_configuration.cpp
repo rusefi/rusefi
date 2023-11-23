d4b5e015a383e391e0952d2ea45d0b20838f45e3
rusefillc <sdfsdfqsf2334234234>
2023-11-23 00:15:20 -0500

    only:alphax-8chan

--- firmware/config/boards/hellen/alphax-8chan/board_configuration.cpp
+++ firmware/config/boards/hellen/alphax-8chan/board_configuration.cpp
@@ -194,10 +194,57 @@ void boardPrepareForStop() {
 }
 
 static Gpio OUTPUTS[] = {
-		Gpio::MM176_INJ1,
-		Gpio::MM176_INJ2,
-//		Gpio::MM176_INJ3,
-//		Gpio::MM176_INJ4,
+	Gpio::MM176_INJ1, // 1D - Injector 1
+	Gpio::MM176_INJ2, // 2D - Injector 2
+//	Gpio::MM176_INJ3, // 3D - Injector 3
+//	Gpio::MM176_INJ4, // 4D - Injector 4
+//	Gpio::MM176_INJ5, // 5D - Injector 5
+//	Gpio::MM176_INJ6, // 6D - Injector 6
+//	Gpio::MM176_INJ7, // 7D - Injector 7
+//	Gpio::MM176_OUT_PWM1, // 8D - VVT 1
+//	Gpio::MM176_GP3, // 9D - Fuel Pump
+//	Gpio::MM176_GP2, // 10D - Fan
+//	Gpio::MM176_GP1, // 11D - Main Relay
+//	Gpio::MM176_OUT_IO8, // 12D - NOS
+//	Gpio::MM176_INJ8, // 13D - Injector 8
+//	Gpio::MM176_OUT_PWM2, // 14D - VVT 2
+//	Gpio::MM176_OUT_IO13, // 15D - Tachometer
+//	Gpio::MM176_OUT_PWM6, // 16C - Low Side 1
+//	Gpio::MM176_GP17, // 19D - Injector 9
+//	Gpio::MM176_OUT_PWM3, // 20D - VVT 3
+//	Gpio::MM176_OUT_PWM4, // 21D - VVT 4
+//	Gpio::MM176_OUT_PWM5, // 22C - Boost
+//	Gpio::MM176_OUT_PWM7, // 23C - Low Side 2
+//	Gpio::MM176_GP20, // 24D - Injector 12
+//	Gpio::MM176_GP19, // 25D - Injector 11
+//	Gpio::MM176_GP18, // 26D - Injector 10
+
+//	Gpio::MM176_OUT_IO3, // 8C - Low Side 3
+//	Gpio::MM176_OUT_IO4, // 9C - Low Side 4
+//	Gpio::MM176_OUT_IO5, // 17C - Low Side 5
+//	Gpio::MM176_OUT_IO6, // 25C - Low Side 6
+//	Gpio::MM176_OUT_IO7, // 34C - Low Side 7
+
+
+//	Gpio::MM176_OUT_PWM8, // 5C - High Side 1
+//	Gpio::MM176_OUT_IO1, // 6C - High Side 2
+//	Gpio::MM176_OUT_IO2, // 7C - High Side 3
+//	Gpio::MM176_IGN1, // 11C - Ignition 1
+//	Gpio::MM176_IGN3, // 13C - Ignition 3
+//	Gpio::MM176_IGN5, // 14C - Ignition 5
+//	Gpio::MM176_IGN8, // 15C - Ignition 8
+//
+//	Gpio::MM176_IGN2, // 19C - Ignition 2
+//	Gpio::MM176_IGN4, // 20C - Ignition 4
+//	Gpio::MM176_IGN7, // 21C - Ignition 7
+//	Gpio::MM176_SPI3_MISO, // 23C - Ignition 10
+//
+//	Gpio::MM176_IGN6, // 28C - Ignition 6
+//	Gpio::MM176_SPI3_SCK, // 29C - Ignition 9
+//	Gpio::MM176_SPI3_MOSI, // 30C - Ignition 11
+//	Gpio::MM176_SPI3_CS, // 31C - Ignition 12
+
+
 };
 
 int getBoardMetaOutputsCount() {
