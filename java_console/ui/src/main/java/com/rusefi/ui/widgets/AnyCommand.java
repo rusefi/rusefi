71bc7d5fb6720e3f1b0bb37f148ad7afb787a2d6
Matthew Kennedy <matthewkennedy@outlook.com>
2023-11-25 01:46:57 -0800

    remove console dependency on autotest, antlr

--- java_console/ui/src/main/java/com/rusefi/ui/widgets/AnyCommand.java
+++ java_console/ui/src/main/java/com/rusefi/ui/widgets/AnyCommand.java
@@ -3,7 +3,6 @@ package com.rusefi.ui.widgets;
 import com.rusefi.FileLog;
 import com.rusefi.NamedThreadFactory;
 import com.rusefi.core.MessagesCentral;
-import com.rusefi.functional_tests.EcuTestHelper;
 import com.rusefi.io.CommandQueue;
 import com.rusefi.io.LinkManager;
 import com.rusefi.ui.RecentCommands;
