4fff20bcbfa9043386d779316cdd41e2ef24f0c0
GitHub gen-configs Action <action@github.com>
2023-11-26 17:45:01 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_harley81.h
+++ firmware/controllers/generated/signature_harley81.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1831903530
-#define TS_SIGNATURE "rusEFI master.2023.11.26.harley81.1831903530"
+#define SIGNATURE_HASH 1915113206
+#define TS_SIGNATURE "rusEFI master.2023.11.26.harley81.1915113206"
