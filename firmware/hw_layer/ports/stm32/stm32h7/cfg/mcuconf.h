16ffc66ab3ccdf26ff92a3a64a87f5674a094af5
andreika-git <andrei.home@gmail.com>
2023-11-28 22:37:37 +0200

    enable STM32_WDG_USE_IWDG for H7 and AT32

--- firmware/hw_layer/ports/stm32/stm32h7/cfg/mcuconf.h
+++ firmware/hw_layer/ports/stm32/stm32h7/cfg/mcuconf.h
@@ -509,7 +509,7 @@
 /*
  * WDG driver system settings.
  */
-#define STM32_WDG_USE_IWDG                  FALSE
+#define STM32_WDG_USE_IWDG                  TRUE
 
 /*
  * WSPI driver system settings.
