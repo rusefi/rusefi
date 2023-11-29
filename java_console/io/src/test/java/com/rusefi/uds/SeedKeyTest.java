54c850c7183a4fb641b8be452ae6a03a1725a6a2
rusefillc <sdfsdfqsf2334234234>
2023-11-28 20:52:08 -0500

    only:m74.9 seed/key

--- java_console/io/src/test/java/com/rusefi/uds/SeedKeyTest.java
+++ java_console/io/src/test/java/com/rusefi/uds/SeedKeyTest.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class SeedKeyTest {
+
+    public static final int BOOTLOADER_SECRET = 0xB24F5249;
+    public static final int SECRET = 0x57649392;
+
     public static int Uds_Security_CalcKey(int secret, int seed, int rnd) {
         if (rnd < 220)
             rnd += 35;
@@ -22,6 +26,7 @@ public class SeedKeyTest {
 
     @Test
     public void testAssert() {
-        assertEquals(0xC42F15AE, Uds_Security_CalcKey(0xB24F5249, 0x5D8A2010, 0xF0));
+        assertEquals(0xC42F15AE, Uds_Security_CalcKey(BOOTLOADER_SECRET, 0x5D8A2010, 0xF0));
+        assertEquals(0x001B6F78, Uds_Security_CalcKey(SECRET, 0x5DA0B808, 0xA4));
     }
 }
