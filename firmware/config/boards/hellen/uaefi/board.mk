33e6ee9b8cbec7b5846accc54cae201417540b25
rusefillc <sdfsdfqsf2334234234>
2023-11-30 14:51:30 -0500

    only:uaefi

---
+++ firmware/config/boards/hellen/uaefi/board.mk
@@ -0,0 +1,22 @@
+# Combine the related files for a specific platform and MCU.
+
+# Target ECU board design
+BOARDCPPSRC = $(BOARD_DIR)/board_configuration.cpp
+# Set this if you want a default engine type other than normal
+ifeq ($(VAR_DEF_ENGINE_TYPE),)
+#  VAR_DEF_ENGINE_TYPE = -DDEFAULT_ENGINE_TYPE=engine_type_e::HELLEN_154_HYUNDAI_COUPE_BK2
+endif
+
+DDEFS += -DEFI_MAIN_RELAY_CONTROL=TRUE
+
+#no mux on mm100
+
+# Add them all together
+DDEFS += -DFIRMWARE_ID=\"uaefi\" $(VAR_DEF_ENGINE_TYPE)
+#DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE -DSTM32_ADC_USE_ADC3=TRUE
+
+SHORT_BOARD_NAME=uaefi
+
+DDEFS += -DHELLEN_BOARD_ID_DEBUG
+
+include $(BOARDS_DIR)/hellen/hellen-common100.mk
