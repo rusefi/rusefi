8f637b9f2ced11ac4c116390fa3ee4bdb2743481
rusefillc <sdfsdfqsf2334234234>
2023-12-02 13:54:07 -0500

    dead

--- firmware/controllers/generated/signature_harley81.h
+++
@@ -1,6 +0,0 @@
-//
-// was generated automatically by rusEFI tool config_definition.jar based on gen_config.sh by SignatureConsumer
-//
-
-#define SIGNATURE_HASH 1097358053
-#define TS_SIGNATURE "rusEFI master.2023.12.01.harley81.1097358053"
--- firmware/controllers/generated/signature_hellen-112-mg1.h
+++
@@ -1,6 +0,0 @@
-//
-// was generated automatically by rusEFI tool config_definition.jar based on gen_config.sh by SignatureConsumer
-//
-
-#define SIGNATURE_HASH 231227313
-#define TS_SIGNATURE "rusEFI master.2023.12.01.hellen-112-mg1.231227313"
