4466133ca1581b7ad8ba3b6b7264827e7a272f2d
rusefillc <sdfsdfqsf2334234234>
2023-12-02 10:58:50 -0500

    dead

--- firmware/config/boards/subaru_eg33/board_configuration.cpp
+++ firmware/config/boards/subaru_eg33/board_configuration.cpp
@@ -274,16 +274,13 @@ static const struct mc33810_config mc33810_even = {
 		.ssport = GPIOF,
 		.sspad = 2,
 		.cr1 =
-			//SPI_CR1_16BIT_MODE |
 			SPI_CR1_SSM |
 			SPI_CR1_SSI |
 			((3 << SPI_CR1_BR_Pos) & SPI_CR1_BR) |	/* div = 16 */
 			SPI_CR1_MSTR |
-			/* SPI_CR1_CPOL | */ // = 0
-			SPI_CR1_CPHA | // = 1
+			SPI_CR1_CPHA |
 			0,
-		.cr2 = //SPI_CR2_16BIT_MODE |
-			SPI_CR2_DS_3 | SPI_CR2_DS_2 | SPI_CR2_DS_1 | SPI_CR2_DS_0
+		.cr2 = SPI_CR2_DS_3 | SPI_CR2_DS_2 | SPI_CR2_DS_1 | SPI_CR2_DS_0
 	},
 	.direct_io = {
 		/* injector drivers */
