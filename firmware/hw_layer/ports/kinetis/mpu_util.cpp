d2f5ee285009937c641b5e298774f074b7bb6dba
Andrey Gusakov <dron0gus@gmail.com>
2023-12-07 00:15:45 +0300

    CAN: show low-level error counters and flags
    STM32 only

--- firmware/hw_layer/ports/kinetis/mpu_util.cpp
+++ firmware/hw_layer/ports/kinetis/mpu_util.cpp
@@ -233,6 +233,12 @@ CANDriver* detectCanDevice(brain_pin_e pinRx, brain_pin_e pinTx) {
    return NULL;
 }
 
+void canHwInfo(CANDriver* cand)
+{
+	/* TODO: */
+	(void)cand;
+}
+
 #endif /* EFI_CAN_SUPPORT */
 
 bool allowFlashWhileRunning() {
