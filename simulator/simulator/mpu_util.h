026c9b34d99dde8bf41da7fd043a6e83b402562f
andreika-git <andrei.home@gmail.com>
2023-12-13 14:41:56 +0200

    Reset_Cause_t for simulator

--- simulator/simulator/mpu_util.h
+++ simulator/simulator/mpu_util.h
@@ -10,6 +10,20 @@ static void canHwInfo(CANDriver*) { return; }
 static bool allowFlashWhileRunning() { return true; }
 static void causeHardFault() { }
 
+
+// Reset Cause
+typedef enum {
+	Reset_Cause_Unknown = 0,
+	Reset_Cause_IWatchdog,		// Independent hardware watchdog (we use it)
+	Reset_Cause_WWatchdog,		// Window watchdog
+	Reset_Cause_Soft_Reset,		// NVIC_SystemReset or by debugger
+	Reset_Cause_NRST_Pin,		// Reset from NRST pin
+	Reset_Cause_Illegal_Mode,	// Reset after illegal Stop, Standby or Shutdown mode entry
+	Reset_Cause_BOR,			// BOR reset
+	Reset_Cause_Firewall,		// Firewall reset
+	Reset_Cause_Option_Byte,	// Option byte load reset
+} Reset_Cause_t;
+
 static Reset_Cause_t getMCUResetCause() {
 	return Reset_Cause_Unknown;
 }
