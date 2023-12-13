2930e2a19c61589425f29e4749f62f01c6c3aa9e
Andrey Gusakov <dron0gus@gmail.com>
2023-12-14 00:21:32 +0300

    sensors: accelerometer: use ChibiOS's lis2dw12 driver

--- firmware/hw_layer/sensors/accelerometer.cpp
+++ firmware/hw_layer/sensors/accelerometer.cpp
@@ -26,67 +26,105 @@
 #include "lis302dl.h"
 #include "periodic_thread_controller.h"
 
-static SPIDriver *driver;
+#if (EFI_ONBOARD_MEMS_LIS2DW12 == TRUE)
+#include "lis2dw12.h"
+#endif
 
 /*
  * SPI1 configuration structure.
  * Speed 5.25MHz, CPHA=1, CPOL=1, 8bits frames, MSb transmitted first.
- * The slave select line is the pin GPIOE_CS_SPI on the port GPIOE.
  */
-static const SPIConfig accelerometerCfg = {
-	.spi_bus = NULL,
+static SPIConfig accelerometerSpiCfg = {
+#if SPI_SUPPORTS_CIRCULAR == TRUE
+	.circular = FALSE,
+#endif
+	.end_cb = NULL,
 	/* HW dependent part.*/
-	.ssport = GPIOE,
-	.sspad = GPIOE_PIN3,
+	.ssport = NULL,
+	.sspad = 0,
 	.cr1 = SPI_CR1_BR_0 | SPI_CR1_BR_1 | SPI_CR1_CPOL | SPI_CR1_CPHA |
 		SPI_CR1_8BIT_MODE,
 	.cr2 = SPI_CR2_8BIT_MODE
 };
 
-static THD_WORKING_AREA(ivThreadStack, UTILITY_THREAD_STACK_SIZE);
+//static OutputPin chipSelect;
+
+#if (EFI_ONBOARD_MEMS_LIS2DW12 == TRUE)
+
+static LIS2DW12Config lis2dw12cfg = {
+#if LIS2DW12_USE_SPI
+	.spip = NULL,
+	.spicfg = &accelerometerSpiCfg,
+#endif
+#ifdef LIS2DW12_USE_I2C
+	/* TODO: */
+#endif
+	.accsensitivity = NULL,
+	.accbias = NULL,
+	.accoutputdatarate = LIS2DW12_ACC_ODR_25HZ,
+	.accoutputresolution = LIS2DW12_ACC_OR_HP,
+	.acclowpowermode = LIS2DW12_ACC_LP_MODE2,
+	.accbadwidthselect = LIS2DW12_ACC_BW_ODR4,
+	.accfullscale = LIS2DW12_ACC_FS_4G,
+};
+
+/* LIS302DL Driver: This object represent an LIS302DL instance */
+static LIS2DW12Driver LIS2DW12;
+
+#endif
+
+//static THD_WORKING_AREA(ivThreadStack, UTILITY_THREAD_STACK_SIZE);
 
 class AccelController : public PeriodicController<UTILITY_THREAD_STACK_SIZE> {
 public:
 	AccelController() : PeriodicController("Acc SPI") { }
 private:
 	void PeriodicTask(efitick_t nowNt) override	{
-		// has to be a thread since we want to use blocking method - blocking method only available in threads, not in interrupt handler
-		// todo: migrate to async SPI API?
-		engine->sensors.accelerometer.x = (int8_t)lis302dlReadRegister(driver, LIS302DL_OUTX);
-		engine->sensors.accelerometer.y = (int8_t)lis302dlReadRegister(driver, LIS302DL_OUTY);
-		chThdSleepMilliseconds(20);
+		float acccooked[LIS2DW12_ACC_NUMBER_OF_AXES];
+
+		lis2dw12AccelerometerReadCooked(&LIS2DW12, acccooked);
+		efiPrintf("Acc: %f, %f, %f", acccooked[0], acccooked[1], acccooked[2]);
+		//chThdSleepMilliseconds(20);
 	}
 };
 
-static BenchController instance;
+static AccelController instance;
 
 void initAccelerometer() {
+	static SPIDriver *bus;
+	msg_t ret = MSG_RESET;
+
+#if HAL_USE_SPI
 	if (!isBrainPinValid(engineConfiguration->accelerometerCsPin))
 		return; // not used
 
-#if HAL_USE_SPI
-	driver = getSpiDevice(engineConfiguration->accelerometerSpiDevice);
-	if (driver == NULL) {
+	bus = getSpiDevice(engineConfiguration->accelerometerSpiDevice);
+	if (bus == NULL) {
 		// error already reported
 		return;
 	}
 
-	// todo: we have centralized SPI management see other usages of 'turnOnSpi'
-    //turnOnSpi(engineConfiguration->accelerometerSpiDevice);
-	spiStart(driver, &accelerometerCfg);
-	initSpiCs((SPIConfig *)driver->config, engineConfiguration->accelerometerCsPin);
+#if (EFI_ONBOARD_MEMS_LIS2DW12 == TRUE)
+	/* Commented until we have configureHellenMegaAccCS2Pin() */
+	//chipSelect.initPin("SPI Acc", engineConfiguration->accelerometerCsPin);
+	accelerometerSpiCfg.ssport = getHwPort("SPI Acc", engineConfiguration->accelerometerCsPin);
+	accelerometerSpiCfg.sspad = getHwPin("SPI Acc", engineConfiguration->accelerometerCsPin);
+	lis2dw12cfg.spip = bus;
 
-//	memsCs.initPin("LIS302 CS", engineConfiguration->accelerometerCsPin);
-//	memsCfg.ssport = getHwPort("mmc", engineConfiguration->sdCardCsPin);
-//	memsCfg.sspad = getHwPin("mmc", engineConfiguration->sdCardCsPin);
+	/* LIS302DL Object Initialization.*/
+	lis2dw12ObjectInit(&LIS2DW12);
 
+	/* Activates the LIS302DL driver.*/
+	ret = lis2dw12Start(&LIS2DW12, &lis2dw12cfg);
+#endif /* EFI_ONBOARD_MEMS_LIS2DW12 == TRUE */
 
-	/* LIS302DL initialization.*/
-	lis302dlWriteRegister(driver, LIS302DL_CTRL_REG1, 0x47); // enable device, enable XYZ
-	lis302dlWriteRegister(driver, LIS302DL_CTRL_REG2, 0x00); // 4 wire mode
-	lis302dlWriteRegister(driver, LIS302DL_CTRL_REG3, 0x00);
+	/* TODO: add support for LIS302 on discovery board */
 
-	chThdCreateStatic(ivThreadStack, sizeof(ivThreadStack), NORMALPRIO, (tfunc_t)(void*) ivThread, NULL);
+	if (ret == MSG_OK) {
+		/* 50 Hz */
+		instance.setPeriod(20 /*ms*/);
+		instance.start();
+	}
 #endif /* HAL_USE_SPI */
 }
 
