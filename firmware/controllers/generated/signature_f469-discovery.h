3f2f7836dcfd3c0be6d799821ed5b906b88bf960
GitHub gen-configs Action <action@github.com>
2023-12-14 17:25:45 +0000

    Auto-generated configs and docs

---
+++ firmware/controllers/generated/signature_f469-discovery.h
@@ -0,0 +1,6 @@
+//
+// was generated automatically by rusEFI tool config_definition.jar based on gen_config.sh by SignatureConsumer
+//
+
+#define SIGNATURE_HASH 3258999710
+#define TS_SIGNATURE "rusEFI master.2023.12.14.f469-discovery.3258999710"
