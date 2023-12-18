c6dcf440c46ccdd038d60c79ef1977a5914893c0
rusefillc <sdfsdfqsf2334234234>
2023-12-18 00:19:12 -0500

    only:docs

--- misc/install/run_openocd_f7.bat
+++ misc/install/run_openocd_f7.bat
@@ -1,1 +1,4 @@
+# it's fragile :( sometimes you have to turn things on/off but cheap stlink v2 does connect to F7 using this script
+# hint: use some other tool to transfer binary, use GDB only for actual debug
+
 openocd\openocd.exe -f openocd/st_nucleo_f7.cfg -c init -c reset
