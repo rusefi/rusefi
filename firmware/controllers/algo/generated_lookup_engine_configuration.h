eefd3751d88ca5f038041db16fb5d60208c37a70
Andrey <rusefillc@gmail.com>
2023-12-19 15:36:28 -0500

    custom FW repos code generation process #5817
    preparation

--- firmware/controllers/algo/generated_lookup_engine_configuration.h
+++ firmware/controllers/algo/generated_lookup_engine_configuration.h
@@ -1,3 +1,16 @@
+#include "efi_quote.h"
+
 #pragma once
 
-#include "engine_configuration_generated_structures.h"
+#pragma message ("SHORT_BOARD_NAME: " QUOTE(SHORT_BOARD_NAME))
+
+#define META_ENGINE_GENERATED_NAME engine_configuration_generated_structures_
+#define META_ENGINE_GENERATED_EXT .h
+
+#if !EFI_UNIT_TEST
+#define META_ENGINE_GENERATED_H_FILENAME QUOTE(META_ENGINE_GENERATED_NAME SHORT_BOARD_NAME META_ENGINE_GENERATED_EXT)
+#include META_ENGINE_GENERATED_H_FILENAME
+
+#pragma message ("META_ENGINE_GENERATED_H_FILENAME: " META_ENGINE_GENERATED_H_FILENAME)
+
+#endif
