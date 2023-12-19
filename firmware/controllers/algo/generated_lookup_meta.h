38e37a72b5ebc5e9d89c64d57e7ab74699b27725
Andrey <rusefillc@gmail.com>
2023-12-19 16:25:45 -0500

    custom FW repos code generation process #5817
    preparation

--- firmware/controllers/algo/generated_lookup_meta.h
+++ firmware/controllers/algo/generated_lookup_meta.h
@@ -2,15 +2,16 @@
 
 #pragma once
 
-#pragma message ("SHORT_BOARD_NAME: " QUOTE(SHORT_BOARD_NAME))
-
 #define META_GENERATED_NAME rusefi_generated_
 #define META_GENERATED_EXT .h
 
-#if !EFI_UNIT_TEST
+#if EFI_PROD_CODE
+#pragma message ("SHORT_BOARD_NAME: " QUOTE(SHORT_BOARD_NAME))
 #define META_GENERATED_H_FILENAME QUOTE(META_GENERATED_NAME SHORT_BOARD_NAME META_GENERATED_EXT)
 #include META_GENERATED_H_FILENAME
 
 #pragma message ("META_GENERATED_H_FILENAME: " META_GENERATED_H_FILENAME)
+#else
+#include "rusefi_generated_f407-discovery.h"
 
 #endif
