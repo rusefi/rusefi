3557f5240891a31e81f15e2ac6dad16ce2712fae
rusefi <rusefillc@gmail.com>
2023-12-19 22:46:34 -0500

    only:clean-up

--- firmware/controllers/generated/signature_nucleo_h743.h
+++
@@ -1,6 +0,0 @@
-//
-// was generated automatically by rusEFI tool config_definition.jar based on gen_config.sh by SignatureConsumer
-//
-
-#define SIGNATURE_HASH 2195950183
-#define TS_SIGNATURE "rusEFI master.2023.12.20.nucleo_h743.2195950183"
