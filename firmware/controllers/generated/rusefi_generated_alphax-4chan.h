52efd6cbec3aabee59c7a077d5f3d58643c797f6
GitHub gen-configs Action <action@github.com>
2023-12-20 15:49:24 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-4chan.h
+++ firmware/controllers/generated/rusefi_generated_alphax-4chan.h
@@ -1146,7 +1146,7 @@
 #define show_Frankenso_presets true
 #define show_Hellen_presets true
 #define show_test_presets true
-#define SIGNATURE_HASH 530329637
+#define SIGNATURE_HASH 2445496472
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1445,7 +1445,7 @@
 #define ts_show_vr_threshold_2 true
 #define ts_show_vr_threshold_all true
 #define ts_show_vr_threshold_pins false
-#define TS_SIGNATURE "rusEFI master.2023.12.20.alphax-4chan.530329637"
+#define TS_SIGNATURE "rusEFI master.2023.12.20.alphax-4chan.2445496472"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- firmware/controllers/generated/signature_alphax-4chan_f7.h
+++ firmware/controllers/generated/signature_alphax-4chan_f7.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 530329637
-#define TS_SIGNATURE "rusEFI master.2023.12.20.alphax-4chan_f7.530329637"
+#define SIGNATURE_HASH 2445496472
+#define TS_SIGNATURE "rusEFI master.2023.12.20.alphax-4chan_f7.2445496472"
