0b18a0e628484ff45c72ada983b52841f0bee715
GitHub gen-configs Action <action@github.com>
2023-12-20 03:09:14 +0000

    Auto-generated configs and docs

---
+++ firmware/controllers/generated/signature_stm32f429_nucleo.h
@@ -0,0 +1,6 @@
+//
+// was generated automatically by rusEFI tool config_definition.jar based on gen_config.sh by SignatureConsumer
+//
+
+#define SIGNATURE_HASH 2195950183
+#define TS_SIGNATURE "rusEFI master.2023.12.20.stm32f429_nucleo.2195950183"
