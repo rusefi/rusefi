278fbfd428ccbfdf43ab9603eda2ab384c48c686
rusefi <rusefillc@gmail.com>
2023-12-21 18:54:56 -0500

    only:uaefi

--- firmware/config/boards/hellen/uaefi/board_configuration.cpp
+++ firmware/config/boards/hellen/uaefi/board_configuration.cpp
@@ -130,19 +130,24 @@ static Gpio OUTPUTS[] = {
 	Gpio::MM100_INJ7, // B7 Low Side output 1
 	Gpio::MM100_IGN8, // B8 Fan Relay Weak Low Side output 2
 	Gpio::MM100_IGN7, // B9 Main Relay Weak Low Side output 1
+	Gpio::MM100_OUT_PWM2, // B16 Low Side output 4 / Fuel Pump
+	Gpio::MM100_OUT_PWM1, // B17 Low Side output 3
+	Gpio::MM100_INJ8, // B18 Low Side output 2
+	// high sides
 	Gpio::MM100_IGN6, // B10 Coil 6
 	Gpio::MM100_IGN4, // B11 Coil 4
 	Gpio::MM100_IGN3, // B12 Coil 3
 	Gpio::MM100_IGN5, // B13 Coil 5
 	Gpio::MM100_IGN2, // B14 Coil 2
 	Gpio::MM100_IGN1, // B15 Coil 1
-	Gpio::MM100_OUT_PWM2, // B16 Low Side output 4 / Fuel Pump
-	Gpio::MM100_OUT_PWM1, // B17 Low Side output 3
-	Gpio::MM100_INJ8, // B18 Low Side output 2
 };
 
 int getBoardMetaOutputsCount() {
     return efi::size(OUTPUTS);
+}
+
+int getBoardMetaLowSideOutputsCount() {
+  return getBoardMetaOutputsCount() - 6;
 }
 
 Gpio* getBoardMetaOutputs() {
