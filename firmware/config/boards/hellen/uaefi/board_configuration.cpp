2499bcc48fce8bb7bc155aad1d00dfca524f493e
rusefillc <sdfsdfqsf2334234234>
2023-12-23 01:17:00 -0500

    https://github.com/andreika-git/hellen-one/issues/353 only:uaefi

--- firmware/config/boards/hellen/uaefi/board_configuration.cpp
+++ firmware/config/boards/hellen/uaefi/board_configuration.cpp
@@ -50,7 +50,6 @@ static void setupDefaultSensorInputs() {
 #include "hellen_leds_100.cpp"
 
 void setBoardConfigOverrides() {
-//  chThdSleepMilliseconds(5); // experiment for https://github.com/andreika-git/hellen-one/issues/353
 	setHellenMegaEnPin();
 	setHellenVbatt();
 
