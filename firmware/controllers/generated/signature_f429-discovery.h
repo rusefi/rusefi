ccac5980749a4724c8135113241e58e18abfd1bc
GitHub gen-configs Action <action@github.com>
2023-12-24 19:45:45 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_f429-discovery.h
+++ firmware/controllers/generated/signature_f429-discovery.h
@@ -1,2 +1,2 @@
 #define SIGNATURE_HASH 909073215
-#define TS_SIGNATURE "rusEFI (FOME) master.2023.12.23.f429-discovery.909073215"
+#define TS_SIGNATURE "rusEFI (FOME) master.2023.12.24.f429-discovery.909073215"
--- firmware/controllers/generated/signature_hellen121vag.h
+++ firmware/controllers/generated/signature_hellen121vag.h
@@ -1,2 +1,2 @@
 #define SIGNATURE_HASH 500230426
-#define TS_SIGNATURE "rusEFI (FOME) master.2023.12.23.hellen121vag.500230426"
+#define TS_SIGNATURE "rusEFI (FOME) master.2023.12.24.hellen121vag.500230426"
