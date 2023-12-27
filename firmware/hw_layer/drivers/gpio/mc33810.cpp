6f6b21472cda6c9bbf4231cca3e3d90bccfcd8fe
rusefi <rusefillc@gmail.com>
2023-12-26 19:54:27 -0500

    only:mc33810maxDwellTimer

--- firmware/hw_layer/drivers/gpio/mc33810.cpp
+++ firmware/hw_layer/drivers/gpio/mc33810.cpp
@@ -368,7 +368,11 @@ int Mc33810::chip_init()
 	{
 		uint16_t spark_settings =
 			//(3 << 9) |	/* max dwell is 16 mS */
-			(2 << 9) |	/* max dwell is 8 mS */
+			//(2 << 9) |	/* max dwell is 8 mS */
+			// Table 11. Maximum Dwell Timer
+			// 4: 32ms
+			// 5,6 and 7 all represent 64ms
+			(engineConfiguration->mc33810maxDwellTimer << 9) |
 			BIT(8) |	/* enable max dwell control */
 			(3 << 2) |	/* Open Secondary OSFLT = 100 uS, default */
 			(1 << 0) |	/* End Spark THreshold: VPWR +5.5V, default */
