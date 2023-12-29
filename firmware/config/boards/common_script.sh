ceae967bfa73f85cfcba996f7088e7aae2dee312
andreika-git <andrei.home@gmail.com>
2023-12-29 18:20:34 +0200

    Specify a different makefile for BL in common_script.sh

--- firmware/config/boards/common_script.sh
+++ firmware/config/boards/common_script.sh
@@ -14,8 +14,10 @@ mkdir -p .dep
 make -j$(nproc) -r PROJECT_BOARD=$PROJECT_BOARD PROJECT_CPU=$PROJECT_CPU
 [ -e build/rusefi.hex ] || { echo "FAILED to compile by $SCRIPT_NAME with $PROJECT_BOARD $DEBUG_LEVEL_OPT and $EXTRA_PARAMS"; exit 1; }
 if [ "$USE_OPENBLT" = "yes" ]; then
-  make openblt PROJECT_BOARD=$PROJECT_BOARD PROJECT_CPU=$PROJECT_CPU
-  [ -e build-openblt/openblt_$PROJECT_BOARD.hex ] || { echo "FAILED to compile OpenBLT by $SCRIPT_NAME with $PROJECT_BOARD"; exit 1; }
+  # TODO: why is this rm necessary?
+  rm -f pch/pch.h.gch/*
+  cd bootloader; make PROJECT_BOARD=$PROJECT_BOARD PROJECT_CPU=$PROJECT_CPU -j12; cd ..
+  [ -e bootloader/blbuild/openblt_$PROJECT_BOARD.hex ] || { echo "FAILED to compile OpenBLT by $SCRIPT_NAME with $PROJECT_BOARD"; exit 1; }
 fi
 
 if uname | grep "NT"; then
