741a135f9ca563fde453e94b9ea1c2f32ea300de
andreika-git <andrei.home@gmail.com>
2023-12-29 21:03:41 +0200

    port makefiles

--- firmware/hw_layer/ports/stm32/stm32h7/openblt/port.mk
+++ firmware/hw_layer/ports/stm32/stm32h7/openblt/port.mk
@@ -5,8 +5,10 @@
 # exclude flash-layout.c as this one is directly included in a source file, when used.
 #PROJ_FILES += $(filter-out $(OPENBLT_PORT_DIR)/flash_layout.c, $(call rwildcard, $(OPENBLT_PORT_DIR), *.c *.h *.s))
 # reuse ST32F7xx HAL and CMSIS from one of OpenBLT examples to avoid having copy in rusEFI git
-OPENBLT_PORT_FILES += $(filter-out uip, $(call rwildcard, $(OPENBLT_TRGT_DIR)/Source/ARMCM4_STM32H7/, *.c *.h *.s))
-OPENBLT_PORT_FILES += $(filter-out uip, $(call rwildcard, $(OPENBLT_TRGT_DIR)/Source/ARMCM4_STM32H7/GCC/, *.c *.h *.s))
+OPENBLT_PORT_DIR = $(OPENBLT_TRGT_DIR)/Source/ARMCM7_STM32H7
+OPENBLT_PORT_FILES += $(filter-out uip, $(call wildcard, $(OPENBLT_PORT_DIR)/*.c $(OPENBLT_PORT_DIR)/*.h $(OPENBLT_PORT_DIR)/*.s))
+OPENBLT_PORT_DIR_GCC = $(OPENBLT_PORT_DIR)/GCC
+OPENBLT_PORT_FILES += $(filter-out uip, $(call wildcard, $(OPENBLT_PORT_DIR_GCC)/*.c $(OPENBLT_PORT_DIR_GCC)/*.h $(OPENBLT_PORT_DIR_GCC)/*.s))
 #|--------------------------------------------------------------------------------------|
 #| Specific options for toolchain binaries                                              |
 #|--------------------------------------------------------------------------------------|
