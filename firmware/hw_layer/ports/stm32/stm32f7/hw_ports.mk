35497a2d80f24ccf91f00e37a82719eee415c1f3
Andrey Gusakov <dron0gus@gmail.com>
2023-12-30 22:44:00 +0300

    port: stm32: f7: support custom board.c files

--- firmware/hw_layer/ports/stm32/stm32f7/hw_ports.mk
+++ firmware/hw_layer/ports/stm32/stm32f7/hw_ports.mk
@@ -9,10 +9,14 @@ HW_LAYER_PORT_CPP += $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f7/mpu_util.cpp \
 # This MCU has a cache, align functions to a cache line for maximum cache efficiency
 USE_OPT += -falign-functions=16
 
+ifeq ($(BOARD_C),)
+	BOARD_C = $(CHIBIOS)/os/hal/boards/ST_NUCLEO144_F767ZI/board.c
+endif
+ALLCSRC += $(BOARD_C)
+
 DDEFS += -DSTM32F767xx
 MCU = cortex-m7
 LDSCRIPT = $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f7/STM32F7.ld
-ALLCSRC += $(CHIBIOS)/os/hal/boards/ST_NUCLEO144_F767ZI/board.c
 CONFDIR = $(PROJECT_DIR)/hw_layer/ports/stm32/stm32f7/cfg
 
 # TODO: remove, for efifeatures.h
