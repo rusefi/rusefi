77a1be65710668d253a64ba92e8e8c470ed890c3
rusefillc <sdfsdfqsf2334234234>
2023-12-31 15:30:14 -0500

    traction control: wheel-slip based close ETB #5843

--- firmware/controllers/algo/defaults/default_base_engine.cpp
+++ firmware/controllers/algo/defaults/default_base_engine.cpp
@@ -75,7 +75,7 @@ void setDefaultBaseEngine() {
 
     engineConfiguration->watchOutForLinearTime = true;
 
-  setLinearCurve(engineConfiguration->tractionControlSlipBins, /*from*/0.8, /*to*/1.2, 0.1);
+  setLinearCurve(engineConfiguration->tractionControlSlipBins, /*from*/0.8, /*to*/1.2, 0.05);
 	setLinearCurve(engineConfiguration->tractionControlSpeedBins, /*from*/10, /*to*/120, 5);
 
 	engineConfiguration->turbochargerFilter = 0.01f;
