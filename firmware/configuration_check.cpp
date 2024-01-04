bbd3373e42d309475df081d6f26d8cc0c1cbf1c8
Andrey Gusakov <dron0gus@gmail.com>
2024-01-04 19:45:38 +0300

    Checks: emit annoing pragma messages only once per build

---
+++ firmware/configuration_check.cpp
@@ -0,0 +1,8 @@
+#include "generated_lookup_engine_configuration.h"
+#include "generated_lookup_meta.h"
+
+#if EFI_PROD_CODE
+#pragma message ("SHORT_BOARD_NAME: " QUOTE(SHORT_BOARD_NAME))
+#pragma message ("META_ENGINE_GENERATED_H_FILENAME: " META_ENGINE_GENERATED_H_FILENAME)
+#pragma message ("META_GENERATED_H_FILENAME: " META_GENERATED_H_FILENAME)
+#endif
