93a862022b2f2882b9ce7248b405448b41a12d04
rusefi <rusefillc@gmail.com>
2024-01-13 18:42:58 -0500

    PCanIoProxySandbox

---
+++ java_console/ui/src/test/java/com/rusefi/binaryprotocol/test/PCanIoProxySandbox.java
@@ -0,0 +1,13 @@
+package com.rusefi.binaryprotocol.test;
+
+import com.rusefi.io.can.PCanIoStream;
+import com.rusefi.tools.CANConnectorStartup;
+
+import java.io.IOException;
+
+public class PCanIoProxySandbox {
+    public static void main(String[] args) throws IOException {
+        PCanIoStream stream = PCanIoStream.createStream();
+        CANConnectorStartup.start(stream, status -> System.out.println("Status: " + status));
+    }
+}
