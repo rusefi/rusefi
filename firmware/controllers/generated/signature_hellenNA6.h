3fd15e2db67abbeadfad5d5ae50710c1fbba05fd
GitHub gen-configs Action <action@github.com>
2024-01-15 15:38:39 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellenNA6.h
+++ firmware/controllers/generated/signature_hellenNA6.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2178462902
-#define TS_SIGNATURE "rusEFI master.2024.01.15.hellenNA6.2178462902"
+#define SIGNATURE_HASH 1248331053
+#define TS_SIGNATURE "rusEFI master.2024.01.15.hellenNA6.1248331053"
