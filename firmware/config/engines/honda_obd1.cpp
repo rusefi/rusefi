5092ec7f0fb4170c3bdec48c0756d771f066592d
Andrey <rusefillc@gmail.com>
2024-01-16 22:30:02 -0500

    only: honda obd1 is back!

--- firmware/config/engines/honda_obd1.cpp
+++ firmware/config/engines/honda_obd1.cpp
@@ -3,6 +3,9 @@
 void setHondaObd1() {
 	strcpy(engineConfiguration->engineMake, ENGINE_MAKE_HONDA);
 
+	engineConfiguration->trigger.type = trigger_type_e::TT_12_TOOTH_CRANK; // 12 crank same as 24 cam
+	engineConfiguration->vvtMode[0] = VVT_SINGLE_TOOTH;
+
 	engineConfiguration->map.sensor.type = MT_DENSO183;
 	engineConfiguration->ignitionMode = IM_ONE_COIL;
 
