fe930d0859147ac5b0d16d1fc9a432bea695d0e4
Matthew Kennedy <matthewkennedy@outlook.com>
2024-01-17 15:48:42 -0800

    simulator gets short board name

--- simulator/compile.sh
+++ simulator/compile.sh
@@ -3,7 +3,7 @@
 set -e
 
 rm -f build/fome_simulator.exe
-make -j6 -r $@
+make -j8 -r $@
 [ $? -eq 0 ] || { echo "Simulator compilation failed"; exit 1; }
 file build/fome_simulator
 echo "TIMESTAMP $(date "+%D %T.%2N") just compiled rusEFI simulator"
