3d06f97ba5674391df68e49943ed13c0dacd2756
rusefillc <sdfsdfqsf2334234234>
2024-01-18 23:21:49 -0500

    only:nicer enum name

--- firmware/config/boards/hellen/hellenNA8_96/board.mk
+++ firmware/config/boards/hellen/hellenNA8_96/board.mk
@@ -4,7 +4,7 @@
 BOARDCPPSRC = $(BOARD_DIR)/board_configuration.cpp
 # Set this if you want a default engine type other than normal hellen-nb1
 ifeq ($(VAR_DEF_ENGINE_TYPE),)
-  VAR_DEF_ENGINE_TYPE = -DDEFAULT_ENGINE_TYPE=engine_type_e::HELLEN_NA8_96
+  VAR_DEF_ENGINE_TYPE = -DDEFAULT_ENGINE_TYPE=engine_type_e::MAZDA_NA8_96
 endif
 
 DDEFS += -DEFI_MAIN_RELAY_CONTROL=TRUE
