cd7815bbddd32dbd2a0e8c78688c6b42e64c5c62
rusefillc <sdfsdfqsf2334234234>
2024-01-20 11:56:43 -0500

    only:uaefi honda obd1

--- firmware/config/engines/honda_obd1.cpp
+++ firmware/config/engines/honda_obd1.cpp
@@ -19,8 +19,22 @@ void setHondaObd1() {
 	// Keihin 06164-P0A-A00
 	engineConfiguration->injector.flow = 248;
 
+	// Disable ETBs
+	engineConfiguration->etbFunctions[0] = dc_function_e::DC_None;
+	engineConfiguration->etbFunctions[1] = dc_function_e::DC_None;
+
+  // ECU does not control main relay
+  engineConfiguration->mainRelayPin = Gpio::Unassigned;
+
 #if HW_HELLEN_UAEFI
+  // Honda ICM is weird it fires spark on RAISING edge and has internal pull-up
+  // thus we use a low side output to drive OEM ignition
+  engineConfiguration->ignitionPins[0] = Gpio::Gpio::MM100_IGN7; // low side!
+
 	engineConfiguration->camInputs[0] = Gpio::MM100_UART8_RX; // "C18 VR1+ Discrete"
 	engineConfiguration->camInputs[1] = Gpio::Unassigned;
+
+	engineConfiguration->tps1_2AdcChannel = EFI_ADC_NONE;
+	setPPSInputs(EFI_ADC_NONE, EFI_ADC_NONE);
 #endif // HW_HELLEN_UAEFI
 }
