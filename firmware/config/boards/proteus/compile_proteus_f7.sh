578b8ac24c2aaeb7e10b1e2cfb553c42d99e989c
rusefillc <sdfsdfqsf2334234234>
2024-01-26 17:19:50 -0500

    f7 openblt

--- firmware/config/boards/proteus/compile_proteus_f7.sh
+++ firmware/config/boards/proteus/compile_proteus_f7.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# does not fit into 512K flash, and we use 512K because of st DFU client bug :(
-# export USE_OPENBLT=yes
+export USE_OPENBLT=yes
 export DEBUG_LEVEL_OPT="-Os -ggdb -g"
 
 bash ../common_make.sh proteus ARCH_STM32F7
