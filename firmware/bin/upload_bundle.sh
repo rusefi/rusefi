c0c8b69f963cf04c85697afe2da48ebd7e6f6bd7
David Holdeman <dave@holdemanenterprises.com>
2024-01-29 07:04:48 -0600

    move bundle upload to its own script

---
+++ firmware/bin/upload_bundle.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+SCRIPT_NAME=$(basename "$0")
+FULL_BUNDLE_FILE="rusefi_bundle_$1.zip"
+UPDATE_BUNDLE_FILE="rusefi_bundle_$1_autoupdate.zip"
+
+if [ -n "$RUSEFI_SSH_USER" ]; then
+ echo "$SCRIPT_NAME: Uploading full bundle"
+ RET=0
+ if [ "${LTS}" = "true" ]; then
+   tar -czf - $FULL_BUNDLE_FILE  | sshpass -p $RUSEFI_SSH_PASS ssh -o StrictHostKeyChecking=no $RUSEFI_SSH_USER@$RUSEFI_SSH_SERVER "mkdir -p build_server/lts/$1; tar -xzf - -C build_server/lts/$1"
+	 RET=$((RET+$?))
+   tar -czf - $UPDATE_BUNDLE_FILE  | sshpass -p $RUSEFI_SSH_PASS ssh -o StrictHostKeyChecking=no $RUSEFI_SSH_USER@$RUSEFI_SSH_SERVER "mkdir -p build_server/lts/$1/autoupdate; tar -xzf - -C build_server/lts/$1/autoupdate"
+	 RET=$((RET+$?))
+ else
+   tar -czf - $FULL_BUNDLE_FILE  | sshpass -p $RUSEFI_SSH_PASS ssh -o StrictHostKeyChecking=no $RUSEFI_SSH_USER@$RUSEFI_SSH_SERVER "tar -xzf - -C build_server"
+	 RET=$((RET+$?))
+   tar -czf - $UPDATE_BUNDLE_FILE  | sshpass -p $RUSEFI_SSH_PASS ssh -o StrictHostKeyChecking=no $RUSEFI_SSH_USER@$RUSEFI_SSH_SERVER "mkdir -p build_server/autoupdate; tar -xzf - -C build_server/autoupdate"
+	 RET=$((RET+$?))
+ fi
+ if [ $RET -ne 0 ]; then
+  echo "$SCRIPT_NAME: Bundle upload failed"
+  exit 1
+ fi
+else
+ echo "$SCRIPT_NAME: Upload not configured"
+fi
