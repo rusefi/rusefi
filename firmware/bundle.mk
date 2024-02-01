3b3958d34792649b6e893b1ad982d02aebb51a9b
David Holdeman <dave@holdemanenterprises.com>
2024-02-01 07:33:16 -0600

    use make for creating dfu

---
+++ firmware/bundle.mk
@@ -0,0 +1,42 @@
+ifneq (,$(findstring NT,$(UNAME_S)))
+	H2D = ../misc/encedo_hex2dfu/hex2dfu.exe
+else
+	H2D = ../misc/encedo_hex2dfu/hex2dfu.bin
+endif
+
+DFUS = deliver/$(PROJECT).dfu
+
+ifeq ($(USE_OPENBLT),yes)
+  BOOTLOADER_HEX = bootloader/blbuild/openblt_$(PROJECT_BOARD).hex
+  BOUTS = deliver/openblt.bin
+else
+  OUTS = deliver/$(PROJECT).bin deliver/$(PROJECT).hex
+ifeq ($(INCLUDE_ELF),yes)
+  OUTS += deliver/$(PROJECT).elf deliver/$(PROJECT).map deliver/$(PROJECT).list
+endif
+endif
+
+deliver:
+	mkdir -p deliver
+
+$(BUILDDIR)/$(PROJECT).map: $(BUILDDIR)/$(PROJECT).elf
+
+$(OUTS): deliver/%: $(BUILDDIR)/%
+	cp $< $@
+
+$(BOUTS): deliver/openblt%: bootloader/blbuild/openblt_$(PROJECT_BOARD)%
+	cp $< $@
+
+$(DFUS): deliver/%.dfu: $(BUILDDIR)/%.hex $(BOOTLOADER_HEX) deliver
+ifeq ($(USE_OPENBLT),yes)
+	$(H2D) -i $(BOOTLOADER_HEX) -i $< -C 0x1C -o $@ -b deliver/$(PROJECT).bin
+else
+	$(H2D) -i $< -C 0x1C -o $@
+endif
+
+BUNDLEFILES = \
+  $(DFUS) \
+  $(OUTS) \
+  $(BOUTS)
+
+bundle: $(BUNDLEFILES) all
