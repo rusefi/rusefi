46b90d752cc5a4308fe9e3e11816d1e252fdb77b
rusefillc <sdfsdfqsf2334234234>
2024-02-01 19:37:46 -0500

    more F7 OpenBLT usages

--- firmware/config/boards/hellen/alphax-4chan/compile_alphax-4chan_f7.sh
+++ firmware/config/boards/hellen/alphax-4chan/compile_alphax-4chan_f7.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+export USE_OPENBLT=yes
+
 cd ..
-bash ../common_make.sh hellen/alphax-4chan ARCH_STM32F7
+bash ../common_make.sh alphax-4chan ARCH_STM32F7 config/boards/hellen/alphax-4chan
--- firmware/config/boards/hellen/alphax-8chan/compile_alphax-8chan_f7.sh
+++ firmware/config/boards/hellen/alphax-8chan/compile_alphax-8chan_f7.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+export USE_OPENBLT=yes
+
 cd ..
-bash ../common_make.sh hellen/alphax-8chan ARCH_STM32F7
+bash ../common_make.sh alphax-8chan ARCH_STM32F7 config/boards/hellen/alphax-8chan
