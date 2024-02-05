6999f5c4c57c9a40d71fdf27cb2c168ea8acdca4
David Holdeman <dave@holdemanenterprises.com>
2024-02-05 17:30:07 -0600

    Call make directly from workflow

--- .github/workflows/hw-ci/build_for_hw_ci.sh
+++ .github/workflows/hw-ci/build_for_hw_ci.sh
@@ -14,6 +14,7 @@ echo "HW CI build [$HW_FOLDER][$HW_TARGET]"
 cd firmware
 
 export BOARD_META_PATH=$(bash bin/find_meta_info.sh ${HW_FOLDER} ${HW_TARGET})
+source config/boards/common_script_read_meta_env.inc "${BOARD_META_PATH}"
 
 ./gen_live_documentation.sh
 ./gen_config_board.sh $HW_FOLDER $HW_TARGET
@@ -24,4 +25,4 @@ make clean
 export EXTRA_2_PARAMS=-DHARDWARE_CI
 
 echo Build Firmware
-bash config/boards/common_script.sh "${BOARD_META_PATH}"
+make -j$(nproc) -r
