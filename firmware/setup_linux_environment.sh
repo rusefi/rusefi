dbaf4e29c793bb2ae26c0b7fbccbc2e4e63b8057
Matthew Kennedy <matthewkennedy@outlook.com>
2024-02-13 14:45:54 -0800

    obselete tools to install compiler and such

--- firmware/setup_linux_environment.sh
+++ firmware/setup_linux_environment.sh
@@ -20,17 +20,5 @@ sudo apt-get update
 # install dependencies
 sudo apt-get install -y build-essential gcc gdb gcc-multilib g++-multilib make openjdk-8-jdk-headless mtools zip xxd libncurses5 libncursesw5
 
-# delete any old tools, create a new folder, and go there
-rm -rf ~/.rusefi-tools
-mkdir ~/.rusefi-tools
-dir=$(pwd)
-cd ~/.rusefi-tools
-
-# provide GCC arm-none-eabi toolchain
-${dir}/provide_gcc.sh
-
-# Add the compiler to your path
-echo 'export PATH=$PATH:$HOME/.rusefi-tools/gcc-arm-none-eabi/bin' >> ~/.profile
-
 # Allow the current user to use serial ports
 sudo usermod -a -G dialout $USER
