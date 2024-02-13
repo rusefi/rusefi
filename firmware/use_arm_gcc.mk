1a0ef4fbff9deec10e473784e539ec9bf0bffb1f
Matthew Kennedy <matthewkennedy@outlook.com>
2024-02-13 14:44:15 -0800

    put compilers in a submodule (#360)
    * add compiler submodule
    
    * use compiler from git if not found on path
    
    * add todo
    
    * select darwin vs. linux properly
    
    * update submodule for darwin compiler
    
    * match formatting
    
    * hide stray ramdisk images
    
    * lfs: true
    
    * lfs in the right spot?
    
    * console doesn't need lfs
    
    * all builds that touch self hosted server can use lfs
    
    * don't ever bother trying to use the installed compiler
    
    * simplify and print a little more info
    
    * remove old submodule
    
    * add build tools submodule
    
    * fix path
    
    * we don't need lfs
    
    * no need to test compiler
    
    * set a branch
    
    * allow passing compiler, auto-submodule if missing
    
    * missing endif

---
+++ firmware/use_arm_gcc.mk
@@ -0,0 +1,26 @@
+ifeq ($(TRGT),)
+	UNAME_S := $(shell uname -s)
+	ifeq ($(UNAME_S),Darwin)
+		COMPILER_PLATFORM = arm-gnu-toolchain-11.3.rel1-darwin-x86_64-arm-none-eabi
+	else
+		COMPILER_PLATFORM = arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi
+	endif
+
+	BUILD_TOOLS_DIR = $(PROJECT_DIR)/ext/build-tools/
+	TRGT = $(BUILD_TOOLS_DIR)$(COMPILER_PLATFORM)/bin/arm-none-eabi-
+
+# If the compiler doesn't exist, try to update the build tools submodule
+ifeq ("$(wildcard $(TRGT)g++)","")
+$(info Compiler at $(TRGT)g++ not found, trying to update build-tools submodule...")
+$(shell git submodule update --init --depth=1 $(BUILD_TOOLS_DIR))
+$(error Please re-run make to execute build!)
+endif
+else
+# If the compiler doesn't exist, fault now
+ifeq ("$(wildcard $(TRGT)g++)","")
+$(error Compiler not found at $(TRGT)g++!)
+endif
+endif
+
+$(info COMPILER_PLATFORM: $(COMPILER_PLATFORM))
+$(info TRGT:              $(TRGT))
