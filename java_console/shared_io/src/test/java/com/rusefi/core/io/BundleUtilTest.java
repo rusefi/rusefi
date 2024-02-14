f9726315f94e8e872295bcbc80bb10cf6ac655c8
rusefillc <sdfsdfqsf2334234234>
2024-02-14 10:59:53 -0500

    only: reducing junit4

--- java_console/shared_io/src/test/java/com/rusefi/core/io/BundleUtilTest.java
+++ java_console/shared_io/src/test/java/com/rusefi/core/io/BundleUtilTest.java
@@ -1,12 +1,11 @@
 package com.rusefi.core.io;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class BundleUtilTest {
     @Test
     public void testExtractBundleTarget() {
-        assertEquals("proteus_f7", BundleUtil.getBundleTarget("rusefi.snapshot.proteus_f7"));
+        Assertions.assertEquals("proteus_f7", BundleUtil.getBundleTarget("rusefi.snapshot.proteus_f7"));
     }
 }
