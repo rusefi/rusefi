294043720ea5c243ae33a81b852a9115e5d6a182
rusefillc <sdfsdfqsf2334234234>
2024-02-14 10:59:37 -0500

    only: reducing junit4

--- java_tools/ts_plugin/src/test/java/com/rusefi/SignatureHelperTest.java
+++ java_tools/ts_plugin/src/test/java/com/rusefi/SignatureHelperTest.java
@@ -1,14 +1,13 @@
 package com.rusefi;
 
 import com.rusefi.core.SignatureHelper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class SignatureHelperTest {
     @Test
     public void test() {
         String url = SignatureHelper.getUrl("rusEFI master.2020.07.06.frankenso_na6.2468827536").first;
-        assertEquals("https://rusefi.com/online/ini/rusefi/master/2020/07/06/frankenso_na6/2468827536.ini", url);
+        Assertions.assertEquals("https://rusefi.com/online/ini/rusefi/master/2020/07/06/frankenso_na6/2468827536.ini", url);
     }
 }
