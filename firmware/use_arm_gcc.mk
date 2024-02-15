c5f6855c355c95160f0e7b6e07004e3ac76a93bc
Matthew Kennedy <matthewkennedy@outlook.com>
2024-02-14 23:47:02 -0800

    s

--- firmware/use_arm_gcc.mk
+++ firmware/use_arm_gcc.mk
@@ -5,7 +5,7 @@ $(info UNAME_SP:          $(UNAME_SP))
 
 	ifeq ($(UNAME_S),Darwin)
 		COMPILER_PLATFORM = arm-gnu-toolchain-11.3.rel1-darwin-x86_64-arm-none-eabi
-	else ifeq ($(UNAME_SP),"Linux x86_64")
+	else ifeq ($(UNAME_SP),Linux x86_64)
 		COMPILER_PLATFORM = arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi
 	else
 $(error Looks like your platform, $(shell uname -sp), doesn't have a supported compiler!)
