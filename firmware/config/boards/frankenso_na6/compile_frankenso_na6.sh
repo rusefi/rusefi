4fed03d7092048e32551e9409970f2d4f983eb3a
David Holdeman <dave@holdemanenterprises.com>
2024-02-17 12:40:55 -0600

    move frankenso declarations

--- firmware/config/boards/frankenso_na6/compile_frankenso_na6.sh
+++ firmware/config/boards/frankenso_na6/compile_frankenso_na6.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-DEFAULT_ENGINE_TYPE=-DDEFAULT_ENGINE_TYPE=FRANKENSO_MIATA_NA6_VAF
-FW_ID_ENV=frankensoNA6
-
-
 #
 # sad technical debt: HW CI uses default bundle and it needs logic analyzer
 # while humans want to use second cam input asap https://rusefi.com/forum/viewtopic.php?f=4&t=2343
