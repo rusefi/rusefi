9bd8ed1e084a537cb0e012e0c5afda675ab2ac32
Andrey <rusefillc@gmail.com>
2024-02-25 16:09:50 -0500

    we do not use dynamic threads

--- firmware/hw_layer/ports/stm32/stm32h7/cfg/chconf.h
+++ firmware/hw_layer/ports/stm32/stm32h7/cfg/chconf.h
@@ -305,7 +305,7 @@
  * @note    Requires @p CH_CFG_USE_HEAP and/or @p CH_CFG_USE_MEMPOOLS.
  */
 #if !defined(CH_CFG_USE_DYNAMIC)
-#define CH_CFG_USE_DYNAMIC                  TRUE
+#define CH_CFG_USE_DYNAMIC                  FALSE
 #endif
 
 /** @} */
