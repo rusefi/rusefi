586d94bf21575687082aab0f58e22b87c952f5e8
David Holdeman <dave@holdemanenterprises.com>
2024-02-28 15:02:07 -0600

    Move ramdisk creation to its own script and make target

---
+++ firmware/bin/gen_image_board.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+BOARD_DIR=${1:-$BOARD_DIR}
+SHORT_BOARD_NAME=${2:-$SHORT_BOARD_NAME}
+INI=${3:-"rusefi_$SHORT_BOARD_NAME.ini"}
+
+which realpath >/dev/null 2>&1 || (which grealpath >/dev/null 2>&1 && alias realpath='grealpath')
+FDIR=$(realpath $(dirname "$0")/..)
+BOARD_DIR=$(realpath --relative-to "$FDIR" "$BOARD_DIR")
+
+cd "$FDIR"
+
+PREPEND_FILE=${BOARD_DIR}/prepend.txt
+
+BOARD_SPECIFIC_URL=$(cat $PREPEND_FILE | grep MAIN_HELP_URL | cut -d " " -f 3 | sed -e 's/^"//' -e 's/"$//')
+
+echo "BOARD_SPECIFIC_URL=[$BOARD_SPECIFIC_URL] for [$SHORT_BOARD_NAME] from [$BOARD_DIR]"
+if [ "" = "$BOARD_SPECIFIC_URL" ]; then
+  BOARD_SPECIFIC_URL=https://rusefi.com/s/wiki
+fi
+echo "BOARD_SPECIFIC_URL=[$BOARD_SPECIFIC_URL]"
+
+# we generate both versions of the header but only one would be actually included due to conditional compilation see EFI_USE_COMPRESSED_INI_MSD
+# todo: make things consistent by
+# 0) having generated content not in the same folder with the tool generating content?
+# 1) using unique file name for each configuration?
+# 2) leverage consistent caching mechanism so that image is generated only in case of fresh .ini. Laziest approach would be to return exit code from java process above
+#
+hw_layer/mass_storage/create_ini_image.sh            ${META_OUTPUT_ROOT_FOLDER}tunerstudio/generated/${INI} ./hw_layer/mass_storage/ramdisk_image.h             128 ${SHORT_BOARD_NAME} ${BOARD_SPECIFIC_URL}
+hw_layer/mass_storage/create_ini_image_compressed.sh ${META_OUTPUT_ROOT_FOLDER}tunerstudio/generated/${INI} ./hw_layer/mass_storage/ramdisk_image_compressed.h 1088 ${SHORT_BOARD_NAME} ${BOARD_SPECIFIC_URL}
--- firmware/rusefi_config.mk
+++ firmware/rusefi_config.mk
@@ -3,12 +3,14 @@ include $(PROJECT_DIR)/../java_tools/java_tools.mk
 INI_FILE = $(META_OUTPUT_ROOT_FOLDER)tunerstudio/generated/rusefi_$(SHORT_BOARD_NAME).ini
 SIG_FILE = $(PROJECT_DIR)/tunerstudio/generated/signature_$(SHORT_BOARD_NAME).txt
 
+RAMDISK = \
+  $(PROJECT_DIR)/hw_layer/mass_storage/ramdisk_image.h \
+  $(PROJECT_DIR)/hw_layer/mass_storage/ramdisk_image_compressed.h
+
 CONFIG_FILES = \
   $(PROJECT_DIR)/$(INI_FILE) \
   $(PROJECT_DIR)/controllers/generated/rusefi_generated_$(SHORT_BOARD_NAME).h \
   $(PROJECT_DIR)/controllers/generated/signature_$(SHORT_BOARD_NAME).h \
-  $(PROJECT_DIR)/hw_layer/mass_storage/ramdisk_image.h \
-  $(PROJECT_DIR)/hw_layer/mass_storage/ramdisk_image_compressed.h \
   $(PROJECT_DIR)/$(BOARD_DIR)/connectors/generated_outputs.h \
   $(PROJECT_DIR)/$(BOARD_DIR)/connectors/generated_ts_name_by_pin.cpp \
   $(FIELDS)
@@ -16,9 +18,15 @@ CONFIG_FILES = \
 .FORCE:
 
 $(TCOBJS): $(CONFIG_FILES)
+mass_storage_init.o: $(RAMDISK)
 
 $(SIG_FILE): .FORCE
 	bash $(PROJECT_DIR)/gen_signature.sh $(SHORT_BOARD_NAME)
+
+$(RAMDISK): .ramdisk-sentinel ;
+
+.ramdisk-sentinel: $(INI_FILE)
+	bash $(PROJECT_DIR)/bin/gen_image_board.sh $(BOARD_DIR) $(SHORT_BOARD_NAME)
 
 $(CONFIG_FILES): .config-sentinel ;
 
