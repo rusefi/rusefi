852b9cf2cc6271b838fe4e2dfd1223c2415be732
David Holdeman <dave@holdemanenterprises.com>
2024-02-28 15:02:07 -0600

    Move ramdisk creation to its own script and make target

--- firmware/gen_config_board.sh
+++ firmware/gen_config_board.sh
@@ -37,18 +37,6 @@ BOARD_DIR=$(realpath --relative-to "$FDIR" "$BOARD_DIR")
 
 cd "$FDIR"
 
-bash gen_signature.sh ${SHORT_BOARD_NAME}
-
-PREPEND_FILE=${BOARD_DIR}/prepend.txt
-
-BOARD_SPECIFIC_URL=$(cat $PREPEND_FILE | grep MAIN_HELP_URL | cut -d " " -f 3 | sed -e 's/^"//' -e 's/"$//')
-
-echo "BOARD_SPECIFIC_URL=[$BOARD_SPECIFIC_URL] for [$SHORT_BOARD_NAME] from [$BOARD_DIR]"
-if [ "" = "$BOARD_SPECIFIC_URL" ]; then
-  BOARD_SPECIFIC_URL=https://rusefi.com/s/wiki
-fi
-echo "BOARD_SPECIFIC_URL=[$BOARD_SPECIFIC_URL]"
-
 source gen_config_common.sh
 echo "Using COMMON_GEN_CONFIG [$COMMON_GEN_CONFIG]"
 
@@ -67,15 +55,6 @@ java \
 if [ -z "META_OUTPUT_ROOT_FOLDER" ]; then
 	META_OUTPUT_ROOT_FOLDER=""
 fi
-
-# we generate both versions of the header but only one would be actually included due to conditional compilation see EFI_USE_COMPRESSED_INI_MSD
-# todo: make things consistent by
-# 0) having generated content not in the same folder with the tool generating content?
-# 1) using unique file name for each configuration?
-# 2) leverage consistent caching mechanism so that image is generated only in case of fresh .ini. Laziest approach would be to return exit code from java process above
-#
-hw_layer/mass_storage/create_ini_image.sh            ${META_OUTPUT_ROOT_FOLDER}tunerstudio/generated/${INI} ./hw_layer/mass_storage/ramdisk_image.h             128 ${SHORT_BOARD_NAME} ${BOARD_SPECIFIC_URL}
-hw_layer/mass_storage/create_ini_image_compressed.sh ${META_OUTPUT_ROOT_FOLDER}tunerstudio/generated/${INI} ./hw_layer/mass_storage/ramdisk_image_compressed.h 1088 ${SHORT_BOARD_NAME} ${BOARD_SPECIFIC_URL}
 
 echo "Happy ${SHORT_BOARD_NAME}!"
 exit 0
--- firmware/gen_config_default.sh
+++ firmware/gen_config_default.sh
@@ -5,5 +5,7 @@ cd ../java_tools
 ./gradlew :config_definition:shadowJar
 cd ../firmware
 
+bash gen_signature.sh f407-discovery
 bash gen_config_board.sh config/boards/f407-discovery f407-discovery rusefi.ini
+bash bin/gen_image_board.sh config/boards/f407-discovery f407-discovery rusefi.ini
 exit $?
