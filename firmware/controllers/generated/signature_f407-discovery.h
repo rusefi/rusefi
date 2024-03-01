068d51cdf0ec0568bf152e362e18ff2599f7147f
GitHub gen-default-tune Action <action@github.com>
2024-03-01 15:29:39 +0000

    Auto-generated default tune

--- firmware/controllers/generated/signature_f407-discovery.h
+++ firmware/controllers/generated/signature_f407-discovery.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1985805685
-#define TS_SIGNATURE "rusEFI master.2024.03.01.f407-discovery.1985805685"
+#define SIGNATURE_HASH 475708051
+#define TS_SIGNATURE "rusEFI master.2024.03.01.f407-discovery.475708051"
