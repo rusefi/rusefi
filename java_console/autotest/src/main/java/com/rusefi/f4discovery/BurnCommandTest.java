b611f575711c3f701f8db3afa5bfc89cf83a255b
rusefillc <sdfsdfqsf2334234234>
2024-03-01 11:07:36 -0500

    UAEFI crash if set to daihatsu 4cyl trigger #6079
    new HW CI test

---
+++ java_console/autotest/src/main/java/com/rusefi/f4discovery/BurnCommandTest.java
@@ -0,0 +1,34 @@
+package com.rusefi.f4discovery;
+
+import com.rusefi.RusefiTestBase;
+import com.rusefi.io.LinkManager;
+import com.rusefi.io.commands.BurnCommand;
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.Assert.assertTrue;
+
+public class BurnCommandTest extends RusefiTestBase {
+    @Test
+    public void executeBurnCommand() throws InterruptedException {
+        LinkManager linkManager = ecu.getLinkManager();
+        AtomicReference<Boolean> result = new AtomicReference<>();
+
+        CountDownLatch latch = new CountDownLatch(1);
+        linkManager.submit(new Runnable() {
+            @Override
+            public void run() {
+                result.set(BurnCommand.execute(ecu.getLinkManager().getBinaryProtocol()));
+                latch.countDown();
+
+            }
+        });
+
+
+        latch.await(30, TimeUnit.SECONDS);
+        assertTrue("burn command", result.get());
+    }
+}
