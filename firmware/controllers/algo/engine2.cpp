1bd96f39e9d0a7c351d9ce37192a27bc92314eaa
rusefillc <sdfsdfqsf2334234234>
2024-03-04 22:40:28 -0500

    only: helping master

--- firmware/controllers/algo/engine2.cpp
+++ firmware/controllers/algo/engine2.cpp
@@ -89,8 +89,10 @@ void EngineState::updateSlowSensors() {
 }
 
 void EngineState::updateSparkSkip() {
+#if EFI_LAUNCH_CONTROL
 		engine->softSparkLimiter.setTargetSkipRatio(luaSoftSparkSkip + engineConfiguration->useHardSkipInTraction ? 0 : tractionControlSparkSkip);
 		engine->hardSparkLimiter.setTargetSkipRatio(luaHardSparkSkip + engineConfiguration->useHardSkipInTraction ? tractionControlSparkSkip : 0);
+#endif // EFI_LAUNCH_CONTROL
 }
 
 void EngineState::periodicFastCallback() {
