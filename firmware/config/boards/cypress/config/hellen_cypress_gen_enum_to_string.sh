b87958a16af28848082204491c470f56beaa2188
David Holdeman <dave@holdemanenterprises.com>
2024-03-06 15:43:20 -0600

    Too many ..s
    How was this ever working?

--- firmware/config/boards/cypress/config/hellen_cypress_gen_enum_to_string.sh
+++ firmware/config/boards/cypress/config/hellen_cypress_gen_enum_to_string.sh
@@ -2,7 +2,7 @@
 
 # This batch files reads rusefi_hw_enums.h and produces auto_generated_enums.* files
 
-cd ../../../../..
+cd ../../../..
 
 java -DSystemOut.name=logs/gen_enum_to_string_hellen_cypress \
  -jar ../java_tools/enum_to_string/build/libs/enum_to_string-all.jar \
