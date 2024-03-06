789dadaae5a26962b318c18262cacf30892ba689
GitHub gen-configs Action <action@github.com>
2024-03-06 22:13:09 +0000

    Auto-generated configs and docs

---
+++ firmware/config/boards/сypress/config/controllers/algo/auto_generated_enums.h
@@ -0,0 +1,10 @@
+// was generated automatically by rusEFI tool  from rusefi_hw_enums.h // by enum_to_string.jar tool on Wed Mar 06 22:12:06 UTC 2024
+// see also gen_config_and_enums.bat
+
+
+
+#include "rusefi_hw_enums.h"
+#pragma once
+#if __cplusplus
+const char *getGpio(Gpio value);
+#endif //__cplusplus
