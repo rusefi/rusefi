98309fe77a2a59f2a57054416910615995b999b5
David Holdeman <dave@holdemanenterprises.com>
2024-03-07 21:17:51 -0600

    check for all needed variables

--- firmware/bin/upload_bundle.sh
+++ firmware/bin/upload_bundle.sh
@@ -5,10 +5,10 @@ SCRIPT_NAME=$(basename "$0")
 FULL_BUNDLE_FILE="rusefi_bundle_${BUNDLE_NAME}.zip"
 UPDATE_BUNDLE_FILE="rusefi_bundle_${BUNDLE_NAME}_autoupdate.zip"
 
-if [ -n "$RUSEFI_SSH_USER" ]; then
+if [ -n "$RUSEFI_SSH_USER" -a -n "$RUSEFI_SSH_PASS" -a -n "$RUSEFI_SSH_SERVER" ]; then
  echo "$SCRIPT_NAME: Uploading full bundle"
  RET=0
- if [ "${LTS}" = "true" ]; then
+ if [ "$LTS" == "true" -a -n "$REF" ]; then
    tar -czf - $FULL_BUNDLE_FILE  | sshpass -p $RUSEFI_SSH_PASS ssh -o StrictHostKeyChecking=no $RUSEFI_SSH_USER@$RUSEFI_SSH_SERVER "mkdir -p build_server/lts/${REF}; tar -xzf - -C build_server/lts/${REF}"
 	 RET=$((RET+$?))
    tar -czf - $UPDATE_BUNDLE_FILE  | sshpass -p $RUSEFI_SSH_PASS ssh -o StrictHostKeyChecking=no $RUSEFI_SSH_USER@$RUSEFI_SSH_SERVER "mkdir -p build_server/lts/${REF}/autoupdate; tar -xzf - -C build_server/lts/${REF}/autoupdate"
