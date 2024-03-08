b00fcccb00bb309b64ad85ce03ab8360bdb1b6e6
David Holdeman <dave@holdemanenterprises.com>
2024-03-08 17:07:20 -0600

    find meta info files for gen config

--- firmware/gen_config.sh
+++ firmware/gen_config.sh
@@ -6,77 +6,26 @@ echo "The storage section of rusefi.ini is updated as well"
 rm -f gen_config.log
 rm -f gen_config_board.log
 
-# todo: who is the consumer of this folder? shall we move that 'mkdir' command closer to usage?
-mkdir build
-
 cd ../java_tools
 ./gradlew :config_definition:shadowJar
 cd ../firmware
 
-# todo: we have a bit of code duplication with build-firmware.yaml here :(
-config/boards/kinetis/config/gen_kinetis_config.sh
-[ $? -eq 0 ] || { echo "ERROR generating board kinetis kin"; exit 1; }
+genConfig ()
+{
+	source config/boards/common_script_read_meta_env.inc $1 >/dev/null
 
-config/boards/cypress/config/gen_cypress_config.sh
-[ $? -eq 0 ] || { echo "ERROR generating board cypress cypress"; exit 1; }
+	if [ -n "$CUSTOM_GEN_CONFIG" ]; then
+		bash $CUSTOM_GEN_CONFIG
+	else
+		bash gen_signature.sh ${SHORT_BOARD_NAME}
+		[ $? -eq 0 ] || { echo "ERROR generating signature for $1"; exit 1; }
+		bash gen_config_board.sh $BOARD_DIR $SHORT_BOARD_NAME
+		[ $? -eq 0 ] || { echo "ERROR generating configs for $1"; exit 1; }
+		bash bin/gen_image_board.sh $BOARD_DIR $SHORT_BOARD_NAME
+		[ $? -eq 0 ] || { echo "ERROR generating images for $1"; exit 1; }		
+	fi
+}
 
-config/boards/subaru_eg33/config/gen_subaru_config.sh
-[ $? -eq 0 ] || { echo "ERROR generating board subaru_eg33 subaru_eg33_f7"; exit 1; }
-
-#
-# see also build-firmware where we compile all versions of firmware
-# *** IMPORTANT *** most inclusive f407-discovery is last for a reason - we want it's generated files to be in repo
-#
-for BOARD in \
-   "config/boards/hellen/alphax-2chan alphax-2chan" \
-   "config/boards/hellen/alphax-4chan alphax-4chan" \
-   "config/boards/hellen/alphax-4chan alphax-4chan_f7" \
-   "config/boards/hellen/alphax-8chan alphax-8chan" \
-   "config/boards/hellen/alphax-8chan alphax-8chan_f7" \
-   "config/boards/hellen/alphax-8chan-revA alphax-8chan-revA" \
-   "config/boards/hellen/hellen128 hellen128" \
-   "config/boards/hellen/hellen121vag hellen121vag" \
-   "config/boards/hellen/hellen121nissan hellen121nissan" \
-   "config/boards/hellen/hellen-honda-k hellen-honda-k" \
-   "config/boards/hellen/hellen154hyundai hellen154hyundai" \
-   "config/boards/hellen/hellen88bmw hellen88bmw" \
-   "config/boards/hellen/uaefi uaefi" \
-   "config/boards/hellen/hellen-112-17 hellen-112-17" \
-   "config/boards/hellen/hellen81 hellen81" \
-   "config/boards/hellen/hellenMiataNA6 hellenMiataNA6" \
-   "config/boards/hellen/hellenMiataNA96 hellenMiataNA96" \
-   "config/boards/hellen/hellenMiataNB1 hellenMiataNB1" \
-   "config/boards/hellen/hellenMiataNB2 hellenMiataNB2" \
-   "config/boards/hellen/hellen-gm-e67 hellen-gm-e67" \
-   "config/boards/hellen/small-can-board small-can-board" \
-   "config/boards/microrusefi mre_f7" \
-   "config/boards/microrusefi mre_f4" \
-   "config/boards/at_start_f435 at_start_f435" \
-   "config/boards/m74_9 m74_9" \
-   "config/boards/s105 s105" \
-   "config/boards/test-build-guards t-b-g" \
-   "config/boards/frankenso_na6 frankenso_na6" \
-   "config/boards/prometheus/f469 prometheus_469" \
-   "config/boards/prometheus/f405 prometheus_405" \
-   "config/boards/proteus proteus_f7" \
-   "config/boards/proteus proteus_f4" \
-   "config/boards/proteus proteus_h7" \
-   "config/boards/f407-discovery f407-discovery" \
-   "config/boards/f429-discovery f429-discovery" \
-   "config/boards/f469-discovery f469-discovery" \
-   "config/boards/nucleo_f413 stm32f413_nucleo" \
-   "config/boards/nucleo_f429 stm32f429_nucleo" \
-   "config/boards/nucleo_f767 stm32f767_nucleo" \
-   "config/boards/nucleo_h743 nucleo_h743" \
-   "config/boards/atlas atlas"\
-   "config/boards/tdg-pdm8 tdg-pdm8"\
-   ; do
- BOARD_DIR=$(echo "$BOARD" | cut -d " " -f 1)
- SHORT_BOARD_NAME=$(echo "$BOARD" | cut -d " " -f 2)
- bash gen_signature.sh ${SHORT_BOARD_NAME}
- bash gen_config_board.sh $BOARD_DIR $SHORT_BOARD_NAME
- bash bin/gen_image_board.sh $BOARD_DIR $SHORT_BOARD_NAME
- [ $? -eq 0 ] || { echo "ERROR generating board dir=[$BOARD_DIR] short=[$SHORT_BOARD_NAME]"; exit 1; }
+find config/boards -name "meta-info*.env" -print0 | while IFS= read -r -d '' f; do
+	echo -n "$(genConfig $f)"
 done
-
-exit 0
