b9f496b22e92c2890e1833aea6ce31675f909f3b
David Holdeman <dave@holdemanenterprises.com>
2024-03-09 11:17:00 -0600

    Use env vars to define serial devices

--- .github/workflows/hw-ci/run_hw_ci.sh
+++ .github/workflows/hw-ci/run_hw_ci.sh
@@ -11,4 +11,13 @@ cd java_tools
 ./gradlew :autotest:shadowJar
 cd ..
 
+if [ -n "$HARDWARE_CI_SERIAL" ]; then
+	for t in /sys/class/tty/tty*/device/../serial; do
+	  if [ "$(cat $t)" == "$HARDWARE_CI_SERIAL" ]; then
+	    export HARDWARE_CI_SERIAL_DEVICE=/dev/$(echo $t | cut -d '/' -f 5)
+			break
+		fi
+	done
+fi
+
 java -cp java_console/autotest/build/libs/autotest-all.jar $HW_SUITE
