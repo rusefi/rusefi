c8609f27ff13d541aca0c3d5f4d53993c278f3ba
David Holdeman <dave@holdemanenterprises.com>
2024-03-09 11:17:00 -0600

    Use env vars to define serial devices

--- .github/workflows/hw-ci/openocd_wipe_and_flash.sh
+++ .github/workflows/hw-ci/openocd_wipe_and_flash.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-HW_SCRIPT=$1
+HW_SCRIPT=${1:-/tmp/openocd.tmp}
+
+if [ -z "$1" ]; then
+# Write whatever is piped to this script to temp file
+	cat >$HW_SCRIPT
+fi
 
 set -e
 
@@ -14,3 +19,7 @@ cd firmware
 
 # works on F4 with ST-LINK/V3 does not seem to work using V2 :(
 openocd -f "$HW_SCRIPT" -c init -c targets -c "reset halt" -c "flash erase_sector 0 0 last" -c "flash write_image "deliver/rusefi.bin" 0x08000000" -c "reset run" -c "shutdown"
+
+if [ -z "$1" ]; then
+	rm $HW_SCRIPT
+fi
