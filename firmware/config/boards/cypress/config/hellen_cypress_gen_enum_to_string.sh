84bac24c3ddd0d45565170083dc4f85fa4a22a3f
David Holdeman <dave@holdemanenterprises.com>
2024-03-09 18:39:33 -0600

    fix weird c

--- firmware/config/boards/cypress/config/hellen_cypress_gen_enum_to_string.sh
+++ firmware/config/boards/cypress/config/hellen_cypress_gen_enum_to_string.sh
@@ -6,6 +6,6 @@ cd ../../../..
 
 java -DSystemOut.name=logs/gen_enum_to_string_hellen_cypress \
  -jar ../java_tools/enum_to_string/build/libs/enum_to_string-all.jar \
- -outputPath config/boards/сypress/config/controllers/algo \
+ -outputPath config/boards/cypress/config/controllers/algo \
  -enumInputFile config/boards/cypress/rusefi_hw_enums.h
 
