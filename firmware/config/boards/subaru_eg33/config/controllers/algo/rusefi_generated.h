1bf2e90136fcf0beecad4aa814b56c38d56bb353
GitHub gen-configs Action <action@github.com>
2024-03-14 23:10:16 +0000

    Auto-generated configs and docs

--- firmware/config/boards/subaru_eg33/config/controllers/algo/rusefi_generated.h
+++ firmware/config/boards/subaru_eg33/config/controllers/algo/rusefi_generated.h
@@ -975,7 +975,7 @@
 #define show_Frankenso_presets false
 #define show_Proteus_presets false
 #define show_test_presets false
-#define SIGNATURE_HASH 86781705
+#define SIGNATURE_HASH 2596208214
 #define spi_device_e_auto_enum 0="SPI_NONE",1="SPI_DEVICE_1",2="SPI_DEVICE_2",3="SPI_DEVICE_3",4="SPI_DEVICE_4",5="SPI_DEVICE_5",6="SPI_DEVICE_6"
 #define spi_device_e_SPI_DEVICE_1 1
 #define spi_device_e_SPI_DEVICE_2 2
@@ -1256,7 +1256,7 @@
 #define ts_show_vr_threshold_2 true
 #define ts_show_vr_threshold_all true
 #define ts_show_vvt true
-#define TS_SIGNATURE "rusEFI (FOME) master.2024.03.14.subaru_eg33_f7.86781705"
+#define TS_SIGNATURE "rusEFI (FOME) master.2024.03.14.subaru_eg33_f7.2596208214"
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't'
