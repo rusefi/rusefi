6c64503cd2df49ccd48eb79627bccd16b3751bcf
rusefillc <sdfsdfqsf2334234234>
2024-03-16 17:20:10 -0600

    funny games with dash via Lua to override CAN dash profile values #6235

--- firmware/controllers/lua/lua_hooks.h
+++ firmware/controllers/lua/lua_hooks.h
@@ -7,3 +7,10 @@ void luaDeInitPins();
 struct AirmassModelBase;
 AirmassModelBase& getLuaAirmassModel();
 bool getAuxDigital(int index);
+
+struct LuaDashOverride {
+  // this helps with all kinds of cool tachometer sweeps!
+  int16_t rpm = -1;
+  int16_t coolant = -1;
+  float vbatt = -1;
+};
