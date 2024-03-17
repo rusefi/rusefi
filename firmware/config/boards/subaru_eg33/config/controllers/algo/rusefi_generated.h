3e473f98907a0820cbd79ed3cd3560661f029572
GitHub gen-configs Action <action@github.com>
2024-03-17 19:10:34 +0000

    Auto-generated configs and docs

--- firmware/config/boards/subaru_eg33/config/controllers/algo/rusefi_generated.h
+++ firmware/config/boards/subaru_eg33/config/controllers/algo/rusefi_generated.h
@@ -382,7 +382,7 @@
 #define ETB_BIAS_CURVE_LENGTH 8
 #define ETB_COUNT 2
 #define ETB_HW_MAX_FREQUENCY 3000
-#define FLASH_DATA_VERSION 20014
+#define FLASH_DATA_VERSION 20015
 #define FUEL_LEVEL_TABLE_COUNT 8
 #define FUEL_LOAD_COUNT 16
 #define fuel_pressure_sensor_mode_e_auto_enum 0="FPM_Absolute",2="FPM_Differential",1="FPM_Gauge"
@@ -975,7 +975,7 @@
 #define show_Frankenso_presets false
 #define show_Proteus_presets false
 #define show_test_presets false
-#define SIGNATURE_HASH 1767830795
+#define SIGNATURE_HASH 2103975448
 #define spi_device_e_auto_enum 0="SPI_NONE",1="SPI_DEVICE_1",2="SPI_DEVICE_2",3="SPI_DEVICE_3",4="SPI_DEVICE_4",5="SPI_DEVICE_5",6="SPI_DEVICE_6"
 #define spi_device_e_SPI_DEVICE_1 1
 #define spi_device_e_SPI_DEVICE_2 2
@@ -1256,7 +1256,7 @@
 #define ts_show_vr_threshold_2 true
 #define ts_show_vr_threshold_all true
 #define ts_show_vvt true
-#define TS_SIGNATURE "rusEFI (FOME) master.2024.03.17.subaru_eg33_f7.1767830795"
+#define TS_SIGNATURE "rusEFI (FOME) master.2024.03.17.subaru_eg33_f7.2103975448"
 #define TS_SINGLE_WRITE_COMMAND 'W'
 #define TS_SINGLE_WRITE_COMMAND_char W
 #define TS_TEST_COMMAND 't'
