9dbb25216863ee05e894d670a0507427e23f27f6
Matthew Kennedy <matthewkennedy@outlook.com>
2024-03-19 15:23:54 -0700

    no need for a separate gen_config_common.sh

--- firmware/gen_config_board.sh
+++ firmware/gen_config_board.sh
@@ -36,16 +36,34 @@ if [ "" = "$BOARD_SPECIFIC_URL" ]; then
 fi
 echo "BOARD_SPECIFIC_URL=[$BOARD_SPECIFIC_URL]"
 
-source gen_config_common.sh
-echo "Using COMMON_GEN_CONFIG [$COMMON_GEN_CONFIG]"
-
 # work in progress: migrating to fome_${BUNDLE_NAME}.txt
 java \
- $COMMON_GEN_CONFIG_PREFIX \
- $COMMON_GEN_CONFIG \
+	-DSystemOut.name=logs/gen_config_${SHORT_BOARDNAME} \
+	-jar ../java_tools/ConfigDefinition.jar \
+	-enumInputFile controllers/algo/engine_types.h \
+	-enumInputFile controllers/algo/rusefi_enums.h \
+	-readfile OUTPUTS_SECTION_FROM_FILE generated/output_channels.ini \
+	-readfile DATALOG_SECTION_FROM_FILE generated/data_logs.ini \
+	-readfile LIVE_DATA_MENU_FROM_FILE generated/fancy_menu.ini \
+	-readfile LIVE_DATA_PANELS_FROM_FILE generated/fancy_content.ini \
+	-ts_destination tunerstudio \
+	-firing_order controllers/algo/firing_order.h \
+	-triggerInputFolder ../unit_tests \
+	-with_c_defines false \
+	-field_lookup_file generated/value_lookup_generated.cpp generated/value_lookup_generated.md \
+	-java_destination ../java_console/models/src/main/java/com/rusefi/config/generated/Fields.java \
+	-initialize_to_zero false \
+	-prepend generated/total_live_data_generated.h \
+	-signature tunerstudio/generated/signature_${SHORT_BOARDNAME}.txt \
+	-signature_destination generated/signature_${SHORT_BOARDNAME}.h \
+	-ts_output_name generated/${INI} \
+	-prepend integration/rusefi_config_shared.txt \
+	-prepend ${BOARD_DIR}/prepend.txt \
+	-board ${BOARD_DIR} \
+	-definition integration/rusefi_config.txt \
 	-enumInputFile controllers/algo/rusefi_hw_enums.h \
-  -c_defines        generated/rusefi_generated.h \
-  -c_destination    generated/engine_configuration_generated_structures.h
+	-c_defines        generated/rusefi_generated.h \
+	-c_destination    generated/engine_configuration_generated_structures.h
 
 [ $? -eq 0 ] || { echo "ERROR generating TunerStudio config for ${BOARD_DIR}"; exit 1; }
 
