8cfd8fffdbafa441c0070cb35c4f09139d7d10b9
Matthew Kennedy <matthewkennedy@outlook.com>
2024-03-20 01:01:52 -0700

    reorder gen config parameters

--- firmware/gen_config_board.sh
+++ firmware/gen_config_board.sh
@@ -40,8 +40,6 @@ echo "BOARD_SPECIFIC_URL=[$BOARD_SPECIFIC_URL]"
 java \
 	-DSystemOut.name=logs/gen_config_${SHORT_BOARDNAME} \
 	-jar ../java_tools/ConfigDefinition.jar \
-	-enumInputFile controllers/algo/engine_types.h \
-	-enumInputFile controllers/algo/rusefi_enums.h \
 	-readfile OUTPUTS_SECTION_FROM_FILE generated/output_channels.ini \
 	-readfile DATALOG_SECTION_FROM_FILE generated/data_logs.ini \
 	-readfile LIVE_DATA_MENU_FROM_FILE generated/fancy_menu.ini \
@@ -53,14 +51,16 @@ java \
 	-field_lookup_file generated/value_lookup_generated.cpp generated/value_lookup_generated.md \
 	-java_destination ../java_console/models/src/main/java/com/rusefi/config/generated/Fields.java \
 	-initialize_to_zero false \
-	-prepend generated/total_live_data_generated.h \
 	-signature tunerstudio/generated/signature_${SHORT_BOARDNAME}.txt \
 	-signature_destination generated/signature_${SHORT_BOARDNAME}.h \
 	-ts_output_name generated/${INI} \
+	-board ${BOARD_DIR} \
+	-prepend generated/total_live_data_generated.h \
 	-prepend integration/rusefi_config_shared.txt \
 	-prepend ${BOARD_DIR}/prepend.txt \
-	-board ${BOARD_DIR} \
 	-definition integration/rusefi_config.txt \
+	-enumInputFile controllers/algo/engine_types.h \
+	-enumInputFile controllers/algo/rusefi_enums.h \
 	-enumInputFile controllers/algo/rusefi_hw_enums.h \
 	-c_defines        generated/rusefi_generated.h \
 	-c_destination    generated/engine_configuration_generated_structures.h
