2e97146e5501943e49a5c0d7d5cd0df258599a7c
rusefillc <sdfsdfqsf2334234234>
2024-03-19 22:23:59 -0400

    Cypress and Kinetis board-specific firmware fails to build #6253
    only:an idea

--- firmware/config/boards/kinetis/config/kinetis_gen_enum_to_string.sh
+++ firmware/config/boards/kinetis/config/kinetis_gen_enum_to_string.sh
@@ -2,6 +2,8 @@
 
 cd ../../../..
 
+# todo: CUSTOM_ENUM_OUTPUT=config/boards/kinetis/config/controllers/algo?
+
 java -DSystemOut.name=logs/gen_enum_to_string_kinetis \
  -jar ../java_tools/enum_to_string/build/libs/enum_to_string-all.jar \
  -outputPath config/boards/kinetis/config/controllers/algo \
