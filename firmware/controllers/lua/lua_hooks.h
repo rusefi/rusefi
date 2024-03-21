116b9a5b2d165cffb164fd503dbd9afcb4e01086
Andrey <rusefillc@gmail.com>
2024-03-21 09:37:00 -0400

    Revert "funny games with dash via Lua to override CAN dash profile values #6235"
    This reverts commit 6c64503cd2df49ccd48eb79627bccd16b3751bcf.

--- firmware/controllers/lua/lua_hooks.h
+++ firmware/controllers/lua/lua_hooks.h
@@ -7,10 +7,3 @@ void luaDeInitPins();
 struct AirmassModelBase;
 AirmassModelBase& getLuaAirmassModel();
 bool getAuxDigital(int index);
-
-struct LuaDashOverride {
-  // this helps with all kinds of cool tachometer sweeps!
-  int16_t rpm = -1;
-  int16_t coolant = -1;
-  float vbatt = -1;
-};
