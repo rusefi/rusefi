d0dd8b1d4badbd477ecbb2e61dbf57deb61586b4
Andrey <rusefillc@gmail.com>
2024-03-24 00:14:41 -0400

    bosch quick start decoding issue or else #6281

--- unit_tests/tests/trigger/test_real_bosch_quick_start.cpp
+++ unit_tests/tests/trigger/test_real_bosch_quick_start.cpp
@@ -5,9 +5,9 @@
 TEST(realBQS, readAsPrimarySensor) {
   CsvReader reader(/*triggerCount*/1, /* vvtCount */ 0);
   reader.open("tests/trigger/resources/BQS-longer.csv");
+  reader.flipOnRead = true;
 
-	EngineTestHelper eth(engine_type_e::TEST_ENGINE);
-	eth.setTriggerType(trigger_type_e::TT_VVT_BOSCH_QUICK_START);
+	EngineTestHelper eth(engine_type_e::ET_BOSCH_QUICK_START);
 
 	int eventCount = 0;
 	bool gotRpm = false;
@@ -20,13 +20,13 @@ TEST(realBQS, readAsPrimarySensor) {
 		if (!gotRpm && rpm) {
 			gotRpm = true;
 
-			EXPECT_EQ(eventCount, 7);
-			EXPECT_NEAR(rpm, 808.32f, 0.1);
+			EXPECT_EQ(eventCount, 13);
+			EXPECT_NEAR(rpm, 2035.53466f, 0.1);
 			break;
 		}
 
   }
   ASSERT_TRUE(Sensor::get(SensorType::Rpm).Valid);
-  ASSERT_EQ(Sensor::get(SensorType::Rpm).Value, 0);
+  ASSERT_FLOAT_EQ(Sensor::get(SensorType::Rpm).Value, 2035.53467);
 
 }
