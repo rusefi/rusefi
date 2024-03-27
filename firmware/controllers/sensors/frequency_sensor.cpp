75bde97253b3d3427bd624fef59d8945920e9595
rusefillc <sdfsdfqsf2334234234>
2024-03-26 23:46:51 -0400

    Revert "frequency_sensor: dead comment"
    This reverts commit 6f564bd73b848efda87565458da967ca34ac65f8.

--- firmware/controllers/sensors/frequency_sensor.cpp
+++ firmware/controllers/sensors/frequency_sensor.cpp
@@ -31,6 +31,7 @@ void FrequencySensor::initIfValid(brain_pin_e pin, SensorConverter &converter, f
 	setFunction(converter);
 
 #if EFI_PROD_CODE
+	// todo: refactor https://github.com/rusefi/rusefi/issues/2123
 	if (efiExtiEnablePin(getSensorName(), pin, PAL_EVENT_MODE_FALLING_EDGE,
 			freqSensorExtiCallback, reinterpret_cast<void*>(this)) < 0) {
 		return;
