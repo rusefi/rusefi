f0a21e1b586a6fa775d14b8dae48687cbcfad3c9
rusefillc <sdfsdfqsf2334234234>
2024-03-29 23:49:47 -0400

    preparing upload_bundle.sh for reuse with different credentials

--- firmware/bin/upload_bundle.sh
+++ firmware/bin/upload_bundle.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 
+USER=$1
+PASS=$2
+HOST=$3
+
 SCRIPT_NAME=$(basename "$0")
 # technical debt: more than one file uses magic 'rusefi_bundle_' constant, can we extract constant?
 FULL_BUNDLE_FILE="rusefi_bundle_${BUNDLE_NAME}.zip"
 UPDATE_BUNDLE_FILE="rusefi_bundle_${BUNDLE_NAME}_autoupdate.zip"
 
-if [ -n "$RUSEFI_SSH_USER" -a -n "$RUSEFI_SSH_PASS" -a -n "$RUSEFI_SSH_SERVER" ]; then
+if [ -n "${USER}" -a -n "$PASS" -a -n "${HOST}" ]; then
  echo "$SCRIPT_NAME: Uploading full bundle"
  RET=0
  if [ "$LTS" == "true" -a -n "$REF" ]; then
-   tar -czf - $FULL_BUNDLE_FILE  | sshpass -p $RUSEFI_SSH_PASS ssh -o StrictHostKeyChecking=no $RUSEFI_SSH_USER@$RUSEFI_SSH_SERVER "mkdir -p build_server/lts/${REF}; tar -xzf - -C build_server/lts/${REF}"
+   tar -czf - $FULL_BUNDLE_FILE  | sshpass -p $PASS ssh -o StrictHostKeyChecking=no ${USER}@${HOST} "mkdir -p build_server/lts/${REF}; tar -xzf - -C build_server/lts/${REF}"
 	 RET=$((RET+$?+PIPESTATUS))
-   tar -czf - $UPDATE_BUNDLE_FILE  | sshpass -p $RUSEFI_SSH_PASS ssh -o StrictHostKeyChecking=no $RUSEFI_SSH_USER@$RUSEFI_SSH_SERVER "mkdir -p build_server/lts/${REF}/autoupdate; tar -xzf - -C build_server/lts/${REF}/autoupdate"
+   tar -czf - $UPDATE_BUNDLE_FILE  | sshpass -p $PASS ssh -o StrictHostKeyChecking=no ${USER}@${HOST} "mkdir -p build_server/lts/${REF}/autoupdate; tar -xzf - -C build_server/lts/${REF}/autoupdate"
 	 RET=$((RET+$?+PIPESTATUS))
  else
-   tar -czf - $FULL_BUNDLE_FILE  | sshpass -p $RUSEFI_SSH_PASS ssh -o StrictHostKeyChecking=no $RUSEFI_SSH_USER@$RUSEFI_SSH_SERVER "tar -xzf - -C build_server"
+   tar -czf - $FULL_BUNDLE_FILE  | sshpass -p $PASS ssh -o StrictHostKeyChecking=no ${USER}@${HOST} "tar -xzf - -C build_server"
 	 RET=$((RET+$?+PIPESTATUS))
-   tar -czf - $UPDATE_BUNDLE_FILE  | sshpass -p $RUSEFI_SSH_PASS ssh -o StrictHostKeyChecking=no $RUSEFI_SSH_USER@$RUSEFI_SSH_SERVER "mkdir -p build_server/autoupdate; tar -xzf - -C build_server/autoupdate"
+   tar -czf - $UPDATE_BUNDLE_FILE  | sshpass -p $PASS ssh -o StrictHostKeyChecking=no ${USER}@${HOST} "mkdir -p build_server/autoupdate; tar -xzf - -C build_server/autoupdate"
 	 RET=$((RET+$?+PIPESTATUS))
  fi
  if [ $RET -ne 0 ]; then
