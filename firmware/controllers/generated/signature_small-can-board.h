4f55fe3db98b4cdb78a9930882f22a55cf9145b5
GitHub gen-configs Action <action@github.com>
2024-04-07 13:43:41 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_small-can-board.h
+++ firmware/controllers/generated/signature_small-can-board.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 4047280920
-#define TS_SIGNATURE "rusEFI master.2024.04.07.small-can-board.4047280920"
+#define SIGNATURE_HASH 1768977843
+#define TS_SIGNATURE "rusEFI master.2024.04.07.small-can-board.1768977843"
