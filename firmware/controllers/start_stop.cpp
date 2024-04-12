6929e57d486f3d59a02f3e534cd5ac7560679105
rusefi <rusefillc@gmail.com>
2024-04-12 00:29:35 -0400

    actual startRequestPinInverted usage only: proteus_f7

--- firmware/controllers/start_stop.cpp
+++ firmware/controllers/start_stop.cpp
@@ -10,7 +10,10 @@ ButtonDebounce startStopButtonDebounce("start_button");
 
 void initStartStopButton() {
 	/* startCrankingDuration is efitimesec_t, so we need to multiply it by 1000 to get milliseconds*/
-	startStopButtonDebounce.init((engineConfiguration->startCrankingDuration*1000), engineConfiguration->startStopButtonPin, engineConfiguration->startStopButtonMode);
+	startStopButtonDebounce.init((engineConfiguration->startCrankingDuration*1000),
+	  engineConfiguration->startStopButtonPin,
+	  engineConfiguration->startStopButtonMode,
+	  engineConfiguration->startRequestPinInverted);
 }
 
 static void onStartStopButtonToggle() {
