cb7fd93cb97c5a0a6bdabaf1200391249ab6606a
rusefillc <sdfsdfqsf2334234234>
2024-04-13 09:35:29 -0400

    only: tle9104 discovery test #6361

--- firmware/controllers/algo/engine_configuration.cpp
+++ firmware/controllers/algo/engine_configuration.cpp
@@ -937,6 +937,9 @@ void resetConfigurationExt(configuration_callback_t boardCallback, engine_type_e
 	case engine_type_e::FRANKENSO_TEST_33810:
 		setDiscovery33810Test();
 		break;
+	case engine_type_e::FRANKENSO_TEST_TLE9104:
+		setDiscoveryTLE9104Test();
+		break;
 	case engine_type_e::TEST_ENGINE:
 		setTestCamEngineConfiguration();
 		break;
