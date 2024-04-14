3ba5a2f0c5d0c2ff04a1bb96677bdfe960b88396
GitHub gen-configs Action <action@github.com>
2024-04-14 18:33:04 +0000

    Auto-generated configs and docs

--- java_console/models/src/main/java/com/rusefi/config/generated/Fields.java
+++ java_console/models/src/main/java/com/rusefi/config/generated/Fields.java
@@ -1178,7 +1178,7 @@ public class Fields {
 	public static final int TS_RESPONSE_UNDERRUN = 0x80;
 	public static final int TS_RESPONSE_UNRECOGNIZED_COMMAND = 0x83;
 	public static final char TS_SET_LOGGER_SWITCH = 'l';
-	public static final String TS_SIGNATURE = "rusEFI (FOME) master.2024.04.12.f407-discovery.2305494381";
+	public static final String TS_SIGNATURE = "rusEFI (FOME) master.2024.04.14.f407-discovery.2305494381";
 	public static final char TS_SINGLE_WRITE_COMMAND = 'W';
 	public static final char TS_TEST_COMMAND = 't';
 	public static final int TS_TOTAL_OUTPUT_SIZE = 1388;
