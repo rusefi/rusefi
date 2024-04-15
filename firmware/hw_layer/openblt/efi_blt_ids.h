3a311a4c551648ffd6781177e32dfe4387039717
rusefi <rusefillc@gmail.com>
2024-04-15 00:44:21 -0400

    moving OpenBLT IDs a bit up to reduce chance of conflict on random OEM, take two
    only:small-can-board

--- firmware/hw_layer/openblt/efi_blt_ids.h
+++ firmware/hw_layer/openblt/efi_blt_ids.h
@@ -1,5 +1,7 @@
 // file efi_blt_ids.h
 
+// Note that an extended 29-bit CAN identifier is configured by OR-ing with mask 0x80000000.
+
 #ifndef BOOT_COM_CAN_BAUDRATE
 /** \brief Configure the desired CAN baudrate. */
 #define BOOT_COM_CAN_BAUDRATE           (500000)
@@ -8,11 +10,11 @@
 #ifndef BOOT_COM_CAN_RX_MSG_ID
 // PC or Android device doing the programming
 /** \brief Configure CAN message ID host->target. */
-#define BOOT_COM_CAN_RX_MSG_ID          (0x1667 /*| 0x80000000*/)
+#define BOOT_COM_CAN_RX_MSG_ID          (0x10667 | 0x80000000)
 #endif
 
 #ifndef BOOT_COM_CAN_TX_MSG_ID
 // ECU talks this CAN is
 /** \brief Configure CAN message ID target->host. */
-#define BOOT_COM_CAN_TX_MSG_ID          (0x17E1 /*| 0x80000000*/)
+#define BOOT_COM_CAN_TX_MSG_ID          (0x107E1 | 0x80000000)
 #endif
