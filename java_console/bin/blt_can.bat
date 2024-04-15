9668f424f572dbb94a636fe85986500f5b265bb6
rusefi <rusefillc@gmail.com>
2024-04-15 00:53:09 -0400

    updating batch file for BLT
    only:small-can-board

--- java_console/bin/blt_can.bat
+++ java_console/bin/blt_can.bat
@@ -1,2 +1,2 @@
 cd ../console
-BootCommander.exe -t=xcp_can -d=peak_pcanusb -c=0 -tid=667h -rid=7E1h ../rusefi_update.srec
+BootCommander.exe -t=xcp_can -d=peak_pcanusb -c=0 -tid=10667h -rid=107E1h -xid=1 ../rusefi_update.srec
