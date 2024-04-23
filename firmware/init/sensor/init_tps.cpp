78694f7adc2a0c759a69502791f84d9d35be7482
rusEFI LLC <rusefillc@gmail.com>
2024-04-22 23:25:41 -0400

    only: better variable name

--- firmware/init/sensor/init_tps.cpp
+++ firmware/init/sensor/init_tps.cpp
@@ -201,8 +201,8 @@ bool isDigitalTps1() {
 
 void initTps() {
     criticalAssertVoid(engineConfiguration != nullptr, "null engineConfiguration");
-	percent_t min = engineConfiguration->tpsErrorDetectionTooLow;
-	percent_t max = engineConfiguration->tpsErrorDetectionTooHigh;
+	percent_t minTpsPps = engineConfiguration->tpsErrorDetectionTooLow;
+	percent_t maxTpsPps = engineConfiguration->tpsErrorDetectionTooHigh;
 
 	if (!engineConfiguration->consumeObdSensors) {
 		bool isFordTps = engineConfiguration->useFordRedundantTps;
@@ -219,14 +219,14 @@ void initTps() {
             sentTps.Register();
         } else {
 		    analogTps1.init(isFordTps, &fordTps1, tpsSecondaryMaximum,
-    			{ engineConfiguration->tps1_1AdcChannel, (float)engineConfiguration->tpsMin, (float)engineConfiguration->tpsMax, min, max },
-	    		{ engineConfiguration->tps1_2AdcChannel, (float)engineConfiguration->tps1SecondaryMin, (float)engineConfiguration->tps1SecondaryMax, min, max }
+    			{ engineConfiguration->tps1_1AdcChannel, (float)engineConfiguration->tpsMin, (float)engineConfiguration->tpsMax, minTpsPps, maxTpsPps },
+	    		{ engineConfiguration->tps1_2AdcChannel, (float)engineConfiguration->tps1SecondaryMin, (float)engineConfiguration->tps1SecondaryMax, minTpsPps, maxTpsPps }
 		    );
 		}
 
 		tps2.init(isFordTps, &fordTps2, tpsSecondaryMaximum,
-			{ engineConfiguration->tps2_1AdcChannel, (float)engineConfiguration->tps2Min, (float)engineConfiguration->tps2Max, min, max },
-			{ engineConfiguration->tps2_2AdcChannel, (float)engineConfiguration->tps2SecondaryMin, (float)engineConfiguration->tps2SecondaryMax, min, max }
+			{ engineConfiguration->tps2_1AdcChannel, (float)engineConfiguration->tps2Min, (float)engineConfiguration->tps2Max, minTpsPps, maxTpsPps },
+			{ engineConfiguration->tps2_2AdcChannel, (float)engineConfiguration->tps2SecondaryMin, (float)engineConfiguration->tps2SecondaryMax, minTpsPps, maxTpsPps }
 		);
 
 		float ppsSecondaryMaximum = engineConfiguration->ppsSecondaryMaximum;
@@ -237,14 +237,14 @@ void initTps() {
 
 	// Pedal sensors
 	pedal.init(isFordPps, &fordPps, ppsSecondaryMaximum,
-		{ engineConfiguration->throttlePedalPositionAdcChannel, engineConfiguration->throttlePedalUpVoltage, engineConfiguration->throttlePedalWOTVoltage, min, max },
-		{ engineConfiguration->throttlePedalPositionSecondAdcChannel, engineConfiguration->throttlePedalSecondaryUpVoltage, engineConfiguration->throttlePedalSecondaryWOTVoltage, min, max },
+		{ engineConfiguration->throttlePedalPositionAdcChannel, engineConfiguration->throttlePedalUpVoltage, engineConfiguration->throttlePedalWOTVoltage, minTpsPps, maxTpsPps },
+		{ engineConfiguration->throttlePedalPositionSecondAdcChannel, engineConfiguration->throttlePedalSecondaryUpVoltage, engineConfiguration->throttlePedalSecondaryWOTVoltage, minTpsPps, maxTpsPps },
 		engineConfiguration->allowIdenticalPps
 	);
 
 		// TPS-like stuff that isn't actually a TPS
-		wastegate.init({ engineConfiguration->wastegatePositionSensor, (float)engineConfiguration->wastegatePositionMin, (float)engineConfiguration->wastegatePositionMax, min, max });
-		idlePos.init({ engineConfiguration->idlePositionChannel, (float)engineConfiguration->idlePositionMin, (float)engineConfiguration->idlePositionMax, min, max });
+		wastegate.init({ engineConfiguration->wastegatePositionSensor, (float)engineConfiguration->wastegatePositionMin, (float)engineConfiguration->wastegatePositionMax, minTpsPps, maxTpsPps });
+		idlePos.init({ engineConfiguration->idlePositionChannel, (float)engineConfiguration->idlePositionMin, (float)engineConfiguration->idlePositionMax, minTpsPps, maxTpsPps });
 	}
 
 	// Route the pedal or TPS to driverIntent as appropriate
