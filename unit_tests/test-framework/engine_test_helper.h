271b36cb72ae3449deb24eec0b8b6e48341347bb
Matthew Kennedy <matthewkennedy@outlook.com>
2024-04-25 22:17:20 -0700

    haha these two functions do the same thing!?

--- unit_tests/test-framework/engine_test_helper.h
+++ unit_tests/test-framework/engine_test_helper.h
@@ -67,7 +67,6 @@ public:
 	void smartFireFall(float delayMs);
 	void moveTimeForwardAndInvokeEventsUs(int deltaTimeUs);
 	void setTimeAndInvokeEventsUs(int timeNowUs);
-	void executeUntil(int timeUs);
 	void moveTimeForwardAndInvokeEventsSec(int deltaTimeSeconds);
 	/**
 	 * both Rise and Fall
