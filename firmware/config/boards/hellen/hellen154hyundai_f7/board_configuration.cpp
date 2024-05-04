d2f34adab92fe437f1bccdd777c72bd6f3ff9312
rusefi <rusefillc@gmail.com>
2024-05-04 17:56:46 -0400

    only:hellen154hyundai_f7

--- firmware/config/boards/hellen/hellen154hyundai_f7/board_configuration.cpp
+++ firmware/config/boards/hellen/hellen154hyundai_f7/board_configuration.cpp
@@ -176,16 +176,15 @@ static const struct mc33810_config mc33810 = {
 
 /*BOARD_WEAK*/ void boardInitHardware() {
     #if (BOARD_MC33810_COUNT > 0)
-      gpio_pin_markUsed(mc33810.spi_config.ssport, mc33810.spi_config.sspad, "IWS MC CS");
+      gpio_pin_markUsed(mc33810.spi_config.ssport, mc33810.spi_config.sspad, "mc33810 CS");
       palSetPadMode(mc33810.spi_config.ssport, mc33810.spi_config.sspad, PAL_MODE_OUTPUT_PUSHPULL);
 
-      addConsoleAction("mc33810", [](){
-        mc33810_add(Gpio::MC33810_0_OUT_0, 0, &mc33810);
-      });
-
-//      auto voltage = Sensor::get(SensorType::BatteryVoltage);
-//      int ret = mc33810_add(Gpio::MC33810_0_OUT_0, 0, &mc33810);
-//      efiPrintf("*****************+ mc33810_add %d +******************* %f", ret, voltage);
+//      addConsoleAction("mc33810", [](){
+//        mc33810_add(Gpio::MC33810_0_OUT_0, 0, &mc33810);
+//      });
+      auto voltage = Sensor::get(SensorType::BatteryVoltage);
+      int ret = mc33810_add(Gpio::MC33810_0_OUT_0, 0, &mc33810);
+      efiPrintf("*****************+ mc33810_add %d +******************* %f", ret, voltage);
     #else
       efiPrintf("**********------------- mc33810 not configured yet ------------************");
     #endif
