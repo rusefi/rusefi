80f6ae0ab57745ea15e727ee03668abfb19bcade
Nathan Schulte <8540239+nmschulte@users.noreply.github.com>
2024-05-03 23:58:07 -0500

    fix building firmware on linux (#420)
    * firmware: fix building on Linux
    
    processor is non-portable, though some Linux distributions patch around this (generally by report machine instead)
    
    * firmware: tidy use_arm_gcc.mk
    
    * fix failure logic

--- firmware/use_arm_gcc.mk
+++ firmware/use_arm_gcc.mk
@@ -1,30 +1,29 @@
 ifeq ($(TRGT),)
-	UNAME_S := $(shell uname -s)
-	UNAME_SP := $(shell uname -sp)
-$(info UNAME_SP:          $(UNAME_SP))
+	UNAME_SM := $(shell uname -sm)
+$(info UNAME_SM:          $(UNAME_SM))
 
-	ifeq ($(UNAME_S),Darwin)
+	ifeq ($(firstword $(UNAME_SM)),Darwin)
 		COMPILER_PLATFORM = arm-gnu-toolchain-11.3.rel1-darwin-x86_64-arm-none-eabi
-	else ifeq ($(UNAME_SP),Linux x86_64)
+	else ifeq ($(UNAME_SM),Linux x86_64)
 		COMPILER_PLATFORM = arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi
 	else
-$(error Looks like your platform, $(UNAME_SP), doesn't have a supported compiler!)
+$(error Looks like your platform, $(UNAME_SM), doesn't have a supported compiler!)
 	endif
 
 	BUILD_TOOLS_DIR = $(PROJECT_DIR)/ext/build-tools/
 	TRGT = $(BUILD_TOOLS_DIR)$(COMPILER_PLATFORM)/bin/arm-none-eabi-
 
-# If the compiler doesn't exist, try to update the build tools submodule
-ifeq ("$(wildcard $(TRGT)g++)","")
+	# If the compiler doesn't exist, try to update the build tools submodule
+	ifeq ("$(wildcard $(TRGT)g++)","")
 $(info Compiler at $(TRGT)g++ not found, trying to update build-tools submodule...")
 $(shell git submodule update --init --depth=1 $(BUILD_TOOLS_DIR))
 $(error Please re-run make to execute build!)
-endif
+	endif
 else
-# If the compiler doesn't exist, fault now
-ifeq ("$(wildcard $(TRGT)g++)","")
+	# If the compiler doesn't exist, fault now
+	ifeq ("$(wildcard $(TRGT)g++)","")
 $(error Compiler not found at $(TRGT)g++!)
-endif
+	endif
 endif
 
 $(info COMPILER_PLATFORM: $(COMPILER_PLATFORM))
