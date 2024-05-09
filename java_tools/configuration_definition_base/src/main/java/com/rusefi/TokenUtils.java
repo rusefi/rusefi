5bf72f9d3cdc22e419a2ca45761f916000648d5f
kifir <kifir239@gmail.com>
2024-05-09 22:45:23 +0300

    light refactoring

--- java_tools/configuration_definition_base/src/main/java/com/rusefi/TokenUtils.java
+++ java_tools/configuration_definition_base/src/main/java/com/rusefi/TokenUtils.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TokenUtils {
-    public static String[] tokenizeWithBraces(String stringToTokenize) {
+    public static String[] tokenizeWithBraces(final String stringToTokenize) {
         final List<String> tokens = new ArrayList<>();
         if (stringToTokenize != null) {
             StringBuilder currentToken = new StringBuilder();
             int openedBracesCount = 0;
-            for (char ch : stringToTokenize.toCharArray()) {
+            for (final char ch : stringToTokenize.toCharArray()) {
                 // we finish token parsing only if it contains no unclosed braces
                 if ((ch == ',') && (openedBracesCount == 0)) {
                     tokens.add(currentToken.toString());
@@ -25,7 +25,10 @@ public class TokenUtils {
                             if (0 < openedBracesCount) {
                                 openedBracesCount--;
                             } else {
-                                throw new IllegalStateException("Unexpected closing brace");
+                                throw new IllegalArgumentException(String.format(
+                                    "Unexpected closing brace: %s",
+                                    stringToTokenize
+                                ));
                             }
                             break;
                         }
@@ -33,7 +36,7 @@ public class TokenUtils {
                 }
             }
             if (openedBracesCount != 0) {
-                throw new IllegalStateException("Unclosed opening brace");
+                throw new IllegalArgumentException(String.format("Unclosed opening brace: %s", stringToTokenize));
             }
             tokens.add(currentToken.toString());
         }
