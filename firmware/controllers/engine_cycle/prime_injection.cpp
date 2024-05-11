8c8d18e435349d632d58e35eee9878aebb87eb44
Matthew Kennedy <matthewkennedy@outlook.com>
2024-05-11 13:35:04 -0700

    avoid float -> long

--- firmware/controllers/engine_cycle/prime_injection.cpp
+++ firmware/controllers/engine_cycle/prime_injection.cpp
@@ -57,9 +57,9 @@ void PrimeController::onIgnitionStateChanged(bool ignitionOn) {
 
 	// start prime injection if this is a 'fresh start'
 	if (ignSwitchCounter == 0) {
-		auto primeDelayMs = engineConfiguration->primingDelay * 1000;
+		uint32_t primeDelayNt = MSF2NT(engineConfiguration->primingDelay * 1000);
 
-		auto startTime = getTimeNowNt() + (uint32_t)MS2NT(primeDelayMs);
+		auto startTime = getTimeNowNt() + primeDelayNt;
 		getExecutorInterface()->scheduleByTimestampNt("prime", nullptr, startTime, { PrimeController::onPrimeStartAdapter, this });
 	} else {
 		efiPrintf("Skipped priming pulse since ignSwitchCounter = %d", ignSwitchCounter);
@@ -96,7 +96,7 @@ void PrimeController::onPrimeStart() {
 
 	efiPrintf("Firing priming pulse of %.2f ms", durationMs);
 
-	auto endTime = getTimeNowNt() + (uint32_t)MS2NT(durationMs);
+	auto endTime = getTimeNowNt() + (uint32_t)MSF2NT(durationMs);
 
 	// Open all injectors, schedule closing later
 	m_isPriming = true;
