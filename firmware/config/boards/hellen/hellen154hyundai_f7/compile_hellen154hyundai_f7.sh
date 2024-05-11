8568dfdf263d7ceece71df6aba02a45ef8d6914c
Andrey Gusakov <dron0gus@gmail.com>
2024-05-11 22:44:40 +0300

    Hellen154Hynday f7: hm?

--- firmware/config/boards/hellen/hellen154hyundai_f7/compile_hellen154hyundai_f7.sh
+++ firmware/config/boards/hellen/hellen154hyundai_f7/compile_hellen154hyundai_f7.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-cd ../../../.. && bash bin/compile.sh config/boards/hellen/hellen154hyundai/meta-info-hellen154hyundai_f7.env
+cd ../../../.. && bash bin/compile.sh config/boards/hellen/hellen154hyundai_f7/meta-info-hellen154hyundai_f7.env
