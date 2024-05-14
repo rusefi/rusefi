2826dd3b4a8acd6dcc5dde45d39afabd9dd8394e
rusefillc <sdfsdfqsf2334234234>
2024-05-14 17:20:27 -0400

    only: BOARD_INDICATORS_FROM_FILE

--- firmware/gen_config_common.sh
+++ firmware/gen_config_common.sh
@@ -19,6 +19,7 @@ set_board_file BOARD_OPTIONS_FILE "${BOARD_DIR}/board_options.ini"
 set_board_file BOARD_MENU_FILE "${BOARD_DIR}/board_menu.ini"
 set_board_file BOARD_TABLES_FILE "${BOARD_DIR}/board_tables.ini"
 set_board_file BOARD_PANELS_FILE "${BOARD_DIR}/board_panels.ini"
+set_board_file BOARD_INDICATORS_FILE "${BOARD_DIR}/board_indicators.ini"
 
 COMMON_GEN_CONFIG="
  -readfile OUTPUTS_SECTION_FROM_FILE console/binary/generated/output_channels.ini \
@@ -31,6 +32,7 @@ COMMON_GEN_CONFIG="
  -readfile BOARD_MENU_FROM_FILE ${BOARD_MENU_FILE} \
  -readfile BOARD_TABLES_FROM_FILE ${BOARD_TABLES_FILE} \
  -readfile BOARD_PANELS_FROM_FILE ${BOARD_PANELS_FILE} \
+ -readfile BOARD_INDICATORS_FROM_FILE ${BOARD_INDICATORS_FILE} \
   -ts_destination tunerstudio \
  -triggerInputFolder ../unit_tests \
  -with_c_defines false \
