e388c7b34186ccb1380272274a9cb037a406dedc
kifir <kifir239@gmail.com>
2024-05-22 23:18:55 +0300

    add test_retard_threshold_rpm #6533

---
+++ unit_tests/tests/launch/test_retard_threshold_rpm.cpp
@@ -0,0 +1,57 @@
+//
+// Created by kifir on 5/22/24.
+//
+
+#include "pch.h"
+
+constexpr int TEST_LAUNCH_RPM = 2500;
+constexpr int TEST_LAUNCH_RPM_WINDOW = 1000;
+
+/* Tests for #6533: */
+
+static void setUpTestParameters() {
+    engineConfiguration->launchControlEnabled = true;
+    engineConfiguration->launchRpm = TEST_LAUNCH_RPM;
+    engineConfiguration->launchRpmWindow = TEST_LAUNCH_RPM_WINDOW;
+    engineConfiguration->launchSparkCutEnable = true;
+
+    engineConfiguration->launchActivationMode = ALWAYS_ACTIVE_LAUNCH; // to satisfy activateSwitchCondition
+    engineConfiguration->launchSpeedThreshold = 0; // to satisfy speedCondition
+    Sensor::setMockValue(SensorType::DriverThrottleIntent, 1.7); // to satisfy tpsCondition
+}
+
+static void updateRpm(const int rpm) {
+    Sensor::setMockValue(SensorType::Rpm, rpm);
+    engine->launchController.update();
+
+    //isn't retardThresholdRpm redundant? Shold we use engineConfiguration->launchRpm instead?
+    EXPECT_EQ(engine->launchController.retardThresholdRpm, engineConfiguration->launchRpm);
+}
+
+static void testRetardThresholdRpm() {
+    updateRpm(TEST_LAUNCH_RPM);
+    EXPECT_TRUE(engine->launchController.isLaunchCondition);
+    EXPECT_FALSE(engine->launchController.isLaunchSparkRpmRetardCondition());
+
+    updateRpm(TEST_LAUNCH_RPM + 1);
+    EXPECT_TRUE(engine->launchController.isLaunchCondition);
+    EXPECT_TRUE(engine->launchController.isLaunchSparkRpmRetardCondition());
+}
+
+TEST(rpmCondition, retardThresholdRpmWithDisabledLaunchRetard) {
+    EngineTestHelper eth(engine_type_e::TEST_ENGINE);
+    setUpTestParameters();
+
+    ASSERT_FALSE(engineConfiguration->enableLaunchRetard); // default value
+
+    testRetardThresholdRpm();
+}
+
+TEST(rpmCondition, retardThresholdRpmWithEnabledLaunchRetard) {
+    EngineTestHelper eth(engine_type_e::TEST_ENGINE);
+    setUpTestParameters();
+
+    engineConfiguration->enableLaunchRetard = true;
+
+    testRetardThresholdRpm();
+}
