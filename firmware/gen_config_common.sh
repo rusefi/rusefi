eff8d85d21f3e6bf46ce7882f886f7b9eb888d63
rusefillc <sdfsdfqsf2334234234>
2024-05-22 09:32:00 -0400

    only: COMMANDS_FROM_FILE

--- firmware/gen_config_common.sh
+++ firmware/gen_config_common.sh
@@ -26,6 +26,7 @@ set_board_file BOARD_PC_VARIABLES_FROM_FILE "${BOARD_DIR}/board_pc_variables.ini
 set_board_file BOARD_INDICATORS_FILE "${BOARD_DIR}/board_indicators.ini"
 set_board_file BOARD_GAUGES_FILE "${BOARD_DIR}/board_gauges.ini"
 set_board_file BOARD_CONSTANTS_EXTENSIONS_FILE "${BOARD_DIR}/board_constants_extensions.ini"
+set_board_file BOARD_COMMANDS_FILE "${BOARD_DIR}/board_commands.ini"
 set_board_file BOARD_DIAG_PANEL1_FILE "${BOARD_DIR}/board_diag_panel1.ini"
 set_board_file BOARD_DIAG_PANEL2_FILE "${BOARD_DIR}/board_diag_panel2.ini"
 
@@ -49,6 +50,7 @@ COMMON_GEN_CONFIG="
  -readfile BOARD_PC_VARIABLES_FROM_FILE ${BOARD_PC_VARIABLES_FROM_FILE} \
  -readfile BOARD_INDICATORS_FROM_FILE ${BOARD_INDICATORS_FILE} \
  -readfile BOARD_GAUGES_FROM_FILE ${BOARD_GAUGES_FILE} \
+ -readfile COMMANDS_FROM_FILE ${BOARD_COMMANDS_FILE} \
   -ts_destination tunerstudio \
  -triggerInputFolder ../unit_tests \
  -with_c_defines false \
