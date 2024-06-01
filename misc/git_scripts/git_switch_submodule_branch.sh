4dfd2a371e3ecbe2abd3d86ad10a820147c5982c
Andrey <rusefillc@gmail.com>
2024-06-01 14:21:33 -0400

    only:more logging

--- misc/git_scripts/git_switch_submodule_branch.sh
+++ misc/git_scripts/git_switch_submodule_branch.sh
@@ -8,10 +8,15 @@ if [[ -z "$SUBMODULE" || -z "$BRANCH" ]]; then
   exit -1
 fi
 
-echo "Running with SUBMODULE=[$SUBMODULE] and BRANCH=[$BRANCH]"
+echo "Switching SUBMODULE=[$SUBMODULE] to BRANCH=[$BRANCH]"
 git submodule set-branch -b $BRANCH $SUBMODULE
 git submodule sync
+
+git status
 
 git add $SUBMODULE
 git add .gitmodules
 git commit -am "GHA says switch $SUBMODULE branch to $BRANCH"
+
+echo Done, plaese take of push!
+git status
