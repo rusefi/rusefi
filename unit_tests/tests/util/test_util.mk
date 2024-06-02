577620e4787d9e0e01e00c57dde5d40569a8c57b
Andrey <rusefillc@gmail.com>
2024-06-02 12:29:41 -0400

    only:making things less weird

--- unit_tests/tests/util/test_util.mk
+++ unit_tests/tests/util/test_util.mk
@@ -4,7 +4,9 @@ CPPSRC += 	$(PROJECT_DIR)/../unit_tests/tests/util/test_buffered_writer.cpp \
 	$(PROJECT_DIR)/../unit_tests/tests/util/test_error_accumulator.cpp \
 	$(PROJECT_DIR)/../unit_tests/tests/util/test_exp_average.cpp \
 	$(PROJECT_DIR)/../unit_tests/tests/util/test_honda_crc.cpp \
+	$(PROJECT_DIR)/../unit_tests/tests/util/test_closed_loop_controller.cpp \
+	$(PROJECT_DIR)/../unit_tests/tests/util/test_scaled_channel.cpp \
+	$(PROJECT_DIR)/../unit_tests/tests/util/test_timer.cpp \
 	$(PROJECT_DIR)/../unit_tests/tests/util/test_hash.cpp \
-	
-INCDIR += $(PROJECT_DIR)/controllers/system	
-	
+
+INCDIR += $(PROJECT_DIR)/controllers/system
