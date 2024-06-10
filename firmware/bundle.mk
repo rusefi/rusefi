441d6c94477d58228bdcf99257bba9f3cf516351
Andrey <rusefillc@gmail.com>
2024-06-09 21:48:19 -0400

    custom boards build both linux and windows simulators always with full compilation #6585 only:f407-discovery

--- firmware/bundle.mk
+++ firmware/bundle.mk
@@ -122,8 +122,13 @@ BUNDLE_FILES = \
 $(SIMULATOR_EXE): $(CONFIG_FILES) .FORCE
 	$(MAKE) -C ../simulator -r OS="Windows_NT" SUBMAKE=yes
 
+# make sure not to invoke in parallel with SIMULATOR_EXE rule above
+../simulator/build/rusefi_simulator.linux: $(CONFIG_FILES) .FORCE
+	$(MAKE) -C ../simulator -r OS="Linux" SUBMAKE=yes
+
 # make Windows simulator a prerequisite so that we don't try compiling them concurrently
-../simulator/build/rusefi_simulator: $(CONFIG_FILES) .FORCE | $(SIMULATOR_EXE)
+# that also means no incremental compilation making that rule less useful. See 'rusefi_simulator.linux' above
+../simulator/build/rusefi_simulator.both: $(CONFIG_FILES) .FORCE | $(SIMULATOR_EXE)
 	$(MAKE) -C ../simulator -r OS="Linux" SUBMAKE=yes
 
 $(BOOTLOADER_HEX) $(BOOTLOADER_BIN): .bootloader-sentinel ;
