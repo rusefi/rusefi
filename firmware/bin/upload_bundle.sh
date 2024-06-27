22e864019ab7d5347385f08e85a961b98cb43f98
kifir <kifir239@gmail.com>
2024-06-27 19:06:34 +0300

    implement `Upload Tagged Daily Release Bundle` step

--- firmware/bin/upload_bundle.sh
+++ firmware/bin/upload_bundle.sh
@@ -4,6 +4,7 @@ USER=$1
 PASS=$2
 HOST=$3
 BUNDLE_FILE_NAME=$4
+RELEASE_TAG=$5
 
 SCRIPT_NAME=$(basename "$0")
 
@@ -23,6 +24,10 @@ if [ -n "${USER}" -a -n "$PASS" -a -n "${HOST}" ]; then
    echo "$SCRIPT_NAME: bundle_upload_folder env variable was not specified using default ${bundle_upload_folder}"
  fi
 
+ if [ -n "${RELEASE_TAG}" ]; then
+   echo "$SCRIPT_NAME: RELEASE_TAG is ${RELEASE_TAG}"
+ fi
+
  # technical debt: more than one file uses magic 'rusefi_bundle_' constant, can we extract constant?
  FULL_BUNDLE_FILE="rusefi_bundle_${BUNDLE_FILE_NAME}.zip"
  UPDATE_BUNDLE_FILE="rusefi_bundle_${BUNDLE_FILE_NAME}_autoupdate.zip"
@@ -33,11 +38,18 @@ mkdir ${bundle_upload_folder}
 SSHCMD
 
  RET=0
- if [ "$AUTOMATION_LTS" == "true" -a -n "$AUTOMATION_REF" ]; then
+ if [ "$AUTOMATION_LTS" == "true" -a -n "$AUTOMATION_REF" ]; then # lts build
    DESTINATION_FOLDER="${bundle_upload_folder}/lts/${AUTOMATION_REF}"
      # sftp does not support -p flag on mkdir :(
      sshpass -p $PASS sftp -o StrictHostKeyChecking=no ${USER}@${HOST} <<SSHCMD
 mkdir ${bundle_upload_folder}/lts
+mkdir ${DESTINATION_FOLDER}
+SSHCMD
+ elif [ -n "${RELEASE_TAG}" ]; then # daily release with tag
+    DESTINATION_FOLDER="${bundle_upload_folder}/${SHORT_BOARD_NAME}/${RELEASE_TAG}"
+    # sftp does not support -p flag on mkdir :(
+    sshpass -p $PASS sftp -o StrictHostKeyChecking=no ${USER}@${HOST} <<SSHCMD
+mkdir ${bundle_upload_folder}/${SHORT_BOARD_NAME}
 mkdir ${DESTINATION_FOLDER}
 SSHCMD
  else
