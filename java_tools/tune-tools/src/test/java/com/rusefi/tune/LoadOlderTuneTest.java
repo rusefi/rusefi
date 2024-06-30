888dd9cd769fd97a5415e92d39356bdc2c92b48f
Andrey Gusakov <dron0gus@gmail.com>
2024-07-01 00:18:17 +0300

    java_tools: heh?

--- java_tools/tune-tools/src/test/java/com/rusefi/tune/LoadOlderTuneTest.java
+++ java_tools/tune-tools/src/test/java/com/rusefi/tune/LoadOlderTuneTest.java
@@ -100,11 +100,9 @@ public class LoadOlderTuneTest {
             "    // default \"false\"\n" +
             "    engineConfiguration->isHip9011Enabled = true;\n" +
             "    // default 0.0\n" +
-            "    engineConfiguration->hip9011PrescalerAndSDO = 6;\n" +
+            "    engineConfiguration->hip9011Prescaler = 6;\n" +
             "    // default 20.0\n" +
             "    engineConfiguration->knockDetectionWindowStart = 35;\n" +
-            "    // default 60.0\n" +
-            "    engineConfiguration->knockDetectionWindowEnd = 135;\n" +
             "    // default 33.0\n" +
             "    engineConfiguration->auxPid[0].offset = 0;\n" +
             "    // default 2.0\n" +
