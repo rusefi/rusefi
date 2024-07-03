b6e578b4c28b313d6acc2e23b86fc17b36e9566f
rusefillc <sdfsdfqsf2334234234>
2024-07-03 13:34:55 -0400

    still very confused and FRONTEND_TITLE_BAR_NAME only:uaefi

--- firmware/console/binary/tunerstudio.cpp
+++ firmware/console/binary/tunerstudio.cpp
@@ -674,7 +674,7 @@ extern CommandHandler console_line_callback;
 // see also handleQueryCommand
 static void handleGetVersion(TsChannelBase* tsChannel) {
 	char versionBuffer[32];
-	chsnprintf(versionBuffer, sizeof(versionBuffer), "%s v%d@%u", "rusEFI", getRusEfiVersion(), SIGNATURE_HASH);
+	chsnprintf(versionBuffer, sizeof(versionBuffer), "%s v%d@%u", FRONTEND_TITLE_BAR_NAME, getRusEfiVersion(), SIGNATURE_HASH);
 	tsChannel->sendResponse(TS_CRC, (const uint8_t *) versionBuffer, strlen(versionBuffer) + 1);
 }
 
