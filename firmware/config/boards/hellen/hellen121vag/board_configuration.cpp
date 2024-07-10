d2d64d0368cc90af89c47ecad95476a8fb3b76ee
rusefillc <sdfsdfqsf2334234234>
2024-07-10 11:50:58 -0400

    only:hellen121vag default settings progress

--- firmware/config/boards/hellen/hellen121vag/board_configuration.cpp
+++ firmware/config/boards/hellen/hellen121vag/board_configuration.cpp
@@ -94,7 +94,7 @@ void setBoardDefaultConfiguration() {
 
 	engineConfiguration->enableSoftwareKnock = true;
 
-	engineConfiguration->fuelPumpPin = Gpio::H144_OUT_IO3;
+	engineConfiguration->fuelPumpPin = Gpio::MM176_GP13;
 	engineConfiguration->malfunctionIndicatorPin = Gpio::G4; // 47 - CEL
 	engineConfiguration->tachOutputPin = Gpio::H144_OUT_PWM7;
 
@@ -102,7 +102,7 @@ void setBoardDefaultConfiguration() {
     engineConfiguration->idle.solenoidPin = Gpio::H144_OUT_IO4;
 
 	engineConfiguration->fanPin = Gpio::H144_OUT_PWM8;
-	engineConfiguration->mainRelayPin = Gpio::H144_OUT_IO1;
+	engineConfiguration->mainRelayPin = Gpio::MM176_OUT_PWM17;
 
 //	engineConfiguration->injectorCompensationMode
 	engineConfiguration->fuelReferencePressure = 300;
