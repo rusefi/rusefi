5d62110a9b9c84d66a74be73221ade51f03903ce
kifir <kifir239@gmail.com>
2024-07-15 23:53:37 +0300

    rename Alternator.TestSetPoint into AlternatorVoltageTargetSetPointTest.HomogeneousAlternatorVoltageTargetTable #6523

--- unit_tests/tests/actuators/test_alternator_voltage_target_set_point.cpp
+++ unit_tests/tests/actuators/test_alternator_voltage_target_set_point.cpp
@@ -3,25 +3,25 @@
 //
 #include "pch.h"
 
-TEST(Alternator, TestSetPoint) {
-EngineTestHelper eth(engine_type_e::TEST_ENGINE);
+TEST(AlternatorVoltageTargetSetPointTest, HomogeneousAlternatorVoltageTargetTable) {
+    EngineTestHelper eth(engine_type_e::TEST_ENGINE);
 
-const float TEST_ALTERNATOR_VOLTAGE_TARGET = 14.2f;
-setTable(config->alternatorVoltageTargetTable, TEST_ALTERNATOR_VOLTAGE_TARGET);
-engineConfiguration->cranking.rpm = 500;
-engineConfiguration->isAlternatorControlEnabled = true;
+    const float TEST_ALTERNATOR_VOLTAGE_TARGET = 14.2f;
+    setTable(config->alternatorVoltageTargetTable, TEST_ALTERNATOR_VOLTAGE_TARGET);
+    engineConfiguration->cranking.rpm = 500;
+    engineConfiguration->isAlternatorControlEnabled = true;
 
-AlternatorController dut;
+    AlternatorController dut;
 
-Sensor::setMockValue(SensorType::Rpm, 500);
-// disabled if rpm <= cranking.rpm
-EXPECT_EQ(-1, dut.getSetpoint().value_or(-1));
+    Sensor::setMockValue(SensorType::Rpm, 500);
+    // disabled if rpm <= cranking.rpm
+    EXPECT_EQ(-1, dut.getSetpoint().value_or(-1));
 
-Sensor::setMockValue(SensorType::Rpm, 501);
-// enabled!
-EXPECT_EQ(TEST_ALTERNATOR_VOLTAGE_TARGET, dut.getSetpoint().value_or(-1));
+    Sensor::setMockValue(SensorType::Rpm, 501);
+    // enabled!
+    EXPECT_EQ(TEST_ALTERNATOR_VOLTAGE_TARGET, dut.getSetpoint().value_or(-1));
 
-engineConfiguration->isAlternatorControlEnabled = false;
-// disabled manually
-EXPECT_EQ(-1, dut.getSetpoint().value_or(-1));
+    engineConfiguration->isAlternatorControlEnabled = false;
+    // disabled manually
+    EXPECT_EQ(-1, dut.getSetpoint().value_or(-1));
 }
