d4a2304ec1e12026792ef86c0fd01be11ff27f3d
Andrey <rusefillc@gmail.com>
2024-07-15 23:00:39 -0400

    making pca_board_id reusable

--- firmware/config/boards/hellen/alphax-silver/board_configuration.cpp
+++ firmware/config/boards/hellen/alphax-silver/board_configuration.cpp
@@ -5,8 +5,7 @@
 #include "hellen_leds_100.cpp"
 #include "smart_gpio.h"
 #include "drivers/gpio/tle9104.h"
-
-#define PCA9555_I2C_ADDR	0x20
+#include "pca_board_id.h"
 
 static OutputPin alphaTempPullUp;
 
@@ -36,44 +35,6 @@ static void setupDefaultSensorInputs() {
 
 	engineConfiguration->clt.adcChannel = MM100_IN_CLT_ANALOG;
 	engineConfiguration->iat.adcChannel = MM100_IN_IAT_ANALOG;
-}
-
-
-static uint32_t getBoardRevision() {
-	static bool isFirstInvocation = true;
-	static uint32_t variant;
-
-	if (isFirstInvocation) {
-		BitbangI2c m_i2c;
-		const uint8_t set_out[3] = {0x06, 0xff, 0xff};
-		const uint8_t read_inputs_cmd[1] = {0x00};
-		uint8_t rx[2] = {0xff, 0xff};
-
-		//same pins as for LPS25
-		m_i2c.init(Gpio::B10, Gpio::B11);
-
-		// configuration registers:
-		// after reset all IO pins should be configured as output, so ttis step can be skipped
-		m_i2c.write(PCA9555_I2C_ADDR, set_out, sizeof(set_out));
-
-		// read registers 0 and 1: Input port registers
-		m_i2c.writeRead(PCA9555_I2C_ADDR, read_inputs_cmd, sizeof(read_inputs_cmd), rx, sizeof(rx));
-
-		variant = (rx[1] << 8) | (rx[0] << 0);
-
-		efiPrintf("Board variant 0x%04x\n", (unsigned int)variant);
-
-		isFirstInvocation = false;
-
-		// release gpios for LPS25 driver
-		m_i2c.deinit();
-	}
-
-	return variant;
-}
-
-int hackHellenBoardId(int /* detectedId */) {
-	return getBoardRevision();
 }
 
 void setBoardConfigOverrides() {
