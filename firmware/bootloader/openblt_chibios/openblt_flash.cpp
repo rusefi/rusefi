882fa63d6b4b39287110e8ade9b0a7824a923ad7
andreika-git <andrei.home@gmail.com>
2024-07-17 20:53:11 +0300

    Add CRC32 check to OpenBLT https://github.com/rusefi/rusefi/issues/6289

--- firmware/bootloader/openblt_chibios/openblt_flash.cpp
+++ firmware/bootloader/openblt_chibios/openblt_flash.cpp
@@ -52,7 +52,30 @@ blt_bool FlashWriteChecksum() {
 
 blt_bool FlashVerifyChecksum() {
 	// Naive check: if the first block is blank, there's no code there
-	return intFlashIsErased(FlashGetUserProgBaseAddress(), 4) ? BLT_FALSE : BLT_TRUE;
+	if (intFlashIsErased(FlashGetUserProgBaseAddress(), 4)) {
+		return BLT_FALSE;
+	}
+
+	static const size_t checksumOffset = 0x1C;
+
+	// Now do the actual CRC check to ensure we didn't get stuck with a half-written firmware image
+	uint8_t* start = reinterpret_cast<uint8_t*>(FlashGetUserProgBaseAddress());
+
+	size_t imageSize = *reinterpret_cast<size_t*>(start + checksumOffset + 4);
+
+	if (imageSize > 1024 * 1024) {
+		// impossibly large size, invalid
+		return BLT_FALSE;
+	}
+
+	// part before checksum+size
+	uint32_t calcChecksum = crc32(start, checksumOffset);
+	// part after checksum+size
+	calcChecksum = crc32inc(start + checksumOffset + 4, calcChecksum, imageSize - (checksumOffset + 4));
+
+	uint32_t storedChecksum = *reinterpret_cast<uint32_t*>(start + checksumOffset);
+
+	return calcChecksum == storedChecksum ? BLT_TRUE : BLT_FALSE;
 }
 
 blt_bool isFlashDualBank(void) {
