c03ff53d8b7a82f72eb7a655fcfa869d454212c7
rusefi <rusefillc@gmail.com>
2024-07-28 12:53:55 -0400

    only:hellen docs

--- firmware/config/boards/hellen_meta.h
+++ firmware/config/boards/hellen_meta.h
@@ -87,9 +87,9 @@ void detectHellenBoardType();
 #define H144_OUT_PWM8 D12
 
 
-// also IO1/IO5 on rev 0.6
+// also IO1/IO5 on non-mega rev 0.6
 #define H144_GP_IO1 D4
-// also IO2/IO5 on rev 0.6
+// also IO2/IO5 on non-mega rev 0.6
 #define H144_GP_IO2 D7
 #define H144_GP_IO3 G10
 #define H144_GP_IO4 G9
