cf3479d45d155b4d396c14c1eb5e32d434ae4f4b
Andrey <rusefillc@gmail.com>
2024-08-02 12:22:41 -0400

    only: thank you macos :(

--- firmware/controllers/algo/generated_lookup_engine_configuration.h
+++ firmware/controllers/algo/generated_lookup_engine_configuration.h
@@ -5,7 +5,8 @@
 #define META_ENGINE_GENERATED_NAME engine_configuration_generated_structures_
 #define META_ENGINE_GENERATED_EXT .h
 
-#if EFI_PROD_CODE || EFI_SIMULATOR
+// todo: sad technical debt: failing to define SHORT_BOARD_NAME for unit_tests and concatenate specifically for mac os?!
+#if defined(SHORT_BOARD_NAME)
 
 #define META_ENGINE_GENERATED_H_FILENAME QUOTE(META_ENGINE_GENERATED_NAME SHORT_BOARD_NAME META_ENGINE_GENERATED_EXT)
 #include META_ENGINE_GENERATED_H_FILENAME
--- firmware/controllers/algo/generated_lookup_meta.h
+++ firmware/controllers/algo/generated_lookup_meta.h
@@ -5,7 +5,8 @@
 #define META_GENERATED_NAME rusefi_generated_
 #define META_GENERATED_EXT .h
 
-#if EFI_PROD_CODE || EFI_SIMULATOR
+// todo: sad technical debt: failing to define SHORT_BOARD_NAME for unit_tests and concatenate specifically for mac os?!
+#if defined(SHORT_BOARD_NAME)
 
 #define META_GENERATED_H_FILENAME QUOTE(META_GENERATED_NAME SHORT_BOARD_NAME META_GENERATED_EXT)
 #include META_GENERATED_H_FILENAME
