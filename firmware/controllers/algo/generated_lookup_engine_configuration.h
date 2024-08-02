796a27bca6fd586b58cf59b7a469c0cd5c90ffca
Andrey <rusefillc@gmail.com>
2024-08-02 12:22:41 -0400

    only: reducing complexity

--- firmware/controllers/algo/generated_lookup_engine_configuration.h
+++ firmware/controllers/algo/generated_lookup_engine_configuration.h
@@ -5,13 +5,5 @@
 #define META_ENGINE_GENERATED_NAME engine_configuration_generated_structures_
 #define META_ENGINE_GENERATED_EXT .h
 
-#if EFI_PROD_CODE || EFI_SIMULATOR
-
 #define META_ENGINE_GENERATED_H_FILENAME QUOTE(META_ENGINE_GENERATED_NAME SHORT_BOARD_NAME META_ENGINE_GENERATED_EXT)
 #include META_ENGINE_GENERATED_H_FILENAME
-
-#else
-
-#include "engine_configuration_generated_structures_f407-discovery.h"
-
-#endif
