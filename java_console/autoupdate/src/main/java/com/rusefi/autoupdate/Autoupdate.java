487737c8ca799c1cd3d3f24d855e7a67145e7326
rusefillc <sdfsdfqsf2334234234>
2024-08-07 10:36:32 -0400

    bugfix:autoupdate was not working because of broken logging dependency

--- java_console/autoupdate/src/main/java/com/rusefi/autoupdate/Autoupdate.java
+++ java_console/autoupdate/src/main/java/com/rusefi/autoupdate/Autoupdate.java
@@ -28,7 +28,7 @@ import static com.rusefi.core.FindFileHelper.findSrecFile;
 
 public class Autoupdate {
     private static final Logging log = getLogging(Autoupdate.class);
-    private static final int VERSION = 20240804;
+    private static final int VERSION = 20240807;
 
     private static final String LOGO_PATH = "/com/rusefi/";
     private static final String LOGO = LOGO_PATH + "logo.png";
