9cc337fb601d88f000c914e61a4cf5ac2e43eef7
GitHub gen-configs Action <action@github.com>
2024-08-08 02:15:33 +0000

    Auto-generated configs and docs

--- firmware/config/boards/hellen/alphax-4K-GDI/connectors/generated_outputs.h
+++ firmware/config/boards/hellen/alphax-4K-GDI/connectors/generated_outputs.h
@@ -5,13 +5,21 @@
 #pragma once
 
 Gpio GENERATED_OUTPUTS = {
-	Gpio::H144_OUT_IO5, // 5B MAIN
+	Gpio::H144_OUT_IO1, // 1B Port injector 4
+	Gpio::H144_OUT_IO2, // 2B Port injector 3
+	Gpio::H144_OUT_IO3, // 3B Port injector 2
+	Gpio::H144_OUT_IO4, // 4B Port injector 1
+	Gpio::H144_OUT_IO5, // 5B Main Relay Control
 	Gpio::H144_OUT_IO6, // 7B Fuel Pump
 	Gpio::H144_IGN_4, // 11A Ignition 4
 	Gpio::H144_IGN_3, // 12A Ignition 3
 	Gpio::H144_IGN_2, // 13A Ignition 2
 	Gpio::H144_IGN_1, // 14A Ignition 1
-// "5B MAIN",
+// "1B Port injector 4",
+// "2B Port injector 3",
+// "3B Port injector 2",
+// "4B Port injector 1",
+// "5B Main Relay Control",
 // "7B Fuel Pump",
 // "11A Ignition 4",
 // "12A Ignition 3",
--- firmware/config/boards/hellen/alphax-4K-GDI/connectors/generated_ts_name_by_pin.cpp
+++ firmware/config/boards/hellen/alphax-4K-GDI/connectors/generated_ts_name_by_pin.cpp
@@ -7,12 +7,16 @@
 // see comments at declaration in pin_repository.h
 const char * getBoardSpecificPinName(brain_pin_e brainPin) {
 	switch(brainPin) {
+		case Gpio::A9: return "2B Port injector 3";
 		case Gpio::C13: return "14A Ignition 1";
-		case Gpio::D2: return "5B MAIN";
+		case Gpio::D2: return "5B Main Relay Control";
+		case Gpio::D3: return "1B Port injector 4";
 		case Gpio::E3: return "11A Ignition 4";
 		case Gpio::E4: return "12A Ignition 3";
 		case Gpio::E5: return "13A Ignition 2";
 		case Gpio::G11: return "7B Fuel Pump";
+		case Gpio::G14: return "3B Port injector 2";
+		case Gpio::G5: return "4B Port injector 1";
 		default: return nullptr;
 	}
 	return nullptr;
