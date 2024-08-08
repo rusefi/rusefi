ef5629c1af7e1f683016fc1e22591bebf04f8654
GitHub gen-configs Action <action@github.com>
2024-08-08 01:03:48 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_alphax-4k-gdi.h
+++ firmware/controllers/generated/signature_alphax-4k-gdi.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3716495720
-#define TS_SIGNATURE "rusEFI master.2024.08.07.alphax-4k-gdi.3716495720"
+#define SIGNATURE_HASH 1281134631
+#define TS_SIGNATURE "rusEFI master.2024.08.08.alphax-4k-gdi.1281134631"
