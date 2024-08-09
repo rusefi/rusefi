3ca0108a65a306d9d5bef94852c54d5ce9a780ea
rusefillc <sdfsdfqsf2334234234>
2024-08-09 11:26:28 -0400

    only:reducing method visibility

---
+++ java_console/ui/src/main/java/UiLinkManagerHelper.java
@@ -0,0 +1,16 @@
+import com.rusefi.io.IoStream;
+import com.rusefi.io.serial.BufferedSerialIoStream;
+import com.rusefi.io.tcp.TcpConnector;
+import com.rusefi.io.tcp.TcpIoStream;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+public class UiLinkManagerHelper {
+    @NotNull
+    public static IoStream open(String port) throws IOException {
+        if (TcpConnector.isTcpPort(port))
+            return TcpIoStream.open(port);
+        return BufferedSerialIoStream.openPort(port);
+    }
+}
