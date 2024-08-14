f5abe534b83e5342cf543936bede017fa479b611
GitHub gen-configs Action <action@github.com>
2024-08-14 00:29:13 +0000

    Auto-generated configs and docs

--- firmware/controllers/algo/auto_generated_enginetypes.h
+++ firmware/controllers/algo/auto_generated_enginetypes.h
@@ -1,6 +1,7 @@
 #include "engine_types.h"
 #pragma once
 const char *getBench_mode_e(bench_mode_e value);
+const char *getCommand_x14_e(command_x14_e value);
 const char *getDebug_mode_e(debug_mode_e value);
 #if __cplusplus
 const char *getEngine_type_e(engine_type_e value);
