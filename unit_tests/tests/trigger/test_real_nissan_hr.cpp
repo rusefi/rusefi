01fc8c915c35eb3fbe2787fa9860b9f11e9d4da7
Andrey <rusefillc@gmail.com>
2024-08-15 12:31:02 -0400

    Nissan HR crank trigger wheel #6773 only:extra test

--- unit_tests/tests/trigger/test_real_nissan_hr.cpp
+++ unit_tests/tests/trigger/test_real_nissan_hr.cpp
@@ -41,3 +41,24 @@ TEST(nissan, realNoSparkPlugsFromFile) {
 	ASSERT_EQ(215, round(Sensor::getOrZero(SensorType::Rpm)))<< reader.lineIndex();
 
 }
+
+
+TEST(nissan, realFromFile4seconds) {
+	CsvReader reader(1, /* vvtCount */ 0);
+
+	reader.open("tests/trigger/resources/nissan-HR12DDR-with-spark-plugs-4-seconds.csv");
+	EngineTestHelper eth(engine_type_e::TEST_ENGINE);
+//	engineConfiguration->isFasterEngineSpinUpEnabled = true;
+//	engineConfiguration->alwaysInstantRpm = true;
+
+	eth.setTriggerType(trigger_type_e::TT_NISSAN_HR);
+
+
+	while (reader.haveMore()) {
+		reader.processLine(&eth);
+//		printf("RPM=%f\n", Sensor::getOrZero(SensorType::Rpm));
+	}
+
+	ASSERT_EQ( 1, eth.recentWarnings()->getCount())<< "warningCounter#nissanRealCranking";
+	ASSERT_EQ(179, round(Sensor::getOrZero(SensorType::Rpm)))<< reader.lineIndex();
+}
