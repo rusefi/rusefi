8633d7ce969dfddba39c9f9da014eccbd708dc57
Matthew Kennedy <matthewkennedy@outlook.com>
2024-08-16 13:21:24 -0700

    dead config entry fix #467

--- firmware/controllers/algo/engine_configuration.cpp
+++ firmware/controllers/algo/engine_configuration.cpp
@@ -466,8 +466,6 @@ static void setDefaultEngineConfiguration() {
 	engineConfiguration->engineSnifferRpmThreshold = 2500;
 	engineConfiguration->sensorSnifferRpmThreshold = 2500;
 
-	engineConfiguration->noAccelAfterHardLimitPeriodSecs = 3;
-
 	/**
 	 * Idle control defaults
 	 */
