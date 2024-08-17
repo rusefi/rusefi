a7a9f9af89f949de24783fcdbb4a00f33bddbfff
GitHub gen-configs Action <action@github.com>
2024-08-17 01:08:12 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellen-nb1.h
+++ firmware/controllers/generated/signature_hellen-nb1.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2583706865
-#define TS_SIGNATURE "rusEFI master.2024.08.16.hellen-nb1.2583706865"
+#define SIGNATURE_HASH 2751909849
+#define TS_SIGNATURE "rusEFI master.2024.08.17.hellen-nb1.2751909849"
