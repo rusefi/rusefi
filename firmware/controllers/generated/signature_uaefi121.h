4d4c0b59f8cee89ecc13e4bdd78c07c79c1cc403
GitHub gen-configs Action <action@github.com>
2024-08-26 14:50:37 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_uaefi121.h
+++ firmware/controllers/generated/signature_uaefi121.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2382634422
-#define TS_SIGNATURE "rusEFI master.2024.08.26.uaefi121.2382634422"
+#define SIGNATURE_HASH 2400634468
+#define TS_SIGNATURE "rusEFI master.2024.08.26.uaefi121.2400634468"
