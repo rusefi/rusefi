1e9afec4dbff2849b6d3daecb7662a2133395621
rusefillc <sdfsdfqsf2334234234>
2024-09-02 10:23:37 -0400

    local compilation should not dirty clone for not reason #6879 only:read header from flexible location

--- firmware/gen_live_documentation.sh
+++ firmware/gen_live_documentation.sh
@@ -18,7 +18,11 @@ java -DSystemOut.name=logs/gen_live_documentation \
  "./${META_OUTPUT_ROOT_FOLDER}"
 [ $? -eq 0 ] || { echo "ERROR generating with LiveDataProcessor"; exit 1; }
 
-java -DSystemOut.name=logs/gen_java_enum -cp ../java_tools/enum_to_string/build/libs/enum_to_string-all.jar com.rusefi.ToJavaEnum -enumInputFile console/binary/generated/live_data_ids.h -outputPath ../java_console/io/src/main/java/com/rusefi/enums
+java -DSystemOut.name=logs/gen_java_enum -cp ../java_tools/enum_to_string/build/libs/enum_to_string-all.jar \
+ com.rusefi.ToJavaEnum -enumInputFile \
+ ${META_OUTPUT_ROOT_FOLDER}/console/binary/generated/live_data_ids.h \
+ -outputPath \
+ ../java_console/io/src/main/java/com/rusefi/enums
 [ $? -eq 0 ] || { echo "ERROR generating live data ids"; exit 1; }
 
 # live_data_ids.h is generated above so order of operations is important here!
