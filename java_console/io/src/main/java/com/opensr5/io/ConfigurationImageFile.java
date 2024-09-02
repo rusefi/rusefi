f7bc42c622087fe3e86acda607e86fcc601906de
kifir <kifir239@gmail.com>
2024-09-02 22:56:41 +0300

    now configuration image file as a .zip-file containing `Image.bin` file with binary image and .yaml file with meta #6863

--- java_console/io/src/main/java/com/opensr5/io/ConfigurationImageFile.java
+++ java_console/io/src/main/java/com/opensr5/io/ConfigurationImageFile.java
@@ -1,10 +1,19 @@
 package com.opensr5.io;
 
+import com.opensr5.ConfigurationImageMeta;
+import com.opensr5.ConfigurationImageMetaVersion0_0;
 import com.opensr5.ConfigurationImage;
+import com.opensr5.ConfigurationImageWithMeta;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.*;
-import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Optional;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Utility class to read/write {@link ConfigurationImage} into a file
@@ -13,25 +22,64 @@ import java.util.Arrays;
  * 6/20/2015.
  */
 public class ConfigurationImageFile {
+    private static final Log log = LogFactory.getLog(ConfigurationImageFile.class);
+
+    private static final String IMAGE_ZIP_ENTRY_NAME = "Image.bin";
+
     private ConfigurationImageFile() {
     }
 
-    public static ConfigurationImage readFromFile(String fileName) throws IOException {
-        File file = new File(fileName);
-        int size = (int) file.length();
-        FileInputStream fis = new FileInputStream(fileName);
-
-        return readFromStream(size, fis);
+    public static ConfigurationImageWithMeta readFromFile(final String fileName) throws IOException {
+        try (final ZipFile zipFile = new ZipFile(fileName)) {
+            Optional<ConfigurationImageMeta> meta = Optional.empty();
+            Enumeration<? extends ZipEntry> zipEntries = zipFile.entries();
+            while (zipEntries.hasMoreElements()) {
+                final ZipEntry zipEntry = zipEntries.nextElement();
+                final String zipEntryName = zipEntry.getName();
+                if (ConfigurationImageMeta.getZipEntryName(ConfigurationImageMetaVersion0_0.class).equals(zipEntryName)) {
+                    if (!meta.isPresent()) {
+                        final ConfigurationImageMetaVersion0_0 metaVersion0_0 = ConfigurationImageMetaYamlUtil.loadMeta(
+                            ConfigurationImageMetaVersion0_0.class,
+                            zipFile.getInputStream(zipEntry)
+                        );
+                        meta = Optional.of(metaVersion0_0);
+                    }
+                } else if (IMAGE_ZIP_ENTRY_NAME.equals(zipEntryName)) {
+                    // just skip
+                } else {
+                    log.warn(String.format("Unexpected entry: `%s` in file `%s`", zipEntryName, fileName));
+                }
+            }
+            if (meta.isPresent()) {
+                final ConfigurationImageMeta foundMeta = meta.get();
+                final ZipEntry calibrationsZipEntry = zipFile.getEntry(IMAGE_ZIP_ENTRY_NAME);
+                try (final InputStream calibrationsInputStream = zipFile.getInputStream(calibrationsZipEntry)) {
+                    final byte[] content = readFromStream(foundMeta.getImageSize(), calibrationsInputStream);
+                    if (content != null) {
+                        return new ConfigurationImageWithMeta(foundMeta, content);
+                    } else {
+                        log.warn(String.format("Loading content from file `%s` failed", fileName));
+                    }
+                }
+            } else {
+                log.warn(String.format("Meta is not found in file `%s`", fileName));
+            }
+            return null;
+        }
     }
 
-    @Nullable
-    private static ConfigurationImage readFromStream(int size, FileInputStream fis) throws IOException {
-       final ConfigurationImage image = new ConfigurationImage(size);
-        final int result = fis.read(image.getContent());
-        return result == image.getContent().length ? image : null;
+    private static byte @Nullable [] readFromStream(final int size, final InputStream fis) throws IOException {
+        final byte[] image = new byte[size];
+        final int result = fis.read(image);
+        if (result == image.length) {
+            log.warn(String.format("%d bytes are read instead of expected %d", result, size));
+            return image;
+        } else {
+            return null;
+        }
     }
 
-    public static byte[] getFileContent(ConfigurationImage configurationImage) {
+    private static byte[] getFileContent(ConfigurationImage configurationImage) {
         try {
             try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
                 baos.write(configurationImage.getContent());
@@ -42,11 +90,29 @@ public class ConfigurationImageFile {
         }
     }
 
-    public static void saveToFile(ConfigurationImage configurationImage, String fileName) throws IOException {
+    public static void saveToFile(
+        final ConfigurationImageWithMeta configurationImage,
+        final String fileName
+    ) throws IOException {
         System.out.printf("Saving " + configurationImage.getSize() + " bytes of configuration into " + fileName);
-        FileOutputStream fos = new FileOutputStream(fileName);
-        fos.write(getFileContent(configurationImage));
-        fos.close();
-        System.out.println("Saved to " + fileName);
+        final File outputFile = new File(fileName);
+        final byte[] calibrationsFileContent = getFileContent(configurationImage);
+        final int calibrationsFileSize = calibrationsFileContent.length;
+        try (
+            final FileOutputStream fos = new FileOutputStream(outputFile);
+            final ZipOutputStream zos = new ZipOutputStream(fos)
+        ) {
+            final ZipEntry calibrationsZipEntry = new ZipEntry(IMAGE_ZIP_ENTRY_NAME);
+            zos.putNextEntry(calibrationsZipEntry);
+            calibrationsZipEntry.setSize(calibrationsFileSize);
+            zos.write(calibrationsFileContent);
+            zos.closeEntry();
+
+            final ConfigurationImageMeta meta = configurationImage.getMeta();
+            final ZipEntry metaZipEntry = new ZipEntry(meta.getZipEntryName());
+            zos.putNextEntry(metaZipEntry);
+            ConfigurationImageMetaYamlUtil.dump(meta, zos);
+            zos.closeEntry();
+        }
     }
 }
