f09e6f2faf7096094f4038e02e5e2403821aff8f
rusefillc <rusefillc@gmail.com>
2024-09-15 13:09:32 -0400

    MAP sync improvements #6920 only:naming

--- unit_tests/tests/trigger/test_map_cam.cpp
+++ unit_tests/tests/trigger/test_map_cam.cpp
@@ -24,7 +24,7 @@ TEST(trigger, map_cam_by_magic_point) {
 	eth.smartFireTriggerEvents2(/*count*/10, /*delayMs*/200);
 	ASSERT_EQ(150,  Sensor::getOrZero(SensorType::Rpm)) << "RPM";
 
-	ASSERT_EQ(1, engine->triggerCentral.mapVvt_map_peak);
+	ASSERT_EQ(1, engine->triggerCentral.mapVvt_sync_counter);
 	ASSERT_EQ(0, engine->triggerCentral.triggerState.camResyncCounter);
 
 	// Nothing should have been scheduled yet
@@ -35,7 +35,7 @@ TEST(trigger, map_cam_by_magic_point) {
 	engine->outputChannels.instantMAPValue = 120;
 	eth.smartFireTriggerEvents2(/*count*/4, /*delayMs*/200);
 
-	ASSERT_EQ(2, engine->triggerCentral.mapVvt_map_peak);
+	ASSERT_EQ(2, engine->triggerCentral.mapVvt_sync_counter);
 	ASSERT_EQ(1, engine->triggerCentral.triggerState.camResyncCounter);
 	ASSERT_EQ(10, engine->triggerCentral.mapVvt_MAP_AT_CYCLE_COUNT);
 
