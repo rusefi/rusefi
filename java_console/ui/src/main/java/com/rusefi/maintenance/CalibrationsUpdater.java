9693d27979a93557440c6260eb3c7c23e356f4d6
kifir <kifir239@gmail.com>
2024-09-18 00:09:37 +0300

    Add `Update Calibrations` button #6864
    (cherry picked from commit 6fd13d446a1a965efebfe281e9c2de01b3ce50ce)

---
+++ java_console/ui/src/main/java/com/rusefi/maintenance/CalibrationsUpdater.java
@@ -0,0 +1,86 @@
+package com.rusefi.maintenance;
+
+import com.devexperts.logging.Logging;
+import com.opensr5.ConfigurationImage;
+import com.rusefi.io.LinkManager;
+import com.rusefi.io.UpdateOperationCallbacks;
+
+import javax.swing.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static com.devexperts.logging.Logging.getLogging;
+
+public enum CalibrationsUpdater {
+    INSTANCE;
+
+    private static final Logging log = getLogging(CalibrationsUpdater.class);
+
+    private volatile ConfigurationImage calibrationsToUpload = null;
+
+    public void setCalibrationsToUpload(final ConfigurationImage calibrations) {
+        calibrationsToUpload = calibrations;
+    }
+
+    public synchronized void updateCalibrations(
+        final String port,
+        final UpdateOperationCallbacks callbacks
+    ) {
+        final ConfigurationImage calibrationsImage = calibrationsToUpload;
+        if (calibrationsImage != null) {
+            final int calibrationsImageSize = calibrationsImage.getSize();
+            try (LinkManager linkManager = new LinkManager()
+                .setNeedPullText(false)
+                .setNeedPullLiveData(true)) {
+
+                callbacks.logLine(String.format("Connecting to port %s...", port));
+                try {
+                    linkManager.connect(port).await(60, TimeUnit.SECONDS);
+                } catch (final InterruptedException e) {
+                    final String errorMsg = String.format("Failed to connect to port %s", port);
+                    log.error(errorMsg, e);
+                    callbacks.logLine(errorMsg);
+                    callbacks.error();
+                    return;
+                }
+                callbacks.logLine(String.format(
+                    "Updating configuration image (%d bytes) to port %s...",
+                    calibrationsImageSize,
+                    port
+                ));
+                final CountDownLatch latch = new CountDownLatch(1);
+                linkManager.execute(() -> {
+                    linkManager.getBinaryProtocol().uploadChanges(calibrationsImage);
+                    latch.countDown();
+                });
+                try {
+                    if (!latch.await(1, TimeUnit.MINUTES)) {
+                        callbacks.logLine(String.format(
+                            "Failed to update configuration image (%d bytes) to port %s in a minute",
+                            calibrationsImageSize,
+                            port
+                        ));
+                        callbacks.error();
+                    } else {
+                        callbacks.logLine(String.format(
+                            "Configuration image (%d bytes) has been uploaded to port %s",
+                            calibrationsImageSize,
+                            port
+                        ));
+                        callbacks.done();
+                    }
+                } catch (final InterruptedException e) {
+                    final String errorMsg = String.format(
+                        "Updating calibrations to port %s was interrupted",
+                        port
+                    );
+                    log.error(errorMsg, e);
+                    callbacks.logLine(errorMsg);
+                    callbacks.error();
+                }
+            }
+        } else {
+            callbacks.logLine("ERROR: Calibrations to update are undefined");
+        }
+    }
+}
