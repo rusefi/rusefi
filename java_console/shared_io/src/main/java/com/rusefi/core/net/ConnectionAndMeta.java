839b64878368aaf5b4252d4c6b58382714c56b96
Andrey <rusefillc@gmail.com>
2024-09-18 11:42:47 -0400

    Autoupdate should get the default white label from its jar filename. #6838 only:explicit handling of less expected

--- java_console/shared_io/src/main/java/com/rusefi/core/net/ConnectionAndMeta.java
+++ java_console/shared_io/src/main/java/com/rusefi/core/net/ConnectionAndMeta.java
@@ -43,23 +43,28 @@ public class ConnectionAndMeta {
     }
 
     public static String getJarFileNamePrefix() {
-        try {
-            CodeSource codeSource = ConnectionAndMeta.class.getProtectionDomain().getCodeSource();
-            if (codeSource != null) {
-                URL jarUrl = codeSource.getLocation();
-                String jarPath = jarUrl.getPath();
-                String jarFileName = jarPath.substring(jarPath.lastIndexOf('/') + 1);
-                int startOfSuffix = jarFileName.lastIndexOf('_');
-                if (startOfSuffix == -1)
-                    throw new IllegalStateException("Unexpected jar file name: " + jarFileName);
-                String jarFilePrefix = jarFileName.substring(0, startOfSuffix);
-                log.info("jarFilePrefix = " + jarFilePrefix);
-                return jarFilePrefix;
-            }
-        } catch (Exception e) {
-            log.error("Unexpected " + e, e);
-        }
-        return "rusefi";
+        CodeSource codeSource = ConnectionAndMeta.class.getProtectionDomain().getCodeSource();
+        if (codeSource == null)
+            throw new IllegalStateException("No codeSource");
+//        try {
+            String jarFileName = getJarFileName(codeSource);
+            int startOfSuffix = jarFileName.lastIndexOf('_');
+            if (startOfSuffix == -1)
+                throw new IllegalStateException("Unexpected jar file name: " + jarFileName);
+            String jarFilePrefix = jarFileName.substring(0, startOfSuffix);
+            log.info("jarFilePrefix = " + jarFilePrefix);
+            return jarFilePrefix;
+//        } catch (NullPointerException e) {
+//            log.error("Unexpected " + e, e);
+//        }
+//        return "rusefi";
+    }
+
+    private static @NotNull String getJarFileName(CodeSource codeSource) {
+        URL jarUrl = codeSource.getLocation();
+        String jarPath = jarUrl.getPath();
+        String jarFileName = jarPath.substring(jarPath.lastIndexOf('/') + 1);
+        return jarFileName;
     }
 
     public static String getWhiteLabel(Properties properties) {
