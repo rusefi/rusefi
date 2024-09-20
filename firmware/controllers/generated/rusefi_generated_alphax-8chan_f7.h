bae2527d09a10191f40ecd742d8e23d5ccc4fa2e
GitHub gen-configs Action <action@github.com>
2024-09-20 01:35:49 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-8chan_f7.h
+++ firmware/controllers/generated/rusefi_generated_alphax-8chan_f7.h
@@ -1304,7 +1304,7 @@
 #define show_Proteus_presets false
 #define show_test_presets false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 974802072
+#define SIGNATURE_HASH 1500583995
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1754,7 +1754,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2024.09.20.alphax-8chan_f7.974802072"
+#define TS_SIGNATURE "rusEFI master.2024.09.20.alphax-8chan_f7.1500583995"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- firmware/controllers/generated/signature_alphax-8chan_f7.h
+++ firmware/controllers/generated/signature_alphax-8chan_f7.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 974802072
-#define TS_SIGNATURE "rusEFI master.2024.09.20.alphax-8chan_f7.974802072"
+#define SIGNATURE_HASH 1500583995
+#define TS_SIGNATURE "rusEFI master.2024.09.20.alphax-8chan_f7.1500583995"
