e8245e5fad168a1a8a181600d925542e187d8f01
kifir <kifir239@gmail.com>
2024-10-02 17:00:39 +0300

    implement shift torque reduction trigger pin state monitoring #5608

--- firmware/controllers/algo/engine_configuration_defaults.h
+++ firmware/controllers/algo/engine_configuration_defaults.h
@@ -10,4 +10,14 @@ namespace engine_configuration_defaults {
     constexpr uint16_t MIN_AC_PRESSURE = 100;
     constexpr uint16_t MAX_AC_PRESSURE = 300;
     constexpr float AC_PRESSURE_ENABLE_HYST = 10.0f;
+
+    /* Shift Torque Reduction (Flat Shift): */
+    constexpr bool ENABLE_SHIFT_TORQUE_REDUCTION = false;
+    constexpr torqueReductionActivationMode_e TORQUE_REDUCTION_ACTIVATION_MODE = TORQUE_REDUCTION_BUTTON;
+    constexpr switch_input_pin_e TORQUE_REDUCTION_TRIGGER_PIN = Gpio::Unassigned;
+    constexpr bool TORQUE_REDUCTION_TRIGGER_PIN_INVERTED = false;
+
+    /* Launch Control: */
+    constexpr switch_input_pin_e LAUNCH_ACTIVATE_PIN = Gpio::Unassigned;
+    constexpr bool LAUNCH_ACTIVATE_PIN_INVERTED = false;
 }
--- firmware/controllers/algo/shift_torque_reduction_controller.h
+++ firmware/controllers/algo/shift_torque_reduction_controller.h
@@ -7,4 +7,9 @@
 #include "shift_torque_reduction_state_generated.h"
 
 class ShiftTorqueReductionController : public shift_torque_reduction_state_s {
+public:
+	void update();
+private:
+	void updateTriggerPinState();
+	void updateTriggerPinState(switch_input_pin_e pin, bool isPinInverted);
 };
