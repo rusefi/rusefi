df39f4435c5f589bb31f311b333d1e19a1f9e3b5
rusefillc <sdfsdfqsf2334234234>
2024-10-07 22:09:17 -0400

    only:canned config

--- java_tools/tune-tools/src/main/java/com/rusefi/tools/tune/TuneCanToolRunner.java
+++ java_tools/tune-tools/src/main/java/com/rusefi/tools/tune/TuneCanToolRunner.java
@@ -35,13 +35,13 @@ public class TuneCanToolRunner extends TuneCanTool {
     public static void runPopular() throws JAXBException, IOException {
         // while adding a line here make sure corresponding line is at rusEfiFunctionalTest.cpp
         // https://github.com/rusefi/rusefi/issues/4038
-        processREOtune(1621, engine_type_e.HONDA_OBD1, "Honda-OBD1", "");
-        processREOtune(985, engine_type_e.MAZDA_MIATA_NB2, "MazdaMiataNB2", "nb2");
-        processREOtune(1508, engine_type_e.HELLEN_154_HYUNDAI_COUPE_BK1, "COUPE-BK1", "coupleBK1");
-        processREOtune(1507, engine_type_e.HELLEN_154_HYUNDAI_COUPE_BK2, "COUPE-BK2", "coupleBK2");
-        processREOtune(1626, engine_type_e.HYUNDAI_PB, "PB", "pb");
-        processREOtune(1591, engine_type_e.BMW_M52, "M52", "");
-        processREOtune(1641, engine_type_e.HELLEN_121_NISSAN_6_CYL, "VQ", "");
-        processREOtune(1622, engine_type_e.MERCEDES_M111, "m111-alex", "");
+//        processREOtune(1621, engine_type_e.HONDA_OBD1, "Honda-OBD1", "");
+//        processREOtune(985, engine_type_e.MAZDA_MIATA_NB2, "MazdaMiataNB2", "nb2");
+//        processREOtune(1508, engine_type_e.HELLEN_154_HYUNDAI_COUPE_BK1, "COUPE-BK1", "coupleBK1");
+//        processREOtune(1507, engine_type_e.HELLEN_154_HYUNDAI_COUPE_BK2, "COUPE-BK2", "coupleBK2");
+//        processREOtune(1626, engine_type_e.HYUNDAI_PB, "PB", "pb");
+//        processREOtune(1591, engine_type_e.BMW_M52, "M52", "");
+//        processREOtune(1641, engine_type_e.HELLEN_121_NISSAN_6_CYL, "VQ", "");
+//        processREOtune(1622, engine_type_e.MERCEDES_M111, "m111-alex", "");
     }
 }
--- java_tools/tune-tools/src/main/java/com/rusefi/tools/tune/WriteSimulatorConfiguration.java
+++ java_tools/tune-tools/src/main/java/com/rusefi/tools/tune/WriteSimulatorConfiguration.java
@@ -40,26 +40,7 @@ public class WriteSimulatorConfiguration {
             } catch (Throwable e) {
                 throw new IllegalStateException("White default tune", e);
             }
-            for (engine_type_e type : new engine_type_e[]{
-                    // [CannedTunes] see 'rusEfiFunctionalTest.cpp' which exports default tunes into binary files for us
-                // [CannedTunes] TuneCanToolRunner for last third step
-                    engine_type_e.MERCEDES_M111,
-                    engine_type_e.BMW_M52,
-                    engine_type_e.MAZDA_MIATA_NA6,
-                    engine_type_e.MAZDA_MIATA_NA94,
-                    engine_type_e.MAZDA_MIATA_NA96,
-                    engine_type_e.MAZDA_MIATA_NB1,
-                    engine_type_e.MAZDA_MIATA_NB2,
-                    engine_type_e.HONDA_K,
-                    engine_type_e.HONDA_OBD1,
-                    engine_type_e.HELLEN_121_NISSAN_6_CYL,
-                    engine_type_e.HELLEN_154_HYUNDAI_COUPE_BK1,
-                    engine_type_e.HELLEN_154_HYUNDAI_COUPE_BK2,
-                    engine_type_e.HYUNDAI_PB,
-                    engine_type_e.MAVERICK_X3,
-                    engine_type_e.POLARIS_RZR,
-                    engine_type_e.HARLEY,
-            }) {
+            for (engine_type_e type : WriteSimulatorEngines.ENGINE_TYPE_ES) {
                 writeSpecificEngineType(iniFileName, type, ini);
             }
             TuneCanToolRunner.runPopular();
