1e444905cea0d722fec06cde0d6ed539b9c2c607
3er0.1ive <3er0.1ive@veeroute.com>
2024-10-22 13:55:57 +0300

    - knock spectrogram TS plugin implementation fix

--- firmware/controllers/sensors/impl/software_knock.h
+++ firmware/controllers/sensors/impl/software_knock.h
@@ -5,5 +5,12 @@
 
 #include "fft/fft.h"
 
+#define FFT_SIZE 1024
+
+struct SpectrogramData {
+	fft::complex_type fftBuffer[FFT_SIZE];
+	float window[FFT_SIZE];
+};
+
 void initSoftwareKnock();
 void knockSamplingCallback(uint8_t cylinderIndex, efitick_t nowNt);
