5a7f41bcd6a4716a0d725cd164bc542f6535150d
kifir <kifir239@gmail.com>
2024-11-21 13:41:46 +0200

    only:reordering: separate `Launch Control` configuration settings from `Shift Torque Reduction (Flat Shift)` settings

--- unit_tests/tests/shift_torque_reduction/shift_torque_reduction_test_base.h
+++ unit_tests/tests/shift_torque_reduction/shift_torque_reduction_test_base.h
@@ -8,14 +8,17 @@
 
 class ShiftTorqueReductionTestConfig {
 public:
+	// Launch Control
+    std::optional<switch_input_pin_e> getLaunchActivatePin() const { return m_launchActivatePin; }
+    std::optional<bool> getLaunchActivateInverted() const { return m_launchActivateInverted; }
+
+	// Shift Torque Reduction (Flat Shift)
     std::optional<bool> getTorqueReductionEnabled() const { return m_isTorqueReductionEnabled; }
     std::optional<torqueReductionActivationMode_e> getTorqueReductionActivationMode() const {
         return m_torqueReductionActivationMode;
     }
     std::optional<switch_input_pin_e> getTorqueReductionTriggerPin() const { return m_torqueReductionTriggerPin; }
     std::optional<bool> getTorqueReductionTriggerPinInverted() const { return m_torqueReductionTriggerPinInverted; }
-    std::optional<switch_input_pin_e> getLaunchActivatePin() const { return m_launchActivatePin; }
-    std::optional<bool> getLaunchActivateInverted() const { return m_launchActivateInverted; }
     std::optional<bool> getLimitTorqueReductionTime() const { return m_limitTorqueReductionTime; }
     std::optional<float> getTorqueReductionTime() const { return m_torqueReductionTime; }
     std::optional<float> getTorqueReductionArmingRpm() const { return m_torqueReductionArmingRpm; }
@@ -25,14 +28,18 @@ public:
 
     // We do not core about performance in tests, but we want to use builder-like style, so setters return new instance
     // of configuration:
+
+	// Launch Control
+    ShiftTorqueReductionTestConfig setLaunchActivatePin(std::optional<switch_input_pin_e> value);
+    ShiftTorqueReductionTestConfig setLaunchActivateInverted(std::optional<bool> value);
+
+	// Shift Torque Reduction (Flat Shift)
     ShiftTorqueReductionTestConfig setTorqueReductionEnabled(std::optional<bool> value);
     ShiftTorqueReductionTestConfig setTorqueReductionActivationMode(
         std::optional<torqueReductionActivationMode_e> value
     );
     ShiftTorqueReductionTestConfig setTorqueReductionTriggerPin(std::optional<switch_input_pin_e> value);
     ShiftTorqueReductionTestConfig setTorqueReductionTriggerPinInverted(std::optional<bool> value);
-    ShiftTorqueReductionTestConfig setLaunchActivatePin(std::optional<switch_input_pin_e> value);
-    ShiftTorqueReductionTestConfig setLaunchActivateInverted(std::optional<bool> value);
     ShiftTorqueReductionTestConfig setLimitTorqueReductionTime(std::optional<bool> value);
     ShiftTorqueReductionTestConfig setTorqueReductionTime(std::optional<float> value);
     ShiftTorqueReductionTestConfig setTorqueReductionArmingRpm(std::optional<float> value);
@@ -40,12 +47,15 @@ public:
     ShiftTorqueReductionTestConfig setTorqueReductionIgnitionCut(std::optional<int8_t> value);
     ShiftTorqueReductionTestConfig setTorqueReductionIgnitionRetard(std::optional<float> value);
 private:
+	// Launch Control
+    std::optional<switch_input_pin_e> m_launchActivatePin;
+    std::optional<bool> m_launchActivateInverted;
+
+	// Shift Torque Reduction (Flat Shift)
     std::optional<bool> m_isTorqueReductionEnabled;
     std::optional<torqueReductionActivationMode_e> m_torqueReductionActivationMode;
     std::optional<switch_input_pin_e> m_torqueReductionTriggerPin;
     std::optional<bool> m_torqueReductionTriggerPinInverted;
-    std::optional<switch_input_pin_e> m_launchActivatePin;
-    std::optional<bool> m_launchActivateInverted;
     std::optional<bool> m_limitTorqueReductionTime;
     std::optional<float> m_torqueReductionTime;
     std::optional<float> m_torqueReductionArmingRpm;
