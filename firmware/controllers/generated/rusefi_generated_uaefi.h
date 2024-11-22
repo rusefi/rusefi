df5ad0e316b833ae93ddd877dac1cd36e56f6647
GitHub gen-configs Action <action@github.com>
2024-11-22 22:35:07 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_uaefi.h
+++ firmware/controllers/generated/rusefi_generated_uaefi.h
@@ -1295,7 +1295,7 @@
 #define SentEtbType_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 1875034890
+#define SIGNATURE_HASH 2623350346
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1739,7 +1739,7 @@
 #define ts_show_top_level_can_menu true
 #define ts_show_tps2 true
 #define ts_show_tps2_error true
-#define ts_show_tps_sent false
+#define ts_show_tps_sent true
 #define ts_show_trigger_advanced true
 #define ts_show_trigger_comparator false
 #define ts_show_trigger_overrides true
@@ -1756,7 +1756,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2024.11.22.uaefi.1875034890"
+#define TS_SIGNATURE "rusEFI master.2024.11.22.uaefi.2623350346"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- firmware/controllers/generated/signature_uaefi.h
+++ firmware/controllers/generated/signature_uaefi.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1875034890
-#define TS_SIGNATURE "rusEFI master.2024.11.22.uaefi.1875034890"
+#define SIGNATURE_HASH 2623350346
+#define TS_SIGNATURE "rusEFI master.2024.11.22.uaefi.2623350346"
