eadfc40b856d1465fd6d5a6e9e9791e05f6e946f
GitHub gen-configs Action <action@github.com>
2024-11-23 04:58:58 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_alphax-2chan.h
+++ firmware/controllers/generated/signature_alphax-2chan.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2156797388
-#define TS_SIGNATURE "rusEFI master.2024.11.23.alphax-2chan.2156797388"
+#define SIGNATURE_HASH 3096246428
+#define TS_SIGNATURE "rusEFI master.2024.11.23.alphax-2chan.3096246428"
--- firmware/controllers/generated/signature_alphax-4chan.h
+++ firmware/controllers/generated/signature_alphax-4chan.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1347417897
-#define TS_SIGNATURE "rusEFI master.2024.11.23.alphax-4chan.1347417897"
+#define SIGNATURE_HASH 1749881465
+#define TS_SIGNATURE "rusEFI master.2024.11.23.alphax-4chan.1749881465"
