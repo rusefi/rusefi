821eff0ef0c6189c682746d813f644d774dafc13
FDSoftware <diego+gh@churrosoft.ar>
2024-12-08 01:48:11 +0000

    bare minimum nucleo F767 HW CI

--- firmware/config/engines/vw.cpp
+++ firmware/config/engines/vw.cpp
@@ -19,6 +19,12 @@ void setVwAba() {
 	setFrankensoConfiguration();
 	engineConfiguration->camInputs[0] = Gpio::E0; // a random unused pin needed for happy HW CI
 
+	engineConfiguration->tps1_1AdcChannel = EFI_ADC_32;
+	engineConfiguration->vbattAdcChannel = EFI_ADC_33;
+	engineConfiguration->clt.adcChannel = EFI_ADC_34;
+	engineConfiguration->iat.adcChannel = EFI_ADC_35;
+	engineConfiguration->afr.hwChannel = EFI_ADC_36;
+
 	setWholeTimingTable(20);
 	// set cranking_timing_angle 10
 	engineConfiguration->crankingTimingAngle = 10;
