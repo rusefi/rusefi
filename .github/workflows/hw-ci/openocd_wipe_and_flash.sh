dd66e7400e297de9712b48d405af51a578e43700
FDSoftware <diego+gh@churrosoft.ar>
2024-12-08 03:38:34 +0000

    remove nucleo hwci cfg, reuse the proteus f7

--- .github/workflows/hw-ci/openocd_wipe_and_flash.sh
+++ .github/workflows/hw-ci/openocd_wipe_and_flash.sh
@@ -18,8 +18,10 @@ set -e
 cd firmware
 
 # works on F4 with ST-LINK/V3 does not seem to work using V2 :(
-openocd -f "$HW_SCRIPT" -c init -c targets -c "reset halt" -c "flash erase_sector 0 0 last" -c "flash write_image "deliver/rusefi.bin" 0x08000000" -c "reset run" -c "shutdown"
+openocd -f "$HW_SCRIPT" -c init -c targets -c "reset halt" -c "flash erase_sector 0 0 last" -c "flash write_image "deliver/rusefi.bin" 0x08000000" -c "reset" -c "shutdown" || true
 
 if [ -z "$1" ]; then
 	rm $HW_SCRIPT
 fi
+
+exit 0
