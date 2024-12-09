536c025204c1def15e2e60bf3a4c60a82a62712c
GitHub gen-configs Action <action@github.com>
2024-12-09 12:57:15 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_hellenNA8_96.h
+++ firmware/controllers/generated/signature_hellenNA8_96.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 2562755544
-#define TS_SIGNATURE "rusEFI master.2024.12.08.hellenNA8_96.2562755544"
+#define TS_SIGNATURE "rusEFI master.2024.12.09.hellenNA8_96.2562755544"
--- firmware/controllers/generated/signature_subaru_eg33_f7.h
+++ firmware/controllers/generated/signature_subaru_eg33_f7.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 2896040616
-#define TS_SIGNATURE "rusEFI master.2024.12.08.subaru_eg33_f7.2896040616"
+#define TS_SIGNATURE "rusEFI master.2024.12.09.subaru_eg33_f7.2896040616"
