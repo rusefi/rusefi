d9ba41f8a824a7c3918761b42503049c43c9e8d0
Andrey Gusakov <dron0gus@gmail.com>
2024-12-12 18:55:03 +0300

    unit_tests: GH GCC ignores weak functions?

--- unit_tests/boards.cpp
+++ unit_tests/boards.cpp
@@ -8,3 +8,17 @@
 #include "pch.h"
 
 #include "boards.h"
+
+int adcGetRawValue(const char * /*msg*/, int /*hwChannel*/) {
+	return 0;
+}
+
+// voltage in MCU universe, from zero to VDD
+expected<float> adcGetRawVoltage(const char *msg, adc_channel_e hwChannel) {
+	return expected(0.0f);
+}
+
+// voltage in ECU universe, with all input dividers and OpAmps gains taken into account, voltage at ECU connector pin
+expected<float> adcGetScaledVoltage(const char *msg, adc_channel_e hwChannel) {
+	return expected(0.0f);
+}
