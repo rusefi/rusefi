40a39e3eb660f6ba670a5c94d841f21228bdc6f8
Andrey Gusakov <dron0gus@gmail.com>
2024-12-13 15:22:54 +0300

    lua: fix heap used accumulator calculation
    Do not increase if allocation failed

--- firmware/controllers/lua/lua.cpp
+++ firmware/controllers/lua/lua.cpp
@@ -81,7 +81,9 @@ public:
 		}
 
 		void *new_mem = alloc(nsize);
-		m_memoryUsed += nsize;
+		if (new_mem) {
+			m_memoryUsed += nsize;
+		}
 
 		if (!ptr) {
 			// No old pointer passed in, simply return allocated block
