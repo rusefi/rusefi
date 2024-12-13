8810aa19c24ba6e6bc2d394b44e50ff3b2672b56
GitHub gen-configs Action <action@github.com>
2024-12-13 22:38:33 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_alphax-silver.h
+++ firmware/controllers/generated/signature_alphax-silver.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2304022179
-#define TS_SIGNATURE "rusEFI master.2024.12.13.alphax-silver.2304022179"
+#define SIGNATURE_HASH 2452765336
+#define TS_SIGNATURE "rusEFI master.2024.12.13.alphax-silver.2452765336"
