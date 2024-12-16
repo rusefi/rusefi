3d9f347984c5672a9cba1f38adea3e5e8dbc3acd
kifir <kifir239@gmail.com>
2024-12-16 17:46:30 +0200

    only:refactoring: extract part of functionality into `TestLuaScriptExecutor::executeFormattedLuaScript` template method

--- unit_tests/tests/util/test_lua_script_executor.h
+++ unit_tests/tests/util/test_lua_script_executor.h
@@ -14,5 +14,17 @@ public:
 	void setSparkHardSkipRatio(float sparkSkipRatio);
 	void setFuelAdd(float fuelAdd);
 private:
+	template<typename... Args> void executeFormattedLuaScript(const char* luaScriptFormatString, Args... args);
+	void executeLuaScript(const char* luaScript);
+
 	static TestLuaScriptExecutor instance;
 };
+
+template<typename... Args>
+void TestLuaScriptExecutor::executeFormattedLuaScript(const char* const luaScriptFormatString, Args... args) {
+	char luaScript[256];
+	const int luaScriptLength = std::snprintf(luaScript, sizeof(luaScript), luaScriptFormatString, args...);
+	ASSERT_TRUE(0 <= luaScriptLength) << "Encoding error" << std::endl << luaScriptFormatString;
+	ASSERT_TRUE(luaScriptLength < sizeof(luaScript)) << "Insufficient buffer" << std::endl <<  luaScriptFormatString;
+	executeLuaScript(luaScript);
+}
