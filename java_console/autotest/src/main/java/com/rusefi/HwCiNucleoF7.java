efc96455f13d2eaa098a2eab3efc106ec38a28e6
FDSoftware <diego+gh@churrosoft.ar>
2024-12-17 00:53:16 +0000

    Add VSS HW test to F767 suite

--- java_console/autotest/src/main/java/com/rusefi/HwCiNucleoF7.java
+++ java_console/autotest/src/main/java/com/rusefi/HwCiNucleoF7.java
@@ -3,6 +3,7 @@ package com.rusefi;
 import com.rusefi.common.MiscTest;
 import com.rusefi.f4discovery.*;
 import com.rusefi.nucleo.NucleoPwmHardwareTest;
+import com.rusefi.nucleo.NucleoVssHardwareTest;
 
 public class HwCiNucleoF7 {
     public static void main(String[] args) {
@@ -11,6 +12,7 @@ public class HwCiNucleoF7 {
             CompositeLoggerTest.class,
             HighRevTest.class,
             NucleoPwmHardwareTest.class,
+			NucleoVssHardwareTest.class,
 //            MiscTest.class,
 //            BurnCommandTest.class,
 //            CommonFunctionalTest.class,
