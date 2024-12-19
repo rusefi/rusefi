5c4a110b777c7eae2a972957f65c689328142f7d
rusefillc <rusefillc@gmail.com>
2024-12-18 21:25:01 -0500

    only:better error message

--- java_console/inifile/src/main/java/com/opensr5/ini/RawIniFile.java
+++ java_console/inifile/src/main/java/com/opensr5/ini/RawIniFile.java
@@ -41,10 +41,10 @@ public class RawIniFile {
     }
 
     @NotNull
-    public Line getMandatoryLine(String key) {
+    public Line getMandatoryLine(String key) throws MandatoryLineMissing {
         Line result = getByKey(key);
         if (result == null)
-            throw new IllegalStateException("Line not found: " + key);
+            throw new MandatoryLineMissing("Line not found: " + key);
         assert result.tokens.length > 1;
         return result;
     }
@@ -72,7 +72,7 @@ public class RawIniFile {
         return getSimpleIntegerProperty(key);
     }
 
-    public List<String> getValues(String key) {
+    public List<String> getValues(String key) throws MandatoryLineMissing {
         RawIniFile.Line line = getMandatoryLine(key);
         return Arrays.asList(line.getTokens()).subList(1, line.getTokens().length);
     }
