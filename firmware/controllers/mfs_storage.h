993b6d04b914819693ca48e3437f10d1941f20bb
Andrey Gusakov <dron0gus@gmail.com>
2024-12-20 15:09:33 +0300

    storage and mfs_storage: introduce, move code
    storage.cpp/h is common implentation for saving/loading settings to
     some storage
    mfs_storage.cpp/h is MFS specific implementation
    
    MFS related code is moved out of flash_main.cpp. Actually we should
    get rid of this file when rework is finished.

---
+++ firmware/controllers/mfs_storage.h
@@ -0,0 +1,17 @@
+/**
+ * @file    mfs_storage.h
+ * @brief
+ *
+ * @date Jan 3, 2025
+ * @author Andrey Gusakov
+ */
+
+#pragma once
+
+#include "flash_main.h"
+
+StorageStatus mfsStorageWrite(int id, const uint8_t *ptr, size_t size);
+StorageStatus mfsStorageRead(int id, uint8_t *ptr, size_t size);
+StorageStatus mfsStorageFormat();
+
+void initStorageMfs();
