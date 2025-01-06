4bb19860892ffacab1233d40d5fc0d896d396efb
kifir <kifir239@gmail.com>
2025-01-06 20:34:13 +0200

    support bit logger fields #7227

---
+++ unit_tests/tests/binary_log/test_bit_logger_field.cpp
@@ -0,0 +1,59 @@
+//
+// Created by kifir on 1/6/25.
+//
+
+#include "pch.h"
+
+#include <array>
+
+#include "log_field.h"
+
+namespace {
+class TestBitLoggerField : public ::testing::Test {
+    protected:
+        void SetUp() override;
+
+        void checkBitLoggerField(bool expected, const char* context);
+        void updateTestBit(bool value);
+
+        std::unique_ptr<output_channels_s> m_testOutputChannels;
+        std::unique_ptr<LogField> m_logField;
+        std::array<char, 6> m_buffer;
+    };
+
+    void TestBitLoggerField::SetUp() {
+        m_testOutputChannels = std::make_unique<output_channels_s>();
+
+        const uint32_t testCoilStateBlockOffset = (reinterpret_cast<const char*>(&m_testOutputChannels->injectorState1)
+            - reinterpret_cast<const char*>(m_testOutputChannels.get())
+        ) - sizeof(uint32_t);
+        ASSERT_EQ(
+            testCoilStateBlockOffset,
+            664
+        ) << "`output_channels_generated.h` header was modified significantly. "
+             << "Please check that coil state bits block precedes `injectorState1` field.";
+        m_logField = std::make_unique<LogField>(*m_testOutputChannels, testCoilStateBlockOffset, 9, "coilState12", "");
+
+        m_buffer.fill(0xAA);
+        ASSERT_THAT(m_buffer, ::testing::ElementsAre(0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA));
+    }
+
+    void TestBitLoggerField::checkBitLoggerField(const bool expected, const char* const context) {
+        EXPECT_EQ(1, m_logField->writeData(m_buffer.data(), nullptr)) << context;
+        EXPECT_THAT(m_buffer, ::testing::ElementsAre((expected ? 0x01 : 0x00), 0xAA, 0xAA, 0xAA, 0xAA, 0xAA));
+    }
+
+    void TestBitLoggerField::updateTestBit(const bool value) {
+        m_testOutputChannels->coilState12 = value;
+    }
+
+    TEST_F(TestBitLoggerField, checkBitSwitching) {
+        checkBitLoggerField(false, "default");
+
+        updateTestBit(true);
+        checkBitLoggerField(true, "bit is on");
+
+        updateTestBit(false);
+        checkBitLoggerField(false, "bit is off");
+    }
+}
