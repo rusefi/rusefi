4090fe130b1ae3aa53415e2d5561b7d4568e3226
Matthew Kennedy <matthewkennedy@outlook.com>
2025-01-07 21:26:28 -0800

    compile coverage
    mac test build

--- unit_tests/unit_test_rules.mk
+++ unit_tests/unit_test_rules.mk
@@ -66,7 +66,7 @@ ifeq ($(USE_CPPOPT),)
   USE_CPPOPT = -std=gnu++2a -fno-rtti -fno-use-cxa-atexit
 endif
 
-USE_CPPOPT += $(RUSEFI_CPPOPT)
+USE_CPPOPT += $(RUSEFI_CPPOPT) -fPIC -fprofile-arcs -ftest-coverage
 
 # Enable address sanitizer for C++ files, but not on Windows since x86_64-w64-mingw32-g++ doesn't support it.
 # only c++ because lua does some things asan doesn't like, but don't actually cause overruns.
@@ -202,6 +202,10 @@ ULIBDIR =
 
 # List all user libraries here
 ULIBS = -lm
+
+ifneq ($(IS_MAC),yes)
+	ULIBS += -lgcov --coverage
+endif
 
 ifeq ($(COVERAGE),yes)
 	ULIBS += --coverage
