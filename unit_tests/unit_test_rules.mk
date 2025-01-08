e69c2718627db290a805023c7841b56e68d0927f
Matthew Kennedy <matthewkennedy@outlook.com>
2025-01-07 21:26:28 -0800

    compile coverage

--- unit_tests/unit_test_rules.mk
+++ unit_tests/unit_test_rules.mk
@@ -66,7 +66,7 @@ ifeq ($(USE_CPPOPT),)
   USE_CPPOPT = -std=gnu++2a -fno-rtti -fno-use-cxa-atexit
 endif
 
-USE_CPPOPT += $(RUSEFI_CPPOPT)
+USE_CPPOPT += $(RUSEFI_CPPOPT) -fPIC -fprofile-arcs -ftest-coverage
 
 # Enable address sanitizer for C++ files, but not on Windows since x86_64-w64-mingw32-g++ doesn't support it.
 # only c++ because lua does some things asan doesn't like, but don't actually cause overruns.
@@ -201,7 +201,7 @@ UINCDIR =
 ULIBDIR =
 
 # List all user libraries here
-ULIBS = -lm
+ULIBS = -lm -lgcov --coverage
 
 ifeq ($(COVERAGE),yes)
 	ULIBS += --coverage
