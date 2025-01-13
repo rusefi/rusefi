42c5b0b24211a83491f74fe10d78929b07f94fe9
rusefillc <rusefillc@gmail.com>
2025-01-13 17:36:52 -0500

    fixing build only:subaru_eg33_f7

--- firmware/hw_layer/mmc_card.cpp
+++ firmware/hw_layer/mmc_card.cpp
@@ -156,7 +156,7 @@ static void sdStatistics() {
 	efiPrintf("SD enabled=%s status=%s", boolToString(engineConfiguration->isSdCardEnabled),
 			sdStatus);
 	printSpiConfig("SD", mmcSpiDevice);
-#if defined(STM32F4XX) || defined(STM32F7XX)
+#if HAL_USE_MMC_SPI && (defined(STM32F4XX) || defined(STM32F7XX))
 	efiPrintf("HS clock %d Hz", spiGetBaseClock(mmccfg.spip) / (2 << ((mmc_hs_spicfg.cr1 & SPI_CR1_BR_Msk) >> SPI_CR1_BR_Pos)));
 	efiPrintf("LS clock %d Hz", spiGetBaseClock(mmccfg.spip) / (2 << ((mmc_ls_spicfg.cr1 & SPI_CR1_BR_Msk) >> SPI_CR1_BR_Pos)));
 #endif
