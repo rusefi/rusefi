87968f5fe8d4459b44e44808834a5409610b376b
Andrey Gusakov <dron0gus@gmail.com>
2025-01-16 11:39:53 +0300

    ext: FatFS: remove old

--- firmware/ext/FatFS/fatfs.mk
+++ firmware/ext/FatFS/fatfs.mk
@@ -2,7 +2,6 @@
 FATFSSRC = ${CHIBIOS}/os/various/fatfs_bindings/fatfs_diskio.c \
            ${CHIBIOS}/os/various/fatfs_bindings/fatfs_syscall.c \
            ${PROJECT_DIR}/ext/FatFS/ff.c \
-           ${PROJECT_DIR}/ext/FatFS/ffunicode.c \
-           ${PROJECT_DIR}/ext/FatFS/option/unicode.c
+           ${PROJECT_DIR}/ext/FatFS/ffunicode.c
 
 FATFSINC = ${PROJECT_DIR}/ext
