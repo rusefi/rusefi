9bb59f5e99062a5d094f5499f4ad9b0ab9342755
Matthew Kennedy <matthewkennedy@outlook.com>
2025-01-18 04:35:48 -0500

    firing order comment

--- firmware/controllers/algo/firing_order.h
+++ firmware/controllers/algo/firing_order.h
@@ -88,4 +88,9 @@ typedef enum __attribute__ ((__packed__)) {
 size_t getNextFiringCylinderId(size_t prevCylinderId);
 #endif // EFI_HIP_9011_LOGIC
 
+/**
+ * @param cylinderIndex Queried position in the firing order. 0 means the first cylinder to fire, 1 means second, etc. Maximum cylinderCount - 1.
+ * @return The cylinder number in the requested position, from 0 to cylindersCount - 1.
+ *         For example, getCylinderNumberAtIndex(2) means the 3rd cylinder to fire, and on a 1342 4-cyl will return 3, indicating cylinder 4.
+ */
 size_t getCylinderNumberAtIndex(size_t cylinderIndex);
