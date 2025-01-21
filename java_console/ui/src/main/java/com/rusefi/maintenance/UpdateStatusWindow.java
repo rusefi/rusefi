84ee8728e9f656399e5ba7ab393d4883784a4c29
kifir23917 <spamforkifir@gmail.com>
2025-01-21 11:42:35 +0300

    refactoring: get rid of redundant `UpdateStatusWindow` class - we do not need additional class to call `showFrame` method in constructor. Now this method is called by `StatusWindow.createAndShowFrame` factory method #7199

--- java_console/ui/src/main/java/com/rusefi/maintenance/UpdateStatusWindow.java
+++
@@ -1,9 +0,0 @@
-package com.rusefi.maintenance;
-
-import com.rusefi.ui.StatusWindow;
-
-public class UpdateStatusWindow extends StatusWindow {
-    public UpdateStatusWindow(String title) {
-        showFrame(title);
-    }
-}
