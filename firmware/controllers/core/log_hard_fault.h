e80d7533906e1ea3b381b74d26648e27d0bf545f
Andrey Gusakov <dron0gus@gmail.com>
2025-01-22 18:59:45 +0300

    error_handling: save few stack values for HardFault too

--- firmware/controllers/core/log_hard_fault.h
+++ firmware/controllers/core/log_hard_fault.h
@@ -9,7 +9,7 @@ extern "C"
 #if EFI_PROD_CODE
 #include <hal.h>
 
-void logHardFault(uint32_t type, uintptr_t faultAddress, port_extctx* ctx, uint32_t csfr);
+void logHardFault(uint32_t type, uintptr_t faultAddress, void* sp, port_extctx* ctx, uint32_t csfr);
 #endif // EFI_PROD_CODE
 
 #ifdef __cplusplus
