7526250d1937189b75a3121064b95d76482f0961
Andrey Gusakov <dron0gus@gmail.com>
2025-01-23 18:31:06 +0300

    FatFS: enable FF_USE_FIND

--- firmware/ext/FatFS/ffconf.h
+++ firmware/ext/FatFS/ffconf.h
@@ -30,7 +30,7 @@
 /   3: f_lseek() function is removed in addition to 2. */
 
 
-#define FF_USE_FIND		0
+#define FF_USE_FIND		2
 /* This option switches filtered directory read functions, f_findfirst() and
 /  f_findnext(). (0:Disable, 1:Enable 2:Enable with matching altname[] too) */
 
