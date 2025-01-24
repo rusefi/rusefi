62aa1337180dc28b3a4f56741cb0362432978475
kifir <kifir239@gmail.com>
2025-01-25 00:45:08 +0200

    extract image creation into `create_image.sh` script
    (cherry picked from commit b535a943b9d4e67a5e41d6291adb823ad19ac83c)

---
+++ firmware/hw_layer/mass_storage/create_image.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Usage:
+# create_image.sh <output_header_file_name> <file_sistem_size_in_kilobytes> <image_compression_flag> <file_to_copy_on_image_1> <file_to_copy_on_image_2> ...
+
+# fail on error
+set -e
+
+H_OUTPUT=$1
+echo "H_OUTPUT=$H_OUTPUT"
+FS_SIZE=$2
+echo "FS_SIZE=$FS_SIZE"
+COMPRESS_IMAGE=$3
+echo "COMPRESS_IMAGE=$COMPRESS_IMAGE"
+shift 3
+
+IMAGE=ramdisk.image
+
+# mkfs.fat and fatlabel are privileged on some systems
+PATH="$PATH:/usr/sbin"
+
+rm -f $IMAGE $IMAGE.gz
+
+# copy *FS_SIZE*KB of zeroes
+dd if=/dev/zero of=$IMAGE bs=1024 count=$FS_SIZE
+
+# create a FAT filesystem inside, name it RUSEFI
+mkfs.fat -v -r 64 $IMAGE
+# labels can be no longer than 11 characters
+fatlabel $IMAGE RUSEFI
+
+for file in "$@"
+do
+  mcopy -i $IMAGE "$file" ::
+  echo "File '$file' is added to the image."
+done
+
+if [ "${COMPRESS_IMAGE,,}" = "true" ]; then
+  # Compress the image as DEFLATE with gzip
+  gzip $IMAGE
+  IMAGE_TO_OUTPUT=$IMAGE.gz
+else
+  IMAGE_TO_OUTPUT=$IMAGE
+fi
+
+# write out as a C array, with "static const" tacked on the front
+xxd -i $IMAGE_TO_OUTPUT \
+    | cat <(echo -n "static const ") - \
+    > $H_OUTPUT
+
+rm $IMAGE_TO_OUTPUT
+exit 0
