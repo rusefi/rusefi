282301ad56c20e516acf4c73b586d4b88919e12d
Andrey Gusakov <dron0gus@gmail.com>
2025-02-01 00:46:15 +0300

    Revert "GDI: HPFP: suspect?"
    This reverts commit b1ac3f6cfc523e728414356499a53be49ad4b212.

--- firmware/controllers/engine_cycle/high_pressure_fuel_pump.cpp
+++ firmware/controllers/engine_cycle/high_pressure_fuel_pump.cpp
@@ -199,7 +199,6 @@ void HpfpController::pinTurnOn(HpfpController *self) {
 	// stops, the valve will be turned off in a certain amount of time regardless.
 	scheduleByAngle(&self->m_event.eventScheduling,
 			self->m_event.eventScheduling.getMomentNt(),
-			/* TODO: why fixed engineConfiguration->hpfpActivationAngle? but not calculated m_requested_pump? */
 			self->m_deadangle + engineConfiguration->hpfpActivationAngle,
 			{ pinTurnOff, self });
 }
