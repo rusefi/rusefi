a2f578f063180e94cc154b6bd5ae621d6654664f
Andrey Gusakov <dron0gus@gmail.com>
2025-02-01 00:18:10 +0300

    GDI: HPFP: more accurate logging

--- firmware/controllers/engine_cycle/high_pressure_fuel_pump.cpp
+++ firmware/controllers/engine_cycle/high_pressure_fuel_pump.cpp
@@ -193,7 +193,7 @@ void HpfpController::onFastCallback() {
 void HpfpController::pinTurnOn(HpfpController *self) {
 	enginePins.hpfpValve.setHigh(HPFP_CONTROLLER);
 	self->HpfpValveState = true;
-	self->HpfdActivationAngle = getTriggerCentral()->currentEngineDecodedPhase;
+	self->HpfdActivationPhase = getTriggerCentral()->getCurrentEnginePhase(getTimeNowNt());
 
 	// By scheduling the close after we already open, we don't have to worry if the engine
 	// stops, the valve will be turned off in a certain amount of time regardless.
@@ -207,7 +207,7 @@ void HpfpController::pinTurnOn(HpfpController *self) {
 void HpfpController::pinTurnOff(HpfpController *self) {
 	enginePins.hpfpValve.setLow(HPFP_CONTROLLER);
 	self->HpfpValveState = false;
-	self->HpfdDeactivationAngle = getTriggerCentral()->currentEngineDecodedPhase;
+	self->HpfdDeactivationPhase = getTriggerCentral()->getCurrentEnginePhase(getTimeNowNt());
 
 	self->scheduleNextCycle();
 }
