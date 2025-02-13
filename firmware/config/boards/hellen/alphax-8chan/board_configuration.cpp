5720f9119a8c827eb9c5d4492a47a2a69b2e3141
rusefi <rusefillc@gmail.com>
2025-02-13 00:20:27 -0500

    making LEDs pattern consistent only:alphax-8chan_f7

--- firmware/config/boards/hellen/alphax-8chan/board_configuration.cpp
+++ firmware/config/boards/hellen/alphax-8chan/board_configuration.cpp
@@ -12,9 +12,6 @@
 #include "hellen_meta.h"
 #include "defaults.h"
 
-// mm176 matches mm144 in terms of LED pinout
-#include "hellen_leds_144.cpp"
-
 static OutputPin alphaCrankPPullUp;
 
 static OutputPin alphaHall1PullDown;
