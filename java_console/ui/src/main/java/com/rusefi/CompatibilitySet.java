2f74f35530be5cf5909839fb15a1837d680b75d0
kifir23917 <spamforkifir@gmail.com>
2025-02-21 22:44:51 +0300

    only:refactoring: now we use variable arguments

--- java_console/ui/src/main/java/com/rusefi/CompatibilitySet.java
+++ java_console/ui/src/main/java/com/rusefi/CompatibilitySet.java
@@ -5,18 +5,11 @@ import java.util.Set;
 
 // [tag:java8] java 11 API in java 8 world
 public class CompatibilitySet {
-    public static <E> Set<E> of(E e1, E e2) {
-        Set<E> result = new HashSet<>();
-        result.add(e1);
-        result.add(e2);
-        return result;
-    }
-
-    public static <E> Set<E> of(E e1, E e2, E e3) {
-        Set<E> result = new HashSet<>();
-        result.add(e1);
-        result.add(e2);
-        result.add(e3);
+    public static <E> Set<E> of(final E... elements) {
+        final Set<E> result = new HashSet<>();
+        for (final E element: elements) {
+            result.add(element);
+        }
         return result;
     }
 }
