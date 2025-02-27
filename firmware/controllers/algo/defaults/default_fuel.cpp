180b05097216822d64aee95316b9e98754a424b7
FDSoftware <diego+gh@churrosoft.ar>
2025-02-26 21:19:45 -0300

    add calibration data for Bosch 02880155868 injector

--- firmware/controllers/algo/defaults/default_fuel.cpp
+++ firmware/controllers/algo/defaults/default_fuel.cpp
@@ -10,9 +10,10 @@ static void setBosch02880155868(injector_s& cfg) {
     static const float vBattBins[8] = { 6.0, 8.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0 };
 	static const float pressureBins[2] = { 206.843, 413.685 };
 
+    // see https://github.com/rusefi/rusefi/issues/7521 for adding more values
     static const float corrBins[2][8] = {
-	    { 3.371, 1.974, 1.383, 1.194, 1.040, 0.914, 0.767, 0.726 },
-		{ 3.371, 1.974, 1.383, 1.194, 1.040, 0.914, 0.767, 0.726 },
+	    { 4.240, 2.483, 1.739, 1.501, 1.308, 1.149, 0.964, 0.913 },
+		{ 3.084, 1.641, 1.149, 1.194, 0.992, 0.759, 0.637, 0.603 },
     };
 
 	copyArray(cfg.battLagCorrBattBins, vBattBins);
--- firmware/controllers/algo/engine_configuration_defaults.h
+++ firmware/controllers/algo/engine_configuration_defaults.h
@@ -42,13 +42,13 @@ namespace engine_configuration_defaults {
     /* Injector */
     constexpr bool INJECTOR_FLOW_AS_MASS_FLOW = false;
     constexpr float INJECTOR_FLOW = 200.0f;
-    constexpr BattLagCorrTable INJECTOR_BATT_LAG_CURR { { 3.371, 1.974, 1.383, 1.194, 1.040, 0.914, 0.767, 0.726  } };
+    constexpr BattLagCorrTable INJECTOR_BATT_LAG_CURR { { 4.240, 2.483, 1.739, 1.501, 1.308, 1.149, 0.964, 0.913 } };
     constexpr float FUEL_REFERENCE_PRESSURE = 300.0f;
     constexpr injector_compensation_mode_e INJECTOR_COMPENSATION_MODE = ICM_None;
 
     /* Secondary injector: */
     constexpr float INJECTOR_SECONDARY_FLOW = INJECTOR_FLOW;
-    constexpr BattLagCorrTable INJECTOR_SECONDARY_BATT_LAG_CURR { { 3.371, 1.974, 1.383, 1.194, 1.040, 0.914, 0.767, 0.726  } };
+    constexpr BattLagCorrTable INJECTOR_SECONDARY_BATT_LAG_CURR { { 4.240, 2.483, 1.739, 1.501, 1.308, 1.149, 0.964, 0.913 } };
     constexpr float SECONDARY_INJECTOR_FUEL_REFERENCE_PRESSURE = 0.0f;
     constexpr injector_compensation_mode_e SECONDARY_INJECTOR_COMPENSATION_MODE = INJECTOR_COMPENSATION_MODE;
 
