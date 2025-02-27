5cfa45654b5e99ddb26197694b172707c05b6fa0
GitHub gen-configs Action <action@github.com>
2025-02-27 02:27:49 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_alphax-8chan_f7.h
+++ firmware/controllers/generated/signature_alphax-8chan_f7.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1276309296
-#define TS_SIGNATURE "rusEFI master.2025.02.27.alphax-8chan_f7.1276309296"
+#define SIGNATURE_HASH 3411668582
+#define TS_SIGNATURE "rusEFI master.2025.02.27.alphax-8chan_f7.3411668582"
