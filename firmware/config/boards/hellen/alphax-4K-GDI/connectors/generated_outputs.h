dd5a909ae9f9b78b374af9c4d77e8717e7f46680
GitHub gen-configs Action <action@github.com>
2025-03-03 04:05:59 +0000

    Auto-generated configs and docs

--- firmware/config/boards/hellen/alphax-4K-GDI/connectors/generated_outputs.h
+++ firmware/config/boards/hellen/alphax-4K-GDI/connectors/generated_outputs.h
@@ -6,56 +6,56 @@
 #pragma once
 
 Gpio GENERATED_OUTPUTS = {
-	Gpio::H144_LS_4, // 3A GDI Injector 4
-	Gpio::H144_LS_3, // 5A GDI Injector 3
-	Gpio::H144_LS_2, // 7A GDI Injector 2
-	Gpio::H144_LS_1, // 9A GDI Injector 1
-	Gpio::TLE9104_3_OUT_0, // 33A Wastegate Solenoid
-	Gpio::TLE9104_3_OUT_1, // 34A AC Control
-	Gpio::TLE9104_0_OUT_3, // 1B Port injector 4
-	Gpio::TLE9104_0_OUT_2, // 2B Port injector 3
-	Gpio::TLE9104_0_OUT_1, // 3B Port injector 2
-	Gpio::TLE9104_0_OUT_0, // 4B Port injector 1
-	Gpio::TLE9104_1_OUT_0, // 5B Main Relay Control
-	Gpio::TLE9104_1_OUT_1, // 7B Fuel Pump
-	Gpio::TLE9104_1_OUT_2, // 8B Fan 1
-	Gpio::TLE9104_1_OUT_3, // 9B Fan 2
-	Gpio::TLE9104_2_OUT_0, // 29B VVT1
-	Gpio::TLE9104_2_OUT_1, // 30B VVT2
-	Gpio::TLE9104_2_OUT_2, // 31B VVT3
-	Gpio::TLE9104_2_OUT_3, // 32B VVT4
+	Gpio::TLE9104_0_OUT_3, // 1A Port injector 4
+	Gpio::TLE9104_0_OUT_2, // 2A Port injector 3
+	Gpio::TLE9104_0_OUT_1, // 3A Port injector 2
+	Gpio::TLE9104_0_OUT_0, // 4A Port injector 1
+	Gpio::TLE9104_1_OUT_0, // 5A Main Relay Control
+	Gpio::TLE9104_1_OUT_1, // 7A Fuel Pump
+	Gpio::TLE9104_1_OUT_2, // 8A Fan 1
+	Gpio::TLE9104_1_OUT_3, // 9A Fan 2
+	Gpio::TLE9104_2_OUT_0, // 29A VVT1
+	Gpio::TLE9104_2_OUT_1, // 30A VVT2
+	Gpio::TLE9104_2_OUT_2, // 31A VVT3
+	Gpio::TLE9104_2_OUT_3, // 32A VVT4
+	Gpio::H144_LS_4, // 3B GDI Injector 4
+	Gpio::H144_LS_3, // 5B GDI Injector 3
+	Gpio::H144_LS_2, // 7B GDI Injector 2
+	Gpio::H144_LS_1, // 9B GDI Injector 1
+	Gpio::TLE9104_3_OUT_0, // 33B Wastegate Solenoid
+	Gpio::TLE9104_3_OUT_1, // 34B AC Control
 	Gpio::H144_LS_6, // HPFP Positive
-	Gpio::H144_IGN_4, // 11A Ignition 4
-	Gpio::H144_IGN_3, // 12A Ignition 3
-	Gpio::H144_IGN_2, // 13A Ignition 2
-	Gpio::H144_IGN_1, // 14A Ignition 1
+	Gpio::H144_IGN_4, // 11B Ignition 4
+	Gpio::H144_IGN_3, // 12B Ignition 3
+	Gpio::H144_IGN_2, // 13B Ignition 2
+	Gpio::H144_IGN_1, // 14B Ignition 1
 	Gpio::MSIOBOX_0_OUT_1, // MS IO-Box 0 OUT 1
 	Gpio::MSIOBOX_0_OUT_2, // MS IO-Box 0 OUT 2
 	Gpio::MSIOBOX_0_OUT_3, // MS IO-Box 0 OUT 3
 	Gpio::MSIOBOX_0_OUT_4, // MS IO-Box 0 OUT 4
-// "3A GDI Injector 4",
-// "5A GDI Injector 3",
-// "7A GDI Injector 2",
-// "9A GDI Injector 1",
-// "33A Wastegate Solenoid",
-// "34A AC Control",
-// "1B Port injector 4",
-// "2B Port injector 3",
-// "3B Port injector 2",
-// "4B Port injector 1",
-// "5B Main Relay Control",
-// "7B Fuel Pump",
-// "8B Fan 1",
-// "9B Fan 2",
-// "29B VVT1",
-// "30B VVT2",
-// "31B VVT3",
-// "32B VVT4",
+// "1A Port injector 4",
+// "2A Port injector 3",
+// "3A Port injector 2",
+// "4A Port injector 1",
+// "5A Main Relay Control",
+// "7A Fuel Pump",
+// "8A Fan 1",
+// "9A Fan 2",
+// "29A VVT1",
+// "30A VVT2",
+// "31A VVT3",
+// "32A VVT4",
+// "3B GDI Injector 4",
+// "5B GDI Injector 3",
+// "7B GDI Injector 2",
+// "9B GDI Injector 1",
+// "33B Wastegate Solenoid",
+// "34B AC Control",
 // "HPFP Positive",
-// "11A Ignition 4",
-// "12A Ignition 3",
-// "13A Ignition 2",
-// "14A Ignition 1",
+// "11B Ignition 4",
+// "12B Ignition 3",
+// "13B Ignition 2",
+// "14B Ignition 1",
 // "MS IO-Box 0 OUT 1",
 // "MS IO-Box 0 OUT 2",
 // "MS IO-Box 0 OUT 3",
