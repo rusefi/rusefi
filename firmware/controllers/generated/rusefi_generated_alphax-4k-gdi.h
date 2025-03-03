49c4cd4a3f69763c69752f607e896c7e71bd5083
GitHub gen-configs Action <action@github.com>
2025-03-03 03:47:01 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-4k-gdi.h
+++ firmware/controllers/generated/rusefi_generated_alphax-4k-gdi.h
@@ -137,7 +137,6 @@
 #define BOARD_ENGINE_CONFIGURATION_FROM_FILE 
 #define BOARD_GAUGES_FROM_FILE 
 #define BOARD_INDICATORS_FROM_FILE 
-#define BOARD_MENU_FROM_FILE 
 #define BOARD_PANELS_FROM_FILE 
 #define BOARD_PC_VARIABLES_FROM_FILE 
 #define BOARD_TABLES_FROM_FILE 
@@ -1380,7 +1379,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 1076529503
+#define SIGNATURE_HASH 4180562894
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1875,7 +1874,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.03.03.alphax-4k-gdi.1076529503"
+#define TS_SIGNATURE "rusEFI master.2025.03.03.alphax-4k-gdi.4180562894"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- firmware/controllers/generated/signature_alphax-4k-gdi.h
+++ firmware/controllers/generated/signature_alphax-4k-gdi.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1076529503
-#define TS_SIGNATURE "rusEFI master.2025.03.03.alphax-4k-gdi.1076529503"
+#define SIGNATURE_HASH 4180562894
+#define TS_SIGNATURE "rusEFI master.2025.03.03.alphax-4k-gdi.4180562894"
