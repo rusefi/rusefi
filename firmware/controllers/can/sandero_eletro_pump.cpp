3080b54446c79ce540bca292fee36ce3bcd9088e
Líbni Junior <libnidsj@gmail.com>
2025-03-09 11:55:20 -0300

    Redo some work on FAN and start sandero pump implementation

---
+++ firmware/controllers/can/sandero_eletro_pump.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+
+#if EFI_CAN_SUPPORT
+
+#include "can_msg_tx.h"
+#include "sandero_eletro_pump.h"
+
+void sendEletroPumpInfo() {
+
+}
+
+#endif 
---
+++ firmware/controllers/can/sandero_eletro_pump.h
@@ -0,0 +1,7 @@
+// file rusefi_wideband.h
+
+#pragma once
+#include "can.h"
+
+// Send info to the wideband controller like battery voltage, heater enable bit, etc.
+void sendEletroPumpInfo();
