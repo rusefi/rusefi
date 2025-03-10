1646813df6c9c3a641ddd302017a9978ad9a306e
rusefi <rusefillc@gmail.com>
2025-03-10 19:07:16 -0400

    refactoring: parameter names

--- firmware/controllers/sensors/converters/fuel_level_func.cpp
+++ firmware/controllers/sensors/converters/fuel_level_func.cpp
@@ -6,20 +6,20 @@
 
 #include "fuel_level_func.h"
 
-SensorResult FuelLevelFunc::convert(const float inputValue) {
-	if (std::isnan(inputValue)) {
+SensorResult FuelLevelFunc::convert(const float inputVoltage) {
+	if (std::isnan(inputVoltage)) {
 		criticalError("temp error FuelLevelFunc NaN input");
 		m_filteredValue.reset();
 		return UnexpectedCode::Unknown;
-	} else if (inputValue < engineConfiguration->fuelLevelLowThresholdVoltage) {
+	} else if (inputVoltage < engineConfiguration->fuelLevelLowThresholdVoltage) {
 		m_filteredValue.reset();
 		return UnexpectedCode::Low;
-	} else if (engineConfiguration->fuelLevelHighThresholdVoltage < inputValue) {
+	} else if (engineConfiguration->fuelLevelHighThresholdVoltage < inputVoltage) {
 		m_filteredValue.reset();
 		return UnexpectedCode::High;
 	} else {
-		const float filteredValue = filterFuelValue(inputValue);
-		const float fuelLevel = interpolate2d(filteredValue, config->fuelLevelBins, config->fuelLevelValues);
+		const float filteredVoltage = filterFuelValue(inputVoltage);
+		const float fuelLevel = interpolate2d(filteredVoltage, config->fuelLevelBins, config->fuelLevelValues);
 		return fuelLevel;
 	}
 }
@@ -29,23 +29,23 @@ float FuelLevelFunc::getFuelLevelAlpha() const {
 	return (0.0f < configuredAlpha ? configuredAlpha : engine_configuration_defaults::FUEL_LEVEL_AVERAGING_ALPHA);
 }
 
-float FuelLevelFunc::filterFuelValue(const float value) {
+float FuelLevelFunc::filterFuelValue(const float inputVoltage) {
 	if (m_filteredValue.has_value()) {
 		if (m_fuelLevelTimer.hasElapsedSec(maxF(
 				engineConfiguration->fuelLevelUpdatePeriodSec,
 				MIN_FUEL_LEVEL_UPDATE_PERIOD_SEC
 		))) {
 			const float prevFilteredValue = m_filteredValue.value();
-			const float diff = value - prevFilteredValue;
+			const float diff = inputVoltage - prevFilteredValue;
 			updateFilteredValue(prevFilteredValue + getFuelLevelAlpha() * diff);
 		}
 	} else {
-		updateFilteredValue(value);
+		updateFilteredValue(inputVoltage);
 	}
 	return m_filteredValue.value();
 }
 
-void FuelLevelFunc::updateFilteredValue(const float value) {
-	m_filteredValue = value;
+void FuelLevelFunc::updateFilteredValue(const float filteredVoltage) {
+	m_filteredValue = filteredVoltage;
 	m_fuelLevelTimer.reset();
 }
