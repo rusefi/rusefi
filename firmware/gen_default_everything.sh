dce2104262d15cdeee76fa6dfa85fe486b9e7b3e
rusefi <rusefillc@gmail.com>
2025-03-12 20:14:31 -0400

    HW CI discovery something is wrong #7604 dead?

--- firmware/gen_default_everything.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "gen_live_documentation.sh as part of everything"
-./gen_live_documentation.sh
-
-# TODO: there is a chance that we can simply remove this invocation? let's try once we fix https://github.com/rusefi/rusefi/issues/7604
-echo "gen_config_default.sh as part of everything"
-./gen_config_default.sh
