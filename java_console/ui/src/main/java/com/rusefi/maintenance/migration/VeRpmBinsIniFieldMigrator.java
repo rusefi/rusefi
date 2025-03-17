19f2e1527d83385c325f11ca4e0e9d5dec8f12f6
kifir23917 <spamforkifir@gmail.com>
2025-03-17 23:23:02 +0300

    only:refactoring: extract part of functionality into `VeRpmBinsIniFieldMigrator` class

---
+++ java_console/ui/src/main/java/com/rusefi/maintenance/migration/VeRpmBinsIniFieldMigrator.java
@@ -0,0 +1,185 @@
+package com.rusefi.maintenance.migration;
+
+import com.opensr5.ini.field.ArrayIniField;
+import com.opensr5.ini.field.IniField;
+import com.rusefi.config.FieldType;
+import com.rusefi.io.UpdateOperationCallbacks;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static com.rusefi.maintenance.migration.VeTableExtensionMigrator.*;
+
+enum VeRpmBinsIniFieldMigrator {
+    INSTANCE;
+
+    private static final int VE_RPM_BINS_COLS = 1;
+    private static final double VE_RPM_BINS_MULTIPLIER = 1;
+    private static final String VE_RPM_BINS_DIGITS = "0";
+    private static final FieldType VE_RPM_BINS_TYPE = FieldType.UINT16;
+
+    Optional<String> tryMigrateVeRpmBins(
+        final IniField prevField,
+        final IniField newField,
+        final String prevValue,
+        final UpdateOperationCallbacks callbacks
+    ) {
+        Optional<String> result = Optional.empty();
+        final Optional<ArrayIniField> prevVeRpmBinsValidatedField = getValidatedVeRpmBinsArrayIniField(
+            prevField,
+            callbacks
+        );
+        final Optional<ArrayIniField> newVeRpmBinsValidatedField = getValidatedVeRpmBinsArrayIniField(
+            newField,
+            callbacks
+        );
+        if (prevVeRpmBinsValidatedField.isPresent() && newVeRpmBinsValidatedField.isPresent()) {
+            final ArrayIniField prevVeRpmBinsField = prevVeRpmBinsValidatedField.get();
+            final ArrayIniField newVeRpmBinsField = newVeRpmBinsValidatedField.get();
+            final int prevVeRpmBinsFieldRows = prevVeRpmBinsField.getRows();
+            final int newVeRpmBinsFieldRows = newVeRpmBinsField.getRows();
+            if ((prevVeRpmBinsFieldRows == OLD_VE_TABLE_COLS) && (newVeRpmBinsFieldRows == NEW_VE_TABLE_COLS)) {
+                final List<String> prevValues = Arrays.stream(prevVeRpmBinsField.getValues(prevValue))
+                    .map(e -> e[0])
+                    .collect(Collectors.toList());
+                final List<Long> prevLongValues = prevValues.stream()
+                    .map(Double::parseDouble)
+                    .map(Math::round)
+                    .collect(Collectors.toList());
+                final long lastValue = prevLongValues.get(prevLongValues.size() - 1);
+                final String max = newVeRpmBinsField.getMax();
+
+                Optional<Long> recommendedStep = Optional.empty();
+                if (max != null) {
+                    final long maxPossibleStep = (long)(
+                        (Double.parseDouble(max) - lastValue) / (NEW_VE_TABLE_COLS - OLD_VE_TABLE_COLS)
+                    );
+                    if (1 <= maxPossibleStep) {
+                        recommendedStep = Optional.of(chooseStep(prevLongValues, maxPossibleStep));
+                    } else {
+                        callbacks.logLine(String.format(
+                            "WARNING! `veRpmBins` ini-field cannot be propagated with increasing values, because max value is %s",
+                            max
+                        ));
+                        return Optional.empty();
+                    }
+                } else {
+                    final long valueBeforeLast = prevLongValues.get(prevLongValues.size() - 2);
+                    recommendedStep = Optional.of(lastValue - valueBeforeLast);
+                }
+                if (recommendedStep.isPresent()) {
+                    final String[][] newValues = new String[NEW_VE_TABLE_COLS][1];
+                    // copy prev values:
+                    for (int i = 0; i < OLD_VE_TABLE_COLS; i++) {
+                        newValues[i] = new String[] { prevValues.get(i) };
+                    }
+                    long lastBin = lastValue;
+                    // add missed bins with recommended step:
+                    for (int i = OLD_VE_TABLE_COLS; i < NEW_VE_TABLE_COLS; i++) {
+                        lastBin += recommendedStep.get();
+                        newValues[i] = new String[] { String.format("%.1f", (double)lastBin) };
+                    }
+                    result = Optional.of(newVeRpmBinsField.formatValue(newValues));
+                }
+            } else {
+                callbacks.logLine("WARNING! We failed to extend `veRpmBins` ini-field");
+            }
+        }
+        return result;
+    }
+
+
+    private static Optional<ArrayIniField> getValidatedVeRpmBinsArrayIniField(
+        final IniField field,
+        final UpdateOperationCallbacks callbacks
+    ) {
+        if (!(field instanceof ArrayIniField)) {
+            callbacks.logLine(String.format(
+                "WARNING! `veRpmBins` ini-field is expected to be `ArrayIniField` instead of %s",
+                field.getClass().getName()
+            ));
+            return Optional.empty();
+        }
+        final ArrayIniField arrayField = (ArrayIniField) field;
+        final FieldType arrayFieldType = arrayField.getType();
+        if (arrayFieldType != VE_RPM_BINS_TYPE) {
+            callbacks.logLine(String.format(
+                "WARNING! Type of `veRpmBins` ini-field is expected to be `%s` instead of `%s`",
+                VE_RPM_BINS_TYPE,
+                arrayFieldType
+            ));
+            return Optional.empty();
+        }
+        final int arrayFieldCols = arrayField.getCols();
+        if (arrayFieldCols != VE_RPM_BINS_COLS) {
+            callbacks.logLine(String.format(
+                "WARNING! `veRpmBins` ini-field is expected to contain %d columns instead of %d",
+                VE_TABLE_ROWS,
+                arrayFieldCols
+            ));
+            return Optional.empty();
+        }
+        final double arrayFieldMultiplier = arrayField.getMultiplier();
+        if (arrayFieldMultiplier != VE_RPM_BINS_MULTIPLIER) {
+            callbacks.logLine(String.format(
+                "WARNING! Multiplier of `veRpmBins` ini-field is expected to be %f instead of %f",
+                VE_RPM_BINS_MULTIPLIER,
+                arrayFieldMultiplier
+            ));
+            return Optional.empty();
+        }
+        final String arrayFieldDigits = arrayField.getDigits();
+        if (!VE_RPM_BINS_DIGITS.equals(arrayFieldDigits)) {
+            callbacks.logLine(String.format(
+                "WARNING! Digits of `veRpmBins` ini-field is expected to be `%s` instead of `%s`",
+                VE_RPM_BINS_DIGITS,
+                arrayFieldDigits
+            ));
+            return Optional.empty();
+        }
+        final int arrayFieldRows = arrayField.getRows();
+        switch (arrayFieldRows) {
+            case OLD_VE_TABLE_COLS:
+            case NEW_VE_TABLE_COLS: {
+                return Optional.of(arrayField);
+            }
+            default: {
+                callbacks.logLine(String.format(
+                    "WARNING! `veRpmBins` ini-field is expected to contain %d or %d rows " +
+                        "instead of %d",
+                    OLD_VE_TABLE_COLS,
+                    NEW_VE_TABLE_COLS,
+                    arrayFieldRows
+                ));
+                return Optional.empty();
+            }
+        }
+    }
+
+    private static long chooseStep(final List<Long> bins, final long maxPossibleStep) {
+        Long lastBin = null;
+        Long lastStep = null;
+        Long maxStep = null;
+        for (final long bin: bins) {
+            if (lastBin != null) {
+                lastStep = bin - lastBin;
+                if (lastStep <= maxPossibleStep) {
+                    if ((maxStep == null) || (maxStep < lastStep)) {
+                        maxStep = lastStep;
+                    }
+                }
+            }
+            lastBin = bin;
+        }
+        if (lastStep != null) {
+            if (lastStep <= maxPossibleStep) {
+                return lastStep;
+            } else if (maxStep != null) {
+                return maxStep;
+            }
+        }
+        return maxPossibleStep;
+    }
+}
