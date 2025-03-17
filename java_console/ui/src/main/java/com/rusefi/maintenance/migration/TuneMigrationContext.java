835b239efe31ee6dfb5210c17fd219feba679487
kifir23917 <spamforkifir@gmail.com>
2025-03-18 01:08:08 +0300

    Now `DefaultTuneMigrator` doesn't migrate already migrated ini fields
    (cherry picked from commit 9bd82949163b42e9d3eb29e6bb625b6773e8ef8b)

--- java_console/ui/src/main/java/com/rusefi/maintenance/migration/TuneMigrationContext.java
+++ java_console/ui/src/main/java/com/rusefi/maintenance/migration/TuneMigrationContext.java
@@ -5,8 +5,11 @@ import com.rusefi.io.UpdateOperationCallbacks;
 import com.rusefi.tune.xml.Constant;
 import com.rusefi.tune.xml.Msq;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class TuneMigrationContext {
     private final IniFileModel prevIniFile;
@@ -14,6 +17,7 @@ public class TuneMigrationContext {
     private final IniFileModel updatedIniFile;
     private final Msq updatedTune;
     private final Map<String, Constant> migratedConstants = new HashMap<>();
+    private final Set<String> migratedFields = new HashSet<>();
     final UpdateOperationCallbacks callbacks;
 
     public TuneMigrationContext(
@@ -47,10 +51,36 @@ public class TuneMigrationContext {
     }
 
     public Map<String, Constant> getMigratedConstants() {
-        return migratedConstants;
+        return Collections.unmodifiableMap(migratedConstants);
     }
 
     public UpdateOperationCallbacks getCallbacks() {
         return callbacks;
+    }
+
+    public void addMigration(final String migratedFieldName, final Constant migratedValue) {
+        addMigration(migratedFieldName, migratedFieldName, migratedValue);
+    }
+
+    public boolean isFieldAlreadyMigrated(final String fieldName) {
+        return migratedFields.contains(fieldName);
+    }
+
+    public void addMigration(
+        final String originalFieldName,
+        final String migratedFieldName,
+        final Constant migratedValue
+    ) {
+        migratedFields.add(originalFieldName);
+        final Constant existingMigration = migratedConstants.get(migratedFieldName);
+        if (existingMigration != null) {
+            callbacks.logLine(String.format(
+                "WARNING!!! We are overwriting already migrated constant `%s`: `%s` -> `%s`",
+                migratedFieldName,
+                existingMigration,
+                migratedFieldName
+            ));
+        }
+        migratedConstants.put(migratedFieldName, migratedValue);
     }
 }
