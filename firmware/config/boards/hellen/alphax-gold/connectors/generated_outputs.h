6b83e3fdd2c0cddc1cc3b24715db5293b67cfffc
GitHub gen-configs Action <action@github.com>
2025-03-19 14:58:19 +0000

    Auto-generated configs and docs

--- firmware/config/boards/hellen/alphax-gold/connectors/generated_outputs.h
+++ firmware/config/boards/hellen/alphax-gold/connectors/generated_outputs.h
@@ -5,50 +5,50 @@
 #pragma once
 
 Gpio GENERATED_OUTPUTS = {
-	Gpio::TLE9104_0_OUT_3, // 1A - Injector 4
-	Gpio::TLE9104_0_OUT_2, // 2A - Injector 3
-	Gpio::TLE9104_0_OUT_1, // 3A - Injector 2
-	Gpio::TLE9104_0_OUT_0, // 4A - Injector 1
-	Gpio::TLE9104_2_OUT_0, // 5A Fuel Pump Relay
-	Gpio::TLE9104_2_OUT_1, // 6A Idle Output
-	Gpio::TLE9104_2_OUT_3, // 14A - Tachometer Output
-	Gpio::TLE9104_1_OUT_3, // 1B - Injector 8
-	Gpio::TLE9104_1_OUT_2, // 2B - Injector 7
-	Gpio::TLE9104_1_OUT_1, // 3B - Injector 6
-	Gpio::TLE9104_1_OUT_0, // 4B - Injector 5
-	Gpio::TLE9104_3_OUT_3, // 5B Main Relay
-	Gpio::TLE9104_3_OUT_1, // 6B LS3
-	Gpio::TLE9104_3_OUT_2, // 7B LS4
-	Gpio::TLE9104_3_OUT_0, // 8B LS5
-	Gpio::H144_IGN_4, // 10A Coil 4
-	Gpio::H144_IGN_3, // 11A Coil 3
-	Gpio::H144_IGN_2, // 12A Coil 2
-	Gpio::H144_IGN_1, // 13A Coil 1
-	Gpio::H144_IGN_8, // 10B Coil 8
-	Gpio::H144_IGN_7, // 11B Coil 7
-	Gpio::H144_IGN_6, // 12B Coil 6
-	Gpio::H144_IGN_5, // 13B Coil 5
-// "1A - Injector 4",
-// "2A - Injector 3",
-// "3A - Injector 2",
-// "4A - Injector 1",
-// "5A Fuel Pump Relay",
-// "6A Idle Output",
-// "14A - Tachometer Output",
-// "1B - Injector 8",
-// "2B - Injector 7",
-// "3B - Injector 6",
-// "4B - Injector 5",
-// "5B Main Relay",
-// "6B LS3",
-// "7B LS4",
-// "8B LS5",
-// "10A Coil 4",
-// "11A Coil 3",
-// "12A Coil 2",
-// "13A Coil 1",
-// "10B Coil 8",
-// "11B Coil 7",
-// "12B Coil 6",
-// "13B Coil 5",
+	Gpio::TLE9104_1_OUT_3, // 1A - Injector 8
+	Gpio::TLE9104_1_OUT_2, // 2A - Injector 7
+	Gpio::TLE9104_1_OUT_1, // 3A - Injector 6
+	Gpio::TLE9104_1_OUT_0, // 4A - Injector 5
+	Gpio::TLE9104_3_OUT_3, // 5A Main Relay
+	Gpio::TLE9104_3_OUT_1, // 6A LS3
+	Gpio::TLE9104_3_OUT_2, // 7A LS4
+	Gpio::TLE9104_3_OUT_0, // 8A LS5
+	Gpio::TLE9104_0_OUT_3, // 1B - Injector 4
+	Gpio::TLE9104_0_OUT_2, // 2B - Injector 3
+	Gpio::TLE9104_0_OUT_1, // 3B - Injector 2
+	Gpio::TLE9104_0_OUT_0, // 4B - Injector 1
+	Gpio::TLE9104_2_OUT_0, // 5B Fuel Pump Relay
+	Gpio::TLE9104_2_OUT_1, // 6B Idle Output
+	Gpio::TLE9104_2_OUT_3, // 14B - Tachometer Output
+	Gpio::H144_IGN_8, // 10A Coil 8
+	Gpio::H144_IGN_7, // 11A Coil 7
+	Gpio::H144_IGN_6, // 12A Coil 6
+	Gpio::H144_IGN_5, // 13A Coil 5
+	Gpio::H144_IGN_4, // 10B Coil 4
+	Gpio::H144_IGN_3, // 11B Coil 3
+	Gpio::H144_IGN_2, // 12B Coil 2
+	Gpio::H144_IGN_1, // 13B Coil 1
+// "1A - Injector 8",
+// "2A - Injector 7",
+// "3A - Injector 6",
+// "4A - Injector 5",
+// "5A Main Relay",
+// "6A LS3",
+// "7A LS4",
+// "8A LS5",
+// "1B - Injector 4",
+// "2B - Injector 3",
+// "3B - Injector 2",
+// "4B - Injector 1",
+// "5B Fuel Pump Relay",
+// "6B Idle Output",
+// "14B - Tachometer Output",
+// "10A Coil 8",
+// "11A Coil 7",
+// "12A Coil 6",
+// "13A Coil 5",
+// "10B Coil 4",
+// "11B Coil 3",
+// "12B Coil 2",
+// "13B Coil 1",
 }
