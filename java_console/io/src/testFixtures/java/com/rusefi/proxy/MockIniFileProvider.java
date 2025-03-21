7ba8706315684cbe5e5ba1c5c30bab8595933100
kifir23917 <spamforkifir@gmail.com>
2025-03-21 14:36:36 +0300

    only:fix tests: add mocked scalar ini-field to prevent tests failure because of empty msq #7617

--- java_console/io/src/testFixtures/java/com/rusefi/proxy/MockIniFileProvider.java
+++ java_console/io/src/testFixtures/java/com/rusefi/proxy/MockIniFileProvider.java
@@ -2,9 +2,14 @@ package com.rusefi.proxy;
 
 import com.opensr5.ini.IniFileMetaInfo;
 import com.opensr5.ini.IniFileModel;
+import com.opensr5.ini.field.IniField;
+import com.opensr5.ini.field.ScalarIniField;
 import com.opensr5.ini.field.StringIniField;
 import com.rusefi.binaryprotocol.BinaryProtocol;
 import com.rusefi.binaryprotocol.IniFileProvider;
+import com.rusefi.config.FieldType;
+
+import java.util.Map;
 
 import static org.mockito.Mockito.*;
 
@@ -16,9 +21,28 @@ public class MockIniFileProvider {
         when(mockMeta.getTotalSize()).thenReturn(15000);
 
         IniFileModel mockModel = mock(IniFileModel.class);
+        when(mockModel.getSignature()).thenReturn("rusEFI mocked-ini-file");
         when(mockModel.getBlockingFactor()).thenReturn(6666);
+
+        final IniField mockedStringIniField = new StringIniField("mockStringIniField", 0, 12);
+        // Empty string ini fields from zero-filled image are not persisted to .msq file, so we need at list scalar ini
+        // field to prevent empty msq error:
+        final IniField mockedScalarIniField = new ScalarIniField(
+            "mockScalarIniField",
+            13,
+            "mocked units",
+            FieldType.UINT16,
+            1.0,
+            "0"
+        );
+        when(mockModel.getAllIniFields()).thenReturn(Map.of(
+            mockedStringIniField.getName(), mockedStringIniField,
+            mockedScalarIniField.getName(), mockedScalarIniField
+        ));
+        when(mockModel.getIniField(anyString())).then(invocationOnMock -> mockedStringIniField);
+        when(mockModel.getIniField(mockedScalarIniField.getName())).then(invocationOnMock -> mockedScalarIniField);
+
         when(mockModel.getMetaInfo()).thenReturn(mockMeta);
-        when(mockModel.getIniField(anyString())).then(invocationOnMock -> new StringIniField("mockStringIniField", 0, 12));
 
         return signature -> mockModel;
     }
