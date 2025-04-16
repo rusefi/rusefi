bd5224006c093b3e4ce07b03a075391a20a7d38b
rusefillc <rusefillc@gmail.com>
2025-04-16 12:28:47 -0400

    only:docs

--- firmware/console/binary_log/binary_logging.cpp
+++ firmware/console/binary_log/binary_logging.cpp
@@ -3,6 +3,7 @@
  * See also mlq_file_format.txt
  *
  * https://www.efianalytics.com/TunerStudio/docs/MLG_Binary_LogFormat_1.0.pdf
+ * https://www.efianalytics.com/TunerStudio/docs/MLG_Binary_LogFormat_2.0.pdf
  */
 
 #include "pch.h"
