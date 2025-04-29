ba0d126a4624431871cb1c47f6f617fdfabf44b5
Andrey Gusakov <dron0gus@gmail.com>
2025-04-29 23:06:53 +0300

    Revert "ETB: isEwgMode() helper"
    This reverts commit b3df8823ea2bac215ce57dd229f2a512c0febbb2.

--- firmware/controllers/actuators/electronic_throttle_impl.h
+++ firmware/controllers/actuators/electronic_throttle_impl.h
@@ -100,10 +100,6 @@ private:
 		return m_function == DC_Throttle1 || m_function == DC_Throttle2;
 	}
 
-	bool isEwgMode() const override {
-		return m_function == DC_Wastegate;
-	}
-
 	Timer m_jamDetectTimer;
 
 	// Pedal -> target map
