70fbf37ef26bc601f36ee4f9f0ade39b9a81c36c
GitHub gen-configs Action <action@github.com>
2025-05-07 01:55:41 +0000

    Auto-generated configs and docs

--- firmware/hw_layer/drivers/can/auto_generated_can_category.cpp
+++ firmware/hw_layer/drivers/can/auto_generated_can_category.cpp
@@ -1,6 +1,6 @@
 #include "global.h"
 #include "can_category.h"
-// was generated automatically by rusEFI tool  from can_category.h // by enum_to_string.jar tool on Sat Apr 20 12:21:13 UTC 2024
+// was generated automatically by rusEFI tool  from can_category.h // by enum_to_string.jar tool on Wed May 07 01:54:45 UTC 2025
 // see also gen_config_and_enums.bat
 
 
@@ -9,12 +9,14 @@ const char *getCanCategory(CanCategory value){
 switch(value) {
 case CanCategory::BENCH_TEST:
   return "BENCH_TEST";
-case CanCategory::DOWNSTREAM_FLASHING:
-  return "DOWNSTREAM_FLASHING";
-case CanCategory::LUA:
-  return "LUA";
 case CanCategory::CAN_IOBOX:
   return "CAN_IOBOX";
+case CanCategory::DOWNSTREAM_FLASHING:
+  return "DOWNSTREAM_FLASHING";
+case CanCategory::GDI:
+  return "GDI";
+case CanCategory::LUA:
+  return "LUA";
 case CanCategory::NBC:
   return "NBC";
 case CanCategory::OBD:
