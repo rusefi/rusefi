348e450150d7888fecd32f2d38d508809cbe75f4
FDSoftware <diego+gh@churrosoft.ar>
2025-05-14 12:40:14 -0300

    test coverage for window sampling single/multiple cyl

--- firmware/controllers/modules/map_averaging/map_averaging.cpp
+++ firmware/controllers/modules/map_averaging/map_averaging.cpp
@@ -223,10 +223,13 @@ void MapAveragingModule::onEnginePhase(float /*rpm*/,
 		int structIndex = getRevolutionCounter() % 2;
 
 		auto & mapAveraging = *engine->module<MapAveragingModule>();
-		mapSampler* s = &mapAveraging.samplers[i][structIndex];
+		mapSampler* m_event = &mapAveraging.samplers[i][structIndex];
 
-		scheduleByAngle(&s->startTimer, edgeTimestamp, angleOffset,
-				{ startMapAveraging, s });
+		engine->module<TriggerScheduler>()->schedule(
+		    "mapAveragingStart",
+			&m_event->startTimer,
+			angleOffset,
+			{ startMapAveraging, m_event });
 	}
 }
 
