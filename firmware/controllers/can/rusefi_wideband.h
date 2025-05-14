9f42410836c6d4a0fe9a5991912aca0218b4b180
Andrey Gusakov <dron0gus@gmail.com>
2025-05-14 16:40:37 +0300

    WBO: warning about some RusEFI WBO functions

--- firmware/controllers/can/rusefi_wideband.h
+++ firmware/controllers/can/rusefi_wideband.h
@@ -4,9 +4,17 @@
 
 // Indicate that an ack response was received from the wideband bootloader
 void handleWidebandBootloaderAck();
+// Send info to the wideband controller like battery voltage, heater enable bit, etc.
+void sendWidebandInfo();
+
+
+// WARNING:
+// Two following functions can block thread execution while waiting for ACK from WBO
+// Do not call from critical tasks!
+// Usually called from bench test task
+// Also do not call from ISR context
+
 // Update the firmware on any connected wideband controller
 void updateWidebandFirmware();
 // Set the CAN index offset of any attached wideband controller
 void setWidebandOffset(uint8_t index);
-// Send info to the wideband controller like battery voltage, heater enable bit, etc.
-void sendWidebandInfo();
