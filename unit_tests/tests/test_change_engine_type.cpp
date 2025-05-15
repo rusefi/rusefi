c727ba4eeade9831974211adf62c2045b76e8d28
rusefillc <rusefillc@gmail.com>
2025-05-14 22:06:00 -0400

    only:moving away from triggerInputDebugPins

--- unit_tests/tests/test_change_engine_type.cpp
+++ unit_tests/tests/test_change_engine_type.cpp
@@ -11,25 +11,24 @@ extern engine_configuration_s & activeConfiguration;
 
 TEST(misc, changeEngineType) {
 	/**
-	 * this configuration has triggerInputDebugPins defined
+	 * this configuration has hpfpValvePin defined
 	 */
-	EngineTestHelper eth(engine_type_e::FORD_ASPIRE_1996);
+	EngineTestHelper eth(engine_type_e::HYUNDAI_PB);
 
-	brain_pin_e brainPin = engineConfiguration->triggerInputDebugPins[0];
+	brain_pin_e brainPin = engineConfiguration->hpfpValvePin;
 	ASSERT_TRUE(brainPin != Gpio::Unassigned);
-	ASSERT_TRUE(activeConfiguration.triggerInputDebugPins[0] != Gpio::Unassigned);
+	ASSERT_TRUE(activeConfiguration.hpfpValvePin != Gpio::Unassigned);
 
 	int pinIndex = brainPin_to_index(brainPin);
 	ASSERT_TRUE(nullptr != getBrainUsedPin(pinIndex));
 
 	// above we have asserted that triggerInputDebugPins is in fact used
-	// now let's change into engine type without triggerInputDebugPins and assert shut down
+	// now let's change into engine type without hpfpValvePin and assert shut down
 
 	setEngineType((int)engine_type_e::DODGE_NEON_1995);
-	ASSERT_TRUE(activeConfiguration.triggerInputDebugPins[0] == Gpio::Unassigned);
+	ASSERT_TRUE(activeConfiguration.hpfpValvePin == Gpio::Unassigned);
 
-	ASSERT_TRUE(engineConfiguration->triggerInputDebugPins[0] == Gpio::Unassigned);
-	ASSERT_TRUE(engineConfiguration->triggerInputDebugPins[0] == Gpio::Unassigned);
+	ASSERT_TRUE(engineConfiguration->hpfpValvePin == Gpio::Unassigned);
 
 	ASSERT_TRUE(nullptr == getBrainUsedPin(pinIndex));
 }
