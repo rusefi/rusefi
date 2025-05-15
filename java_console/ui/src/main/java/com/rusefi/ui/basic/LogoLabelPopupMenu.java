9ed61885083bfb34f17ceb158a1aabc81de272e1
kifir23917 <spamforkifir@gmail.com>
2025-05-15 20:46:18 +0300

    only:implement `Print unit label` logo context menu item

--- java_console/ui/src/main/java/com/rusefi/ui/basic/LogoLabelPopupMenu.java
+++ java_console/ui/src/main/java/com/rusefi/ui/basic/LogoLabelPopupMenu.java
@@ -4,9 +4,14 @@ import javax.swing.*;
 
 public class LogoLabelPopupMenu extends JPopupMenu {
     private final JMenuItem uploadTuneMenuItem;
+    private final JMenuItem printUnitLabelMenuItem;
 
-    public LogoLabelPopupMenu(final Runnable onUploadTuneMenuItemSelected) {
-        JMenuItem instanceNameMenuItem = new JMenuItem("Instance name");
+    public LogoLabelPopupMenu(
+        final Runnable onUploadTuneMenuItemSelected,
+        final Runnable onPrintUnitLabelMenuItemSelected,
+        final boolean displayInstanceNameAndUploadTuneMenuItems
+    ) {
+        final JMenuItem instanceNameMenuItem = new JMenuItem("Instance name");
         instanceNameMenuItem.addActionListener(
             e -> InstanceNameEditor.INSTANCE.editInstanceName(instanceNameMenuItem)
         );
@@ -15,12 +20,20 @@ public class LogoLabelPopupMenu extends JPopupMenu {
         uploadTuneMenuItem.setEnabled(false);
         uploadTuneMenuItem.addActionListener(e -> onUploadTuneMenuItemSelected.run());
 
-        add(uploadTuneMenuItem);
-        add(instanceNameMenuItem);
+        printUnitLabelMenuItem = new JMenuItem("Print unit label");
+        printUnitLabelMenuItem.setEnabled(false);
+        printUnitLabelMenuItem.addActionListener(e -> onPrintUnitLabelMenuItemSelected.run());
+
+        if (displayInstanceNameAndUploadTuneMenuItems) {
+            add(uploadTuneMenuItem);
+            add(instanceNameMenuItem);
+        }
+        add(printUnitLabelMenuItem);
     }
 
-    public void refreshUploadTuneMenuItem(final boolean canBeEnabled) {
+    public void refreshUploadTuneAndPrintUnitLabelsMenuItems(final boolean canBeEnabled) {
         final boolean enableUploadTuneMenuItem = (canBeEnabled ? InstanceNameEditor.loadInstanceName() != null : false);
         uploadTuneMenuItem.setEnabled(enableUploadTuneMenuItem);
+        printUnitLabelMenuItem.setEnabled(canBeEnabled);
     }
 }
