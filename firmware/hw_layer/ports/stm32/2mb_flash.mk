cccc4cec52ddfcdda920d4d6732a742acc926673
rugged <rusefillc@gmail.com>
2025-05-18 11:50:54 -0400

    Dual bank F7 interrupts execution while erasing/writing flash following "burn" command #7700 only:docs

--- firmware/hw_layer/ports/stm32/2mb_flash.mk
+++ firmware/hw_layer/ports/stm32/2mb_flash.mk
@@ -1,3 +1,9 @@
+#
+# on F7 EFI_FLASH_WRITE_THREAD is TRUE by default and allowFlashWhileRunning() does runtime chip size check
+# do we have a defect below when we allow code to overflow above 1MB? would the fix be to set
+# EFI_FLASH_SIZE to 1Mb minus bootloader size?
+#
+
 # Configure the linker script to use more flash for this board (2Mb assumed)
 USE_OPT += -Wl,--defsym=EFI_FLASH_SIZE=1536k
 # affects config placement
