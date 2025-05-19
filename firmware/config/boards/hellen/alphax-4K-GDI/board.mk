1ae4b9bb4ca5c65befadeaa10e37e19ea0b7f08f
Andrey Gusakov <dron0gus@gmail.com>
2025-05-19 10:59:36 +0300

    AlphaX 4K GDI: remove 2Mb hack, use -0g for debug build

--- firmware/config/boards/hellen/alphax-4K-GDI/board.mk
+++ firmware/config/boards/hellen/alphax-4K-GDI/board.mk
@@ -3,9 +3,6 @@
 # Target ECU board design
 BOARDCPPSRC = $(BOARD_DIR)/board_configuration.cpp
 
-include $(PROJECT_DIR)/hw_layer/ports/stm32/2mb_flash.mk
-
-
 DDEFS += -DEFI_MC33816=TRUE
 DDEFS += -DBOARD_TLE9104_COUNT=4
 DDEFS += -DLUA_USER_HEAP=92000
