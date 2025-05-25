964b5ce5a3889b061f1065464dc277b211a35ff2
rusefillc <rusefillc@gmail.com>
2025-05-24 23:51:38 -0400

    only:ecoboost

--- firmware/config/engines/ford_ecoboost.cpp
+++ firmware/config/engines/ford_ecoboost.cpp
@@ -1,5 +1,17 @@
 
 #include "pch.h"
+#include "defaults.h"
+
+#ifdef HW_HELLEN
+#include "hellen_meta.h"
+#endif // HW_HELLEN
 
 void setFordEcoboost() {
+  engineConfiguration->hpfpCamLobes = 4;
+  setHpfpLobeProfileAngle(engineConfiguration->hpfpCamLobes);
+  engineConfiguration->vvtMode[1] = VVT_BOSCH_QUICK_START;
+	setInline4();
+
+#ifdef HW_HELLEN_4K_GDI
+#endif // HW_HELLEN_4K_GDI
 }
