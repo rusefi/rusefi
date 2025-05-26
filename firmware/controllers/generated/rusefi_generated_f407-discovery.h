8ed014a1d5bd6e216323b3e4b6a71a9d0d7f0071
GitHub gen-configs Action <action@github.com>
2025-05-26 16:06:16 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_f407-discovery.h
+++ firmware/controllers/generated/rusefi_generated_f407-discovery.h
@@ -1441,7 +1441,7 @@
 #define show_tcu_gauges false
 #define show_test_presets true
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 1466279122
+#define SIGNATURE_HASH 3912188104
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1937,7 +1937,7 @@
 #define ts_show_tle8888 false
 #define ts_show_top_level_can_menu true
 #define ts_show_tps2 true
-#define ts_show_tps_sent true
+#define ts_show_tps_sent false
 #define ts_show_trigger_advanced true
 #define ts_show_trigger_gap_override_flag true
 #define ts_show_trigger_overrides true
@@ -1960,7 +1960,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.05.26.f407-discovery.1466279122"
+#define TS_SIGNATURE "rusEFI master.2025.05.26.f407-discovery.3912188104"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- firmware/controllers/generated/signature_f407-discovery.h
+++ firmware/controllers/generated/signature_f407-discovery.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1466279122
-#define TS_SIGNATURE "rusEFI master.2025.05.26.f407-discovery.1466279122"
+#define SIGNATURE_HASH 3912188104
+#define TS_SIGNATURE "rusEFI master.2025.05.26.f407-discovery.3912188104"
