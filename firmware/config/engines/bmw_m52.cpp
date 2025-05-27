8903c166876d2e6bc365018026c803de5afa7405
FDSoftware <diego+gh@churrosoft.ar>
2025-05-27 02:04:29 -0300

    fix CannedTunes engine configuration tests only:one more

--- firmware/config/engines/bmw_m52.cpp
+++ firmware/config/engines/bmw_m52.cpp
@@ -5,4 +5,5 @@ void bmwM52() {
     engineConfiguration->ignitionMode = IM_WASTED_SPARK;
     engineConfiguration->cylindersCount = 6;
     engineConfiguration->displacement = 3;
+    engineConfiguration->firingOrder = FO_1_5_3_6_2_4;
 }
