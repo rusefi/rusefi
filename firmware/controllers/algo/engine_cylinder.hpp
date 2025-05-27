68b3d1d3b38dc2b5b6218baf818664824f3f2c58
FDSoftware <diego+gh@churrosoft.ar>
2025-05-27 01:27:00 -0300

    add OneCylinder base class

---
+++ firmware/controllers/algo/engine_cylinder.hpp
@@ -0,0 +1,37 @@
+/*
+ * @file engine_cylinder.hpp
+ *
+ * @date: may 25, 2025
+ * @author Matthew Kennedy, FDSoftware
+ */
+
+#ifndef ENGINE_CILINDER_HPP
+#define ENGINE_CILINDER_HPP
+#include "rusefi_types.h"
+
+class OneCylinder final {
+public:
+    void updateCylinderNumber(uint8_t index, uint8_t cylinderNumber);
+    void invalidCylinder();
+
+#if EFI_UNIT_TEST
+    bool getIsValid() const {
+      return m_valid;
+    }
+#endif
+
+private:
+    int m_index = 0;
+    bool m_valid = false;
+
+    // This cylinder's position in the firing order (0-based)
+    uint8_t m_cylinderIndex = 0;
+    // This cylinder's physical cylinder number (0-based)
+    uint8_t m_cylinderNumber = 0;
+};
+
+namespace EngineCylinders {
+    void updateCylinders(void);
+};
+
+#endif //ENGINE_CILINDER_HPP
