e16c43fb706da36b05b9efdf03fa3359d468c992
rusefillc <rusefillc@gmail.com>
2025-05-30 12:08:42 -0400

    only:uaefi

--- java_console/core_ui/src/main/java/com/rusefi/core/ui/ErrorMessageHelper.java
+++ java_console/core_ui/src/main/java/com/rusefi/core/ui/ErrorMessageHelper.java
@@ -8,8 +8,11 @@ public class ErrorMessageHelper {
     public static @NotNull JFrame createOnTopParent() {
         JFrame frame = new JFrame();
         // https://stackoverflow.com/questions/438463/joptionpane-wont-show-its-dialog-on-top-of-other-windows
+        frame.pack();
+        // https://stackoverflow.com/questions/65552872/how-do-i-center-a-jframe
+        // we need some magic to center since JOptionPane is placed in relation to parent
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-// ?        frame.setLocation(100, 100);
         frame.setAlwaysOnTop(true);
         return frame;
     }
