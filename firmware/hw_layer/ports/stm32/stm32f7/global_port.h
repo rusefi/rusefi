182bc63fe7f2458ad261f6cf8538be84414660d3
Andrey Gusakov <dron0gus@gmail.com>
2025-06-09 11:40:31 +0300

    ports: BKUP_RAM_NOINIT region for backuped data storing

--- firmware/hw_layer/ports/stm32/stm32f7/global_port.h
+++ firmware/hw_layer/ports/stm32/stm32f7/global_port.h
@@ -4,3 +4,5 @@
 #define SDRAM_OPTIONAL __attribute__((section(".ram7")))
 // SRAM2 is 16k and set to disable dcache
 #define NO_CACHE __attribute__((section(".ram2")))
+
+#define BKUP_RAM_NOINIT __attribute__((section(".bkup_ram_noinit")))
