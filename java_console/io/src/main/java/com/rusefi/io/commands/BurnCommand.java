c1e84539dd0ba1d79b027da3761923b637c01073
rugged <rusefillc@gmail.com>
2025-06-09 11:00:21 -0400

    burn has pages only:java

--- java_console/io/src/main/java/com/rusefi/io/commands/BurnCommand.java
+++ java_console/io/src/main/java/com/rusefi/io/commands/BurnCommand.java
@@ -7,8 +7,11 @@ import static com.rusefi.binaryprotocol.IoHelper.checkResponseCode;
 import static com.rusefi.config.generated.Integration.TS_RESPONSE_BURN_OK;
 
 public class BurnCommand {
+
+    private static final byte[] PAGE_0 = new byte[2];
+
     public static boolean execute(BinaryProtocol bp) {
-        byte[] response = bp.executeCommand(Integration.TS_BURN_COMMAND, "burn");
+        byte[] response = bp.executeCommand(Integration.TS_BURN_COMMAND, PAGE_0, "burn");
         boolean isExpectedBurnResponseCode = checkResponseCode(response, (byte) TS_RESPONSE_BURN_OK);
         boolean isExpectedBurnResponseLength = response.length == 1;
         return isExpectedBurnResponseCode && isExpectedBurnResponseLength;
