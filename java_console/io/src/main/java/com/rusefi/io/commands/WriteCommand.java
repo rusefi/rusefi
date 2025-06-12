e3608b375f04f585d6632c100d8a780e5a23e0a9
rusefillc <rusefillc@gmail.com>
2025-06-12 19:31:18 -0400

    Something is very broken around Lua editor #8064

--- java_console/io/src/main/java/com/rusefi/io/commands/WriteCommand.java
+++ java_console/io/src/main/java/com/rusefi/io/commands/WriteCommand.java
@@ -3,11 +3,12 @@ package com.rusefi.io.commands;
 import org.jetbrains.annotations.NotNull;
 
 public class WriteCommand {
+    private static final int HEADER_SIZE = 6;
     public static byte @NotNull [] getWritePacket(byte[] content, int contentOffset, int ecuOffset, int size) {
-        byte[] packet = new byte[6 + size];
+        byte[] packet = new byte[HEADER_SIZE + size];
         ByteRange.packPageOffsetAndSize(ecuOffset, size, packet);
 
-        System.arraycopy(content, contentOffset, packet, 4, size);
+        System.arraycopy(content, contentOffset, packet, HEADER_SIZE, size);
         return packet;
     }
 }
