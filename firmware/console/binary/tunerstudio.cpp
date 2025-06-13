rusefi/rusefi

commit 77a8d4354a11d17182ccc4c7666b58448e885e2b

TS: pages: write command

firmware/console/binary/tunerstudio.cpp
@@ -764,9 +764,11 @@ int TunerStudio::handleCrcCommand(TsChannelBase* tsChannel, char *data, int inco
 		handleExecuteCommand(tsChannel, data, incomingPacketSize - 1);
 		break;
 	case TS_CHUNK_WRITE_COMMAND:
-		// command with no page argument, default page = 0
-		handleWriteChunkCommand(tsChannel, page, offset, count, data + sizeof(TunerStudioRWChunkRequest));
-		calibrationsWriteTimer.reset();
+		/* command with page argument */
+		page = data16[0];
+		offset = data16[1];
+		count = data16[2];
+		handleWriteChunkCommand(tsChannel, page, offset, count, data + sizeof(TunerStudioPageRWChunkRequest));
 		break;
 	case TS_SINGLE_WRITE_COMMAND:
 		// command with no page argument, default page = 0

firmware/console/binary/tunerstudio.h
@@ -60,6 +60,12 @@ typedef struct {
 	uint16_t count;
 } TunerStudioRWChunkRequest;
 
+typedef struct {
+	uint16_t page;
+	uint16_t offset;
+	uint16_t count;
+} TunerStudioPageRWChunkRequest;
+
 #if EFI_PROD_CODE || EFI_SIMULATOR
 #define CONNECTIVITY_THREAD_STACK (3 * UTILITY_THREAD_STACK_SIZE)
 

firmware/tunerstudio/tunerstudio.template.ini
@@ -74,8 +74,8 @@ enable2ndByteCanID = false
 	pageSize			= @@persistent_config_s_size@@
 	pageReadCommand		= "@@TS_READ_COMMAND_char@@%2i%2o%2c"
 	burnCommand			= "@@TS_BURN_COMMAND_char@@%2i"
-	pageValueWrite		= "@@TS_SINGLE_WRITE_COMMAND_char@@%2o%v"
-	pageChunkWrite		= "@@TS_CHUNK_WRITE_COMMAND_char@@%2o%2c%v"
+	pageValueWrite		= "@@TS_CHUNK_WRITE_COMMAND_char@@%2i%2o%2c%v"
+	pageChunkWrite		= "@@TS_CHUNK_WRITE_COMMAND_char@@%2i%2o%2c%v"
 	crc32CheckCommand	= "@@TS_CRC_CHECK_COMMAND_char@@%2i%2o%2c"
 	retrieveConfigError = "e"
 

commit fad17e5511918b61b1761edd7b7680c64fe9f99e

tunerstudio.cpp: fix calibrationsWriteTimer
This fixes issue introduced in 77a8d43

firmware/console/binary/tunerstudio.cpp
@@ -267,6 +267,9 @@ void TunerStudio::handleWriteChunkCommand(TsChannelBase* tsChannel, uint16_t pag
 		// Force any board configuration options that humans shouldn't be able to change
 		// huh, why is this NOT within above 'needToTriggerTsRefresh()' condition?
 		setBoardConfigOverrides();
+
+		// we don't care about writes to scatter page
+		calibrationsWriteTimer.reset();
 #if EFI_TS_SCATTER
 	} else if (page == TS_PAGE_SCATTER_OFFSETS) {
 		// Ensure we are writing in bounds