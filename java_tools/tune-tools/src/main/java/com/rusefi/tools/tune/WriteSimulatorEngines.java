faf46b7cdb65822c254b2136a0723d8840fc51ed
rusefillc <rusefillc@gmail.com>
2025-06-15 08:48:50 -0400

    reducing defaults only:uaefi

--- java_tools/tune-tools/src/main/java/com/rusefi/tools/tune/WriteSimulatorEngines.java
+++ java_tools/tune-tools/src/main/java/com/rusefi/tools/tune/WriteSimulatorEngines.java
@@ -6,20 +6,20 @@ public class WriteSimulatorEngines {
     public static final engine_type_e[] ENGINE_TYPE_ES = {
         // [CannedTunes] see 'rusEfiFunctionalTest.cpp' which exports default tunes into binary files for us
         // [CannedTunes] TuneCanToolRunner for last third step
-        engine_type_e.MERCEDES_M111,
-        engine_type_e.BMW_M52,
-        engine_type_e.MAZDA_MIATA_NA6,
-        engine_type_e.MAZDA_MIATA_NA94,
-        engine_type_e.MAZDA_MIATA_NA96,
-        engine_type_e.MAZDA_MIATA_NB1,
-        engine_type_e.MAZDA_MIATA_NB2,
+//        engine_type_e.MERCEDES_M111,
+//        engine_type_e.BMW_M52,
+//        engine_type_e.MAZDA_MIATA_NA6,
+//        engine_type_e.MAZDA_MIATA_NA94,
+//        engine_type_e.MAZDA_MIATA_NA96,
+//        engine_type_e.MAZDA_MIATA_NB1,
+//        engine_type_e.MAZDA_MIATA_NB2,
         engine_type_e.HONDA_K,
         engine_type_e.HONDA_OBD1,
-        engine_type_e.HELLEN_121_NISSAN_6_CYL,
-        engine_type_e.HELLEN_154_HYUNDAI_COUPE_BK1,
-        engine_type_e.HELLEN_154_HYUNDAI_COUPE_BK2,
+//        engine_type_e.HELLEN_121_NISSAN_6_CYL,
+//        engine_type_e.HELLEN_154_HYUNDAI_COUPE_BK1,
+//        engine_type_e.HELLEN_154_HYUNDAI_COUPE_BK2,
         engine_type_e.HYUNDAI_PB,
-        engine_type_e.MAVERICK_X3,
-        engine_type_e.POLARIS_RZR,
+//        engine_type_e.MAVERICK_X3,
+//        engine_type_e.POLARIS_RZR,
     };
 }
