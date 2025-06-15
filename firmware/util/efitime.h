6f898ed19d81bde59f9a56d65b4e777c5871db2d
rusefillc <rusefillc@gmail.com>
2025-06-15 11:37:22 -0400

    todo: document pros and cons

--- firmware/util/efitime.h
+++ firmware/util/efitime.h
@@ -9,12 +9,9 @@
 
 #pragma once
 
-#include "efifeatures.h"
 #include "rusefi_types.h"
-#include "global.h"
 #include "error_handling.h"
 #include <rusefi/rusefi_time_math.h>
-#include <limits.h>
 
 
 #if EFI_PROD_CODE
@@ -26,11 +23,68 @@ inline int time2print(int64_t time) {
   return static_cast<int>(time);
 }
 
-inline int32_t assertFloatFitsInto32BitsAndCast(const char *msg, float value) {
-  if (value < INT_MIN || value > INT_MAX) {
-    criticalError("%s value out of range: %f", msg, value);
+// For the sole purpose of satisfying weird Mac OS + some Win compilers' stdlibs impl...
+constexpr bool constexpr_isfinite(float f) {
+#if __cplusplus >= 202302L
+  // In C++23, std::isfinite is officially constexpr
+  return std::isfinite(f);
+#elif defined(_WIN32) || defined(__APPLE__) || defined(__CYGWIN__) || defined(__MINGW32__) || defined(__MINGW64__)
+  // On Windows and macOS, std::isfinite might not be constexpr pre-C++23
+  return true;
+#else
+  // This is meant to correspond to normal/target platforms
+  return std::isfinite(f);
+#endif
+}
+
+constexpr bool _assertFloatFitsInto32BitsAndCast(float value) {
+  constexpr auto FirstUnrepresentableBigFloat = static_cast<float>(INT32_MAX);
+  constexpr auto kInt32MinF = static_cast<float>(INT32_MIN);
+
+  // 32bit float has 24bit precision that lead to fiasco like:
+  // 2147483648.0 > 2147483647 == false
+  // Also at 2147483648.0 we need 2^7=128 step to go to next value of 2147483904.0f
+  // And 2147483648.0f is already non-representable in 32 bit int
+  // So if someone using corner value of max int for smth like invalid float
+  // we might accidentally give green light if we are not strictly under 2147483648.0
+  // i.e. do not change this check to implicit convertion int->float with non strict condition!
+  return constexpr_isfinite(value) && value >= kInt32MinF && value < FirstUnrepresentableBigFloat;
+}
+
+static_assert(_assertFloatFitsInto32BitsAndCast(INT32_MAX) == false);
+static_assert(_assertFloatFitsInto32BitsAndCast(static_cast<float>(INT32_MAX)) == false);
+
+// 64 for int because during conversion it is rounding to nearest
+// i.e. -64 to +63 = 128 or 2^7 because 2^31 (int) / 2^24 (float)
+static_assert(_assertFloatFitsInto32BitsAndCast(INT32_MAX - 1) == false);
+static_assert(_assertFloatFitsInto32BitsAndCast(INT32_MAX - 63) == false);
+static_assert(_assertFloatFitsInto32BitsAndCast(INT32_MAX - 64) == true);
+static_assert(_assertFloatFitsInto32BitsAndCast(static_cast<float>(INT32_MAX - 1)) == false);
+static_assert(_assertFloatFitsInto32BitsAndCast(static_cast<float>(INT32_MAX - 63)) == false);
+static_assert(_assertFloatFitsInto32BitsAndCast(static_cast<float>(INT32_MAX - 64)) == true);
+
+// For INT32_MIN cases
+static_assert(_assertFloatFitsInto32BitsAndCast(INT32_MIN) == true);
+static_assert(_assertFloatFitsInto32BitsAndCast(static_cast<float>(INT32_MIN)) == true);
+static_assert(_assertFloatFitsInto32BitsAndCast(static_cast<float>(INT32_MIN) - 0.1f) == true);
+static_assert(_assertFloatFitsInto32BitsAndCast(static_cast<float>(static_cast<int64_t>(INT32_MIN))) == true);
+static_assert(_assertFloatFitsInto32BitsAndCast(static_cast<float>(static_cast<int64_t>(INT32_MIN) - 1)) == true);
+static_assert(_assertFloatFitsInto32BitsAndCast(static_cast<float>(static_cast<int64_t>(INT32_MIN) - 128)) == true);
+static_assert(_assertFloatFitsInto32BitsAndCast(static_cast<float>(static_cast<int64_t>(INT32_MIN) - 129)) == false);
+
+// Extreme negative float (definitely invalid)
+static_assert(_assertFloatFitsInto32BitsAndCast(-1e38f) == false);
+// NaN
+static_assert(!_assertFloatFitsInto32BitsAndCast(NAN));
+// -Infinity
+static_assert(!_assertFloatFitsInto32BitsAndCast(-INFINITY));
+
+constexpr int32_t assertFloatFitsInto32BitsAndCast(const char *msg, float value) {
+  if (!_assertFloatFitsInto32BitsAndCast(value)) {
+    criticalError("%s value is not representable in 32bit int: %f", msg, value);
   }
-  return (int32_t)value;
+
+  return static_cast<int32_t>(value);
 }
 
 inline /*64bit*/ efitick_t sumTickAndFloat(/*64bit*/efitick_t ticks, /*32bit*/float extra) {
