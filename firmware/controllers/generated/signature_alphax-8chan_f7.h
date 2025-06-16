5754836754232873d8566d1a3b3c003b8c83ae1c
GitHub gen-configs Action <action@github.com>
2025-06-16 18:18:33 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/signature_alphax-8chan_f7.h
+++ firmware/controllers/generated/signature_alphax-8chan_f7.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2309385770
-#define TS_SIGNATURE "rusEFI master.2025.06.16.alphax-8chan_f7.2309385770"
+#define SIGNATURE_HASH 548488653
+#define TS_SIGNATURE "rusEFI master.2025.06.16.alphax-8chan_f7.548488653"
--- firmware/controllers/generated/signature_proteus_f7.h
+++ firmware/controllers/generated/signature_proteus_f7.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1814072703
-#define TS_SIGNATURE "rusEFI master.2025.06.16.proteus_f7.1814072703"
+#define SIGNATURE_HASH 3550301517
+#define TS_SIGNATURE "rusEFI master.2025.06.16.proteus_f7.3550301517"
