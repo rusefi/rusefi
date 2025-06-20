430622b84661ae30aac256ada584b5428e74eecd
Anton Lantcevich <awpe00@gmail.com>
2025-06-20 21:35:37 +0200

    engine state up

--- firmware/controllers/algo/engine_state.h
+++ firmware/controllers/algo/engine_state.h
@@ -8,9 +8,9 @@
 
 #pragma once
 
-#include "global.h"
 #include "engine_parts.h"
 #include "engine_state_generated.h"
+#include "trigger_decoder.h"
 
 class EngineState : public engine_state_s {
 public:
@@ -21,17 +21,18 @@ public:
 	void updateSparkSkip();
 
 	/**
+	 * it's important for wrapAngle() that engineCycle field never has zero
 	 * always 360 or 720, never zero
 	 */
-	angle_t engineCycle;
+	angle_t engineCycle{getEngineCycle(FOUR_STROKE_CRANK_SENSOR)};
 
 	bool useOddFireWastedSpark = false;
 
 	float injectionStage2Fraction = 0;
 
-	Timer crankingTimer;
+	Timer crankingTimer{};
 
-	WarningCodeState warnings;
+	WarningCodeState warnings{};
 
 	// Estimated airflow based on whatever airmass model is active
 	float airflowEstimate = 0;
@@ -42,18 +43,18 @@ public:
 	/**
 	 * MAP averaging angle start, in relation to '0' trigger index index
 	 */
-	angle_t mapAveragingStart[MAX_CYLINDER_COUNT];
+	angle_t mapAveragingStart[MAX_CYLINDER_COUNT]{};
 	angle_t mapAveragingDuration = 0;
 
 	/**
 	 * timing advance is angle distance before Top Dead Center (TDP), i.e. "10 degree timing advance" means "happens 10 degrees before TDC"
 	 */
-	angle_t timingAdvance[MAX_CYLINDER_COUNT] = {0};
+	angle_t timingAdvance[MAX_CYLINDER_COUNT]{};
 
 	// Angle between firing the main (primary) spark and the secondary (trailing) spark
 	angle_t trailingSparkAngle = 0;
 
-	Timer timeSinceLastTChargeK;
+	Timer timeSinceLastTChargeK{};
 
 	/**
 	 * Raw fuel injection duration produced by current fuel algorithm, without any correction
@@ -75,7 +76,7 @@ public:
 
 	angle_t injectionOffset = 0;
 
-	multispark_state multispark;
+	multispark_state multispark{};
 
 	bool shouldUpdateInjectionTiming = true;
 };
