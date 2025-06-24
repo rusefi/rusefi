ba43e7a8135ae5d40fc524e8d5f42ffcb442d497
Andrey Gusakov <dron0gus@gmail.com>
2025-06-25 02:26:10 +0300

    trigger decoder: Jeep EVA 36-2-2

--- firmware/controllers/algo/engine_types.h
+++ firmware/controllers/algo/engine_types.h
@@ -234,13 +234,16 @@ enum class trigger_type_e : uint32_t {
 
   TT_TOYOTA_3_TOOTH_UZ = 91,
 
+	// See also TT_CHRYSLER_NGC_36_2_2
+	TT_JEEP_EVA_36_2_2 = 92,
+
 	// do not forget to edit "#define trigger_type_e_enum" line in integration/rusefi_config.txt file to propogate new value to rusefi.ini TS project
 	// do not forget to invoke "gen_config.bat" once you make changes to integration/rusefi_config.txt
 	// todo: one day a hero would integrate some of these things into Makefile in order to reduce manual magic
 	//
 	// Another point: once you add a new trigger, run get_trigger_images.bat which would run rusefi_test.exe from unit_tests
 	//
-	TT_UNUSED = 92, // this is used if we want to iterate over all trigger types
+	TT_UNUSED = 93, // this is used if we want to iterate over all trigger types
 };
 
 typedef enum {
