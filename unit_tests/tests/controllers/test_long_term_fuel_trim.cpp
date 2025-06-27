f80aafdddfdd23e19861c8e3aa50f3892b7dcab4
Andrey Gusakov <dron0gus@gmail.com>
2025-06-27 14:57:14 +0300

    unit test: LTFT: test on real logs

--- unit_tests/tests/controllers/test_long_term_fuel_trim.cpp
+++ unit_tests/tests/controllers/test_long_term_fuel_trim.cpp
@@ -83,3 +83,85 @@ TEST(LTFT, testLearning)
 	ASSERT_LTFT_RESULT(ltft.getTrims(10000, 1000), 1.0, 1.0);
 #endif
 }
+
+#include "mlq_reader.h"
+
+static constexpr bool verbose = false;
+
+static constexpr float executor_dt = FAST_CALLBACK_PERIOD_MS * 0.001f;
+
+TEST(LTFT, testRealLogLearning)
+{
+	EngineTestHelper eth(engine_type_e::FORD_ESCORT_GT);
+
+	// Allow learning only
+	engineConfiguration->ltft.enabled = true;
+	engineConfiguration->ltft.correctionEnabled = false;
+	engineConfiguration->ltft.deadband = 0.5; // %
+	engineConfiguration->ltft.maxAdd = 15.0; // %
+	engineConfiguration->ltft.maxRemove = 5; // %
+	engineConfiguration->ltft.timeConstant = 30; // seconds
+
+	BinarySensorReader reader;
+	reader.openMlq("pretty-happy-reference.mlg");
+
+	bool firstRun = true;
+	float prevTime = 0;
+	float dt = 0;
+
+	while (!reader.eof()) {
+		auto snapshot = reader.readBlock();
+
+		float time = snapshot["Time"];
+
+		if (!firstRun) {
+			// TODO:
+			// dt = time - prevTime;
+			// eth.moveTimeForwardSec(dt);
+			// eth.executeActions();
+
+			dt += time - prevTime;
+
+			size_t counter = 0;
+			while (dt > executor_dt) {
+				// TODO: adjust time
+				eth.moveTimeForwardSec(executor_dt);
+				// run the ignition math
+				engine->periodicFastCallback();
+				dt -= executor_dt;
+				counter++;
+			}
+
+			if (verbose) {
+				if (counter) {
+					std::cout << time << ": periodicFastCallback() executed " << counter << " times\n";
+				}
+
+				printf("%.3f: CLT: %3.0f, RPM %3.0f, MAP %3.0f, Lambdas: %f %f\n",
+					time,
+					snapshot["CLT"],
+					snapshot["RPM"],
+					snapshot["MAP"],
+					snapshot["Front Lambda"],
+					snapshot["Rear Lambda"]);
+			}
+		}
+
+		// TODO: find way to mock target AFR?
+
+		// now update sensors with new snapshot
+		Sensor::setMockValue(SensorType::Clt, snapshot["CLT"]);
+		Sensor::setMockValue(SensorType::Lambda1, snapshot["Front Lambda"]);
+		Sensor::setMockValue(SensorType::Lambda2, snapshot["Rear Lambda"]);
+		Sensor::setMockValue(SensorType::Rpm, snapshot["RPM"]);
+		Sensor::setMockValue(SensorType::Map, snapshot["MAP"]);
+
+		prevTime = time;
+		firstRun = false;
+	}
+
+	printf("LTFT test: miss: %d, hit %d, deadband %d\n",
+		engine->module<LongTermFuelTrim>()->ltftCntMiss,
+		engine->module<LongTermFuelTrim>()->ltftCntHit,
+		engine->module<LongTermFuelTrim>()->ltftCntDeadband);
+}
