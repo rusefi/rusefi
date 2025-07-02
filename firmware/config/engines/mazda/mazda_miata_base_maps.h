c9ca4da26a6060b8f048aea95cb8041f8e10c023
FDSoftware <diego+gh@churrosoft.ar>
2025-07-02 16:50:37 -0300

    remove unused mazda miata canned tuned fields

--- firmware/config/engines/mazda/mazda_miata_base_maps.h
+++ firmware/config/engines/mazda/mazda_miata_base_maps.h
@@ -10,7 +10,6 @@
 #include "global.h"
 
 void miataNA_setCrankingCycleBins();
-void miataNA_setCrankingFuelBins();
 void miataNA_setIacCoastingBins();
 void miataNA_setCltIdleCorrBins();
 void miataNA_setCltIdleRpmBins();
