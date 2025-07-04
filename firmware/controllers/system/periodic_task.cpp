dfaf803de00d11d7d260aa497e93cbf3ec3a53d0
Andrey Gusakov <dron0gus@gmail.com>
2025-07-04 18:35:34 +0300

    periodic_task: update for new ChibiOS API

--- firmware/controllers/system/periodic_task.cpp
+++ firmware/controllers/system/periodic_task.cpp
@@ -10,12 +10,12 @@
 #include "perf_trace.h"
 
 #if !EFI_UNIT_TEST
-void runAndScheduleNext(PeriodicTimerController *controller) {
+void runAndScheduleNext(virtual_timer_t *vtp, PeriodicTimerController *controller) {
 	{
 		ScopePerf perf(PE::PeriodicTimerControllerPeriodicTask);
 		controller->PeriodicTask();
 	}
 
-	chVTSetAny(&controller->timer, TIME_MS2I(controller->getPeriodMs()), (vtfunc_t) &runAndScheduleNext, controller);
+	chVTSetAny(vtp, TIME_MS2I(controller->getPeriodMs()), (vtfunc_t) &runAndScheduleNext, controller);
 }
 #endif /* EFI_UNIT_TEST */
