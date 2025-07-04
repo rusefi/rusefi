5cb3d1024c6c692c344e8ce26e5453c5a079f2d3
Andrey Gusakov <dron0gus@gmail.com>
2025-07-04 17:57:08 +0300

    ChibiOS update: STM32_PWM_USE_ADVANCED is gone

--- firmware/hw_layer/ports/stm32/stm32h7/cfg/mcuconf.h
+++ firmware/hw_layer/ports/stm32/stm32h7/cfg/mcuconf.h
@@ -361,7 +361,6 @@
 /*
  * PWM driver system settings.
  */
-#define STM32_PWM_USE_ADVANCED              FALSE
 #define STM32_PWM_USE_TIM1                  FALSE
 #define STM32_PWM_USE_TIM2                  FALSE
 
