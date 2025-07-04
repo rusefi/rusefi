9f97895161df94d4025524e72edb3d091b39d169
rusefillc <rusefillc@gmail.com>
2025-07-04 19:18:12 -0400

    only:unchanged

--- firmware/bin/assume_unchaged.sh
+++ firmware/bin/assume_unchaged.sh
@@ -3,6 +3,8 @@
 # todo: proper fix would be to split file so that board-specific stuff is not even here?
 git update-index --assume-unchanged ../java_console/models/src/main/java/com/rusefi/config/generated/VariableRegistryValues.java
 
+git update-index --assume-unchanged controllers/algo/auto*.*
 git update-index --assume-unchanged controllers/lua/generated/value_lookup_generated.*
 git update-index --assume-unchanged console/binary/generated/*
 git update-index --assume-unchanged controllers/generated/*
+git update-index --assume-unchanged hw_layer/mass_storage/ramdisk_image_compressed.h
