20ec155ed2c08ae8d75c946df0650dec6df709c8
GitHub gen-configs Action <action@github.com>
2025-07-09 23:09:57 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-4k-gdi.h
+++ firmware/controllers/generated/rusefi_generated_alphax-4k-gdi.h
@@ -2029,7 +2029,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.07.09.alphax-4k-gdi.3829189603"
+#define TS_SIGNATURE "rusEFI master.2025.07.10.alphax-4k-gdi.3829189603"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_alphax-gold.h
+++ firmware/controllers/generated/rusefi_generated_alphax-gold.h
@@ -2031,7 +2031,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.07.09.alphax-gold.2325942732"
+#define TS_SIGNATURE "rusEFI master.2025.07.10.alphax-gold.2325942732"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
