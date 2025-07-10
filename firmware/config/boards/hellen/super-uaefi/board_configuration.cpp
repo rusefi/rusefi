3031adece3364faade9b4b1a239b4ed25e568146
rusEFI LLC <rusefillc@gmail.com>
2025-07-09 22:06:56 -0400

    only:super-uaefi

--- firmware/config/boards/hellen/super-uaefi/board_configuration.cpp
+++ firmware/config/boards/hellen/super-uaefi/board_configuration.cpp
@@ -140,8 +140,12 @@ static Gpio OUTPUTS[] = {
 	Gpio::MM100_INJ6, // 8B INJ_6
 	Gpio::MM100_SPI2_CS, // 19B INJ_7
 	Gpio::MM100_SPI2_SCK, // 18B INJ_8
-	Gpio::MM100_INJ7,
-	Gpio::MM100_INJ8,
+	Gpio::MM100_INJ7, // 20B LS1
+	Gpio::MM100_INJ8, // 21B LS2
+	Gpio::MM100_OUT_PWM1, // 22B LS3
+	Gpio::MM100_OUT_PWM2, // 31C LS4
+	Gpio::MM100_IGN7, // 30C LS5_HOT
+	Gpio::MM100_IGN8, // 29C LS6_HOT
 /*
 	Gpio::MM100_INJ7, // B7 Low Side output 1
 	Gpio::MM100_IGN8, // B8 Fan Relay Weak Low Side output 2
