9bcb6a4bed5b4b641e85eab491bbbef7965b92ee
FDSoftware <diego+gh@churrosoft.ar>
2025-07-11 16:20:56 -0300

    correct working-directory on upload step, added cd to gcov_working_area on upload_coverage.sh

--- unit_tests/upload_coverage.sh
+++ unit_tests/upload_coverage.sh
@@ -3,7 +3,7 @@
 #
 # this script is used by github actions
 #
-
+cd gcov_working_area
 echo -e "\nUploading HTML"
 tar -czf - -C gcov . | sshpass -p "$RUSEFI_SSH_PASS" ssh -o StrictHostKeyChecking=no "$RUSEFI_SSH_USER"@"$RUSEFI_SSH_SERVER" "tar -xzf - -C docs/unit_tests_coverage"
 echo -e "\nHappy End."
