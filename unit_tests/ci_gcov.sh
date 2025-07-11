43bcc491986960f41cc4414deabaf41eb790a332
FDSoftware <diego+gh@churrosoft.ar>
2025-07-11 14:47:47 -0300

    LCOV => GCOVR

--- unit_tests/ci_gcov.sh
+++ unit_tests/ci_gcov.sh
@@ -8,27 +8,11 @@ rm -rf gcov_working_area
 
 mkdir gcov_working_area
 cd gcov_working_area
-
-echo "Looking for source code"
-find     ..                          -name *.c* >  source_files.txt
-find     ../../firmware/console/     -name *.c* >> source_files.txt
-find     ../../firmware/controllers/ -name *.c* >> source_files.txt
-
-wc -l source_files.txt
-
-xargs -L 1 -I {} cp {} . < source_files.txt
-
-cp ../build/obj/* .
+mkdir gcov
 
 echo -e "\nGenerating rusEFI unit test coverage"
-gcov *.c* > gcov.log 2>gcov.err
 
-echo -e "\nCollecting rusEFI unit test coverage"
-#FIXME: we have some problem related to google test macro "TEST" and the ouput code on lcov
-# todo gtest vs gcov https://github.com/rusefi/rusefi/issues/8129
-# Currently we cannot obtain coverage on the tests themselves, but on the tests towards the code.
-lcov --ignore-errors mismatch --capture --directory . --output-file coverage.info
-
-echo -e "\nGenerating rusEFI unit test HTML"
-genhtml coverage.info --output-directory gcov
-echo -e "\nGenerating rusEFI unit test HTML"
+# for debug use --html-details --html-single-page --verbose  to generate a single html
+gcovr --exclude-throw-branches --exclude-unreachable-branches --decisions \
+      --exclude '/.*/googletest/' \
+      -j4  -r ../.. --html-nested gcov/index.html
