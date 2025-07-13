1cacaaaf140452c458a8bfa84985d4ba0ff72851
GitHub gen-configs Action <action@github.com>
2025-07-13 09:46:54 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_f429-discovery.h
+++ firmware/controllers/generated/rusefi_generated_f429-discovery.h
@@ -2034,7 +2034,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.07.12.f429-discovery.3791887497"
+#define TS_SIGNATURE "rusEFI master.2025.07.13.f429-discovery.3791887497"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_proteus_h7.h
+++ firmware/controllers/generated/rusefi_generated_proteus_h7.h
@@ -2032,7 +2032,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.07.12.proteus_h7.2898641880"
+#define TS_SIGNATURE "rusEFI master.2025.07.13.proteus_h7.2898641880"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
