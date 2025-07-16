e5ee764226394ed75c37882337ce8355ae362a45
Andrey Gusakov <dron0gus@gmail.com>
2025-07-16 11:28:27 +0300

    Introduce EFI_STORAGE_SD

---
+++ firmware/controllers/storage_sd.h
@@ -0,0 +1,11 @@
+/**
+ * @file    storage_sd.h
+ * @brief   Storage interface to FatFS header
+ *
+ * @date Jul 16, 2025
+ * @author Andrey Gusakov
+ */
+
+#pragma once
+
+SettingStorageBase *initStorageSD();
