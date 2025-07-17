26fd68f0f92e6f83952be9af2b9ce23924190f72
Andrey Gusakov <dron0gus@gmail.com>
2025-07-17 12:50:51 +0300

    storage: extract internal flash code to separate storage driver

---
+++ firmware/controllers/storage_flash.h
@@ -0,0 +1,11 @@
+/**
+ * @file    storage_flash.h
+ * @brief   Storage interface to internal MCU flash header
+ *
+ * @date Sep 19, 2013
+ * @author Andrey Belomutskiy, (c) 2012-2025
+ */
+
+#pragma once
+
+SettingStorageBase *initStorageFlash();
