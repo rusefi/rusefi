7f9a3b17b8f48f436302fbf847f7c5ea224e750e
GitHub gen-configs Action <action@github.com>
2025-07-18 23:57:43 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-8chan-revA.h
+++ firmware/controllers/generated/rusefi_generated_alphax-8chan-revA.h
@@ -2040,7 +2040,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.07.18.alphax-8chan-revA.1317020130"
+#define TS_SIGNATURE "rusEFI master.2025.07.19.alphax-8chan-revA.1317020130"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_hellenNA6.h
+++ firmware/controllers/generated/rusefi_generated_hellenNA6.h
@@ -2043,7 +2043,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.07.18.hellenNA6.1909690884"
+#define TS_SIGNATURE "rusEFI master.2025.07.19.hellenNA6.1909690884"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
