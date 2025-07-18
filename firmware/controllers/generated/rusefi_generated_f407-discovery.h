e5e4cc21d36a16dde846b959391c90f67d61ae5f
GitHub gen-configs Action <action@github.com>
2025-07-18 13:30:06 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_f407-discovery.h
+++ firmware/controllers/generated/rusefi_generated_f407-discovery.h
@@ -1499,7 +1499,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 3363615085
+#define SIGNATURE_HASH 4270926177
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2040,7 +2040,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.07.18.f407-discovery.3363615085"
+#define TS_SIGNATURE "rusEFI master.2025.07.18.f407-discovery.4270926177"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
