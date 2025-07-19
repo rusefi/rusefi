a0da961512b3d5a33ca274f8856cfc341cbf515b
GitHub gen-configs Action <action@github.com>
2025-07-19 16:13:37 +0000

    Auto-generated configs and docs

--- firmware/live_data_generated/long_term_fuel_trim_state_generated.h
+++ firmware/live_data_generated/long_term_fuel_trim_state_generated.h
@@ -40,89 +40,90 @@ struct long_term_fuel_trim_state_s {
 	offset 12 bit 3 */
 	bool ltftLoadPending : 1 {};
 	/**
+	 * LTFT loading calibration error
 	offset 12 bit 4 */
-	bool unusedBit_7_4 : 1 {};
+	bool ltftLoadError : 1 {};
 	/**
 	offset 12 bit 5 */
-	bool unusedBit_7_5 : 1 {};
+	bool unusedBit_8_5 : 1 {};
 	/**
 	offset 12 bit 6 */
-	bool unusedBit_7_6 : 1 {};
+	bool unusedBit_8_6 : 1 {};
 	/**
 	offset 12 bit 7 */
-	bool unusedBit_7_7 : 1 {};
+	bool unusedBit_8_7 : 1 {};
 	/**
 	offset 12 bit 8 */
-	bool unusedBit_7_8 : 1 {};
+	bool unusedBit_8_8 : 1 {};
 	/**
 	offset 12 bit 9 */
-	bool unusedBit_7_9 : 1 {};
+	bool unusedBit_8_9 : 1 {};
 	/**
 	offset 12 bit 10 */
-	bool unusedBit_7_10 : 1 {};
+	bool unusedBit_8_10 : 1 {};
 	/**
 	offset 12 bit 11 */
-	bool unusedBit_7_11 : 1 {};
+	bool unusedBit_8_11 : 1 {};
 	/**
 	offset 12 bit 12 */
-	bool unusedBit_7_12 : 1 {};
+	bool unusedBit_8_12 : 1 {};
 	/**
 	offset 12 bit 13 */
-	bool unusedBit_7_13 : 1 {};
+	bool unusedBit_8_13 : 1 {};
 	/**
 	offset 12 bit 14 */
-	bool unusedBit_7_14 : 1 {};
+	bool unusedBit_8_14 : 1 {};
 	/**
 	offset 12 bit 15 */
-	bool unusedBit_7_15 : 1 {};
+	bool unusedBit_8_15 : 1 {};
 	/**
 	offset 12 bit 16 */
-	bool unusedBit_7_16 : 1 {};
+	bool unusedBit_8_16 : 1 {};
 	/**
 	offset 12 bit 17 */
-	bool unusedBit_7_17 : 1 {};
+	bool unusedBit_8_17 : 1 {};
 	/**
 	offset 12 bit 18 */
-	bool unusedBit_7_18 : 1 {};
+	bool unusedBit_8_18 : 1 {};
 	/**
 	offset 12 bit 19 */
-	bool unusedBit_7_19 : 1 {};
+	bool unusedBit_8_19 : 1 {};
 	/**
 	offset 12 bit 20 */
-	bool unusedBit_7_20 : 1 {};
+	bool unusedBit_8_20 : 1 {};
 	/**
 	offset 12 bit 21 */
-	bool unusedBit_7_21 : 1 {};
+	bool unusedBit_8_21 : 1 {};
 	/**
 	offset 12 bit 22 */
-	bool unusedBit_7_22 : 1 {};
+	bool unusedBit_8_22 : 1 {};
 	/**
 	offset 12 bit 23 */
-	bool unusedBit_7_23 : 1 {};
+	bool unusedBit_8_23 : 1 {};
 	/**
 	offset 12 bit 24 */
-	bool unusedBit_7_24 : 1 {};
+	bool unusedBit_8_24 : 1 {};
 	/**
 	offset 12 bit 25 */
-	bool unusedBit_7_25 : 1 {};
+	bool unusedBit_8_25 : 1 {};
 	/**
 	offset 12 bit 26 */
-	bool unusedBit_7_26 : 1 {};
+	bool unusedBit_8_26 : 1 {};
 	/**
 	offset 12 bit 27 */
-	bool unusedBit_7_27 : 1 {};
+	bool unusedBit_8_27 : 1 {};
 	/**
 	offset 12 bit 28 */
-	bool unusedBit_7_28 : 1 {};
+	bool unusedBit_8_28 : 1 {};
 	/**
 	offset 12 bit 29 */
-	bool unusedBit_7_29 : 1 {};
+	bool unusedBit_8_29 : 1 {};
 	/**
 	offset 12 bit 30 */
-	bool unusedBit_7_30 : 1 {};
+	bool unusedBit_8_30 : 1 {};
 	/**
 	offset 12 bit 31 */
-	bool unusedBit_7_31 : 1 {};
+	bool unusedBit_8_31 : 1 {};
 	/**
 	 * LTFT: Bank
 	 * units: %
