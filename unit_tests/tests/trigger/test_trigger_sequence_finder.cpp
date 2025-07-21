c8c54b2db74f06a58c8941971a12ec11e343da63
rusefillc <rusefillc@gmail.com>
2025-07-21 18:11:44 -0400

    A dream about trigger shapes: a tool to locate gap sequence based on logic analyzer file brute force #7947 only:progress

--- unit_tests/tests/trigger/test_trigger_sequence_finder.cpp
+++ unit_tests/tests/trigger/test_trigger_sequence_finder.cpp
@@ -2,81 +2,97 @@
 #include "trigger_simulator.h"
 #include "mock_trigger_configuration.h"
 
-static float wrap(float angle) {
+static float wrap(float angle, float cycle) {
 	if (angle < 0)
-		angle += 720;
+		angle += cycle;
 	return angle;
 }
+
+static int toothOffset = 0;
 
 static float getPos(TriggerWaveform *form, int index) {
 	if (index < 0) {
 		index += form->getSize() / 2;
 	}
-	return form->getSwitchAngle(2 * index + 1);
+	return form->getSwitchAngle(2 * index
+	+ toothOffset
+			);
 
 }
 
 static float ratios[400];
 
-TEST(trigger, finder) {
-    trigger_config_s triggerConfig;
-    triggerConfig.type = trigger_type_e::TT_VVT_BOSCH_QUICK_START;
+static void runTest(trigger_type_e t) {
+	EngineTestHelper eth(engine_type_e::TEST_ENGINE);
 
-    TriggerWaveform form;
-    form.initializeTriggerWaveform(FOUR_STROKE_CAM_SENSOR, triggerConfig, /*isCrank*/false);
+	trigger_config_s triggerConfig;
+	triggerConfig.type = t;
 
-    int toothCount = form.getSize() / 2;
+	TriggerWaveform form;
+	form.initializeTriggerWaveform(FOUR_STROKE_CAM_SENSOR, triggerConfig, /*isCrank*/
+	false);
 
-    for (int i = 0;i<toothCount;i++) {
-    	printf("%d %f\n", i, getPos(&form, i));
+	operation_mode_e om = form.getWheelOperationMode();
+	float cycle = 720 / getCrankDivider(om);
 
-    	float duration0 = wrap(getPos(&form, i) - getPos(&form, i - 1));
-    	float duration1 = wrap(getPos(&form, i - 1) - getPos(&form, i - 2));
+	int toothCount = form.getSize() / 2;
 
-    	float ratio = duration0 / duration1;
+	for (int i = 0; i < toothCount; i++) {
+//		printf("%d andle %f\n", i, getPos(&form, i));
 
-    	printf("%d ratio %f\n", i, ratio);
-    	ratios[i] = ratio;
-    }
+		float duration0 = wrap(getPos(&form, i) - getPos(&form, i - 1), cycle);
+		float duration1 = wrap(getPos(&form, i - 1) - getPos(&form, i - 2), cycle);
 
-    int length = 2;
+		float ratio = duration0 / duration1;
 
-    for (int sourceIndex =0;sourceIndex<toothCount;sourceIndex++) {
+		printf("%d ratio %f\n", i, ratio);
+		ratios[i] = ratio;
+	}
 
-    	for (int gapIndex = 0;gapIndex < length;gapIndex++) {
+	int length = 3;
 
-    		int ratioIndex = sourceIndex - gapIndex;
-    		if (ratioIndex < 0)
-    			ratioIndex += toothCount;
-    		float ratio = ratios[ratioIndex];
+	for (int sourceIndex = 0; sourceIndex < toothCount; sourceIndex++) {
 
-    		printf("trying %d ratio %f\n", gapIndex, ratio);
+		for (int gapIndex = 0; gapIndex < length; gapIndex++) {
 
-    		form.setTriggerSynchronizationGap4(gapIndex, ratio);
+			int ratioIndex = sourceIndex - gapIndex;
+			if (ratioIndex < 0)
+				ratioIndex += toothCount;
+			float ratio = ratios[ratioIndex];
 
-    	}
+			printf("trying %d ratio %f\n", gapIndex, ratio);
+
+			form.setTriggerSynchronizationGap4(gapIndex, ratio);
+
+		}
 
 		TriggerDecoderBase decoder("test");
 
-  MockTriggerConfiguration triggerConfiguration(triggerConfig);
-  triggerConfiguration.TriggerType = triggerConfig; // technical debt :(
-/*
-		try {
-			expected<uint32_t> res = TriggerStimulatorHelper::findTriggerSyncPoint(form,
-					triggerConfiguration,
-					decoder);
+		MockTriggerConfiguration triggerConfiguration(triggerConfig);
+		triggerConfiguration.TriggerType = triggerConfig; // technical debt :(
 
-			printf("GREAT %d\n", sourceIndex);
+		try {
+			form.setShapeDefinitionError(false);
+			uint32_t res = decoder.findTriggerZeroEventIndex(form,
+					triggerConfiguration);
+
+			if (!form.shapeDefinitionError)
+				printf("************** GREAT %d\n", sourceIndex);
 
 		} catch (...) {
 			printf("Not good %d\n", sourceIndex);
 		}
-*/
-    }
 
-
-
+	}
 
 //    setVerboseTrigger(true);
+
+}
+
+TEST(trigger, finder) {
+	toothOffset = 1;
+	runTest(trigger_type_e::TT_VVT_BOSCH_QUICK_START);
+	toothOffset = 0;
+	runTest(trigger_type_e::TT_GM_24x_3);
 
 }
