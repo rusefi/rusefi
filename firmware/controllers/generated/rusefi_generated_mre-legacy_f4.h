04c8ee05988354e0a23d6cd5f1c3d942eb049bab
GitHub gen-configs Action <action@github.com>
2025-07-23 08:43:05 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_mre-legacy_f4.h
+++ firmware/controllers/generated/rusefi_generated_mre-legacy_f4.h
@@ -2042,7 +2042,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.07.22.mre-legacy_f4.1236029262"
+#define TS_SIGNATURE "rusEFI master.2025.07.23.mre-legacy_f4.1236029262"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
