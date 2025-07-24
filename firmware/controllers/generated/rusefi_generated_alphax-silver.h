a1ddacb4fda64a3e0070207178b3491d28579b1b
GitHub gen-configs Action <action@github.com>
2025-07-24 09:28:44 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-silver.h
+++ firmware/controllers/generated/rusefi_generated_alphax-silver.h
@@ -2040,7 +2040,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.07.23.alphax-silver.3355019113"
+#define TS_SIGNATURE "rusEFI master.2025.07.24.alphax-silver.3355019113"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
