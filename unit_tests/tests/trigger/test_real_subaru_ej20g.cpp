962ad5a8e05653c06e237a73fa780e44e8a38d94
Andrey Gusakov <dron0gus@gmail.com>
2025-07-24 12:21:13 +0300

    unit_tests: trigger: add TT_SUBARU_7_WITHOUT_6 test
    only: unit_tests

--- unit_tests/tests/trigger/test_real_subaru_ej20g.cpp
+++ unit_tests/tests/trigger/test_real_subaru_ej20g.cpp
@@ -36,6 +36,39 @@ TEST(realSubaruEj20g, cranking) {
 	}
 }
 
+TEST(realSubaruEj20g, crankingCamOnly) {
+	CsvReader reader(/*triggerCount*/ 1, /* vvtCount */ 0);
+
+	/* 0 - cam */
+	reader.open("tests/trigger/resources/subaru_6_7.csv");
+
+	EngineTestHelper eth(engine_type_e::TEST_ENGINE);
+	//setVerboseTrigger(true);
+	eth.setTriggerType(trigger_type_e::TT_SUBARU_7_WITHOUT_6);
+	engineConfiguration->invertPrimaryTriggerSignal = true;
+	engineConfiguration->invertSecondaryTriggerSignal = true;
+
+	engineConfiguration->isFasterEngineSpinUpEnabled = true;
+	engineConfiguration->alwaysInstantRpm = true;
+
+	int n = 0;
+	bool firstRpm = false;
+	while (reader.haveMore()) {
+		reader.processLine(&eth);
+		//printf("%5d: RPM %f\n", n++, Sensor::getOrZero(SensorType::Rpm));
+		auto rpm = Sensor::getOrZero(SensorType::Rpm);
+		if ((rpm) && (!firstRpm)) {
+			printf("Got first RPM %f, at %d\n", rpm, n);
+			firstRpm = true;
+		}
+		if ((rpm == 0) && (firstRpm)) {
+			printf("Lost RPM at %d\n", n);
+			firstRpm = false;
+		}
+		n++;
+	}
+}
+
 TEST(realSubaruEj20g, crankingWot) {
 	CsvReader reader(/*triggerCount*/ 2, /* vvtCount */ 0);
 
