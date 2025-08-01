f63523616d926d5570746714be028cbbf3979e72
GitHub gen-configs Action <action@github.com>
2025-08-01 02:06:31 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_frankenso_na6.h
+++ firmware/controllers/generated/rusefi_generated_frankenso_na6.h
@@ -2059,7 +2059,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.07.31.frankenso_na6.4047746555"
+#define TS_SIGNATURE "rusEFI master.2025.08.01.frankenso_na6.4047746555"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_stm32f767_nucleo.h
+++ firmware/controllers/generated/rusefi_generated_stm32f767_nucleo.h
@@ -2059,7 +2059,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.07.31.stm32f767_nucleo.2400339366"
+#define TS_SIGNATURE "rusEFI master.2025.08.01.stm32f767_nucleo.2400339366"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
