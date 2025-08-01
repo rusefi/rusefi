a80236d6ffdd3702315f2c3495a3214ea0bef890
rusefi <rusefillc@gmail.com>
2025-07-31 22:26:54 -0400

    only:super-uaefi

--- firmware/config/boards/hellen/super-uaefi/board_configuration.cpp
+++ firmware/config/boards/hellen/super-uaefi/board_configuration.cpp
@@ -139,21 +139,13 @@ static Gpio OUTPUTS[] = {
 	Gpio::MM100_OUT_PWM2, // 31C LS4
 	Gpio::MM100_IGN7, // 30C LS5_HOT
 	Gpio::MM100_IGN8, // 29C LS6_HOT
-/*
-	Gpio::MM100_INJ7, // B7 Low Side output 1
-	Gpio::MM100_IGN8, // B8 Fan Relay Weak Low Side output 2
-	Gpio::MM100_IGN7, // B9 Main Relay Weak Low Side output 1
-	Gpio::MM100_OUT_PWM2, // B16 Low Side output 4 / Fuel Pump
-	Gpio::MM100_OUT_PWM1, // B17 Low Side output 3
-	Gpio::MM100_INJ8, // B18 Low Side output 2
-	// high sides
-	Gpio::MM100_IGN6, // B10 Coil 6
-	Gpio::MM100_IGN4, // B11 Coil 4
-	Gpio::MM100_IGN3, // B12 Coil 3
-	Gpio::MM100_IGN5, // B13 Coil 5
-	Gpio::MM100_IGN2, // B14 Coil 2
-	Gpio::MM100_IGN1, // B15 Coil 1
-*/
+	Gpio::MM100_IGN1, // Coil 1
+	Gpio::MM100_IGN2, // Coil 2
+	Gpio::MM100_IGN3, // Coil 3
+	Gpio::MM100_IGN4, // Coil 4
+	Gpio::MM100_IGN5, // Coil 5
+	Gpio::MM100_IGN6, // Coil 6
+	Gpio::MM100_LED2_GREEN, // 20D High Side Output
 };
 
 int getBoardMetaOutputsCount() {
@@ -161,7 +153,7 @@ int getBoardMetaOutputsCount() {
 }
 
 int getBoardMetaLowSideOutputsCount() {
-  return getBoardMetaOutputsCount() - 0;
+  return getBoardMetaOutputsCount() - 7;
 }
 
 Gpio* getBoardMetaOutputs() {
