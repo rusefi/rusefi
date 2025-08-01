b4bc2f9df8ddb84c9d32b95b50c9578b8f2e9219
Andrey Gusakov <dron0gus@gmail.com>
2025-08-01 18:23:30 +0300

    unit_tests: trigger: add VVT test for 6+7

--- unit_tests/tests/trigger/test_real_subaru_ej20g.cpp
+++ unit_tests/tests/trigger/test_real_subaru_ej20g.cpp
@@ -98,3 +98,51 @@ TEST(real, SubaruEj20gCrankingWot) {
 		n++;
 	}
 }
+
+TEST(real, SubaruEj20gDefaultCrankingSeparateTrigger) {
+	EngineCsvReader reader(/*triggerCount*/ 1, /* vvtCount */ 1);
+
+	/* 1 - cam
+	 * 0 - crank */
+	reader.open("tests/trigger/resources/subaru_6_7_crank_first.csv");
+	// test CSV file was captured without inversion of cam and crank signals.
+	// triggers are defined with SyncEdge::RiseOnly, while with real VR sensor we should rely on falling edges only.
+	// FallOnly is not supported (yet?). So trigger is defined as SyncEdge::RiseOnly and with 1 degree tooths. 
+	// setting both crank and cam inversion allows us to feed trigger decoder with correct (falling) edges.
+	// invertPrimaryTriggerSignal and invertCamVVTSignal do invertion twice: see https://github.com/rusefi/rusefi/issues/8373
+	// we keep both set to true to align with settings for real car.
+	reader.setFlipOnRead(true);
+	reader.setFlipVvtOnRead(true);
+
+	EngineTestHelper eth(engine_type_e::TEST_ENGINE);
+	//setVerboseTrigger(true);
+	engineConfiguration->vvtMode[0] = VVT_SUBARU_7TOOTH;
+	eth.setTriggerType(trigger_type_e::TT_SUBARU_7_6_CRANK);
+	engineConfiguration->invertPrimaryTriggerSignal = true;
+	engineConfiguration->invertCamVVTSignal = true;
+
+	engineConfiguration->isFasterEngineSpinUpEnabled = true;
+	engineConfiguration->alwaysInstantRpm = true;
+
+	bool gotFullSync = false;
+	while (reader.haveMore()) {
+		reader.processLine(&eth);
+
+		auto rpm = Sensor::getOrZero(SensorType::Rpm);
+		bool hasFullSync = getTriggerCentral()->triggerState.hasSynchronizedPhase();
+
+		reader.assertFirstRpm(182, /*expectedFirstRpmAtIndex*/13);
+		if (0) {
+			float vvt = engine->triggerCentral.getVVTPosition(/*bankIndex*/0, /*camIndex*/0);
+			printf("%5d: RPM %f, hasFullSync %d, vvt = %f\n", reader.lineIndex(), Sensor::getOrZero(SensorType::Rpm), hasFullSync, vvt);
+		}
+
+		if (!gotFullSync && hasFullSync && rpm) {
+			gotFullSync = true;
+
+			// Should get full sync and non-zero RPM
+			EXPECT_EQ(reader.lineIndex(), 36);
+			EXPECT_NEAR(rpm, 220.0f, 1.0f);
+		}
+	}
+}
