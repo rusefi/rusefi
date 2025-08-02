f2c1a0ae28428dfcb68837e9a081274973de6417
rusefillc <rusefillc@gmail.com>
2025-08-02 17:50:29 -0400

    only:extract

--- java_console/ui/src/main/java/com/rusefi/SerialPortScanner.java
+++ java_console/ui/src/main/java/com/rusefi/SerialPortScanner.java
@@ -1,7 +1,6 @@
 package com.rusefi;
 
 import com.devexperts.logging.Logging;
-import com.rusefi.binaryprotocol.IncomingDataBuffer;
 import com.rusefi.binaryprotocol.IoHelper;
 import com.rusefi.config.generated.Integration;
 import com.rusefi.io.IoStream;
@@ -269,34 +268,7 @@ public enum SerialPortScanner {
 
     private static boolean isPortOpenblt(String port) {
         try (IoStream stream = BufferedSerialIoStream.openPort(port)) {
-            if (stream == null) {
-                return false;
-            }
-
-            byte[] request = new byte[3];
-            request[0] = 2; // packet length
-            request[1] = (byte) 0xff; // XCPLOADER_CMD_CONNECT
-            request[2] = 0; // connectMode
-
-            stream.write(request);
-
-            IncomingDataBuffer idb = stream.getDataBuffer();
-
-            byte responseLength = idb.readByte(250);
-
-            // Invalid length, ignore
-            if (responseLength != 8) {
-                return false;
-            }
-
-            // Read length worth of bytes
-            byte[] response = new byte[responseLength];
-            idb.waitForBytes(100, "isPortOpenblt", System.currentTimeMillis(), responseLength);
-            idb.read(response);
-
-            // Response packet should start with FF
-            // Not much else to check, as the rest of the response is protocol settings from the device.
-            return response[0] == (byte) 0xFF;
+            return OpenbltDetectorStrategy.isPortOpenblt(stream);
         } catch (IOException e) {
             return false;
         }
