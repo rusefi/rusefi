bde13386a931e7b0198af55d97ff8ce3d6c06d2c
GitHub gen-configs Action <action@github.com>
2025-08-05 01:10:16 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-2chan.h
+++ firmware/controllers/generated/rusefi_generated_alphax-2chan.h
@@ -2056,7 +2056,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.08.04.alphax-2chan.3537996020"
+#define TS_SIGNATURE "rusEFI master.2025.08.05.alphax-2chan.3537996020"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_proteus_h7.h
+++ firmware/controllers/generated/rusefi_generated_proteus_h7.h
@@ -2057,7 +2057,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.08.04.proteus_h7.2328942700"
+#define TS_SIGNATURE "rusEFI master.2025.08.05.proteus_h7.2328942700"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
