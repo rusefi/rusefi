4fa31ddd5220f930fa591fbe9ca1eea831f323a6
GitHub gen-configs Action <action@github.com>
2025-08-07 11:57:28 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellen154hyundai_f7.h
+++ firmware/controllers/generated/rusefi_generated_hellen154hyundai_f7.h
@@ -2058,7 +2058,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.08.06.hellen154hyundai_f7.1444366163"
+#define TS_SIGNATURE "rusEFI master.2025.08.07.hellen154hyundai_f7.1444366163"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_mre_f7.h
+++ firmware/controllers/generated/rusefi_generated_mre_f7.h
@@ -2059,7 +2059,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.08.06.mre_f7.3135660262"
+#define TS_SIGNATURE "rusEFI master.2025.08.07.mre_f7.3135660262"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
