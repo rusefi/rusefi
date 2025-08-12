cf1e15362301da4e81c60c6165d7bd10a0f8f877
artemrusefi <artemrusefi@gmail.com>
2025-08-13 00:01:00 +0300

    Implement `PinOutputModeWithAndWithoutDollarTest` only:uaefi

---
+++ java_console/io/src/test/java/com/rusefi/io/pin_output_mode_with_and_without_dollar/PinOutputModeWithAndWithoutDollarTest.java
@@ -0,0 +1,49 @@
+package com.rusefi.io.pin_output_mode_with_and_without_dollar;
+
+import com.opensr5.ConfigurationImageWithMeta;
+import com.opensr5.ini.IniFileModelImpl;
+import com.opensr5.io.ConfigurationImageFile;
+import com.rusefi.maintenance.CalibrationsInfo;
+import com.rusefi.tune.xml.Msq;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PinOutputModeWithAndWithoutDollarTest {
+    private final static String TEST_DATA_FOLDER_PATH =
+        "src/test/java/com/rusefi/io/pin_output_mode_with_and_without_dollar/test_data";
+
+    @Test
+    void testNitrousRelayPinModeWithAndWithoutDollar() throws IOException {
+
+        final String NITROUS_RELAY_PIN_MODE_FIELD_NAME = "nitrousRelayPinMode";
+        final String EXPECTED_NITROUS_RELAY_PIN_MODE = "\"open collector inverted\"";
+
+        final ConfigurationImageWithMeta testConfigurationImage = ConfigurationImageFile.readFromFile(
+            String.format("%s/image.zip", TEST_DATA_FOLDER_PATH)
+        );
+
+        final CalibrationsInfo calibrationsInfoWithDollars = new CalibrationsInfo(
+            IniFileModelImpl.readIniFile(String.format("%s/rusefi_uaefi.ini", TEST_DATA_FOLDER_PATH)),
+            testConfigurationImage
+        );
+
+        final Msq msqWithDollar = calibrationsInfoWithDollars.generateMsq();
+        assertEquals(
+            EXPECTED_NITROUS_RELAY_PIN_MODE,
+            msqWithDollar.getConstantsAsMap().get(NITROUS_RELAY_PIN_MODE_FIELD_NAME).getValue()
+        );
+
+        final CalibrationsInfo calibrationsInfoWithoutDollars = new CalibrationsInfo(
+            IniFileModelImpl.readIniFile(String.format("%s/rusefi_uaefi_without_dollar.ini", TEST_DATA_FOLDER_PATH)),
+            testConfigurationImage
+        );
+        final Msq msqWithoutDollar = calibrationsInfoWithoutDollars.generateMsq();
+        assertEquals(
+            EXPECTED_NITROUS_RELAY_PIN_MODE,
+            msqWithoutDollar.getConstantsAsMap().get(NITROUS_RELAY_PIN_MODE_FIELD_NAME).getValue()
+        );
+    }
+}
