6fa2a378b1db257ecc4f5695a85ff07379e540ec
GitHub gen-configs Action <action@github.com>
2025-08-13 13:14:20 +0000

    Auto-generated configs and docs

--- firmware/live_data_generated/antilag_system_state_generated.h
+++ firmware/live_data_generated/antilag_system_state_generated.h
@@ -1,4 +1,4 @@
-// this section was generated automatically by rusEFI tool config_definition_base.jar based on (unknown script) controllers/algo/antilag_system_state.txt Wed Jan 03 20:30:10 UTC 2024
+// this section was generated automatically by rusEFI tool config_definition_base-all.jar based on (unknown script) controllers/algo/antilag_system_state.txt
 // by class com.rusefi.output.CHeaderConsumer
 // begin
 #pragma once
@@ -37,70 +37,70 @@ struct antilag_system_state_s {
 	bool ALSTimerCondition : 1 {};
 	/**
 	offset 0 bit 10 */
-	bool unusedBit_10_10 : 1 {};
+	bool luaAntilagState : 1 {};
 	/**
 	offset 0 bit 11 */
-	bool unusedBit_10_11 : 1 {};
+	bool unusedBit_11_11 : 1 {};
 	/**
 	offset 0 bit 12 */
-	bool unusedBit_10_12 : 1 {};
+	bool unusedBit_11_12 : 1 {};
 	/**
 	offset 0 bit 13 */
-	bool unusedBit_10_13 : 1 {};
+	bool unusedBit_11_13 : 1 {};
 	/**
 	offset 0 bit 14 */
-	bool unusedBit_10_14 : 1 {};
+	bool unusedBit_11_14 : 1 {};
 	/**
 	offset 0 bit 15 */
-	bool unusedBit_10_15 : 1 {};
+	bool unusedBit_11_15 : 1 {};
 	/**
 	offset 0 bit 16 */
-	bool unusedBit_10_16 : 1 {};
+	bool unusedBit_11_16 : 1 {};
 	/**
 	offset 0 bit 17 */
-	bool unusedBit_10_17 : 1 {};
+	bool unusedBit_11_17 : 1 {};
 	/**
 	offset 0 bit 18 */
-	bool unusedBit_10_18 : 1 {};
+	bool unusedBit_11_18 : 1 {};
 	/**
 	offset 0 bit 19 */
-	bool unusedBit_10_19 : 1 {};
+	bool unusedBit_11_19 : 1 {};
 	/**
 	offset 0 bit 20 */
-	bool unusedBit_10_20 : 1 {};
+	bool unusedBit_11_20 : 1 {};
 	/**
 	offset 0 bit 21 */
-	bool unusedBit_10_21 : 1 {};
+	bool unusedBit_11_21 : 1 {};
 	/**
 	offset 0 bit 22 */
-	bool unusedBit_10_22 : 1 {};
+	bool unusedBit_11_22 : 1 {};
 	/**
 	offset 0 bit 23 */
-	bool unusedBit_10_23 : 1 {};
+	bool unusedBit_11_23 : 1 {};
 	/**
 	offset 0 bit 24 */
-	bool unusedBit_10_24 : 1 {};
+	bool unusedBit_11_24 : 1 {};
 	/**
 	offset 0 bit 25 */
-	bool unusedBit_10_25 : 1 {};
+	bool unusedBit_11_25 : 1 {};
 	/**
 	offset 0 bit 26 */
-	bool unusedBit_10_26 : 1 {};
+	bool unusedBit_11_26 : 1 {};
 	/**
 	offset 0 bit 27 */
-	bool unusedBit_10_27 : 1 {};
+	bool unusedBit_11_27 : 1 {};
 	/**
 	offset 0 bit 28 */
-	bool unusedBit_10_28 : 1 {};
+	bool unusedBit_11_28 : 1 {};
 	/**
 	offset 0 bit 29 */
-	bool unusedBit_10_29 : 1 {};
+	bool unusedBit_11_29 : 1 {};
 	/**
 	offset 0 bit 30 */
-	bool unusedBit_10_30 : 1 {};
+	bool unusedBit_11_30 : 1 {};
 	/**
 	offset 0 bit 31 */
-	bool unusedBit_10_31 : 1 {};
+	bool unusedBit_11_31 : 1 {};
 	/**
 	 * offset 4
 	 */
@@ -119,4 +119,4 @@ struct antilag_system_state_s {
 static_assert(sizeof(antilag_system_state_s) == 12);
 
 // end
-// this section was generated automatically by rusEFI tool config_definition_base.jar based on (unknown script) controllers/algo/antilag_system_state.txt Wed Jan 03 20:30:10 UTC 2024
+// this section was generated automatically by rusEFI tool config_definition_base-all.jar based on (unknown script) controllers/algo/antilag_system_state.txt
