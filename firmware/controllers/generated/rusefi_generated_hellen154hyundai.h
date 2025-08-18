55cd5970b55d27bce46830bfd141938451f1c641
GitHub gen-configs Action <action@github.com>
2025-08-18 16:32:20 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellen154hyundai.h
+++ firmware/controllers/generated/rusefi_generated_hellen154hyundai.h
@@ -1514,7 +1514,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2207992845
+#define SIGNATURE_HASH 3312389163
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2066,7 +2066,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.08.18.hellen154hyundai.2207992845"
+#define TS_SIGNATURE "rusEFI master.2025.08.18.hellen154hyundai.3312389163"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_super-uaefi.h
+++ firmware/controllers/generated/rusefi_generated_super-uaefi.h
@@ -1514,7 +1514,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 3832674350
+#define SIGNATURE_HASH 2726672392
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2066,7 +2066,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.08.18.super-uaefi.3832674350"
+#define TS_SIGNATURE "rusEFI master.2025.08.18.super-uaefi.2726672392"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
