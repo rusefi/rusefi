abbf0ebf50a53b3570bbc8a13b4cf236a280b688
rusefillc <48498823+rusefillc@users.noreply.github.com>
2025-08-18 23:34:53 -0400

    NOISY_RPM cleanup (#8462)
    only:uaefi
    
    Co-authored-by: Matthew Kennedy <matthewkennedy@outlook.com>

--- firmware/controllers/engine_cycle/rpm_calculator.cpp
+++ firmware/controllers/engine_cycle/rpm_calculator.cpp
@@ -46,10 +46,6 @@ uint32_t RpmCalculator::getRevolutionCounterSinceStart(void) const {
 	return revolutionCounterSinceStart;
 }
 
-/**
- * @return -1 in case of isNoisySignal(), current RPM otherwise
- * See NOISY_RPM
- */
 float RpmCalculator::getCachedRpm() const {
 	return cachedRpmValue;
 }
@@ -157,6 +153,10 @@ void RpmCalculator::assignRpmValue(float floatRpmValue) {
 }
 
 void RpmCalculator::setRpmValue(float value) {
+	if (value > MAX_ALLOWED_RPM) {
+		value = 0;
+	}
+
 	assignRpmValue(value);
 	spinning_state_e oldState = state;
 	// Change state
@@ -280,7 +280,7 @@ void rpmShaftPositionCallback(trigger_event_e ckpSignalType,
 		 */
 			if (!alwaysInstantRpm) {
 				if (periodSeconds == 0) {
-					rpmState->setRpmValue(NOISY_RPM);
+					rpmState->setRpmValue(0);
 					rpmState->rpmRate = 0;
 				} else {
 				  // todo: extract utility method? see duplication with high_pressure_pump.cpp
@@ -290,7 +290,7 @@ void rpmShaftPositionCallback(trigger_event_e ckpSignalType,
 					auto rpmDelta = rpm - rpmState->previousRpmValue;
 					rpmState->rpmRate = rpmDelta / (mult * periodSeconds);
 
-					rpmState->setRpmValue(rpm > UNREALISTIC_RPM ? NOISY_RPM : rpm);
+					rpmState->setRpmValue(rpm);
 				}
 			}
 		} else {
@@ -363,7 +363,7 @@ void tdcMarkCallback(
 		int revIndex2 = getRevolutionCounter() % 2;
 		float rpm = Sensor::getOrZero(SensorType::Rpm);
 		// todo: use tooth event-based scheduling, not just time-based scheduling
-		if (isValidRpm(rpm)) {
+		if (rpm != 0) {
 			angle_t tdcPosition = tdcPosition();
 			// we need a positive angle offset here
 			wrapAngle(tdcPosition, "tdcPosition", ObdCode::CUSTOM_ERR_6553);
