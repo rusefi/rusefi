14a66c08ef86c8f06daf19cf3391b4172bf7e9a2
Andrey Gusakov <dron0gus@gmail.com>
2025-08-23 12:55:34 +0300

    ADC: add/extract helpers for ADCConversionGroup manipulations

---
+++ firmware/hw_layer/adc/adc_onchip.h
@@ -0,0 +1,18 @@
+/**
+ * @file	adc_onchip.h
+ * @brief	Low level ChibiOS ADC helpers header
+ *
+ * @date Aug 23, 2025
+ * @author Andrey Gusakov, (c) 2025
+ */
+
+#pragma once
+
+#include "global.h"
+
+#ifndef ADC_MAX_CHANNELS_COUNT
+#define ADC_MAX_CHANNELS_COUNT 16
+#endif /* ADC_MAX_CHANNELS_COUNT */
+
+int adcConversionGroupSetSeqInput(ADCConversionGroup* cfg, size_t sqn, size_t input);
+int adcConversionGroupGetSeqInput(ADCConversionGroup* cfg, size_t sqn);
