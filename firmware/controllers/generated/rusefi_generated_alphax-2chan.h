fff2e589fc2e71c822bdac297ac96275e6d667c0
GitHub gen-configs Action <action@github.com>
2025-08-24 03:05:35 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-2chan.h
+++ firmware/controllers/generated/rusefi_generated_alphax-2chan.h
@@ -2064,7 +2064,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.08.23.alphax-2chan.2930101797"
+#define TS_SIGNATURE "rusEFI master.2025.08.24.alphax-2chan.2930101797"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
