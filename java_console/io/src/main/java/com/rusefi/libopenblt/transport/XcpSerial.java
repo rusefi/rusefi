d02f9ddce1945983f4df859b6b179397646488b0
Alexey Esaulenko <esaulenka@gmail.com>
2025-08-28 12:41:41 +0300

    OpenBlt java client: add transport layer to the serial #8495

--- java_console/io/src/main/java/com/rusefi/libopenblt/transport/XcpSerial.java
+++ java_console/io/src/main/java/com/rusefi/libopenblt/transport/XcpSerial.java
@@ -68,16 +68,50 @@ public class XcpSerial implements IXcpTransport{
         synchronized (mLock) {
             checkPort();
 
-            mPort.writeBytes(request, request.length);
+            mPort.setComPortTimeouts(
+                SerialPort.TIMEOUT_READ_BLOCKING | SerialPort.TIMEOUT_WRITE_BLOCKING,
+                timeoutMs, timeoutMs);
 
-            byte[] response = new byte[expectResponseBytes];
-            int actualRead = mPort.readBytes(response, response.length);
+            // on serial there is a prefix byte both on request and response
+            byte[] buf = new byte[1 + request.length];
+            buf[0] = (byte)request.length;
+            System.arraycopy(request, 0, buf, 1, request.length);
 
-            if (actualRead != expectResponseBytes) {
-                throw new IllegalStateException("Unexpected bytes read, requested " + expectResponseBytes + " but got " + actualRead);
+            int actualWritten = mPort.writeBytes(buf, buf.length);
+            if (actualWritten != buf.length) {
+                throw new IllegalStateException("Cannot write to serial, expected " + buf.length + " turned out " + actualWritten);
+            }
+
+            byte[] responseLen = new byte[1];
+            int actualRead = mPort.readBytes(responseLen, 1);
+            if (actualRead != 1) {
+                throw new IllegalStateException("Cannot read response");
+            }
+
+            byte[] response = new byte[responseLen[0]];
+            actualRead = mPort.readBytes(response, response.length);
+            if (actualRead != expectResponseBytes ||
+                responseLen[0] != expectResponseBytes) {
+                throw new IllegalStateException("Unexpected bytes read on command " + Integer.toHexString(request[0] & 0xFF) +
+                    ", requested " + expectResponseBytes + " but got " + actualRead + ": " + hexlify(response));
             }
 
             return response;
         }
+    }
+
+    static private String hexlify(byte [] bytes) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : bytes) {
+            if (hexString.length() > 0) {
+                hexString.append(' ');
+            }
+            String hex = Integer.toHexString(b & 0xFF);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
     }
 }
