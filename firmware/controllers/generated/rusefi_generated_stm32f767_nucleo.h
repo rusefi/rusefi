859425823fd3e8e74dd7ba6c71cbfcddf0d391d1
GitHub gen-configs Action <action@github.com>
2025-08-30 01:47:45 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_stm32f767_nucleo.h
+++ firmware/controllers/generated/rusefi_generated_stm32f767_nucleo.h
@@ -2068,7 +2068,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.08.29.stm32f767_nucleo.3675237989"
+#define TS_SIGNATURE "rusEFI master.2025.08.30.stm32f767_nucleo.3675237989"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
