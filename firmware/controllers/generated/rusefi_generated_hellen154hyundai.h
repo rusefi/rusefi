f9d96fa163fff8698aab780c3dc944121aa383c8
GitHub gen-configs Action <action@github.com>
2025-08-31 02:40:05 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellen154hyundai.h
+++ firmware/controllers/generated/rusefi_generated_hellen154hyundai.h
@@ -1512,7 +1512,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2640377050
+#define SIGNATURE_HASH 704584211
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2066,7 +2066,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.08.31.hellen154hyundai.2640377050"
+#define TS_SIGNATURE "rusEFI master.2025.08.31.hellen154hyundai.704584211"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
