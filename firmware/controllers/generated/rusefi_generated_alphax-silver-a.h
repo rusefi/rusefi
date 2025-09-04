65a2b0329d6309ddb145669d4b56672a3531ce41
GitHub gen-configs Action <action@github.com>
2025-09-04 11:28:00 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-silver-a.h
+++ firmware/controllers/generated/rusefi_generated_alphax-silver-a.h
@@ -1512,7 +1512,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 898991392
+#define SIGNATURE_HASH 3220476708
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2066,7 +2066,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.09.04.alphax-silver-a.898991392"
+#define TS_SIGNATURE "rusEFI master.2025.09.04.alphax-silver-a.3220476708"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
