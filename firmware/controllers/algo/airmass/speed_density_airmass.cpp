9b52c98732c35e9b732173de1b0283ace9d577ff
AlphaX <normsp@gmail.com>
2025-09-09 12:47:48 -0400

    change blend duration to be grabbed from RPM corr table

--- firmware/controllers/algo/airmass/speed_density_airmass.cpp
+++ firmware/controllers/algo/airmass/speed_density_airmass.cpp
@@ -53,7 +53,12 @@ float SpeedDensityAirmass::getAirflow(float rpm, float map, bool postState) {
 }
 
 float SpeedDensityAirmass::getPredictiveMap(float rpm, bool postState, float mapSensor) {
-	float blendDuration = engineConfiguration->mapPredictionBlendDuration;
+	float blendDuration = = interpolate2d(rpm, config->tpsTspCorrValuesBins,
+						config->tpsTspCorrValues);
+	if ( blendDuration > 2) {
+		blendDuration = 1;
+	}
+
 	float elapsedTime = m_predictionTimer.getElapsedSeconds();
 
 	if (m_isMapPredictionActive && elapsedTime >= blendDuration) {
