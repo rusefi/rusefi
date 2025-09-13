480bfccf5be9d940bf1a613a77f560d00371d8a0
rusefillc <rusefillc@gmail.com>
2025-09-13 11:31:20 -0400

    FlatLaF looks nice!

--- java_console/ui/src/main/java/com/rusefi/ui/basic/UiHelper.java
+++ java_console/ui/src/main/java/com/rusefi/ui/basic/UiHelper.java
@@ -1,8 +1,11 @@
 package com.rusefi.ui.basic;
 
 import com.devexperts.logging.Logging;
+import com.formdev.flatlaf.FlatLightLaf;
 import com.rusefi.FileLog;
 import com.rusefi.ui.util.DefaultExceptionHandler;
+
+import javax.swing.*;
 
 import static com.devexperts.logging.Logging.getLogging;
 
@@ -12,6 +15,12 @@ public class UiHelper {
     public static void commonUiStartup() {
         log.info("OS name: " + FileLog.getOsName());
         log.info("OS version: " + System.getProperty(FileLog.OS_VERSION));
+
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
 
         DefaultExceptionHandler.install();
     }
