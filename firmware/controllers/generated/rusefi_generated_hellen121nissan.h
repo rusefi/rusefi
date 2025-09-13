3783c8fc7ddb3771b95d6798b8855a073b59c87a
GitHub gen-configs Action <action@github.com>
2025-09-13 15:13:42 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellen121nissan.h
+++ firmware/controllers/generated/rusefi_generated_hellen121nissan.h
@@ -1515,7 +1515,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2799943933
+#define SIGNATURE_HASH 730529826
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2072,7 +2072,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.09.13.hellen121nissan.2799943933"
+#define TS_SIGNATURE "rusEFI master.2025.09.13.hellen121nissan.730529826"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
