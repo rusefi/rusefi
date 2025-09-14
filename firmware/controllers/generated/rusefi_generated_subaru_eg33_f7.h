9d217ef95ecea86666236c0f4a701e058d42b670
GitHub gen-configs Action <action@github.com>
2025-09-14 21:02:21 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_subaru_eg33_f7.h
+++ firmware/controllers/generated/rusefi_generated_subaru_eg33_f7.h
@@ -1515,7 +1515,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2382768824
+#define SIGNATURE_HASH 2410699642
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2075,7 +2075,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.09.14.subaru_eg33_f7.2382768824"
+#define TS_SIGNATURE "rusEFI master.2025.09.14.subaru_eg33_f7.2410699642"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
