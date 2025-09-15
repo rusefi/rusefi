672a99e48cadf364bb1720fd1cd862d342c0b534
artemrusefi <artemrusefi@gmail.com>
2025-09-15 23:40:44 +0300

    Support migration in case of .ini field type/degits extension (closes #8585) only:uaefi

--- java_console/ui/src/main/java/com/rusefi/maintenance/migration/IniFieldMigrationUtils.java
+++ java_console/ui/src/main/java/com/rusefi/maintenance/migration/IniFieldMigrationUtils.java
@@ -1,13 +1,53 @@
 package com.rusefi.maintenance.migration;
 
 import com.rusefi.CompatibilitySet;
+import com.rusefi.config.FieldType;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
+import static com.rusefi.config.FieldType.*;
+
 public class IniFieldMigrationUtils {
+    public static boolean checkIfTypeCanBeMigrated(final FieldType prevType, final FieldType newType) {
+        if (Objects.equals(prevType, newType)) {
+            return true;
+        } else {
+            switch (prevType) {
+                case INT8: {
+                    switch (newType) {
+                        case INT16:
+                        case INT:
+                        case FLOAT: {
+                            return true;
+                        }
+                    }
+                }
+                case INT16: {
+                    switch (newType) {
+                        case INT:
+                        case FLOAT: {
+                            return true;
+                        }
+                    }
+                }
+                case UINT8: {
+                    switch (newType) {
+                        case UINT16:
+                        case INT16:
+                        case INT:
+                        case FLOAT: {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
 
     //ATTENTION! please add only lower-cased value in the list below:
     private final static List<Set<String>> UNITS_SYNONYMS = Arrays.asList(
@@ -33,5 +73,15 @@ public class IniFieldMigrationUtils {
             }
         }
         return false;
+    }
+
+    public static boolean checkIfDigitsCanBeMigrated(final String prevDigits, final String newDigits) {
+        if (Objects.equals(prevDigits, newDigits)) {
+            return true;
+        } else {
+            final int prevDecimalCount = Integer.parseInt(prevDigits);
+            final int newDecimalCount = Integer.parseInt(newDigits);
+            return prevDecimalCount <= newDecimalCount;
+        }
     }
 }
