a7ba4eb1a8f1269a24437c4efdf1a77ba75a5fac
rusefillc <rusefillc@gmail.com>
2025-09-17 18:24:45 -0400

    COMPATIBILITY MODE: older single-page firmware only:uaefi

--- java_console/io/src/main/java/com/rusefi/io/commands/BurnCommand.java
+++ java_console/io/src/main/java/com/rusefi/io/commands/BurnCommand.java
@@ -3,7 +3,6 @@ package com.rusefi.io.commands;
 import com.devexperts.logging.Logging;
 import com.rusefi.binaryprotocol.BinaryProtocol;
 import com.rusefi.config.generated.Integration;
-import com.rusefi.core.FindFileHelper;
 
 import static com.rusefi.binaryprotocol.IoHelper.checkResponseCode;
 import static com.rusefi.config.generated.Integration.TS_RESPONSE_BURN_OK;
@@ -13,7 +12,8 @@ public class BurnCommand {
     private static final byte[] PAGE_0 = new byte[2];
 
     public static boolean execute(BinaryProtocol bp) {
-        byte[] response = bp.executeCommand(Integration.TS_BURN_COMMAND, PAGE_0, "burn");
+        byte[] page = bp.isSinglePageController() ? null : PAGE_0;
+        byte[] response = bp.executeCommand(Integration.TS_BURN_COMMAND, page, "burn");
         boolean isExpectedBurnResponseCode = checkResponseCode(response, (byte) TS_RESPONSE_BURN_OK);
         if (response == null) {
             log.warn("empty BURN response on " + bp.getStream());
--- java_console/io/src/main/java/com/rusefi/io/commands/WriteCommand.java
+++ java_console/io/src/main/java/com/rusefi/io/commands/WriteCommand.java
@@ -1,14 +1,21 @@
 package com.rusefi.io.commands;
 
+import com.rusefi.binaryprotocol.BinaryProtocol;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * @see WriteChunkCommand
+ */
 public class WriteCommand {
-    private static final int HEADER_SIZE = 6;
-    public static byte @NotNull [] getWritePacket(byte[] content, int contentOffset, int ecuOffset, int size) {
-        byte[] packet = new byte[HEADER_SIZE + size];
-        ByteRange.packPageOffsetAndSize(ecuOffset, size, packet);
+    public static byte @NotNull [] getWritePacket(BinaryProtocol binaryProtocol, byte[] content, int contentOffset, int ecuOffset, int size) {
+        byte[] header = binaryProtocol.smartPacketPrefix(ecuOffset, size);
 
-        System.arraycopy(content, contentOffset, packet, HEADER_SIZE, size);
+        byte[] packet = new byte[header.length + size];
+        // dynamic size header first
+        System.arraycopy(header, 0, packet, 0, header.length);
+
+        // followed by payload
+        System.arraycopy(content, contentOffset, packet, header.length, size);
         return packet;
     }
 }
