670856fa3aab81e25d89042bde8307d60f56e451
GitHub gen-configs Action <action@github.com>
2025-09-21 13:04:32 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_stm32h743_nucleo.h
+++ firmware/controllers/generated/rusefi_generated_stm32h743_nucleo.h
@@ -2078,7 +2078,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.09.20.stm32h743_nucleo.1027912044"
+#define TS_SIGNATURE "rusEFI master.2025.09.21.stm32h743_nucleo.1027912044"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- java_console/models/src/main/java/com/rusefi/config/generated/VariableRegistryValues.java
+++ java_console/models/src/main/java/com/rusefi/config/generated/VariableRegistryValues.java
@@ -1716,7 +1716,7 @@ public class VariableRegistryValues {
 	public static final int TS_RESPONSE_UNRECOGNIZED_COMMAND = 0x83;
 	public static final int TS_SCATTER_OFFSETS_COUNT = 128;
 	public static final char TS_SET_LOGGER_SWITCH = 'l';
-	public static final String TS_SIGNATURE = "rusEFI master.2025.09.20.f429-discovery.4053092954";
+	public static final String TS_SIGNATURE = "rusEFI master.2025.09.21.f429-discovery.4053092954";
 	public static final char TS_SIMULATE_CAN = '>';
 	public static final char TS_TEST_COMMAND = 't';
 	public static final int TS_TOTAL_OUTPUT_SIZE = 2060;
