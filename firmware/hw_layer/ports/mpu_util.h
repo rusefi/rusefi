088617b3d085ca7f416f22e573231160f0a52599
rusefillc <rusefillc@gmail.com>
2025-09-22 09:07:59 -0400

    openblt BL03 only:uaefi

--- firmware/hw_layer/ports/mpu_util.h
+++ firmware/hw_layer/ports/mpu_util.h
@@ -137,8 +137,8 @@ void HardFaultVector(void);
 #endif /* __cplusplus */
 
 // search:openblt_version
-// ascii 'BL02' in reverse LBS byte order
-#define BLT_CURRENT_VERSION 0x32304C42
+// ascii 'BL03' in reverse LBS byte order
+#define BLT_CURRENT_VERSION 0x33304C42
 #define BLT_BIN_VERSION_ADDR              ((uint32_t)0x08000024U)       /*! 3rd reserved DWORD in vector table search:openblt_version */
 
 #if EFI_USE_OPENBLT
