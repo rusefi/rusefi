b34ca1f12cc558edba0b9b0abfe5bf68624b13da
FDSoftware <diego+gh@churrosoft.ar>
2025-09-22 00:35:02 -0300

    #8576 digits migrator fix migrateField on DigitsIniFieldMigrator.java update DigitIniFieldMigratorTest to use DigitIniFieldMigrationContext

---
+++ java_console/ui/src/main/java/com/rusefi/maintenance/migration/DigitsIniFieldMigrator.java
@@ -0,0 +1,112 @@
+package com.rusefi.maintenance.migration;
+
+import com.devexperts.logging.Logging;
+import com.opensr5.ini.field.IniField;
+import com.rusefi.tune.xml.Constant;
+
+import java.util.Locale;
+import java.util.Optional;
+
+import static com.devexperts.logging.Logging.getLogging;
+import static javax.management.ObjectName.quote;
+import static javax.management.ObjectName.unquote;
+
+import java.text.MessageFormat;
+
+//note: this migration is different from the one on IniFieldMigratorUtils.java,
+//here we execute a lambda for every field to migrate to patch the old scale to the new
+
+interface DigitMigrator {
+   Double doMigration(Double prevValue);
+}
+
+class IntDigitField {
+    private String fieldName;           // ex: ppsExpAverageAlpha
+    private String expectedPreviousDigits; // ex: 3
+    private String expectedNewDigits;      // ex: 1
+    private DigitMigrator migrator;     // lambda used to migrate the field, unique param is the old value
+
+    public IntDigitField(String fieldName, DigitMigrator migrator, String expectedPreviousDigits, String expectedNewDigits ) {
+        this.fieldName = fieldName;
+        this.migrator = migrator;
+        this.expectedPreviousDigits = expectedPreviousDigits;
+        this.expectedNewDigits = expectedNewDigits;
+    }
+
+    public Optional<Boolean> shouldMigrateField(String previousDigits, String newDigits) {
+        // "we want to migrate only if prev/new are equal to the defined ones on this class initiation and they are different"
+        boolean isValidToMigrate = previousDigits.startsWith(this.expectedPreviousDigits) && newDigits.startsWith(this.expectedNewDigits) && !previousDigits.startsWith(newDigits);
+        // ie: already migrated on previous run/migration/etc
+        boolean isMigratedValue = previousDigits == newDigits;
+
+        if (!isValidToMigrate && !isMigratedValue) {
+            // invalid!, error on migration code? or on updated .ini?
+            return Optional.empty();
+        }
+        return Optional.of(isValidToMigrate);
+    }
+
+    public String migrateField(String oldTuneFieldValue) {
+        // "12" => 12
+        Double fieldValue =  Double.parseDouble(oldTuneFieldValue);
+        
+        // 12 => 12.456
+        Double migratedField = this.migrator.doMigration(fieldValue);
+        
+        // migratedField = 12.456; expectedNewDigits = 2; => 12.45
+        String migratedValue = String.format(Locale.ROOT, MessageFormat.format("%.{0}f", this.expectedNewDigits), migratedField);
+        
+        return migratedValue;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+}
+
+public enum DigitsIniFieldMigrator implements TuneMigrator {
+    INSTANCE;
+
+    private static final Logging log = getLogging(DigitsIniFieldMigrator.class);
+
+    private static final IntDigitField[] fieldsToMigrate = {
+        // List all the fields to migrate here:
+        new IntDigitField("ppsExpAverageAlpha", (v) -> v * 100, "3" , "1")
+    };
+
+    @Override
+    public void migrateTune(final TuneMigrationContext context) {
+        for (IntDigitField field : fieldsToMigrate) {
+
+            final Optional<IniField> prevBooleanIniField = context.getPrevIniFile().findIniField(field.getFieldName());
+            final Optional<IniField> updatedBooleanIniField = context.getUpdatedIniFile().findIniField(field.getFieldName());
+
+            if (!prevBooleanIniField.isPresent() || !updatedBooleanIniField.isPresent()) {
+                continue;
+            }
+
+            final Constant prevBooleanConst = context.getPrevTune().getConstantsAsMap().get(field.getFieldName());
+            final Constant updatedBooleanConst = context.getUpdatedTune().getConstantsAsMap().get(field.getFieldName());
+            final String prevBooleanValue = prevBooleanConst.getValue();
+
+            if ((null == prevBooleanConst) || (null == updatedBooleanConst)) {
+                continue;
+            }
+
+            Optional<Boolean> shouldMigrate = field.shouldMigrateField(prevBooleanConst.getDigits(), updatedBooleanConst.getDigits());
+
+            // check if migration of this value is possible/valid
+            if (shouldMigrate.isPresent()) {
+                if(shouldMigrate.get()) {
+                    final Constant migratedBooleanConst = prevBooleanConst.cloneWithValue(
+                        String.valueOf(field.migrateField(prevBooleanValue))
+                    );
+                    context.addMigration(field.getFieldName(), migratedBooleanConst);
+                }
+            } else {
+                log.warn(String.format("Invalid scalar migration on oldTune `%s` :(", field.getFieldName()));
+            }
+
+        }
+    }
+}
