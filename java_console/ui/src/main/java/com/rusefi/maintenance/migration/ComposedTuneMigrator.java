53f9b7348e1de2a1bca42678d4aa618072c0c3a7
artemrusefi <artemrusefi@gmail.com>
2025-09-27 23:50:29 +0300

    refactoring: now we have a singleton for `veTable` migrator (closes #8644) only:uaefi

--- java_console/ui/src/main/java/com/rusefi/maintenance/migration/ComposedTuneMigrator.java
+++ java_console/ui/src/main/java/com/rusefi/maintenance/migration/ComposedTuneMigrator.java
@@ -1,20 +1,16 @@
 package com.rusefi.maintenance.migration;
 
-import com.rusefi.config.FieldType;
 import com.rusefi.maintenance.DefaultTuneMigrator;
 import com.rusefi.ts.TsProjectUpdater;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static com.rusefi.maintenance.migration.TableAddColumnsMigrator.VE_RPM_BINS_FIELD_NAME;
-import static com.rusefi.maintenance.migration.TableAddColumnsMigrator.VE_TABLE_FIELD_NAME;
-
 public enum ComposedTuneMigrator implements TuneMigrator {
     INSTANCE;
 
     private final List<TuneMigrator> tuneMigrators = Arrays.asList(
-        new TableAddColumnsMigrator(VE_TABLE_FIELD_NAME, FieldType.UINT16, VE_RPM_BINS_FIELD_NAME),
+        TableAddColumnsMigrator.VE_TABLE_MIGRATOR,
         BattLagCorrExtensionMigrator.INSTANCE,
         CltIdleCorrMigrator.INSTANCE,
         DisplacementIniFieldMigrator.INSTANCE,
