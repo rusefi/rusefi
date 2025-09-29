4bac00be8f29e2bcf4f16f2da51cf09771f304e5
Andrey Gusakov <dron0gus@gmail.com>
2025-09-29 16:26:39 +0300

    unit test: add flex sensor test

--- unit_tests/tests/sensor/test_flex_sensor.cpp
+++ unit_tests/tests/sensor/test_flex_sensor.cpp
@@ -22,3 +22,23 @@ TEST(FlexSensorTest, highPinStart) {
 
 	ASSERT_EQ(0u, getRecentWarnings()->getCount());
 }
+
+TEST(FlexSensorTest, converter) {
+	efitick_t nowNt = 0;
+	EngineTestHelper eth(engine_type_e::TEST_ENGINE);
+	FlexSensor sensor(MS2NT(1000));
+	sensor.Register(true);
+
+	// feed BiQuad filters and wait it get close to expected values
+	for (size_t i = 0; i < 1000; i++) {
+		sensor.callback(getTimeNowNt(), 1);
+		// emulate 25 degrees C
+		eth.moveTimeForwardUs(2576);
+		sensor.callback(getTimeNowNt(), 0);
+		// emulate 100 Hz = 50% ethanol
+		eth.moveTimeForwardUs(10000 - 2576);
+	}
+
+	EXPECT_NEAR(50, Sensor::getOrZero(SensorType::FuelEthanolPercent), 0.1);
+	EXPECT_NEAR(25, Sensor::getOrZero(SensorType::FuelTemperature), 0.1);
+}
