7c1979b443b71c1da9d21dd339012145da651327
Andrey Gusakov <dron0gus@gmail.com>
2025-09-29 12:53:36 +0300

    flex sensor: extract testable code to flex_sensor.cpp

--- firmware/init/sensor/init_flex.cpp
+++ firmware/init/sensor/init_flex.cpp
@@ -7,55 +7,13 @@
 #include "digital_input_exti.h"
 
 // 30 sec timeout to avoid issues around cranking
-static FunctionalSensor flexSensor(SensorType::FuelEthanolPercent, MS2NT(30000));
-static StoredValueSensor flexFuelTemp(SensorType::FuelTemperature, MS2NT(30000));
-static FlexConverter converter;
+static FlexSensor flexSensor(MS2NT(30000));
 
-static Biquad flexTempFilter;
 #if EFI_PROD_CODE
-
-static Timer flexFreq, flexPulse;
-
-int flexCallbackCounter = 0;
-static int lowFlexCallbackCounter = 0;
-static float frequency;
-static float pulseWidthUs;
-static efitick_t latestCallbackTime;
-
-static void flexCallback(efitick_t nowNt, bool value) {
-    latestCallbackTime = nowNt;
-    flexCallbackCounter++;
-	if (value) {
-		frequency = 1 / flexFreq.getElapsedSecondsAndReset(nowNt);
-		flexSensor.postRawValue(frequency, nowNt);
-
-		// Start timing pulse width on rising edge
-		flexPulse.reset(nowNt);
-	} else {
-	    lowFlexCallbackCounter++;
-		// End pulse timing on falling edge
-		pulseWidthUs = flexPulse.getElapsedUs(nowNt);
-
-		if (pulseWidthUs < 900) {
-			flexFuelTemp.invalidate(UnexpectedCode::Low);
-			warning(ObdCode::CUSTOM_FLEX_LOW, "flex low %f", pulseWidthUs);
-		} else if (pulseWidthUs > 5100) {
-			flexFuelTemp.invalidate(UnexpectedCode::High);
-			warning(ObdCode::CUSTOM_FLEX_HIGH, "flex high %f", pulseWidthUs);
-		} else {
-			// -40C = 1000us
-			// 125C = 5000us
-			float tempC = interpolateClamped(1000, -40, 5000, 125, pulseWidthUs);
-			tempC = flexTempFilter.filter(tempC);
-			flexFuelTemp.setValidValue(tempC, nowNt);
-		}
-	}
-}
-
 static Gpio flexPin = Gpio::Unassigned;
 
 static void flexExtiCallback(void*, efitick_t nowNt) {
-	flexCallback(nowNt, efiReadPin(flexPin) ^ engineConfiguration->flexSensorInverted);
+	flexSensor.callback(nowNt, efiReadPin(flexPin) ^ engineConfiguration->flexSensorInverted);
 }
 #endif
 
@@ -68,36 +26,20 @@ void initFlexSensor(bool isFirstTime) {
 		return;
 	}
 	flexPin = engineConfiguration->flexSensorPin;
-#endif
 
-	// 0.01 means filter bandwidth of ~1hz with ~100hz sensor
-	flexTempFilter.configureLowpass(1, 0.01f);
-	flexSensor.setFunction(converter);
-
-#if EFI_PROD_CODE
-    if (isFirstTime) {
-	    addConsoleAction("flexinfo", []() {
-	        efiPrintf("flex counter %d", flexCallbackCounter);
-	        efiPrintf("lowFlexCallbackCounter counter %d", lowFlexCallbackCounter);
-	        efiPrintf("flex freq %f", frequency);
-	        efiPrintf("pulseWidthUs %f", pulseWidthUs);
-	        efiPrintf("latestCallbackTime %lld", latestCallbackTime);
-	    });
+	if (isFirstTime) {
+		addConsoleAction("flexinfo", []() {
+			flexSensor.debug();
+		});
 	}
-
 #endif // EFI_PROD_CODE
 
-	flexSensor.Register();
-
 	// If an analog fuel temp sensor is configured, don't use the flex sensor for fuel temp
-	if (!isAdcChannelValid(engineConfiguration->fuelTempSensor.adcChannel)) {
-		flexFuelTemp.Register();
-	}
+	flexSensor.Register(!isAdcChannelValid(engineConfiguration->fuelTempSensor.adcChannel));
 }
 
 void deInitFlexSensor() {
 	flexSensor.unregister();
-	flexFuelTemp.unregister();
 
 #if EFI_PROD_CODE
 	if (!isBrainPinValid(flexPin)) {
