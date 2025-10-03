4130c48a0121e589a249e4e6798057f48c0f2f39
GitHub gen-configs Action <action@github.com>
2025-10-03 03:04:09 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellen72.h
+++ firmware/controllers/generated/rusefi_generated_hellen72.h
@@ -1538,7 +1538,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 1744699743
+#define SIGNATURE_HASH 2616017987
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2104,7 +2104,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.10.03.hellen72.1744699743"
+#define TS_SIGNATURE "rusEFI master.2025.10.03.hellen72.2616017987"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
