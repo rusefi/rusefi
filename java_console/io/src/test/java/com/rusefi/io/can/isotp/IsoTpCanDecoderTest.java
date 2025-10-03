2dcb0c1af9e569bb94a8482b65b6eea233738eca
rusefillc <rusefillc@gmail.com>
2025-10-03 13:07:50 -0400

    IsoTpCanDecoderTest #8677 only:74.9

--- java_console/io/src/test/java/com/rusefi/io/can/isotp/IsoTpCanDecoderTest.java
+++ java_console/io/src/test/java/com/rusefi/io/can/isotp/IsoTpCanDecoderTest.java
@@ -17,7 +17,10 @@ public class IsoTpCanDecoderTest {
     @Test
     public void decodeMultiFrame() {
         IsoTpCanDecoder decoder = new IsoTpCanDecoder();
-        byte[] result1 = decoder.decodePacket(new byte[]{0x14, 1, 2, 3, 4});
+        byte[] result1 = decoder.decodePacket(new byte[]{0x10, 0x0B, 0x59, 0x02, (byte) 0x89, (byte) 0xA1, 0x41, 0x00  });
+        assertTrue(Arrays.equals(result1, new byte[]{ 0x59, 0x02, (byte) 0x89, (byte) 0xA1, 0x41, 0x00}));
 
+        byte[] result2 = decoder.decodePacket(new byte[]{0x21, 0x08, (byte) 0xA1, 0x46, 0x00, 0x08, 0x00, 0x00  });
+        assertTrue(Arrays.equals(result2, new byte[]{ 0x08, (byte) 0xA1, 0x46, 0x00, 0x08}));
     }
 }
