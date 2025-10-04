480a5885b65c6127dc5c4df83891a14b5110ce7a
GitHub gen-configs Action <action@github.com>
2025-10-04 10:41:18 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellenNA8_96.h
+++ firmware/controllers/generated/rusefi_generated_hellenNA8_96.h
@@ -2104,7 +2104,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.10.03.hellenNA8_96.1721299031"
+#define TS_SIGNATURE "rusEFI master.2025.10.04.hellenNA8_96.1721299031"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_mre_f4.h
+++ firmware/controllers/generated/rusefi_generated_mre_f4.h
@@ -2103,7 +2103,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.10.03.mre_f4.363499159"
+#define TS_SIGNATURE "rusEFI master.2025.10.04.mre_f4.363499159"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
