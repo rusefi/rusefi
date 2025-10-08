4cd30a06db4083bc06349fe94f22a63a31970d06
rusefillc <rusefillc@gmail.com>
2025-10-07 21:58:53 -0400

    Tune Library Columns only:uaefi
    (cherry picked from commit c625eef01a7dad27ad71fe3ca49405ac4142c424)

--- java_console/ui/src/main/java/com/rusefi/tune_manifest/TuneModel.java
+++ java_console/ui/src/main/java/com/rusefi/tune_manifest/TuneModel.java
@@ -5,18 +5,27 @@ import org.json.simple.JSONObject;
 
 public class TuneModel {
     private final String notes;
+    private final int size;
     private final String url;
+    private final String mapId;
+    private final String fileName;
     private boolean error;
 
-    public TuneModel(String notes, String url) {
+    public TuneModel(String notes, String url, int size, String mapId, String fileName) {
         this.notes = notes;
+        this.size = size;
         this.url = url;
+        this.mapId = mapId;
+        this.fileName = fileName;
     }
 
     static TuneModel parse(JSONObject object) {
+        String mapId = (String) object.get("MapID");
         String notes = (String) object.get("Notes");
         String url = (String) object.get("Url");
-        return new TuneModel(notes, url);
+        int size = Integer.valueOf((String)object.get("EngineSize"));
+        String fileName = (String) object.get("FileName");
+        return new TuneModel(notes, url, size, mapId, fileName);
     }
 
     public @NotNull String getSaferLocalFileName() {
@@ -25,6 +34,18 @@ public class TuneModel {
 
     public String getNotes() {
         return notes;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getMapId() {
+        return mapId;
     }
 
     public String getUrl() {
