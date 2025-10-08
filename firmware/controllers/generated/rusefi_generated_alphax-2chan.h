6ac006cbda9fc3170e8ddf91be1111896124e6cb
GitHub gen-configs Action <action@github.com>
2025-10-08 01:16:04 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-2chan.h
+++ firmware/controllers/generated/rusefi_generated_alphax-2chan.h
@@ -2101,7 +2101,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.10.07.alphax-2chan.1669743245"
+#define TS_SIGNATURE "rusEFI master.2025.10.08.alphax-2chan.1669743245"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_hellen154hyundai.h
+++ firmware/controllers/generated/rusefi_generated_hellen154hyundai.h
@@ -2102,7 +2102,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.10.07.hellen154hyundai.2406715338"
+#define TS_SIGNATURE "rusEFI master.2025.10.08.hellen154hyundai.2406715338"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
