c949a7208da7af8caafaebbf7e40b4d9d2f62a2f
Andrey Gusakov <dron0gus@gmail.com>
2025-10-10 00:26:15 +0300

    init EGT: not so hard
    We do not have SPI support on H7, do not critical if MAX31855 is
    configured.

--- firmware/init/sensor/init_egt.cpp
+++ firmware/init/sensor/init_egt.cpp
@@ -49,6 +49,6 @@ void startEgt() {
 #if EFI_MAX_31855
 	startMax3185x(engineConfiguration->max31855spiDevice, engineConfiguration->max31855_cs);
 #else
-  criticalAssertVoid(SPI_NONE == engineConfiguration->max31855spiDevice, "not EFI_MAX_31855");
+  //criticalAssertVoid(SPI_NONE == engineConfiguration->max31855spiDevice, "not EFI_MAX_31855");
 #endif /* EFI_MAX_31855 */
 }
