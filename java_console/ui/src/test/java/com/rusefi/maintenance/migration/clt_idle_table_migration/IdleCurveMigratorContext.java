4d0df56b04c3a5f3f3775d689a16c7157da03674
FDSoftware <diego+gh@churrosoft.ar>
2025-10-11 15:11:50 -0300

    IdleCurveMigrator TDD: assert current (bad) behavior

--- java_console/ui/src/test/java/com/rusefi/maintenance/migration/clt_idle_table_migration/IdleCurveMigratorContext.java
+++ java_console/ui/src/test/java/com/rusefi/maintenance/migration/clt_idle_table_migration/IdleCurveMigratorContext.java
@@ -11,10 +11,22 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class IdleCurveMigratorContext {
 
-    public static TestTuneMigrationContext load() throws JAXBException {
+    public static TestTuneMigrationContext loadReducedCltBins() throws JAXBException {
         final TestTuneMigrationContext result = TestTuneMigrationContext.load(
-            "src/test/java/com/rusefi/maintenance/migration/clt_idle_table_migration/test_data"
+            "src/test/java/com/rusefi/maintenance/migration/clt_idle_table_migration/test_data/reducedCltBins"
         );
         return result;
     }
+     public static TestTuneMigrationContext loadIncreasedCltBins() throws JAXBException {
+            final TestTuneMigrationContext result = TestTuneMigrationContext.load(
+                "src/test/java/com/rusefi/maintenance/migration/clt_idle_table_migration/test_data/increasedCltBins"
+            );
+            return result;
+        }
+         public static TestTuneMigrationContext loadSameSizeCltBins() throws JAXBException {
+                final TestTuneMigrationContext result = TestTuneMigrationContext.load(
+                    "src/test/java/com/rusefi/maintenance/migration/clt_idle_table_migration/test_data/sameSizeCltBins"
+                );
+                return result;
+            }
 }
