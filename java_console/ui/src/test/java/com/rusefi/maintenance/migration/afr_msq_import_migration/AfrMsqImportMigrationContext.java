6461641c9aa002ec5b8c73480576cbe9b80612c3
FDSoftware <diego+gh@churrosoft.ar>
2025-10-12 01:06:49 -0300

    Add unit tests and migration data for AfrMsqImportMigrator

---
+++ java_console/ui/src/test/java/com/rusefi/maintenance/migration/afr_msq_import_migration/AfrMsqImportMigrationContext.java
@@ -0,0 +1,22 @@
+package com.rusefi.maintenance.migration.afr_msq_import_migration;
+
+import com.rusefi.maintenance.TestTuneMigrationContext;
+
+import javax.xml.bind.JAXBException;
+
+
+public class AfrMsqImportMigrationContext {
+
+    public static TestTuneMigrationContext loadAfrToLambda() throws JAXBException {
+        final TestTuneMigrationContext result = TestTuneMigrationContext.load(
+            "src/test/java/com/rusefi/maintenance/migration/afr_msq_import_migration/test_data/AFRtoLambda"
+        );
+        return result;
+    }
+    public static TestTuneMigrationContext loadLambdaToAfr() throws JAXBException {
+        final TestTuneMigrationContext result = TestTuneMigrationContext.load(
+            "src/test/java/com/rusefi/maintenance/migration/afr_msq_import_migration/test_data/LambdaToAFR"
+        );
+        return result;
+    }
+}
