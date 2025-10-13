7fe1f34d3e4f3c03bfb87053e389ddb4305d1c03
Andrey Gusakov <dron0gus@gmail.com>
2025-10-13 11:33:24 +0300

    ports: h7: fix no-cache region

--- firmware/hw_layer/ports/stm32/stm32h7/cfg/mcuconf_stm32h723.h
+++ firmware/hw_layer/ports/stm32/stm32h7/cfg/mcuconf_stm32h723.h
@@ -1,9 +1,9 @@
 /*
  * Memory attributes settings.
  */
-#define STM32_NOCACHE_ENABLE                FALSE
+#define STM32_NOCACHE_ENABLE                TRUE
 #define STM32_NOCACHE_MPU_REGION            MPU_REGION_6
-#define STM32_NOCACHE_RBAR                  0x24000000U
+#define STM32_NOCACHE_RBAR                  0x30020000U
 #define STM32_NOCACHE_RASR                  MPU_RASR_SIZE_16K
 
 /*
