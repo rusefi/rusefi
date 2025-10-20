e05603fb58978905b2a48fc68d6a526484a2425c
rusefillc <rusefillc@gmail.com>
2025-10-20 10:23:12 -0400

    unused in board.h

--- firmware/hw_layer/ports/stm32/stm32f4/board.h
+++ firmware/hw_layer/ports/stm32/stm32f4/board.h
@@ -81,16 +81,16 @@
 #define PIN_MODE_OUTPUT(n)          (1U << ((n) * 2U))
 #define PIN_MODE_ALTERNATE(n)       (2U << ((n) * 2U))
 #define PIN_MODE_ANALOG(n)          (3U << ((n) * 2U))
-#define PIN_ODR_LOW(n)              (0U << (n))
-#define PIN_ODR_HIGH(n)             (1U << (n))
+//#define PIN_ODR_LOW(n)              (0U << (n))
+//#define PIN_ODR_HIGH(n)             (1U << (n))
 #define PIN_OTYPE_PUSHPULL(n)       (0U << (n))
-#define PIN_OTYPE_OPENDRAIN(n)      (1U << (n))
-#define PIN_OSPEED_VERYLOW(n)       (0U << ((n) * 2U))
-#define PIN_OSPEED_LOW(n)           (1U << ((n) * 2U))
-#define PIN_OSPEED_MEDIUM(n)        (2U << ((n) * 2U))
+//#define PIN_OTYPE_OPENDRAIN(n)      (1U << (n))
+//#define PIN_OSPEED_VERYLOW(n)       (0U << ((n) * 2U))
+//#define PIN_OSPEED_LOW(n)           (1U << ((n) * 2U))
+//#define PIN_OSPEED_MEDIUM(n)        (2U << ((n) * 2U))
 #define PIN_OSPEED_HIGH(n)          (3U << ((n) * 2U))
-#define PIN_PUPDR_FLOATING(n)       (0U << ((n) * 2U))
-#define PIN_PUPDR_PULLUP(n)         (1U << ((n) * 2U))
+//#define PIN_PUPDR_FLOATING(n)       (0U << ((n) * 2U))
+//#define PIN_PUPDR_PULLUP(n)         (1U << ((n) * 2U))
 #define PIN_PUPDR_PULLDOWN(n)       (2U << ((n) * 2U))
 #define PIN_AFIO_AF(n, v)           ((v) << (((n) % 8U) * 4U))
 
@@ -214,9 +214,9 @@
 
 /*
  * GPIOB setup:
- * 
+ *
  * Default except SWO configured on PB3
- * 
+ *
  */
 #define VAL_GPIOB_MODER             (EFI_PIN_MODE_DEFAULT(0) |           \
                                      EFI_PIN_MODE_DEFAULT(1) |           \
