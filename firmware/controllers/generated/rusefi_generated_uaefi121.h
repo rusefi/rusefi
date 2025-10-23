c5ae54090ead5805fa9306686bb2b3e11bf23807
GitHub gen-configs Action <action@github.com>
2025-10-23 11:42:39 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_uaefi121.h
+++ firmware/controllers/generated/rusefi_generated_uaefi121.h
@@ -1537,7 +1537,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 3036232
+#define SIGNATURE_HASH 721404603
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2117,7 +2117,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.10.23.uaefi121.3036232"
+#define TS_SIGNATURE "rusEFI master.2025.10.23.uaefi121.721404603"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
