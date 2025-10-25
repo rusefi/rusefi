7fe382599a813c45d71a8d242ccfe20452854978
GitHub gen-configs Action <action@github.com>
2025-10-25 23:25:57 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-gold.h
+++ firmware/controllers/generated/rusefi_generated_alphax-gold.h
@@ -2116,7 +2116,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.10.25.alphax-gold.973555330"
+#define TS_SIGNATURE "rusEFI master.2025.10.26.alphax-gold.973555330"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_hellen-honda-k.h
+++ firmware/controllers/generated/rusefi_generated_hellen-honda-k.h
@@ -2120,7 +2120,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.10.25.hellen-honda-k.2401733760"
+#define TS_SIGNATURE "rusEFI master.2025.10.26.hellen-honda-k.2401733760"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
