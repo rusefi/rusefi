44c37ed4810f7e4837341529fdfea4e0e515dea2
David Holdeman <dave@holdemanenterprises.com>
2025-10-26 22:43:38 +0800

    More buttonshift testing

--- unit_tests/tests/test_tcu.cpp
+++ unit_tests/tests/test_tcu.cpp
@@ -7,30 +7,79 @@
 
 #include "gear_controller.h"
 #include "pch.h"
-#include "rusefi_enums.h"
+
+void blipGearControllerPin(EngineTestHelper* eth, brain_pin_e pin, int time) {
+	engine->gearController->update();
+	// Close switch/button
+	setMockState(pin, false);
+	engine->gearController->update();
+
+	// update gearController every ms
+	for (int i = 0; i < time; i = i+1000) {
+		eth->moveTimeForwardAndInvokeEventsUs(minI(time - i, 1000));
+		engine->gearController->update();
+	}
+	// And release
+	setMockState(pin, true);
+	engine->gearController->update();
+}
 
 TEST(tcu, testButtonshift) {
-	EngineTestHelper eth(engine_type_e::TCU_4R70W);
-	engineConfiguration->gearControllerMode = GearControllerMode::ButtonShift;
-	initGearController();
+  EngineTestHelper eth(engine_type_e::TCU_4R70W);
+  engineConfiguration->gearControllerMode = GearControllerMode::ButtonShift;
+  initGearController();
 
-	// pinMode is PI_PULLUP, so true = off        
-	setMockState(engineConfiguration->tcuUpshiftButtonPin, true);
+	// pinMode is PI_PULLUP, so true = off
+  setMockState(engineConfiguration->tcuUpshiftButtonPin, true);
+  setMockState(engineConfiguration->tcuDownshiftButtonPin, true);
 
 	ASSERT_NE(nullptr, engine->gearController);
 	ASSERT_EQ(NEUTRAL, engine->gearController->getDesiredGear());
 
-	engine->gearController->update();
+	// Press upshift button for 200ms
+	blipGearControllerPin(&eth, engineConfiguration->tcuUpshiftButtonPin, 200000);
 
-	// Close switch/button        
-	setMockState(engineConfiguration->tcuUpshiftButtonPin, false);
-	engine->gearController->update();
-	// For 200ms
-	eth.moveTimeForwardAndInvokeEventsUs(200000);
-	// And release        
-	setMockState(engineConfiguration->tcuUpshiftButtonPin, true);
+	// and now a bounce
+	eth.moveTimeForwardAndInvokeEventsUs(20);
+	blipGearControllerPin(&eth, engineConfiguration->tcuUpshiftButtonPin, 20);
 
-	engine->gearController->update();
+	ASSERT_EQ(GEAR_1, engine->gearController->getDesiredGear());
+
+	// Wait 500ms
+	eth.moveTimeForwardAndInvokeEventsUs(500000);
+	// Press upshift button for 200ms
+	blipGearControllerPin(&eth, engineConfiguration->tcuUpshiftButtonPin, 200000);
+
+	ASSERT_EQ(GEAR_2, engine->gearController->getDesiredGear());
+
+	// Wait 500ms
+	eth.moveTimeForwardAndInvokeEventsUs(500000);
+	// Press upshift button for 1ms
+	blipGearControllerPin(&eth, engineConfiguration->tcuUpshiftButtonPin, 1000);
+
+	ASSERT_EQ(GEAR_3, engine->gearController->getDesiredGear());
+
+	// Wait 500ms
+	eth.moveTimeForwardAndInvokeEventsUs(500000);
+	// Press upshift button for 3s
+	blipGearControllerPin(&eth, engineConfiguration->tcuUpshiftButtonPin, 3000000);
+
+	ASSERT_EQ(GEAR_4, engine->gearController->getDesiredGear());
+
+	// Wait 10ms
+	// Because this is a different pin, the 500ms debounce timeout is not in play.
+	eth.moveTimeForwardAndInvokeEventsUs(10000);
+	// Press downshift button for 200ms
+	blipGearControllerPin(&eth, engineConfiguration->tcuDownshiftButtonPin, 200000);
+
+	ASSERT_EQ(GEAR_3, engine->gearController->getDesiredGear());
+
+	// Wait 10ms
+	// This shouldn't be long enough for the debounce to have reset,
+	// so the downshift won't trigger until ~490ms after pressing the button.
+	eth.moveTimeForwardAndInvokeEventsUs(10000);
+	// Press downshift button for 1.2s
+	blipGearControllerPin(&eth, engineConfiguration->tcuDownshiftButtonPin, 1200000);
 
 	ASSERT_EQ(GEAR_1, engine->gearController->getDesiredGear());
 }
