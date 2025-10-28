92f7cd7733a858cbd4c51772d440ad96277df99e
rusefillc <rusefillc@gmail.com>
2025-10-28 09:38:20 -0400

    only:docs

--- firmware/config/boards/hellen/hellen_leds_100.cpp
+++ firmware/config/boards/hellen/hellen_leds_100.cpp
@@ -6,6 +6,7 @@ Gpio getRunningLedPin() {
 	return Gpio::MM100_LED2_GREEN;
 }
 
+// fun fact: the red LED for critical error is defined via LED_CRITICAL_ERROR_BRAIN_PIN
 Gpio getWarningLedPin() {
 	return Gpio::MM100_LED4_YELLOW;
 }
