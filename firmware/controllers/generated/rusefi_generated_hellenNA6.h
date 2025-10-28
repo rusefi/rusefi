ec5d5f2fb25e89aa429e7e29dd4394ae2df059e2
GitHub gen-configs Action <action@github.com>
2025-10-28 01:42:19 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellenNA6.h
+++ firmware/controllers/generated/rusefi_generated_hellenNA6.h
@@ -2123,7 +2123,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.10.27.hellenNA6.3353538608"
+#define TS_SIGNATURE "rusEFI master.2025.10.28.hellenNA6.3353538608"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
