820fd9e7e88a578c592e4a217121d4b577ce342a
FDSoftware <diego+gh@churrosoft.ar>
2025-11-14 01:56:40 -0300

    Add tests for Map Start Angle migration

---
+++ java_console/ui/src/test/java/com/rusefi/maintenance/migration/map_start_migration/MapstartMigrationTest.java
@@ -0,0 +1,52 @@
+package com.rusefi.maintenance.migration.map_start_migration;
+
+import com.rusefi.maintenance.TestTuneMigrationContext;
+import com.rusefi.maintenance.migration.migrators.ComposedTuneMigrator;
+import com.rusefi.tune.xml.Constant;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.xml.bind.JAXBException;
+import java.util.Map;
+
+import static com.rusefi.maintenance.migration.migrators.TableAddColumnsMigrator.MAP_SAMPLING_FIELD_NAME;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MapstartMigrationTest {
+
+    @Test
+    void checkValidMigration() throws JAXBException {
+        final TestTuneMigrationContext testContext = MapstartMigrationTestMigrationContext.loadValidMigration();
+        ComposedTuneMigrator.INSTANCE.migrateTune(testContext);
+        final Map<String, Constant> migratedConstants = testContext.getMigratedConstants();
+
+        assertEquals(1, migratedConstants.size());
+
+        final Constant migratedMapStartAngle = migratedConstants.get(
+            MAP_SAMPLING_FIELD_NAME
+        );
+        assertNotNull(migratedMapStartAngle);
+
+        assertEquals(
+            "\n" +
+                "         55.00\n" +
+                "         59.00\n" +
+                "         64.00\n" +
+                "         68.00\n" +
+                "         72.00\n" +
+                "         76.00\n" +
+                "         81.00\n" +
+                "         85.00\n",
+            migratedMapStartAngle.getValue()
+        );
+    }
+
+    @Test
+    void checkInvalidMigration() throws JAXBException{
+        final TestTuneMigrationContext testContext = MapstartMigrationTestMigrationContext.loadInvalidMigration();
+        ComposedTuneMigrator.INSTANCE.migrateTune(testContext);
+        final Map<String, Constant> migratedConstants = testContext.getMigratedConstants();
+
+        assertEquals(0, migratedConstants.size());
+    }
+}
---
+++ java_console/ui/src/test/java/com/rusefi/maintenance/migration/map_start_migration/MapstartMigrationTestMigrationContext.java
@@ -0,0 +1,22 @@
+package com.rusefi.maintenance.migration.map_start_migration;
+
+import com.rusefi.maintenance.TestTuneMigrationContext;
+
+import javax.xml.bind.JAXBException;
+
+public class MapstartMigrationTestMigrationContext {
+
+    public static TestTuneMigrationContext loadValidMigration() throws JAXBException {
+        final TestTuneMigrationContext result = TestTuneMigrationContext.load(
+            "src/test/java/com/rusefi/maintenance/migration/map_start_migration/test_data/valid_migration"
+        );
+        return result;
+    }
+
+    public static TestTuneMigrationContext loadInvalidMigration() throws JAXBException {
+        final TestTuneMigrationContext result = TestTuneMigrationContext.load(
+            "src/test/java/com/rusefi/maintenance/migration/map_start_migration/test_data/invalid_migration"
+        );
+        return result;
+    }
+}
