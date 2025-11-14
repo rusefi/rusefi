e042c1039140df2bf7ae9b0b7647bc7838c49a4d
GitHub gen-configs Action <action@github.com>
2025-11-14 15:59:19 +0000

    Auto-generated configs and docs

--- firmware/hw_layer/drivers/can/auto_generated_can_category.cpp
+++ firmware/hw_layer/drivers/can/auto_generated_can_category.cpp
@@ -1,6 +1,6 @@
 #include "global.h"
 #include "can_category.h"
-// was generated automatically by rusEFI tool  from can_category.h // by enum_to_string.jar tool on Wed May 07 01:54:45 UTC 2025
+// was generated automatically by rusEFI tool  from can_category.h // by enum_to_string.jar tool on Fri Nov 14 15:58:03 UTC 2025
 // see also gen_config_and_enums.bat
 
 
@@ -23,6 +23,8 @@ case CanCategory::OBD:
   return "OBD";
 case CanCategory::SERIAL:
   return "SERIAL";
+case CanCategory::TCU:
+  return "TCU";
 case CanCategory::VERBOSE:
   return "VERBOSE";
 case CanCategory::WBO_SERVICE:
