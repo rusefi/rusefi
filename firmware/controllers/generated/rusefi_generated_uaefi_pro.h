bef365332967849e925b90af75305b05b72e79fd
GitHub gen-configs Action <action@github.com>
2025-11-17 16:48:22 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_uaefi_pro.h
+++ firmware/controllers/generated/rusefi_generated_uaefi_pro.h
@@ -1539,7 +1539,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 1757612592
+#define SIGNATURE_HASH 2446295066
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2014,10 +2014,12 @@
 #define ts_show_ign_key_analog_input true
 #define ts_show_ign_key_menu true
 #define ts_show_ign_key_switch false
+#define ts_show_ignition true
 #define ts_show_ignitionMode true
 #define ts_show_ignitionOutputs true
 #define ts_show_ignitionPinMode true
 #define ts_show_inj_diag false
+#define ts_show_injection true
 #define ts_show_injection_hardware true
 #define ts_show_injection_mode true
 #define ts_show_injection_phase true
@@ -2122,7 +2124,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.11.17.uaefi_pro.1757612592"
+#define TS_SIGNATURE "rusEFI master.2025.11.17.uaefi_pro.2446295066"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- java_console/models/src/main/java/com/rusefi/config/generated/VariableRegistryValues.java
+++ java_console/models/src/main/java/com/rusefi/config/generated/VariableRegistryValues.java
@@ -1434,6 +1434,7 @@ public class VariableRegistryValues {
 	public static final int SentInput_INPUT6 = 6;
 	public static final int SentInput_INPUT7 = 7;
 	public static final int SentInput_NONE = 0;
+	public static final int SIGNATURE_HASH = 1292095861;
 	public static final String SIMULATOR_TUNE_BIN_FILE_NAME = "generated/simulator_tune_image.bin";
 	public static final String SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX = "generated/simulator_tune_image";
 	public static final String SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX = ".bin";
@@ -1740,7 +1741,7 @@ public class VariableRegistryValues {
 	public static final int TS_RESPONSE_UNRECOGNIZED_COMMAND = 0x83;
 	public static final int TS_SCATTER_OFFSETS_COUNT = 128;
 	public static final char TS_SET_LOGGER_SWITCH = 'l';
-	public static final String TS_SIGNATURE = "rusEFI master.2025.11.17.stm32h743_nucleo.3020905311";
+	public static final String TS_SIGNATURE = "rusEFI master.2025.11.17.stm32h743_nucleo.1292095861";
 	public static final char TS_SIMULATE_CAN = '>';
 	public static final char TS_TEST_COMMAND = 't';
 	public static final int TS_TOTAL_OUTPUT_SIZE = 2068;
