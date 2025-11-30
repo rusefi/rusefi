8f2d3d02ead6fbaf1e8a3157032246a902d88cb0
GitHub gen-configs Action <action@github.com>
2025-11-30 22:55:31 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-8chan_f7.h
+++ firmware/controllers/generated/rusefi_generated_alphax-8chan_f7.h
@@ -2128,7 +2128,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.11.29.alphax-8chan_f7.3547597969"
+#define TS_SIGNATURE "rusEFI master.2025.11.30.alphax-8chan_f7.3547597969"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_t-b-g.h
+++ firmware/controllers/generated/rusefi_generated_t-b-g.h
@@ -2133,7 +2133,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.11.29.t-b-g.506921607"
+#define TS_SIGNATURE "rusEFI master.2025.11.30.t-b-g.506921607"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
