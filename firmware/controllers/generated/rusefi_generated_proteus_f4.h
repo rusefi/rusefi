5552383f864983ffb9ce0883ba331ab61f5e75f8
GitHub gen-configs Action <action@github.com>
2025-12-01 01:20:17 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_proteus_f4.h
+++ firmware/controllers/generated/rusefi_generated_proteus_f4.h
@@ -2132,7 +2132,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.11.30.proteus_f4.4154203575"
+#define TS_SIGNATURE "rusEFI master.2025.12.01.proteus_f4.4154203575"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
