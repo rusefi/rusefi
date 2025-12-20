9a00dc250a2a3ef36e62b6261102e6ad76914a8e
rusefi <rusefillc@gmail.com>
2025-12-20 18:39:10 -0500

    only:docs

--- firmware/hw_layer/mmc_card.cpp
+++ firmware/hw_layer/mmc_card.cpp
@@ -821,7 +821,7 @@ static int sdModeSwitcher()
 	case SD_MODE_IDLE:
 		return 0;
 	case SD_MODE_UNMOUNT:
-		// everithing is done in sdModeSwitchToIdle();
+		// everything is done in sdModeSwitchToIdle();
 		sdMode = SD_MODE_UNMOUNT;
 		sdTargetMode = SD_MODE_IDLE;
 		return 0;
@@ -1028,6 +1028,8 @@ void initEarlyMmcCard() {
 
 	addConsoleAction("sdinfo", sdStatistics);
 	addConsoleActionS("del", removeFile);
+	// sdmode pc
+	// sdmode ecu
 	addConsoleActionS("sdmode", sdSetMode);
 	addConsoleAction("delreports", sdCardRemoveReportFiles);
 	//incLogFileName() use same shared FDLogFile, calling it while FDLogFile is used by log writer will cause damage
