bfdbd04e93110718b5c297d0a4650dd73dcd3e85
GitHub gen-configs Action <action@github.com>
2025-12-22 10:03:10 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-gold.h
+++ firmware/controllers/generated/rusefi_generated_alphax-gold.h
@@ -2135,7 +2135,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.12.21.alphax-gold.3523355263"
+#define TS_SIGNATURE "rusEFI master.2025.12.22.alphax-gold.3523355263"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_subaru_eg33_f7.h
+++ firmware/controllers/generated/rusefi_generated_subaru_eg33_f7.h
@@ -2139,7 +2139,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2025.12.21.subaru_eg33_f7.2994927803"
+#define TS_SIGNATURE "rusEFI master.2025.12.22.subaru_eg33_f7.2994927803"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
