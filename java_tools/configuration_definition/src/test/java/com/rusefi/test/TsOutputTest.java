749b4f92fe3c548ef7d39a50ad48441fd277c2b6
David Holdeman <dave@holdemanenterprises.com>
2025-12-22 17:58:27 +0000

    Test pin help generation

--- java_tools/configuration_definition/src/test/java/com/rusefi/test/TsOutputTest.java
+++ java_tools/configuration_definition/src/test/java/com/rusefi/test/TsOutputTest.java
@@ -202,4 +202,84 @@ class TsOutputTest {
         assertTrue(content.contains("U08"));
         assertTrue(content.contains("\"afr\", 0.1, 0"));
     }
+
+    @Test
+    void testWriteOneFieldHelp() {
+        TsOutput tsOutput = new TsOutput(false);
+
+        ReaderStateImpl state = new ReaderStateImpl();
+        String configText = "#define switch_input_pin_e_enum \"NONE\", \"INVALID\", \"PA0\"\n" +
+            "custom switch_input_pin_e 2 bits, U16, @OFFSET@, [0:8], $switch_input_pin_e_list\n" +
+            "struct_no_prefix engine_configuration_s\n" +
+            "switch_input_pin_e nitrousControlTriggerPin;Pin that activates nitrous control\n" +
+            "end_struct";
+
+        TestTSProjectConsumer consumer = new TestTSProjectConsumer(state);
+        state.readBufferedReader(configText, consumer);
+        ConfigStructure structure = state.getStructures().get("engine_configuration_s");
+
+        int tsPosition = 0;
+        tsOutput.run(state, structure, tsPosition, "", "");
+
+        // Get the generated content
+        String help = tsOutput.getSettingContextHelp();
+        String expected = "nitrousControlTriggerPin = \"Pin that activates nitrous control\"";
+
+        assertTrue(help.contains(expected),
+                   () -> "Expected help to contain " + expected + " but was:\n" + help);
+    }
+
+    @Test
+    void testWriteOneFieldHelpPinout() {
+        TsOutput tsOutput = new TsOutput(false);
+
+        ReaderStateImpl state = new ReaderStateImpl();
+        state.getVariableRegistry().register("PINOUT_URL", "https://example.com/");
+        String configText = "#define switch_input_pin_e_enum \"NONE\", \"INVALID\", \"PA0\"\n" +
+            "custom switch_input_pin_e 2 bits, U16, @OFFSET@, [0:8], $switch_input_pin_e_list\n" +
+            "struct_no_prefix engine_configuration_s\n" +
+            "switch_input_pin_e nitrousControlTriggerPin;Pin that activates nitrous control\n" +
+            "end_struct";
+
+        TestTSProjectConsumer consumer = new TestTSProjectConsumer(state);
+        state.readBufferedReader(configText, consumer);
+        ConfigStructure structure = state.getStructures().get("engine_configuration_s");
+
+        int tsPosition = 0;
+        tsOutput.run(state, structure, tsPosition, "", "");
+
+        // Get the generated content
+        String help = tsOutput.getSettingContextHelp();
+        String expected = "nitrousControlTriggerPin = \"Pin that activates nitrous control\\nhttps://example.com/?highlight=class~switch_inputs\"";
+
+        assertTrue(help.contains(expected),
+                   () -> "Expected help to contain " + expected + " but was:\n" + help);
+    }
+
+    @Test
+    void testWriteOneFieldPinout() {
+        TsOutput tsOutput = new TsOutput(false);
+
+        ReaderStateImpl state = new ReaderStateImpl();
+        state.getVariableRegistry().register("PINOUT_URL", "https://example.com/");
+        String configText = "#define switch_input_pin_e_enum \"NONE\", \"INVALID\", \"PA0\"\n" +
+            "custom switch_input_pin_e 2 bits, U16, @OFFSET@, [0:8], $switch_input_pin_e_list\n" +
+            "struct_no_prefix engine_configuration_s\n" +
+            "switch_input_pin_e nitrousControlTriggerPin\n" +
+            "end_struct";
+
+        TestTSProjectConsumer consumer = new TestTSProjectConsumer(state);
+        state.readBufferedReader(configText, consumer);
+        ConfigStructure structure = state.getStructures().get("engine_configuration_s");
+
+        int tsPosition = 0;
+        tsOutput.run(state, structure, tsPosition, "", "");
+
+        // Get the generated content
+        String help = tsOutput.getSettingContextHelp();
+        String expected = "nitrousControlTriggerPin = \"https://example.com/?highlight=class~switch_inputs\"";
+
+        assertTrue(help.contains(expected),
+                   () -> "Expected help to contain " + expected + " but was:\n" + help);
+    }
 }
