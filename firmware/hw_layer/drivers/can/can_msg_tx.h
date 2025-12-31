0526c03ce5b732d9c8f87fb36aa79edcd302a3b1
FDSoftware <diego+gh@churrosoft.ar>
2025-12-30 22:38:38 -0300

    prepare can_dash for unit tests

--- firmware/hw_layer/drivers/can/can_msg_tx.h
+++ firmware/hw_layer/drivers/can/can_msg_tx.h
@@ -119,7 +119,7 @@ class CanTxTyped final : public CanTxMessage
 public:
 	explicit CanTxTyped(CanCategory p_category, uint32_t p_id, bool p_isExtended, size_t canChannel) : CanTxMessage(p_category, p_id, sizeof(TData), canChannel, p_isExtended) { }
 
-#if EFI_CAN_SUPPORT
+#if HAS_CAN_FRAME
 	/**
 	 * Access members of the templated type.
 	 *
@@ -134,7 +134,7 @@ public:
 	TData& get() {
 		return *reinterpret_cast<TData*>(&m_frame.data8);
 	}
-#endif // EFI_CAN_SUPPORT
+#endif // HAS_CAN_FRAME
 };
 
 template <typename TData>
