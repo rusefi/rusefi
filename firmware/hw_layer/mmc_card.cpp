28c3f6bf17625b9522f0cebb352111cad73732bb
Andrey Gusakov <dron0gus@gmail.com>
2026-01-05 01:19:30 +0300

    MMC: report mount error

--- firmware/hw_layer/mmc_card.cpp
+++ firmware/hw_layer/mmc_card.cpp
@@ -571,7 +571,7 @@ static void deinitMmc() {
 // Mount the SD card.
 // Returns true if the filesystem was successfully mounted for writing.
 static bool mountMmc() {
-	bool ret = false;
+	FRESULT ret = FR_NOT_READY;
 
 	// if no card, don't try to mount FS
 	if (cardBlockDevice != nullptr) {
@@ -579,24 +579,23 @@ static bool mountMmc() {
 		memset(&resources, 0x00, sizeof(resources));
 		// We were able to connect the SD card, mount the filesystem
 		memset(&MMC_FS, 0, sizeof(FATFS));
-		ret = (f_mount(&MMC_FS, "", /* Mount immediately */ 1) == FR_OK);
+		ret = f_mount(&MMC_FS, "", /* Mount immediately */ 1);
 
-		if (ret == false) {
+		if (ret != FR_OK) {
 			sdStatus = SD_STATUS_MOUNT_FAILED;
-			efiPrintf("SD card mount failed!");
+			printFatFsError("Mount failed", ret);
+		} else {
+			sdStatus = SD_STATUS_MOUNTED;
+			efiPrintf("SD card mounted!");
 		}
 	}
 
-	if (ret) {
-		sdStatus = SD_STATUS_MOUNTED;
-		efiPrintf("SD card mounted!");
-	}
-
 #if EFI_TUNER_STUDIO
-	engine->outputChannels.sd_logging_internal = ret;
+	engine->outputChannels.sd_error = (uint8_t) ret;
+	engine->outputChannels.sd_logging_internal = (ret == FR_OK);
 #endif
 
-	return ret;
+	return (ret == FR_OK);
 }
 
 /*
@@ -612,6 +611,7 @@ static void unmountMmc() {
 	}
 
 #if EFI_TUNER_STUDIO
+	engine->outputChannels.sd_error = (uint8_t) ret;
 	engine->outputChannels.sd_logging_internal = false;
 #endif
 
