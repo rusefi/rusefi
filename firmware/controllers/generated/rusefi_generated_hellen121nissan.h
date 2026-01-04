72d9398ba316b0e2cd149e54559487d649200eb2
GitHub gen-configs Action <action@github.com>
2026-01-04 13:03:16 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellen121nissan.h
+++ firmware/controllers/generated/rusefi_generated_hellen121nissan.h
@@ -1548,7 +1548,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 239824340
+#define SIGNATURE_HASH 1093649361
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2139,7 +2139,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2026.01.03.hellen121nissan.239824340"
+#define TS_SIGNATURE "rusEFI master.2026.01.04.hellen121nissan.1093649361"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_hellen154hyundai.h
+++ firmware/controllers/generated/rusefi_generated_hellen154hyundai.h
@@ -1546,7 +1546,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 3002704840
+#define SIGNATURE_HASH 4254928333
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2137,7 +2137,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2026.01.03.hellen154hyundai.3002704840"
+#define TS_SIGNATURE "rusEFI master.2026.01.04.hellen154hyundai.4254928333"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
