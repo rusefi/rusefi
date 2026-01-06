ffbf380df84d43535c5664b3195e76beef9d2fa8
Andrey Gusakov <dron0gus@gmail.com>
2026-01-06 12:18:46 +0300

    NO_CACHE also need to be aligned
    Usualy NO_CACHE is used for data that is accessed by DMA.
    DMA needs so alignment.
    Set some safe 4 byte align for all NO_CACHE

--- firmware/hw_layer/ports/stm32/stm32h7/global_port.h
+++ firmware/hw_layer/ports/stm32/stm32h7/global_port.h
@@ -3,6 +3,6 @@
 //TODO: update LD file!
 #define SDRAM_OPTIONAL __attribute__((section(".ram8")))
 // SRAM3 is 32k and set to disable dcache
-#define NO_CACHE __attribute__((section(".ram3")))
+#define NO_CACHE __attribute__((aligned(4))) __attribute__((section(".ram3")))
 
 #define BKUP_RAM_NOINIT __attribute__((section(".bkup_ram_noinit")))
