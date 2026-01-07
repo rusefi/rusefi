82f1e8b20d74d4e87ea22014373ca78fb86350cf
rusefillc <rusefillc@gmail.com>
2026-01-06 22:42:41 -0500

    git workflow broken: error: Your local changes to the following files would be overwritten by merge: java_console/models/src/main/java/com/rusefi/config/generated/VariableRegistryValues.java #8906 only:docs

--- firmware/bin/assume_unchaged.sh
+++ firmware/bin/assume_unchaged.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
+# Makefile invokes 'detect_github.sh'. If $GITHUB_ACTIONS context is NOT detected, this bash script is invoked.
+
 # problem: this seem to not help actually?
+# a way to undo: '--no-assume-unchanged'
 
 # todo: proper fix would be to split file so that board-specific stuff is not even here?
 git update-index --assume-unchanged ../java_console/models/src/main/java/com/rusefi/config/generated/VariableRegistryValues.java
+git update-index --assume-unchanged hw_layer/mass_storage/ramdisk_image_compressed.h
 
+# problem: looks like pathspec patterns *.txt are not supported, unclear why bash expansion is not happening?
+# todo: try 'git ls-files "*.env" | xargs git update-index --assume-unchanged'?
 git update-index --assume-unchanged controllers/algo/auto*.*
 git update-index --assume-unchanged controllers/lua/generated/*
 git update-index --assume-unchanged live_data_generated/*
 git update-index --assume-unchanged console/binary/generated/*
 git update-index --assume-unchanged tunerstudio/generated/*
 git update-index --assume-unchanged controllers/generated/*
-git update-index --assume-unchanged hw_layer/mass_storage/ramdisk_image_compressed.h
