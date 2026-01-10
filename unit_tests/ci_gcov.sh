1277c3dc316c7798641df64f8e6d0ef6585af92f
FDSoftware <diego+gh@churrosoft.ar>
2026-01-09 23:19:35 -0300

    ci_gcov --merge-mode-functions=separate

--- unit_tests/ci_gcov.sh
+++ unit_tests/ci_gcov.sh
@@ -13,6 +13,6 @@ mkdir gcov
 echo -e "\nGenerating rusEFI unit test coverage"
 
 # for debug use --html-details --html-single-page --verbose  to generate a single html
-gcovr --exclude-throw-branches --exclude-unreachable-branches --decisions \
+gcovr --exclude-throw-branches --exclude-unreachable-branches --decisions --merge-mode-functions=separate \
       --exclude '/.*/googletest/' \
       -j4  -r ../.. --html-nested gcov/index.html
