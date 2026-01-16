247ed3f99b9b3874d851902446f9c742ce81e1e4
GitHub gen-configs Action <action@github.com>
2026-01-16 03:04:33 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-2chan.h
+++ firmware/controllers/generated/rusefi_generated_alphax-2chan.h
@@ -1545,7 +1545,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 723599254
+#define SIGNATURE_HASH 6205164
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2136,7 +2136,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2026.01.15.alphax-2chan.723599254"
+#define TS_SIGNATURE "rusEFI master.2026.01.16.alphax-2chan.6205164"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
