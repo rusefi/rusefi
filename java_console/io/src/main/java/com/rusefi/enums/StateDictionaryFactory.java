eb416fb0e8c84a7e63571233562e6284e6236a72
GitHub gen-configs Action <action@github.com>
2026-01-20 04:07:24 +0000

    Auto-generated configs and docs

--- java_console/io/src/main/java/com/rusefi/enums/StateDictionaryFactory.java
+++ java_console/io/src/main/java/com/rusefi/enums/StateDictionaryFactory.java
@@ -1,5 +1,5 @@
 package com.rusefi.enums;
-//was generated automatically by rusEFI tool config_definition_base-all.jar based on StateDictionaryGenerator integration/LiveData.yaml on Tue Jan 20 03:42:59 UTC 2026n
+//was generated automatically by rusEFI tool config_definition_base-all.jar based on StateDictionaryGenerator integration/LiveData.yaml on Tue Jan 20 04:06:01 UTC 2026n
 import com.rusefi.config.generated.*;
 import com.rusefi.ldmp.StateDictionary;
 
