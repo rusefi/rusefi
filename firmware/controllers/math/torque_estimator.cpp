cab8c6424b3fa4f38a54ac057e1023f45d6f6256
rusefillc <rusefillc@gmail.com>
2026-01-26 22:18:17 -0500

    torque table generator #9023 sin of pasta only:uaefi

--- firmware/controllers/math/torque_estimator.cpp
+++ firmware/controllers/math/torque_estimator.cpp
@@ -5,19 +5,22 @@ void estimateTorqueTable() {
     for (int loadIndex = 0;loadIndex<TORQUE_CURVE_SIZE;loadIndex++) {
       float rpmValue = config->torqueRpmBins[rpmIndex];
 
-      // let's assume speed density for onw
+      // let's assume speed density for now
       float mapValue = config->torqueLoadBins[loadIndex];
 
       percent_t veValue = interpolate3d(
-            			config->idleVeTable,
-            			config->idleVeLoadBins, mapValue,
-            			config->idleVeRpmBins, rpmValue
+            			config->veTable,
+            			config->veLoadBins, mapValue,
+            			config->veRpmBins, rpmValue
             		);
       float estimated = engineConfiguration->referenceTorqueForGenerator
        * veValue / engineConfiguration->referenceVeForGenerator
        * mapValue / engineConfiguration->referenceMapForGenerator;
 
       config->torqueTable[loadIndex][rpmIndex] = estimated;
+      if (loadIndex == 0 && rpmIndex == 0) {
+        efiPrintf("estimateTorqueTable [%f] %f", veValue, estimated);
+      }
 
     }
   }
