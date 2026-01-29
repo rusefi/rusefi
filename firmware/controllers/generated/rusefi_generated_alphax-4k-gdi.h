ef980e6051cd638082724a66087094fef0d4ffb7
GitHub gen-configs Action <action@github.com>
2026-01-29 00:47:12 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-4k-gdi.h
+++ firmware/controllers/generated/rusefi_generated_alphax-4k-gdi.h
@@ -1552,7 +1552,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2466019422
+#define SIGNATURE_HASH 1723081630
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2147,7 +2147,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2026.01.28.alphax-4k-gdi.2466019422"
+#define TS_SIGNATURE "rusEFI master.2026.01.29.alphax-4k-gdi.1723081630"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
