bcc9b0afd37d21b23e63b6b3e6ee71d586202996
rusefillc <rusefillc@gmail.com>
2026-01-29 23:31:54 -0500

    icons

--- java_console/ui/src/test/java/com/rusefi/ui/widgets/MainMenuTreeWidget.java
+++ java_console/ui/src/test/java/com/rusefi/ui/widgets/MainMenuTreeWidget.java
@@ -5,9 +5,11 @@ import com.opensr5.ini.IniFileModel;
 import com.opensr5.ini.MenuItem;
 import com.opensr5.ini.MenuModel;
 import com.opensr5.ini.SubMenuModel;
+import com.rusefi.core.ui.AutoupdateUtil;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
 import java.awt.*;
 
 public class MainMenuTreeWidget {
@@ -26,7 +28,33 @@ public class MainMenuTreeWidget {
         }
 
         JTree tree = new JTree(root);
+        Font font = tree.getFont();
+        tree.setFont(new Font(font.getName(), font.getStyle(), font.getSize() * 2));
         tree.setRootVisible(false);
+
+        ImageIcon setupIcon = AutoupdateUtil.loadIcon("setup48.png");
+        ImageIcon fuelIcon = AutoupdateUtil.loadIcon("fuel48.png");
+        ImageIcon ignitionIcon = AutoupdateUtil.loadIcon("ignition48.png");
+
+        tree.setCellRenderer(new DefaultTreeCellRenderer() {
+            @Override
+            public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+                super.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus);
+                if (value instanceof DefaultMutableTreeNode) {
+                    DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
+                    Object userObject = node.getUserObject();
+                    if ("Setup".equals(userObject)) {
+                        setIcon(setupIcon);
+                    } else if ("Fuel".equals(userObject)) {
+                        setIcon(fuelIcon);
+                    } else if ("Ignition".equals(userObject)) {
+                        setIcon(ignitionIcon);
+                    }
+                }
+                return this;
+            }
+        });
+
         contentPane.add(new JScrollPane(tree), BorderLayout.CENTER);
     }
 
--- java_console/ui/src/test/java/com/rusefi/ui/widgets/MainMenuTreeWidgetTest.java
+++ java_console/ui/src/test/java/com/rusefi/ui/widgets/MainMenuTreeWidgetTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeCellRenderer;
 import javax.swing.tree.TreeModel;
+import java.awt.*;
 import java.io.FileNotFoundException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,25 +34,53 @@ public class MainMenuTreeWidgetTest {
 
         // Find "Setup" menu
         DefaultMutableTreeNode setupNode = null;
+        int setupRow = -1;
         for (int i = 0; i < root.getChildCount(); i++) {
             DefaultMutableTreeNode node = (DefaultMutableTreeNode) root.getChildAt(i);
             if (node.getUserObject().equals("Setup")) {
                 setupNode = node;
+                setupRow = i; // This is NOT the row in the tree, but let's find it properly
                 break;
             }
         }
         assertNotNull(setupNode, "Setup node should exist");
 
+        // Verify icon for Setup
+        verifyIcon(tree, setupNode, "Setup");
+
+        // Find "Fuel" menu
+        DefaultMutableTreeNode fuelNode = findNode(root, "Fuel");
+        assertNotNull(fuelNode, "Fuel node should exist");
+        verifyIcon(tree, fuelNode, "Fuel");
+
+        // Find "Ignition" menu
+        DefaultMutableTreeNode ignitionNode = findNode(root, "Ignition");
+        assertNotNull(ignitionNode, "Ignition node should exist");
+        verifyIcon(tree, ignitionNode, "Ignition");
+
         // Find "Limits and protection" group in Setup
-        DefaultMutableTreeNode limitsNode = null;
-        for (int i = 0; i < setupNode.getChildCount(); i++) {
-            DefaultMutableTreeNode node = (DefaultMutableTreeNode) setupNode.getChildAt(i);
-            if (node.getUserObject().equals("Limits and protection")) {
-                limitsNode = node;
-                break;
-            }
-        }
+        DefaultMutableTreeNode limitsNode = findNode(setupNode, "Limits and protection");
         assertNotNull(limitsNode, "Limits and protection node should exist");
         assertTrue(limitsNode.getChildCount() > 0, "Limits group should have children");
+    }
+
+    private DefaultMutableTreeNode findNode(DefaultMutableTreeNode parent, String name) {
+        for (int i = 0; i < parent.getChildCount(); i++) {
+            DefaultMutableTreeNode node = (DefaultMutableTreeNode) parent.getChildAt(i);
+            if (node.getUserObject().equals(name)) {
+                return node;
+            }
+        }
+        return null;
+    }
+
+    private void verifyIcon(JTree tree, DefaultMutableTreeNode node, String name) {
+        TreeCellRenderer renderer = tree.getCellRenderer();
+        Component rendererComponent = renderer.getTreeCellRendererComponent(tree, node, false, false, false, 0, false);
+        assertTrue(rendererComponent instanceof JLabel, name + " renderer should be a JLabel");
+        JLabel label = (JLabel) rendererComponent;
+        assertNotNull(label.getIcon(), name + " should have an icon");
+        assertEquals(48, label.getIcon().getIconWidth(), name + " icon width");
+        assertEquals(48, label.getIcon().getIconHeight(), name + " icon height");
     }
 }
