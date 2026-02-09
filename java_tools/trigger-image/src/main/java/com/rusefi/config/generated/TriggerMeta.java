9734ddb8fd05d50541a6f67cf86e2d9784b0ac7c
David Holdeman <dave@holdemanenterprises.com>
2026-02-09 13:23:15 +0000

    Remove unused addJavaDestination

--- java_tools/trigger-image/src/main/java/com/rusefi/config/generated/TriggerMeta.java
+++
@@ -1,9 +0,0 @@
-package com.rusefi.config.generated;
-
-// this file was generated automatically by rusEFI tool config_definition_base-all.jar based on (unknown script) integration/rusefi_config_trigger.txt
-
-// by class com.rusefi.output.FileJavaFieldsConsumer
-import com.rusefi.config.*;
-
-public class TriggerMeta {
-}
