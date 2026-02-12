305e39e5b4c474279eeb0f76c0a654b11b3066ca
David Holdeman <dave@holdemanenterprises.com>
2026-02-12 11:01:34 +0000

    Don't change dir in generate_matrix

--- firmware/bin/generate_matrix.sh
+++ firmware/bin/generate_matrix.sh
@@ -5,11 +5,9 @@ SCAN_DIR=$1
 # The full path of the firmware directory
 FDIR=$(cd "$(dirname "$0")/.."; pwd -P)
 
-cd $FDIR
-
 getTarget ()
 {
-	source config/boards/common_script_read_meta_env.inc $1 >/dev/null
+	source "${2}/config/boards/common_script_read_meta_env.inc" "$1" >/dev/null
 	SKIP_RATE=${SKIP_RATE:-0}
 	ONLY=$(echo "$COMMIT_MESSAGE" | grep -Po '(?<=only:)[^\s]*')
 	if [[ ( "$EVENT_NAME" == 'workflow_dispatch' \
@@ -24,8 +22,8 @@ getTarget ()
 }
 
 echo -n '{"include": ['
-find ${SCAN_DIR} -name "meta-info*.env" -print0 | while IFS= read -r -d '' f; do
-	echo -n "$(getTarget $f)"
+find "${SCAN_DIR}" -name "meta-info*.env" -print0 | while IFS= read -r -d '' F; do
+	echo -n $(getTarget "$F" "$FDIR")
 done
 echo "]}"
 
