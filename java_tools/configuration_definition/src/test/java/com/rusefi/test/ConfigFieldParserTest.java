438baf452fafc8db01e2fcd15bd5187260638b7f
rusefi <rusefillc@gmail.com>
2026-02-13 10:37:39 -0500

    only:split

--- java_tools/configuration_definition/src/test/java/com/rusefi/test/ConfigFieldParserTest.java
+++ java_tools/configuration_definition/src/test/java/com/rusefi/test/ConfigFieldParserTest.java
@@ -178,79 +178,6 @@ public class ConfigFieldParserTest {
     }
 
     @Test
-    public void testCustomEnumAlignment() {
-        assertThrows(SizeMismatchException.class, new Executable() {
-            @Override
-            public void execute() throws Throwable {
-                String test = "struct pid_s\n" +
-                    "#define can_wbo_type_e_enum \"RusEFI\", \"AEM X-series\", \"Disabled/Analog\"\n" +
-                    "\tcustom can_wbo_type_e 2 bits, U08, @OFFSET@, [0:1], @@can_wbo_type_e_enum@@\n" +
-                    "\tcan_wbo_type_e wboType1;\n" +
-                    "end_struct\n";
-                ReaderStateImpl state = new ReaderStateImpl();
-
-                TestTSProjectConsumer tsProjectConsumer = new TestTSProjectConsumer(state);
-                state.readBufferedReader(test, tsProjectConsumer);
-            }
-        });
-    }
-
-    @Test
-    public void testCustomEnumWithTsVariable() {
-        String test = "struct pid_s\n" +
-            "#define ego_sensor_e_enum \"BPSX\", \"Innovate\", \"14Point7\"\n" +
-            "custom ego_sensor_e 1 bits, S08, @OFFSET@, [0:1], $ego_sensor_e_list\n" +
-            "ego_sensor_e afr_type1;\n" +
-            "int16_t int\n" +
-            "end_struct\n";
-        ReaderStateImpl state = new ReaderStateImpl();
-
-        TestTSProjectConsumer tsProjectConsumer = new TestTSProjectConsumer(state);
-        state.readBufferedReader(test, tsProjectConsumer);
-        assertEquals("afr_type1 = bits, S08, 0, [0:1], $ego_sensor_e_list\n" +
-            "int = scalar, S16, 2, \"\", 1, 0, 0, 32000, 0\n" +
-            "; total TS size = 4\n", tsProjectConsumer.getContent());
-    }
-
-    @Test
-    public void testShortForm() {
-        String test = "struct pid_s\n" +
-                "#define ego_sensor_e_enum \"BPSX\"\n" +
-                "custom ego_sensor_e 1 bits, S08, @OFFSET@, [0:1], @@ego_sensor_e_enum@@\n" +
-                "ego_sensor_e afr_type1;\n" +
-                "ego_sensor_e afr_type2;\n" +
-                "int8_t int\n" +
-                "end_struct\n";
-        ReaderStateImpl state = new ReaderStateImpl();
-
-        TestTSProjectConsumer tsProjectConsumer = new TestTSProjectConsumer(state);
-        state.readBufferedReader(test, tsProjectConsumer);
-        assertEquals("afr_type1 = bits, S08, 0, [0:1], \"BPSX\"\n" +
-                "afr_type2 = bits, S08, 1, [0:1], \"BPSX\"\n" +
-                "int = scalar, S08, 2, \"\", 1, 0, 0, 100, 0\n" +
-                "; total TS size = 4\n", tsProjectConsumer.getContent());
-    }
-
-    @Test
-    public void test2byteCustomEnum() {
-        String test = "struct pid_s\n" +
-                "#define ego_sensor_e_enum \"BPSX\", \"Innovate\", \"14Point7\"\n" +
-                "custom ego_sensor_e2 2 bits, S16, @OFFSET@, [0:1], @@ego_sensor_e_enum@@\n" +
-                "int8_t int\n" +
-                "ego_sensor_e2 afr_type1;\n" +
-                "ego_sensor_e2 afr_type2;\n" +
-                "end_struct\n";
-        ReaderStateImpl state = new ReaderStateImpl();
-
-        TestTSProjectConsumer tsProjectConsumer = new TestTSProjectConsumer(state);
-        state.readBufferedReader(test, tsProjectConsumer);
-        assertEquals("int = scalar, S08, 0, \"\", 1, 0, 0, 100, 0\n" +
-                "afr_type1 = bits, S16, 2, [0:1], \"BPSX\", \"Innovate\", \"14Point7\", \"INVALID\"\n" +
-                "afr_type2 = bits, S16, 4, [0:1], \"BPSX\", \"Innovate\", \"14Point7\", \"INVALID\"\n" +
-                "; total TS size = 8\n", tsProjectConsumer.getContent());
-    }
-
-    @Test
     public void test4byteCustomEnum() {
         String test = "struct pid_s\n" +
                 "#define ego_sensor_e_enum \"BPSX\", \"Innovate\", \"14Point7\"\n" +
@@ -364,26 +291,6 @@ public class ConfigFieldParserTest {
                 "periodMs2 = scalar, S16, 2, \"ms\", 1, 0, 0, 3000, 0\n" +
                 "afrTable = array, U08, 4, [4x4],\"deg\", 0.1, 0, 0, 25.0, 1\n" +
                 "; total TS size = 20\n", tsProjectConsumer.getContent());
-    }
-
-    @Test
-    public void useCustomType() {
-        ReaderStateImpl state = new ReaderStateImpl();
-        String test = "struct pid_s\n" +
-                "#define ERROR_BUFFER_SIZE 120\n" +
-                "\tcustom critical_error_message_t @@ERROR_BUFFER_SIZE@@ string, ASCII, @OFFSET@, @@ERROR_BUFFER_SIZE@@\n" +
-                "\tcritical_error_message_t var;\n" +
-                "\tint16_t periodMs;PID dTime;\"ms\",      1,      0,       0, 3000,      0\n" +
-                "end_struct\n" +
-                "";
-
-        JavaFieldsConsumer javaFieldsConsumer = new TestJavaFieldsConsumer(state);
-        state.readBufferedReader(test, javaFieldsConsumer);
-
-        assertEquals("\tpublic static final Field VAR = Field.create(\"VAR\", 0, 120, FieldType.STRING).setScale(1.0).setBaseOffset(0);\n" +
-                        "\tpublic static final Field PERIODMS = Field.create(\"PERIODMS\", 120, FieldType.INT16).setScale(1.0).setBaseOffset(0);\n" +
-                        "\tpublic static final Field ALIGNMENTFILL_AT_122 = Field.create(\"ALIGNMENTFILL_AT_122\", 122, FieldType.INT8).setScale(1.0).setBaseOffset(0);\n",
-                javaFieldsConsumer.getContent());
     }
 
     @Test
