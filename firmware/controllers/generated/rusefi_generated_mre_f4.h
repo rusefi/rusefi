bbcf0c370c58660c82a7da306a6774c2be6bf41a
GitHub gen-configs Action <action@github.com>
2026-02-13 00:28:58 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_mre_f4.h
+++ firmware/controllers/generated/rusefi_generated_mre_f4.h
@@ -2156,7 +2156,7 @@
 #define ts_show_wbo_can_menu true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2026.02.12.mre_f4.881440191"
+#define TS_SIGNATURE "rusEFI master.2026.02.13.mre_f4.881440191"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_uaefi_pro_h7.h
+++ firmware/controllers/generated/rusefi_generated_uaefi_pro_h7.h
@@ -2153,7 +2153,7 @@
 #define ts_show_wbo_can_menu true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2026.02.12.uaefi_pro_h7.3666128195"
+#define TS_SIGNATURE "rusEFI master.2026.02.13.uaefi_pro_h7.3666128195"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
