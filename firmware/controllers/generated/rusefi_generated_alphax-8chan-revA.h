51e4d0a969e8cf75bb46fcbaff31ce1df504fb92
GitHub gen-configs Action <action@github.com>
2026-02-14 00:42:39 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-8chan-revA.h
+++ firmware/controllers/generated/rusefi_generated_alphax-8chan-revA.h
@@ -2154,7 +2154,7 @@
 #define ts_show_wbo_can_menu true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2026.02.13.alphax-8chan-revA.157525354"
+#define TS_SIGNATURE "rusEFI master.2026.02.14.alphax-8chan-revA.157525354"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_super-uaefi.h
+++ firmware/controllers/generated/rusefi_generated_super-uaefi.h
@@ -2155,7 +2155,7 @@
 #define ts_show_wbo_can_menu true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2026.02.13.super-uaefi.2875361195"
+#define TS_SIGNATURE "rusEFI master.2026.02.14.super-uaefi.2875361195"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
