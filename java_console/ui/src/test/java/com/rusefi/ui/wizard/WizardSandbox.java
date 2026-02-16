07d0e302b69e69729b2c7038eb9c423bcfd4767b
rusefillc <rusefillc@gmail.com>
2026-02-15 21:32:51 -0500

    console to have setup wizard #9110 L&F candy only:llm

--- java_console/ui/src/test/java/com/rusefi/ui/wizard/WizardSandbox.java
+++ java_console/ui/src/test/java/com/rusefi/ui/wizard/WizardSandbox.java
@@ -1,18 +1,37 @@
 package com.rusefi.ui.wizard;
 
+import com.formdev.flatlaf.FlatDarkLaf;
 import com.rusefi.core.ui.FrameHelper;
+import com.rusefi.ui.basic.UiHelper;
 
 import javax.swing.*;
+import java.awt.*;
 
 import static com.rusefi.core.ui.FrameHelper.createFrame;
 
 public class WizardSandbox {
     public static void main(String[] args) {
+        UiHelper.configureLaf();
+
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                FrameHelper fh = createFrame("Sand");
-                fh.getFrame().setSize(600, 400);
+                FrameHelper fh = createFrame("Wizard Sandbox");
+
+                JPanel content = new JPanel(new BorderLayout());
+                content.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+                content.add(new JLabel("This is a modern Wizard Sandbox"), BorderLayout.NORTH);
+
+                JPanel buttons = new JPanel();
+                buttons.add(new JButton("Previous"));
+                buttons.add(new JButton("Next"));
+                buttons.add(new JButton("Finish"));
+                content.add(buttons, BorderLayout.SOUTH);
+
+                fh.getFrame().add(content);
+
+                fh.getFrame().setSize(800, 600);
+                fh.getFrame().setLocationRelativeTo(null);
                 fh.getFrame().setVisible(true);
             }
         });
