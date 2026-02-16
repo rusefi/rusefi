47e7e3eb1e07f5a3e57c630fcc9962bd7dcae9ac
rusefillc <rusefillc@gmail.com>
2026-02-16 11:00:13 -0500

    code generator should support 33 and more bits defined in a row #9126 only:test

--- java_tools/configuration_definition/src/test/java/com/rusefi/test/BitParsingTest.java
+++ java_tools/configuration_definition/src/test/java/com/rusefi/test/BitParsingTest.java
@@ -1,13 +1,16 @@
 package com.rusefi.test;
 
+import com.rusefi.BitState;
 import com.rusefi.ReaderStateImpl;
 import com.rusefi.TsFileContent;
 import com.rusefi.output.TSProjectConsumer;
 import com.rusefi.util.Output;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import java.io.*;
 
+import static com.rusefi.AssertCompatibility.assertThrows;
 import static com.rusefi.AssertCompatibility.assertTrue;
 
 public class BitParsingTest {
@@ -35,6 +38,41 @@ public class BitParsingTest {
 
         assertTrue(sw.toString().contains("\"false\", \"true\""));
         assertTrue(sw.toString().contains("\"nada\", \"si\""));
+    }
+
+    @Test
+    public void test35Bits() {
+        assertThrows(BitState.TooManyBitsInARow.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                ReaderStateImpl state = new ReaderStateImpl();
+
+                StringBuilder input = new StringBuilder("struct bit_s\n");
+                for (int i = 0; i < 35; i++) {
+                    input.append(ReaderStateImpl.BIT).append(" fieldName").append(i).append(",\"true\",\"false\"\n");
+                }
+                input.append("end_struct\n");
+
+                StringWriter sw = new StringWriter();
+
+                TSProjectConsumer javaFieldsConsumer = new TSProjectConsumer("", state) {
+                    @Override
+                    protected void writeTunerStudioFile(String tsPath, String fieldsSection) throws IOException {
+                        writeContent(fieldsSection, new TsFileContent("", ""), createOutput(sw));
+                    }
+                };
+                state.setHeaderMessage("test");
+                state.readBufferedReader(input.toString(), javaFieldsConsumer);
+
+                String result = sw.toString();
+                System.out.println(result);
+
+                assertTrue(result.contains("fieldName0 = bits, U32, 0, [0:0], \"false\", \"true\""));
+                assertTrue(result.contains("fieldName31 = bits, U32, 0, [31:31], \"false\", \"true\""));
+                assertTrue(result.contains("fieldName32 = bits, U32, 4, [0:0], \"false\", \"true\""));
+                assertTrue(result.contains("fieldName34 = bits, U32, 4, [2:2], \"false\", \"true\""));
+            }
+        });
     }
 
     private Output createOutput(StringWriter sw) {
