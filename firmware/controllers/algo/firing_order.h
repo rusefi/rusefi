55e5ae8980d75f8670952ff77bb1c30bcbddc6b4
rusefillc <rusefillc@gmail.com>
2026-02-15 22:04:40 -0500

    docs

--- firmware/controllers/algo/firing_order.h
+++ firmware/controllers/algo/firing_order.h
@@ -27,8 +27,8 @@ typedef enum __attribute__ ((__packed__)) {
 	// 4 cylinder
 	FO_1_3_4_2 = 1, // typical inline 4
 	FO_1_2_4_3 = 2,
-	FO_1_3_2_4 = 3, // for example horizontally opposed engine
-	FO_1_4_3_2 = 17, // for example VW aircooled boxer engine
+	FO_1_3_2_4 = 3, // horizontally opposed engine
+	FO_1_4_3_2 = 17, // VW aircooled boxer engine
 
 	// 5 cylinder
 	FO_1_2_4_5_3 = 6,
@@ -39,7 +39,7 @@ typedef enum __attribute__ ((__packed__)) {
 	FO_1_4_5_2_3_6 = 31, // Chevrolet Corvair
 	FO_1_4_2_5_3_6 = 7, // Mercedes-Benz M104 engine
 	FO_1_2_3_4_5_6 = 9, // all Nissan v6, GM 60-Degree V6 engine
-	FO_1_6_3_2_5_4 = 13, // EG33
+	FO_1_6_3_2_5_4 = 13, // Subaru EG33
 	FO_1_4_3_6_2_5 = 27, // VAG v6 different from VAG VR6
 	FO_1_6_2_4_3_5 = 29, // Some 911
 
@@ -56,7 +56,7 @@ typedef enum __attribute__ ((__packed__)) {
 	FO_1_2_3_4_5_6_7_8 = 25, // linearly incrementing, for V8 testing
 	FO_1_5_4_8_6_3_7_2 = 26, // Audi 4.2 40v V8
 	FO_1_5_4_8_3_7_2_6 = 32, // Ford Voodoo
-	FO_1_8_7_3_6_5_4_2 = 28, // VH41DE (Japaneese Y32 Variant)
+	FO_1_8_7_3_6_5_4_2 = 28, // VH41DE (Japanese Y32 Variant)
 	FO_1_8_6_2_7_3_4_5 = 34, // Ferrari-Maserati F136
 
 	// 9 cylinder - for instance radial :)
