3081c42232111f73e4411b1f71126b1b193b6125
GitHub gen-configs Action <action@github.com>
2026-02-22 04:11:34 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_small-can-board.h
+++ firmware/controllers/generated/rusefi_generated_small-can-board.h
@@ -2156,7 +2156,7 @@
 #define ts_show_wbo_can_menu true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2026.02.21.small-can-board.207411917"
+#define TS_SIGNATURE "rusEFI master.2026.02.22.small-can-board.207411917"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_uaefi121.h
+++ firmware/controllers/generated/rusefi_generated_uaefi121.h
@@ -2156,7 +2156,7 @@
 #define ts_show_wbo_can_menu true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2026.02.21.uaefi121.2049147945"
+#define TS_SIGNATURE "rusEFI master.2026.02.22.uaefi121.2049147945"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
