245810862460c9bc551a63ea9b8c296d4534f1c7
GitHub gen-configs Action <action@github.com>
2026-02-22 13:23:18 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-8chan_f7.h
+++ firmware/controllers/generated/rusefi_generated_alphax-8chan_f7.h
@@ -1554,7 +1554,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 739554083
+#define SIGNATURE_HASH 3316781505
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2152,7 +2152,7 @@
 #define ts_show_wbo_can_menu true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2026.02.22.alphax-8chan_f7.739554083"
+#define TS_SIGNATURE "rusEFI master.2026.02.22.alphax-8chan_f7.3316781505"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_frankenso_na6.h
+++ firmware/controllers/generated/rusefi_generated_frankenso_na6.h
@@ -1559,7 +1559,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2667990376
+#define SIGNATURE_HASH 1990258570
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2157,7 +2157,7 @@
 #define ts_show_wbo_can_menu true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI master.2026.02.22.frankenso_na6.2667990376"
+#define TS_SIGNATURE "rusEFI master.2026.02.22.frankenso_na6.1990258570"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
