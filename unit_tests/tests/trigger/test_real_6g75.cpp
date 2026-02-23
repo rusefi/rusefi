92ee96e97d70c7525fdb7a15a03686e009428c2d
rusefillc <rusefillc@gmail.com>
2026-02-23 18:45:09 -0500

    36-2-1-1 but with terrible compression effects trigger support #8827 only:skeleton

--- unit_tests/tests/trigger/test_real_6g75.cpp
+++ unit_tests/tests/trigger/test_real_6g75.cpp
@@ -3,5 +3,13 @@
 #include "logicdata_csv_reader.h"
 
 TEST(real6g75, real) {
+    CsvReader reader(/*triggerCount*/ 1, /* vvtCount */ 1);
 
+    reader.open("tests/trigger/resources/6g75-without-spark-crank.csv", NORMAL_ORDER, NORMAL_ORDER);
+
+    EngineTestHelper eth(engine_type_e::TEST_ENGINE);
+    setVerboseTrigger(true);
+
+    engineConfiguration->vvtMode[0] = VVT_INACTIVE;
+    eth.setTriggerType(trigger_type_e::TT_36_2_1_1);
 }
