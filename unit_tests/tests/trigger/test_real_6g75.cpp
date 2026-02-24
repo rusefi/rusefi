38c311361d7772039c044e35c9819b23d7cfe5f9
rusefillc <rusefillc@gmail.com>
2026-02-23 23:36:11 -0500

    36-2-1-1 but with terrible compression effects trigger support #8827 only:test

--- unit_tests/tests/trigger/test_real_6g75.cpp
+++ unit_tests/tests/trigger/test_real_6g75.cpp
@@ -3,7 +3,7 @@
 #include "logicdata_csv_reader.h"
 
 TEST(real6g75, real) {
-    CsvReader reader(/*triggerCount*/ 1, /* vvtCount */ 1);
+    CsvReader reader(/*triggerCount*/ 1, /* vvtCount */ 0);
 
     reader.open("tests/trigger/resources/6g75-without-spark-crank.csv", NORMAL_ORDER, NORMAL_ORDER);
 
@@ -12,4 +12,13 @@ TEST(real6g75, real) {
 
     engineConfiguration->vvtMode[0] = VVT_INACTIVE;
     eth.setTriggerType(trigger_type_e::TT_36_2_1_1);
+
+    while (reader.haveMore()) {
+        reader.processLine(&eth);
+    }
+
+    ASSERT_EQ(2, eth.recentWarnings()->getCount());
+    float rpm = Sensor::getOrZero(SensorType::Rpm);
+    printf("Final RPM: %f\n", rpm);
+    ASSERT_NEAR(144.73, rpm, 0.1);
 }
