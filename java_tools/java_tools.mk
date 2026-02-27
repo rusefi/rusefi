d22972d3a1836f05d3db9d00117cff1ce42b1e30
rusefillc <rusefillc@gmail.com>
2026-02-26 21:04:01 -0500

    GRADLE_ROOT only:step

--- java_tools/java_tools.mk
+++ java_tools/java_tools.mk
@@ -1,3 +1,4 @@
+GRADLE_ROOT = $(PROJECT_DIR)/../java_tools
 JAVA_TOOLS = $(PROJECT_DIR)/../java_tools
 
 # We run multiple processes in parallel by passing the -j option to Make.
@@ -21,25 +22,25 @@ AUTOUPDATE_JAR = $(PROJECT_DIR)/../console/rusefi_autoupdate.jar
 # so we don't have to worry about triggering rebuilds of things that have these tools as a prerequisite.
 
 $(CONFIG_DEFINITION_JAR): .docsenums-sentinel .FORCE
-	cd $(JAVA_TOOLS) && $(FLOCK) ./gradlew :config_definition:shadowJar
+	cd $(GRADLE_ROOT) && $(FLOCK) ./gradlew :config_definition:shadowJar
 
 $(CONFIG_DEFINITION_BASE_JAR): .FORCE
-	cd $(JAVA_TOOLS) && $(FLOCK) ./gradlew :config_definition_base:shadowJar
+	cd $(GRADLE_ROOT) && $(FLOCK) ./gradlew :config_definition_base:shadowJar
 
 $(ENUM_TO_STRING_JAR): .FORCE
-	cd $(JAVA_TOOLS) && $(FLOCK) ./gradlew :enum_to_string:shadowJar
+	cd $(GRADLE_ROOT) && $(FLOCK) ./gradlew :enum_to_string:shadowJar
 
 #$(TUNE_TOOLS_JAR):
-	#cd $(JAVA_TOOLS) && $(FLOCK) ./gradlew :tune-tools:shadowJar
+	#cd $(GRADLE_ROOT) && $(FLOCK) ./gradlew :tune-tools:shadowJar
 
 $(TS_PLUGIN_LAUNCHER_JAR): .FORCE
-	cd $(JAVA_TOOLS) && $(FLOCK) ./gradlew :ts_plugin_launcher:shadowJar
+	cd $(GRADLE_ROOT) && $(FLOCK) ./gradlew :ts_plugin_launcher:shadowJar
 
 $(CONSOLE_JAR): .docsenums-sentinel .config-sentinel .FORCE
-	cd $(JAVA_TOOLS) && $(FLOCK) ./gradlew :ui:shadowJar
+	cd $(GRADLE_ROOT) && $(FLOCK) ./gradlew :ui:shadowJar
 
 $(AUTOUPDATE_JAR): .FORCE
-	cd $(JAVA_TOOLS) && $(FLOCK) ./gradlew :autoupdate:shadowJar
+	cd $(GRADLE_ROOT) && $(FLOCK) ./gradlew :autoupdate:shadowJar
 
 .FORCE:
 
