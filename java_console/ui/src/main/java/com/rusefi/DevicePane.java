ce4261b34161d1fbf650eb281332561fd8488ad2
rusefillc <rusefillc@gmail.com>
2026-02-26 21:36:11 -0500

    autoupdate settings #9178 only:step

--- java_console/ui/src/main/java/com/rusefi/DevicePane.java
+++ java_console/ui/src/main/java/com/rusefi/DevicePane.java
@@ -1,5 +1,6 @@
 package com.rusefi;
 
+import com.rusefi.core.preferences.storage.PersistentConfiguration;
 import com.rusefi.io.UpdateOperationCallbacks;
 import com.rusefi.maintenance.jobs.OpenBltAutoJob;
 import com.rusefi.maintenance.jobs.OpenBltSwitchJob;
@@ -10,9 +11,14 @@ import javax.swing.*;
 import java.awt.*;
 
 public class DevicePane {
+    private static final String AUTO_UPDATE_BUNDLE_PROPERTY = "AUTO_UPDATE_BUNDLE";
     private final JPanel content = new JPanel();
 
     public DevicePane(UIContext uiContext, String port, SerialPortType serialPortType) {
+        JCheckBox autoUpdateBundle = new JCheckBox("Auto-update bundle", PersistentConfiguration.getBoolProperty(AUTO_UPDATE_BUNDLE_PROPERTY, true));
+        autoUpdateBundle.addActionListener(e -> PersistentConfiguration.setBoolProperty(AUTO_UPDATE_BUNDLE_PROPERTY, autoUpdateBundle.isSelected()));
+        content.add(autoUpdateBundle);
+
         if (serialPortType == SerialPortType.Ecu) {
             content.add(new JLabel("Legacy Device Without OpenBLT"));
         } else if (serialPortType == SerialPortType.EcuWithOpenblt) {
