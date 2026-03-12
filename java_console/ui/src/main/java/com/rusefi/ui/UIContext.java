9bb0d1ef47bc2675aa58380a667dbc701c3a7b06
FDSoftware <diego+gh@churrosoft.ar>
2026-03-12 17:22:55 -0300

    notify listeners on configuration image updates

--- java_console/ui/src/main/java/com/rusefi/ui/UIContext.java
+++ java_console/ui/src/main/java/com/rusefi/ui/UIContext.java
@@ -1,11 +1,16 @@
 package com.rusefi.ui;
 
+import com.opensr5.ConfigurationImage;
 import com.rusefi.binaryprotocol.BinaryProtocol;
 import com.rusefi.ini.IniFileState;
 import com.rusefi.io.CommandQueue;
 import com.rusefi.io.LinkManager;
 import com.rusefi.sensor_logs.SensorLogger;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * key frontend app state singleton
@@ -31,5 +36,18 @@ public class UIContext {
 
     public CommandQueue getCommandQueue() {
         return linkManager.getCommandQueue();
+    }
+
+    // ---- Config-image change notifications ----
+
+    private final List<Consumer<ConfigurationImage>> configImageListeners = new ArrayList<>();
+
+    public void addConfigImageListener(Consumer<ConfigurationImage> listener) {
+        configImageListeners.add(listener);
+    }
+
+    /** Call on the Swing EDT whenever the working configuration image changes. */
+    public void fireConfigImageChanged(ConfigurationImage ci) {
+        for (Consumer<ConfigurationImage> l : configImageListeners) l.accept(ci);
     }
 }
