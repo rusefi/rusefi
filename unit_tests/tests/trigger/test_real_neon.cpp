e6ff5f4b3acc9fa8e5f588ca64fb7c5f48d9bc08
rusefillc <rusefillc@gmail.com>
2026-03-13 03:15:50 -0400

    Neon 2003 trigger #8977 Dodge Neon 2003 crank actually uses crank code

--- unit_tests/tests/trigger/test_real_neon.cpp
+++ unit_tests/tests/trigger/test_real_neon.cpp
@@ -1,11 +1,17 @@
 #include "pch.h"
 #include "logicdata_csv_reader.h"
 
-TEST(realNeon, srt4_only_crank) {
+TEST(realNeon, srt4_looks_like_cam) {
 	CsvReader reader(1, /* vvtCount */ 0);
 
 	reader.open("tests/trigger/resources/neonsrt4.csv");
-	EngineTestHelper eth (engine_type_e::ET_DODGE_NEON_2003);
+
+	EngineTestHelper eth (engine_type_e::TEST_ENGINE);
+	reader.readingOffset = 0; // first column is cam signal
+	//    setVerboseTrigger(true);
+
+	engineConfiguration->vvtMode[0] = VVT_INACTIVE;
+	eth.setTriggerType(trigger_type_e::TT_DODGE_NEON_2003_CAM);
 	engineConfiguration->alwaysInstantRpm = true;
 
 	while (reader.haveMore()) {
@@ -13,7 +19,26 @@ TEST(realNeon, srt4_only_crank) {
 	}
 
 	ASSERT_EQ(56, round(Sensor::getOrZero(SensorType::Rpm)));
-	EXPECT_EQ(ObdCode::CUSTOM_OUT_OF_ORDER_COIL, eth.recentWarnings()->get(0).Code);
-	EXPECT_EQ(1u, eth.recentWarnings()->getCount());
+	EXPECT_EQ(0, eth.recentWarnings()->getCount());
 }
 
+TEST(realNeon, srt4_crank) {
+
+	CsvReader reader(/*triggerCount*/ 1, /* vvtCount */ 0);
+	reader.readingOffset = 1; // second channel is crank
+
+	reader.open("tests/trigger/resources/neonsrt4.csv", NORMAL_ORDER, NORMAL_ORDER);
+
+	EngineTestHelper eth(engine_type_e::TEST_ENGINE);
+	//    setVerboseTrigger(true);
+
+	engineConfiguration->vvtMode[0] = VVT_INACTIVE;
+	eth.setTriggerType(trigger_type_e::TT_DODGE_NEON_2003_CRANK);
+
+	while (reader.haveMore()) {
+		reader.processLine(&eth);
+	}
+
+//	ASSERT_EQ(, eth.recentWarnings()->getCount());
+//	ASSERT_NEAR(TODO, Sensor::getOrZero(SensorType::Rpm), 0.1);
+}
