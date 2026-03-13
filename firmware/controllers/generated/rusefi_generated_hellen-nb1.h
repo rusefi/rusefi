d8d86a1b21b3c82953a8d95007d016d286987315
GitHub gen-configs Action <action@github.com>
2026-03-13 19:33:07 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellen-nb1.h
+++ firmware/controllers/generated/rusefi_generated_hellen-nb1.h
@@ -1560,7 +1560,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 164738061
+#define SIGNATURE_HASH 2006823333
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2161,7 +2161,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.03.13.hellen-nb1.164738061"
+#define TS_SIGNATURE "rusEFI master.2026.03.13.hellen-nb1.2006823333"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
