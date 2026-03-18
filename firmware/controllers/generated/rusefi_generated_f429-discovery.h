95d626e27f1f826ef6987b5920f91da96610ab1f
GitHub gen-configs Action <action@github.com>
2026-03-18 09:18:47 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_f429-discovery.h
+++ firmware/controllers/generated/rusefi_generated_f429-discovery.h
@@ -2162,7 +2162,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.03.17.f429-discovery.1891778217"
+#define TS_SIGNATURE "rusEFI master.2026.03.18.f429-discovery.1891778217"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_frankenso_na6.h
+++ firmware/controllers/generated/rusefi_generated_frankenso_na6.h
@@ -2161,7 +2161,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.03.17.frankenso_na6.855962824"
+#define TS_SIGNATURE "rusEFI master.2026.03.18.frankenso_na6.855962824"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
