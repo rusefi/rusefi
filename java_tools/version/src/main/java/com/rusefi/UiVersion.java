6094f195a508bea58b63eaa420f51f60820e5f3c
rusefillc <rusefillc@gmail.com>
2026-03-20 17:13:55 -0400

    CONSOLE_VERSION

--- java_tools/version/src/main/java/com/rusefi/UiVersion.java
+++ java_tools/version/src/main/java/com/rusefi/UiVersion.java
@@ -5,5 +5,5 @@ public interface UiVersion {
      * *** BE CAREFUL WE HAVE SEPARATE AUTOUPDATE_VERSION also managed manually ***
      * @see com.rusefi.autoupdate.Autoupdate#AUTOUPDATE_VERSION
      */
-    int CONSOLE_VERSION = 20260312;
+    int CONSOLE_VERSION = 20260320;
 }
