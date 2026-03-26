677540df73485a5479848052f40f271286610da6
GitHub gen-configs Action <action@github.com>
2026-03-26 01:43:49 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-8chan_f7.h
+++ firmware/controllers/generated/rusefi_generated_alphax-8chan_f7.h
@@ -1556,7 +1556,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2591141512
+#define SIGNATURE_HASH 3865719003
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2160,7 +2160,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.03.25.alphax-8chan_f7.2591141512"
+#define TS_SIGNATURE "rusEFI master.2026.03.26.alphax-8chan_f7.3865719003"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
