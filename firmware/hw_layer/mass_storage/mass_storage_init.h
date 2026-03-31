4e7eb423f3a440acc4bf1fb887f5905103fe3e3e
rusefillc <rusefillc@gmail.com>
2026-03-31 18:51:45 -0400

    only:ECU_IMAGE_INFO
    (cherry picked from commit 865e7e5af20547d5b150b75de4ec4cacbcc36ba1)

--- firmware/hw_layer/mass_storage/mass_storage_init.h
+++ firmware/hw_layer/mass_storage/mass_storage_init.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "hal.h"
+#include "pch.h"
 
 #if HAL_USE_USB_MSD
 void initUsbMsd();
@@ -8,4 +8,22 @@ void attachMsdSdCard(BaseBlockDevice* blkdev, uint8_t *blkbuf, size_t blkbufsize
 void deattachMsdSdCard(void);
 // 'sdinfo' console command support: MSD command/failure counters and SD I/O error counts
 void printMsdDiagnostics();
-#endif
+
+#if EFI_EMBED_INI_MSD
+	#if EFI_USE_COMPRESSED_INI_MSD
+		#include "compressed_block_device.h"
+		#include "ramdisk_image_compressed.h"
+	#else // EFI_USE_COMPRESSED_INI_MSD
+		#include "ramdisk.h"
+		#include "ramdisk_image.h"
+	#endif // EFI_USE_COMPRESSED_INI_MSD
+
+	// If the ramdisk image told us not to use it, don't use it.
+	#ifdef RAMDISK_INVALID
+		#undef EFI_EMBED_INI_MSD
+		#define EFI_EMBED_INI_MSD FALSE
+	#endif // RAMDISK_INVALID
+#endif // EFI_EMBED_INI_MSD
+
+
+#endif // HAL_USE_USB_MSD
