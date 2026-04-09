6d86ea6a8ed626b2659c15a2f39cc36a99ab1c32
FDSoftware <diego+gh@churrosoft.ar>
2026-04-09 16:38:19 -0300

    VariableRegistry: fix escape handling with `quoteReplacement`

--- java_tools/enum_to_string/src/main/java/com/rusefi/VariableRegistry.java
+++ java_tools/enum_to_string/src/main/java/com/rusefi/VariableRegistry.java
@@ -212,8 +212,7 @@ public class VariableRegistry {
                 throw new IllegalStateException("Something broken in: [" + line + "]");
             String key = m.group(2);
             String newValue = function.apply(key);
-            newValue = newValue.replace("\\", "\\\\"); // hack out symbol escaping
-            line = m.replaceFirst(newValue);
+            line = m.replaceFirst(Matcher.quoteReplacement(newValue));
         }
         return line;
     }
