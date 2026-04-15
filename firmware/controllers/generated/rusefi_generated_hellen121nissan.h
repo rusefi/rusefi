8e0193ddde13339fd340982d62de28273e38d158
GitHub gen-configs Action <action@github.com>
2026-04-15 23:27:30 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellen121nissan.h
+++ firmware/controllers/generated/rusefi_generated_hellen121nissan.h
@@ -2173,7 +2173,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.04.15.hellen121nissan.3267955929"
+#define TS_SIGNATURE "rusEFI master.2026.04.16.hellen121nissan.3267955929"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
