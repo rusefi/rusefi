0b270616c61cf8fdc8f9c1a0517c13339fcda82a
Andrey Gusakov <dron0gus@gmail.com>
2026-04-17 11:14:48 +0300

    Nucleo F429/F767: OpenBLT early init hook

--- firmware/config/boards/nucleo_f429/board_configuration.cpp
+++ firmware/config/boards/nucleo_f429/board_configuration.cpp
@@ -18,20 +18,37 @@ Gpio getWarningLedPin() {
 }
 
 static void nucleo_f429_preHalInit() {
-	efiSetPadMode("Ethernet",  Gpio::A1, PAL_MODE_ALTERNATE(0xb));
-	efiSetPadMode("Ethernet",  Gpio::A2, PAL_MODE_ALTERNATE(0xb));
-	efiSetPadMode("Ethernet",  Gpio::A7, PAL_MODE_ALTERNATE(0xb));
-
+	/* TX_EN */
+	efiSetPadMode("Ethernet", Gpio::G11, PAL_MODE_ALTERNATE(0xb));
+	/* TXD0 */
+	efiSetPadMode("Ethernet", Gpio::G13, PAL_MODE_ALTERNATE(0xb));
+	/* TXD1 */
 	efiSetPadMode("Ethernet", Gpio::B13, PAL_MODE_ALTERNATE(0xb));
 
-	efiSetPadMode("Ethernet",  Gpio::C1, PAL_MODE_ALTERNATE(0xb));
+	/* RXD0 */
 	efiSetPadMode("Ethernet",  Gpio::C4, PAL_MODE_ALTERNATE(0xb));
+	/* RXD1 */
 	efiSetPadMode("Ethernet",  Gpio::C5, PAL_MODE_ALTERNATE(0xb));
+	/* CSR DV */
+	efiSetPadMode("Ethernet",  Gpio::A7, PAL_MODE_ALTERNATE(0xb));
 
-	efiSetPadMode("Ethernet", Gpio::G11, PAL_MODE_ALTERNATE(0xb));
-	efiSetPadMode("Ethernet", Gpio::G13, PAL_MODE_ALTERNATE(0xb));
+	/* MDIO */
+	efiSetPadMode("Ethernet",  Gpio::A2, PAL_MODE_ALTERNATE(0xb));
+	/* MDC */
+	efiSetPadMode("Ethernet",  Gpio::C1, PAL_MODE_ALTERNATE(0xb));
+
+	/* REF_CLK */
+	efiSetPadMode("Ethernet",  Gpio::A1, PAL_MODE_ALTERNATE(0xb));
 }
 
 void setup_custom_board_overrides() {
 	custom_board_preHalInit = nucleo_f429_preHalInit;
+}
+
+extern "C" {
+
+void OpenBLT__early_init() {
+	nucleo_f429_preHalInit();
+}
+
 }
