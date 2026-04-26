518fc40418e02d133a53483cc32603d1f7995bd5
GitHub gen-configs Action <action@github.com>
2026-04-26 14:49:34 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellenNA6.h
+++ firmware/controllers/generated/rusefi_generated_hellenNA6.h
@@ -1567,7 +1567,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 1957677750
+#define SIGNATURE_HASH 3845485452
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2176,7 +2176,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.04.26.hellenNA6.1957677750"
+#define TS_SIGNATURE "rusEFI master.2026.04.26.hellenNA6.3845485452"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_super-uaefi.h
+++ firmware/controllers/generated/rusefi_generated_super-uaefi.h
@@ -1566,7 +1566,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 992224140
+#define SIGNATURE_HASH 2864618166
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2175,7 +2175,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.04.26.super-uaefi.992224140"
+#define TS_SIGNATURE "rusEFI master.2026.04.26.super-uaefi.2864618166"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
