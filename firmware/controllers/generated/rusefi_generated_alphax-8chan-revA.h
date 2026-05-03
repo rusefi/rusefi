e4e6cda6359ab89e2351ea77432d36f37c999d9a
GitHub gen-configs Action <action@github.com>
2026-05-03 23:19:03 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-8chan-revA.h
+++ firmware/controllers/generated/rusefi_generated_alphax-8chan-revA.h
@@ -2176,7 +2176,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.05.03.alphax-8chan-revA.526078070"
+#define TS_SIGNATURE "rusEFI master.2026.05.04.alphax-8chan-revA.526078070"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_f429-discovery.h
+++ firmware/controllers/generated/rusefi_generated_f429-discovery.h
@@ -2180,7 +2180,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.05.03.f429-discovery.3437682695"
+#define TS_SIGNATURE "rusEFI master.2026.05.04.f429-discovery.3437682695"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
