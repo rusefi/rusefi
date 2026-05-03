8bbd1e0fb3d30a7fe276868c30f8a32e0b2fa71c
rusefillc <rusefillc@gmail.com>
2026-05-03 19:15:28 -0400

    JSplitButton only:uaefi

---
+++ java_console/ui/src/test/java/com/rusefi/ui/JSplitButtonSandbox.java
@@ -0,0 +1,27 @@
+package com.rusefi.ui;
+
+import com.rusefi.ui.widgets.JSplitButton;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JSplitButtonSandbox {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("JSplitButton Sandbox");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 200);
+        frame.setLayout(new FlowLayout());
+
+        JSplitButton splitButton = new JSplitButton("Default Action");
+        splitButton.addActionListener(e -> System.out.println("Main action clicked!"));
+
+        JPopupMenu menu = new JPopupMenu();
+        menu.add(new JMenuItem("Advanced Action 1")).addActionListener(e -> System.out.println("Advanced 1 clicked!"));
+        menu.add(new JMenuItem("Advanced Action 2")).addActionListener(e -> System.out.println("Advanced 2 clicked!"));
+        splitButton.setPopupMenu(menu);
+
+        frame.add(splitButton);
+
+        frame.setVisible(true);
+    }
+}
