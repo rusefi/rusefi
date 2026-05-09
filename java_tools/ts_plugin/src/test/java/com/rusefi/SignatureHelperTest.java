611665d7d0710c8f8ab337b2fef4271fd9232b92
rusefi <rusefillc@gmail.com>
2026-05-09 10:35:01 -0400

    SignatureHelperTest only:merge

--- java_tools/ts_plugin/src/test/java/com/rusefi/SignatureHelperTest.java
+++ java_tools/ts_plugin/src/test/java/com/rusefi/SignatureHelperTest.java
@@ -1,13 +1,24 @@
 package com.rusefi;
 
+import com.rusefi.core.RusEfiSignature;
 import com.rusefi.core.SignatureHelper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SignatureHelperTest {
     @Test
-    public void test() {
+    public void testUrl() {
         String url = SignatureHelper.getUrl("rusEFI master.2020.07.06.frankenso_na6.2468827536").first;
         Assertions.assertEquals("https://rusefi.com/online/ini/rusefi/master/2020/07/06/frankenso_na6/2468827536.ini", url);
+    }
+
+    @Test
+    public void parseSignature() {
+        RusEfiSignature s = SignatureHelper.parse("rusEFI master.2021.09.22.all.3378169541");
+
+        assertEquals("master", s.getBranch());
+        assertEquals("all", s.getBundleTarget());
     }
 }
