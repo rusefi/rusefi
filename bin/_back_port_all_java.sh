13e4f7625ce59f65ecad68e64aacc5e403e32b5a
rusefillc <rusefillc@gmail.com>
2026-05-11 22:51:31 -0400

    hello script

---
+++ bin/_back_port_all_java.sh
@@ -0,0 +1,9 @@
+# this script is helps to back-port complete master java into some very old branch by just dumping it there
+
+# todo make this bash script
+
+TARGET_DIR=../rusefi.2
+
+remoge all java files in target dir
+
+copy all java
