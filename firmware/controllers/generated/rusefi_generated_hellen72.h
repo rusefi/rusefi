2ef1352ea48699f785893495644af3dac2c1fc6d
GitHub gen-configs Action <action@github.com>
2026-05-20 23:46:15 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellen72.h
+++ firmware/controllers/generated/rusefi_generated_hellen72.h
@@ -2186,7 +2186,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.05.20.hellen72.2166695817"
+#define TS_SIGNATURE "rusEFI master.2026.05.21.hellen72.2166695817"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
