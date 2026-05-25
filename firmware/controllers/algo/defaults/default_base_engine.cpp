9c709ea8f61c48611353cada11af6b7a958aa0bc
rusefillc <rusefillc@gmail.com>
2026-05-25 15:16:59 -0400

    engineShutDownPeriod only:uaefi

--- firmware/controllers/algo/defaults/default_base_engine.cpp
+++ firmware/controllers/algo/defaults/default_base_engine.cpp
@@ -131,6 +131,10 @@ void defaultsOrFixOnBurn() {
     setDynoDefaults();
   }
 
+  if (engineConfiguration->engineShutDownPeriod == 0) {
+    engineConfiguration->engineShutDownPeriod = 5;
+  }
+
 	if (engineConfiguration->mapExpAverageAlpha <= 0 || engineConfiguration->mapExpAverageAlpha > 1) {
 	  engineConfiguration->mapExpAverageAlpha = 1;
 	}
