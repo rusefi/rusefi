6ec22f049b0e3e0d6aeb96ae92527f039f80d1a1
rusefillc <rusefillc@gmail.com>
2026-05-25 11:32:49 -0400

    tooth logger CSV coverage only:uaefi
    (cherry picked from commit 13eb94fa7a7d719f9d3f27d05f6c967e79f68c49)

--- unit_tests/tests/tests.mk
+++ unit_tests/tests/tests.mk
@@ -165,6 +165,7 @@ TESTS_SRC_CPP = \
 	tests/test_hpfp_integrated.cpp \
 	tests/test_fuel_math.cpp \
 	tests/test_binary_log.cpp \
+	tests/test_tooth_logger.cpp \
 	tests/binary_log/test_bit_logger_field.cpp \
 	tests/test_dynoview.cpp \
 	tests/test_gpio.cpp \
