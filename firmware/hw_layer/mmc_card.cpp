7ba1f7de56ac7b71a9ab41ab585681ab20253b16
Andrey Gusakov <dron0gus@gmail.com>
2026-05-26 16:50:41 +0300

    mmc: properly close logBuffer

--- firmware/hw_layer/mmc_card.cpp
+++ firmware/hw_layer/mmc_card.cpp
@@ -682,6 +682,7 @@ static void sdLoggerStart()
 
 static void sdLoggerStop()
 {
+	logBuffer.stop();
 	sdLoggerCloseFile(&resources.fd);
 #if EFI_TOOTH_LOGGER
 	// TODO: pick this config option from cached
