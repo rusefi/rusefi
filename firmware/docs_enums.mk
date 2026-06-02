4efbc421b3946d89b6c65c0b62e792c7e1108428
FDSoftware <diego+gh@churrosoft.ar>
2026-06-02 17:50:32 -0300

    missing docs_enums txt definition

--- firmware/docs_enums.mk
+++ firmware/docs_enums.mk
@@ -12,6 +12,7 @@ DOCS_ENUMS_INPUTS = \
   $(PROJECT_DIR)/controllers/long_term_fuel_trim_state.txt \
   $(PROJECT_DIR)/controllers/math/short_term_fuel_trim_state.txt \
   $(PROJECT_DIR)/controllers/trigger/trigger_state.txt \
+  $(PROJECT_DIR)/controllers/trigger/trigger_state_primary.txt \
   $(PROJECT_DIR)/controllers/algo/shift_torque_reduction_state.txt \
   $(PROJECT_DIR)/controllers/engine_cycle/high_pressure_fuel_pump.txt \
   $(PROJECT_DIR)/controllers/actuators/idle_state.txt \
