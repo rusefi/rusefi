c29afb2e6ca74f1b1191c4a278bdbd7bc37a5f0d
rusefillc <rusefillc@gmail.com>
2026-06-03 15:29:49 -0400

    more clean? only:git

--- misc/git_scripts/git_super_clean.sh
+++ misc/git_scripts/git_super_clean.sh
@@ -3,4 +3,4 @@ git reset --hard
 # we are avoiding '--recursive' since we do not really need complete git dependency tree
 # for instance wideband/ext/openblt is not needed
 git submodule update --init
-git clean -fxd
+git clean -ffxd
