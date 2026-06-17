39c89c379b4a9adaf23d4923d2bd84eb9829d076
GitHub gen-configs Action <action@github.com>
2026-06-17 12:49:37 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-4chan_f7.h
+++ firmware/controllers/generated/rusefi_generated_alphax-4chan_f7.h
@@ -1588,7 +1588,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2970632535
+#define SIGNATURE_HASH 3859969531
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2205,7 +2205,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.06.17.alphax-4chan_f7.2970632535"
+#define TS_SIGNATURE "rusEFI master.2026.06.17.alphax-4chan_f7.3859969531"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_hellen72.h
+++ firmware/controllers/generated/rusefi_generated_hellen72.h
@@ -1591,7 +1591,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 987255211
+#define SIGNATURE_HASH 1843029255
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2208,7 +2208,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.06.17.hellen72.987255211"
+#define TS_SIGNATURE "rusEFI master.2026.06.17.hellen72.1843029255"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
