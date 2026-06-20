ba634fb2a73458182e3db9cbaef1e3a29dfd9ba7
GitHub gen-configs Action <action@github.com>
2026-06-20 14:47:31 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_proteus_f7.h
+++ firmware/controllers/generated/rusefi_generated_proteus_f7.h
@@ -1593,7 +1593,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 1834451984
+#define SIGNATURE_HASH 1422830224
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2211,7 +2211,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.06.20.proteus_f7.1834451984"
+#define TS_SIGNATURE "rusEFI master.2026.06.20.proteus_f7.1422830224"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
