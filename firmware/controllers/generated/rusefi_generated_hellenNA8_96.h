a617c6619a31a80593794e1989f6f04ab795b46f
GitHub gen-configs Action <action@github.com>
2026-06-27 03:08:34 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellenNA8_96.h
+++ firmware/controllers/generated/rusefi_generated_hellenNA8_96.h
@@ -1607,7 +1607,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2952544969
+#define SIGNATURE_HASH 1630085561
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2233,7 +2233,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.06.27.hellenNA8_96.2952544969"
+#define TS_SIGNATURE "rusEFI master.2026.06.27.hellenNA8_96.1630085561"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
