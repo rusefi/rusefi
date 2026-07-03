0f6c904ab7d0d4b9d40827a56f649a7cee06789e
rusefillc <rusefillc@gmail.com>
2026-07-03 18:41:39 -0400

    only:back-port

--- firmware/controllers/trigger/trigger_decoder.cpp
+++ firmware/controllers/trigger/trigger_decoder.cpp
@@ -245,6 +245,7 @@ angle_t PrimaryTriggerDecoder::syncEnginePhase(int divider, int remainder, angle
 	m_hasSynchronizedPhase = true;
 
 	if (totalShift > 0) {
+		m_phaseAdjustment = totalShift;
 		camResyncCounter++;
 		onTransitionEvent(TransitionEvent::EngineResync);
 	}
