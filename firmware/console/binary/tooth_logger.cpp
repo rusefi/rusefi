2e347493164e0f6ab8c1941cd808c4ad36d2d787
rusefillc <rusefillc@gmail.com>
2026-07-04 11:19:49 -0400

    instantMAPValue only:uaefi

--- firmware/console/binary/tooth_logger.cpp
+++ firmware/console/binary/tooth_logger.cpp
@@ -504,7 +504,7 @@ int ToothLoggerWriter(FileBufferedWriter &writer) {
 int ToothLoggerWriteCsvHeader(Writer &writer) {
 	// keep in sync with composite_logger_s
 	// drop trigger - purpose not clear
-	const char header[] = "Time[s], Primary, Cam 1, Cam 2, Cam 3, Cam 4, Sync, TDC, Coils, Injectors, ACR, VBatt, ET\r\n";
+	const char header[] = "Time[s], Primary, Cam 1, Cam 2, Cam 3, Cam 4, Sync, TDC, Coils, Injectors, ACR, VBatt, ET, InstantMAP\r\n";
 
 	// no tailing '\0'
 	writer.write(header, sizeof(header) - 1);
@@ -524,16 +524,17 @@ int ToothLoggerWriteCsv(Writer &writer, CompositeBuffer* buffer) {
 		// todo: take these data points from structure, not current values. Kind of works for slow sensors, but still!
 		float vbatt = Sensor::get(SensorType::BatteryVoltage).value_or(0);
 		float et = Sensor::get(SensorType::Clt).value_or(0);
+		float instantMap = engine->outputChannels.instantMAPValue;
 
 		// it is cheaper to write all data, even we have 1 cylinder engine with single crank sensor
 		int ret = chsnprintf(tmp, sizeof(tmp), "%d.%06d, "
 					"%d, %d, %d, %d, %d, "
 					"%d, %d, "
-					"%d, %d, %d, %.2f, %.2f\r\n",	// TODO: convert to bitwise?
+					"%d, %d, %d, %.2f, %.2f, %.2f\r\n",	// TODO: convert to bitwise?
 				c.timestamp / 1000000, c.timestamp % 1000000,
 				c.priLevel, c.cam1, c.cam2, c.cam3, c.cam4,
 				c.sync, c.tdc,
-				c.coil, c.injector, c.acr, vbatt, et);
+				c.coil, c.injector, c.acr, vbatt, et, instantMap);
 
 		if ((ret < 0) || (ret >= (int)sizeof(tmp))) {
 			return -1;
