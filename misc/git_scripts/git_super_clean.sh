d3e8d76889c88d56134a1209a8f7b6f199d87a19
andrey <andrey@rusefi.com>
2026-07-08 09:33:49 -0400

    only:clean

--- misc/git_scripts/git_super_clean.sh
+++ misc/git_scripts/git_super_clean.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+set -e
+
+cd "$(dirname "$0")/../.."
+
 git reset --hard
 # we are avoiding '--recursive' since we do not really need complete git dependency tree
 # for instance wideband/ext/openblt is not needed
 git submodule update --init
+# remove untracked/ignored files, '-ff' also removes nested git checkouts which are not submodules
 git clean -ffxd
+# top-level 'git clean' does not descend into submodule working trees, clean those explicitly
+git submodule foreach --recursive 'git reset --hard && git clean -ffxd'
