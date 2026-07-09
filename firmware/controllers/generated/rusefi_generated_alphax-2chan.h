0cdd912e226b76254df22cfe20132742567be2be
GitHub gen-configs Action <action@github.com>
2026-07-09 23:06:31 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-2chan.h
+++ firmware/controllers/generated/rusefi_generated_alphax-2chan.h
@@ -1609,7 +1609,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 1106444337
+#define SIGNATURE_HASH 3112626884
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2235,7 +2235,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.07.09.alphax-2chan.1106444337"
+#define TS_SIGNATURE "rusEFI master.2026.07.10.alphax-2chan.3112626884"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_alphax-4chan.h
+++ firmware/controllers/generated/rusefi_generated_alphax-4chan.h
@@ -1609,7 +1609,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 1181324242
+#define SIGNATURE_HASH 3189537063
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2235,7 +2235,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.07.09.alphax-4chan.1181324242"
+#define TS_SIGNATURE "rusEFI master.2026.07.10.alphax-4chan.3189537063"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
