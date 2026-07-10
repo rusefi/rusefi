125c9281a2aab3d99fd53a843426fb9d7fd3baa2
Andrey Gusakov <dron0gus@gmail.com>
2026-07-10 10:17:52 +0300

    stm32: h7: kill STM32_NOCACHE_IN_AXI option, make this default

--- firmware/hw_layer/ports/stm32/stm32h7/global_port.h
+++ firmware/hw_layer/ports/stm32/stm32h7/global_port.h
@@ -2,11 +2,7 @@
 #define CCM_OPTIONAL __attribute__((section(".ram5")))
 //TODO: update LD file!
 #define SDRAM_OPTIONAL __attribute__((section(".ram8")))
-#if STM32_NOCACHE_IN_AXI
-	#define NO_CACHE __attribute__((aligned(4))) __attribute__((section(".ram0")))
-#else
-	// SRAM3 is 32k and set to disable dcache
-	#define NO_CACHE __attribute__((aligned(4))) __attribute__((section(".ram3")))
-#endif
+
+#define NO_CACHE __attribute__((aligned(4))) __attribute__((section(".ram0")))
 
 #define BKUP_RAM_NOINIT __attribute__((section(".bkup_ram_noinit")))
