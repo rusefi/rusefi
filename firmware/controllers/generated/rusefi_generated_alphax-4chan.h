75a1dc19612d44bb2c0667842f2b42f7d9aa496c
GitHub gen-configs Action <action@github.com>
2026-07-11 03:43:53 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-4chan.h
+++ firmware/controllers/generated/rusefi_generated_alphax-4chan.h
@@ -2235,7 +2235,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.07.10.alphax-4chan.3189537063"
+#define TS_SIGNATURE "rusEFI master.2026.07.11.alphax-4chan.3189537063"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_hellen154hyundai_f7.h
+++ firmware/controllers/generated/rusefi_generated_hellen154hyundai_f7.h
@@ -2236,7 +2236,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.07.10.hellen154hyundai_f7.3817692197"
+#define TS_SIGNATURE "rusEFI master.2026.07.11.hellen154hyundai_f7.3817692197"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
