02b864622998bb57fa780bbabb44fe9f1a72496f
Jonathon H <fastferrari5555@gmail.com>
2026-07-18 04:02:36 -0400

    build2_MIL1+BiasResistor

--- java_console/models/src/main/java/com/rusefi/config/generated/VariableRegistryValues.java
+++ java_console/models/src/main/java/com/rusefi/config/generated/VariableRegistryValues.java
@@ -1837,7 +1837,7 @@ public class VariableRegistryValues {
 	public static final int TS_RESPONSE_UNRECOGNIZED_COMMAND = 0x83;
 	public static final int TS_SCATTER_OFFSETS_COUNT = 128;
 	public static final char TS_SET_LOGGER_SWITCH = 'l';
-	public static final String TS_SIGNATURE = "rusEFI master.2026.07.18.uaefi_pro.2628461124";
+	public static final String TS_SIGNATURE = "rusEFI master.2026.07.18.uaefi_pro.2331072539";
 	public static final char TS_SIMULATE_CAN = '>';
 	public static final char TS_TEST_COMMAND = 't';
 	public static final int TS_TOTAL_OUTPUT_SIZE = 2192;
