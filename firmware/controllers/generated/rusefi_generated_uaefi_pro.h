f13642ab81b27bbdd7957f72103c74c586424fb4
Jonathon H <fastferrari5555@gmail.com>
2026-07-18 04:11:52 -0400

    build2_MIL2+BiasResistor

--- firmware/controllers/generated/rusefi_generated_uaefi_pro.h
+++ firmware/controllers/generated/rusefi_generated_uaefi_pro.h
@@ -1624,7 +1624,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2331072539
+#define SIGNATURE_HASH 1957626555
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2034,7 +2034,7 @@
 #define ts_show_can_wbo_type true
 #define ts_show_can_weird true
 #define ts_show_charge_estimation true
-#define ts_show_check_engine false
+#define ts_show_check_engine true
 #define ts_show_clt true
 #define ts_show_clt_iat_pullup true
 #define ts_show_clutch_down true
@@ -2262,7 +2262,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.07.18.uaefi_pro.2331072539"
+#define TS_SIGNATURE "rusEFI master.2026.07.18.uaefi_pro.1957626555"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- java_console/models/src/main/java/com/rusefi/config/generated/VariableRegistryValues.java
+++ java_console/models/src/main/java/com/rusefi/config/generated/VariableRegistryValues.java
@@ -1512,6 +1512,7 @@ public class VariableRegistryValues {
 	public static final int SentInput_INPUT6 = 6;
 	public static final int SentInput_INPUT7 = 7;
 	public static final int SentInput_NONE = 0;
+	public static final int SIGNATURE_HASH = 1957626555;
 	public static final String SIMULATOR_TUNE_BIN_FILE_NAME = "generated/simulator_tune_image.bin";
 	public static final String SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX = "generated/simulator_tune_image";
 	public static final String SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX = ".bin";
@@ -1837,7 +1838,7 @@ public class VariableRegistryValues {
 	public static final int TS_RESPONSE_UNRECOGNIZED_COMMAND = 0x83;
 	public static final int TS_SCATTER_OFFSETS_COUNT = 128;
 	public static final char TS_SET_LOGGER_SWITCH = 'l';
-	public static final String TS_SIGNATURE = "rusEFI master.2026.07.18.uaefi_pro.2331072539";
+	public static final String TS_SIGNATURE = "rusEFI master.2026.07.18.uaefi_pro.1957626555";
 	public static final char TS_SIMULATE_CAN = '>';
 	public static final char TS_TEST_COMMAND = 't';
 	public static final int TS_TOTAL_OUTPUT_SIZE = 2192;
