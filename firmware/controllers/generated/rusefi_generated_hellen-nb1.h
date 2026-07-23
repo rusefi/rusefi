8f300be48c9ee3e52fe24e94bb90303b81b5058a
GitHub gen-configs Action <action@github.com>
2026-07-23 13:27:56 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellen-nb1.h
+++ firmware/controllers/generated/rusefi_generated_hellen-nb1.h
@@ -2268,7 +2268,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.07.22.hellen-nb1.1072226938"
+#define TS_SIGNATURE "rusEFI master.2026.07.23.hellen-nb1.1072226938"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_hellen128.h
+++ firmware/controllers/generated/rusefi_generated_hellen128.h
@@ -2268,7 +2268,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.07.22.hellen128.1836114346"
+#define TS_SIGNATURE "rusEFI master.2026.07.23.hellen128.1836114346"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
