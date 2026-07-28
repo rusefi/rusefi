6d1ab8e1971c64b9f001d11bbef1932b5fa9712b
GitHub gen-configs Action <action@github.com>
2026-07-28 04:45:10 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_f407-discovery.h
+++ firmware/controllers/generated/rusefi_generated_f407-discovery.h
@@ -2265,7 +2265,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.07.27.f407-discovery.3442241841"
+#define TS_SIGNATURE "rusEFI master.2026.07.28.f407-discovery.3442241841"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_f429-discovery.h
+++ firmware/controllers/generated/rusefi_generated_f429-discovery.h
@@ -2269,7 +2269,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.07.27.f429-discovery.298274864"
+#define TS_SIGNATURE "rusEFI master.2026.07.28.f429-discovery.298274864"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
