4bd860f0dbdf2e806903093879ae12d7b4ed1d40
GitHub gen-configs Action <action@github.com>
2026-07-30 02:34:05 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_frankenso_na6.h
+++ firmware/controllers/generated/rusefi_generated_frankenso_na6.h
@@ -2268,7 +2268,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.07.29.frankenso_na6.1375836753"
+#define TS_SIGNATURE "rusEFI master.2026.07.30.frankenso_na6.1375836753"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_hellen-honda-k.h
+++ firmware/controllers/generated/rusefi_generated_hellen-honda-k.h
@@ -2269,7 +2269,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.07.29.hellen-honda-k.3360081511"
+#define TS_SIGNATURE "rusEFI master.2026.07.30.hellen-honda-k.3360081511"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
