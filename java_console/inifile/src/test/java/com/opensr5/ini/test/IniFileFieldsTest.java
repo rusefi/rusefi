748faddf1d023275aa8c00e20e94c8f6ac62a8cd
rusefi <rusefillc@gmail.com>
2026-08-03 20:07:44 -0400

    error when updating purple gateway #9993 only:TDB

--- java_console/inifile/src/test/java/com/opensr5/ini/test/IniFileFieldsTest.java
+++ java_console/inifile/src/test/java/com/opensr5/ini/test/IniFileFieldsTest.java
@@ -9,8 +9,10 @@ import com.opensr5.ini.field.IniField;
 import com.rusefi.config.FieldType;
 import com.rusefi.ini.reader.IniFileReaderUtil;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import java.io.ByteArrayInputStream;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -48,6 +50,35 @@ public class IniFileFieldsTest {
         assertEquals(-155, curve.getyAxis().getMin());
         assertEquals(150, curve.getyAxis().getMax());
         assertEquals(12, curve.getyAxis().getStep());
+    }
+
+    @Test
+    public void testCurveAxisWithoutStep() {
+        assertThrows(RuntimeException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                // seen in a real TS project mainController.ini: "yAxis = -5,  5," - min and max only,
+                // trailing comma, no step value
+                String string =
+                    "[Constants]\n" +
+                        "page = 1\n" +
+                        "scriptCurve1Bins = array, F32, 4828, [16], \"x\", 1, 0, -10000, 10000, 3\n" +
+                        "scriptCurve1 = array, F32, 4892, [16], \"y\", 1, 0, -10000, 10000, 3\n" +
+                        "[CurveEditor]\n" +
+                        "\tcurve = scriptCurve1, \"Script Curve #1\"\n" +
+                        "\t\txAxis\t\t=  0, 128, 10\n" +
+                        "\t\tyAxis\t\t= -5,  5,\n" +
+                        "\t\txBins\t\t= scriptCurve1Bins\n" +
+                        "\t\tyBins\t\t= scriptCurve1\n";
+                RawIniFile lines = IniFileReaderUtil.read(new ByteArrayInputStream(string.getBytes()));
+                IniFileModel model = IniFileReaderTest.readLines(lines);
+
+                CurveModel curve = model.getCurves().get("scriptCurve1");
+                assertNotNull(curve);
+                assertEquals(-5, curve.getyAxis().getMin());
+                assertEquals(5, curve.getyAxis().getMax());
+            }
+        });
     }
 
     @Test
