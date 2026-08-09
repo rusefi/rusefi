33a3cde5f8377c399f66af7c2897e534a93e1da4
rusefillc <rusefillc@gmail.com>
2026-08-09 11:12:01 -0400

    as-is only:human

--- firmware/controllers/lua/examples/cantcu.lua
+++ firmware/controllers/lua/examples/cantcu.lua
@@ -1,48 +1,33 @@
--- todo: add documentation
--- todo: code formatting and comments?
-
-inter = 0
-cut = 0
 blip = 0
 blipout = 0
+cut = 0
+cut2 = 0
 cutout = 0
 
-canRxAdd(0x301) -- cantcu cut blip intervention
+canRxAdd(0x301)
+
+setTickRate(100)
 
 function onCanRx(bus, id, dlc, data)
-if id == 0x301 then
-		inter = data [6]
-		cut = data [7]
-		blip = data [8]
-	end
-end
 
-function Trans()
-	rpm = getSensor("RPM")
-	if inter == 1 then
-		blipout = table3d(1, rpm, blip)
-	else
-		blipout = 0
-	end
-	if inter == 2 then
+	if id == 0x301 then
+		inter = data [6]
+
+		cut = data [7]
 		cut2 = curve(1, cut)
 		cutout = - cut2
-	else
-		cutout = 0
-	end
-	if inter == 0 then
-		blipout = 0
-		cutout = 0
+		setTimingAdd(cutout)
+
+		blip = data [8]
+		blipout = 0.3 * blip
+		setEtbAdd(blipout)
 	end
 
 end
-
-setTickRate(50)
 
 function onTick()
 
-	Trans()
-        setEtbAdd(blipout)
-	setTimingAdd(cutout)
-
+	print('cut: ' ..cutout)
+	print('blip: ' ..blip)
+	print('blipout:' ..blipout)
 end
