31ee9a9c27af54b525b24a4b42b2a5b792e30548
romioRN <128554106+romioRN@users.noreply.github.com>
2026-08-13 11:03:20 +0300

    Update rusefi_enums.h

--- firmware/controllers/algo/rusefi_enums.h
+++ firmware/controllers/algo/rusefi_enums.h
@@ -140,11 +140,13 @@ typedef enum  __attribute__ ((__packed__)) {
 
   VVT_MITSUBISHI_6G75 = 31,
 
-  VVT_3GR_FSE = 32,
+  VVT_CUSTAM_3 = 32,
 
   VVT_CUSTOM_4 = 33,
 
   VVT_CUSTOM_5 = 34,
+    
+  VVT_3GR_FSE = 35,
 
 } vvt_mode_e;
 
