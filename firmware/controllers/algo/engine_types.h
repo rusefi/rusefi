f0023f90a809619f4812c23c1150a3e3fabb5bc8
romioRN <128554106+romioRN@users.noreply.github.com>
2026-08-13 10:44:41 +0300

    Update engine_types.h

--- firmware/controllers/algo/engine_types.h
+++ firmware/controllers/algo/engine_types.h
@@ -278,7 +278,8 @@ enum class trigger_type_e : uint32_t {
 	//
 	// Another point: once you add a new trigger, run get_trigger_images.bat which would run rusefi_test.exe from unit_tests
 	//
-	TT_UNUSED = 99, // this is used if we want to iterate over all trigger types
+  TT_VVT_3GR_FSE = 99,
+	TT_UNUSED = 100, // this is used if we want to iterate over all trigger types
 };
 
 typedef enum {
