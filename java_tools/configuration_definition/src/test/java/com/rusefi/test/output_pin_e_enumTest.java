961e9a89e61dc0a15edd6b09981251952cb820b7
Diego <diego+gh@churrosoft.ar>
2026-08-17 13:22:18 -0300

    h-bridge as gpio (#10104)
    * h-bridge as gpio
    
    * better wording

--- java_tools/configuration_definition/src/test/java/com/rusefi/test/output_pin_e_enumTest.java
+++ java_tools/configuration_definition/src/test/java/com/rusefi/test/output_pin_e_enumTest.java
@@ -4,9 +4,6 @@ import com.rusefi.VariableRegistry;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.Arrays;
-
-import static com.rusefi.VariableRegistry.unquote;
 import static com.rusefi.AssertCompatibility.assertEquals;
 
 public class output_pin_e_enumTest {
@@ -14,7 +11,9 @@ public class output_pin_e_enumTest {
     public void test() throws IOException {
         VariableRegistry r = ConfigDefinitionTest.readRealConfig();
         String packed = r.get("output_pin_e_enum");
-        String[] array = packed.split("\\s+");
-        assertEquals(unquote(array[287+32]), "MC33810_0_GD_3");
+        String[] array = packed.replace("\"", "").split(",\\s*");
+        assertEquals(array[319], "MC33810_0_GD_3");
+        assertEquals(array[361], "HBRIDGE_1_OUT");
+        assertEquals(array[362], "HBRIDGE_2_OUT");
     }
 }
