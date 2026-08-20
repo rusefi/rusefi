8f76f162a82cc8d04d756bb13c3cb65d71278062
FDSoftware <diego+gh@churrosoft.ar>
2026-08-20 15:48:09 -0300

    #10117 preparation: Add basic configuration module
    we have existing but confusing `applyActuatorDefaults`, let's expand it to a "basic configuration", so we can setup etb/tps modes from one place, with explicit actions, so the TS side is only a new button
    
    not for now, but if we want an explicit action of "use on board WBO / use Analog WBO" or something that touches multiple configs, we can use this new module!

--- unit_tests/tests/tests.mk
+++ unit_tests/tests/tests.mk
@@ -128,6 +128,7 @@ TESTS_SRC_CPP = \
 	tests/lua/test_lua_vin.cpp \
 	tests/lua/test_lua_debounce.cpp \
 	tests/test_change_engine_type.cpp \
+	tests/test_basic_configuration.cpp \
 	tests/test_big_buffer.cpp \
 	tests/system/test_periodic_thread_controller.cpp \
 	tests/system/test_scheduler.cpp \
