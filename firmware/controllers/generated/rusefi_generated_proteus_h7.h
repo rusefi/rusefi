58b94c8ede186e383d5adda3baa787eedeac1b69
GitHub gen-configs Action <action@github.com>
2026-08-24 02:47:40 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_proteus_h7.h
+++ firmware/controllers/generated/rusefi_generated_proteus_h7.h
@@ -1647,7 +1647,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 3495892463
+#define SIGNATURE_HASH 3201559246
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2289,7 +2289,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.08.23.proteus_h7.3495892463"
+#define TS_SIGNATURE "rusEFI master.2026.08.24.proteus_h7.3201559246"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_uaefi_pro_h7.h
+++ firmware/controllers/generated/rusefi_generated_uaefi_pro_h7.h
@@ -1642,7 +1642,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 477528856
+#define SIGNATURE_HASH 1929018425
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2285,7 +2285,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.08.23.uaefi_pro_h7.477528856"
+#define TS_SIGNATURE "rusEFI master.2026.08.24.uaefi_pro_h7.1929018425"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
