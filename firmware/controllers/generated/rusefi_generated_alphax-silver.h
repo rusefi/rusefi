f467d218b8a5950f1fd246d771c476c883cbea78
GitHub gen-configs Action <action@github.com>
2026-08-29 16:50:01 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-silver.h
+++ firmware/controllers/generated/rusefi_generated_alphax-silver.h
@@ -1644,7 +1644,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 1680621428
+#define SIGNATURE_HASH 3722912612
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2056,6 +2056,7 @@
 #define ts_show_can_wbo true
 #define ts_show_can_wbo_type true
 #define ts_show_can_weird true
+#define ts_show_canbus3 false
 #define ts_show_canbus_sniffer false
 #define ts_show_charge_estimation true
 #define ts_show_check_engine true
@@ -2286,7 +2287,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.08.29.alphax-silver.1680621428"
+#define TS_SIGNATURE "rusEFI master.2026.08.29.alphax-silver.3722912612"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_subaru_eg33_f7.h
+++ firmware/controllers/generated/rusefi_generated_subaru_eg33_f7.h
@@ -1647,7 +1647,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 4015815118
+#define SIGNATURE_HASH 1452762590
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2059,6 +2059,7 @@
 #define ts_show_can_wbo true
 #define ts_show_can_wbo_type true
 #define ts_show_can_weird true
+#define ts_show_canbus3 false
 #define ts_show_canbus_sniffer false
 #define ts_show_charge_estimation true
 #define ts_show_check_engine false
@@ -2290,7 +2291,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.08.29.subaru_eg33_f7.4015815118"
+#define TS_SIGNATURE "rusEFI master.2026.08.29.subaru_eg33_f7.1452762590"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
