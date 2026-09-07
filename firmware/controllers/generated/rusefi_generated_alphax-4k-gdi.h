84fbaa066040e566d0feddc96de0daa64baa4a61
GitHub gen-configs Action <action@github.com>
2026-09-07 23:39:46 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-4k-gdi.h
+++ firmware/controllers/generated/rusefi_generated_alphax-4k-gdi.h
@@ -2286,7 +2286,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.09.07.alphax-4k-gdi.798323835"
+#define TS_SIGNATURE "rusEFI master.2026.09.08.alphax-4k-gdi.798323835"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_small-can-board.h
+++ firmware/controllers/generated/rusefi_generated_small-can-board.h
@@ -2290,7 +2290,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.09.07.small-can-board.3557862155"
+#define TS_SIGNATURE "rusEFI master.2026.09.08.small-can-board.3557862155"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
