f5ac1d94817b32f990c886481e98b7fe345c1fba
GitHub gen-configs Action <action@github.com>
2026-09-07 15:52:26 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_mre_f4.h
+++ firmware/controllers/generated/rusefi_generated_mre_f4.h
@@ -1646,7 +1646,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2085932984
+#define SIGNATURE_HASH 4017985636
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2290,7 +2290,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.09.07.mre_f4.2085932984"
+#define TS_SIGNATURE "rusEFI master.2026.09.07.mre_f4.4017985636"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
