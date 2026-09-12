ba28a79a7a6506bb1f6a7440455d5cf032bbc588
GitHub gen-configs Action <action@github.com>
2026-09-12 23:08:06 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_alphax-silver.h
+++ firmware/controllers/generated/rusefi_generated_alphax-silver.h
@@ -2288,7 +2288,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.09.12.alphax-silver.2738128567"
+#define TS_SIGNATURE "rusEFI master.2026.09.13.alphax-silver.2738128567"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_uaefi.h
+++ firmware/controllers/generated/rusefi_generated_uaefi.h
@@ -2287,7 +2287,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.09.12.uaefi.798797155"
+#define TS_SIGNATURE "rusEFI master.2026.09.13.uaefi.798797155"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
