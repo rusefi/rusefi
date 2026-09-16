24319d9cf6ce411776447344df4e39e7812419d5
GitHub gen-configs Action <action@github.com>
2026-09-16 09:13:47 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellen121vag.h
+++ firmware/controllers/generated/rusefi_generated_hellen121vag.h
@@ -1647,7 +1647,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 701944085
+#define SIGNATURE_HASH 685356703
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2292,7 +2292,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.09.16.hellen121vag.701944085"
+#define TS_SIGNATURE "rusEFI master.2026.09.16.hellen121vag.685356703"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
