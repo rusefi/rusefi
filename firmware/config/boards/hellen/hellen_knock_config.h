f9f52bb2e61bfd82fde917eb3af3a675e001224a
Andrey Gusakov <dron0gus@gmail.com>
2026-09-17 16:54:17 +0300

    hellen: knock conig for H7

--- firmware/config/boards/hellen/hellen_knock_config.h
+++ firmware/config/boards/hellen/hellen_knock_config.h
@@ -8,9 +8,19 @@
 #define KNOCK_ADC ADCD3
 
 // knock 1 - pin PF7
-#define KNOCK_ADC_CH1 ADC_CHANNEL_IN5
 #define KNOCK_PIN_CH1 Gpio::F7
 
 // Sample rate & time - depends on the exact MCU
-#define KNOCK_SAMPLE_TIME ADC_SAMPLE_84
-#define KNOCK_SAMPLE_RATE (STM32_PCLK2 / (4 * (84 + 12)))
+#if defined(STM32H7XX)
+	// H7
+	#define KNOCK_ADC_CH1 3
+
+	#define H7_KNOCK_OVERSAMPLE 4
+	#define KNOCK_SAMPLE_TIME ADC_SMPR_SMP_8P5
+	#define KNOCK_SAMPLE_RATE (STM32_ADC3_CLOCK / (H7_KNOCK_OVERSAMPLE * (8 + 8)))
+#else
+	#define KNOCK_ADC_CH1 ADC_CHANNEL_IN5
+
+	#define KNOCK_SAMPLE_TIME ADC_SAMPLE_84
+	#define KNOCK_SAMPLE_RATE (STM32_PCLK2 / (4 * (84 + 12)))
+#endif
