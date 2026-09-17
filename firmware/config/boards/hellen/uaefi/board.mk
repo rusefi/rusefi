ee0f120b84011b1c1d46bb0811b8ed0d3eb56cb0
Andrey Gusakov <dron0gus@gmail.com>
2026-09-17 17:33:15 +0300

    uaefi_h7: knock comment

--- firmware/config/boards/hellen/uaefi/board.mk
+++ firmware/config/boards/hellen/uaefi/board.mk
@@ -36,7 +36,8 @@ ifeq ($(PROJECT_CPU),ARCH_STM32H7)
 	# We have enough USB endpoints for one more CDC ACM, lets use it for CAN sniffer
 	DDEFS += -DHAL_USE_USB_CDC_2=TRUE
 else
-	#Knock is available on F4 and F7 only
+	# Knock is available on F4, F7 and H74x/H75x while this board is populated with H723
+	# H723 has ADC3 of other type and there is no ChibiOS driver
 	DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE -DSTM32_ADC_USE_ADC3=TRUE
 endif
 
