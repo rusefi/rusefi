1359b3a2d23baa33d8a3649a229a64fb739a99aa
rusefi <rusefillc@gmail.com>
2026-09-16 23:55:59 -0400

    stm32h7

--- firmware/hw_layer/ports/stm32/stm32h7/mpu_util.cpp
+++ firmware/hw_layer/ports/stm32/stm32h7/mpu_util.cpp
@@ -10,7 +10,8 @@
 #include "flash_int.h"
 
 bool mcuCanFlashWhileRunning() {
-#ifdef STM32H723xx
+#if defined(STM32H723xx) || EFI_FLASH_USE_1500_OF_2MB
+	// The larger H743 image shares bank 2 with settings, so erases can stall code.
 	return false;
 #else
 	return true;
@@ -28,6 +29,9 @@ static_assert(sizeof(persistent_config_container_s) <= 128 * 1024, "Settings doe
 uintptr_t getFlashAddrFirstCopy() {
 #ifdef STM32H723xx
 	return 0x080E0000;
+#elif EFI_FLASH_USE_1500_OF_2MB
+	// Right after the first 1.5 MiB, matching 2mb_flash.mk and STM32H743xI.ld.
+	return 0x08180000;
 #else
 	return 0x08100000;
 #endif
