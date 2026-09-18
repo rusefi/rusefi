2eb03eaeddd7ea6783112cbd7407c25d15004ed9
thiagomucci <thmucc@protonmail.com>
2026-09-18 17:03:58 -0400

    fix: inhibit PWM fan activation when engine is stopped

--- firmware/controllers/modules/fan_control/fan_control.cpp
+++ firmware/controllers/modules/fan_control/fan_control.cpp
@@ -112,6 +112,12 @@ void FanController::onSlowCallbackPwm(bool acActive) {
 		target += getPwmAcAdder();
 	}
 	target = clampF(getMinPwm(), target, getMaxPwm());
+
+	if (disableWhenStopped() && !engine->rpmCalculator.isRunning()) {
+		target = 0;
+		m_currentPwm = 0;
+	}
+
 	pwmTargetPwm = target;
 
 	// Soft-start: limit upward slew rate so ramp from 0-100 takes softStartSec seconds
