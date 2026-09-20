304281707af4cbe6451f4a750d89d5a4d258cd3e
FDSoftware <diego+gh@churrosoft.ar>
2026-09-20 20:45:56 -0300

    fix WUE analyzer table aliases and page offsets

--- java_tools/configuration_definition/src/test/java/com/rusefi/test/TestTSProjectConsumer.java
+++ java_tools/configuration_definition/src/test/java/com/rusefi/test/TestTSProjectConsumer.java
@@ -1,7 +1,11 @@
 package com.rusefi.test;
 
 import com.rusefi.ReaderStateImpl;
+import com.rusefi.TsFileContent;
 import com.rusefi.output.TSProjectConsumer;
+import com.rusefi.util.Output;
+
+import java.io.IOException;
 
 public class TestTSProjectConsumer extends TSProjectConsumer {
     public TestTSProjectConsumer(ReaderStateImpl state) {
@@ -15,5 +19,20 @@ public class TestTSProjectConsumer extends TSProjectConsumer {
     // exposes the protected page-table emission for unit testing
     public void registerTsPagesBlockForTest() {
         registerTsPagesBlock();
+    }
+
+    public String writeContentForTest(String fieldsSection, TsFileContent tsContent) throws IOException {
+        StringBuilder result = new StringBuilder();
+        writeContent(fieldsSection, tsContent, new Output() {
+            @Override
+            public void write(String line) {
+                result.append(line);
+            }
+
+            @Override
+            public void close() {
+            }
+        });
+        return result.toString();
     }
 }
