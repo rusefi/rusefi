d3bc1bdb2450f09cc620cab580c778b34e58a3d0
GitHub gen-configs Action <action@github.com>
2026-09-21 15:47:34 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellen-honda-k.h
+++ firmware/controllers/generated/rusefi_generated_hellen-honda-k.h
@@ -1646,7 +1646,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 1741316110
+#define SIGNATURE_HASH 3585700498
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2293,7 +2293,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.09.21.hellen-honda-k.1741316110"
+#define TS_SIGNATURE "rusEFI master.2026.09.21.hellen-honda-k.3585700498"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- firmware/controllers/generated/rusefi_generated_stm32f429_nucleo.h
+++ firmware/controllers/generated/rusefi_generated_stm32f429_nucleo.h
@@ -1651,7 +1651,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2467567966
+#define SIGNATURE_HASH 560401346
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2296,7 +2296,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.09.21.stm32f429_nucleo.2467567966"
+#define TS_SIGNATURE "rusEFI master.2026.09.21.stm32f429_nucleo.560401346"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
