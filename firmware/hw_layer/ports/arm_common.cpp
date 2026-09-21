0f1280380192dd7064ebe943812f413a155dc93b
rusefillc <rusefillc@gmail.com>
2026-09-21 17:43:19 -0400

    assertInterruptPriority

--- firmware/hw_layer/ports/arm_common.cpp
+++ firmware/hw_layer/ports/arm_common.cpp
@@ -11,6 +11,17 @@
 
 #if EFI_PROD_CODE
 #include "mpu_util.h"
+
+void assertInterruptPriority(const char* func, uint8_t expectedPrio) {
+	auto isr = static_cast<uint8_t>(SCB->ICSR & SCB_ICSR_VECTACTIVE_Msk) - 16;
+
+	auto actualMask = NVIC->IP[isr];
+	auto expectedMask = NVIC_PRIORITY_MASK(expectedPrio);
+
+	if (actualMask != expectedMask) {
+		firmwareError(ObdCode::RUNTIME_CRITICAL_WRONG_IRQ_PRIORITY, "bad isr priority at %s expected %02x got %02x", func, expectedMask, actualMask);
+	}
+}
 #endif  /* EFI_PROD_CODE */
 
 void causeHardFault() {
--- firmware/hw_layer/ports/stm32/stm32_common.cpp
+++ firmware/hw_layer/ports/stm32/stm32_common.cpp
@@ -321,17 +321,6 @@ PUBLIC_API_WEAK void boardPrepareForStandby() {
 	boardPreparePA0ForStandby();
 }
 
-void assertInterruptPriority(const char* func, uint8_t expectedPrio) {
-	auto isr = static_cast<uint8_t>(SCB->ICSR & SCB_ICSR_VECTACTIVE_Msk) - 16;
-
-	auto actualMask = NVIC->IP[isr];
-	auto expectedMask = NVIC_PRIORITY_MASK(expectedPrio);
-
-	if (actualMask != expectedMask) {
-		firmwareError(ObdCode::RUNTIME_CRITICAL_WRONG_IRQ_PRIORITY, "bad isr priority at %s expected %02x got %02x", func, expectedMask, actualMask);
-	}
-}
-
 const char *getStm32McuName(int mcuRevision) {
   switch (mcuRevision) {
     case STM32_DEVICE_ID_F40x: // x413
