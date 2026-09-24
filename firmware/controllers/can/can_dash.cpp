64da069dfc03bfd79c21f0b97282b269063cfcc1
bugrow <65629970+bugrow@users.noreply.github.com>
2026-09-24 17:52:33 +0400

    Update can_dash.cpp
    CAN rx8 CLT offset "+57"

--- firmware/controllers/can/can_dash.cpp
+++ firmware/controllers/can/can_dash.cpp
@@ -154,7 +154,7 @@ void canMazdaRX8(CanCycle cycle) {
 		{
    CanTxMessage msg(CanCategory::NBC, CAN_MAZDA_RX_STATUS_2, 8, DEFAULT_BUS_INDEX);
 			auto clt = Sensor::get(SensorType::Clt);
-			msg[0] = (uint8_t)(clt.value_or(0) + 69); //temp gauge //~170 is red, ~165 last bar, 152 centre, 90 first bar, 92 second bar
+			msg[0] = (uint8_t)(clt.value_or(0) + 57); //temp gauge //~170 is red, ~165 last bar, 152 centre, 90 first bar, 92 second bar
 			// TODO: fixme!
 			//msg[1] = ((int16_t)(engine->engineState.vssEventCounter*(engineConfiguration->vehicleSpeedCoef*0.277*2.58))) & 0xff;
 			msg[2] = 0x00; // unknown
