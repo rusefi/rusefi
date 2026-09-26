0352ae17fe5fcf26716909e77e30cb9ae24048d4
GitHub gen-configs Action <action@github.com>
2026-09-26 02:48:01 +0000

    Auto-generated configs and docs

--- firmware/controllers/generated/rusefi_generated_hellen-honda-k.h
+++ firmware/controllers/generated/rusefi_generated_hellen-honda-k.h
@@ -2293,7 +2293,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI master.2026.09.25.hellen-honda-k.625634418"
+#define TS_SIGNATURE "rusEFI master.2026.09.26.hellen-honda-k.625634418"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
